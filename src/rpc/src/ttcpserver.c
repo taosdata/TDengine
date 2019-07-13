d0c37df8bb1866d62fa6dfeb6948f6ecf621eb8f
slguan <slguan@taosdata.com>
2019-07-13 10:15:40 +0800

    Change the free function to tfree

--- src/rpc/src/ttcpserver.c
+++ src/rpc/src/ttcpserver.c
@@ -199,7 +199,7 @@ static void taosProcessTcpData(void *param) {
       if (headLen != sizeof(STaosHeader)) {
         tError("%s read error, headLen:%d", pThreadObj->label, headLen);
         taosCleanUpFdObj(pFdObj);
-        free(buffer);
+        tfree(buffer);
         continue;
       }
 
@@ -215,7 +215,7 @@ static void taosProcessTcpData(void *param) {
       if (leftLen != retLen) {
         tError("%s read error, leftLen:%d retLen:%d", pThreadObj->label, leftLen, retLen);
         taosCleanUpFdObj(pFdObj);
-        free(buffer);
+        tfree(buffer);
         continue;
       }
 
