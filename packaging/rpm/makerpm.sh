2a36659e10b44ad8f4c571a5aecc5e1f22c62978
plum-lihui <2849823933@qq.com>
2019-07-19 15:39:44 +0800

    [Modify the use of sudo commands]

--- packaging/rpm/makerpm.sh
+++ packaging/rpm/makerpm.sh
@@ -19,18 +19,23 @@ echo "compile_dir: ${compile_dir}"
 echo "pkg_dir: ${pkg_dir}"
 echo "spec_file: ${spec_file}"
 
-if [ -d ${pkg_dir} ]; then
-	 rm -rf ${pkg_dir}
+csudo=""
+if command -v sudo > /dev/null; then
+    csudo="sudo"
 fi
-mkdir -p ${pkg_dir}
+
+if [ -d ${pkg_dir} ]; then
+	 ${csudo} rm -rf ${pkg_dir}
+fi
+${csudo} mkdir -p ${pkg_dir}
 cd ${pkg_dir}
 
-mkdir -p BUILD BUILDROOT RPMS SOURCES SPECS SRPMS
+${csudo} mkdir -p BUILD BUILDROOT RPMS SOURCES SPECS SRPMS
 
-rpmbuild --define="_version ${tdengine_ver}" --define="_topdir ${pkg_dir}" --define="_compiledir ${compile_dir}" -bb ${spec_file}
+${csudo} rpmbuild --define="_version ${tdengine_ver}" --define="_topdir ${pkg_dir}" --define="_compiledir ${compile_dir}" -bb ${spec_file}
 
 # copy rpm package to output_dir, then clean temp dir
 #echo "rmpbuild end, cur_dir: $(pwd) "
-cp -rf RPMS/* ${output_dir} 
+${csudo} cp -rf RPMS/* ${output_dir} 
 cd ..
-rm -rf ${pkg_dir}
+${csudo} rm -rf ${pkg_dir}
