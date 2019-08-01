b0971e8f0ea990ae28f29c280dd78cf663e368c3
huili <huili@taosdata.com>
2019-08-01 10:59:16 +0800

    [Fixed dsn parsing for network addr and port by caeret. #214]

--- src/connector/go/src/taosSql/taosSqlCgo.go
+++ src/connector/go/src/taosSql/taosSqlCgo.go
@@ -32,9 +32,8 @@ import (
 func (mc *taosConn) taosConnect(ip, user, pass, db string, port int) (taos unsafe.Pointer, err error){
 	cuser := C.CString(user)
 	cpass := C.CString(pass)
-	cip   := C.CString(ip) // TODO: Addr : x.x.x.x:port, must process to ip and port format
+	cip   := C.CString(ip)
 	cdb   := C.CString("")
-	port   = 0
 	defer C.free(unsafe.Pointer(cip))
 	defer C.free(unsafe.Pointer(cuser))
 	defer C.free(unsafe.Pointer(cpass))
