39478de2751e84287a19bbc3fcbe971347019361
slguan <slguan@taosdata.com>
2019-08-02 17:08:53 +0800

    fix the issue #264 #253 #251

--- src/util/src/version.c
+++ src/util/src/version.c
@@ -1,4 +1,4 @@
-char version[64] = "1.6.1.2";
+char version[64] = "1.6.1.3";
 char compatible_version[64] = "1.6.0.0";
 char gitinfo[128] = "ddcb2519e895c2e2101089aedaf529cee5cefe04";
 char buildinfo[512] = "Built by plum at 2019-07-29 10:41";
