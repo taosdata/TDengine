0a3006f2b5e404dfcf0c2ebac504a8a6c71ebf22
fang <fpang@taosdata.com>
2019-08-05 10:41:28 +0800

    alter_examples_bash

--- tests/examples/bash/demo.sql
+++ tests/examples/bash/demo.sql
@@ -1,17 +1,14 @@
 create database if not exists db0
-create table if not exists db0.tb0 (ts timestamp, field int)
+create table if not exists db0.tb0 (ts timestamp, voltage int, current float)
 import into db0.tb0 file demo.csv
+
 
 create database if not exists db1
 use db1
-create table if not exists tb1 (ts timestamp, name binary(8))
-insert into tb1 values('2010-07-23 11:01:02.000', 'xxxx')
-insert into tb1 values(now, 'aaaa');    
-insert into tb1 values(now+1a, 'bbbb'); 
-insert into tb1 values(now+1s, 'cccc'); 
-insert into tb1 values(now+1m, 'dddd'); 
-insert into tb1 values(now+1h, 'eeee'); 
-insert into tb1 values(now+1d, 'ffff'); 
-insert into tb1 values(now+1w, 'gggg');
-insert into tb1 values(now+1n, 'hhhh');
-insert into tb1 values(now+1y, 'iiii');
+create table if not exists tb1 (ts timestamp, temperature int, humidity float)
+insert into tb1 values('2010-07-23 11:01:02.000', 37, 50.1)
+insert into tb1 values(now, 36, 47.8);    
+insert into tb1 values(now+1a, 38, 65.3); 
+insert into tb1 values(now+1s, 38, 53.9 ); 
+insert into tb1 values(now+1m, 37, 45.6); 
+insert into tb1 values(now+1h, 35, 41.1); 
