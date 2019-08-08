1b7944bc4e61172bde676ef0724cdbf8fb7448d3
slguan <slguan@taosdata.com>
2019-08-08 13:53:32 +0800

    the version number is upgraded to 1.6.1.5

--- src/util/src/version.c
+++ src/util/src/version.c
@@ -1,4 +1,4 @@
-char version[64] = "1.6.1.4";
+char version[64] = "1.6.1.5";
 char compatible_version[64] = "1.6.1.0";
-char gitinfo[128] = "36936e19eb26b5e45107bca95394133e3ac7c3a1";
-char buildinfo[512] = "Built by slguan at 2019-08-05 09:24";
+char gitinfo[128] = "a99770cd8aefc1b75f5721905b63460f8fa4fdd6";
+char buildinfo[512] = "Built by ubuntu at 2019-08-08 10:23";
