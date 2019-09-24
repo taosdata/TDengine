2d058e6f856fcc2e5114f100051f6896822d28db
slguan <slguan@taosdata.com>
2019-09-24 22:44:04 +0800

    release version 1.6.2.2

--- src/util/src/version.c
+++ src/util/src/version.c
@@ -1,4 +1,4 @@
-char version[64] = "1.6.2.1";
+char version[64] = "1.6.2.2";
 char compatible_version[64] = "1.6.1.0";
-char gitinfo[128] = "48bba82d33741801c50a48b805ae82d1ca216cc1";
-char buildinfo[512] = "Built by ubuntu at 2019-09-23 09:46";
+char gitinfo[128] = "17cac5b56743b4796181b8ab6754ecac46137558";
+char buildinfo[512] = "Built by ubuntu at 2019-09-24 22:35";
