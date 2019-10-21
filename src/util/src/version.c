eccfeed8f4dde5047a7941e995a9ffa68887f39c
slguan <slguan@taosdata.com>
2019-10-21 19:35:02 +0800

    release version 1.6.3.0

--- src/util/src/version.c
+++ src/util/src/version.c
@@ -1,4 +1,4 @@
-char version[64] = "1.6.2.2";
+char version[64] = "1.6.3.0";
 char compatible_version[64] = "1.6.1.0";
-char gitinfo[128] = "17cac5b56743b4796181b8ab6754ecac46137558";
-char buildinfo[512] = "Built by ubuntu at 2019-09-24 22:35";
+char gitinfo[128] = "49080adeee33c95d1174ac96eca9e62536e2da0f";
+char buildinfo[512] = "Built by ubuntu at 2019-10-21 19:33";
