5d1b1735285a61a33655c5ccedb94978956518e9
hjxilinx <hjxilinx@163.com>
2019-11-04 13:47:29 +0800

    Restore the state of the meter in case of failing to launch the update procedure.

--- src/system/src/vnodeMeter.c
+++ src/system/src/vnodeMeter.c
@@ -752,7 +752,7 @@ void vnodeUpdateMeter(void *param, void *tmrId) {
   vnodeSaveMeterObjToFile(pObj);
   vnodeClearMeterState(pObj, TSDB_METER_STATE_UPDATING);
 
-  dTrace("vid:%d sid:%d id:%s, schema is updated", pNew->vnode, pNew->sid, pNew->meterId);
+  dTrace("vid:%d sid:%d id:%s, schema is updated, state:%d", pObj->vnode, pObj->sid, pObj->meterId, pObj->state);
   free(pNew);
 }
 
