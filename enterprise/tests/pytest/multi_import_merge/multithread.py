97d800578524ac72c7c65a9c880e7f3e12a9fd2e
slguan <slguan@taosdata.com>
2019-11-14 13:05:37 +0800

    [TBASE-1005] import test script

--- enterprise/tests/pytest/multi_import_merge/multithread.py
+++ enterprise/tests/pytest/multi_import_merge/multithread.py
@@ -46,7 +46,7 @@ class TDTestCase:
           else:
             err = 'affected rows %d != expected %d' %(affrows, ninserted)
             print("\033[1;31m%s %s\033[0m\nfailed sqlcmd: %s" \
-                         % (datetime.datetime.now(), err, sqlcmd[1])
+                         % (datetime.datetime.now(), err, sqlcmd[1]))
             ##sys.exit(1) 
             ninserted = 0
             sqlcmd = ['import into']
@@ -57,7 +57,7 @@ class TDTestCase:
         else:
           err = 'affected rows %d != expected %d' %(affrows, ninserted)
           print("\033[1;31m%s %s\033[0m\nfailed sqlcmd: %s" \
-                         % (datetime.datetime.now(), err, sqlcmd[1])
+                         % (datetime.datetime.now(), err, sqlcmd[1]))
           ##sys.exit(1) 
           ninserted = 0
           sqlcmd = ['import into']
@@ -73,7 +73,7 @@ class TDTestCase:
       for line in cursor:
         continue
       print("query %d finished" %count)
-      if (count == 1000):break
+      if (count == 200):break
       count += 1
     conn.close()
 
