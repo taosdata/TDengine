fbca8b353ae91275778c0afe8b25e51085ac5416
fang <fpang@taosdata.com>
2019-11-25 09:48:37 +0800

    [TBASE-1005]

--- enterprise/tests/pytest/util/dnodes.py
+++ enterprise/tests/pytest/util/dnodes.py
@@ -174,6 +174,8 @@ class TDDnode:
 			if os.system(cmd) != 0 :
 				tdLog.exit(cmd)
 			tdLog.debug("dnode:%d is stopped by kill -SIGINT" % (self.index))
+		  tdLog.debug("wait 2 seconds for the dnode:%d to stop." % (self.index))
+		  time.sleep(2)
 			
 	def forcestop(self):
 		if self.running != 0:
@@ -181,6 +183,8 @@ class TDDnode:
 			if os.system(cmd) != 0 :
 				tdLog.exit(cmd)
 			tdLog.debug("dnode:%d is stopped by kill -9" % (self.index))
+		tdLog.debug("wait 2 seconds for the dnode:%d to stop." % (self.index))
+		time.sleep(2)
 			
 	def startIP(self):
 		cmd = "sudo ifconfig lo:%d 192.168.0.%d up" % (self.index, self.index)
