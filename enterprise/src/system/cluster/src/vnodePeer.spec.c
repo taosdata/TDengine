f3bb2f480a6ed844825a4fd027d3f0c5daeac42d
slguan <slguan@taosdata.com>
2019-11-26 23:07:49 +0800

    [TBASE-1226]

--- enterprise/src/system/cluster/src/vnodePeer.spec.c
+++ enterprise/src/system/cluster/src/vnodePeer.spec.c
@@ -556,7 +556,7 @@ void vnodeRestartConnection(SVnodePeer *pVPeer)
     return;
   }
 
-  pthread_mutex_trylock(&(pVnode->vmutex));
+  pthread_mutex_lock(&(pVnode->vmutex));
   dTrace("vid:%d, peer:%s:%d do restart connection", pVPeer->ownId, pVPeer->ipstr, pVPeer->vid);
 
   if (pVPeer->peerFd >= 0) vnodeClosePeerFd(pVPeer);
