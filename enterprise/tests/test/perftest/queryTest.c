077cd23da95f61902ddd039c09491f2f00cdc0b0
hjxilinx <hjxilinx@163.com>
2019-11-27 18:48:00 +0800

    [tbase-874]

--- enterprise/tests/test/perftest/queryTest.c
+++ enterprise/tests/test/perftest/queryTest.c
@@ -449,9 +449,9 @@ int main(int argc, char **argv) {
         total = strtol(argv[4], NULL, 10);
     }
     
-    executeSQL(conn, "use ac_db", NULL);
+    executeSQL(conn, "use m_alt_db0", NULL);
     // selectivity + tags/ts + group by normal columns
-    executeSQL(conn, "insert into t1 values(now, 1)", NULL);
+    executeSQL(conn, "insert into tb values('2018-11-1 16:00:00', 1, '2', 1)", NULL);
 //    createEnvironment(conn, 5000, 5000, 100, 30);
 //    executeSQL(conn, "select last_row(ts) from m1 where tbname in ('tm0', 'tm1') group by tbname", NULL);
 //    executeSQL(conn, "select top(k, 5) from tm0", NULL);
