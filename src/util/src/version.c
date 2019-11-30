0224972e63befb26827cf85bfd258229f26be7dd
slguan <slguan@taosdata.com>
2019-11-30 23:17:58 +0800

    fix some release scripts

--- src/util/src/version.c
+++ src/util/src/version.c
@@ -1,4 +1,4 @@
 char version[64] = "1.6.4.0";
 char compatible_version[64] = "1.6.1.0";
-char gitinfo[128] = "b6e308866e315483915f4c42a2717547ed0b9d36";
-char buildinfo[512] = "Built by ubuntu at 2019-11-26 21:56";
+char gitinfo[128] = "e86237890ee9daa8f6ab64ed225a027ae63ebdfc";
+char buildinfo[512] = "Built by ubuntu at 2019-11-30 23:16";
