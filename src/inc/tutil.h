8f3fe94ae97bebb98b848373209a11ccd05bd74e
slguan <slguan@taosdata.com>
2019-12-25 18:35:21 +0800

    #1014 [TBASE-1376]

--- src/inc/tutil.h
+++ src/inc/tutil.h
@@ -173,6 +173,8 @@ uint32_t MurmurHash3_32(const void *key, int32_t len);
 
 bool taosMbsToUcs4(char *mbs, int32_t mbs_len, char *ucs4, int32_t ucs4_max_len);
 
+int tasoUcs4Compare(void* f1_ucs4, void *f2_ucs4, int bytes);
+
 bool taosUcs4ToMbs(void *ucs4, int32_t ucs4_max_len, char *mbs);
 
 bool taosValidateEncodec(char *encodec);
