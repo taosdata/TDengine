c48c78e6ee47986a1b62b76339acb592c963ca29
fangpanpan <fpang@taosdata.com>
2020-01-17 09:38:14 +0800

    [update release info]

--- src/util/src/version.c
+++ src/util/src/version.c
@@ -1,5 +1,5 @@
-char version[64] = "1.6.4.4";
+char version[64] = "1.6.4.6";
 char compatible_version[64] = "1.6.1.0";
-char gitinfo[128] = "d62c5c30231d04a736d437cf428af6e12599bd9f";
-char gitinfoOfInternal[128] = "8094a32d78dc519bd883d01ac2ba6ec49ac57a80";
-char buildinfo[512] = "Built by ubuntu at 2019-12-16 21:40";
+char gitinfo[128] = "0d2351b4a7043f4e907734d7b1696d13574cb8e0";
+char gitinfoOfInternal[128] = "a898ecfc1f6b9f1c626ca98a0b9a77b8d5b52abe";
+char buildinfo[512] = "Built by root at 2020-01-14 11:05";
