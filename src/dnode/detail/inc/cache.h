2d6b50cd8d7b13f117169cb237802e4eb346bc2c
Hongze Cheng <hzcheng@taosdata.com>
2020-02-05 14:28:57 +0800

    add TSDB initial interface files

---
+++ src/dnode/detail/inc/cache.h
@@ -0,0 +1,9 @@
+#if !defined(_TD_CACHE_H_)
+#define _TD_CACHE_H_
+
+typedef struct {
+
+} SCachePool;
+
+
+#endif // _TD_CACHE_H_
---
+++ src/dnode/detail/src/tsdb/tsdb.c
@@ -0,0 +1,5 @@
+#include "tsdb.h"
+
+typedef struct {
+    // TODO
+} STSDBRepo;
