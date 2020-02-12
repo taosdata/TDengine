70462035571bd9513233aeb68d0b6c686799e19c
slguan <slguan@taosdata.com>
2020-02-12 12:46:01 +0800

    split meter to table and stable

---
+++ src/mnode/src/mgmtStable.c
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2019 TAOS Data, Inc. <jhtao@taosdata.com>
+ *
+ * This program is free software: you can use, redistribute, and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3
+ * or later ("AGPL"), as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#define _DEFAULT_SOURCE
+#include "os.h"
+
+#include "mnode.h"
+#include "mgmtAcct.h"
+#include "mgmtGrant.h"
+#include "mgmtUtil.h"
+#include "mgmtDb.h"
+#include "mgmtDnodeInt.h"
+#include "mgmtVgroup.h"
+#include "mgmtSupertableQuery.h"
+#include "mgmtTable.h"
+#include "taosmsg.h"
+#include "tast.h"
+#include "textbuffer.h"
+#include "tschemautil.h"
+#include "tscompression.h"
+#include "tskiplist.h"
+#include "tsqlfunction.h"
+#include "ttime.h"
+#include "tstatus.h"
