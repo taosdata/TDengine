46724513a2d151e9cc710766357e2bddacf985f3
lihui <huili@taosdata.com>
2020-02-19 19:22:31 +0800

    [release for fnk]

--- src/util/src/version.c
+++ src/util/src/version.c
@@ -1,7 +1,7 @@
 char version[64] = "1.6.5.5";
 char compatible_version[64] = "1.6.0.0";
-char gitinfo[128] = "28336fbcc8f5d56976fef8fdff567301245ecb32";
+char gitinfo[128] = "4ad876d4b466566c82661b4120a8c14354c57d96";
 char gitinfoOfInternal[128] = "da88f4a2474737d1f9c76adcf0ff7fd0975e7342";
-char buildinfo[512] = "Built by root at 2020-02-19 16:36";
+char buildinfo[512] = "Built by root at 2020-02-19 19:16";
 
 void libtaos_1_6_5_5_Linux_x64() {};
