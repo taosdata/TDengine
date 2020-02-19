d09640c14d8151ddf92d2df1d65e39a1ce8687b7
haojun Liao <hjxilinx@users.noreply.github.com>
2020-02-19 14:24:00 +0800

    Update tscUtil.c
    fix a typo

--- src/client/src/tscUtil.c
+++ src/client/src/tscUtil.c
@@ -72,7 +72,7 @@ void tscGetMetricMetaCacheKey(SSqlCmd* pCmd, char* str, uint64_t uid) {
   char* tmp = calloc(1, bufSize);
 
   int32_t keyLen = snprintf(tmp, bufSize, "%s,%s,%s,%d,%s,[%s],%d", pMeterMetaInfo->name,
-                            (cond != NULL && cond->cond != NULL) ? cond->cond : NULL), (tbnameCondLen > 0 ? pTagCond->tbnameCond.cond : NULL),
+                            ((cond != NULL && cond->cond != NULL) ? cond->cond : NULL), (tbnameCondLen > 0 ? pTagCond->tbnameCond.cond : NULL),
                             pTagCond->relType, join, tagIdBuf, pCmd->groupbyExpr.orderType);
 
   assert(keyLen <= bufSize);
