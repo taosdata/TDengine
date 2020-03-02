8f3d8c64c4c2ec31c5f803a8c328fbe50272081b
plum-lihui <2849823933@qq.com>
2020-03-02 22:39:04 +0800

    [update version information after compile]

--- src/util/src/version.c
+++ src/util/src/version.c
@@ -1,7 +1,7 @@
-char version[64] = "1.6.5.4";
-char compatible_version[64] = "1.6.1.0";
-char gitinfo[128] = "3264067e97300c84caa61ac909d548c9ca56de6b";
-char gitinfoOfInternal[128] = "da88f4a2474737d1f9c76adcf0ff7fd0975e7342";
-char buildinfo[512] = "Built by root at 2020-02-05 14:38";
+char version[64] = "1.6.6.1";
+char compatible_version[64] = "1.6.0.0";
+char gitinfo[128] = "f031b308b76837a792d58a61f48179a9082606d9";
+char gitinfoOfInternal[128] = "67c22fa076de3e4bc1b0b65666dd360b14c3c32a";
+char buildinfo[512] = "Built by root at 2020-03-02 22:37";
 
-void libtaos_1_6_5_4_Linux_x64() {};
+void libtaos_1_6_6_1_Linux_x64_beta() {};
