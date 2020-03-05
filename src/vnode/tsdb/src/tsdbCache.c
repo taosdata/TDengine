db07abc85174344d32781c6a01b88717f1729ec0
hzcheng <hzcheng@taosdata.com>
2020-03-05 06:08:28 +0000

    format file

--- src/vnode/tsdb/src/tsdbCache.c
+++ src/vnode/tsdb/src/tsdbCache.c
@@ -2,21 +2,19 @@
 
 #include "tsdbCache.h"
 
-
 STsdbCache *tsdbCreateCache(int32_t numOfBlocks) {
-    STsdbCache *pCacheHandle = (STsdbCache *)malloc(sizeof(STsdbCache));
-    if (pCacheHandle == NULL) {
-        // TODO : deal with the error
-        return NULL;
-    }
+  STsdbCache *pCacheHandle = (STsdbCache *)malloc(sizeof(STsdbCache));
+  if (pCacheHandle == NULL) {
+    // TODO : deal with the error
+    return NULL;
+  }
 
-    return pCacheHandle;
-
+  return pCacheHandle;
 }
 
 int32_t tsdbFreeCache(STsdbCache *pHandle) { return 0; }
 
 void *tsdbAllocFromCache(STsdbCache *pCache, int64_t bytes) {
-    // TODO: implement here
-    return NULL;
+  // TODO: implement here
+  return NULL;
 }
