8f1eed23de140254c6d734ca4321b83da38b1f6e
plum-lihui <2849823933@qq.com>
2020-03-11 10:10:27 +0800

    [#1343]

--- src/util/src/version.c
+++ src/util/src/version.c
@@ -2,6 +2,6 @@ char version[64] = "1.6.6.1";
 char compatible_version[64] = "1.6.0.0";
 char gitinfo[128] = "0b5b412ef0ae2449ece538601a29b899b2b727b9";
 char gitinfoOfInternal[128] = "8ae0d83a3610b9b4726373dd3073e4a8f444fb26";
-char buildinfo[512] = "Built by root at 2020-03-03 17:26";
+char buildinfo[512] = "Built by root at 2020-03-11 09:08";
 
 void libtaos_1_6_6_1_Linux_x64_beta() {};
