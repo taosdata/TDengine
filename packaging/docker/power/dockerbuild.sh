0e93e982ffba680c982b0ca540eee1eca47bad9c
lihui <huili@taosdata.com>
2020-03-18 14:07:53 +0800

    [TD-20]

--- packaging/docker/power/dockerbuild.sh
+++ packaging/docker/power/dockerbuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
-$1
-docker build --rm -f "Dockerfile" -t zdpf/powerdb:$1 "."
-docker login -u zdpf -p tbase125!  #replace the docker registry username and password
-docker push zdpf/powerdb:$1
+echo input tag: $1
+sudo docker build --rm -f "Dockerfile" -t zdpf/powerdb:$1 "."
+sudo docker login -u zdpf -p tbase125!  #replace the docker registry username and password
+sudo docker push zdpf/powerdb:$1
