9961bf33321b9372de6bf3cba946af47c5c8e758
slguan <slguan@taosdata.com>
2020-03-27 00:38:00 +0800

    [TD-16] change account codes

--- src/mnode/src/mgmtAcct.c
+++ src/mnode/src/mgmtAcct.c
@@ -14,86 +14,12 @@
  */
 
 #define _DEFAULT_SOURCE
+#ifndef _ACCOUNT
 #include "os.h"
 #include "mnode.h"
 #include "mgmtAcct.h"
 
 static SAcctObj tsAcctObj = {0};
-
-int32_t mgmtAddDbIntoAcct(SAcctObj *pAcct, SDbObj *pDb) {
-  pthread_mutex_lock(&pAcct->mutex);
-  pDb->next = pAcct->pHead;
-  pDb->prev = NULL;
-
-  if (pAcct->pHead) {
-    pAcct->pHead->prev = pDb;
-  }
-
-  pAcct->pHead = pDb;
-  pAcct->acctInfo.numOfDbs++;
-  pthread_mutex_unlock(&pAcct->mutex);
-
-  return 0;
-}
-
-int32_t mgmtRemoveDbFromAcct(SAcctObj *pAcct, SDbObj *pDb) {
-  pthread_mutex_lock(&pAcct->mutex);
-  if (pDb->prev) {
-    pDb->prev->next = pDb->next;
-  }
-
-  if (pDb->next) {
-    pDb->next->prev = pDb->prev;
-  }
-
-  if (pDb->prev == NULL) {
-    pAcct->pHead = pDb->next;
-  }
-
-  pAcct->acctInfo.numOfDbs--;
-  pthread_mutex_unlock(&pAcct->mutex);
-
-  return 0;
-}
-
-int32_t mgmtAddUserIntoAcct(SAcctObj *pAcct, SUserObj *pUser) {
-  pthread_mutex_lock(&pAcct->mutex);
-  pUser->next = pAcct->pUser;
-  pUser->prev = NULL;
-
-  if (pAcct->pUser) {
-    pAcct->pUser->prev = pUser;
-  }
-
-  pAcct->pUser = pUser;
-  pAcct->acctInfo.numOfUsers++;
-  pUser->pAcct = pAcct;
-  pthread_mutex_unlock(&pAcct->mutex);
-
-  return 0;
-}
-
-int32_t mgmtRemoveUserFromAcct(SAcctObj *pAcct, SUserObj *pUser) {
-  pthread_mutex_lock(&pAcct->mutex);
-  if (pUser->prev) {
-    pUser->prev->next = pUser->next;
-  }
-
-  if (pUser->next) {
-    pUser->next->prev = pUser->prev;
-  }
-
-  if (pUser->prev == NULL) {
-    pAcct->pUser = pUser->next;
-  }
-
-  pAcct->acctInfo.numOfUsers--;
-  pthread_mutex_unlock(&pAcct->mutex);
-
-  return 0;
-}
-
-#ifndef _ACCOUNT
 
 int32_t mgmtInitAccts() {
   tsAcctObj.acctId = 0;
@@ -104,6 +30,14 @@ int32_t mgmtInitAccts() {
 SAcctObj *mgmtGetAcct(char *acctName) { return &tsAcctObj; }
 
 void mgmtCleanUpAccts() {}
+
+int32_t mgmtAddDbIntoAcct(SAcctObj *pAcct, SDbObj *pDb) { return TSDB_CODE_SUCCESS; }
+
+int32_t mgmtRemoveDbFromAcct(SAcctObj *pAcct, SDbObj *pDb) { return TSDB_CODE_SUCCESS; }
+
+int32_t mgmtAddUserIntoAcct(SAcctObj *pAcct, SUserObj *pUser) { return TSDB_CODE_SUCCESS; }
+
+int32_t mgmtRemoveUserFromAcct(SAcctObj *pAcct, SUserObj *pUser) { return TSDB_CODE_SUCCESS; }
 
 int32_t mgmtCheckUserLimit(SAcctObj *pAcct) { return TSDB_CODE_SUCCESS; }
 
