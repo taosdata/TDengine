5fb1dd5e3d8120a794c3e2fffd531f08f052236b
slguan <slguan@taosdata.com>
2020-03-31 00:47:17 +0800

    [TD-73]

--- tests/tsim/src/simMain.c
+++ tests/tsim/src/simMain.c
@@ -1,13 +1,17 @@
-/*******************************************************************
- *           Copyright (c) 2001 by TAOS Networks, Inc.
- *                     All rights reserved.
+/*
+ * Copyright (c) 2019 TAOS Data, Inc. <jhtao@taosdata.com>
  *
- *  This file is proprietary and confidential to TAOS Networks, Inc.
- *  No part of this file may be reproduced, stored, transmitted,
- *  disclosed or used in any form or by any means other than as
- *  expressly provided by the written permission from Jianhui Tao
+ * This program is free software: you can use, redistribute, and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3
+ * or later ("AGPL"), as published by the Free Software Foundation.
  *
- * ****************************************************************/
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "sim.h"
 #include <errno.h>
