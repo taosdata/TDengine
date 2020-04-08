4926dddebb203dc7016bece9e59ac2c33ebd95af
Shuduo Sang <sangshuduo@gmail.com>
2020-04-08 23:14:44 +0800

    fix kill script to avoid others' path with taosdata be killed by mistake.

--- tests/script/sh/stop_dnodes.sh
+++ tests/script/sh/stop_dnodes.sh
@@ -6,8 +6,8 @@ if [ -n "$PID" ]; then
 	sudo systemctl stop taosd
 fi
   
-PID=`ps -ef|grep taosd | grep -v grep | awk '{print $2}'`
+PID=`ps -ef|grep -w taosd | grep -v grep | awk '{print $2}'`
 if [ -n "$PID" ]; then 
 	echo sudo kill -9 $PID 
-	sudo kill -9 $PID
+	sudo pkill taosd
 fi 
