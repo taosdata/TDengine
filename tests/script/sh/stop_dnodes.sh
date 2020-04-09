37b293720337b7cf443b4cec4e889b0dcabf9d64
Shuduo Sang <sangshuduo@gmail.com>
2020-04-09 19:08:26 +0800

    fix stop_dnodes.sh with postpone socket cleanup and same timestamp issue in general/table/basic2.sim.

--- tests/script/sh/stop_dnodes.sh
+++ tests/script/sh/stop_dnodes.sh
@@ -7,7 +7,9 @@ if [ -n "$PID" ]; then
 fi
   
 PID=`ps -ef|grep -w taosd | grep -v grep | awk '{print $2}'`
-if [ -n "$PID" ]; then 
+while [ -n "$PID" ]; do
 	echo sudo kill -9 $PID 
-	sudo pkill taosd
-fi 
+	sudo pkill -9 taosd
+  sudo fuser -k -n tcp 6030
+  PID=`ps -ef|grep -w taosd | grep -v grep | awk '{print $2}'`
+done
