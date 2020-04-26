238879bc80e7a934a267c41e4656eb794fc65aa9
Shuduo Sang <sangshuduo@gmail.com>
2020-04-26 15:34:15 +0800

    add None type support in sql.py.

--- tests/pytest/simpletest.sh
+++ tests/pytest/simpletest.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
-python3 ./test.py -f insert/basic.py $1
+python3 ./test.py $1 -f insert/basic.py
 python3 ./test.py -s $1
 sleep 1
-python3 ./test.py -f insert/int.py $1
+python3 ./test.py $1 -f insert/int.py
 python3 ./test.py -s $1
 sleep 1
-python3 ./test.py -f insert/float.py $1
+python3 ./test.py $1 -f insert/float.py
 python3 ./test.py -s $1
+sleep 1
+python3 ./test.py $1 -f insert/bigint.py
+python3 ./test.py -s $1
+sleep 1
+python3 ./test.py $1 -f insert/bool.py
+python3 ./test.py -s $1
+sleep 1
+python3 ./test.py $1 -f insert/double.py
+python3 ./test.py -s $1
+sleep 1
+python3 ./test.py $1 -f insert/smallint.py
+python3 ./test.py -s $1
+sleep 1
+python3 ./test.py $1 -f insert/tinyint.py
+python3 ./test.py -s $1
+sleep 1
