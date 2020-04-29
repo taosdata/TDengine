8386d26ceb59d16b2c65f33ee3cc904bd96de58f
Shuduo Sang <sangshuduo@gmail.com>
2020-04-29 17:34:32 +0800

    fix hash entry assertion issue.

--- src/util/src/hash.c
+++ src/util/src/hash.c
@@ -521,6 +521,7 @@ SHashMutableIterator *taosHashCreateIter(SHashObj *pHashObj) {
 static SHashNode *getNextHashNode(SHashMutableIterator *pIter) {
   assert(pIter != NULL);
 
+  pIter->entryIndex++;
   while (pIter->entryIndex < pIter->pHashObj->capacity) {
     SHashEntry *pEntry = pIter->pHashObj->hashList[pIter->entryIndex];
     if (pEntry->next == NULL) {
