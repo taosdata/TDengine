7d0abd1d4e45984d3a7b6bd8e6a4ba9a6805d9fb
Jeff Tao <jhtao@taosdata.com>
2020-05-21 04:56:48 +0000

    free message is not in right way

--- src/rpc/src/rpcMain.c
+++ src/rpc/src/rpcMain.c
@@ -869,7 +869,7 @@ static void *rpcProcessMsgFromPeer(SRecvInfo *pRecv) {
 
   if (pRecv->ip==0 && pConn) {
     rpcProcessBrokenLink(pConn); 
-    tfree(pRecv->msg);
+    rpcFreeMsg(pRecv->msg);
     return NULL;
   }
 
@@ -894,7 +894,7 @@ static void *rpcProcessMsgFromPeer(SRecvInfo *pRecv) {
     }
   }
 
-  if (code) tfree(pRecv->msg); // parsing failed, msg shall be freed
+  if (code) rpcFreeMsg(pRecv->msg); // parsing failed, msg shall be freed
   return pConn;
 }
 
