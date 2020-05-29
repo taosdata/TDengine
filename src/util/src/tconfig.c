7e0783a5a813c40c5dab87192155b301e48272b2
Hui Li <huili@taosdata.com>
2020-05-29 16:22:44 +0800

    [TD-439]

--- src/util/src/tconfig.c
+++ src/util/src/tconfig.c
@@ -241,7 +241,12 @@ void taosReadGlobalLogCfg() {
 
   wordexp_t full_path;
   wordexp(configDir, &full_path, 0);
-  if (full_path.we_wordv != NULL && full_path.we_wordv[0] != NULL) {
+  if (full_path.we_wordv != NULL && full_path.we_wordv[0] != NULL) {    
+    if (strlen(full_path.we_wordv[0]) > TSDB_FILENAME_LEN - 1) {
+      printf("\nconfig file: %s path overflow max len %d, all variables are set to default\n", full_path.we_wordv[0], TSDB_FILENAME_LEN - 1);
+      wordfree(&full_path);
+      return;
+    }
     strcpy(configDir, full_path.we_wordv[0]);
   } else {
     printf("configDir:%s not there, use default value: /etc/taos", configDir);
