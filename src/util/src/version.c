feef3bde2f956d0dbe879b61f2a8c4ea595f3da1
plum-lihui <2849823933@qq.com>
2020-06-04 09:47:07 +0800

    [update version info]

--- src/util/src/version.c
+++ src/util/src/version.c
@@ -1,7 +1,7 @@
 char version[64] = "1.6.5.9";
 char compatible_version[64] = "1.6.0.0";
-char gitinfo[128] = "0b5b412ef0ae2449ece538601a29b899b2b727b9";
-char gitinfoOfInternal[128] = "8ae0d83a3610b9b4726373dd3073e4a8f444fb26";
-char buildinfo[512] = "Built by root at 2020-03-11 09:08";
+char gitinfo[128] = "8c033a514e7c8564ef321bde948e1c94889c301a";
+char gitinfoOfInternal[128] = "";
+char buildinfo[512] = "Built by root at 2020-06-01 16:00";
 
-void libtaos_1_6_6_1_Linux_x64_beta() {};
+void libtaos_edge_1_6_5_9_Linux_x64() {};
