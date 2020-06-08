370c67361c20d63c8913bb9afa00efc585bea3c0
Hui Li <huili@taosdata.com>
2020-06-08 15:12:50 +0800

    [modify string len for version]

--- src/util/src/version.c
+++ src/util/src/version.c
@@ -1,5 +1,5 @@
-char version[TSDB_VERSION_LEN] = "2.0.0.0";
-char compatible_version[TSDB_VERSION_LEN] = "2.0.0.0";
+char version[12] = "2.0.0.0";
+char compatible_version[12] = "2.0.0.0";
 char gitinfo[48] = "3264067e97300c84caa61ac909d548c9ca56de6b";
 char gitinfoOfInternal[48] = "da88f4a2474737d1f9c76adcf0ff7fd0975e7342";
 char buildinfo[64] = "Built by root at 2020-04-01 14:38";
