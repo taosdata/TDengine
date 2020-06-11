a1663508c37e19051381cb8c31b4f4557cb073b0
Bomin Zhang <bmzhang@taosdata.com>
2020-06-11 14:04:50 +0800

    fix compile error

--- src/inc/taosmsg.h
+++ src/inc/taosmsg.h
@@ -501,7 +501,7 @@ typedef struct {
 } SVnodeLoad;
 
 typedef struct {
-  char     db[TSDB_USERID_LEN + TSDB_DB_NAME_LEN];
+  char     db[TSDB_ACCT_LEN + TSDB_DB_NAME_LEN];
   int32_t  cacheBlockSize; //MB
   int32_t  totalBlocks;
   int32_t  maxTables;
