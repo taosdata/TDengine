c10730f8f54f8c9ebc2958eca99b292b7b058033
Bomin Zhang <bmzhang@taosdata.com>
2020-06-12 09:44:05 +0800

    fix compile error under higher version of gcc

--- src/client/src/tscLocal.c
+++ src/client/src/tscLocal.c
@@ -279,7 +279,8 @@ static void tscProcessCurrentUser(SSqlObj *pSql) {
   pExpr->resType = TSDB_DATA_TYPE_BINARY;
   
   char* vx = calloc(1, pExpr->resBytes);
-  STR_WITH_MAXSIZE_TO_VARSTR(vx, pSql->pTscObj->user, sizeof(pSql->pTscObj->user));
+  size_t size = sizeof(pSql->pTscObj->user);
+  STR_WITH_MAXSIZE_TO_VARSTR(vx, pSql->pTscObj->user, size);
   
   tscSetLocalQueryResult(pSql, vx, pExpr->aliasName, pExpr->resType, pExpr->resBytes);
   free(vx);
