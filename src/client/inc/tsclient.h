e258f1ace0baa5648adcaabacbbd87f71b52852e
Bomin Zhang <bmzhang@taosdata.com>
2020-06-15 11:49:29 +0800

    fix td-320

--- src/client/inc/tsclient.h
+++ src/client/inc/tsclient.h
@@ -286,7 +286,7 @@ typedef struct STscObj {
   char               user[TSDB_USER_LEN];
   char               pass[TSDB_KEY_LEN];
   char               acctId[TSDB_ACCT_LEN];
-  char               db[TSDB_DB_NAME_LEN];
+  char               db[TSDB_ACCT_LEN + TSDB_DB_NAME_LEN];
   char               sversion[TSDB_VERSION_LEN];
   char               writeAuth : 1;
   char               superAuth : 1;
