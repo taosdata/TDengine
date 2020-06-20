724bd9bd2295cc8c6ec1312b3f2ea17baa1ef048
Shuduo Sang <sdsang@taosdata.com>
2020-06-20 11:12:02 +0800

    add stable/query_after_reset.py
    [TD-709]

--- tests/pytest/fulltest.sh
+++ tests/pytest/fulltest.sh
@@ -126,6 +126,9 @@ python3 ./test.py -f import_merge/importInsertThenImport.py
 python3 ./test.py -f user/user_create.py
 python3 ./test.py -f user/pass_len.py
 
+# stable
+python3 ./test.py -f stable/query_after_reset.py
+
 # table
 python3 ./test.py -f table/del_stable.py
 
