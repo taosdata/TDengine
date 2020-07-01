72028706481972a1add2405d0bb7fc99d20b0769
dengyihao <luomoxyz@126.com>
2020-07-01 17:59:43 +0800

    fixbug use_after_free

--- src/rpc/src/rpcMain.c
+++ src/rpc/src/rpcMain.c
@@ -1530,9 +1530,9 @@ static void rpcDecRef(SRpcInfo *pRpc)
 { 
   if (atomic_sub_fetch_32(&pRpc->refCount, 1) == 0) {
     taosHashCleanup(pRpc->hash);
-    taosTmrCleanUp(pRpc->tmrCtrl);
     taosIdPoolCleanUp(pRpc->idPool);
     rpcCloseConnCache(pRpc->pCache);
+    taosTmrCleanUp(pRpc->tmrCtrl);
 
     tfree(pRpc->connList);
     pthread_mutex_destroy(&pRpc->mutex);
