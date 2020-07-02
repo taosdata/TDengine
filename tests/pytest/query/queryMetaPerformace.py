1e379d6a3541d6020499fe6fa47f15dec4266edb
dengyihao <luomoxyz@126.com>
2020-07-02 08:44:17 +0800

    add queryMetaPerformace

--- tests/pytest/query/queryMetaPerformace.py
+++ tests/pytest/query/queryMetaPerformace.py
@@ -1,3 +1,4 @@
+
 ###################################################################
 #           Copyright (c) 2016 by TAOS Technologies, Inc.
 #                     All rights reserved.
@@ -16,8 +17,23 @@ import taos
 import threading
 import time
 from datetime import datetime
+import numpy as np
 
+class MyThread(threading.Thread):
 
+    def __init__(self, func, args=()):
+        super(MyThread, self).__init__()
+        self.func = func
+        self.args = args
+
+    def run(self):
+        self.result = self.func(*self.args)
+
+    def get_result(self):
+        try:
+            return self.result   # 如果子线程不使用join方法，此处可能会报没有self.result的错误
+        except Exception:
+            return None
 
 class MetadataQuery:
     def initConnection(self):
@@ -29,9 +45,8 @@ class MetadataQuery:
         self.user = "root"
         self.password = "taosdata"
         self.config = "/etc/taos"
-
-    def connectDB(self):
         self.conn = taos.connect( self.host, self.user, self.password, self.config)
+    def connectDB(self):
         return self.conn.cursor()
 
     def createStable(self):
@@ -75,10 +90,26 @@ class MetadataQuery:
                 cursor.execute(
                     "insert into t%d values(%d, %d)" %
                     (base + i + 1, self.ts + j, j))
-        print("end@%d"%(threadID)) 
         cursor.close()
-        #self.conn.close()
-
+    def queryWithTagId(self, threadId, tagId, queryNum): 
+        print("---------thread%d start-----------"%threadId)
+        query = '''select tgcol1, tgcol2, tgcol3, tgcol4, tgcol5, tgcol6, tgcol7, tgcol8, tgcol9,
+                tgcol10, tgcol11, tgcol12, tgcol13, tgcol14, tgcol15, tgcol16, tgcol17, tgcol18,
+                tgcol19, tgcol20, tgcol21, tgcol22, tgcol23, tgcol24, tgcol25, tgcol26, tgcol27,
+                tgcol28, tgcol29, tgcol30, tgcol31, tgcol32, tgcol33, tgcol34, tgcol35, tgcol36,
+                tgcol37, tgcol38, tgcol39, tgcol40, tgcol41, tgcol42, tgcol43, tgcol44, tgcol45,
+                tgcol46, tgcol47, tgcol48, tgcol49, tgcol50, tgcol51, tgcol52, tgcol53, tgcol54
+                from meters where tgcol{id} > {condition}'''
+        latancy = []
+        cursor = self.connectDB()
+        cursor.execute("use test")
+        for i in range(queryNum): 
+            startTime = time.time()
+            cursor.execute(query.format(id = tagId, condition = i))
+            cursor.fetchall()
+            latancy.append((time.time() - startTime)) 
+        print("---------thread%d end-----------"%threadId)
+        return latancy
     def queryData(self, query):
         cursor = self.connectDB()
         cursor.execute("use test")
@@ -100,35 +131,19 @@ if __name__ == '__main__':
 
     t = MetadataQuery()
     t.initConnection()
-    t.createStable()
 
-    print(
-        "================= Create %d tables and insert %d records into each table =================" %
-        (t.tables, t.records))
-    threads = []
-    startTime = datetime.now()
-    for i in range(t.numOfTherads):
-        thread = threading.Thread(
-            target=t.createTablesAndInsertData, args=(i,))
-        threads.append(thread)
-        thread.start()
-    for i in range(len(threads)):
-        threads[i].join()
-    endTime = datetime.now()
-    diff = (endTime - startTime).seconds
-    print(
-        "spend %d seconds to create %d tables and insert %d records into each table" %
-        (diff, t.tables, t.records))
-          
-    query = '''select tgcol1, tgcol2, tgcol3, tgcol4, tgcol5, tgcol6, tgcol7, tgcol8, tgcol9,
-            tgcol10, tgcol11, tgcol12, tgcol13, tgcol14, tgcol15, tgcol16, tgcol17, tgcol18,
-             tgcol19, tgcol20, tgcol21, tgcol22, tgcol23, tgcol24, tgcol25, tgcol26, tgcol27,
-            tgcol28, tgcol29, tgcol30, tgcol31, tgcol32, tgcol33, tgcol34, tgcol35, tgcol36,
-            tgcol37, tgcol38, tgcol39, tgcol40, tgcol41, tgcol42, tgcol43, tgcol44, tgcol45,
-            tgcol46, tgcol47, tgcol48, tgcol49, tgcol50, tgcol51, tgcol52, tgcol53, tgcol54
-            from meters where tgcol1 > 10 AND tgcol1 < 100 and tgcol2 > 100 and tgcol2 < 1000 or tgcol3 > 10000 or tgcol7 = true
-            or tgcol8 like '%2' and tgcol10 < 10'''
     latancys = [] 
-     
-      
-    t.queryData(query)
+    threads =  []
+    tagId = 1
+    queryNum = 1000 
+    for i in range(t.numOfTherads):
+        thread = MyThread(t.queryWithTagId, args = (i, tagId, queryNum))
+        threads.append(thread) 
+        thread.start()
+    for i in range(t.numOfTherads):  
+        threads[i].join()
+        latancys.extend(threads[i].get_result()) 
+    print("Total query: %d"%(queryNum * t.numOfTherads))    
+    print("statistic(s): mean= %f, P50 = %f, P75 = %f, P95 = %f, P99 = %f"
+            %(sum(latancys)/(queryNum * t.numOfTherads), np.percentile(latancys, 50), np.percentile(latancys, 75), np.percentile(latancys, 95), np.percentile(latancys, 99)))  
+
