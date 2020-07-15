edc19b015c00f8d4b5bbf08420164d4d9ca8d029
Shengliang Guan <slguan@taosdata.com>
2020-07-15 16:51:16 +0800

    [TD-927] Re-pull metadata when the table structure is modified

--- src/client/src/tscAsync.c
+++ src/client/src/tscAsync.c
@@ -492,7 +492,23 @@ void tscTableMetaCallBack(void *param, TAOS_RES *res, int code) {
            */
           pSql->fp = pSql->fetchFp;  // restore the fp
           tscHandleInsertRetry(pSql);
-        } else {// in case of other query type, continue
+        } else if (pCmd->command == TSDB_SQL_SELECT) {  // in case of other query type, continue
+          tscDebug("%p redo parse sql string and proceed", pSql);
+          //tscDebug("before  %p fp:%p, fetchFp:%p", pSql, pSql->fp, pSql->fetchFp);
+          pCmd->parseFinished = false;
+          tscResetSqlCmdObj(pCmd);
+
+          //tscDebug("after %p fp:%p, fetchFp:%p", pSql, pSql->fp, pSql->fetchFp);
+          code = tsParseSql(pSql, true);
+
+          if (code == TSDB_CODE_TSC_ACTION_IN_PROGRESS) {
+            return;
+          } else if (code != TSDB_CODE_SUCCESS) {
+            goto _error;
+          }
+
+          tscProcessSql(pSql);
+        } else {  // in all other cases, simple retry
           tscProcessSql(pSql);
         }
 
