d61a05858a758b75feee15b09eec4c7f29b81005
Haojun Liao <hjliao@taosdata.com>
2020-07-28 17:45:12 +0800

    [td-225]

--- src/query/src/qExecutor.c
+++ src/query/src/qExecutor.c
@@ -6749,14 +6749,14 @@ void** qAcquireQInfo(void* pMgmt, uint64_t key) {
   }
 }
 
-void** qReleaseQInfo(void* pMgmt, void* pQInfo, bool needFree) {
+void** qReleaseQInfo(void* pMgmt, void* pQInfo, bool freeHandle) {
   SQueryMgmt *pQueryMgmt = pMgmt;
 
   if (pQueryMgmt->qinfoPool == NULL) {
     return NULL;
   }
 
-  taosCacheRelease(pQueryMgmt->qinfoPool, pQInfo, needFree);
+  taosCacheRelease(pQueryMgmt->qinfoPool, pQInfo, freeHandle);
   return 0;
 }
 
