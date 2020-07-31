07726783e1ff732558b0b513b52356f8cc5a27b6
Haojun Liao <hjliao@taosdata.com>
2020-07-31 17:58:07 +0800

    [td-225]

--- src/query/src/qExecutor.c
+++ src/query/src/qExecutor.c
@@ -6742,7 +6742,6 @@ void** qAcquireQInfo(void* pMgmt, uint64_t key) {
 
 void** qReleaseQInfo(void* pMgmt, void* pQInfo, bool freeHandle) {
   SQueryMgmt *pQueryMgmt = pMgmt;
-
   if (pQueryMgmt->qinfoPool == NULL) {
     return NULL;
   }
--- src/vnode/src/vnodeRead.c
+++ src/vnode/src/vnodeRead.c
@@ -213,7 +213,7 @@ static int32_t vnodeProcessQueryMsg(SVnodeObj *pVnode, SReadMsg *pReadMsg) {
         }
       }
 
-      qReleaseQInfo(pVnode->qMgmt, (void**) &handle, false);
+      qReleaseQInfo(pVnode->qMgmt, (void**) &handle, freehandle);
     }
   }
 
@@ -268,7 +268,7 @@ static int32_t vnodeProcessFetchMsg(SVnodeObj *pVnode, SReadMsg *pReadMsg) {
     code = vnodeDumpQueryResult(pRet, pVnode, *handle, &freeHandle);
   }
 
-  qReleaseQInfo(pVnode->qMgmt, (void**) &handle, false);
+  qReleaseQInfo(pVnode->qMgmt, (void**) &handle, freeHandle);
   return code;
 }
 
