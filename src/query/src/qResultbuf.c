39dc3c422c95101817e21f3d94818d1055ffc796
Haojun Liao <hjliao@taosdata.com>
2020-08-07 12:08:42 +0800

    [td-1065]

--- src/query/src/qResultbuf.c
+++ src/query/src/qResultbuf.c
@@ -7,6 +7,7 @@
 #include "taoserror.h"
 
 #define GET_DATA_PAYLOAD(_p) ((_p)->pData + POINTER_BYTES)
+#define NO_IN_MEM_AVAILABLE_PAGES(_b) (listNEles((_b)->lruList) >= (_b)->inMemPages)
 
 int32_t createDiskbasedResultBuffer(SDiskbasedResultBuf** pResultBuf, int32_t rowSize, int32_t pagesize,
                                     int32_t inMemBufSize, const void* handle) {
@@ -25,7 +26,7 @@ int32_t createDiskbasedResultBuffer(SDiskbasedResultBuf** pResultBuf, int32_t ro
   pResBuf->comp         = true;
   pResBuf->file         = NULL;
   pResBuf->handle       = handle;
-  pResBuf->fileSize = 0;
+  pResBuf->fileSize     = 0;
 
   // at least more than 2 pages must be in memory
   assert(inMemBufSize >= pagesize * 2);
@@ -186,8 +187,6 @@ static char* loadPageFromDisk(SDiskbasedResultBuf* pResultBuf, SPageInfo* pg) {
   return GET_DATA_PAYLOAD(pg);
 }
 
-#define NO_IN_MEM_AVAILABLE_PAGES(_b) (listNEles((_b)->lruList) >= (_b)->inMemPages)
-
 static SIDList addNewGroup(SDiskbasedResultBuf* pResultBuf, int32_t groupId) {
   assert(taosHashGet(pResultBuf->groupSet, (const char*) &groupId, sizeof(int32_t)) == NULL);
 
@@ -211,11 +210,12 @@ static SPageInfo* registerPage(SDiskbasedResultBuf* pResultBuf, int32_t groupId,
   pResultBuf->numOfPages += 1;
 
   SPageInfo* ppi = malloc(sizeof(SPageInfo));//{ .info = PAGE_INFO_INITIALIZER, .pageId = pageId, .pn = NULL};
-  ppi->info     = PAGE_INFO_INITIALIZER;
-  ppi->pageId   = pageId;
-  ppi->pData    = NULL;
-  ppi->pn       = NULL;
-  ppi->used     = true;
+
+  ppi->pageId = pageId;
+  ppi->pData  = NULL;
+  ppi->info   = PAGE_INFO_INITIALIZER;
+  ppi->used   = true;
+  ppi->pn     = NULL;
 
   return *(SPageInfo**) taosArrayPush(list, &ppi);
 }
@@ -246,6 +246,8 @@ static char* evicOneDataPage(SDiskbasedResultBuf* pResultBuf) {
   // all pages are referenced by user, try to allocate new space
   if (pn == NULL) {
     int32_t prev = pResultBuf->inMemPages;
+
+    // increase by 50% of previous mem pages
     pResultBuf->inMemPages = pResultBuf->inMemPages * 1.5;
 
     qWarn("%p in memory buf page not sufficient, expand from %d to %d, page size:%d", pResultBuf, prev,
@@ -353,6 +355,8 @@ tFilePage* getResBufPage(SDiskbasedResultBuf* pResultBuf, int32_t id) {
     ((void**)((*pi)->pData))[0] = (*pi);
 
     lruListPushFront(pResultBuf->lruList, *pi);
+    (*pi)->used = true;
+
     loadPageFromDisk(pResultBuf, *pi);
     return GET_DATA_PAYLOAD(*pi);
   }
