a5d1e97058abc5dfa669a58caf15249a21f35f49
Hui Li <huili@taosdata.com>
2020-08-13 11:19:10 +0800

    [TD-1121]

---
+++ packaging/tools/set_core.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# This file is used to set config for core when taosd crash
+
+set -e
+# set -x
+
+csudo=""
+if command -v sudo > /dev/null; then
+  csudo="sudo"
+fi
+
+#ulimit -c unlimited
+${csudo} echo "ulimit -c unlimited" >> /etc/profile
+source /etc/profile
+
+${csudo} mkdir -p /coredump
+${csudo} sysctl -w kernel.core_pattern='/coredump/core-%e-%p'
+${csudo} echo '/coredump/core-%e-%p' | ${csudo} tee /proc/sys/kernel/core_pattern
