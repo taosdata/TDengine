6d7faf97f1d2bdca4237862c9dc9c25cc53244e8
Hui Li <huili@taosdata.com>
2020-08-26 12:09:54 +0800

    [update release ver info]

--- src/util/src/version.c
+++ src/util/src/version.c
@@ -1,7 +1,7 @@
-char version[12] = "2.0.2.0";
+char version[12] = "2.0.2.1";
 char compatible_version[12] = "2.0.0.0";
-char gitinfo[48] = "d711657139620f6c50f362597020705b8ad26bd2";
-char gitinfoOfInternal[48] = "1d74ae24c541ffbb280e8630883c0236cd45f8c7";
-char buildinfo[64] = "Built by root at 2020-08-24 16:31";
+char gitinfo[48] = "1527828603722a0ed8ea706439e8c172a13b7eb1";
+char gitinfoOfInternal[48] = "490a983428d02b00d06b2a067b3f6e74f4b8d1a8";
+char buildinfo[64] = "Built by root at 2020-08-26 12:00";
 
-void libtaos_2_0_2_0_Linux_x64_beta() {};
+void libtaos_2_0_2_1_Linux_x64() {};
