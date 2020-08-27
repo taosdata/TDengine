6160f2c7b995f516c4f677d50caa82947b4ac5fb
Haojun Liao <hjliao@taosdata.com>
2020-08-27 15:04:19 +0800

    [td-255] fix compiler error

--- enterprise/tests/test/perftest/queryTest.c
+++ enterprise/tests/test/perftest/queryTest.c
@@ -375,8 +375,10 @@ int main(int argc, char** argv) {
 
 //  multiThreadQuery(atoi(argv[1]), argv[2]);
 //  return 0;
+  executeSQL(conn, "drop database test", NULL);
+  executeSQL(conn, "create database test", NULL);
   executeSQL(conn, "use test", NULL);
-  executeSQL(conn,"select last_row(*) from tm1,tm2 where tm1.ts=tm2.ts", NULL);
+//  executeSQL(conn,"select last_row(*) from tm1,tm2 where tm1.ts=tm2.ts", NULL);
 //  executeSQL( conn, "select count(*) from join_mt0, join_mt1 where join_mt0.ts = join_mt1.ts and
 //  join_mt0.t1=join_mt1.t1 and join_mt0.c2=99;;", NULL);
 //  return 0;
