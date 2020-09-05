56c8b5c7981dde4a228f9ddf935035dd5acdd7be
Hui Li <huili@taosdata.com>
2020-09-05 15:40:32 +0800

    [modify script]

--- packaging/docker/dockerbuild.sh
+++ packaging/docker/dockerbuild.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -x
-$1
 docker build --rm -f "Dockerfile" -t tdengine/tdengine:$1 "."
 docker login -u tdengine -p $2  #replace the docker registry username and password
 docker push tdengine/tdengine:$1
