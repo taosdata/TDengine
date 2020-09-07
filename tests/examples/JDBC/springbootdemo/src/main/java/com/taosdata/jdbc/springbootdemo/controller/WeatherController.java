e31f80ed83e4fbe47974b4101a7758a830b9097d
zyyang-taosdata <zyyang@taosdata.com>
2020-09-08 00:13:42 +0800

    update springbootDemo

--- tests/examples/JDBC/springbootdemo/src/main/java/com/taosdata/jdbc/springbootdemo/controller/WeatherController.java
+++ tests/examples/JDBC/springbootdemo/src/main/java/com/taosdata/jdbc/springbootdemo/controller/WeatherController.java
@@ -16,43 +16,47 @@ public class WeatherController {
 
     /**
      * create database and table
+     *
      * @return
      */
     @GetMapping("/init")
-    public boolean init(){
+    public boolean init() {
         return weatherService.init();
     }
 
     /**
      * Pagination Query
+     *
      * @param limit
      * @param offset
      * @return
      */
     @GetMapping("/{limit}/{offset}")
-    public List<Weather> queryWeather(@PathVariable Long limit, @PathVariable Long offset){
+    public List<Weather> queryWeather(@PathVariable Long limit, @PathVariable Long offset) {
         return weatherService.query(limit, offset);
     }
 
     /**
      * upload single weather info
+     *
      * @param temperature
      * @param humidity
      * @return
      */
     @PostMapping("/{temperature}/{humidity}")
-    public int saveWeather(@PathVariable int temperature, @PathVariable float humidity){
+    public int saveWeather(@PathVariable int temperature, @PathVariable float humidity) {
 
         return weatherService.save(temperature, humidity);
     }
 
     /**
      * upload multi weather info
+     *
      * @param weatherList
      * @return
      */
     @PostMapping("/batch")
-    public int batchSaveWeather(@RequestBody List<Weather> weatherList){
+    public int batchSaveWeather(@RequestBody List<Weather> weatherList) {
 
         return weatherService.save(weatherList);
     }
