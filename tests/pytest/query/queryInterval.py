601342b333c4da7706b5fe7252a7b87a956c2212
Ping Xiao <pxiao@taosdata.com>
2020-09-18 10:40:02 +0800

    [TD-1245]<test> add test case for interval

--- tests/pytest/query/queryInterval.py
+++ tests/pytest/query/queryInterval.py
@@ -29,18 +29,29 @@ class TDTestCase:
         tdSql.prepare()
 
         tdSql.execute("create table st (ts timestamp, voltage int) tags (loc nchar(30))")
-        tdSql.execute("insert into t0 using st tags('beijing') values(now, 220) (now - 15d, 221) (now - 30d, 225) (now - 35d, 228) (now - 45d, 222)" )
+        tdSql.execute("insert into t0 using st tags('beijing') values(now, 220) (now - 15d, 221) (now - 30d, 225) (now - 35d, 228) (now - 45d, 222)")
+        tdSql.execute("insert into t1 using st tags('shanghai') values(now, 220) (now - 60d, 221) (now - 50d, 225) (now - 40d, 228) (now - 20d, 222)")
 
         tdSql.query("select avg(voltage) from st interval(1n)")
-        tdSql.checkRows(2)
-        tdSql.checkData(0, 1, 225.0)
-        tdSql.checkData(1, 1, 220.5)
+        tdSql.checkRows(3)
+        tdSql.checkData(0, 1, 223.0)
+        tdSql.checkData(1, 1, 225.0)
+        tdSql.checkData(2, 1, 220.333333)
         
         tdSql.query("select avg(voltage) from st interval(1n, 15d)")
         tdSql.checkRows(3)
+        tdSql.checkData(0, 1, 224.8)
+        tdSql.checkData(1, 1, 222.666666)
+        tdSql.checkData(2, 1, 220.0)
+
+        tdSql.query("select avg(voltage) from st interval(1n, 15d) group by loc")
+        tdSql.checkRows(6)
         tdSql.checkData(0, 1, 225.0)
         tdSql.checkData(1, 1, 223.0)
         tdSql.checkData(2, 1, 220.0)
+        tdSql.checkData(3, 1, 224.666666)
+        tdSql.checkData(4, 1, 222.0)
+        tdSql.checkData(5, 1, 220.0)
 
     def stop(self):
         tdSql.close()
