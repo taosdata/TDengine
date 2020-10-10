cd3e06567302166acc46456e9b48c3f23cb740b9
liuyq-617 <yqliu@taosdata.com>
2020-10-10 11:21:12 +0800

    [TD-1535] add script for jenkins

---
+++ enterprise/packaging/new_ver_release_j.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+version=$1
+versionComp=2.0.0.0
+
+## master
+branchName=release/$2
+verType=stable
+dockerPass="tbase125!"
+tagVal=ver-${version}
+dockerinput=TDengine-server-${version}-Linux-x64.tar.gz
+
+## develop
+# branchName=develop
+# verType=beta
+# dockerPass="tbase125!"
+# tagVal=ver-${version}-beta
+# dockerinput=TDengine-server-${version}-Linux-x64-beta.tar.gz
+
+bash generate_community.sh  $version $versionComp $branchName $verType
+bash generate_enterprise.sh $version $versionComp $branchName $verType
+bash docker_generate.sh $version $dockerPass $dockerinput
+bash tag.sh $tagVal $branchName
