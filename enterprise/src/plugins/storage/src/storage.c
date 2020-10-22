fb15beff78cfe4cfe2cb641831967ca4e74511eb
Shengliang Guan <slguan@taosdata.com>
2020-10-22 14:35:47 +0000

    TD-1767

--- enterprise/src/plugins/storage/src/storage.c
+++ enterprise/src/plugins/storage/src/storage.c
@@ -24,22 +24,29 @@ void taosReadDataDirCfg(char *v1, char *v2, char *v3) {
     int length = strlen(v2);
     if (length > 0) level = atoi(v2);
     if (level < 0 || level >= TSDB_MAX_TIERS) {
-      uError("config option:dataDir, input value2:%s, not in range [0, %d), set default 0", v2, TSDB_MAX_TIERS);
+      uError("config option:dataDir, input level:%s, not in range [0, %d), set default 0", v2, TSDB_MAX_TIERS);
       level = 0;
     }
   }
 
-  int primary = 0;
+  int primary = 1;
   if (v3 != NULL) {
     int length = strlen(v3);
     if (length > 0) primary = atoi(v3);
     if (primary < 0 || primary > 1) {
-      uError("config option:dataDir, input value3:%s, not in range [0, 1], set default 1", v3);
-      primary = 0;
+      uError("config option:dataDir, input primary:%s, not in range [0, 1], set default 1", v3);
+      primary = 1;
     }
   }
 
   if (tsDiskCfgNum >= TSDB_MAX_DISKS) return;
   taosAddDataDir(tsDiskCfgNum, v1, level, primary);
   tsDiskCfgNum++;
+}
+
+void taosPrintDataDirCfg() {
+  for (int i = 0; i < tsDiskCfgNum; ++i) {
+    SDiskCfg *cfg = &tsDiskCfg[i];
+    uInfo(" dataDir: %s level:%d primary:%d", cfg->dir, cfg->level, cfg->primary);
+  }
 }
