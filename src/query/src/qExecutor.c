6b49d26693110381549d9b3e18c47d82e42aab73
Haojun Liao <hjliao@taosdata.com>
2020-10-24 16:45:09 +0800

    [td-1373]

--- src/query/src/qExecutor.c
+++ src/query/src/qExecutor.c
@@ -1410,7 +1410,9 @@ static void rowwiseApplyFunctions(SQueryRuntimeEnv *pRuntimeEnv, SDataStatis *pS
     item->lastKey = (QUERY_IS_ASC_QUERY(pQuery)? pDataBlockInfo->window.ekey:pDataBlockInfo->window.skey) + step;
   }
 
-  item->cur = tsBufGetCursor(pRuntimeEnv->pTSBuf);
+  if (pRuntimeEnv->pTSBuf != NULL) {
+    item->cur = tsBufGetCursor(pRuntimeEnv->pTSBuf);
+  }
 
   // todo refactor: extract method
   for(int32_t i = 0; i < pQuery->numOfOutput; ++i) {
