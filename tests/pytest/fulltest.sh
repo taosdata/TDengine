ddd9b50e24c20b373a8ea38f7fe4f34e271343ff
Ping Xiao <pxiao@taosdata.com>
2020-10-29 16:48:18 +0800

    [TD-1823] <test> add test case for data loss

--- tests/pytest/fulltest.sh
+++ tests/pytest/fulltest.sh
@@ -152,6 +152,7 @@ python3 ./test.py -f query/queryInsertValue.py
 python3 ./test.py -f query/queryConnection.py
 python3 ./test.py -f query/natualInterval.py
 python3 ./test.py -f query/bug1471.py
+python3 ./test.py -f query/dataLossTest.py
 
 #stream
 python3 ./test.py -f stream/metric_1.py
