7017fc365e072361a4c53a29e7e1f6e9f04666a5
Shengliang Guan <slguan@taosdata.com>
2020-11-02 17:04:32 +0800

    TD-1887

--- src/wal/src/walWrite.c
+++ src/wal/src/walWrite.c
@@ -264,6 +264,8 @@ static int32_t walRestoreWalFile(SWal *pWal, void *pVnode, FWalWrite writeFp, ch
     wTrace("vgId:%d, fileId:%" PRId64 ", restore wal ver:%" PRIu64 ", head ver:%" PRIu64 " len:%d", pWal->vgId,
            pWal->fileId, pWal->version, pHead->version, pHead->len);
 
+    if (pWal->keep) pWal->version = pHead->version;
+
     (*writeFp)(pVnode, pHead, TAOS_QTYPE_WAL);
   }
 
