2e12e819f4297aa36b52954b4ab4db59b2597acb
Shengliang Guan <slguan@taosdata.com>
2020-11-02 17:01:02 +0800

    TD-1887

--- src/wal/src/walMgmt.c
+++ src/wal/src/walMgmt.c
@@ -145,18 +145,7 @@ static int32_t walInitObj(SWal *pWal) {
     return TAOS_SYSTEM_ERROR(errno);
   }
 
-  if (pWal->keep) {
-    return TSDB_CODE_SUCCESS;
-  }
-
-  walRenew(pWal);
-
-  if (pWal && pWal->fd < 0) {
-    wError("vgId:%d, file:%s, failed to open file since %s", pWal->vgId, pWal->path, strerror(errno));
-    return TAOS_SYSTEM_ERROR(errno);
-  }
-
-  wDebug("vgId:%d, file is initialized", pWal->vgId);
+  wDebug("vgId:%d, object is initialized", pWal->vgId);
   return TSDB_CODE_SUCCESS;
 }
 
