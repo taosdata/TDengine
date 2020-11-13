e05a07e504c08532e866db8e6c4449e0c7fddb6b
zyyang <zyyang@taosdata.com>
2020-11-13 11:30:42 +0800

    [TD-1869]<feature>: mybatis plus demo for Java Developers

---
+++ tests/examples/JDBC/mybatisplus-demo/src/main/java/com/taosdata/example/mybatisplusdemo/domain/Weather.java
@@ -0,0 +1,14 @@
+package com.taosdata.example.mybatisplusdemo.domain;
+
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Data
+public class Weather {
+
+    private Timestamp ts;
+    private float temperature;
+    private int humidity;
+
+}
---
+++ tests/examples/JDBC/mybatisplus-demo/src/test/java/com/taosdata/example/mybatisplusdemo/mapper/WeatherMapperTest.java
@@ -0,0 +1,30 @@
+package com.taosdata.example.mybatisplusdemo.mapper;
+
+
+import com.taosdata.example.mybatisplusdemo.domain.Weather;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WeatherMapperTest {
+
+    @Autowired
+    private WeatherMapper mapper;
+
+    @Test
+    public void testSelect() {
+        System.out.println(("----- selectAll method test ------"));
+        List<Weather> weatherList = mapper.selectList(null);
+        Assert.assertEquals(5, weatherList.size());
+        weatherList.forEach(System.out::println);
+    }
+
+
+}
