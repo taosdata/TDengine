6b0b783aaff83027613ad8836703cd5137ec4b50
Haojun Liao <hjliao@taosdata.com>
2020-11-14 11:06:42 +0800

    [TD-225]

--- src/client/src/tscUtil.c
+++ src/client/src/tscUtil.c
@@ -71,7 +71,7 @@ void tsSetSTableQueryCond(STagCond* pTagCond, uint64_t uid, SBufferWriter* bw) {
 }
 
 bool tscQueryTags(SQueryInfo* pQueryInfo) {
-  int32_t numOfCols = (int32_t)(pQueryInfo);
+  int32_t numOfCols = (int32_t) tscSqlExprNumOfExprs(pQueryInfo);
   for (int32_t i = 0; i < numOfCols; ++i) {
     SSqlExpr* pExpr = tscSqlExprGet(pQueryInfo, i);
     int32_t functId = pExpr->functionId;
