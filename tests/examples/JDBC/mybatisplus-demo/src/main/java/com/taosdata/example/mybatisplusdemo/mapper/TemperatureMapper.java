5454f092bff351f17e4c98b373c5153decfb7782
zyyang <zyyang@taosdata.com>
2020-11-16 14:57:35 +0800

    change

--- tests/examples/JDBC/mybatisplus-demo/src/main/java/com/taosdata/example/mybatisplusdemo/mapper/TemperatureMapper.java
+++ tests/examples/JDBC/mybatisplus-demo/src/main/java/com/taosdata/example/mybatisplusdemo/mapper/TemperatureMapper.java
@@ -2,14 +2,20 @@ package com.taosdata.example.mybatisplusdemo.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.taosdata.example.mybatisplusdemo.domain.Temperature;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Update;
 
 public interface TemperatureMapper extends BaseMapper<Temperature> {
 
+    @Update("CREATE TABLE if not exists temperature(ts timestamp, temperature float) tags(location nchar(64), tbIndex int)")
     int createSuperTable();
 
+    @Update("create table #{tbName} using temperature tags( #{location} )")
     int createTable(String tbName, String location);
 
+    @Update("drop table if exists temperature")
     void dropSuperTable();
 
+    @Insert("insert into t${tbIndex}(ts, temperature) values(ts, temperature)")
     int insertOne(Temperature one);
 }
