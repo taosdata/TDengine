0b869643f27d057e43f72efc0f0d72f43fd9c51c
Hui Li <huili@taosdata.com>
2020-11-30 09:58:35 +0800

    [TD-2260]<fix> set default value for some terminal

--- src/kit/shell/src/shellDarwin.c
+++ src/kit/shell/src/shellDarwin.c
@@ -407,7 +407,12 @@ void get_history_path(char *history) { sprintf(history, "%s/%s", getpwuid(getuid
 
 void clearScreen(int ecmd_pos, int cursor_pos) {
   struct winsize w;
-  ioctl(0, TIOCGWINSZ, &w);
+  if (ioctl(0, TIOCGWINSZ, &w) < 0 || w.ws_col == 0 || w.ws_row == 0) {
+    //fprintf(stderr, "No stream device, and use default value(col 120, row 30)\n");
+    //exit(EXIT_FAILURE);
+    w.ws_col = 120;
+    w.ws_row = 30;
+  }
 
   int cursor_x = cursor_pos / w.ws_col;
   int cursor_y = cursor_pos % w.ws_col;
@@ -425,8 +430,10 @@ void clearScreen(int ecmd_pos, int cursor_pos) {
 void showOnScreen(Command *cmd) {
   struct winsize w;
   if (ioctl(0, TIOCGWINSZ, &w) < 0 || w.ws_col == 0 || w.ws_row == 0) {
-    fprintf(stderr, "No stream device\n");
-    exit(EXIT_FAILURE);
+    //fprintf(stderr, "No stream device\n");
+    //exit(EXIT_FAILURE);
+    w.ws_col = 120;
+    w.ws_row = 30;
   }
 
   wchar_t wc;
