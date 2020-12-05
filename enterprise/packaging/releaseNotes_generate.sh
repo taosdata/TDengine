b984b74c129c7dbe658c1da30ef0584c516485b2
plum-lihui <huili@taosdata.com>
2020-12-05 13:42:31 +0800

    [TD-2343]<hotfix>: collect commit message from pull request to Release notes

--- enterprise/packaging/releaseNotes_generate.sh
+++ enterprise/packaging/releaseNotes_generate.sh
@@ -15,14 +15,16 @@ fi
 #
 echo "generate release notes>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 cd $communityDir/
+#git log origin/$fromBranch..origin/$toBranch --pretty=format:'%b' > ./releaseNotes.tmp
 git log origin/$fromBranch..origin/$toBranch --pretty=format:'%s' > ./releaseNotes.tmp
+
 # fix
 echo "Release Notes" > ReleaseNotes
 echo "" >> ReleaseNotes
 # fix
 echo "FIX BUGS:" >> ReleaseNotes
 grep '\[[tT][dD]-.*\] *<hotfix>:' ./releaseNotes.tmp | sed 's/ *<hotfix>//' | awk -F: '{print $1 ":" $2}' >> ReleaseNotes
-# grep '\[[tT][dD]-.*\] *<fix>:' ./releaseNotes.tmp | sed 's/ *<fix>//' | awk -F: '{print $1 ":" $2}'
+grep '\[[tT][dD]-.*\] *<fix>:' ./releaseNotes.tmp | sed 's/ *<fix>//' | awk -F: '{print $1 ":" $2}' >> ReleaseNotes
 echo "" >> ReleaseNotes
 
 # enhance
