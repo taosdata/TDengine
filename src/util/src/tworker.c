7e84d868d9bc93b66743856d450b75d08716fabf
Haojun Liao <hjliao@taosdata.com>
2020-12-09 23:10:02 +0800

    [TD-225] fix compiler error.

--- src/util/src/tworker.c
+++ src/util/src/tworker.c
@@ -36,14 +36,14 @@ int32_t tWorkerInit(SWorkerPool *pPool) {
 void tWorkerCleanup(SWorkerPool *pPool) {
   for (int i = 0; i < pPool->max; ++i) {
     SWorker *pWorker = pPool->worker + i;
-    if (pWorker->thread) {
+    if (pWorker->thread != 0) {
       taosQsetThreadResume(pPool->qset);
     }
   }
 
   for (int i = 0; i < pPool->max; ++i) {
     SWorker *pWorker = pPool->worker + i;
-    if (pWorker->thread) {
+    if (pWorker->thread != 0) {
       pthread_join(pWorker->thread, NULL);
     }
   }
