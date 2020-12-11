230e848679bad8aaf63efd83e5f874dd3bdaddd9
zyyang <zyyang@taosdata.com>
2020-12-11 14:30:12 +0800

    [TD-2191]<feature>: taosdemo tool for java developers

---
+++ tests/examples/JDBC/taosdemo/src/main/java/com/taosdata/taosdemo/TaosdemoApplication.java
@@ -0,0 +1,13 @@
+package com.taosdata.taosdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaosdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaosdemoApplication.class, args);
+    }
+
+}
