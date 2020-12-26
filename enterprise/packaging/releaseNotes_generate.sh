ec241b27a761b4ac1f0a0ac6266e5be7fbb17ae6
plum-lihui <huili@taosdata.com>
2020-12-26 13:21:20 +0800

    change release note script

--- enterprise/packaging/releaseNotes_generate.sh
+++ enterprise/packaging/releaseNotes_generate.sh
@@ -16,7 +16,7 @@ fi
 echo "generate release notes>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 cd $communityDir/
 git log origin/$fromBranch..origin/$toBranch --pretty=format:'%b' > ./releaseNotes.tmp
-#git log origin/$fromBranch..origin/$toBranch --pretty=format:'%s' > ./releaseNotes.tmp
+#git log origin/$fromBranch..origin/$toBranch --pretty=format:'%s' >> ./releaseNotes.tmp
 
 # fix
 echo "Release Notes" > ReleaseNotes
@@ -39,6 +39,7 @@ echo "" >> ReleaseNotes
 
 # docs
 echo "DOCS:" >> ReleaseNotes
+grep '\[TECO-.*\] *<docs>:' ./releaseNotes.tmp | sed 's/ *<docs>//' | awk -F: '{print $1 ":" $2}' >> ReleaseNotes
 grep '\[[tT][dD]-.*\] *<docs>:' ./releaseNotes.tmp | sed 's/ *<docs>//' | awk -F: '{print $1 ":" $2}' >> ReleaseNotes
 echo "" >> ReleaseNotes
 
