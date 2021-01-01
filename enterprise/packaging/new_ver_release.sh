254151fa5d54448d4f8cc3d613b39d0bc4b236c1
plum-lihui <huili@taosdata.com>
2021-01-01 23:05:53 +0800

    change version number

--- enterprise/packaging/new_ver_release.sh
+++ enterprise/packaging/new_ver_release.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #
-version=2.0.12.0
+version=2.0.13.0
 versionComp=2.0.0.0
 
 ## master
-branchName=release/s110
+branchName=release/s111
 verType=stable
 dockerPass="tbase125!"
 tagVal=ver-${version}
