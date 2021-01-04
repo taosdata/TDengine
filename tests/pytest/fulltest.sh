96279359521a71441aa34769027a556d6b6e5442
Ping Xiao <pxiao@taosdata.com>
2021-01-04 18:40:01 +0800

    update fulltest.sh

--- tests/pytest/fulltest.sh
+++ tests/pytest/fulltest.sh
@@ -221,6 +221,7 @@ python3 ./test.py -f functions/function_sum.py -r 1
 python3 ./test.py -f functions/function_top.py -r 1
 python3 ./test.py -f functions/function_twa.py -r 1
 python3 ./test.py -f functions/function_twa_test2.py
+python3 ./test.py -f functions/all_null_value.py
 python3 queryCount.py
 python3 ./test.py -f query/queryGroupbyWithInterval.py
 python3 client/twoClients.py
