393de696482dc7ed58ff4cbc474a47a7469bc902
zyyang <zyyang@taosdata.com>
2021-01-07 10:59:43 +0800

    [TD-2286]<fix>: fix wrong error message when connect unexists databases

---
+++ src/connector/jdbc/src/test/java/com/taosdata/jdbc/cases/ConnectWrongDatabaseTest.java
@@ -0,0 +1,22 @@
+package com.taosdata.jdbc.cases;
+
+import org.junit.Test;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectWrongDatabaseTest {
+
+    @Test
+    public void connect() {
+        try {
+            Class.forName("com.taosdata.jdbc.TSDBDriver");
+            DriverManager.getConnection("jdbc:TAOS://localhost:6030/wrong_db?user=root&password=taosdata");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
