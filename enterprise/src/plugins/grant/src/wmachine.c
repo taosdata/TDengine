058266f265d2a654bbf750057b5309f6b563a450
Shengliang Guan <slguan@taosdata.com>
2021-01-15 22:45:47 +0800

    TD-1207

--- enterprise/src/plugins/grant/src/wmachine.c
+++ enterprise/src/plugins/grant/src/wmachine.c
@@ -16,6 +16,21 @@
 
 SGrantObj grantObj;
 
-void grantActiveSystem(const char *inputCfgFile) {}
+void grantActiveSystem(const char *inputCfgFile) {
+  grantObj.granted = true;
+  grantObj.officialVersion = 1;
+  grantObj.expireTimeSec = GRANT_EXPIRE_TIME;
+  grantObj.limitStorage = GRANT_STORAGE_LIMITS;
+  grantObj.limitSpeed = GRANT_WRITING_SPEED_LIMITS;
+  grantObj.limitTimeSeries = GRANT_TIME_SERIES_LIMITS;
+  grantObj.limitQueryTime = GRANT_QUERY_TIME_LIMITS;
+  grantObj.limitDbs = GRANT_DATABASE_LIMITS;
+  grantObj.limitUsers = GRANT_USER_LIMITS;
+  grantObj.limitConns = GRANT_CONNECTION_LIMITS;
+  grantObj.limitStreams = GRANT_STREAM_LIMITS;
+  grantObj.limitAccts = GRANT_ACCT_LIMITS;
+  grantObj.limitDnodes = GRANT_DNODE_LIMITS;
+  grantObj.limitCpuCores = GRANT_CPU_LIMITS;
+}
 
 char *grantGetMachineSerials() { return "1234567890"; }
