aaef89ef6e6fde2a2128bbb8809aa1aeada3652f
Hui Li <huili@taosdata.com>
2021-01-20 15:21:00 +0800

    [TD-2774]<feature> tdengine docker image support muliti platform

--- enterprise/packaging/docker_generate.sh
+++ enterprise/packaging/docker_generate.sh
@@ -2,7 +2,8 @@
 #
 version=$1
 password=$2
-dockerinput=$3
+pkgFile=$3
+cpuType=$4
 
 scriptDir=`pwd`
 topDir=$scriptDir/../..         # TDinternal
@@ -11,10 +12,8 @@ archiveDir=$scriptDir/../release
 
 echo "make docker for community version >>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 cd $communityDir/packaging/docker
-cp -f $archiveDir/v$version/community/${dockerinput} ./tdengine.tar.gz
-./dockerbuild.sh ${version} ${password}
-echo ">>>>>>>>>>>>> check whether the docker image has been published"
-docker pull tdengine/tdengine:${version}
+cp -f $archiveDir/v$version/community/${pkgFile} .
 
-docker tag tdengine/tdengine:$version tdengine/tdengine:latest
-docker push tdengine/tdengine:latest
+./dockerbuild.sh -c ${cpuType} -f ${pkgFile} -n ${version} -p ${password}
+
+rm -f ${pkgFile}
