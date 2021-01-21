2761876bcdb4d73ab97444043f869f1226ef4837
zyyang <zyyang@taosdata.com>
2021-01-21 19:40:11 +0800

    change

--- src/connector/jdbc/src/test/java/com/taosdata/jdbc/ImportTest.java
+++ src/connector/jdbc/src/test/java/com/taosdata/jdbc/ImportTest.java
@@ -10,7 +10,7 @@ import static org.junit.Assert.assertEquals;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ImportTest {
-    Connection connection;
+    static Connection connection;
     String dbName = "test";
     String tName = "t0";
     String host = "127.0.0.1";
