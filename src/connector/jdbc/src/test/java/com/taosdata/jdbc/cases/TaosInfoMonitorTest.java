b5eebd099caa4bd6d69baceb06d791c7675b8aea
zyyang <zyyang@taosdata.com>
2021-01-29 15:24:16 +0800

    change

--- src/connector/jdbc/src/test/java/com/taosdata/jdbc/cases/TaosInfoMonitorTest.java
+++ src/connector/jdbc/src/test/java/com/taosdata/jdbc/cases/TaosInfoMonitorTest.java
@@ -13,9 +13,8 @@ import java.util.stream.IntStream;
 public class TaosInfoMonitorTest {
 
     @Test
-    public void testCreateTooManyConnection() throws ClassNotFoundException, SQLException, InterruptedException {
+    public void testCreateTooManyConnection() throws ClassNotFoundException {
         Class.forName("com.taosdata.jdbc.TSDBDriver");
-        int conCnt = 0;
         final String url = "jdbc:TAOS://127.0.0.1:6030/?user=root&password=taosdata";
 
         List<Connection> connectionList = IntStream.range(0, 100).mapToObj(i -> {
@@ -27,6 +26,7 @@ public class TaosInfoMonitorTest {
             }
             return null;
         }).collect(Collectors.toList());
+
         connectionList.stream().forEach(conn -> {
             try {
                 conn.close();
