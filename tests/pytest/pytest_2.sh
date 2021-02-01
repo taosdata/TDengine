15c83944d3e3777e91cec54b354c8008e0166667
Shengliang Guan <slguan@taosdata.com>
2021-02-01 17:06:40 +0800

    TD-1207

--- tests/pytest/pytest_2.sh
+++ tests/pytest/pytest_2.sh
@@ -7,12 +7,12 @@ python3 ./test.py -f update/append_commit_data.py
 python3 ./test.py -f update/append_commit_last-0.py
 python3 ./test.py -f update/append_commit_last.py
 #python3 ./test.py -f update/merge_commit_data.py
-python3 ./test.py -f update/merge_commit_data-0.py
-python3 ./test.py -f update/merge_commit_data2.py
-python3 ./test.py -f update/merge_commit_data2_update0.py
-python3 ./test.py -f update/merge_commit_last-0.py
-python3 ./test.py -f update/merge_commit_last.py
-python3 ./test.py -f update/bug_td2279.py
+#python3 ./test.py -f update/merge_commit_data-0.py
+#python3 ./test.py -f update/merge_commit_data2.py
+#python3 ./test.py -f update/merge_commit_data2_update0.py
+#python3 ./test.py -f update/merge_commit_last-0.py
+#python3 ./test.py -f update/merge_commit_last.py
+#python3 ./test.py -f update/bug_td2279.py
 
 # wal
 python3 ./test.py -f wal/addOldWalTest.py
