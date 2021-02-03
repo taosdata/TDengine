8b6b8192dc14ea42e16e9d45f08610b88aa41da4
zyyang <zyyang@taosdata.com>
2021-02-03 19:36:10 +0800

    change

--- src/connector/jdbc/src/main/java/com/taosdata/jdbc/AbstractStatement.java
+++ src/connector/jdbc/src/main/java/com/taosdata/jdbc/AbstractStatement.java
@@ -246,17 +246,17 @@ public abstract class AbstractStatement implements Statement {
         return this.closeOnCompletion;
     }
 
-//    @Override
-//    public <T> T unwrap(Class<T> iface) throws SQLException {
-//        try {
-//            return iface.cast(this);
-//        } catch (ClassCastException cce) {
-//            throw new SQLException("Unable to unwrap to " + iface.toString());
-//        }
-//    }
-//
-//    @Override
-//    public boolean isWrapperFor(Class<?> iface) throws SQLException {
-//        return iface.isInstance(this);
-//    }
+    @Override
+    public <T> T unwrap(Class<T> iface) throws SQLException {
+        try {
+            return iface.cast(this);
+        } catch (ClassCastException cce) {
+            throw new SQLException("Unable to unwrap to " + iface.toString());
+        }
+    }
+
+    @Override
+    public boolean isWrapperFor(Class<?> iface) throws SQLException {
+        return iface.isInstance(this);
+    }
 }
