7c0c4e60a8dd194f2684f0b7f808d2b293e24069
Shuduo Sang <sangshuduo@gmail.com>
2021-02-27 21:56:24 +0800

    [TD-2210] <fix>: use LWP tid. remove few darwin-related code.

--- src/client/src/tscUtil.c
+++ src/client/src/tscUtil.c
@@ -2578,11 +2578,7 @@ bool tscSetSqlOwner(SSqlObj* pSql) {
   SSqlRes* pRes = &pSql->res;
 
   // set the sql object owner
-#ifdef __APPLE__
-  pthread_t threadId = (pthread_t)taosGetSelfPthreadId();
-#else // __APPLE__
   int64_t threadId = taosGetSelfPthreadId();
-#endif // __APPLE__
   if (atomic_val_compare_exchange_64(&pSql->owner, 0, threadId) != 0) {
     pRes->code = TSDB_CODE_QRY_IN_EXEC;
     return false;
