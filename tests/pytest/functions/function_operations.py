b6d80466b74960495013b87cc2a442d073fee23b
liuyq-617 <yqliu@taosdata.com>
2021-03-05 18:51:14 +0800

    inc code coverage

--- tests/pytest/functions/function_operations.py
+++ tests/pytest/functions/function_operations.py
@@ -95,11 +95,12 @@ class TDTestCase:
         for i in col_list :
             for j in col_list :
                 for k in op_list :
-                    sql = " select %s %s %s from test1 " % ( i , k , j )
-                    if i in err_list or j in err_list:
-                        tdSql.error(sql)
-                    else:
-                        tdSql.query(sql)
+                    for l in [' order by ts' , ' order by ts desc' ]:
+                        sql = " select %s %s %s from test1 %s" % ( i , k , j , l )
+                        if i in err_list or j in err_list:
+                            tdSql.error(sql)
+                        else:
+                            tdSql.query(sql)
     def stop(self):
         tdSql.close()
         tdLog.success("%s successfully executed" % __file__)
