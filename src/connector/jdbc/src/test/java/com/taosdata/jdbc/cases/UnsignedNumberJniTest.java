54c2a43842b1c2034ae50cdaa1aaa16e982687e9
zyyang <zyyang@taosdata.com>
2021-03-13 18:09:49 +0800

    change

--- src/connector/jdbc/src/test/java/com/taosdata/jdbc/cases/UnsignedNumberJniTest.java
+++ src/connector/jdbc/src/test/java/com/taosdata/jdbc/cases/UnsignedNumberJniTest.java
@@ -14,12 +14,12 @@ import java.util.Properties;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class UnsignedNumberJniTest {
     private static final String host = "127.0.0.1";
-    private static Connection jniConn;
+    private static Connection conn;
 
     @Test
     public void testCase001() {
-        try (Statement stmt = jniConn.createStatement()) {
-            ResultSet rs = stmt.executeQuery("select * from us_test");
+        try (Statement stmt = conn.createStatement()) {
+            ResultSet rs = stmt.executeQuery("select * from us_table");
             ResultSetMetaData meta = rs.getMetaData();
             while (rs.next()) {
                 for (int i = 1; i <= meta.getColumnCount(); i++) {
@@ -29,6 +29,97 @@ public class UnsignedNumberJniTest {
             }
         } catch (SQLException e) {
             e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testCase002() {
+        try (Statement stmt = conn.createStatement()) {
+            long now = System.currentTimeMillis();
+            ResultSet rs = stmt.executeQuery("select * from us_table");
+            ResultSetMetaData meta = rs.getMetaData();
+            while (rs.next()) {
+                System.out.print(meta.getColumnLabel(1) + ": " + rs.getTimestamp(1) + "\t");
+                System.out.print(meta.getColumnLabel(2) + ": " + rs.getByte(2) + "\t");
+                System.out.print(meta.getColumnLabel(3) + ": " + rs.getShort(3) + "\t");
+                System.out.print(meta.getColumnLabel(4) + ": " + rs.getInt(4) + "\t");
+                System.out.print(meta.getColumnLabel(5) + ": " + rs.getLong(5) + "\t");
+                System.out.println();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test(expected = SQLException.class)
+    public void testCase003() throws SQLException {
+        try (Statement stmt = conn.createStatement()) {
+            long now = System.currentTimeMillis();
+            stmt.executeUpdate("insert into us_table(ts,f1,f2,f3,f4) values(" + now + ", 127, 32767,2147483647, 18446744073709551614)");
+            ResultSet rs = stmt.executeQuery("select * from us_table where ts = " + now);
+            ResultSetMetaData meta = rs.getMetaData();
+            while (rs.next()) {
+                System.out.print(meta.getColumnLabel(1) + ": " + rs.getTimestamp(1) + "\t");
+                System.out.print(meta.getColumnLabel(2) + ": " + rs.getByte(2) + "\t");
+                System.out.print(meta.getColumnLabel(3) + ": " + rs.getShort(3) + "\t");
+                System.out.print(meta.getColumnLabel(4) + ": " + rs.getInt(4) + "\t");
+                System.out.print(meta.getColumnLabel(5) + ": " + rs.getLong(5) + "\t");
+                System.out.println();
+            }
+        }
+    }
+
+    @Test(expected = SQLException.class)
+    public void testCase004() throws SQLException {
+        try (Statement stmt = conn.createStatement()) {
+            long now = System.currentTimeMillis();
+            stmt.executeUpdate("insert into us_table(ts,f1,f2,f3,f4) values(" + now + ", 127, 32767,4294967294, 18446744073709551614)");
+            ResultSet rs = stmt.executeQuery("select * from us_table where ts = " + now);
+            ResultSetMetaData meta = rs.getMetaData();
+            while (rs.next()) {
+                System.out.print(meta.getColumnLabel(1) + ": " + rs.getTimestamp(1) + "\t");
+                System.out.print(meta.getColumnLabel(2) + ": " + rs.getByte(2) + "\t");
+                System.out.print(meta.getColumnLabel(3) + ": " + rs.getShort(3) + "\t");
+                System.out.print(meta.getColumnLabel(4) + ": " + rs.getInt(4) + "\t");
+                System.out.print(meta.getColumnLabel(5) + ": " + rs.getLong(5) + "\t");
+                System.out.println();
+            }
+        }
+    }
+
+    @Test(expected = SQLException.class)
+    public void testCase005() throws SQLException {
+        try (Statement stmt = conn.createStatement()) {
+            long now = System.currentTimeMillis();
+            stmt.executeUpdate("insert into us_table(ts,f1,f2,f3,f4) values(" + now + ", 127, 65534,4294967294, 18446744073709551614)");
+            ResultSet rs = stmt.executeQuery("select * from us_table where ts = " + now);
+            ResultSetMetaData meta = rs.getMetaData();
+            while (rs.next()) {
+                System.out.print(meta.getColumnLabel(1) + ": " + rs.getTimestamp(1) + "\t");
+                System.out.print(meta.getColumnLabel(2) + ": " + rs.getByte(2) + "\t");
+                System.out.print(meta.getColumnLabel(3) + ": " + rs.getShort(3) + "\t");
+                System.out.print(meta.getColumnLabel(4) + ": " + rs.getInt(4) + "\t");
+                System.out.print(meta.getColumnLabel(5) + ": " + rs.getLong(5) + "\t");
+                System.out.println();
+            }
+        }
+    }
+
+    @Test(expected = SQLException.class)
+    public void testCase006() throws SQLException {
+        try (Statement stmt = conn.createStatement()) {
+            long now = System.currentTimeMillis();
+            stmt.executeUpdate("insert into us_table(ts,f1,f2,f3,f4) values(" + now + ", 254, 65534,4294967294, 18446744073709551614)");
+            ResultSet rs = stmt.executeQuery("select * from us_table where ts = " + now);
+            ResultSetMetaData meta = rs.getMetaData();
+            while (rs.next()) {
+                System.out.print(meta.getColumnLabel(1) + ": " + rs.getTimestamp(1) + "\t");
+                System.out.print(meta.getColumnLabel(2) + ": " + rs.getByte(2) + "\t");
+                System.out.print(meta.getColumnLabel(3) + ": " + rs.getShort(3) + "\t");
+                System.out.print(meta.getColumnLabel(4) + ": " + rs.getInt(4) + "\t");
+                System.out.print(meta.getColumnLabel(5) + ": " + rs.getLong(5) + "\t");
+                System.out.println();
+            }
         }
     }
 
@@ -42,16 +133,15 @@ public class UnsignedNumberJniTest {
         try {
             Class.forName("com.taosdata.jdbc.TSDBDriver");
             final String url = "jdbc:TAOS://" + host + ":6030/?user=root&password=taosdata";
-            jniConn = DriverManager.getConnection(url, properties);
-            try (Statement stmt = jniConn.createStatement()) {
-                stmt.execute("drop database if exists unsigned_jni");
-                stmt.execute("create database if not exists unsigned_jni");
-                stmt.execute("use unsigned_jni");
-                stmt.execute("create table us_test(ts timestamp, f1 tinyint unsigned, f2 smallint unsigned, f3 int unsigned, f4 bigint unsigned)");
-                stmt.executeUpdate("insert into us_test(ts,f1,f2,f3,f4) values(now, 254, 65534,4294967294, 18446744073709551614)");
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+            conn = DriverManager.getConnection(url, properties);
+
+            Statement stmt = conn.createStatement();
+            stmt.execute("drop database if exists unsign_restful");
+            stmt.execute("create database if not exists unsign_restful");
+            stmt.execute("use unsign_restful");
+            stmt.execute("create table us_table(ts timestamp, f1 tinyint unsigned, f2 smallint unsigned, f3 int unsigned, f4 bigint unsigned)");
+            stmt.executeUpdate("insert into us_table(ts,f1,f2,f3,f4) values(now, 127, 32767,2147483647, 9223372036854775807)");
+            stmt.close();
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
@@ -60,10 +150,11 @@ public class UnsignedNumberJniTest {
     @AfterClass
     public static void afterClass() {
         try {
-            if (jniConn != null)
-                jniConn.close();
+            if (conn != null)
+                conn.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
+
 }
