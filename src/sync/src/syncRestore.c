7d232a7914c2a1b7869798ca56f648486f5666e7
Shengliang Guan <slguan@taosdata.com>
2021-03-16 05:19:14 +0000

    TD-3309

--- src/sync/src/syncRestore.c
+++ src/sync/src/syncRestore.c
@@ -238,7 +238,7 @@ static int32_t syncRestoreDataStepByStep(SSyncPeer *pPeer) {
 
   (*pNode->stopSyncFileFp)(pNode->vgId, fversion);
   nodeVersion = fversion;
-  (*pNode->resetVersionFp)(pNode->vgId, fversion);
+  if (pNode->resetVersionFp) (*pNode->resetVersionFp)(pNode->vgId, fversion);
 
   sInfo("%s, start to restore wal, fver:%" PRIu64, pPeer->id, nodeVersion);
   uint64_t wver = 0;
