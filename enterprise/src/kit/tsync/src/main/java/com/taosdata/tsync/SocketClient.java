910e4846479c573f3f311b639b0e938cc749c577
zyyang <zyyang@taosdata.com>
2021-03-17 17:47:31 +0800

    socket client and server

--- enterprise/src/kit/tsync/src/main/java/com/taosdata/tsync/SocketClient.java
+++ enterprise/src/kit/tsync/src/main/java/com/taosdata/tsync/SocketClient.java
@@ -1,2 +1,19 @@
-package com.taosdata.tsync;public class SocketClient {
+package com.taosdata.tsync;
+
+import java.io.*;
+import java.net.Socket;
+
+public class SocketClient {
+
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("192.168.1.208", 8899);
+
+        PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(new BufferedOutputStream(socket.getOutputStream()))));
+        for (int i = 0; i < 10; i++) {
+            out.println("[" + i + "] : this is a test String ");
+            out.flush();
+        }
+
+        socket.close();
+    }
 }
--- enterprise/src/kit/tsync/src/main/java/com/taosdata/tsync/SocketServer.java
+++ enterprise/src/kit/tsync/src/main/java/com/taosdata/tsync/SocketServer.java
@@ -1,2 +1,24 @@
-package com.taosdata.tsync;public class SocketServer {
+package com.taosdata.tsync;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SocketServer {
+
+    public static void main(String[] args) throws IOException {
+        final int port = 8899;
+        ServerSocket ss = new ServerSocket(port);
+        Socket sock = ss.accept();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new BufferedInputStream(sock.getInputStream())));
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+            System.out.println("RECV >>> " + line);
+        }
+
+        sock.close();
+        ss.close();
+    }
+
 }
