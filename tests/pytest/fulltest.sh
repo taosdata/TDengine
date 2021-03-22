5d2fa94960004b718173c1db4b35c5a96343abab
wu champion <72908628+wu-champion@users.noreply.github.com>
2021-03-22 18:51:49 +0800

    Update fulltest.sh

--- tests/pytest/fulltest.sh
+++ tests/pytest/fulltest.sh
@@ -196,8 +196,6 @@ python3 ./test.py -f query/bug2119.py
 python3 ./test.py -f query/isNullTest.py
 python3 ./test.py -f query/queryWithTaosdKilled.py
 python3 ./test.py -f query/floatCompare.py
-python3 ./test.py -f query/query1970YearsAf.py
-python3 ./test.py -f query/bug3351.py
 python3 ./test.py -f query/bug3375.py
 
 
