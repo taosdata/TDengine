2e7cb6c367de3099df0e409b7b0fcae1e72a8947
Haojun Liao <hjliao@taosdata.com>
2021-03-30 14:20:35 +0800

    [td-2819] refactor codes.

--- enterprise/tests/test/perftest/queryTest.c
+++ enterprise/tests/test/perftest/queryTest.c
@@ -328,8 +328,8 @@ void generatedData(TAOS* taos) {
 }
 
 int main(int argc, char** argv) {
-  taos_options(TSDB_OPTION_CONFIGDIR, "~/first/cfg");
-//  taos_options(TSDB_OPTION_CONFIGDIR, "/home/lisa/Documents/workspace/TDinternal/sim/tsim/cfg");
+//  taos_options(TSDB_OPTION_CONFIGDIR, "~/first/cfg");
+  taos_options(TSDB_OPTION_CONFIGDIR, "/home/lisa/Documents/workspace/TDinternal/sim/tsim/cfg");
 //  taos_options(TSDB_OPTION_CONFIGDIR, "/home/lisa/Documents/workspace/TDinternal/community/sim/psim/cfg");
   taos_init();
   TAOS* conn = taos_connect("ubuntu", "root", "taosdata", 0, 0);
@@ -385,8 +385,10 @@ int main(int argc, char** argv) {
 //      TAOS_RES*  taos_query(conn, t);
 //      taos_free_result(res);
 //    }
-    executeSQL(conn, "use test", NULL);
-    executeSQL(conn, "select count(*) from t1", NULL);
+    executeSQL(conn, "use group_db0", NULL);
+    executeSQL(conn, "select stddev(c) from st group by c;", NULL);
+
+//    executeSQL(conn, "select top(c1, 1) from lm_stb0 where ts >= 1537146000000 and ts <= 1537151400009 limit 5 offset 1;", NULL);
 //    executeSQL(conn, "select count(*) from t2m1 where ts<'2015-6-11 1:1:1.3' and ts>='2015-6-11 1:1:1' interval(20a) fill(next)", NULL);
 //    executeSQL(conn, "describe tux1", NULL);
 //    createEnvironment(conn, 2, 2, 10000, 30);
