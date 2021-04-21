6038a4791e5c87147e5e147692aa7645f34ea077
wu champion <wuchampion@foxmail.com>
2021-04-21 14:56:38 +0800

    [TD-3891] <test> fix the bug: not 1st timestamp column,when insert data < 0,data process error

--- tests/pytest/fulltest.sh
+++ tests/pytest/fulltest.sh
@@ -238,6 +238,7 @@ python3 ./test.py -f stream/table_n.py
 #alter table
 python3 ./test.py -f alter/alter_table_crash.py
 python3 ./test.py -f alter/alterTabAddTagWithNULL.py
+python3 ./test.py -f alter/alterTimestampColDataProcess.py
 
 # client
 python3 ./test.py -f client/client.py
