733001f633b1bed10bd3e8469d5352fdba802bb4
Shengliang Guan <slguan@taosdata.com>
2021-05-09 15:47:07 +0800

    TD-4088

--- src/os/inc/osInc.h
+++ src/os/inc/osInc.h
@@ -13,8 +13,8 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef TDENGINE_OS_LINUX64_H
-#define TDENGINE_OS_LINUX64_H
+#ifndef TDENGINE_OS_INC_H
+#define TDENGINE_OS_INC_H
 
 #ifdef __cplusplus
 extern "C" {
