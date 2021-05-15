dae9262562cedbb6c6c61448f39342fb5f7e2b2e
Minglei Jin <stephenk@gmx.us>
2021-05-15 20:42:48 +0800

    vnode/read: use app not ready instead of invalid vgroup id to make crash_gen happy

--- src/vnode/src/vnodeRead.c
+++ src/vnode/src/vnodeRead.c
@@ -118,9 +118,8 @@ static SVReadMsg *vnodeBuildVReadMsg(SVnodeObj *pVnode, void *pCont, int32_t con
 
 int32_t vnodeWriteToRQueue(void *vparam, void *pCont, int32_t contLen, int8_t qtype, void *rparam) {
   SVnodeObj *pVnode = vparam;
-
   if (pVnode->dropped) {
-    return TSDB_CODE_VND_INVALID_VGROUP_ID;
+    return TSDB_CODE_APP_NOT_READY;
   }
 
   SVReadMsg *pRead = vnodeBuildVReadMsg(vparam, pCont, contLen, qtype, rparam);
