b30ee5eb0c9f3d0e94445da3ae5a0f173012723b
Shuduo Sang <sdsang@taosdata.com>
2021-05-21 11:12:27 +0800

    [TD-3913]<fix>: mips compile support.
    verified on real loongson machine.

--- src/os/src/detail/osSignal.c
+++ src/os/src/detail/osSignal.c
@@ -23,14 +23,14 @@
 typedef void (*FLinuxSignalHandler)(int32_t signum, siginfo_t *sigInfo, void *context);
 
 void taosSetSignal(int32_t signum, FSignalHandler sigfp) {
-  struct sigaction act = {{0}};	
+  struct sigaction act = {0};
 #if 1
   act.sa_flags = SA_SIGINFO;
   act.sa_sigaction = (FLinuxSignalHandler)sigfp;
 #else  
-  act.sa_handler = sigfp;	
+  act.sa_handler = sigfp;
 #endif  
-  sigaction(signum, &act, NULL);		
+  sigaction(signum, &act, NULL);
 }
 
 void taosIgnSignal(int32_t signum) {
