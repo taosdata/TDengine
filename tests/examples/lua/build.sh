dc3826bbfab5529d969748b4fe35342f689c5ba5
Shuduo Sang <sdsang@taosdata.com>
2021-05-31 23:58:43 +0800

    make lua build.sh install necessary lib and specify include path.

--- tests/examples/lua/build.sh
+++ tests/examples/lua/build.sh
@@ -1,2 +1,7 @@
-gcc -std=c99 lua_connector.c -fPIC -shared -o luaconnector.so -Wall -ltaos
+lua_header_installed=`apt-cache policy liblua5.1-0-dev|grep Installed|grep none > /dev/null; echo $?`
+if [ "$lua_header_installed" = "0" ]; then
+  sudo apt install -y liblua5.1-0-dev
+fi
+
+gcc -std=c99 lua_connector.c -fPIC -shared -o luaconnector.so -Wall -ltaos -I/usr/include/lua5.1
 
