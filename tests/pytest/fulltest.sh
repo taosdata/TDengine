50d9a1c5a5f3ab85c3b90db96c55f16b360e1e10
bryanchang0603 <changb9@mcmaster.ca>
2021-05-31 11:11:02 +0800

    [TD-4439] add test case

--- tests/pytest/fulltest.sh
+++ tests/pytest/fulltest.sh
@@ -334,5 +334,6 @@ python3 ./test.py -f tag_lite/alter_tag.py
 
 python3 test.py -f tools/taosdemoAllTest/taosdemoTestInsertWithJson.py 
 python3 test.py -f tools/taosdemoAllTest/taosdemoTestQueryWithJson.py
+python3 test.py -f insert/insert_before_use_db.py
 
 #======================p4-end===============
