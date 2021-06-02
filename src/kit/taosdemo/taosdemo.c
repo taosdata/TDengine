9cdacf3bb6662400043d7cc76b6ec9cbcf8e29a5
zyyang-taosdata <zyyang@taosdata.com>
2021-06-02 21:12:20 +0800

    change

--- src/kit/taosdemo/taosdemo.c
+++ src/kit/taosdemo/taosdemo.c
@@ -579,7 +579,7 @@ SArguments g_args = {
                      "root",          // user
                      #ifdef _TD_POWER_
                      "powerdb",      // password
-                     #elseif _TD_TQ_
+                     #elif (_TD_TQ_ == true)
                      "tqueue",      // password
                      #else
                      "taosdata",      // password
@@ -684,7 +684,7 @@ static void printHelp() {
           "The password to use when connecting to the server. Default is 'powerdb'.");
   printf("%s%s%s%s\n", indent, "-c", indent,
           "Configuration directory. Default is '/etc/power/'.");
-#elseif _TD_TQ_
+#elif (_TD_TQ_ == true)
 	printf("%s%s%s%s\n", indent, "-P", indent,
 			"The password to use when connecting to the server. Default is 'tqueue'.");
 	printf("%s%s%s%s\n", indent, "-c", indent,
