2c83206d657dd94ddd16b160575e0bd8018d3d87
tomchon <haoran920c@163.com>
2021-06-09 19:26:17 +0800

    [TD-4314]<test>: add testcase of nested query  with top/bottom function

--- tests/pytest/query/nestedQuery/queryInterval.py
+++ tests/pytest/query/nestedQuery/queryInterval.py
@@ -44,7 +44,7 @@ class TDTestCase:
                 
         # interval function
         tdSql.query("select avg(value) from st interval(10m)")
-        print(tdSql.queryResult)
+        # print(tdSql.queryResult)
         tdSql.checkRows(6)        
         tdSql.checkData(0, 0, "2020-07-01 04:20:00")
         tdSql.checkData(1, 1, 107.4)
@@ -54,37 +54,38 @@ class TDTestCase:
         tdSql.checkData(0, 0, 109.0)
 
         # subquery with interval and select two Column in parent query
-        # tdSql.query("select ts,avg(avg_val) from(select avg(value) as avg_val from st where loc='beijing0' interval(10m));")
+        tdSql.error("select ts,avg(avg_val) from(select avg(value) as avg_val from st where loc='beijing0' interval(10m));")
 
         # subquery with interval and sliding 
         tdSql.query("select avg(value) as avg_val from st where loc='beijing0' interval(8m) sliding(30s) limit 1;")   
         tdSql.checkData(0, 0, "2020-07-01 04:17:00")
-        tdSql.checkData(0, 1, 107.4) 
+        tdSql.checkData(0, 1, 100) 
         tdSql.query("select avg(avg_val) from(select avg(value) as avg_val from st where loc='beijing1' interval(8m) sliding(30s));")
         tdSql.checkData(0, 0, 111)
         
         # subquery with interval and offset
         tdSql.query("select avg(value) as avg_val from st where loc='beijing0' interval(5m,1m);")
         tdSql.checkData(0, 0, "2020-07-01 04:21:00")
-        tdSql.query("select avg(avg_val) from(select avg(value) as avg_val from st where loc='beijing0' interval(5m,1m));")
-        tdSql.checkData(0, 0, 111)
+        tdSql.checkData(0, 1, 100)
+        tdSql.query("select avg(avg_val) from(select avg(value) as avg_val from st where loc='beijing0' interval(5m,1m) group by loc);")
+        tdSql.checkData(0, 0, 109)
 
         # subquery  with interval,sliding and group by ; parent query with interval
         tdSql.query("select avg(value) as avg_val from st where loc='beijing0' interval(8m) sliding(1m) group by loc limit 1 offset 52 ;")
         tdSql.checkData(0, 0, "2020-07-01 05:09:00")
         tdSql.checkData(0, 1, 118)
-        tdSql.query("select avg(avg_val) from(select avg(value) as avg_val from st where loc!='beijing0' interval(8m) sliding(1m)  group by loc )interval(5m);")
-        tdSql.checkData(0, 0, 111)
+        tdSql.query("select avg(avg_val) as ncst from(select avg(value) as avg_val from st where loc!='beijing0' interval(8m) sliding(1m)  group by loc )  interval(5m);")
+        tdSql.checkData(1, 1, 105)
 
         # #  subquery and parent query with interval and sliding 
-        # tdSql.query("select avg(avg_val) from(select avg(value) as avg_val from st where loc='beijing1' interval(8m) sliding(30s)) interval(5m) sliding(1s);")
-        # tdSql.checkData(0, 0, 111)
+        tdSql.query("select avg(avg_val) from(select avg(value) as avg_val from st where loc='beijing1' interval(8m) sliding(5m)) interval(10m) sliding(2m);")
+        tdSql.checkData(29, 0, "2020-07-01 05:10:00.000")
 
         # subquery and parent query with top and bottom
         tdSql.query("select top(avg_val,2) from(select avg(value) as avg_val,num from st where loc!='beijing0' group by num);")
-        tdSql.checkData(0, 0, 111)
+        tdSql.checkData(0, 1, 115)
         tdSql.query("select bottom(avg_val,2) from(select avg(value) as avg_val,num from st where loc!='beijing0' group by num);")
-        tdSql.checkData(0, 0, 111)
+        tdSql.checkData(1, 1, 111)
 
 
 
