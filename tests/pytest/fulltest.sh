67d6339f1a4c2072a34d2d6afdc3b654feb774ac
bryanchang0603 <changb9@mcamster.ca>
2021-06-15 12:48:02 +0800

    [4711] uncomment the test case in fulltest.sh

--- tests/pytest/fulltest.sh
+++ tests/pytest/fulltest.sh
@@ -346,6 +346,6 @@ python3 test.py -f alter/alter_keep.py
 python3 test.py -f alter/alter_cacheLastRow.py
 python3 ./test.py -f query/querySession.py 
 python3 test.py -f  alter/alter_create_exception.py
-# python3 test.py -f client/change_time_1_1.py
-# python3 test.py -f client/change_time_1_2.py
+python3 test.py -f client/change_time_1_1.py
+python3 test.py -f client/change_time_1_2.py
 #======================p4-end===============
