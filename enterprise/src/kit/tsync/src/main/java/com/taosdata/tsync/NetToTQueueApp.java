91f0a0f3d7e16ee9f1e4338ae520102447c3a1a3
zyyang <zyyang@taosdata.com>
2021-06-21 15:29:33 +0800

    change

--- enterprise/src/kit/tsync/src/main/java/com/taosdata/tsync/NetToTQueueApp.java
+++ enterprise/src/kit/tsync/src/main/java/com/taosdata/tsync/NetToTQueueApp.java
@@ -14,37 +14,26 @@ import org.slf4j.LoggerFactory;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 
 public class NetToTQueueApp extends AbstractApp {
     private static final Logger logger = LoggerFactory.getLogger(NetToTQueueApp.class);
     private static final String helpLine = "Usage: java -jar net-to-tqueue.jar --config <config file path>";
 
     public static void main(String[] args) throws IOException {
+        File configFile = readCommandLine(args, helpLine);
 
-        long start = System.nanoTime();
-        try {
-            TimeUnit.SECONDS.sleep(1);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        long end = System.nanoTime();
-        System.out.println("start: " + start + ",end: " + end + ", cost: " + (end - start) + " ns");
-
-//        File configFile = readCommandLine(args, helpLine);
-//
-//        logger.info("NetToTQueueApp started.");
-//        // given
-//        ConfigurationRepository configurationRepository = ConfigurationRepository.getInstance();
-//        JSONObject configJSON = JSONObject.parseObject(IOUtils.toString(new FileInputStream(configFile)));
-//        // when
-//        Job job = JobFactory.build(ConfigurationType.NET_TO_TQUEUE, configJSON, configurationRepository);
-//        // when
-//        JobService jobService = new NetToTQueueJobServiceImpl();
-//        job.prepare(jobService);
-//        // when
-//        job.execute(jobService);
-//        logger.info("NetToTQueueApp stopped.");
+        logger.info("NetToTQueueApp started.");
+        // given
+        ConfigurationRepository configurationRepository = ConfigurationRepository.getInstance();
+        JSONObject configJSON = JSONObject.parseObject(IOUtils.toString(new FileInputStream(configFile)));
+        // when
+        Job job = JobFactory.build(ConfigurationType.NET_TO_TQUEUE, configJSON, configurationRepository);
+        // when
+        JobService jobService = new NetToTQueueJobServiceImpl();
+        job.prepare(jobService);
+        // when
+        job.execute(jobService);
+        logger.info("NetToTQueueApp stopped.");
     }
 
 
