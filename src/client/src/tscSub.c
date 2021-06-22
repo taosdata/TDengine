8ae2c04c36739473dbd71a5b41f7ce68ddb35bb3
wpan <wpan@taosdata.com>
2021-06-22 14:57:58 +0800

    fix bug

--- src/client/src/tscSub.c
+++ src/client/src/tscSub.c
@@ -509,6 +509,7 @@ TAOS_RES *taos_consume(TAOS_SUB *tsub) {
     }
     pSub->pSql = pSql;
     pSql->pSubscription = pSub;
+    pSub->lastSyncTime = 0;
 
     // no table list now, force to update it
     tscDebug("begin table synchronization");
