aff2770a4c2ac24473fb407e263e1d0bfca230db
XYWang <xywang@taosdata.com>
2021-07-20 16:46:27 +0800

    [TD-5234]<fix>: fixed a compatibility bug on MacOS

--- src/os/inc/osDef.h
+++ src/os/inc/osDef.h
@@ -214,7 +214,12 @@ extern "C" {
 #if defined(_TD_LINUX_64) || defined(_TD_LINUX_32) || defined(_TD_MIPS_64)  || defined(_TD_ARM_32) || defined(_TD_ARM_64)  || defined(_TD_DARWIN_64)
   #if defined(_TD_DARWIN_64)
     // MacOS
-    #define setThreadName(name) do { pthread_setname_np((name)); } while (0)
+    #if !defined(_GNU_SOURCE)
+      #define setThreadName(name) do { pthread_setname_np((name)); } while (0)
+    #else
+      // pthread_setname_np not defined
+      #define setThreadName(name)
+    #endif
   #else
     // Linux, length of name must <= 16 (the last '\0' included)
     #define setThreadName(name) do { prctl(PR_SET_NAME, (name)); } while (0)
