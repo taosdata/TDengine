a1a692fa91f9d4241368923df47b6a9d43d63423
Shuduo Sang <sangshuduo@gmail.com>
2021-07-21 14:58:54 +0800

    set thread name disabled on master branch.

--- src/kit/taosdemo/taosdemo.c
+++ src/kit/taosdemo/taosdemo.c
@@ -55,6 +55,11 @@
 
 #define STMT_IFACE_ENABLED  0
 #define NANO_SECOND_ENABLED  0
+#define SET_THREADNAME_ENABLED  0
+
+#if SET_THREADNAME_ENABLED == 0
+#define setThreadName(name)
+#endif
 
 #define REQ_EXTRA_BUF_LEN   1024
 #define RESP_BUF_LEN        4096
