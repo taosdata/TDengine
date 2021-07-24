7409fda3a266f9090067ae8beb95163bed4b1d68
Haojun Liao <hjliao@taosdata.com>
2021-07-24 14:47:37 +0800

    [td-225] update the thread name

--- src/query/src/qExecutor.c
+++ src/query/src/qExecutor.c
@@ -6554,8 +6554,10 @@ static SSDataBlock* hashDistinct(void* param, bool* newgroup) {
     return NULL;
   }
 
+
   SDistinctOperatorInfo* pInfo = pOperator->info;
   SSDataBlock* pRes = pInfo->pRes;
+
 
   pRes->info.rows = 0;
   SSDataBlock* pBlock = NULL;
