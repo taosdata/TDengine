391b5c6a3a9615811e2aa4947c5b70e885e4709b
Liu Jicong <liujicong@qq.com>
2021-08-04 03:20:16 +0800

    [TD-5694]<fix>: finish

--- src/common/src/tdataformat.c
+++ src/common/src/tdataformat.c
@@ -375,8 +375,6 @@ int tdInitDataCols(SDataCols *pCols, STSchema *pSchema) {
 
   for (i = 0; i < schemaNCols(pSchema); i++) {
     dataColInit(pCols->cols + i, schemaColAt(pSchema, i), pCols->maxPoints);
-    pCols->cols[i].spaceSize = 0;
-    tdAllocMemForCol(pCols->cols + i, pCols->maxPoints);
   }
   
   return 0;
