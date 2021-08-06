6be0344507dfb60acfb64dce3eee3826ddee5bbb
zyyang <zyyang@taosdata.com>
2021-08-06 15:46:30 +0800

    add without password test case

--- src/connector/jdbc/src/test/java/com/taosdata/jdbc/cases/AuthenticationTest.java
+++ src/connector/jdbc/src/test/java/com/taosdata/jdbc/cases/AuthenticationTest.java
@@ -20,7 +20,7 @@ public class AuthenticationTest {
             DriverManager.getConnection("jdbc:TAOS://" + host + ":0/?");
         } catch (SQLException e) {
             Assert.assertEquals(TSDBErrorNumbers.ERROR_USER_IS_REQUIRED, e.getErrorCode());
-            Assert.assertEquals("ERROR (0x2339): user is required", e.getMessage());
+            Assert.assertEquals("ERROR (2319): user is required", e.getMessage());
         }
     }
 
@@ -30,7 +30,27 @@ public class AuthenticationTest {
             DriverManager.getConnection("jdbc:TAOS-RS://" + host + ":6041/?");
         } catch (SQLException e) {
             Assert.assertEquals(TSDBErrorNumbers.ERROR_USER_IS_REQUIRED, e.getErrorCode());
-            Assert.assertEquals("ERROR (0x2339): user is required", e.getMessage());
+            Assert.assertEquals("ERROR (2319): user is required", e.getMessage());
+        }
+    }
+
+    @Test
+    public void connectWithoutPasswordByJni() {
+        try {
+            DriverManager.getConnection("jdbc:TAOS://" + host + ":0/?user=root");
+        } catch (SQLException e) {
+            Assert.assertEquals(TSDBErrorNumbers.ERROR_USER_IS_REQUIRED, e.getErrorCode());
+            Assert.assertEquals("ERROR (231a): password is required", e.getMessage());
+        }
+    }
+
+    @Test
+    public void connectWithoutPasswordByRestful() {
+        try {
+            DriverManager.getConnection("jdbc:TAOS-RS://" + host + ":6041/?user=root");
+        } catch (SQLException e) {
+            Assert.assertEquals(TSDBErrorNumbers.ERROR_USER_IS_REQUIRED, e.getErrorCode());
+            Assert.assertEquals("ERROR (231a): password is required", e.getMessage());
         }
     }
 
