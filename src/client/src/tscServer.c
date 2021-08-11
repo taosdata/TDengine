08eda02c2cc0ffb5fe70c11f6e4d91dbc504cb02
Ganlin Zhao <ganlinzhao@hotmail.com>
2021-08-12 00:03:41 +0800

    [TD-5623]<feature>: fix malloc pointer issue

--- src/client/src/tscServer.c
+++ src/client/src/tscServer.c
@@ -2631,7 +2631,7 @@ int tscProcessQueryRsp(SSqlObj *pSql) {
 static void decompressQueryColData(SSqlRes *pRes, SQueryInfo* pQueryInfo, char *data, int8_t compressed, int compLen) {
   int32_t decompLen = 0;
   int32_t numOfCols = pQueryInfo->fieldsInfo.numOfOutput;
-  int32_t *compSizes = tcalloc(numOfCols, sizeof(int32_t));
+  int32_t *compSizes;
   char *pData = data;
   compSizes = (int32_t *)(pData + compLen);
 
@@ -2651,7 +2651,6 @@ static void decompressQueryColData(SSqlRes *pRes, SQueryInfo* pQueryInfo, char *
     pData += compSizes[i];
   }
   tfree(outputBuf);
-  tfree(compSizes);
 }
 
 int tscProcessRetrieveRspFromNode(SSqlObj *pSql) {
