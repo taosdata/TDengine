d7311c0eaa8efcb135ede7f8b852fee06e1ba028
wangmm0220 <wangmm0220@gmail.com>
2021-08-11 13:59:58 +0800

    [TD-5838]<feature> add function of speed test

--- src/kit/shell/inc/shell.h
+++ src/kit/shell/inc/shell.h
@@ -56,8 +56,8 @@ typedef struct SShellArguments {
   int   abort;
   int   port;
   int   pktLen;
-  int   pktNum；
-  char* pktType,
+  int   pktNum;
+  char* pktType;
   char* netTestRole;
 } SShellArguments;
 
