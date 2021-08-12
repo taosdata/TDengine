d4bf3b567d1da5932d5694ca55d517827e9f335f
shenglian zhou <shenglian_zhou@163.com>
2021-08-12 16:57:01 +0800

    [TD-5861]<feature>:add copyright and add quote to string

--- src/plugins/http/inc/httpMetricsHandle.h
+++ src/plugins/http/inc/httpMetricsHandle.h
@@ -1,7 +1,17 @@
-//
-// Created by slzhou on 8/6/21.
-//
-
+/*
+ * Copyright (c) 2019 TAOS Data, Inc. <jhtao@taosdata.com>
+ *
+ * This program is free software: you can use, redistribute, and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3
+ * or later ("AGPL"), as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef TDENGINE_HTTPMETRICSHANDLE_H
 #define TDENGINE_HTTPMETRICSHANDLE_H
 
--- src/plugins/http/src/httpMetricsHandle.c
+++ src/plugins/http/src/httpMetricsHandle.c
@@ -1,6 +1,17 @@
-//
-// Created by slzhou on 8/6/21.
-//
+/*
+ * Copyright (c) 2019 TAOS Data, Inc. <jhtao@taosdata.com>
+ *
+ * This program is free software: you can use, redistribute, and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3
+ * or later ("AGPL"), as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 #define _DEFAULT_SOURCE
 #include "os.h"
 #include "taoserror.h"
@@ -39,8 +50,8 @@ bool metricsProcessRequest(HttpContext* pContext) {
       httpJsonToken(jsonBuf, JsonObjStt);
       char* keyTagName = "name";
       char* keyTagValue = "value";
-      httpJsonPairOriginString(jsonBuf, keyTagName, (int32_t)strlen(keyTagName), "dnode_id",
-                               (int32_t)strlen("dnode_id"));
+      httpJsonPairOriginString(jsonBuf, keyTagName, (int32_t)strlen(keyTagName), "\"dnode_id\"",
+                               (int32_t)strlen("\"dnode_id\""));
       int32_t dnodeId = dnodeGetDnodeId();
       httpJsonPairIntVal(jsonBuf, keyTagValue, (int32_t)strlen(keyTagValue), dnodeId);
       httpJsonToken(jsonBuf, JsonObjEnd);
