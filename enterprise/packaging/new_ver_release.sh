a45ddf8a7fdf9658de7dcc227c0b67a4ea6fdd95
tomchon <haoran920c@163.com>
2021-08-17 00:52:52 +0800

    modify new_ver_release.sh about docker's dirname

--- enterprise/packaging/new_ver_release.sh
+++ enterprise/packaging/new_ver_release.sh
@@ -75,12 +75,14 @@ if [ "$branchName" == "2.0" ];then
   verType=stable
   tagVal=ver-${version}
   dockerinput=TDengine-server-${version}-Linux-$cpuType.tar.gz
+  dockerinput_x64=TDengine-server-${version}-Linux-amd64.tar.gz
   dockerim=tdengine/tdengine
 elif [ "$branchName" == "master" ];then
   branchName=master
   verType=beta
   tagVal=ver-${version}-beta
   dockerinput=TDengine-server-${version}-${verType}-Linux-$cpuType.tar.gz  
+  dockerinput_x64=TDengine-server-${version}-${verType}-Linux-amd64.tar.gz
   dockerim=tdengine/tdengine-beta
 fi
 
@@ -101,7 +103,7 @@ fi
 
 # docker parameters
 dockerPass="tbase125!"
-dockerinput_x64=TDengine-server-${version}-Linux-amd64.tar.gz
+
 
 ####################### build docker image and push
 if [ "$dockerMode" == "isdocker" ];then
