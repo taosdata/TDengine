b84ee2cc6e800b87fe6a34a7751c3ce3be401746
tomchon <haoran920c@163.com>
2021-08-16 18:11:05 +0800

    modify new_ver_release.sh

--- enterprise/packaging/new_ver_release.sh
+++ enterprise/packaging/new_ver_release.sh
@@ -70,19 +70,18 @@ communityDir=${scriptDir}/../../community
 comunityArchiveDir=/nas/TDengine/v$version/community   # community version’package directory
 
 
-if [ "$branchName" == "master" ];then
-  branchName=master
+if [ "$branchName" == "2.0" ];then
+  branchName=2.0
   verType=stable
   tagVal=ver-${version}
   dockerinput=TDengine-server-${version}-Linux-$cpuType.tar.gz
-  dockerinput_x64=TDengine-server-${version}-Linux-amd64.tar.gz
   dockerim=tdengine/tdengine
-elif [ "$branchName" == "develop" ];then
-  branchName=develop
+elif [ "$branchName" == "master"  ];then
+  branchName=master
   verType=beta
   tagVal=ver-${version}-beta
   dockerinput=TDengine-server-${version}-${verType}-Linux-$cpuType.tar.gz  
-  dockerinput_x64=TDengine-server-${version}-${verType}-Linux-amd64.tar.gz
+  docrsion}-${verType}-Linux-amd64.tar.gz
   dockerim=tdengine/tdengine-beta
 fi
 
