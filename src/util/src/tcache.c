ce6141b4afdda13e981160b86ff4d095cb76c9e4
afwerar <1296468573@qq.com>
2021-08-23 18:17:41 +0800

    [TD-6169]<fix>: windows dll client can not quit.

--- src/util/src/tcache.c
+++ src/util/src/tcache.c
@@ -538,8 +538,8 @@ void taosCacheCleanup(SCacheObj *pCacheObj) {
 
   // wait for the refresh thread quit before destroying the cache object.
   // But in the dll, the child thread will be killed before atexit takes effect.So here we only wait for one second.
-  while(atomic_load_8(&pCacheObj->deleting)) {
-  // for (int i = 0; i < 20&&atomic_load_8(&pCacheObj->deleting) != 0; i++) {
+  // while(atomic_load_8(&pCacheObj->deleting)) {
+  for (int i = 0; i < 60&&atomic_load_8(&pCacheObj->deleting) != 0; i++) {
     taosMsleep(50);
   }
 
