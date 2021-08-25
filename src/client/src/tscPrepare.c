2302bc62ea28b88355ae6eb673eea2d814d082c8
wangmm0220 <wangmm0220@gmail.com>
2021-08-25 17:43:17 +0800

    [TD-6167]<fix> process stmt is null in stmt_close

--- src/client/src/tscPrepare.c
+++ src/client/src/tscPrepare.c
@@ -1781,6 +1781,9 @@ int taos_stmt_set_tbname(TAOS_STMT* stmt, const char* name) {
 
 int taos_stmt_close(TAOS_STMT* stmt) {
   STscStmt* pStmt = (STscStmt*)stmt;
+  if (pStmt == NULL){
+    STMT_RET(TSDB_CODE_SUCCESS);
+  }
   if (!pStmt->isInsert) {
     SNormalStmt* normal = &pStmt->normal;
     if (normal->params != NULL) {
