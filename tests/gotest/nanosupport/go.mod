d6b432b74b85bf1209a6c829d146a71e224714df
wenzhouwww <wenzhouwww@live.cn>
2021-08-25 20:17:34 +0800

    [TD-5661]<test>: add nano support test case for all connector

---
+++ tests/gotest/nanosupport/go.mod
@@ -0,0 +1,5 @@
+module taos
+
+go 1.16
+
+require github.com/taosdata/go-utils v0.0.0-20210811075537-2ce661ccb743
--- tests/pytest/fulltest.sh
+++ tests/pytest/fulltest.sh
@@ -82,8 +82,8 @@ python3 ./test.py -f tag_lite/tinyint.py
 python3 ./test.py -f tag_lite/timestamp.py
 python3 ./test.py -f tag_lite/TestModifyTag.py
 
-#python3 ./test.py -f dbmgmt/database-name-boundary.py
-python3 test.py -f dbmgmt/nanoSecondCheck.py
+#nano support test 
+python3 test.py -f  nanosupport/nanoSecondCheck.py
 
 python3 ./test.py -f import_merge/importBlock1HO.py
 python3 ./test.py -f import_merge/importBlock1HPO.py
