bf81b8b1288262c08df2a954a793ac5a99f751b3
xialei_li <xlli@taosdata.com>
2021-09-13 18:23:23 +0800

    commit index,showdatabase

--- src/connector/node-rest/examples/show-databases.js
+++ src/connector/node-rest/examples/show-databases.js
@@ -1,8 +1,18 @@
 import taos from "../src"
+// import taos from "taos-rest"
 
-let conn = taos.connect();
+let option = {
+	host:'u195',
+	port:'6041',
+	pass:'taosdata',
+	user:'root'
+}
+let conn = taos.connect(option);
+console.log("url showDataBases:" + conn._apiUrl()) ;
 
 (async () => {
-	data = await conn.query("show databases");
+	data = await conn.query("show databases").catch(err => {
+		console.log(err);
+	});
 	console.log(data);
 })()
--- src/connector/node-rest/src/index.js
+++ src/connector/node-rest/src/index.js
@@ -1,31 +1,48 @@
-import fetch from 'node-fetch'
+import fetch  from 'node-fetch'
 
 export class Taos {
-	host = "localhost"
-	port = 6041
-	user = "root"
-	pass = "taosdata"
-	https = false
+  host = "localhost"
+  port = 6041
+  user = "root"
+  pass = "taosdata"
+  https = false
 
-	_apiUrl() {
-		return (this.https ? "https" : "http") + "://" +
-			this.host + ":" + this.port + "/rest/sql";
-	}
-	_token() {
-		return 'Basic ' + Buffer.from(this.user + ':' + this.pass).toString('base64');
-	}
-	async query(sql) {
-		let res = await fetch(this._apiUrl(), {
-			method: 'post',
-			body: sql,
-			headers: { 'Authorization': this._token() }
-		});
-		return res.json();
-	}
+  constructor(options) {
+    console.log("in constructor")
+    this.host = options['host']
+    this.port = options['port']
+    this.user = options['user']
+    this.pass = options['pass']
+  }
+
+  _apiUrl() {
+    return (this.https ? "https" : "http") + "://" +
+      this.host + ":" + this.port + "/rest/sql/";
+  }
+
+  _token() {
+    return 'Basic ' + Buffer.from(this.user + ':' + this.pass).toString('base64');
+  }
+
+  async query(sql) {
+    let res = await fetch(this._apiUrl(), { //'http://u195:6041/rest/sql '
+      method: 'post',
+      body: sql,
+      headers: {'Authorization': this._token()}
+    });
+    return res.json();
+  }
 }
-export const connect = (opts) => {
-	return new Taos(opts)
+
+export const connect = (options) => {
+  console.log("call index.js connect")
+  return new Taos(options)
 }
+// const taos = {
+//   connect
+// }
+// export default taos
+
 export default {
-	connect
+  connect
 }
