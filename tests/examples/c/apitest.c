0aad8c112d77856b315efdfc7263ec1452463793
wpan <wpan@taosdata.com>
2021-09-17 11:32:59 +0800

    merge code from master

--- tests/examples/c/apitest.c
+++ tests/examples/c/apitest.c
@@ -12,7 +12,7 @@
 
 
 static void prepare_data(TAOS* taos) {
-  TAOS_RES* result;
+  TAOS_RES *result;
   result = taos_query(taos, "drop database if exists test;");
   taos_free_result(result);
   usleep(100000);
@@ -45,25 +45,24 @@ static void prepare_data(TAOS* taos) {
   result = taos_query(taos, "create table t9 using meters tags(9);");
   taos_free_result(result);
 
-  result = taos_query(taos,
-                      "insert into t0 values('2020-01-01 00:00:00.000', 0)"
-                      " ('2020-01-01 00:01:00.000', 0)"
-                      " ('2020-01-01 00:02:00.000', 0)"
-                      " t1 values('2020-01-01 00:00:00.000', 0)"
-                      " ('2020-01-01 00:01:00.000', 0)"
-                      " ('2020-01-01 00:02:00.000', 0)"
-                      " ('2020-01-01 00:03:00.000', 0)"
-                      " t2 values('2020-01-01 00:00:00.000', 0)"
-                      " ('2020-01-01 00:01:00.000', 0)"
-                      " ('2020-01-01 00:01:01.000', 0)"
-                      " ('2020-01-01 00:01:02.000', 0)"
-                      " t3 values('2020-01-01 00:01:02.000', 0)"
-                      " t4 values('2020-01-01 00:01:02.000', 0)"
-                      " t5 values('2020-01-01 00:01:02.000', 0)"
-                      " t6 values('2020-01-01 00:01:02.000', 0)"
-                      " t7 values('2020-01-01 00:01:02.000', 0)"
-                      " t8 values('2020-01-01 00:01:02.000', 0)"
-                      " t9 values('2020-01-01 00:01:02.000', 0)");
+  result = taos_query(taos, "insert into t0 values('2020-01-01 00:00:00.000', 0)"
+    " ('2020-01-01 00:01:00.000', 0)"
+    " ('2020-01-01 00:02:00.000', 0)"
+    " t1 values('2020-01-01 00:00:00.000', 0)"
+    " ('2020-01-01 00:01:00.000', 0)"
+    " ('2020-01-01 00:02:00.000', 0)"
+    " ('2020-01-01 00:03:00.000', 0)"
+    " t2 values('2020-01-01 00:00:00.000', 0)"
+    " ('2020-01-01 00:01:00.000', 0)"
+    " ('2020-01-01 00:01:01.000', 0)"
+    " ('2020-01-01 00:01:02.000', 0)"
+    " t3 values('2020-01-01 00:01:02.000', 0)"
+    " t4 values('2020-01-01 00:01:02.000', 0)"
+    " t5 values('2020-01-01 00:01:02.000', 0)"
+    " t6 values('2020-01-01 00:01:02.000', 0)"
+    " t7 values('2020-01-01 00:01:02.000', 0)"
+    " t8 values('2020-01-01 00:01:02.000', 0)"
+    " t9 values('2020-01-01 00:01:02.000', 0)");
   int affected = taos_affected_rows(result);
   if (affected != 18) {
     printf("\033[31m%d rows affected by last insert statement, but it should be 18\033[0m\n", affected);
@@ -82,7 +81,7 @@ static int print_result(TAOS_RES* res, int blockFetch) {
   if (blockFetch) {
     int rows = 0;
     while ((rows = taos_fetch_block(res, &row))) {
-      // for (int i = 0; i < rows; i++) {
+      //for (int i = 0; i < rows; i++) {
       //  char temp[256];
       //  taos_print_row(temp, row + i, fields, num_fields);
       //  puts(temp);
@@ -131,9 +130,9 @@ static void verify_query(TAOS* taos) {
 
   TAOS_RES* res = taos_query(taos, "select * from meters");
   check_row_count(__LINE__, res, 18);
-  printf("result precision is: %d\n", taos_result_precision(res));
+  printf("result precision is: %d\n",  taos_result_precision(res));
   int c = taos_field_count(res);
-  printf("field count is: %d\n", c);
+  printf("field count is: %d\n",  c);
   int* lengths = taos_fetch_lengths(res);
   for (int i = 0; i < c; i++) {
     printf("length of column %d is %d\n", i, lengths[i]);
@@ -154,7 +153,7 @@ static void verify_query(TAOS* taos) {
   taos_free_result(res);
 }
 
-void subscribe_callback(TAOS_SUB* tsub, TAOS_RES* res, void* param, int code) {
+void subscribe_callback(TAOS_SUB* tsub, TAOS_RES *res, void* param, int code) {
   int rows = print_result(res, *(int*)param);
   printf("%d rows consumed in subscribe_callback\n", rows);
 }
@@ -169,7 +168,7 @@ static void verify_subscribe(TAOS* taos) {
   res = taos_consume(tsub);
   check_row_count(__LINE__, res, 0);
 
-  TAOS_RES* result;
+  TAOS_RES *result;
   result = taos_query(taos, "insert into t0 values('2020-01-01 00:02:00.001', 0);");
   taos_free_result(result);
   result = taos_query(taos, "insert into t8 values('2020-01-01 00:01:03.000', 0);");
@@ -263,11 +262,11 @@ void verify_async(TAOS* taos) {
   usleep(1000000);
 }
 
-void stream_callback(void* param, TAOS_RES* res, TAOS_ROW row) {
+void stream_callback(void *param, TAOS_RES *res, TAOS_ROW row) {
   if (res == NULL || row == NULL) {
     return;
   }
-
+  
   int         num_fields = taos_num_fields(res);
   TAOS_FIELD* fields = taos_fetch_fields(res);
 
@@ -279,9 +278,14 @@ void stream_callback(void* param, TAOS_RES* res, TAOS_ROW row) {
 
 void verify_stream(TAOS* taos) {
   prepare_data(taos);
-  TAOS_STREAM* strm =
-      taos_open_stream(taos, "select count(*) from meters interval(1m)", stream_callback, 0, NULL, NULL);
-  printf("waiting for stream data\n");
+  TAOS_STREAM* strm = taos_open_stream(
+    taos,
+    "select count(*) from meters interval(1m)",
+    stream_callback,
+    0,
+    NULL,
+    NULL);
+  printf("waiting for stream data\n"); 
   usleep(100000);
   TAOS_RES* result = taos_query(taos, "insert into t0 values(now, 0)(now+5s,1)(now+10s, 2);");
   taos_free_result(result);
@@ -290,7 +294,7 @@ void verify_stream(TAOS* taos) {
 }
 
 int32_t verify_schema_less(TAOS* taos) {
-  TAOS_RES* result;
+  TAOS_RES *result;
   result = taos_query(taos, "drop database if exists test;");
   taos_free_result(result);
   usleep(100000);
@@ -312,29 +316,30 @@ int32_t verify_schema_less(TAOS* taos) {
       "st,t1=4i64,t2=5f64,t3=\"t4\" c1=3i64,c3=L\"passitagain\",c2=true,c4=5f64 1626006833642000000ns",
       "ste,t2=5f64,t3=L\"ste2\" c3=\"iamszhou\",c4=false 1626056811843316532ns",
       "ste,t2=5f64,t3=L\"ste2\" c3=\"iamszhou\",c4=false,c5=32i8,c6=64i16,c7=32i32,c8=88.88f32 1626056812843316532ns",
-      "st,t1=4i64,t3=\"t4\",t2=5f64,t4=5f64 c1=3i64,c3=L\"passitagin\",c2=true,c4=5f64,c5=5f64,c6=7u64 "
-      "1626006933640000000ns",
-      "stf,t1=4i64,t3=\"t4\",t2=5f64,t4=5f64 c1=3i64,c3=L\"passitagin\",c2=true,c4=5f64,c5=5f64,c6=7u64 "
-      "1626006933640000000ns",
-      "stf,t1=4i64,t3=\"t4\",t2=5f64,t4=5f64 c1=3i64,c3=L\"passitagin_stf\",c2=false,c5=5f64,c6=7u64 "
-      "1626006933641000000ns"};
+      "st,t1=4i64,t3=\"t4\",t2=5f64,t4=5f64 c1=3i64,c3=L\"passitagin\",c2=true,c4=5f64,c5=5f64,c6=7u64 1626006933640000000ns",
+      "stf,t1=4i64,t3=\"t4\",t2=5f64,t4=5f64 c1=3i64,c3=L\"passitagin\",c2=true,c4=5f64,c5=5f64,c6=7u64 1626006933640000000ns",
+      "stf,t1=4i64,t3=\"t4\",t2=5f64,t4=5f64 c1=3i64,c3=L\"passitagin_stf\",c2=false,c5=5f64,c6=7u64 1626006933641000000ns"
+  };
 
-  code = taos_insert_lines(taos, lines, sizeof(lines) / sizeof(char*));
+  code = taos_insert_lines(taos, lines , sizeof(lines)/sizeof(char*));
 
   char* lines2[] = {
       "stg,t1=3i64,t2=4f64,t3=\"t3\" c1=3i64,c3=L\"passit\",c2=false,c4=4f64 1626006833639000000ns",
-      "stg,t1=4i64,t3=\"t4\",t2=5f64,t4=5f64 c1=3i64,c3=L\"passitagin\",c2=true,c4=5f64,c5=5f64 1626006833640000000ns"};
+      "stg,t1=4i64,t3=\"t4\",t2=5f64,t4=5f64 c1=3i64,c3=L\"passitagin\",c2=true,c4=5f64,c5=5f64 1626006833640000000ns"
+  };
   code = taos_insert_lines(taos, &lines2[0], 1);
   code = taos_insert_lines(taos, &lines2[1], 1);
 
   char* lines3[] = {
-      "sth,t1=4i64,t2=5f64,t4=5f64,ID=\"childtable\" c1=3i64,c3=L\"passitagin_stf\",c2=false,c5=5f64,c6=7u64 "
-      "1626006933641ms",
-      "sth,t1=4i64,t2=5f64,t4=5f64 c1=3i64,c3=L\"passitagin_stf\",c2=false,c5=5f64,c6=7u64 1626006933654ms"};
+      "sth,t1=4i64,t2=5f64,t4=5f64,ID=\"childtable\" c1=3i64,c3=L\"passitagin_stf\",c2=false,c5=5f64,c6=7u64 1626006933641ms",
+      "sth,t1=4i64,t2=5f64,t4=5f64 c1=3i64,c3=L\"passitagin_stf\",c2=false,c5=5f64,c6=7u64 1626006933654ms"
+  };
   code = taos_insert_lines(taos, lines3, 2);
 
-  char* lines4[] = {"st123456,t1=3i64,t2=4f64,t3=\"t3\" c1=3i64,c3=L\"passit\",c2=false,c4=4f64 1626006833639000000ns",
-                    "dgtyqodr,t2=5f64,t3=L\"ste\" c1=tRue,c2=4i64,c3=\"iam\" 1626056811823316532ns"};
+  char* lines4[] = {
+      "st123456,t1=3i64,t2=4f64,t3=\"t3\" c1=3i64,c3=L\"passit\",c2=false,c4=4f64 1626006833639000000ns",
+      "dgtyqodr,t2=5f64,t3=L\"ste\" c1=tRue,c2=4i64,c3=\"iam\" 1626056811823316532ns"
+  };
   code = taos_insert_lines(taos, lines4, 2);
 
   char* lines5[] = {
@@ -350,8 +355,11 @@ int32_t verify_schema_less(TAOS* taos) {
   code = taos_insert_lines(taos, &lines5[0], 1);
   code = taos_insert_lines(taos, &lines5[1], 1);
 
-  char* lines6[] = {"st123456,t1=3i64,t2=4f64,t3=\"t3\" c1=3i64,c3=L\"passit\",c2=false,c4=4f64 1626006833639000000ns",
-                    "dgtyqodr,t2=5f64,t3=L\"ste\" c1=tRue,c2=4i64,c3=\"iam\" 1626056811823316532ns"};
+
+  char* lines6[] = {
+      "st123456,t1=3i64,t2=4f64,t3=\"t3\" c1=3i64,c3=L\"passit\",c2=false,c4=4f64 1626006833639000000ns",
+      "dgtyqodr,t2=5f64,t3=L\"ste\" c1=tRue,c2=4i64,c3=\"iam\" 1626056811823316532ns"
+  };
   code = taos_insert_lines(taos, lines6, 2);
   return (code);
 }
@@ -371,9 +379,9 @@ void verify_telnet_insert(TAOS* taos) {
 
   /* metric */
   char* lines0[] = {
-      "stb0_0 1626006833639000000ns 4i8 host=\"host0\",interface=\"eth0\"",
-      "stb0_1 1626006833639000000ns 4i8 host=\"host0\",interface=\"eth0\"",
-      "stb0_2 1626006833639000000ns 4i8 host=\"host0\",interface=\"eth0\"",
+      "stb0_0 1626006833639000000ns 4i8 host=\"host0\" interface=\"eth0\"",
+      "stb0_1 1626006833639000000ns 4i8 host=\"host0\" interface=\"eth0\"",
+      "stb0_2 1626006833639000000ns 4i8 host=\"host0\" interface=\"eth0\"",
   };
   code = taos_insert_telnet_lines(taos, lines0, 3);
   if (code) {
@@ -395,7 +403,7 @@ void verify_telnet_insert(TAOS* taos) {
   }
 
   /* metric value */
-  //tinyin
+  //tinyint
   char* lines2_0[] = {
       "stb2_0 1626006833651ms -127i8 host=\"host0\"",
       "stb2_0 1626006833652ms 127i8 host=\"host0\""
@@ -514,8 +522,8 @@ void verify_telnet_insert(TAOS* taos) {
   /* tags */
   //tag value types
   char* lines3_0[] = {
-      "stb3_0 1626006833610ms 1 t1=127i8,t2=32767i16,t3=2147483647i32,t4=9223372036854775807i64,t5=3.4E38f32,t6=1.7E308f64,t7=true,t8=\"binary_val_1\",t9=L\"标签值1\"",
-      "stb3_0 1626006833610ms 2 t1=-127i8,t2=-32767i16,t3=-2147483647i32,t4=-9223372036854775807i64,t5=-3.4E38f32,t6=-1.7E308f64,t7=false,t8=\"binary_val_2\",t9=L\"标签值2\""
+      "stb3_0 1626006833610ms 1 t1=127i8 t2=32767i16 t3=2147483647i32 t4=9223372036854775807i64 t5=3.4E38f32 t6=1.7E308f64 t7=true t8=\"binary_val_1\" t9=L\"标签值1\"",
+      "stb3_0 1626006833610ms 2 t1=-127i8 t2=-32767i16 t3=-2147483647i32 t4=-9223372036854775807i64 t5=-3.4E38f32 t6=-1.7E308f64 t7=false t8=\"binary_val_2\" t9=L\"标签值2\""
   };
   code = taos_insert_telnet_lines(taos, lines3_0, 2);
   if (code) {
@@ -524,9 +532,9 @@ void verify_telnet_insert(TAOS* taos) {
 
   //tag ID as child table name
   char* lines3_1[] = {
-      "stb3_1 1626006833610ms 1 id=\"child_table1\",host=\"host1\"",
-      "stb3_1 1626006833610ms 2 host=\"host2\",iD=\"child_table2\"",
-      "stb3_1 1626006833610ms 3 ID=\"child_table3\",host=\"host3\""
+      "stb3_1 1626006833610ms 1 id=\"child_table1\" host=\"host1\"",
+      "stb3_1 1626006833610ms 2 host=\"host2\" iD=\"child_table2\"",
+      "stb3_1 1626006833610ms 3 ID=\"child_table3\" host=\"host3\""
   };
   code = taos_insert_telnet_lines(taos, lines3_1, 3);
   if (code) {
@@ -1269,7 +1277,7 @@ void verify_json_insert(TAOS* taos) {
   cJSON_Delete(payload);
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   const char* host = "127.0.0.1";
   const char* user = "root";
   const char* passwd = "taosdata";
@@ -1304,8 +1312,9 @@ int main(int argc, char* argv[]) {
   printf("*********** verify subscribe ************\n");
   verify_subscribe(taos);
 
-  printf("************ verify prepare *************\n");
-  verify_prepare(taos);
-
-  printf("************ verify prepare2 *************\n");
-  v
+  printf("************ verify stream  *************\n");
+  verify_stream(taos);
+  printf("done\n");
+  taos_close(taos);
+  taos_cleanup();
+}
