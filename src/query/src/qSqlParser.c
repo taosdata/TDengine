4adcc7fcafff3ee6f4cd3bf7dd7f8eb6d2eff6a7
wangmm0220 <wangmm0220@gmail.com>
2021-09-23 15:14:06 +0800

    TD-6129<feature> add tag-> select logic

--- src/query/src/qSqlParser.c
+++ src/query/src/qSqlParser.c
@@ -715,7 +715,7 @@ void tSetColumnType(TAOS_FIELD *pField, SStrToken *type) {
   pField->type = i;
   pField->bytes = tDataTypes[i].bytes;
 
-  if (i == TSDB_DATA_TYPE_NCHAR || (i == TSDB_DATA_TYPE_JSON && JSON_TYPE_NCHAR) {
+  if (i == TSDB_DATA_TYPE_NCHAR || (i == TSDB_DATA_TYPE_JSON && JSON_TYPE_NCHAR)) {
     /*
      * for nchar, the TOKENTYPE is the number of character, so the length is the
      * number of bytes in UCS-4 format, which is 4 times larger than the number of characters
@@ -732,7 +732,7 @@ void tSetColumnType(TAOS_FIELD *pField, SStrToken *type) {
       }
       pField->bytes = (int16_t)bytes;
     }
-  } else if (i == TSDB_DATA_TYPE_BINARY || (i == TSDB_DATA_TYPE_JSON && JSON_TYPE_BINARY) {
+  } else if (i == TSDB_DATA_TYPE_BINARY || (i == TSDB_DATA_TYPE_JSON && JSON_TYPE_BINARY)) {
     /* for binary, the TOKENTYPE is the length of binary */
     if (type->type == 0) {
       pField->bytes = 0;
