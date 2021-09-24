3908cbd341e0ccb69c0100538957bc44c45cca76
Hongze Cheng <hzcheng@taosdata.com>
2021-09-24 18:05:56 +0800

    add more module

--- source/libs/planner/src/planner.c
+++ source/libs/planner/src/planner.c
@@ -12,30 +12,3 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
-#ifndef _TD_TSDB_H_
-#define _TD_TSDB_H_
-
-#include "os.h"
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-typedef struct STsdb STsdb;
-typedef struct {
-} STsdbCfg;
-
-int    tsdbInit(int nthreads);
-int    tsdbClear();
-int    tsdbCreateRepo(int id);
-int    tsdbDropRepo(int id);
-STsdb *tsdbOpenRepo(STsdbCfg *pCfg);
-int    tsdbCloseRepo(STsdb *pTsdb);
-int    tsdbForceCloseRepo(STsdb *pTsdb);
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif /*_TD_TSDB_H_*/
