b5e26c6ec181b03370f7b8de93c3e4e33efa670d
Shengliang Guan <slguan@taosdata.com>
2021-10-08 11:12:54 +0800

    minor changes

--- include/client/taos.h
+++ include/client/taos.h
@@ -31,23 +31,21 @@ typedef void   TAOS_SUB;
 typedef void **TAOS_ROW;
 
 // Data type definition
-#ifndef TSDB_DATA_TYPE_NULL
-  #define TSDB_DATA_TYPE_NULL       0     // 1 bytes
-  #define TSDB_DATA_TYPE_BOOL       1     // 1 bytes
-  #define TSDB_DATA_TYPE_TINYINT    2     // 1 byte
-  #define TSDB_DATA_TYPE_SMALLINT   3     // 2 bytes
-  #define TSDB_DATA_TYPE_INT        4     // 4 bytes
-  #define TSDB_DATA_TYPE_BIGINT     5     // 8 bytes
-  #define TSDB_DATA_TYPE_FLOAT      6     // 4 bytes
-  #define TSDB_DATA_TYPE_DOUBLE     7     // 8 bytes
-  #define TSDB_DATA_TYPE_BINARY     8     // string
-  #define TSDB_DATA_TYPE_TIMESTAMP  9     // 8 bytes
-  #define TSDB_DATA_TYPE_NCHAR      10    // unicode string
-  #define TSDB_DATA_TYPE_UTINYINT   11    // 1 byte
-  #define TSDB_DATA_TYPE_USMALLINT  12    // 2 bytes
-  #define TSDB_DATA_TYPE_UINT       13    // 4 bytes
-  #define TSDB_DATA_TYPE_UBIGINT    14    // 8 bytes
-#endif
+#define TSDB_DATA_TYPE_NULL       0     // 1 bytes
+#define TSDB_DATA_TYPE_BOOL       1     // 1 bytes
+#define TSDB_DATA_TYPE_TINYINT    2     // 1 byte
+#define TSDB_DATA_TYPE_SMALLINT   3     // 2 bytes
+#define TSDB_DATA_TYPE_INT        4     // 4 bytes
+#define TSDB_DATA_TYPE_BIGINT     5     // 8 bytes
+#define TSDB_DATA_TYPE_FLOAT      6     // 4 bytes
+#define TSDB_DATA_TYPE_DOUBLE     7     // 8 bytes
+#define TSDB_DATA_TYPE_BINARY     8     // string
+#define TSDB_DATA_TYPE_TIMESTAMP  9     // 8 bytes
+#define TSDB_DATA_TYPE_NCHAR      10    // unicode string
+#define TSDB_DATA_TYPE_UTINYINT   11    // 1 byte
+#define TSDB_DATA_TYPE_USMALLINT  12    // 2 bytes
+#define TSDB_DATA_TYPE_UINT       13    // 4 bytes
+#define TSDB_DATA_TYPE_UBIGINT    14    // 8 bytes
 
 typedef enum {
   TSDB_OPTION_LOCALE,
