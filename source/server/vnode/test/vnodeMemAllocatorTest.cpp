34f7051eec85f5643bf6f15ed74710bcd8f3dfa4
Hongze Cheng <hzcheng@taosdata.com>
2021-10-09 17:13:40 +0800

    add VMATest

---
+++ source/server/vnode/test/vnodeMemAllocatorTest.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+#include <iostream>
+
+#include "vnodeMemAllocator.h"
+
+TEST(VMATest, basic_create_and_destroy_test) {
+    std::cout << "Hello, this is VMA test" << std::endl;
+}
