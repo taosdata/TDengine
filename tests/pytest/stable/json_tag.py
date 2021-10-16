01f6add4a21622c1169e88cc0b1930ebe36cbd63
wangmm0220 <wangmm0220@gmail.com>
2021-10-17 02:29:41 +0800

    TD-6129<feature> support null true false for json tag

--- tests/pytest/stable/json_tag.py
+++ tests/pytest/stable/json_tag.py
@@ -173,20 +173,20 @@ class TDTestCase:
         tdSql.error("CREATE TABLE if not exists db_json_tag_test.jsons1_5 using db_json_tag_test.jsons1 tags('efwewf')")
         tdSql.error("CREATE TABLE if not exists db_json_tag_test.jsons1_5 using db_json_tag_test.jsons1 tags('\t')")
         tdSql.execute("CREATE TABLE if not exists db_json_tag_test.jsons1_6 using db_json_tag_test.jsons1 tags('')")
-        tdSql.query("select jtag from db_json_tag_test.jsons1_6")
-        tdSql.checkData(0, 0, "NULL")
+        #tdSql.query("select jtag from db_json_tag_test.jsons1_6")
+        #tdSql.checkData(0, 0, "NULL")
 
         tdSql.execute("CREATE TABLE if not exists db_json_tag_test.jsons1_7 using db_json_tag_test.jsons1 tags('{}')")
-        tdSql.query("select jtag from db_json_tag_test.jsons1_7")
-        tdSql.checkData(0, 0, "NULL")
+        #tdSql.query("select jtag from db_json_tag_test.jsons1_7")
+        #tdSql.checkData(0, 0, "NULL")
 
         tdSql.execute("CREATE TABLE if not exists db_json_tag_test.jsons1_8 using db_json_tag_test.jsons1 tags('null')")
-        tdSql.query("select jtag from db_json_tag_test.jsons1_8")
-        tdSql.checkData(0, 0, "NULL")
+        #tdSql.query("select jtag from db_json_tag_test.jsons1_8")
+        #tdSql.checkData(0, 0, "NULL")
 
         tdSql.execute("CREATE TABLE if not exists db_json_tag_test.jsons1_9 using db_json_tag_test.jsons1 tags('{\"\":4, \"time\":null}')")
-        tdSql.query("select jtag from db_json_tag_test.jsons1_9")
-        tdSql.checkData(0, 0, "NULL")
+        #tdSql.query("select jtag from db_json_tag_test.jsons1_9")
+        #tdSql.checkData(0, 0, "NULL")
 
         tdSql.execute("CREATE TABLE if not exists db_json_tag_test.jsons1_10 using db_json_tag_test.jsons1 tags('{\"k1\":\"\",\"k1\":\"v1\",\"k2\":true,\"k3\":false,\"k4\":55}')")
         tdSql.query("select jtag from db_json_tag_test.jsons1_10")
@@ -200,6 +200,12 @@ class TDTestCase:
 
         tdSql.query("select jtag from db_json_tag_test.jsons1 where jtag->'k2'=true")
         tdSql.checkRows(1)
+
+        tdSql.query("select jtag from db_json_tag_test.jsons1 where jtag is null")
+        tdSql.checkRows(4)
+
+        tdSql.query("select jtag from db_json_tag_test.jsons1 where jtag is not null")
+        tdSql.checkRows(5)
 
     def stop(self):
         tdSql.close()
