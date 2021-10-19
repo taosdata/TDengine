2aac41d92b0185f76b25174edd1de006a5a7c9e5
zhaoyanggh <yzhao@taosdata.com>
2021-10-19 16:43:21 +0800

    save random data init mem usage

--- src/kit/taosdemo/taosdemo.c
+++ src/kit/taosdemo/taosdemo.c
@@ -385,7 +385,7 @@ typedef struct SDbs_S {
     uint32_t    threadCount;
     uint32_t    threadCountForCreateTbl;
     uint32_t    dbCount;
-
+    int64_t     maxInsertRows;
     // statistics
     uint64_t    totalInsertRows;
     uint64_t    totalAffectedRows;
@@ -593,12 +593,12 @@ static int regexMatch(const char *s, const char *reg, int cflags);
 
 /* ************ Global variables ************  */
 
-int32_t  g_randint[MAX_PREPARED_RAND];
-uint32_t  g_randuint[MAX_PREPARED_RAND];
-int64_t  g_randbigint[MAX_PREPARED_RAND];
-uint64_t  g_randubigint[MAX_PREPARED_RAND];
-float    g_randfloat[MAX_PREPARED_RAND];
-double   g_randdouble[MAX_PREPARED_RAND];
+int32_t*  g_randint;
+uint32_t*  g_randuint;
+int64_t*  g_randbigint;
+uint64_t*  g_randubigint;
+float*    g_randfloat;
+double*   g_randdouble;
 
 char    *g_randbool_buff = NULL;
 char    *g_randint_buff = NULL;
@@ -2234,157 +2234,157 @@ static void selectAndGetResult(
 static char *rand_bool_str() {
     static int cursor;
     cursor++;
-    if (cursor > (MAX_PREPARED_RAND - 1)) cursor = 0;
-    return g_randbool_buff + ((cursor % MAX_PREPARED_RAND) * BOOL_BUFF_LEN);
+    if (cursor > (g_Dbs.maxInsertRows - 1)) cursor = 0;
+    return g_randbool_buff + ((cursor % g_Dbs.maxInsertRows) * BOOL_BUFF_LEN);
 }
 
 static int32_t rand_bool() {
     static int cursor;
     cursor++;
-    if (cursor > (MAX_PREPARED_RAND - 1)) cursor = 0;
-    return g_randint[cursor % MAX_PREPARED_RAND] % 2;
+    if (cursor > (g_Dbs.maxInsertRows - 1)) cursor = 0;
+    return g_randint[cursor % g_Dbs.maxInsertRows] % 2;
 }
 
 static char *rand_tinyint_str()
 {
     static int cursor;
     cursor++;
-    if (cursor > (MAX_PREPARED_RAND - 1)) cursor = 0;
+    if (cursor > (g_Dbs.maxInsertRows - 1)) cursor = 0;
     return g_randtinyint_buff +
-        ((cursor % MAX_PREPARED_RAND) * TINYINT_BUFF_LEN);
+        ((cursor % g_Dbs.maxInsertRows) * TINYINT_BUFF_LEN);
 }
 
 static int32_t rand_tinyint()
 {
     static int cursor;
     cursor++;
-    if (cursor > (MAX_PREPARED_RAND - 1)) cursor = 0;
-    return g_randint[cursor % MAX_PREPARED_RAND] % 128;
+    if (cursor > (g_Dbs.maxInsertRows - 1)) cursor = 0;
+    return g_randint[cursor % g_Dbs.maxInsertRows] % 128;
 }
 
 static char *rand_utinyint_str()
 {
     static int cursor;
     cursor++;
-    if (cursor > (MAX_PREPARED_RAND - 1)) cursor = 0;
+    if (cursor > (g_Dbs.maxInsertRows - 1)) cursor = 0;
     return g_randutinyint_buff +
-        ((cursor % MAX_PREPARED_RAND) * TINYINT_BUFF_LEN);
+        ((cursor % g_Dbs.maxInsertRows) * TINYINT_BUFF_LEN);
 }
 
 static int32_t rand_utinyint()
 {
     static int cursor;
     cursor++;
-    if (cursor > (MAX_PREPARED_RAND - 1)) cursor = 0;
-    return g_randuint[cursor % MAX_PREPARED_RAND] % 255;
+    if (cursor > (g_Dbs.maxInsertRows - 1)) cursor = 0;
+    return g_randuint[cursor % g_Dbs.maxInsertRows] % 255;
 }
 
 static char *rand_smallint_str()
 {
     static int cursor;
     cursor++;
-    if (cursor > (MAX_PREPARED_RAND - 1)) cursor = 0;
+    if (cursor > (g_Dbs.maxInsertRows - 1)) cursor = 0;
     return g_randsmallint_buff +
-        ((cursor % MAX_PREPARED_RAND) * SMALLINT_BUFF_LEN);
+        ((cursor % g_Dbs.maxInsertRows) * SMALLINT_BUFF_LEN);
 }
 
 static int32_t rand_smallint()
 {
     static int cursor;
     cursor++;
-    if (cursor > (MAX_PREPARED_RAND - 1)) cursor = 0;
-    return g_randint[cursor % MAX_PREPARED_RAND] % 32768;
+    if (cursor > (g_Dbs.maxInsertRows - 1)) cursor = 0;
+    return g_randint[cursor % g_Dbs.maxInsertRows] % 32768;
 }
 
 static char *rand_usmallint_str()
 {
     static int cursor;
     cursor++;
-    if (cursor > (MAX_PREPARED_RAND - 1)) cursor = 0;
+    if (cursor > (g_Dbs.maxInsertRows - 1)) cursor = 0;
     return g_randusmallint_buff +
-        ((cursor % MAX_PREPARED_RAND) * SMALLINT_BUFF_LEN);
+        ((cursor % g_Dbs.maxInsertRows) * SMALLINT_BUFF_LEN);
 }
 
 static int32_t rand_usmallint()
 {
     static int cursor;
     cursor++;
-    if (cursor > (MAX_PREPARED_RAND - 1)) cursor = 0;
-    return g_randuint[cursor % MAX_PREPARED_RAND] % 65535;
+    if (cursor > (g_Dbs.maxInsertRows - 1)) cursor = 0;
+    return g_randuint[cursor % g_Dbs.maxInsertRows] % 65535;
 }
 
 static char *rand_int_str()
 {
     static int cursor;
     cursor++;
-    if (cursor > (MAX_PREPARED_RAND - 1)) cursor = 0;
-    return g_randint_buff + ((cursor % MAX_PREPARED_RAND) * INT_BUFF_LEN);
+    if (cursor > (g_Dbs.maxInsertRows - 1)) cursor = 0;
+    return g_randint_buff + ((cursor % g_Dbs.maxInsertRows) * INT_BUFF_LEN);
 }
 
 static int32_t rand_int()
 {
     static int cursor;
     cursor++;
-    if (cursor > (MAX_PREPARED_RAND - 1)) cursor = 0;
-    return g_randint[cursor % MAX_PREPARED_RAND];
+    if (cursor > (g_Dbs.maxInsertRows - 1)) cursor = 0;
+    return g_randint[cursor % g_Dbs.maxInsertRows];
 }
 
 static char *rand_uint_str()
 {
     static int cursor;
     cursor++;
-    if (cursor > (MAX_PREPARED_RAND - 1)) cursor = 0;
-    return g_randuint_buff + ((cursor % MAX_PREPARED_RAND) * INT_BUFF_LEN);
+    if (cursor > (g_Dbs.maxInsertRows - 1)) cursor = 0;
+    return g_randuint_buff + ((cursor % g_Dbs.maxInsertRows) * INT_BUFF_LEN);
 }
 
 static int32_t rand_uint()
 {
     static int cursor;
     cursor++;
-    if (cursor > (MAX_PREPARED_RAND - 1)) cursor = 0;
-    return g_randuint[cursor % MAX_PREPARED_RAND];
+    if (cursor > (g_Dbs.maxInsertRows - 1)) cursor = 0;
+    return g_randuint[cursor % g_Dbs.maxInsertRows];
 }
 
 static char *rand_bigint_str()
 {
     static int cursor;
     cursor++;
-    if (cursor > (MAX_PREPARED_RAND - 1)) cursor = 0;
+    if (cursor > (g_Dbs.maxInsertRows - 1)) cursor = 0;
     return g_randbigint_buff +
-        ((cursor % MAX_PREPARED_RAND) * BIGINT_BUFF_LEN);
+        ((cursor % g_Dbs.maxInsertRows) * BIGINT_BUFF_LEN);
 }
 
 static int64_t rand_bigint()
 {
     static int cursor;
     cursor++;
-    if (cursor > (MAX_PREPARED_RAND - 1)) cursor = 0;
-    return g_randbigint[cursor % MAX_PREPARED_RAND];
+    if (cursor > (g_Dbs.maxInsertRows - 1)) cursor = 0;
+    return g_randbigint[cursor % g_Dbs.maxInsertRows];
 }
 
 static char *rand_ubigint_str()
 {
     static int cursor;
     cursor++;
-    if (cursor > (MAX_PREPARED_RAND - 1)) cursor = 0;
+    if (cursor > (g_Dbs.maxInsertRows - 1)) cursor = 0;
     return g_randubigint_buff +
-        ((cursor % MAX_PREPARED_RAND) * BIGINT_BUFF_LEN);
+        ((cursor % g_Dbs.maxInsertRows) * BIGINT_BUFF_LEN);
 }
 
 static int64_t rand_ubigint()
 {
     static int cursor;
     cursor++;
-    if (cursor > (MAX_PREPARED_RAND - 1)) cursor = 0;
-    return g_randubigint[cursor % MAX_PREPARED_RAND];
+    if (cursor > (g_Dbs.maxInsertRows - 1)) cursor = 0;
+    return g_randubigint[cursor % g_Dbs.maxInsertRows];
 }
 
 static char *rand_float_str()
 {
     static int cursor;
     cursor++;
-    if (cursor > (MAX_PREPARED_RAND - 1)) cursor = 0;
-    return g_randfloat_buff + ((cursor % MAX_PREPARED_RAND) * FLOAT_BUFF_LEN);
+    if (cursor > (g_Dbs.maxInsertRows - 1)) cursor = 0;
+    return g_randfloat_buff + ((cursor % g_Dbs.maxInsertRows) * FLOAT_BUFF_LEN);
 }
 
 
@@ -2392,58 +2392,58 @@ static float rand_float()
 {
     static int cursor;
     cursor++;
-    if (cursor > (MAX_PREPARED_RAND - 1)) cursor = 0;
-    return g_randfloat[cursor % MAX_PREPARED_RAND];
+    if (cursor > (g_Dbs.maxInsertRows - 1)) cursor = 0;
+    return g_randfloat[cursor % g_Dbs.maxInsertRows];
 }
 
 static char *demo_current_float_str()
 {
     static int cursor;
     cursor++;
-    if (cursor > (MAX_PREPARED_RAND - 1)) cursor = 0;
+    if (cursor > (g_Dbs.maxInsertRows - 1)) cursor = 0;
     return g_rand_current_buff +
-        ((cursor % MAX_PREPARED_RAND) * FLOAT_BUFF_LEN);
+        ((cursor % g_Dbs.maxInsertRows) * FLOAT_BUFF_LEN);
 }
 
 static float UNUSED_FUNC demo_current_float()
 {
     static int cursor;
     cursor++;
-    if (cursor > (MAX_PREPARED_RAND - 1)) cursor = 0;
-    return (float)(9.8 + 0.04 * (g_randint[cursor % MAX_PREPARED_RAND] % 10)
-            + g_randfloat[cursor % MAX_PREPARED_RAND]/1000000000);
+    if (cursor > (g_Dbs.maxInsertRows - 1)) cursor = 0;
+    return (float)(9.8 + 0.04 * (g_randint[cursor % g_Dbs.maxInsertRows] % 10)
+            + g_randfloat[cursor % g_Dbs.maxInsertRows]/1000000000);
 }
 
 static char *demo_voltage_int_str()
 {
     static int cursor;
     cursor++;
-    if (cursor > (MAX_PREPARED_RAND - 1)) cursor = 0;
+    if (cursor > (g_Dbs.maxInsertRows - 1)) cursor = 0;
     return g_rand_voltage_buff +
-        ((cursor % MAX_PREPARED_RAND) * INT_BUFF_LEN);
+        ((cursor % g_Dbs.maxInsertRows) * INT_BUFF_LEN);
 }
 
 static int32_t UNUSED_FUNC demo_voltage_int()
 {
     static int cursor;
     cursor++;
-    if (cursor > (MAX_PREPARED_RAND - 1)) cursor = 0;
-    return 215 + g_randint[cursor % MAX_PREPARED_RAND] % 10;
+    if (cursor > (g_Dbs.maxInsertRows - 1)) cursor = 0;
+    return 215 + g_randint[cursor % g_Dbs.maxInsertRows] % 10;
 }
 
 static char *demo_phase_float_str() {
     static int cursor;
     cursor++;
-    if (cursor > (MAX_PREPARED_RAND - 1)) cursor = 0;
-    return g_rand_phase_buff + ((cursor % MAX_PREPARED_RAND) * FLOAT_BUFF_LEN);
+    if (cursor > (g_Dbs.maxInsertRows - 1)) cursor = 0;
+    return g_rand_phase_buff + ((cursor % g_Dbs.maxInsertRows) * FLOAT_BUFF_LEN);
 }
 
 static float UNUSED_FUNC demo_phase_float() {
     static int cursor;
     cursor++;
-    if (cursor > (MAX_PREPARED_RAND - 1)) cursor = 0;
-    return (float)((115 + g_randint[cursor % MAX_PREPARED_RAND] % 10
-                + g_randfloat[cursor % MAX_PREPARED_RAND]/1000000000)/360);
+    if (cursor > (g_Dbs.maxInsertRows - 1)) cursor = 0;
+    return (float)((115 + g_randint[cursor % g_Dbs.maxInsertRows] % 10
+                + g_randfloat[cursor % g_Dbs.maxInsertRows]/1000000000)/360);
 }
 
 #if 0
@@ -2482,7 +2482,7 @@ static char *rand_double_str()
 {
     static int cursor;
     cursor++;
-    if (cursor > (MAX_PREPARED_RAND - 1)) cursor = 0;
+    if (cursor > (g_Dbs.maxInsertRows - 1)) cursor = 0;
     return g_randdouble_buff + (cursor * DOUBLE_BUFF_LEN);
 }
 
@@ -2490,42 +2490,57 @@ static double rand_double()
 {
     static int cursor;
     cursor++;
-    cursor = cursor % MAX_PREPARED_RAND;
+    cursor = cursor % g_Dbs.maxInsertRows;
     return g_randdouble[cursor];
 }
 
 static void init_rand_data() {
-
-    g_randint_buff = calloc(1, INT_BUFF_LEN * MAX_PREPARED_RAND);
+    if (g_Dbs.maxInsertRows > MAX_PREPARED_RAND) {
+        g_Dbs.maxInsertRows = MAX_PREPARED_RAND;
+    }
+    
+    g_randint_buff = calloc(1, INT_BUFF_LEN * g_Dbs.maxInsertRows);
     assert(g_randint_buff);
-    g_rand_voltage_buff = calloc(1, INT_BUFF_LEN * MAX_PREPARED_RAND);
+    g_rand_voltage_buff = calloc(1, INT_BUFF_LEN * g_Dbs.maxInsertRows);
     assert(g_rand_voltage_buff);
-    g_randbigint_buff = calloc(1, BIGINT_BUFF_LEN * MAX_PREPARED_RAND);
+    g_randbigint_buff = calloc(1, BIGINT_BUFF_LEN * g_Dbs.maxInsertRows);
     assert(g_randbigint_buff);
-    g_randsmallint_buff = calloc(1, SMALLINT_BUFF_LEN * MAX_PREPARED_RAND);
+    g_randsmallint_buff = calloc(1, SMALLINT_BUFF_LEN * g_Dbs.maxInsertRows);
     assert(g_randsmallint_buff);
-    g_randtinyint_buff = calloc(1, TINYINT_BUFF_LEN * MAX_PREPARED_RAND);
+    g_randtinyint_buff = calloc(1, TINYINT_BUFF_LEN * g_Dbs.maxInsertRows);
     assert(g_randtinyint_buff);
-    g_randbool_buff = calloc(1, BOOL_BUFF_LEN * MAX_PREPARED_RAND);
+    g_randbool_buff = calloc(1, BOOL_BUFF_LEN * g_Dbs.maxInsertRows);
     assert(g_randbool_buff);
-    g_randfloat_buff = calloc(1, FLOAT_BUFF_LEN * MAX_PREPARED_RAND);
+    g_randfloat_buff = calloc(1, FLOAT_BUFF_LEN * g_Dbs.maxInsertRows);
     assert(g_randfloat_buff);
-    g_rand_current_buff = calloc(1, FLOAT_BUFF_LEN * MAX_PREPARED_RAND);
+    g_rand_current_buff = calloc(1, FLOAT_BUFF_LEN * g_Dbs.maxInsertRows);
     assert(g_rand_current_buff);
-    g_rand_phase_buff = calloc(1, FLOAT_BUFF_LEN * MAX_PREPARED_RAND);
+    g_rand_phase_buff = calloc(1, FLOAT_BUFF_LEN * g_Dbs.maxInsertRows);
     assert(g_rand_phase_buff);
-    g_randdouble_buff = calloc(1, DOUBLE_BUFF_LEN * MAX_PREPARED_RAND);
+    g_randdouble_buff = calloc(1, DOUBLE_BUFF_LEN * g_Dbs.maxInsertRows);
     assert(g_randdouble_buff);
-    g_randuint_buff = calloc(1, INT_BUFF_LEN * MAX_PREPARED_RAND);
+    g_randuint_buff = calloc(1, INT_BUFF_LEN * g_Dbs.maxInsertRows);
     assert(g_randuint_buff);
-    g_randutinyint_buff = calloc(1, TINYINT_BUFF_LEN * MAX_PREPARED_RAND);
+    g_randutinyint_buff = calloc(1, TINYINT_BUFF_LEN * g_Dbs.maxInsertRows);
     assert(g_randutinyint_buff);
-    g_randusmallint_buff = calloc(1, SMALLINT_BUFF_LEN * MAX_PREPARED_RAND);
+    g_randusmallint_buff = calloc(1, SMALLINT_BUFF_LEN * g_Dbs.maxInsertRows);
     assert(g_randusmallint_buff);
-    g_randubigint_buff = calloc(1, BIGINT_BUFF_LEN * MAX_PREPARED_RAND);
+    g_randubigint_buff = calloc(1, BIGINT_BUFF_LEN * g_Dbs.maxInsertRows);
     assert(g_randubigint_buff);
+    g_randint = calloc(1, sizeof(int32_t) * g_Dbs.maxInsertRows);
+    assert(g_randint);
+    g_randuint = calloc(1, sizeof(uint32_t) * g_Dbs.maxInsertRows);
+    assert(g_randuint);
+    g_randbigint = calloc(1, sizeof(int64_t) * g_Dbs.maxInsertRows);
+    assert(g_randbigint);
+    g_randubigint = calloc(1, sizeof(uint64_t) * g_Dbs.maxInsertRows);
+    assert(g_randubigint);
+    g_randfloat = calloc(1, sizeof(float) * g_Dbs.maxInsertRows);
+    assert(g_randfloat);
+    g_randdouble = calloc(1, sizeof(double) * g_Dbs.maxInsertRows);
+    assert(g_randdouble);
 
-    for (int i = 0; i < MAX_PREPARED_RAND; i++) {
+    for (int i = 0; i < g_Dbs.maxInsertRows; i++) {
         g_randint[i] = (int)(taosRandom() % RAND_MAX - (RAND_MAX >> 1));
         g_randuint[i] = (int)(taosRandom());
         sprintf(g_randint_buff + i * INT_BUFF_LEN, "%d",
@@ -5803,8 +5818,12 @@ static bool getMetaFromInsertJsonFile(cJSON* root) {
                     goto PARSE_OVER;
                 }
                 g_Dbs.db[i].superTbls[j].insertRows = insertRows->valueint;
+                if (g_Dbs.db[i].superTbls[j].insertRows > g_Dbs.maxInsertRows) {
+                    g_Dbs.maxInsertRows = g_Dbs.db[i].superTbls[j].insertRows;
+                }
             } else if (!insertRows) {
                 g_Dbs.db[i].superTbls[j].insertRows = 0x7FFFFFFFFFFFFFFF;
+                g_Dbs.maxInsertRows = g_Dbs.db[i].superTbls[j].insertRows;
             } else {
                 errorPrint("%s", "failed to read json, insert_rows input mistake\n");
                 goto PARSE_OVER;
@@ -11957,7 +11976,7 @@ static void setParaFromArg() {
     tstrncpy(g_Dbs.resultFile, g_args.output_file, MAX_FILE_NAME_LEN);
 
     g_Dbs.use_metric = g_args.use_metric;
-
+    g_Dbs.maxInsertRows = g_args.insertRows;
     g_Dbs.aggr_func = g_args.aggr_func;
 
     char dataString[TSDB_MAX_BYTES_PER_ROW];
