ec9fcda9629ce06b79ae709b5e58f46a26c0a2d0
shenglian zhou <shenglian_zhou@163.com>
2021-10-23 08:33:07 +0800

    make scalar functions projection query and test failed

--- src/client/src/tscUtil.c
+++ src/client/src/tscUtil.c
@@ -315,7 +315,7 @@ bool tscIsProjectionQuery(SQueryInfo* pQueryInfo) {
 
     if (f != TSDB_FUNC_PRJ && f != TSDB_FUNC_TAGPRJ && f != TSDB_FUNC_TAG &&
         f != TSDB_FUNC_TS && f != TSDB_FUNC_ARITHM && f != TSDB_FUNC_DIFF &&
-        f != TSDB_FUNC_DERIVATIVE) {
+        f != TSDB_FUNC_DERIVATIVE && !TSDB_FUNC_IS_SCALAR(f)) {
       return false;
     }
   }
