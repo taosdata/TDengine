904c19c61bfe8e0ba416b8a9898555ffd2f147cb
xywang <xywang@taosdata.com>
2021-10-26 10:45:14 +0800

    [TS-361]<fix>: fixed count of query was incorrect when 'like' was used

--- src/util/src/tcompare.c
+++ src/util/src/tcompare.c
@@ -417,14 +417,20 @@ int32_t compareFindItemInSet(const void *pLeft, const void* pRight)  {
 
 int32_t compareWStrPatternComp(const void* pLeft, const void* pRight) {
   SPatternCompareInfo pInfo = {'%', '_'};
+  size_t size = varDataLen(pLeft)/TSDB_NCHAR_SIZE;
 
   assert(varDataLen(pRight) <= TSDB_MAX_FIELD_LEN * TSDB_NCHAR_SIZE);
 
   wchar_t *pattern = calloc(varDataLen(pRight) + 1, sizeof(wchar_t));
-  memcpy(pattern, varDataVal(pRight), varDataLen(pRight));
+  wchar_t *str = calloc(size + 1, sizeof(wchar_t));
 
-  int32_t ret = WCSPatternMatch(pattern, varDataVal(pLeft), varDataLen(pLeft)/TSDB_NCHAR_SIZE, &pInfo);
+  memcpy(pattern, varDataVal(pRight), varDataLen(pRight));
+  memcpy(str, varDataVal(pLeft), size * sizeof(wchar_t));
+
+  int32_t ret = WCSPatternMatch(pattern, str, size, &pInfo);
+
   free(pattern);
+  free(str);
 
   return (ret == TSDB_PATTERN_MATCH) ? 0 : 1;
 }
