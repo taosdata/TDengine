eed0e8f98242ddda441e64165755e37dcfdd9dc9
tomchon <haoran920c@163.com>
2021-10-28 19:30:05 +0800

    [TD-10551]<test>: add  taosdemo testcase  that it provides define timestamp column in  sample.csv

--- tests/pytest/tools/taosdemoAllTest/taosdemoTestInsertWithJson.py
+++ tests/pytest/tools/taosdemoAllTest/taosdemoTestInsertWithJson.py
@@ -263,6 +263,25 @@ class TDTestCase:
         tdSql.checkData(0, 0, 10)
 
         # insert:  sample json
+        os.system("%staosdemo -f tools/taosdemoAllTest/insert-sample-ts.json -y " % binPath)
+        tdSql.execute("use dbtest123")
+        tdSql.query("select c2 from stb0")
+        tdSql.checkData(0, 0, 2147483647)
+        tdSql.query("select c0 from stb0_0 order by ts")
+        tdSql.checkData(3, 0, 4)
+        tdSql.query("select count(*) from stb0 order by ts")
+        tdSql.checkData(0, 0, 40)
+        tdSql.query("select * from stb0_1 order by ts")
+        tdSql.checkData(0, 0, '2021-10-28 15:34:44.735')
+        tdSql.checkData(3, 0, '2021-10-31 15:34:44.735')
+        tdSql.query("select * from stb1 where t1=-127")
+        tdSql.checkRows(20)
+        tdSql.query("select * from stb1 where t2=127")
+        tdSql.checkRows(10)
+        tdSql.query("select * from stb1 where t2=126")
+        tdSql.checkRows(10)
+
+        # insert:  sample json
         os.system("%staosdemo -f tools/taosdemoAllTest/insert-sample.json -y " % binPath)
         tdSql.execute("use dbtest123")
         tdSql.query("select c2 from stb0")
@@ -273,6 +292,7 @@ class TDTestCase:
         tdSql.checkRows(10)
         tdSql.query("select * from stb1 where t2=126")
         tdSql.checkRows(10)
+
 
         # insert: test interlace parament
         os.system("%staosdemo -f tools/taosdemoAllTest/insert-interlace-row.json -y " % binPath)
