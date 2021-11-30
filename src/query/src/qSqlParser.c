873ab6d97fc5228eed415b116040e81d9d4d251e
dapan1121 <wpan@taosdata.com>
2021-11-30 15:12:49 +0800

    [TD-11584]invalid sql can be executed successfully

--- src/query/src/qSqlParser.c
+++ src/query/src/qSqlParser.c
@@ -744,6 +744,10 @@ void tSetColumnType(TAOS_FIELD *pField, SStrToken *type) {
 
       pField->bytes = (int16_t)bytes;
     }
+  } else {
+    if (type->type > 0) {
+      pField->type = -1;
+    }
   }
 }
 
