a44b2423f2af0450f804cecf15689e740a7d9a50
Hongze Cheng <hzcheng@taosdata.com>
2021-12-01 17:44:28 +0800

    more

--- deps/test/sqlite/sqliteTest.c
+++ deps/test/sqlite/sqliteTest.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 #include "sqlite3.h"
 
 int main(int argc, char const *argv[]) {
@@ -29,31 +31,33 @@ int main(int argc, char const *argv[]) {
     return 1;
   }
 
-  // Write a lot of data
-  int  nrows = 100000;
-  int  batch = 1000;
-  char tsql[1024];
+  {
+    // Write a lot of data
+    int  nrows = 100000;
+    int  batch = 1000;
+    char tsql[1024];
 
-  int v = 0;
-  for (int k = 0; k < nrows / batch; k++) {
-    sqlite3_exec(db, "begin;", 0, 0, &err_msg);
+    int v = 0;
+    for (int k = 0; k < nrows / batch; k++) {
+      sqlite3_exec(db, "begin;", 0, 0, &err_msg);
 
-    for (int i = 0; i < batch; i++) {
-      v++;
-      sprintf(tsql, "insert into t values (%d)", v);
-      rc = sqlite3_exec(db, tsql, 0, 0, &err_msg);
+      for (int i = 0; i < batch; i++) {
+        v++;
+        sprintf(tsql, "insert into t values (%d)", v);
+        rc = sqlite3_exec(db, tsql, 0, 0, &err_msg);
 
-      if (rc != SQLITE_OK) {
-        fprintf(stderr, "SQL error: %s\n", err_msg);
+        if (rc != SQLITE_OK) {
+          fprintf(stderr, "SQL error: %s\n", err_msg);
 
-        sqlite3_free(err_msg);
-        sqlite3_close(db);
+          sqlite3_free(err_msg);
+          sqlite3_close(db);
 
-        return 1;
+          return 1;
+        }
       }
-    }
 
-    sqlite3_exec(db, "commit;", 0, 0, &err_msg);
+      sqlite3_exec(db, "commit;", 0, 0, &err_msg);
+    }
   }
 
   sqlite3_close(db);
