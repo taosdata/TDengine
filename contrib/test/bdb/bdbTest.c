4bcc7778db9947f39d2ddfac715deb74e0d02475
Hongze Cheng <hzcheng@taosdata.com>
2021-12-05 16:24:30 +0800

    more

--- contrib/test/bdb/bdbTest.c
+++ contrib/test/bdb/bdbTest.c
@@ -6,23 +6,24 @@
 // refer: https://docs.oracle.com/cd/E17076_05/html/gsg/C/BerkeleyDB-Core-C-GSG.pdf
 
 int main(int argc, char const *argv[]) {
-  DB *     db;
-  int      ret;
-  uint32_t flags;
+  DB *      dbp;
+  int       ret;
+  u_int32_t flags;
+  DBT       key, value;
 
-  ret = db_create(&db, NULL, 0);
+  ret = db_create(&dbp, NULL, 0);
   if (ret != 0) {
     exit(1);
   }
 
   flags = DB_CREATE;
 
-  ret = db->open(db, NULL, "test.db", NULL, DB_BTREE, flags, 0);
+  ret = dbp->open(dbp, NULL, "meta.db", NULL, DB_BTREE, flags, 0);
   if (ret != 0) {
     exit(1);
   }
 
-  db->close(db, 0);
+  dbp->close(dbp, 0);
 
   return 0;
 }
