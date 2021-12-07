84fe2c895baa3a0f5d3b906f5af41942d767a9e6
zhaoyanggh <yzhao@taosdata.com>
2021-12-07 00:34:03 -0800

    revert

--- packaging/tools/makeclient_pro.sh
+++ packaging/tools/makeclient_pro.sh
@@ -73,7 +73,7 @@ if [ "$osType" != "Darwin" ]; then
   else 
     cp ${build_dir}/bin/taos          ${install_dir}/bin/prodbc
     cp ${script_dir}/remove_pro.sh  ${install_dir}/bin
-    cp ${build_dir}/bin/taosBenchmark      ${install_dir}/bin/prodemo
+    cp ${build_dir}/bin/taosdemo      ${install_dir}/bin/prodemo
     cp ${build_dir}/bin/taosdump      ${install_dir}/bin/prodump
     cp ${script_dir}/set_core.sh      ${install_dir}/bin
     cp ${script_dir}/get_client.sh    ${install_dir}/bin
