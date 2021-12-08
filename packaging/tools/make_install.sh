b39ab90fcb5cc8b91451d8f261de0b29333e88ec
Shuduo Sang <sangshuduo@gmail.com>
2021-12-08 11:52:53 +0800

    modified more scripts.

--- packaging/tools/make_install.sh
+++ packaging/tools/make_install.sh
@@ -173,7 +173,7 @@ function install_bin() {
 
         ${csudo} cp -r ${script_dir}/remove.sh     ${install_main_dir}/bin
         ${csudo} cp -r ${script_dir}/set_core.sh   ${install_main_dir}/bin
-        ${csudo} cp -r ${script_dir}/run_taosd.sh  ${install_main_dir}/bin
+        ${csudo} cp -r ${script_dir}/run_taosd.sh   ${install_main_dir}/bin
         ${csudo} cp -r ${script_dir}/startPre.sh   ${install_main_dir}/bin
 
         ${csudo} chmod 0555 ${install_main_dir}/bin/*
@@ -185,7 +185,7 @@ function install_bin() {
         [ -x ${install_main_dir}/bin/taosdemo ]  && ${csudo} ln -s ${install_main_dir}/bin/taosdemo ${bin_link_dir}/taosdemo || :
         [ -x ${install_main_dir}/bin/perfMonitor ]  && ${csudo} ln -s ${install_main_dir}/bin/perfMonitor ${bin_link_dir}/perfMonitor || :
         [ -x ${install_main_dir}/set_core.sh ]  && ${csudo} ln -s ${install_main_dir}/bin/set_core.sh ${bin_link_dir}/set_core || :
-        [ -x ${install_main_dir}/run_taosd.sh ]  && ${csudo} ln -s ${install_main_dir}/bin/run_taosd.sh ${bin_link_dir}/run_taosd || :
+        [ -x ${install_main_dir}/run_taosd.sh ]  && ${csudo} ln -s ${install_main_dir}/bin/run_taosd.sh ${bin_link_dir}/run_taosd.sh || :
         [ -x ${install_main_dir}/bin/remove.sh ] && ${csudo} ln -s ${install_main_dir}/bin/remove.sh ${bin_link_dir}/rmtaos  || :
    else
 
