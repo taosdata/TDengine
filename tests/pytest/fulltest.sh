5ce5f1b78dfb58f013ccd7aafb7ab06a8966c7e8
zhaoyanggh <yzhao@taosdata.com>
2021-12-08 09:18:38 +0800

    fix

--- tests/pytest/fulltest.sh
+++ tests/pytest/fulltest.sh
@@ -165,6 +165,7 @@ python3 ./test.py -f update/merge_commit_data.py
 python3 test.py -f tools/taosdumpTest.py
 python3 test.py -f tools/taosdumpTest2.py
 
+#python3 test.py -f tools/taosdemoTest.py
 #python3 test.py -f tools/taosdemoTestWithoutMetric.py
 #python3 test.py -f tools/taosdemoTestWithJson.py
 #python3 test.py -f tools/taosdemoTestLimitOffset.py
