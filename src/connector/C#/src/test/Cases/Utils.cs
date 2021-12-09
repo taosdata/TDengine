7a05996a8cf7aa182c8ebe3a41c55e2cc74b61af
xiaolei li <85657333+xleili@users.noreply.github.com>
2021-12-09 12:37:35 +0800

    [TD-10522]<feature>:csharp support stmt (#8924)
    * [TD-10522]<feature>:csharp support stmt
    
    * submit twice
    
    * add fetchfeild test case
    
    * fix singlebind error in linux
    
    * delete extra writeline

---
+++ src/connector/C#/src/test/Cases/Utils.cs
@@ -0,0 +1,166 @@
+using System;
+using TDengineDriver;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Collections.Generic;
+namespace Test.UtilsTools
+{
+    public class UtilsTools
+    {
+
+        static string configDir = "C:/TDengine/cfg";
+
+        public static IntPtr TDConnection(string ip, string user, string password, string db, short port)
+        {
+            TDengine.Options((int)TDengineInitOption.TDDB_OPTION_CONFIGDIR, configDir);
+            TDengine.Options((int)TDengineInitOption.TDDB_OPTION_SHELL_ACTIVITY_TIMER, "60");
+            TDengine.Init();
+            return TDengine.Connect(ip, user, password, db, port);
+        }
+
+        public static IntPtr ExecuteQuery(IntPtr conn, String sql)
+        {
+            IntPtr res = TDengine.Query(conn, sql);
+            if ((res == IntPtr.Zero) || (TDengine.ErrorNo(res) != 0))
+            {
+                Console.Write(sql.ToString() + " failure, ");
+                if (res != IntPtr.Zero)
+                {
+                    Console.Write("reason: " + TDengine.Error(res));
+
+                }
+                Console.WriteLine("");
+                ExitProgram();
+            }
+            else
+            {
+                Console.WriteLine(sql.ToString() + " success");
+
+            }
+            return res;
+        }
+        public static void DisplayRes(IntPtr res)
+        {
+            long queryRows = 0;
+            if ((res == IntPtr.Zero) || (TDengine.ErrorNo(res) != 0))
+            {
+                if (res != IntPtr.Zero)
+                {
+                    Console.Write("reason: " + TDengine.Error(res));
+                }
+                Console.WriteLine("");
+                ExitProgram();
+            }
+
+            int fieldCount = TDengine.FieldCount(res);
+            List<TDengineMeta> metas = TDengine.FetchFields(res);
+            for (int j = 0; j < metas.Count; j++)
+            {
+                TDengineMeta meta = (TDengineMeta)metas[j];
+            }
+
+            IntPtr rowdata;
+            StringBuilder builder = new StringBuilder();
+            while ((rowdata = TDengine.FetchRows(res)) != IntPtr.Zero)
+            {
+                queryRows++;
+                for (int fields = 0; fields < fieldCount; ++fields)
+                {
+                    TDengineMeta meta = metas[fields];
+                    int offset = IntPtr.Size * fields;
+                    IntPtr data = Marshal.ReadIntPtr(rowdata, offset);
+
+                    builder.Append("---");
+
+                    if (data == IntPtr.Zero)
+                    {
+                        builder.Append("NULL");
+                        continue;
+                    }
+
+                    switch ((TDengineDataType)meta.type)
+                    {
+                        case TDengineDataType.TSDB_DATA_TYPE_BOOL:
+                            bool v1 = Marshal.ReadByte(data) == 0 ? false : true;
+                            builder.Append(v1);
+                            break;
+                        case TDengineDataType.TSDB_DATA_TYPE_TINYINT:
+                            byte v2 = Marshal.ReadByte(data);
+                            builder.Append(v2);
+                            break;
+                        case TDengineDataType.TSDB_DATA_TYPE_SMALLINT:
+                            short v3 = Marshal.ReadInt16(data);
+                            builder.Append(v3);
+                            break;
+                        case TDengineDataType.TSDB_DATA_TYPE_INT:
+                            int v4 = Marshal.ReadInt32(data);
+                            builder.Append(v4);
+                            break;
+                        case TDengineDataType.TSDB_DATA_TYPE_BIGINT:
+                            long v5 = Marshal.ReadInt64(data);
+                            builder.Append(v5);
+                            break;
+                        case TDengineDataType.TSDB_DATA_TYPE_FLOAT:
+                            float v6 = (float)Marshal.PtrToStructure(data, typeof(float));
+                            builder.Append(v6);
+                            break;
+                        case TDengineDataType.TSDB_DATA_TYPE_DOUBLE:
+                            double v7 = (double)Marshal.PtrToStructure(data, typeof(double));
+                            builder.Append(v7);
+                            break;
+                        case TDengineDataType.TSDB_DATA_TYPE_BINARY:
+                            string v8 = Marshal.PtrToStringAnsi(data);
+                            builder.Append(v8);
+                            break;
+                        case TDengineDataType.TSDB_DATA_TYPE_TIMESTAMP:
+                            long v9 = Marshal.ReadInt64(data);
+                            builder.Append(v9);
+                            break;
+                        case TDengineDataType.TSDB_DATA_TYPE_NCHAR:
+                            string v10 = Marshal.PtrToStringAnsi(data);
+                            builder.Append(v10);
+                            break;
+                    }
+                }
+                builder.Append("---");
+
+                if (queryRows <= 10)
+                {
+                    Console.WriteLine(builder.ToString());
+                }
+                builder.Clear();
+            }
+
+            if (TDengine.ErrorNo(res) != 0)
+            {
+                Console.Write("Query is not complete, Error {0:G}", TDengine.ErrorNo(res), TDengine.Error(res));
+            }
+            TDengine.FreeResult(res); Console.WriteLine("");
+        }
+
+        public static void CloseConnection(IntPtr conn)
+        {
+            if (conn != IntPtr.Zero)
+            {
+                if (TDengine.Close(conn) == 0)
+                {
+                    Console.WriteLine("close connection sucess");
+                }
+                else
+                {
+                    Console.WriteLine("close Connection failed");
+                }
+            }
+        }
+        public static List<TDengineMeta> getField(IntPtr res)
+        {
+            List<TDengineMeta> metas = TDengine.FetchFields(res);
+            return metas;
+        }
+        public static void ExitProgram()
+        {
+            TDengine.Cleanup();
+            System.Environment.Exit(0);
+        }
+    }
+}
