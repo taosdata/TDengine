ccda69505ed8db3d91e0874be5c2255064b92dd0
jiajingbin <jiajaybin@126.com>
2021-12-09 11:23:45 +0800

    modify

--- tests/stability-scripts/taosadapter_stability/agent_dockerfile/run_all.sh
+++ tests/stability-scripts/taosadapter_stability/agent_dockerfile/run_all.sh
@@ -8,7 +8,7 @@ case "$1" in
     echo "4th arg: tcollector_count"
     echo "5th arg: telegraf_count"
     echo "6th arg: node_exporter port range"
-    echo "eg: ./run_all.sh 10 10 1 10 50 10000:10020"
+    echo "eg: ./run_all.sh 10 10 1 10 50 10000:10010"
     exit 0
 ;;
 esac
@@ -22,18 +22,18 @@ node_exporter_count=$6
 ./icinga2_docker/run_icinga2.sh $2 taosadapter1_icinga2_agent* 172.26.10.86 6048 1
 ./statsd_docker/run_statsd.sh $3 taosadapter1_statsd_agent 172.26.10.86 6044
 ./tcollector_docker/run_tcollector.sh $4 taosadapter1_tcollector_agent* 172.26.10.86 6049
-./telegraf_docker/run_telegraf.sh $5 taosadapter1_telegraf_agent* 172.26.10.86 6041 1s taosadapter1_telegraf
+./telegraf_docker/run_telegraf.sh $5 taosadapter1_telegraf_agent* 172.26.10.86 6041 10s taosadapter1_telegraf
 
 ./collectd_docker/run_collectd.sh $1 taosadapter2_collectd_agent* 172.26.10.85 6047 1
 ./icinga2_docker/run_icinga2.sh $2 taosadapter2_icinga2_agent* 172.26.10.85 6048 1
 ./statsd_docker/run_statsd.sh $3 taosadapter2_statsd_agent 172.26.10.85 6044
 ./tcollector_docker/run_tcollector.sh $4 taosadapter2_tcollector_agent* 172.26.10.85 6049
-./telegraf_docker/run_telegraf.sh $5 taosadapter2_telegraf_agent* 172.26.10.85 6041 1s taosadapter2_telegraf
+./telegraf_docker/run_telegraf.sh $5 taosadapter2_telegraf_agent* 172.26.10.85 6041 10s taosadapter2_telegraf
 
 ./collectd_docker/run_collectd.sh $1 taosadapter3_collectd_agent* 172.26.10.84 6047 1
 ./icinga2_docker/run_icinga2.sh $2 taosadapter3_icinga2_agent* 172.26.10.84 6048 1
 ./statsd_docker/run_statsd.sh $3 taosadapter3_statsd_agent 172.26.10.84 6044
 ./tcollector_docker/run_tcollector.sh $4 taosadapter3_tcollector_agent* 172.26.10.84 6049
-./telegraf_docker/run_telegraf.sh $5 taosadapter3_telegraf_agent* 172.26.10.84 6041 1s taosadapter3_telegraf
+./telegraf_docker/run_telegraf.sh $5 taosadapter3_telegraf_agent* 172.26.10.84 6041 10s taosadapter3_telegraf
 
 ./node_exporter_docker/run_node_exporter.sh $6 node_exporter_agent*
