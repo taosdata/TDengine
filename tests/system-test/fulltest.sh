b61b049841cad0cd5d751976e0043b5740bf2bb7
wenzhouwww <wenzhouwww@live.cn>
2021-12-13 16:41:02 +0800

    [TD-11945]<test>: add test case coredump for TD-11945 ,and this issue has been fix on branch feature/TD-6140

--- tests/system-test/fulltest.sh
+++ tests/system-test/fulltest.sh
@@ -1,2 +1,3 @@
 python3 test.py -f 1-insert/0-sql/basic.py 
 python3 test.py -f 0-management/1-stable/create_col_tag.py 
+# python3 test.py -f 2-query/9-others/TD-11945_crash.py # this test case must need TD-6140 merge into develop
