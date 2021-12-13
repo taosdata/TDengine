49d7f38504da1317e49fa704dc1fb1521f3f7cde
jiajingbin <jiajaybin@126.com>
2021-12-13 12:14:45 +0000

    finish jmeter-deploy and telnet line generate

--- tests/perftest-scripts/taosadapter_perftest/src/common/common.py
+++ tests/perftest-scripts/taosadapter_perftest/src/common/common.py
@@ -5,3 +5,26 @@ from config.env_init import *
 class Common:
     def __init__(self):
         pass
+
+    def genTelnetMulTagStr(self, count):
+        tag_str = ""
+        for i in range(0, count):
+            if i < (count-1):
+                tag_str += f't{i}={i} '
+            else:
+                tag_str += f't{i}={i}'
+        return tag_str
+
+    def genTelnetLine(self, tag_count):
+        tag_str = self.genTelnetMulTagStr(tag_count)
+        stb_line = "stb_${stb_counter} " + "1626006833640 " + "32.261068286779754 " + tag_str
+        ltag = tag_str.split("=")
+        ltag[-1] = "${tb_counter}"
+        stag = '='.join(ltag)
+        tb_line = "stb_${stb_counter} " + "1626006833640 " + "32.261068286779754 " + stag
+        row_line = "stb_${stb_counter} " + "${ts_counter} " + "32.261068286779754 " + stag
+        return stb_line, tb_line, row_line
+
+if __name__ == '__main__':
+    com = Common()
+    print(com.genTelnetLine(10))
