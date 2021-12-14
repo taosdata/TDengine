242e8246e5d2373af6bfffcc3b4631d08286e952
Liu Jicong <liujicong@qq.com>
2021-12-14 10:02:36 +0800

    make wal cleanup called once for each init

--- source/libs/wal/src/walMgmt.c
+++ source/libs/wal/src/walMgmt.c
@@ -68,9 +68,12 @@ int32_t walInit() {
 }
 
 void walCleanUp() {
+  int old = atomic_val_compare_exchange_8(&tsWal.inited, 1, 0);
+  if(old == 0) {
+    return;
+  }
   walStopThread();
   taosCloseRef(tsWal.refSetId);
-  atomic_store_8(&tsWal.inited, 0);
   wInfo("wal module is cleaned up");
 }
 
