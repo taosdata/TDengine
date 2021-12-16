acc5c0ec22e8327167a57e66f438d002c2fe9c39
jiajingbin <jiajaybin@126.com>
2021-12-16 10:17:21 +0000

    add telnet-tcp

--- tests/perftest-scripts/taosadapter_perftest/src/common/common.py
+++ tests/perftest-scripts/taosadapter_perftest/src/common/common.py
@@ -27,10 +27,15 @@ class Common:
         return tag_str
 
     def genProtocolLine(self, protocol, tag_count, col_count=None):
-        if protocol == "telnet":
+        if protocol == "telnet-restful":
             base_str = 'stb_${stb_csv_count} ${row_csv_count} 32.261068286779754 t0=${tb_csv_count} '
             tag_str = self.genTelnetMulTagStr(tag_count)
             telnet_line = base_str + tag_str
+            return telnet_line
+        elif protocol == "telnet-tcp":
+            base_str = 'stb_${stb_csv_count} ${row_csv_count} 32.261068286779754 t0=${tb_csv_count} '
+            tag_str = self.genTelnetMulTagStr(tag_count)
+            telnet_line = base_str + tag_str + '${__unescape(\r\n)}'
             return telnet_line
         elif protocol == "json":
             # TODO
@@ -70,13 +75,13 @@ class Common:
                         ts_start += 1
                         f_w.write(input_line)
 
-    def genJmxFile(self):
+    def genJmxFile(self, testcase):
         des_jmx_file_list = list()
         base_jmx_file = os.path.join(self.current_dir, '../../config/taosadapter_performance_test.jmx')
         if config["taosadapter_separate_deploy"]:
             for key in config:
                 if "taosd_dnode" in str(key) and "taosd_dnode1" not in str(key):
-                    des_jmx_file = os.path.join(self.current_dir, f'../../config/{key}.jmx')
+                    des_jmx_file = os.path.join(self.current_dir, f'../../config/{testcase}_{key}.jmx')
                     shutil.copyfile(base_jmx_file, des_jmx_file)
                     with open(des_jmx_file, 'r', encoding='utf-8') as f:
                         file_data = ""
@@ -85,6 +90,10 @@ class Common:
                                 line = line.replace("restful_ip", config[key]['ip'])
                             if "restful_port" in line:
                                 line = line.replace("restful_port", str(config[key]['restful_port']))
+                            if "telnet_ip" in line:
+                                line = line.replace("telnet_ip", config[key]['ip'])
+                            if "telnet_port" in line:
+                                line = line.replace("telnet_port", str(config[key]['telnet_port']))
                             # if "db_name" in line:
                             #     line = line.replace("db_name", key)
                             file_data += line
@@ -102,6 +111,10 @@ class Common:
                         line = line.replace("restful_ip", config['taosd_dnode1']['ip'])
                     if "restful_port" in line:
                         line = line.replace("restful_port", str(config['taosd_dnode1']['restful_port']))
+                    if "telnet_ip" in line:
+                        line = line.replace("telnet_ip", config['taosd_dnode1']['ip'])
+                    if "telnet_port" in line:
+                        line = line.replace("telnet_port", str(config['taosd_dnode1']['telnet_port']))
                     # if "db_name" in line:
                     #     line = line.replace("db_name", "taosd_dnode1")
                     file_data += line
@@ -126,14 +139,14 @@ class Common:
         else:
             os.makedirs(path)
 
-    def genJmeterCmd(self, jmx_file_list, testcase):
+    def genJmeterCmd(self, jmx_file_list):
         jmeter_cmd_list = list()
         for jmx_file in jmx_file_list:
             jmeter_cmd = f'jmeter -n -t {jmx_file}'
             if config['jmeter']['aggregate_report']:
                 current_time = time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime(time.time()))
                 jmx_filename = jmx_file.split('/')[-1].replace('.jmx', '')
-                jmx_filelog = f'{testcase}_{jmx_filename}_{current_time}'
+                jmx_filelog = f'{jmx_filename}_{current_time}'
                 jmeter_report_dir = f'{self.log_dir}/{jmx_filelog}'
                 self.recreateReportDir(jmeter_report_dir)
                 jmeter_cmd += f' -l {jmeter_report_dir}/{jmx_filelog}.log -e -o {jmeter_report_dir}'
@@ -159,8 +172,9 @@ class Common:
             jmx_file_list = list()
             logger.info(f'executing {key}')
             # if config["taosadapter_separate_deploy"]:
-            for jmx_file in self.genJmxFile():
+            for jmx_file in self.genJmxFile(key):
                 jmx_filename = jmx_file.split('/')[-1]
+                # jmx_filename = key + "_" + jmx_filename
                 import_file_name = jmx_filename.replace('jmx', 'txt')
                 import_file = os.path.join(self.current_dir, f'../../config/{import_file_name}')
                 loop_count = self.getLoopCount(value["stb_count"], value["tb_count"], value["row_count"], value["threads"])
@@ -169,8 +183,21 @@ class Common:
                 with open(jmx_file, 'r', encoding='utf-8') as f:
                     file_data = ""
                     for line in f:
-                        if "line_protocol" in line:
-                            line = line.replace("line_protocol", value['protocol'])
+                        if value['protocol'] == 'telnet-tcp':
+                            if "telnet_tcp_status" in line:
+                                line = line.replace("telnet_tcp_status", "true")
+                        if value['protocol'] == 'telnet-restful':
+                            if "drop_db_status" in line:
+                                line = line.replace("drop_db_status", "true")
+                            if "create_db_status" in line:
+                                line = line.replace("create_db_status", "true")
+                            if "telnet_restful_status" in line:
+                                line = line.replace("telnet_restful_status", "true")
+                            if "line_protocol" in line:
+                                line = line.replace("line_protocol", 'telnet')
+                            if "db_name" in line:
+                                db_name = jmx_filename.split('.')[0]
+                                line = line.replace("db_name", db_name)
                         if "import_file" in line:
                             line = line.replace("import_file", import_file)
                         if "input_line" in line:
@@ -179,14 +206,11 @@ class Common:
                             line = line.replace("perf_threads", str(value['threads']))
                         if "loop_count" in line:
                             line = line.replace("loop_count", str(loop_count))
-                        if "db_name" in line:
-                            db_name = jmx_filename.split('.')[0] + '_' + key
-                            line = line.replace("db_name", db_name)
                         file_data += line
                 with open(jmx_file, "w", encoding="utf-8") as f:
                     f.write(file_data)
                 jmx_file_list.append(jmx_file)
-            jmeter_cmd_list = self.genJmeterCmd(jmx_file_list, key)
+            jmeter_cmd_list = self.genJmeterCmd(jmx_file_list)
             self.multiThreadRun(self.genJmeterThreads(jmeter_cmd_list))
             time.sleep(int(''.join(list(filter(str.isdigit, str(value["sleep_time"]))))))
 
