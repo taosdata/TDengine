e743e523c8971d0bc7d2341ed13d2cdcfa96bfd0
huolibo <huolibo@qq.com>
2021-12-16 15:14:13 +0800

    Delete useless code

--- src/connector/jdbc/src/main/java/com/taosdata/jdbc/rs/RestfulResultSet.java
+++ src/connector/jdbc/src/main/java/com/taosdata/jdbc/rs/RestfulResultSet.java
@@ -280,10 +280,6 @@ public class RestfulResultSet extends AbstractResultSet implements ResultSet {
         }
     }
 
-    public static void main(String[] args) {
-
-    }
-
     public static class Field {
         String name;
         int type;
