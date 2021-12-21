ae5cb788ea2e49e1909258f41a994bf9a3f4db83
wenzhouwww <wenzhouwww@live.cn>
2021-12-21 15:06:58 +0800

    [TD-11943]<test>: add test case for taosd crash about select tbname for regular table

--- tests/system-test/fulltest.sh
+++ tests/system-test/fulltest.sh
@@ -2,5 +2,8 @@ python3 test.py -f 0-management/1-stable/create_col_tag.py
 python3 test.py -f 4-taosAdapter/taosAdapter_query.py
 python3 test.py -f 4-taosAdapter/taosAdapter_insert.py
 
+
+
+python3 test.py -f  2-query/9-others/TD-11943.py
 #python3 test.py -f 2-query/9-others/TD-11389.py # this case will run when this bug fix  TD-11389
 python3 test.py -f 5-taos-tools/taosdump/basic.py 
