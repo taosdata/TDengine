fec3dacb1245a5c2fa34a5379c621c446a893daa
Shuduo Sang <sangshuduo@gmail.com>
2021-12-21 17:42:33 +0800

    Revert "add 5-taos-tools/taosdump/taosdumpTestTypeJson.py"
    This reverts commit 6fa2a810afbf07dcf3029937926789fd431a1b8b.

--- tests/develop-test/fulltest.sh
+++ tests/develop-test/fulltest.sh
@@ -1,4 +1,3 @@
 python3 test.py -f 0-management/3-tag/json_tag.py
 python3 test.py -f 1-insert/0-sql/batchInsert.py
 python3 test.py -f 2-query/7-nest/ts_hidden_column.py
-python3 test.py -f 5-taos-tools/taosdump/taosdumpTestTypeJson.py
