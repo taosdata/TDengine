372d67367f35a39e23062494ef5c5b1d59b5659a
cpwu <cpwu@tapsdata.com>
2021-12-21 11:39:06 +0800

    add case to fulltest

--- tests/system-test/fulltest.sh
+++ tests/system-test/fulltest.sh
@@ -3,4 +3,6 @@ python3 test.py -f 4-taosAdapter/taosAdapter_query.py
 python3 test.py -f 4-taosAdapter/taosAdapter_insert.py
 
 #python3 test.py -f 2-query/9-others/TD-11389.py # this case will run when this bug fix  TD-11389
-python3 test.py -f 5-taos-tools/taosdump/basic.py 
+python3 test.py -f 5-taos-tools/taosdump/basic.py
+
+python3 test.py -f 2-query/0-aggregate/TD-11256.py
