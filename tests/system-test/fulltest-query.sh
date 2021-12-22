4db68aecf79911dfbb8a41c23f0505d84eaa095f
wenzhouwww <wenzhouwww@live.cn>
2021-12-22 20:01:35 +0800

    Update fulltest-query.sh

--- tests/system-test/fulltest-query.sh
+++ tests/system-test/fulltest-query.sh
@@ -1,5 +1,3 @@
 python3 ./test.py -f 2-query/ts_hidden_column.py
 python3 ./test.py -f 2-query/union-order.py
-python3 ./test.py -f 2-query/TD-12165.py
-python3 ./test.py -f 2-query/TD-12145.py
 python3 ./test.py -f 2-query/TD-11483.py
