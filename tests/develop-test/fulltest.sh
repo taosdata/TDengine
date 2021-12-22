71717e0a5df3d26076ceb70ca69c2fbf1a6affd8
shenglian zhou <slzhou@taosdata.com>
2021-12-22 13:54:24 +0800

    remove fulltest.sh

--- tests/develop-test/fulltest.sh
+++
@@ -1,4 +0,0 @@
-python3 test.py -f 0-management/3-tag/json_tag.py
-python3 test.py -f 1-insert/0-sql/batchInsert.py
-python3 test.py -f 2-query/7-nest/ts_hidden_column.py
-python3 test.py -f 2-query/9-others/session_two_stage.py
