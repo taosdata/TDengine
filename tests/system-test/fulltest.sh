08bdb04dbe76ca6963eeebabf692efa69ee1cb09
cpwu <cpwu@taosdata.com>
2021-12-22 13:50:37 +0800

    Update fulltest.sh

--- tests/system-test/fulltest.sh
+++ tests/system-test/fulltest.sh
@@ -4,4 +4,5 @@ python3 test.py -f 0-management/1-stable/create_col_tag.py
 python3 test.py -f 4-taosAdapter/taosAdapter_query.py
 python3 test.py -f 4-taosAdapter/taosAdapter_insert.py
 python3 test.py -f 5-taos-tools/taosdump/basic.py 
+
 python3 test.py -f 4-taosAdapter/TD-12163.py
