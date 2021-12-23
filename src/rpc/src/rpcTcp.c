2c5301abfd9912ee8ca0962c2f72712d6fb7a0b9
xywang <xywang@taosdata.com>
2021-12-24 02:03:17 +0800

    [TS-933]<fix>: fixed memory leak

--- src/rpc/src/rpcTcp.c
+++ src/rpc/src/rpcTcp.c
@@ -244,13 +244,9 @@ static void *taosAcceptTcpConnection(void *arg) {
   tDebug("%s TCP server is ready, ip:0x%x:%hu", pServerObj->label, pServerObj->ip, pServerObj->port);
   setThreadName("acceptTcpConn");
 
-  while (1) {
+  while (!pServerObj->stop) {
     socklen_t addrlen = sizeof(caddr);
     connFd = accept(pServerObj->fd, (struct sockaddr *)&caddr, &addrlen);
-    if (pServerObj->stop) {
-      tDebug("%s TCP server stop accepting new connections", pServerObj->label);
-      break;
-    }
 
     if (connFd == -1) {
       if (errno == EINVAL) {
@@ -272,7 +268,6 @@ static void *taosAcceptTcpConnection(void *arg) {
       continue;
     }
 
-
     // pick up the thread to handle this connection
     pThreadObj = pServerObj->pThreadObj[threadId];
 
@@ -292,6 +287,8 @@ static void *taosAcceptTcpConnection(void *arg) {
     threadId++;
     threadId = threadId % pServerObj->numOfThreads;
   }
+
+  tDebug("%s TCP server stop accepting new connections", pServerObj->label);
 
   taosCloseSocket(pServerObj->fd);
   return NULL;
@@ -546,12 +543,8 @@ static void *taosProcessTcpData(void *param) {
   snprintf(name, tListLen(name), "%s-tcp", pThreadObj->label);
   setThreadName(name);
 
-  while (1) {
+  while (!pThreadObj->stop) {
     int fdNum = epoll_wait(pThreadObj->pollFd, events, maxEvents, TAOS_EPOLL_WAIT_TIME);
-    if (pThreadObj->stop) {
-      tDebug("%s TCP thread get stop event, exiting...", pThreadObj->label);
-      break;
-    }
     if (fdNum < 0) continue;
 
     for (int i = 0; i < fdNum; ++i) {
@@ -586,6 +579,8 @@ static void *taosProcessTcpData(void *param) {
 
     if (pThreadObj->stop) break;
   }
+
+  tDebug("%s TCP thread get stop event, exiting...", pThreadObj->label);
 
   if (pThreadObj->pollFd >=0) {
     EpollClose(pThreadObj->pollFd);
@@ -676,14 +671,12 @@ static void taosFreeFdObj(SFdObj *pFdObj) {
 
   if (pFdObj->prev) {
     (pFdObj->prev)->next = pFdObj->next;
-    pFdObj->prev = NULL;
   } else {
     pThreadObj->pHead = pFdObj->next;
   }
 
   if (pFdObj->next) {
     (pFdObj->next)->prev = pFdObj->prev;
-    pFdObj->next = NULL;
   }
 
   pthread_mutex_unlock(&pThreadObj->mutex);
