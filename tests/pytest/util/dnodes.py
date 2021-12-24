fe4229234004a635f4ec285eb9904ee75573adc3
liuyq-617 <yqliu@taosdata.com>
2021-12-24 10:39:33 +0800

    fix

--- tests/pytest/util/dnodes.py
+++ tests/pytest/util/dnodes.py
@@ -112,7 +112,7 @@ class TDSimClient:
         
         # update extra client config
         clientCfgkeys = ["clientMerge"]
-        if updatecfgDict[0] and updatecfgDict[0][0]:
+        if bool(updatecfgDict) and updatecfgDict[0] and updatecfgDict[0][0]:
             for key, value in updatecfgDict[0][0].items():                
                 if key in clientCfgkeys:
                     self.cfg(key, value)
@@ -238,7 +238,7 @@ class TDDnode:
         # self.cfg("logDir",self.logDir)
         # print(updatecfgDict)
         isFirstDir = 1
-        if bool(updatecfgDict) and updatecfgDict[0] and updatecfgDict[0][0]:
+        if updatecfgDict[0] and updatecfgDict[0][0]:
             print(updatecfgDict[0][0])
             for key,value in updatecfgDict[0][0].items():
                 if value == 'dataDir' :
