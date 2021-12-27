4982b48f6fb5c2a396b1b64b9c00b4d1b62b4748
Shengliang Guan <slguan@taosdata.com>
2021-12-26 23:29:58 -0800

    minor changes

--- source/client/src/clientMsgHandler.c
+++ source/client/src/clientMsgHandler.c
@@ -31,6 +31,13 @@ int genericRspCallback(void* param, const SDataBuf* pMsg, int32_t code) {
 
 int processConnectRsp(void* param, const SDataBuf* pMsg, int32_t code) {
   SRequestObj* pRequest = param;
+  if (code != TSDB_CODE_SUCCESS) {
+    pRequest->code = code;
+    terrno = code;
+
+    sem_post(&pRequest->body.rspSem);
+    return code;
+  }
 
   STscObj *pTscObj = pRequest->pTscObj;
 
