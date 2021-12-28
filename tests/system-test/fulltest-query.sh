c418f22d4387df36e9d45191bb2e614b8e1fe4ad
wenzhouwww <wenzhouwww@live.cn>
2021-12-28 17:22:37 +0800

    [TD-12145]<test>(query): only add case

--- tests/system-test/fulltest-query.sh
+++ tests/system-test/fulltest-query.sh
@@ -1,4 +1,5 @@
-python3 ./test.py -f 2-query/ts_hidden_column.py
-python3 ./test.py -f 2-query/union-order.py
-python3 ./test.py -f 2-query/TD-12165.py
-python3 ./test.py -f 2-query/TD-12145.py 
+python3 ./test.py -f 2-query/TD-11256.py
+# python3 ./test.py -f 2-query/TD-11389.py
+python3 ./test.py -f 2-query/TD-11945_crash.py
+python3 ./test.py -f 2-query/TD-12340-12342.py
+python3 ./test.py -f 2-query/TD-12204.py
