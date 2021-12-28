61c1014cda10232712b05ae253566656d2f981be
Cary Xu <klxu@taosdata.com>
2021-12-28 17:59:38 +0800

    [TS-995]<fix>(query): validate pTableCheckInfo when querying last/last_row

--- src/tsdb/src/tsdbRead.c
+++ src/tsdb/src/tsdbRead.c
@@ -596,11 +596,11 @@ void tsdbResetQueryHandleForNewTable(TsdbQueryHandleT queryHandle, STsdbQueryCon
 static int32_t lazyLoadCacheLast(STsdbQueryHandle* pQueryHandle) {
   STsdbRepo* pRepo = pQueryHandle->pTsdb;
 
-  // if (!pQueryHandle->pTableCheckInfo) {
-  //   tsdbError("%p table check info is NULL", pQueryHandle);
-  //   terrno = TSDB_CODE_QRY_APP_ERROR;
-  //   return -1;
-  // }
+  if (!pQueryHandle->pTableCheckInfo) {
+    tsdbError("%p table check info is NULL", pQueryHandle);
+    terrno = TSDB_CODE_QRY_APP_ERROR;
+    return -1;
+  }
 
   size_t  numOfTables = taosArrayGetSize(pQueryHandle->pTableCheckInfo);
   int32_t code = 0;
@@ -634,10 +634,9 @@ TsdbQueryHandleT tsdbQueryLastRow(STsdbRepo *tsdb, STsdbQueryCond *pCond, STable
     return NULL;
   }
 
-  lazyLoadCacheLast(pQueryHandle);
-  // if (lazyLoadCacheLast(pQueryHandle) != TSDB_CODE_SUCCESS) {
-  //   return NULL;
-  // };
+  if (lazyLoadCacheLast(pQueryHandle) != TSDB_CODE_SUCCESS) {
+    return NULL;
+  };
 
   int32_t code = checkForCachedLastRow(pQueryHandle, groupList);
   if (code != TSDB_CODE_SUCCESS) { // set the numOfTables to be 0
@@ -659,10 +658,9 @@ TsdbQueryHandleT tsdbQueryCacheLast(STsdbRepo *tsdb, STsdbQueryCond *pCond, STab
     return NULL;
   }
 
-  lazyLoadCacheLast(pQueryHandle);
-  // if (lazyLoadCacheLast(pQueryHandle) != TSDB_CODE_SUCCESS) {
-  //   return NULL;
-  // };
+  if (lazyLoadCacheLast(pQueryHandle) != TSDB_CODE_SUCCESS) {
+    return NULL;
+  };
 
   int32_t code = checkForCachedLast(pQueryHandle);
   if (code != TSDB_CODE_SUCCESS) { // set the numOfTables to be 0
