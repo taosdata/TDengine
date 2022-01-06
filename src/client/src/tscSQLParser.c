53894643ad8b84e04613a4fc3e2fda5e20b79645
wangmm0220 <wangmm0220@gmail.com>
2022-01-06 18:37:05 +0800

    [TD-12252]<feature>(connector,query,insert,other,tools,taosAdapter):deal with escape character

--- src/client/src/tscSQLParser.c
+++ src/client/src/tscSQLParser.c
@@ -4978,15 +4978,13 @@ static int32_t validateMatchExpr(tSqlExpr* pExpr, STableMeta* pTableMeta, int32_
     regex_t regex;
     char    regErrBuf[256] = {0};
 
-    //remove the quote at the begin end of original sql string.
-    uint32_t lenPattern = pRight->exprToken.n - 2;
-    char* pattern = malloc(lenPattern + 1);
-    strncpy(pattern, pRight->exprToken.z+1, lenPattern);
-    pattern[lenPattern] = '\0';
+    char* pattern = malloc(pRight->exprToken.n + 1);
+    strncpy(pattern, pRight->exprToken.z, pRight->exprToken.n);
+    pattern[pRight->exprToken.n] = '\0';
 
     tfree(pRight->value.pz);
     pRight->value.pz = pattern;
-    pRight->value.nLen = lenPattern;
+    pRight->value.nLen = pRight->exprToken.n;
 
     int cflags = REG_EXTENDED;
     if ((errCode = regcomp(&regex, pattern, cflags)) != 0) {
