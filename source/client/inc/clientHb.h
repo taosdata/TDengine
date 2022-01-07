7583b45ecb263e69526c4a0bef1deec2d9d4b2f6
Liu Jicong <liujicong@qq.com>
2022-01-07 10:36:42 +0800

    refine heartbeat interface

--- source/client/inc/clientHb.h
+++ source/client/inc/clientHb.h
@@ -25,44 +25,6 @@ typedef enum {
   HEARTBEAT_TYPE_MAX
 } EHbType;
 
-typedef struct SKlv {
-  int32_t keyLen;
-  int32_t valueLen;
-  void*   key;
-  void*   value;
-} SKlv;
-
-typedef struct SClientHbKey {
-  int32_t connId;
-  int32_t hbType;
-} SClientHbKey;
-
-typedef struct SClientHbReq {
-  SClientHbKey connKey;
-  SHashObj*    info;  // hash<Slv.key, Sklv>
-} SClientHbReq;
-
-typedef struct SClientHbBatchReq {
-  int64_t reqId;
-  SArray* reqs;  // SArray<SClientHbReq>
-} SClientHbBatchReq;
-
-typedef struct SClientHbHandleResult {
-} SClientHbHandleResult;
-
-typedef struct SClientHbRsp {
-  SClientHbKey connKey;
-  int32_t status;
-  int32_t bodyLen;
-  void*   body;
-} SClientHbRsp;
-
-typedef struct SClientHbBatchRsp {
-  int64_t reqId;
-  int64_t rspId;
-  SArray* rsps;  // SArray<SClientHbRsp>
-} SClientHbBatchRsp;
-
 typedef int32_t (*FHbRspHandle)(SClientHbRsp* pReq);
 typedef int32_t (*FGetConnInfo)(SClientHbKey connKey, void* param);
 
@@ -81,73 +43,11 @@ static SClientHbMgr clientHbMgr = {0};
 
 int  hbMgrInit();
 void hbMgrCleanUp();
-
 int hbHandleRsp(void* hbMsg);
+
 
 int hbRegisterConn(SClientHbKey connKey, FGetConnInfo func);
 
+
 int hbAddConnInfo(SClientHbKey connKey, void* key, void* value, int32_t keyLen, int32_t valueLen);
 
-static FORCE_INLINE int taosEncodeSKlv(void** buf, const SKlv* pKlv) {
-  int tlen = 0;
-  tlen += taosEncodeFixedI32(buf, pKlv->keyLen);
-  tlen += taosEncodeFixedI32(buf, pKlv->valueLen);
-  tlen += taosEncodeBinary(buf, pKlv->key, pKlv->keyLen);
-  tlen += taosEncodeBinary(buf, pKlv->value, pKlv->valueLen);
-  return tlen;
-}
-
-static FORCE_INLINE void* taosDecodeSKlv(void* buf, SKlv* pKlv) {
-  buf = taosDecodeFixedI32(buf, &pKlv->keyLen);
-  buf = taosDecodeFixedI32(buf, &pKlv->valueLen);
-  buf = taosDecodeBinary(buf, &pKlv->key, pKlv->keyLen);
-  buf = taosDecodeBinary(buf, &pKlv->value, pKlv->valueLen);
-  return buf;
-}
-
-static FORCE_INLINE int taosEncodeSClientHbKey(void** buf, const SClientHbKey* pKey) {
-  int tlen = 0;
-  tlen += taosEncodeFixedI32(buf, pKey->connId);
-  tlen += taosEncodeFixedI32(buf, pKey->hbType);
-  return tlen;
-}
-
-static FORCE_INLINE void* taosDecodeSClientHbKey(void* buf, SClientHbKey* pKey) {
-  buf = taosDecodeFixedI32(buf, &pKey->connId);
-  buf = taosDecodeFixedI32(buf, &pKey->hbType);
-  return buf;
-}
-
-static FORCE_INLINE int tSerializeSClientHbReq(void** buf, const SClientHbReq* pReq) {
-  int tlen = 0;
-  tlen += taosEncodeSClientHbKey(buf, &pReq->connKey);
-
-  void* pIter = NULL;
-  void* data;
-  SKlv  klv;
-  data = taosHashIterate(pReq->info, pIter);
-  while (data != NULL) {
-    taosHashGetKey(data, &klv.key, (size_t*)&klv.keyLen);
-    klv.valueLen = taosHashGetDataLen(data);
-    klv.value = data;
-    taosEncodeSKlv(buf, &klv);
-
-    data = taosHashIterate(pReq->info, pIter);
-  }
-  return tlen;
-}
-
-static FORCE_INLINE void* tDeserializeClientHbReq(void* buf, SClientHbReq* pReq) {
-  ASSERT(pReq->info != NULL);
-  buf = taosDecodeSClientHbKey(buf, &pReq->connKey);
-
-  // TODO: error handling
-  if (pReq->info == NULL) {
-    pReq->info = taosHashInit(64, taosGetDefaultHashFunction(TSDB_DATA_TYPE_BINARY), true, HASH_NO_LOCK);
-  }
-  SKlv klv;
-  buf = taosDecodeSKlv(buf, &klv);
-  taosHashPut(pReq->info, klv.key, klv.keyLen, klv.value, klv.valueLen);
-
-  return buf;
-}
--- source/client/src/clientHb.c
+++ source/client/src/clientHb.c
@@ -19,27 +19,44 @@ static int32_t mqHbRspHandle(SClientHbRsp* pReq) {
   return 0;
 }
 
+uint32_t hbKeyHashFunc(const char* key, uint32_t keyLen) {
+  return 0;
+}
+
+static void hbMgrInitMqHbFunc() {
+  clientHbMgr.handle[mq] = mqHbRspHandle;
+}
+
 int hbMgrInit() {
   //init once
-  //
-  //init lock
-  //
-  //init handle funcs
-  clientHbMgr.handle[mq] = mqHbRspHandle;
+  int8_t old = atomic_val_compare_exchange_8(&clientHbMgr.inited, 0, 1);
+  if (old == 1) return 0;
+
+  //init config
+  clientHbMgr.reportInterval = 1500;
 
   //init stat
   clientHbMgr.stats = 0;
   
-  //init config
-  clientHbMgr.reportInterval = 1500;
+  //init lock
+  taosInitRWLatch(&clientHbMgr.lock);
+
+  //init handle funcs
+  hbMgrInitMqHbFunc();
 
   //init hash info
-  //
+  clientHbMgr.activeInfo = taosHashInit(64, hbKeyHashFunc, 1, HASH_ENTRY_LOCK);
+  //init getInfoFunc
+  clientHbMgr.getInfoFuncs = taosHashInit(64, hbKeyHashFunc, 1, HASH_ENTRY_LOCK);
   return 0;
 }
 
 void hbMgrCleanUp() {
+  int8_t old = atomic_val_compare_exchange_8(&clientHbMgr.inited, 1, 0);
+  if (old == 0) return;
 
+  taosHashCleanup(clientHbMgr.activeInfo);
+  taosHashCleanup(clientHbMgr.getInfoFuncs);
 }
 
 int hbRegisterConn(SClientHbKey connKey, FGetConnInfo func) {
@@ -51,6 +68,9 @@ int hbAddConnInfo(SClientHbKey connKey, void* key, void* value, int32_t keyLen, 
   //lock
 
   //find req by connection id
+  SClientHbReq* data = taosHashGet(clientHbMgr.activeInfo, &connKey, sizeof(SClientHbKey));
+  ASSERT(data != NULL);
+  taosHashPut(data->info, key, keyLen, value, valueLen);
 
   //unlock
   return 0;
