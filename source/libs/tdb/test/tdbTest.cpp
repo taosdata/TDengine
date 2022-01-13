ac87dcf9661c1b2c2793add1219a15dbec2b8ffa
Hongze Cheng <hzcheng@taosdata.com>
2022-01-13 08:04:43 +0000

    more

--- source/libs/tdb/test/tdbTest.cpp
+++ source/libs/tdb/test/tdbTest.cpp
@@ -1,5 +1,14 @@
 #include "gtest/gtest.h"
 
-TEST(tdb_api_test, tdb_create_open_close_db_test) {
+#include "tdb.h"
 
+TEST(tdb_api_test, tdb_create_open_close_db_test) {
+  int  ret;
+  TDB *dbp;
+
+  tdbCreateDB(&dbp);
+
+  tdbOpenDB(dbp, TDB_BTREE, 0);
+
+  tdbCloseDB(dbp, 0);
 }
