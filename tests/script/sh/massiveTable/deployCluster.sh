ea57fba331c99db40d9cc3dc360d659105fb6cdc
plum-lihui <huili@taosdata.com>
2022-01-18 18:22:13 +0800

    [modify]

--- tests/script/sh/massiveTable/deployCluster.sh
+++ tests/script/sh/massiveTable/deployCluster.sh
@@ -8,17 +8,18 @@ set -e
 # deployCluster.sh 
 
 curr_dir=$(pwd)
+echo "currect pwd: ${curr_dir}"
 
-source ./cleanCluster.sh -r /data
-source ./cleanCluster.sh -r /data2
+./cleanCluster.sh -r "/data"
+./cleanCluster.sh -r "/data2"
 
-source ./compileVersion.sh -r ${curr_dir}/../../../../ -v "3.0"
+./compileVersion.sh -r ${curr_dir}/../../../../ -v "3.0"
 
-source ./setupDnodes.sh -r /data  -n 1 -f trd02:7000 -p 7000
-source ./setupDnodes.sh -r /data2 -n 1 -f trd02:7000 -p 8000
+./setupDnodes.sh -r "/data"  -n 1 -f "trd02:7000" -p 7000
+./setupDnodes.sh -r "/data2" -n 1 -f "trd02:7000" -p 8000
 
-#source ./setupDnodes.sh -r /data  -n 2 -f trd02:7000 -p 7000
-#source ./setupDnodes.sh -r /data2 -n 2 -f trd02:7000 -p 8000
+#./setupDnodes.sh -r "/data"  -n 2 -f trd02:7000 -p 7000
+#./setupDnodes.sh -r "/data2" -n 2 -f trd02:7000 -p 8000
 
 
 
