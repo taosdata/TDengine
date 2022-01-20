c978eb12ff5bbd901a4ce81c7346401bf82500c2
Shengliang Guan <slguan@taosdata.com>
2022-01-19 20:33:52 -0800

    TD-13101

--- source/libs/tfs/test/tfsTest.cpp
+++ source/libs/tfs/test/tfsTest.cpp
@@ -60,6 +60,7 @@ TEST_F(TfsTest, 02_AllocDisk) {
   dCfg.level = 0;
   dCfg.primary = 1;
 
+  taosRemoveDir(root);
   taosMkDir(root);
   STfs *pTfs = tfsOpen(&dCfg, 1);
   ASSERT_NE(pTfs, nullptr);
@@ -110,6 +111,7 @@ TEST_F(TfsTest, 03_Dir) {
   dCfg.level = 0;
   dCfg.primary = 1;
 
+  taosRemoveDir(root);
   taosMkDir(root);
   STfs *pTfs = tfsOpen(&dCfg, 1);
   ASSERT_NE(pTfs, nullptr);
@@ -130,7 +132,7 @@ TEST_F(TfsTest, 03_Dir) {
   EXPECT_EQ(taosDirExist(ap2), 0);
 
   char p3[] = "p3/p2/p1/p0";
-  char ap3[128] ={0};
+  char ap3[128] = {0};
   snprintf(ap3, 128, "%s%s%s", root, TD_DIRSEP, p3);
   EXPECT_NE(taosDirExist(ap3), 0);
   EXPECT_NE(tfsMkdir(pTfs, p3), 0);
@@ -156,3 +158,42 @@ TEST_F(TfsTest, 03_Dir) {
 
   tfsClose(pTfs);
 }
+#if 0
+TEST_F(TfsTest, 04_File) {
+  int32_t  code = 0;
+  SDiskCfg dCfg = {0};
+  tstrncpy(dCfg.dir, root, TSDB_FILENAME_LEN);
+  dCfg.level = 0;
+  dCfg.primary = 1;
+
+  taosRemoveDir(root);
+  taosMkDir(root);
+  STfs *pTfs = tfsOpen(&dCfg, 1);
+  ASSERT_NE(pTfs, nullptr);
+
+  STfsFile file0;
+  STfsFile file1;
+  STfsFile file2;
+  STfsFile file3;
+  STfsFile file4;
+  SDiskID  did0 = {0};
+  SDiskID  did1 = {0};
+  SDiskID  did2 = {0};
+  SDiskID  did3 = {0};
+  SDiskID  did4 = {0};
+  did3.id = 1;
+  did4.level = 1;
+  tfsInitFile(pTfs, &file0, did0, "fname");
+  tfsInitFile(pTfs, &file1, did1, "fname");
+  tfsInitFile(pTfs, &file2, did2, "fnamex");
+  tfsInitFile(pTfs, &file3, did3, "fname");
+  tfsInitFile(pTfs, &file4, did4, "fname");
+
+  EXPECT_TRUE(tfsIsSameFile(&file0, &file1));
+  EXPECT_FALSE(tfsIsSameFile(&file0, &file2));
+  EXPECT_FALSE(tfsIsSameFile(&file0, &file3));
+  EXPECT_FALSE(tfsIsSameFile(&file0, &file4));
+
+  tfsClose(pTfs);
+}
+#endif
