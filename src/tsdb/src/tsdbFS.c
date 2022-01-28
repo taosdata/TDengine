6e82734109cf850346da2ae4b803263daf3799d9
Cary Xu <klxu@taosdata.com>
2022-01-28 17:52:49 +0800

    update

--- src/tsdb/src/tsdbFS.c
+++ src/tsdb/src/tsdbFS.c
@@ -92,9 +92,7 @@ static int tsdbEncodeDFileSetArray(void **buf, SArray *pArray) {
 
 static int tsdbDecodeDFileSetArray(void **originBuf, void *buf, SArray *pArray, SFSHeader *pSFSHeader) {
   uint64_t  nset = 0;
-  SDFileSet dset = {0};
-  dset.ver = TSDB_FSET_VER_0;  // default value
-
+  
   taosArrayClear(pArray);
 
   buf = taosDecodeFixedU64(buf, &nset);
@@ -113,6 +111,7 @@ static int tsdbDecodeDFileSetArray(void **originBuf, void *buf, SArray *pArray, 
   }
 
   for (size_t i = 0; i < nset; i++) {
+    SDFileSet dset = {0}; // ver is TSDB_FSET_VER_0(0) at default
     buf = tsdbDecodeDFileSet(buf, &dset, pSFSHeader->version);
     taosArrayPush(pArray, (void *)(&dset));
   }
