d0bb521a2252c1753b37e549cc04f0cc6877686e
Ganlin Zhao <ganlinzhao@hotmail.com>
2022-02-08 14:29:41 +0800

    [TD-11220]<feature>(query): time related functions

--- src/common/src/texpr.c
+++ src/common/src/texpr.c
@@ -1189,7 +1189,8 @@ int32_t exprValidateTimeNode(tExprNode *pExpr) {
         return TSDB_CODE_TSC_OUT_OF_MEMORY;
       }
       child->pVal->nType = TSDB_DATA_TYPE_TIMESTAMP;
-      child->pVal->i64   = taosGetTimestampToday() * pExpr->_func.precision;
+      int64_t timeValSec = taosGetTimestampToday() * 1000;
+      child->pVal->i64 = convertTimePrecision(timeValSec, TSDB_TIME_PRECISION_MILLI, pExpr->_func.precision);
       pExpr->resultType  = TSDB_DATA_TYPE_TIMESTAMP;
       pExpr->resultBytes = (int16_t)tDataTypes[pExpr->resultType].bytes;
       break;
