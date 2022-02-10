29cb13cf281bc35106237340a061d11aed782737
Shengliang Guan <slguan@taosdata.com>
2022-02-10 16:37:22 +0800

    update vload info every time

--- source/dnode/mnode/impl/src/mndDnode.c
+++ source/dnode/mnode/impl/src/mndDnode.c
@@ -322,6 +322,27 @@ static int32_t mndProcessStatusReq(SMnodeMsg *pReq) {
     }
   }
 
+  int32_t numOfVloads = (int32_t)taosArrayGetSize(statusReq.pVloads);
+  for (int32_t v = 0; v < numOfVloads; ++v) {
+    SVnodeLoad *pVload = taosArrayGet(statusReq.pVloads, v);
+
+    SVgObj *pVgroup = mndAcquireVgroup(pMnode, pVload->vgId);
+    if (pVgroup != NULL) {
+      if (pVload->role == TAOS_SYNC_STATE_LEADER) {
+        pVgroup->numOfTables = pVload->numOfTables;
+        pVgroup->numOfTimeSeries = pVload->numOfTimeSeries;
+        pVgroup->totalStorage = pVload->totalStorage;
+        pVgroup->compStorage = pVload->compStorage;
+        pVgroup->pointsWritten = pVload->pointsWritten;
+      }
+      for (int32_t vg = 0; vg < pVgroup->replica; ++vg) {
+        pVgroup->vnodeGid[vg].role = pVload->role;
+      }
+    }
+
+    mndReleaseVgroup(pMnode, pVgroup);
+  }
+
   int64_t curMs = taosGetTimestampMs();
   bool    online = mndIsDnodeOnline(pMnode, pDnode, curMs);
   bool    dnodeChanged = (statusReq.dver != sdbGetTableVer(pMnode->pSdb, SDB_DNODE));
@@ -373,27 +394,6 @@ static int32_t mndProcessStatusReq(SMnodeMsg *pReq) {
     pDnode->rebootTime = statusReq.rebootTime;
     pDnode->numOfCores = statusReq.numOfCores;
     pDnode->numOfSupportVnodes = statusReq.numOfSupportVnodes;
-
-    int32_t numOfVloads = (int32_t)taosArrayGetSize(statusReq.pVloads);
-    for (int32_t v = 0; v < numOfVloads; ++v) {
-      SVnodeLoad *pVload = taosArrayGet(statusReq.pVloads, v);
-
-      SVgObj *pVgroup = mndAcquireVgroup(pMnode, pVload->vgId);
-      if (pVgroup != NULL) {
-        if (pVload->role == TAOS_SYNC_STATE_LEADER) {
-          pVgroup->numOfTables = pVload->numOfTables;
-          pVgroup->numOfTimeSeries = pVload->numOfTimeSeries;
-          pVgroup->totalStorage = pVload->totalStorage;
-          pVgroup->compStorage = pVload->compStorage;
-          pVgroup->pointsWritten = pVload->pointsWritten;
-        }
-        for (int32_t vg = 0; vg < pVgroup->replica; ++vg) {
-          pVgroup->vnodeGid[vg].role = pVload->role;
-        }
-      }
-
-      mndReleaseVgroup(pMnode, pVgroup);
-    }
 
     SStatusRsp statusRsp = {0};
     statusRsp.dver = sdbGetTableVer(pMnode->pSdb, SDB_DNODE);
