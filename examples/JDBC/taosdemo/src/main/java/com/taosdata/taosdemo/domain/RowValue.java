b3efc470c8cdea3f69a6865950ab67063790dc97
Shuduo Sang <sangshuduo@gmail.com>
2022-02-10 16:19:06 +0800

    [TD-13408]<test>: move examples back to TDegnine (#10196)
    for 3.0

---
+++ examples/JDBC/taosdemo/src/main/java/com/taosdata/taosdemo/domain/RowValue.java
@@ -0,0 +1,14 @@
+package com.taosdata.taosdemo.domain;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class RowValue {
+    private List<FieldValue> fields;
+
+    public RowValue(List<FieldValue> fields) {
+        this.fields = fields;
+    }
+}
