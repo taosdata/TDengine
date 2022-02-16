54763720408b7189f1de78a23bdb10a5fdb4d393
zyyang <zyyang@taosdata.com>
2022-02-16 15:16:01 +0800

    change

--- packaging/tools/install.sh
+++ packaging/tools/install.sh
@@ -173,7 +173,9 @@ function install_main_path() {
   ${csudo}mkdir -p ${install_main_dir}/bin
   #  ${csudo}mkdir -p ${install_main_dir}/connector
   ${csudo}mkdir -p ${install_main_dir}/driver
-  ${csudo}mkdir -p ${install_main_dir}/examples
+  if [ $productName == "TDengine" ]; then
+    ${csudo}mkdir -p ${install_main_dir}/examples
+  fi
   ${csudo}mkdir -p ${install_main_dir}/include
   #  ${csudo}mkdir -p ${install_main_dir}/init.d
   if [ "$verMode" == "cluster" ]; then
