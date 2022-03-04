3896b1df925799f894a9bdaa55491a95fefc70fe
xywang <xywang@taosdata.com>
2022-03-04 14:49:40 +0800

    [TS-1233]<fix>: fixed coredump when timestamp collection excessed threshold

--- src/query/src/qTsbuf.c
+++ src/query/src/qTsbuf.c
@@ -492,6 +492,10 @@ void tsBufAppend(STSBuf* pTSBuf, int32_t id, tVariant* tag, const char* pData, i
 }
 
 void tsBufFlush(STSBuf* pTSBuf) {
+  if (pTSBuf->numOfGroups <= 0) {
+    return;
+  }
+
   writeDataToDisk(pTSBuf);
   shrinkBuffer(&pTSBuf->tsData);
   
