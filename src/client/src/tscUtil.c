78ea7de4744273e1254cae938391a1f328a301fc
Ganlin Zhao <ganlinzhao@hotmail.com>
2022-03-08 14:22:21 +0800

    [TD-11216]<feature>: Time window related keywords

--- src/client/src/tscUtil.c
+++ src/client/src/tscUtil.c
@@ -2551,8 +2551,8 @@ SExprInfo* tscExprCreate(STableMetaInfo* pTableMetaInfo, int16_t functionId, SCo
     p->colInfo.colId = TSDB_TBNAME_COLUMN_INDEX;
     p->colBytes = s->bytes;
     p->colType  = s->type;
-  } else if (pColIndex->columnIndex > TSDB_TBNAME_COLUMN_INDEX &&
-             pColIndex->columnIndex <= TSDB_TBNAME_COLUMN_INDEX) {
+  } else if (pColIndex->columnIndex < TSDB_TBNAME_COLUMN_INDEX &&
+             pColIndex->columnIndex >= TSDB_MIN_VALID_COLUMN_INDEX) {
     SSchema* s = tGetTimeWindowColumnSchema(pColIndex->columnIndex);
     p->colInfo.colId = s->colId;
     p->colBytes = s->bytes;
