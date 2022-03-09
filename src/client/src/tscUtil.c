5f9f234f937d5b2202a16e767ca960fe2c96519a
wangmm0220 <wangmm0220@gmail.com>
2022-03-09 15:54:55 +0800

    finish basic logic for state function

--- src/client/src/tscUtil.c
+++ src/client/src/tscUtil.c
@@ -5083,6 +5083,7 @@ int32_t tscCreateQueryFromQueryInfo(SQueryInfo* pQueryInfo, SQueryAttr* pQueryAt
   pQueryAttr->limit             = pQueryInfo->limit;
   pQueryAttr->slimit            = pQueryInfo->slimit;
   pQueryAttr->order             = pQueryInfo->order;
+  pQueryAttr->originOrder       = pQueryInfo->order.order;
   pQueryAttr->fillType          = pQueryInfo->fillType;
   pQueryAttr->havingNum         = pQueryInfo->havingFieldNum;
   pQueryAttr->pUdfInfo          = pQueryInfo->pUdfInfo;
