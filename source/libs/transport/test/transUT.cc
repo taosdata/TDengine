af3a8be54e69598c4f71a694e96a1955822c6f8c
yihaoDeng <luomoxyz@126.com>
2022-03-14 22:21:30 +0800

    add test UT

--- source/libs/transport/test/transUT.cc
+++ source/libs/transport/test/transUT.cc
@@ -29,24 +29,25 @@ const char *ckey = "ckey";
 class Server;
 int port = 7000;
 // server process
+typedef void (*CB)(void *parent, SRpcMsg *pMsg, SEpSet *pEpSet);
 static void processReq(void *parent, SRpcMsg *pMsg, SEpSet *pEpSet);
 // client process;
 static void processResp(void *parent, SRpcMsg *pMsg, SEpSet *pEpSet);
 class Client {
  public:
   void Init(int nThread) {
-    memset(&rpcInit, 0, sizeof(rpcInit));
-    rpcInit.localPort = 0;
-    rpcInit.label = (char *)label;
-    rpcInit.numOfThreads = nThread;
-    rpcInit.cfp = processResp;
-    rpcInit.user = (char *)user;
-    rpcInit.secret = (char *)secret;
-    rpcInit.ckey = (char *)ckey;
-    rpcInit.spi = 1;
-    rpcInit.parent = this;
-    rpcInit.connType = TAOS_CONN_CLIENT;
-    this->transCli = rpcOpen(&rpcInit);
+    memset(&rpcInit_, 0, sizeof(rpcInit_));
+    rpcInit_.localPort = 0;
+    rpcInit_.label = (char *)label;
+    rpcInit_.numOfThreads = nThread;
+    rpcInit_.cfp = processResp;
+    rpcInit_.user = (char *)user;
+    rpcInit_.secret = (char *)secret;
+    rpcInit_.ckey = (char *)ckey;
+    rpcInit_.spi = 1;
+    rpcInit_.parent = this;
+    rpcInit_.connType = TAOS_CONN_CLIENT;
+    this->transCli = rpcOpen(&rpcInit_);
     tsem_init(&this->sem, 0, 0);
   }
   void SetResp(SRpcMsg *pMsg) {
@@ -55,9 +56,27 @@ class Client {
   }
   SRpcMsg *Resp() { return &this->resp; }
 
-  void Restart() {
+  void Restart(CB cb) {
     rpcClose(this->transCli);
-    this->transCli = rpcOpen(&rpcInit);
+    rpcInit_.cfp = cb;
+    this->transCli = rpcOpen(&rpcInit_);
+  }
+  void setPersistFP(bool (*pfp)(void *parent, tmsg_t msgType)) {
+    rpcClose(this->transCli);
+    rpcInit_.pfp = pfp;
+    this->transCli = rpcOpen(&rpcInit_);
+  }
+  void setConstructFP(void *(*mfp)(void *parent, tmsg_t msgType)) {
+    rpcClose(this->transCli);
+    rpcInit_.mfp = mfp;
+    this->transCli = rpcOpen(&rpcInit_);
+  }
+  void setPAndMFp(bool (*pfp)(void *parent, tmsg_t msgType), void *(*mfp)(void *parent, tmsg_t msgType)) {
+    rpcClose(this->transCli);
+
+    rpcInit_.pfp = pfp;
+    rpcInit_.mfp = mfp;
+    this->transCli = rpcOpen(&rpcInit_);
   }
 
   void SendAndRecv(SRpcMsg *req, SRpcMsg *resp) {
@@ -79,7 +98,7 @@ class Client {
 
  private:
   tsem_t   sem;
-  SRpcInit rpcInit;
+  SRpcInit rpcInit_;
   void *   transCli;
   SRpcMsg  resp;
 };
@@ -133,39 +152,56 @@ static void processResp(void *parent, SRpcMsg *pMsg, SEpSet *pEpSet) {
   client->SetResp(pMsg);
   client->SemPost();
 }
+
+static void initEnv() {
+  dDebugFlag = 143;
+  vDebugFlag = 0;
+  mDebugFlag = 143;
+  cDebugFlag = 0;
+  jniDebugFlag = 0;
+  tmrDebugFlag = 143;
+  uDebugFlag = 143;
+  rpcDebugFlag = 143;
+  qDebugFlag = 0;
+  wDebugFlag = 0;
+  sDebugFlag = 0;
+  tsdbDebugFlag = 0;
+  tsLogEmbedded = 1;
+  tsAsyncLog = 0;
+
+  std::string path = "/tmp/transport";
+  taosRemoveDir(path.c_str());
+  taosMkDir(path.c_str());
+
+  tstrncpy(tsLogDir, path.c_str(), PATH_MAX);
+  if (taosInitLog("taosdlog", 1) != 0) {
+    printf("failed to init log file\n");
+  }
+}
 class TransObj {
  public:
   TransObj() {
-    dDebugFlag = 143;
-    vDebugFlag = 0;
-    mDebugFlag = 143;
-    cDebugFlag = 0;
-    jniDebugFlag = 0;
-    tmrDebugFlag = 143;
-    uDebugFlag = 143;
-    rpcDebugFlag = 143;
-    qDebugFlag = 0;
-    wDebugFlag = 0;
-    sDebugFlag = 0;
-    tsdbDebugFlag = 0;
-    tsLogEmbedded = 1;
-    tsAsyncLog = 0;
-
-    std::string path = "/tmp/transport";
-    taosRemoveDir(path.c_str());
-    taosMkDir(path.c_str());
-
-    tstrncpy(tsLogDir, path.c_str(), PATH_MAX);
-    if (taosInitLog("taosdlog", 1) != 0) {
-      printf("failed to init log file\n");
-    }
+    initEnv();
     cli = new Client;
     cli->Init(1);
     srv = new Server;
     srv->Start();
   }
-  void RestartCli() { cli->Restart(); }
+
+  void RestartCli(CB cb) { cli->Restart(cb); }
   void StopSrv() { srv->Stop(); }
+  void SetCliPersistFp(bool (*pfp)(void *parent, tmsg_t msgType)) {
+    // do nothing
+    cli->setPersistFP(pfp);
+  }
+  void SetCliMFp(void *(*mfp)(void *parent, tmsg_t msgType)) {
+    // do nothing
+    cli->setConstructFP(mfp);
+  }
+  void SetMAndPFp(bool (*pfp)(void *parent, tmsg_t msgType), void *(*mfp)(void *parent, tmsg_t msgType)) {
+    // do nothing
+    cli->setPAndMFp(pfp, mfp);
+  }
   void RestartSrv() { srv->Restart(); }
   void cliSendAndRecv(SRpcMsg *req, SRpcMsg *resp) { cli->SendAndRecv(req, resp); }
   ~TransObj() {
@@ -191,16 +227,16 @@ class TransEnv : public ::testing::Test {
   TransObj *tr = NULL;
 };
 
-// TEST_F(TransEnv, 01sendAndRec) {
-//  for (int i = 0; i < 1; i++) {
-//    SRpcMsg req = {0}, resp = {0};
-//    req.msgType = 0;
-//    req.pCont = rpcMallocCont(10);
-//    req.contLen = 10;
-//    tr->cliSendAndRecv(&req, &resp);
-//    assert(resp.code == 0);
-//  }
-//}
+TEST_F(TransEnv, 01sendAndRec) {
+  for (int i = 0; i < 1; i++) {
+    SRpcMsg req = {0}, resp = {0};
+    req.msgType = 0;
+    req.pCont = rpcMallocCont(10);
+    req.contLen = 10;
+    tr->cliSendAndRecv(&req, &resp);
+    assert(resp.code == 0);
+  }
+}
 
 TEST_F(TransEnv, 02StopServer) {
   for (int i = 0; i < 1; i++) {
@@ -218,6 +254,31 @@ TEST_F(TransEnv, 02StopServer) {
   tr->StopSrv();
   // tr->RestartSrv();
   tr->cliSendAndRecv(&req, &resp);
-
   assert(resp.code != 0);
+}
+TEST_F(TransEnv, clientUserDefined) {}
+
+TEST_F(TransEnv, cliPersistHandle) {
+  // impl late
+}
+TEST_F(TransEnv, srvPersistHandle) {
+  // impl later
+}
+
+TEST_F(TransEnv, srvPersisHandleExcept) {
+  // conn breken
+  //
+}
+TEST_F(TransEnv, cliPersisHandleExcept) {
+  // conn breken
+}
+
+TEST_F(TransEnv, multiCliPersisHandleExcept) {
+  // conn breken
+}
+TEST_F(TransEnv, multiSrvPersisHandleExcept) {
+  // conn breken
+}
+TEST_F(TransEnv, queryExcept) {
+  // query and conn is broken
 }
