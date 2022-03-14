a3231260d2f4f91453753b215602a72452e1a4e1
Shuduo Sang <sangshuduo@gmail.com>
2022-03-14 18:48:46 +0800

    port more files

--- include/os/osSocket.h
+++ include/os/osSocket.h
@@ -33,8 +33,15 @@
   #include <winbase.h>
   #include <Winsock2.h>
 #else
-  #include <netinet/in.h>
-  #include <sys/epoll.h>
+    #include <netinet/in.h>
+    #include <sys/socket.h>
+
+    #if defined(_TD_DARWIN_64)
+        #include <osEok.h>
+    #else
+        #include <netinet/in.h>
+        #include <sys/epoll.h>
+    #endif
 #endif
 
 #ifdef __cplusplus
@@ -49,7 +56,29 @@ extern "C" {
 #endif
 
 #if defined(_TD_DARWIN_64)
-  #define htobe64 htonll
+//  #define htobe64 htonll
+
+#	include <libkern/OSByteOrder.h>
+
+#	define htobe16(x) OSSwapHostToBigInt16(x)
+#	define htole16(x) OSSwapHostToLittleInt16(x)
+#	define be16toh(x) OSSwapBigToHostInt16(x)
+#	define le16toh(x) OSSwapLittleToHostInt16(x)
+ 
+#	define htobe32(x) OSSwapHostToBigInt32(x)
+#	define htole32(x) OSSwapHostToLittleInt32(x)
+#	define be32toh(x) OSSwapBigToHostInt32(x)
+#	define le32toh(x) OSSwapLittleToHostInt32(x)
+ 
+#	define htobe64(x) OSSwapHostToBigInt64(x)
+#	define htole64(x) OSSwapHostToLittleInt64(x)
+#	define be64toh(x) OSSwapBigToHostInt64(x)
+#	define le64toh(x) OSSwapLittleToHostInt64(x)
+
+#	define __BYTE_ORDER    BYTE_ORDER
+#	define __BIG_ENDIAN    BIG_ENDIAN
+#	define __LITTLE_ENDIAN LITTLE_ENDIAN
+#	define __PDP_ENDIAN    PDP_ENDIAN
 #endif
 
 #define TAOS_EPOLL_WAIT_TIME 500
