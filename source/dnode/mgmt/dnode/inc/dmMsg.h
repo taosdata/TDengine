33fe9fa25ef1aa2793c364a606ca3ed9e0fae7e7
Shengliang Guan <slguan@taosdata.com>
2022-03-15 10:48:56 +0800

    rename

--- source/dnode/mgmt/dnode/inc/dmMsg.h
+++ source/dnode/mgmt/dnode/inc/dmMsg.h
@@ -13,8 +13,8 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef _TD_DND_DNODE_HADNLE_H_
-#define _TD_DND_DNODE_HADNLE_H_
+#ifndef _TD_DND_DNODE_MSG_H_
+#define _TD_DND_DNODE_MSG_H_
 
 #include "dmInt.h"
 
@@ -34,4 +34,4 @@ void    dmProcessGrantRsp(SDnode *pDnode, SRpcMsg *pRsp);
 }
 #endif
 
-#endif /*_TD_DND_DNODE_HADNLE_H_*/
+#endif /*_TD_DND_DNODE_MSG_H_*/
