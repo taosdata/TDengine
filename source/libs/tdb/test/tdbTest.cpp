f906282c6c3be09b1f085a6ed7ca8aad1a52dfaf
Hongze Cheng <hzcheng@taosdata.com>
2022-03-15 09:58:50 +0000

    more TDB

--- source/libs/tdb/test/tdbTest.cpp
+++ source/libs/tdb/test/tdbTest.cpp
@@ -5,7 +5,7 @@
 TEST(tdb_test, simple_test) {
   int    ret;
   STEnv *pEnv;
-  STDb * pDb;
+  STDb  *pDb;
 
   // Open Env
   ret = tdbEnvOpen("tdb", 1024, 20, &pEnv);
@@ -15,9 +15,16 @@ TEST(tdb_test, simple_test) {
   ret = tdbDbOpen("db.db", TDB_VARIANT_LEN, TDB_VARIANT_LEN, NULL, pEnv, &pDb);
   GTEST_ASSERT_EQ(ret, 0);
 
-  // // Insert some data
-  ret = tdbDbInsert(pDb, "key1", 4, "value1", 6);
-  GTEST_ASSERT_EQ(ret, 0);
+  {  // Insert some data
+    ret = tdbDbInsert(pDb, "key1", 4, "value1", 6);
+    GTEST_ASSERT_EQ(ret, 0);
+
+    ret = tdbDbInsert(pDb, "key2", 4, "value1", 6);
+    GTEST_ASSERT_EQ(ret, 0);
+
+    ret = tdbDbInsert(pDb, "key3", 4, "value1", 6);
+    GTEST_ASSERT_EQ(ret, 0);
+  }
 
   ret = tdbDbDrop(pDb);
   GTEST_ASSERT_EQ(ret, 0);
