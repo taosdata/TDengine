488d44a55515152d0982b1a343591343830b4a9c
Hongze Cheng <hzcheng@taosdata.com>
2022-03-24 10:14:46 +0000

    w/r 10M K-V pairs

--- source/libs/tdb/test/tdbTest.cpp
+++ source/libs/tdb/test/tdbTest.cpp
@@ -6,10 +6,10 @@ TEST(tdb_test, simple_test) {
   int    ret;
   STEnv *pEnv;
   STDb  *pDb;
-  int    nData = 2000000;
+  int    nData = 10000000;
 
   // Open Env
-  ret = tdbEnvOpen("tdb", 4096, 25600, &pEnv);
+  ret = tdbEnvOpen("tdb", 4096, 256000, &pEnv);
   GTEST_ASSERT_EQ(ret, 0);
 
   // Create a database
