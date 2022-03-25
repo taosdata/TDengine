dfbd2c205ee4acdd921f85cbca8caa60f670deb3
xleili <xlli@taosdata.com>
2022-03-25 09:51:42 +0800

    [TD-14212]<fix>:add R example code to CI 8th

--- examples/R/rjdbc.sample.R
+++ examples/R/rjdbc.sample.R
@@ -9,7 +9,7 @@ jdbc<- paste(args[1],args[2],sep="/")
 #print(args[1])
 #print(args[2])
 print(jdbc)
-print("***************************taos-jdbcDriver support RJDBC sample begine*************************************")
+print("***************************taos-jdbcDriver support RJDBC sample *************************************")
 #JNI
 # Get JBDC-JNI connection
 drv<-JDBC("com.taosdata.jdbc.TSDBDriver",jdbc, identifier.quote="\"")
@@ -33,8 +33,11 @@ dbSendUpdate(conn,"create table if not exists test(ts timestamp,i8 tinyint,i16 s
 dbSendUpdate(conn,"insert into test_s01 using test tags(1,'sub_01') values(now,1,2,3,4,'binary1','nchar1')(now+1s,5,6,7,8,'binary2','nchar2')(now+2s,9,0,1,2,'binary3','nchar3')")
 
 # View all tables
-table1<-dbGetQuery(conn,"show tables")
+table1<-dbGetQuery(conn,"select * from test")
+print("================================")
 print(table1)
+print("================================")
+
 
 # Functional support for RJDBC
 
@@ -45,26 +48,31 @@ dbListTables(conn)
 dbExistsTable(conn,"test")
 
 # Connect summary information
-summary(conn)
-dbGetInfo(conn)
+#summary(conn)
+#dbGetInfo(conn)
 
  
 # Read all the data from the T1 table
-dbReadTable(conn,"test_s01")
+print("=============dbReadTable===================")
+dbReadTable(conn,"test")
+print("=============dbReadTable===================")
 
 # Execute any non-query SQL statements
-dbSendUpdate(conn, "create table t1(a timestamp, b int, c nchar(12))");
+dbSendUpdate(conn, "create table t1(ts timestamp,id int,u nchar(40))")
+dbGetTables(conn, "test1")
 sampleData=data.frame(ts=c('2022-03-22 00:00:00.000','2022-03-22 10:00:00.000'),id=c(1,2),u=c('涛思数据','TDengine'))
-dbWriteTable(conn, "t1", sampleData, overwrite=FALSE, append=TRUE)
+dbWriteTable(conn, "test1", sampleData, overwrite=FALSE, append=TRUE)
+dbReadTable(conn,"test1")
 
 # Delete table t1
-dbRemoveTable(conn,"t1")
+dbRemoveTable(conn,"test1")
 
 # Extracting data on demand using SQL statements
-dbGetQuery(conn, "select * from test")
-
+print("line 64")
+dbGetQuery(conn, "select * from test_s01")
+print("line 65")
 # Drop database
-dbSendUpdate(conn,"drop database if exists r_example_db")
+# dbSendUpdate(conn,"drop database if exists r_example_db")
 
 # Close the connection
 dbDisconnect(conn)
