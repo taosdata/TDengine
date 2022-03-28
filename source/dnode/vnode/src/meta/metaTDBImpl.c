8ff248a308bb4135e62537bf418bfaeaeb1493b6
Hongze Cheng <hzcheng@taosdata.com>
2022-03-28 06:35:00 +0000

    meta with tdb compile

--- source/dnode/vnode/src/meta/metaTDBImpl.c
+++ source/dnode/vnode/src/meta/metaTDBImpl.c
@@ -15,75 +15,27 @@
 
 #include "metaDef.h"
 
-#include "tdb.h"
+#include "tdbInt.h"
 
 struct SMetaDB {
   TENV *pEnv;
-  TDB * pTbDB;
-  TDB * pSchemaDB;
-  TDB * pNameIdx;
-  TDB * pStbIdx;
-  TDB * pNtbIdx;
-  TDB * pCtbIdx;
-  // tag index hash table
-  // suid+colid --> TDB *
-  struct {
-  } tagIdxHt;
+  TDB  *pTbDB;
+  TDB  *pSchemaDB;
+  TDB  *pNameIdx;
+  TDB  *pStbIdx;
+  TDB  *pNtbIdx;
+  TDB  *pCtbIdx;
 };
 
-#define A(op, flag)                 \
-  do {                              \
-    if ((ret = op) != 0) goto flag; \
-  } while (0)
-
 int metaOpenDB(SMeta *pMeta) {
-  SMetaDB *pDb;
-  TENV *   pEnv;
-  TDB *    pTbDB;
-  TDB *    pSchemaDB;
-  TDB *    pNameIdx;
-  TDB *    pStbIdx;
-  TDB *    pNtbIdx;
-  TDB *    pCtbIdx;
-  int      ret;
-
-  pDb = (SMetaDB *)taosMemoryCalloc(1, sizeof(*pDb));
-  if (pDb == NULL) {
-    return -1;
-  }
-
-  // Create and open the ENV
-  A((tdbEnvCreate(&pEnv)), _err);
-#if 0
-  // Set options of the environment
-  A(tdbEnvSetPageSize(pEnv, 8192), _err);
-  A(tdbEnvSetCacheSize(pEnv, 16 * 1024 * 1024), _err);
-#endif
-  A((tdbEnvOpen(&pEnv)), _err);
-
-  // Create and open each DB
-  A(tdbCreate(&pTbDB), _err);
-  A(tdbOpen(&pTbDB, "table.db", NULL, pEnv), _err);
-
-  A(tdbCreate(&pSchemaDB), _err);
-  A(tdbOpen(&pSchemaDB, "schema.db", NULL, pEnv), _err);
-
-  A(tdbCreate(&pNameIdx), _err);
-  A(tdbOpen(&pNameIdx, "name.db", NULL, pEnv), _err);
-  // tdbAssociate();
-
-  pDb->pEnv = pEnv;
-  pDb->pTbDB = pTbDB;
-  pDb->pSchemaDB = pSchemaDB;
-  pMeta->pDB = pDb;
+  // TODO
+  ASSERT(0);
   return 0;
-
-_err:
-  return -1;
 }
 
 void metaCloseDB(SMeta *pMeta) {
   // TODO
+  ASSERT(0);
 }
 
 int metaSaveTableToDB(SMeta *pMeta, STbCfg *pTbCfg) {
@@ -93,53 +45,117 @@ int metaSaveTableToDB(SMeta *pMeta, STbCfg *pTbCfg) {
 
 int metaRemoveTableFromDb(SMeta *pMeta, tb_uid_t uid) {
   // TODO
+  ASSERT(0);
   return 0;
 }
 
 STbCfg *metaGetTbInfoByUid(SMeta *pMeta, tb_uid_t uid) {
   // TODO
+  ASSERT(0);
   return NULL;
 }
 
 STbCfg *metaGetTbInfoByName(SMeta *pMeta, char *tbname, tb_uid_t *uid) {
   // TODO
+  ASSERT(0);
   return NULL;
 }
 
 SSchemaWrapper *metaGetTableSchema(SMeta *pMeta, tb_uid_t uid, int32_t sver, bool isinline) {
   // TODO
+  ASSERT(0);
   return NULL;
 }
 
 STSchema *metaGetTbTSchema(SMeta *pMeta, tb_uid_t uid, int32_t sver) {
   // TODO
+  ASSERT(0);
   return NULL;
 }
 
 SMTbCursor *metaOpenTbCursor(SMeta *pMeta) {
   // TODO
+  ASSERT(0);
   return NULL;
 }
 
 void metaCloseTbCursor(SMTbCursor *pTbCur) {
   // TODO
+  ASSERT(0);
 }
 
 char *metaTbCursorNext(SMTbCursor *pTbCur) {
   // TODO
+  ASSERT(0);
   return NULL;
 }
 
 SMCtbCursor *metaOpenCtbCursor(SMeta *pMeta, tb_uid_t uid) {
   // TODO
+  ASSERT(0);
   return NULL;
 }
 
 void metaCloseCtbCurosr(SMCtbCursor *pCtbCur) {
   // TODO
+  ASSERT(0);
 }
 
 tb_uid_t metaCtbCursorNext(SMCtbCursor *pCtbCur) {
   // TODO
+  ASSERT(0);
   return 0;
+}
+
+int metaGetTbNum(SMeta *pMeta) {
+  // TODO
+  ASSERT(0);
+  return 0;
+}
+
+STSmaWrapper *metaGetSmaInfoByTable(SMeta *pMeta, tb_uid_t uid) {
+  // TODO
+  ASSERT(0);
+  return NULL;
+}
+
+int metaRemoveSmaFromDb(SMeta *pMeta, int64_t indexUid) {
+  // TODO
+  ASSERT(0);
+  return 0;
+}
+
+int metaSaveSmaToDB(SMeta *pMeta, STSma *pSmaCfg) {
+  // TODO
+  ASSERT(0);
+  return 0;
+}
+
+STSma *metaGetSmaInfoByIndex(SMeta *pMeta, int64_t indexUid) {
+  // TODO
+  ASSERT(0);
+  return NULL;
+}
+
+const char *metaSmaCursorNext(SMSmaCursor *pCur) {
+  // TODO
+  ASSERT(0);
+  return NULL;
+}
+
+void metaCloseSmaCurosr(SMSmaCursor *pCur) {
+  // TODO
+  ASSERT(0);
+}
+
+SArray *metaGetSmaTbUids(SMeta *pMeta, bool isDup) {
+  // TODO
+  ASSERT(0);
+  return NULL;
+}
+
+SMSmaCursor *metaOpenSmaCursor(SMeta *pMeta, tb_uid_t uid) {
+  // TODO
+  ASSERT(0);
+  return NULL;
 }
