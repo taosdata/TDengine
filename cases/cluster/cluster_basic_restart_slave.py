d4d872c89240184e0c13f4a4a9208c538de79624
tangfangzhi <fztang@taosdata.com>
2022-03-31 16:37:28 +0800

    check status after stop/start dnode

--- cases/cluster/cluster_basic_restart_slave.py
+++ cases/cluster/cluster_basic_restart_slave.py
@@ -29,10 +29,14 @@ class ClusterTestBasic(TDCase):
         self.table_num = 10
         self.row_num = 500000
         self.replicas = 3
+        self.status = True
         self.conf = self.get_component_by_name(
             "taosd")[0]
 
     def write_thread(self):
+        '''
+        thread to write data
+        '''
         client_0 = self.tdSql.get_connection(self.conf)
         # clean
         sql = "drop database if exists %s" % (self.db_name)
@@ -63,23 +67,107 @@ class ClusterTestBasic(TDCase):
                 ts = self.ts + j
                 k = 0
                 value_statement = ""
-                while k < 60:
+                while k < 50:
                     value_statement = f"{value_statement} ({ts}+{k}a, {j})"
                     k = k + 1
-                j = j + 60
+                j = j + 50
                 sql = f"insert into {tb} values {value_statement}"
                 # self.logger.info(sql)
                 client_0.execute(sql)
         client_0.close()
         self.logger.info("write thread exit")
 
+    def wait_dnode_status(self, endpoint, status):
+        '''
+        query dnode status periodically until timeout or status match
+        @endpoint
+        @status
+        '''
+        client_0 = self.tdSql.get_connection(self.conf)
+        sql = "use %s" % (self.db_name)
+        client_0.execute(sql)
+        sql = "show dnodes"
+        '''
+        [(1, 'dnode_1:6030', 1, 24, 'ready', 'any', datetime.datetime(2022, 3, 30, 14, 38, 50, 264000), ''),
+         (2, 'dnode_2:6030', 2, 24, 'ready', 'any', datetime.datetime(2022, 3, 30, 14, 39, 11, 665000), ''),
+         (3, 'dnode_3:6030', 1, 24, 'offline', 'any', datetime.datetime(2022, 3, 30, 14, 39, 23, 60000), 'status msg timeout')]
+        '''
+        status_match = False
+        max_retry_time = 10
+        retry_time = 0
+        while status_match == False and retry_time < max_retry_time:
+            i = 0
+            time.sleep(2)
+            result = client_0.query(sql)
+            data = result.fetch_all()
+            self.logger.info("%d show dnodes: %s", retry_time, str(data))
+            while i < len(data):
+                if data[i][1] == endpoint and data[i][4] == status:
+                    status_match = True
+                    break
+                i = i + 1
+            retry_time = retry_time + 1
+        return status_match
+
+    def show_vgroups(self):
+        '''
+        show vgroups
+        '''
+        client_0 = self.tdSql.get_connection(self.conf)
+        sql = "use %s" % (self.db_name)
+        client_0.execute(sql)
+        sql = "show vgroups"
+        '''
+        [(6, 5, 'ready', 3, 4, 'master', 3, 'slave', 2, 'slave', 0)]
+        '''
+        result = client_0.query(sql)
+        data = result.fetch_all()
+        self.logger.info("show vgroups: %s", str(data))
+
     def restart_dnode_thread(self):
+        '''
+        thread to restart dnode
+        '''
         time.sleep(5)
+        # stop dnode_3
         self.logger.info("stop dnode dnode_3:6030")
         self.envMgr.stopDnode("dnode_3:6030")
         time.sleep(5)
+        ret = self.wait_dnode_status("dnode_3:6030", "offline")
+        if ret == False:
+            self.logger.error("dnode status not match")
+            self.error_msg = "dnode status not match"
+            self.status = False
+        self.show_vgroups()
         self.logger.info("start dnode dnode_3:6030")
+        # start dnode_3
         self.envMgr.startDnode("dnode_3:6030")
+        time.sleep(5)
+        ret = self.wait_dnode_status("dnode_3:6030", "ready")
+        if ret == False:
+            self.logger.error("dnode status not match")
+            self.error_msg = "dnode status not match"
+            self.status = False
+        self.show_vgroups()
+        self.logger.info("stop dnode dnode_3:6030")
+        # stop dnode_3
+        self.envMgr.stopDnode("dnode_3:6030")
+        time.sleep(5)
+        ret = self.wait_dnode_status("dnode_3:6030", "offline")
+        if ret == False:
+            self.logger.error("dnode status not match")
+            self.error_msg = "dnode status not match"
+            self.status = False
+        self.show_vgroups()
+        self.logger.info("start dnode dnode_3:6030")
+        # start dnode_3
+        self.envMgr.startDnode("dnode_3:6030")
+        ret = self.wait_dnode_status("dnode_3:6030", "ready")
+        if ret == False:
+            self.logger.error("dnode status not match")
+            self.error_msg = "dnode status not match"
+            self.status = False
+        self.show_vgroups()
         self.logger.info("restart dnode thread exit")
 
     def check_result(self):
@@ -91,9 +179,36 @@ class ClusterTestBasic(TDCase):
         # query
         sql = "select count(*) from %s"  % (self.table_name)
         self.logger.info(sql)
-        result = client_0.query(sql)
-        data = result.fetch_all()
-        self.logger.info("result: %s", str(data[0][0]))
+        i = 0
+        while i < 10:
+            result = client_0.query(sql)
+            data = result.fetch_all()
+            self.logger.info("result: %s", str(data[0][0]))
+            if data[0][0] == 5000000:
+                break
+            i = i + 1
+            time.sleep(2)
+        # total row = self.table_num * self.row_num = 5000000
+        if data[0][0] != 5000000:
+            self.logger.error("row not match")
+            self.error_msg = "row not match"
+            self.status = False
+            return
+        # check data
+        '''c1 = 0
+        i = 0;
+        while i < 10000:
+            sql = "select count(*) from %s where c1 = %d"  % (self.table_name, c1)
+            # self.logger.info(sql)
+            result = client_0.query(sql)
+            data = result.fetch_all()
+            # self.logger.info("result: %s", str(data[0][0]))
+            if data[0][0] != 500:
+                self.logger.error("row not match c1 = %d", c1)
+                self.error_msg = f"row not match c1 = {c1}"
+                self.status = False
+            c1 = c1 + 50
+            i = i + 1'''
 
     def cleanup(self):
         pass
@@ -111,6 +226,7 @@ class ClusterTestBasic(TDCase):
         mthread.join()
         # check result
         self.check_result()
+        return self.status
 
     def cleanup(self):
         pass
