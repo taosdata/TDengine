de946e02c58e4cd138fe875d4fc4e245aeb1813d
dingbo <dingbo8128@163.com>
2022-04-01 14:43:52 +0800

    node async query example

--- docs-examples/node/nativeexample/async_query_example.js
+++ docs-examples/node/nativeexample/async_query_example.js
@@ -1,7 +1,12 @@
 const taos = require("td2.0-connector");
 
-const conn = taos.connect({
-  host: "localhost",
-});
+async function queryExample() {
+  const conn = taos.connect({ host: "localhost", database: "power" });
+  const cursor = conn.cursor();
+  const query = cursor.query("SELECT ts, current FROM meters LIMIT 2");
+  const result = await query.execute_a();
+  console.log(result);
+}
 
-// 未完成, 等待 TD-14448 解决
+queryExample();
+
