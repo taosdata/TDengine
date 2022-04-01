50deb2869924b82d82eb7a413011819a420571a4
xleili <xlli@taosdata.com>
2022-04-01 10:17:33 +0800

    [TD-13707]<feature>:TypeScript RESTful 2st, add CI test script

---
+++ tests/develop-test/3-connectors/TypeScript-REST/test.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+function stopProcess {
+  echo "Stop $1"
+  sudo systemctl stop $1 || echo 'no sudo or systemctl or stop fail'
+  PID=`ps -ef|grep -w $1 | grep -v grep | awk '{print $2}'`
+  while [ -n "$PID" ]
+  do
+    pkill -TERM -x $1
+    sleep 1
+    PID=`ps -ef|grep -w $1 | grep -v grep | awk '{print $2}'`
+  done
+}
+
+stopProcess taosadapter
+stopProcess taosd
+rm -rf /var/lib/taos/*
+rm -rf /var/log/taos/*
+
+curDir=$(dirname $(readlink -f "$0"))
+taosdConfig=$curDir/../../../../packaging/cfg
+adapterConfig=$curDir/../../../../src/plugins/taosadapter/example/config/taosadapter.toml
+
+nohup taosd -c ${taosdConfig} > /dev/null 2>&1 &
+nohup taosadapter -c ${adapterConfig} > /dev/null 2>&1 &
+sleep 10
+
+cd ../../../../
+WKC=`pwd`
+cd ${WKC}/src/connector/TypeScript-REST
+
+npm run example 
+# npm run test 
+
+
+
