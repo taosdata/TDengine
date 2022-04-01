a6cd4c0c51636de735520105fe3bbd0cfd558e55
dingbo <dingbo8128@163.com>
2022-04-01 14:43:52 +0800

    node async query example

--- docs-examples/node/nativeexample/query_example.js
+++ docs-examples/node/nativeexample/query_example.js
@@ -6,4 +6,12 @@ const query = cursor.query("SELECT ts, current FROM meters LIMIT 2");
 query.execute().then(function (result) {
   result.pretty();
 });
-// 目前执行失败，原因不明： TD-14448
+
+// stdout:
+// Successfully connected to TDengine
+// Query OK, 2 row(s) in set (0.00317767s)
+
+//            ts             |         current          |
+// =======================================================
+// 2018-10-03 14:38:05.000   | 10.3                     |
+// 2018-10-03 14:38:15.000   | 12.6                     |
