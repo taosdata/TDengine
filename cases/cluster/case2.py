f1d5e4673ad540a26ab7e3bae15c7e106f055f08
tangfangzhi <fztang@taosdata.com>
2022-04-02 15:12:17 +0800

    multiple thread insert table

--- cases/cluster/case2.py
+++ cases/cluster/case2.py
@@ -22,20 +22,22 @@ class Case2(ClusterCase):
         super().init()
         self.db_name = "testdb"
         self.replicas = 3
+        self.thread_num = 10
         self.stable_name = "stb"
+        self.table_name = "tb"
         self.table_num = 10
         self.row_num = 500000  # row number per table
-        self.max_restart_interval = 5
+        self.max_restart_interval = [1, 10]
         self.restart_times = 3
 
-    def check_result(self):
-        client_0 = self.tdSql.get_connection(self._conf)
+    def check_result_db(self, db, stb):
+        client_0 = self.tdSql.get_connection(self._conf, "dnode_3:6030")
         # use database
-        sql = "use %s" % (self.db_name)
+        sql = "use %s" % (db)
         self.logger.info(sql)
         client_0.execute(sql)
         # query
-        sql = "select count(*) from %s"  % (self.stable_name)
+        sql = "select count(*) from %s"  % (stb)
         self.logger.info(sql)
         i = 0
         while i < 10:
@@ -56,7 +58,7 @@ class Case2(ClusterCase):
         '''c1 = 0
         i = 0;
         while i < 10000:
-            sql = "select count(*) from %s where c1 = %d"  % (self.stable_name, c1)
+            sql = "select count(*) from %s where c1 = %d"  % (self.stb, c1)
             # self.logger.info(sql)
             result = client_0.query(sql)
             data = result.fetch_all()
@@ -68,20 +70,41 @@ class Case2(ClusterCase):
             c1 = c1 + 50
             i = i + 1'''
 
+    def check_result(self):
+        i = 0
+        while i < self.thread_num:
+            db = f"{self.db_name}_{i}"
+            stb = f"{self.stable_name}_{i}"
+            self.check_result_db(db, stb)
+            i = i + 1
+
     def cleanup(self):
         pass
 
     def run(self):
         # insert into database thread
-        mthread=threading.Thread(target=self.insert_into_table,args=(self.db_name, self.stable_name, self.table_num, self.row_num, self.replicas))
+        mthreads = []
+        i = 0
+        while i < self.thread_num:
+            db = f"{self.db_name}_{i}"
+            stb = f"{self.stable_name}_{i}"
+            tb = f"{self.table_name}_{i}"
+            mthread=threading.Thread(target=self.insert_into_table,args=(db, stb, tb, self.table_num, self.row_num, self.replicas))
+            mthreads.append(mthread)
+            mthread.start()
+            i = i + 1
         # restart slave dnode thread
         dthread=threading.Thread(target=self.repeatedly_restart_dnode,args=("dnode_3:6030", self.max_restart_interval, self.restart_times))
         # start thread
-        mthread.start()
         dthread.start()
         # wait thread
         dthread.join()
-        mthread.join()
+        i = 0
+        while i < self.thread_num:
+            mthread = mthreads[i]
+            mthread.join()
+            i = i + 1
+        self.logger.info("checking result ...")
         # check result
         self.check_result()
         return self._status
