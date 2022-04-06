7709f37bca354f28e314bfceba45e614230a8b82
Yang Zhao <yzhao@taosdata.com>
2022-04-07 00:58:36 +0800

    add taosbenchmark example json files (#11260)

--- packaging/tools/makeclient.sh
+++ packaging/tools/makeclient.sh
@@ -145,6 +145,7 @@ if [[ $productName == "TDengine" ]]; then
     cp -r ${examples_dir}/go ${install_dir}/examples
     cp -r ${examples_dir}/nodejs ${install_dir}/examples
     cp -r ${examples_dir}/C# ${install_dir}/examples
+    mkdir -p ${install_dir}/examples/taosbenchmark-json && cp ${examples_dir}/../src/kit/taos-tools/example/* ${install_dir}/examples/taosbenchmark-json
   fi
 
   # Copy connector
