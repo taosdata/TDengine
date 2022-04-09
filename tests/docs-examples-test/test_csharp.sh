f2c638f6271fb3b8e01fe6cfa50f77a0b49c5d35
dingbo <dingbo8128@163.com>
2022-04-09 18:18:26 +0800

    test: add test_csharp.sh

--- tests/docs-examples-test/test_csharp.sh
+++ tests/docs-examples-test/test_csharp.sh
@@ -6,3 +6,21 @@ pgrep taosd || taosd >> /dev/null 2>&1 &
 pgrep taosadapter || taosadapter >> /dev/null 2>&1 &
 cd ../../docs-examples/csharp
 
+dotnet run --project connect.csproj
+
+taos -s "drop database if exists power"
+dotnet run --project sqlinsert.csproj
+dotnet run --project query.csproj
+dotnet run --project asyncquery.csproj
+
+taos -s "drop database if exists power"
+dotnet run --project stmtinsert.csproj
+
+taos -s "drop database if exists test"
+dotnet run --project influxdbline.csproj
+
+taos -s "drop database if exists test"
+dotnet run --project optstelnet.csproj
+
+taos -s "drop database if exists test"
+dotnet run --project optsjson.csproj
