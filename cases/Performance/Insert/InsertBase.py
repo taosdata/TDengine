85db18b918b9b6f64c6b16e52e9023068dcb4fee
jiacy-jcy <714897623@qq.com>
2022-04-09 10:13:44 +0800

    update insert base case

--- cases/Performance/Insert/InsertBase.py
+++ cases/Performance/Insert/InsertBase.py
@@ -1,10 +1,11 @@
 # utf-8
+import os
+from taostest.util.file import read_yaml
 from datetime import datetime
 from typing import List
 from taostest import TDCase
 from taostest.performance.perfor_basic import InsertFile
 from taostest.performance.result_reduction import Perf_Base_func
-
 
 
 class InsertTest(TDCase):
@@ -22,25 +23,44 @@ class InsertTest(TDCase):
         pass
 
     def run(self):
-
         taosBenchmark_iplist: List = self.get_fqdn("taosBenchmark")
         taosd_list = self.get_fqdn("taosd")
         json_data: List = []
         file_name = []
 
+        test_root = os.environ['TEST_ROOT']
+        cfg = read_yaml(test_root + "./cases/Performance/Insert/insert.yaml")
 
         jfile = InsertFile()
-        Insert_file = Perf_Base_func(self.logger,self.run_log_dir)
-        col = jfile.schemacfg(intcount=4, binarycount=(2, 16), doublecount=4)
-        tag = jfile.schemacfg(intcount=1, binarycount=(1, 16))
+        Insert_file = Perf_Base_func(self.logger, self.run_log_dir)
+        col = jfile.schemacfg(intcount=cfg["stb_info"]["col_int_count"],
+                              binarycount=(cfg["stb_info"]["col_binary_count"], cfg["stb_info"]["col_binary_length"]),
+                              doublecount=cfg["stb_info"]["col_double_count"])
+        tag = jfile.schemacfg(intcount=cfg["stb_info"]["tag_float_count"],
+                              binarycount=(cfg["stb_info"]["tag_binary_count"], cfg["stb_info"]["tag_binary_length"]))
         # set json_files for taosBenchmark
         for i in range(len(taosBenchmark_iplist)):
-            db = jfile.setDBinfo(name=self.get_default_database(), drop="no")
-            stb = jfile.setStbinfo(name="stb", childtable_prefix="stb_" + str(i), childtable_count=100,
-                                   insert_rows=2000000, columns=col, tags=tag,timestamp_step=1000,start_timestamp="2020-01-01 00:00:00")
+            result_file_name = self.run_log_dir + '/perf_report.txt'
+            f = open(result_file_name, 'a')
+            f.write(
+                "-------- insert\t" + str(
+                    cfg["stb_info"]["childtable_count"] * cfg["stb_info"]["insert_rows"]) + " rows with\t" +
+                cfg["stb_info"]["insert_mode"] + "\tinsert mode--------\n")
+            f.close()
+            db = jfile.setDBinfo(name=cfg["db_info"]["db_name"], drop=cfg["db_info"]["drop"])
+            stb = jfile.setStbinfo(name=cfg["stb_info"]["stb_name"],
+                                   childtable_prefix=cfg["stb_info"]["childtable_prefix"] + str(i),
+                                   childtable_count=cfg["stb_info"]["childtable_count"],
+                                   insert_rows=cfg["stb_info"]["insert_rows"], columns=col, tags=tag,
+                                   timestamp_step=cfg["stb_info"]["timestamp_step"],
+                                   start_timestamp=cfg["stb_info"]["start_timestamp"],
+                                   insert_mode=cfg["stb_info"]["insert_mode"],
+                                   line_protocol=cfg["stb_info"]["line_protocol"],
+                                   tcp_transfer=cfg["stb_info"]["tcp_transfer"])
 
             database1 = jfile.setDatabases(dbinfo=db, super_tables=[stb])
-            json_info = jfile.setJsoninfo(host=taosd_list[0], databases=[database1],thread_count=16)
+            json_info = jfile.setJsoninfo(host=taosd_list[0], databases=[database1],
+                                          thread_count=cfg["json_info"]["thread_count"])
             json_info.update({"test_log": "/root/testlog/"})
             json_data.append({})
             json_data[i] = json_info
@@ -49,7 +69,7 @@ class InsertTest(TDCase):
                 self.run_log_dir, file_name[i], json_info)
 
         # put the file to target
-        Insert_file.put_file(taosBenchmark_iplist, json_data,file_name)
+        Insert_file.put_file(taosBenchmark_iplist, json_data, file_name)
         timestamp_start = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
 
         # run taosBenchmark
@@ -62,10 +82,7 @@ class InsertTest(TDCase):
         Insert_file.taosBenchmark_insert_summary_result(result_filename)
         Insert_file.taosBenchmark_id_insert_result(result_filename)
 
-
         # get node_info and process_info
         env_setting = self.get_component_by_name("prometheus")
-        Insert_file.get_process_exporter_info(env_setting, 1,timestamp_start,timestamp_end)
-        Insert_file.get_node_exporter_info(env_setting, 1,timestamp_start,timestamp_end)
-
-
+        Insert_file.get_process_exporter_info(env_setting, 1, timestamp_start, timestamp_end)
+        Insert_file.get_node_exporter_info(env_setting, 1, timestamp_start, timestamp_end)
