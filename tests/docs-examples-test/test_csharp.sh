6cca676f88b5fe5f1b799ab173f9396b2fdd6f6a
dingbo <dingbo8128@163.com>
2022-04-09 17:54:18 +0800

    test: fix csharp demo program

--- tests/docs-examples-test/test_csharp.sh
+++ tests/docs-examples-test/test_csharp.sh
@@ -2,6 +2,7 @@
 
 set -e
 
-taosd >> /dev/null 2>&1 &
-taosadapter >> /dev/null 2>&1 &
+pgrep taosd || taosd >> /dev/null 2>&1 &
+pgrep taosadapter || taosadapter >> /dev/null 2>&1 &
 cd ../../docs-examples/csharp
+
