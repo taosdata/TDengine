5480cea580d9aaa6dccb940f43932768421be198
jiajingbin <jiajaybin@126.com>
2022-04-09 20:06:36 +0800

    [test]: add create_table tool for 3.0

---
+++ cases/create_table_tool/create_table.py
@@ -0,0 +1,122 @@
+###################################################################
+#           Copyright (c) 2020 by TAOS Technologies, Inc.
+#                     All rights reserved.
+#
+#  This file is proprietary and confidential to TAOS Technologies.
+#  No part of this file may be reproduced, stored, transmitted,
+#  disclosed or used in any form or by any means other than as
+#  expressly provided by the written permission from Jianhui Tao
+#
+###################################################################
+
+# -*- coding: utf-8 -*-
+
+from taostest import TDCase, T
+from taostest.util.common import TDCom
+from taostest.util.remote import Remote
+import os
+import threading
+from taostest.util.file import read_yaml, dict2file
+from queue import Queue
+
+class CreateTable(TDCase):
+    def __init__(self):
+        self._remote: Remote = None
+        self._fqdn: str = None
+        self.case_configs = read_yaml(os.path.join(os.path.abspath(os.path.dirname(__file__)), "testcases.yaml"))
+        # self.create_table_cmd_dict = dict()
+        self.case_config_list = list()
+        self.error_msg = None
+
+    def init(self):
+        self.tdCom = TDCom(self.tdSql)
+        self._remote: Remote = Remote(self.logger)
+
+    def gen_create_table_cmd_dict(self):
+        for case, case_config in self.case_configs["testcases"].items():
+            for config in case_config:
+                for fqdn in config["fqdn"]:
+                    base_cmd = f'create_table -c {config["config_dir"]} '
+                    case_config_dict = dict()
+                    create_table_cmd_list = list()
+                    case_config_dict["fqdn"] = fqdn
+                    if "taos_config" in config:
+                        dict2file(self.run_log_dir, "taos.cfg", config["taos_config"])
+                        if "dataDir" in config["taos_config"]:
+                            self._remote.mkdir(fqdn, config["taos_config"]["dataDir"])
+                        if "logDir" in config["taos_config"]:
+                            self._remote.mkdir(fqdn, config["taos_config"]["logDir"])
+                        cfgPath = os.path.join(self.run_log_dir, "taos.cfg")
+                        self._remote.mkdir(fqdn, config["config_dir"])
+                        self._remote.put(fqdn, cfgPath, config["config_dir"])
+
+                    if "db_name" in config:
+                        base_cmd += f'-d {config["db_name"]} '
+                    if "stb_name" in config:
+                        base_cmd += f'-s {config["stb_name"]} '
+                    if "thread_count" in config:
+                        base_cmd += f'-t {config["thread_count"]} '
+                    if "table_count" in config:
+                        base_cmd += f'-n {config["table_count"]} '
+                    if "start_offset" in config:
+                        base_cmd += f'-g {config["start_offset"]} '
+                    if "vgroup_count" in config:
+                        base_cmd += f'-v {config["vgroup_count"]} '
+                    if "create_table" in config:
+                        base_cmd += f'-a {config["create_table"]} '
+                    if "insert_data" in config:
+                        base_cmd += f'-i {config["insert_data"]} '
+                    if "batch_of_tbl" in config:
+                        base_cmd += f'-b {config["batch_of_tbl"]} '
+                    if "show_tables_flag" in config:
+                        base_cmd += f'-w {config["show_tables_flag"]} '
+                    if "query_flag" in config:
+                        base_cmd += f'-q {config["query_flag"]} '
+                    if "batch_of_row" in config:
+                        base_cmd += f'-l {config["batch_of_row"]} '
+                    if "total_rows_of_per_tbl" in config:
+                        base_cmd += f'-q {config["total_rows_of_per_tbl"]} '
+                    create_table_cmd_list.append(base_cmd)
+                    case_config_dict["cmd_list"] = create_table_cmd_list
+                    case_config_dict["case"] = case
+                    self.case_config_list.append(case_config_dict)
+                    # self.create_table_cmd_dict[case] = case_config_list
+
+    def threads_run_create_table(self):
+        self.gen_create_table_cmd_dict()
+        t = list()
+        que = Queue()
+        for case_config_dict in self.case_config_list:
+            print(case_config_dict)
+            # t.extend(threading.Thread(target=self._remote.cmd, args=(case_config_dict["fqdn"], [cmd])) for cmd in case_config_dict["cmd_list"])
+            for cmd in case_config_dict["cmd_list"]:
+                # t.append(threading.Thread(target=self._remote.cmd, args=(case_config_dict["fqdn"], [cmd])))
+                t.append(threading.Thread(target=lambda q, arg1, arg2: q.put(self._remote.cmd(arg1, arg2)), args=(que, case_config_dict["fqdn"], [cmd])))
+        for i in t:
+            i.start()
+        for i in t:
+            i.join()
+        with open(f'{self.run_log_dir}/create_table.log', 'a') as f:
+            while not que.empty():
+                result = que.get()
+                f.write(str(self.case_config_list[0]) + "\n\n")
+                self.case_config_list.pop(0)
+                f.write(result.split('\n')[-1] + "\n\n")
+
+    def run(self) -> bool:
+        self.threads_run_create_table()
+
+    def cleanup(self):
+        pass
+
+    def desc(self) -> str:
+        return """
+            thread run create_table tool
+        """
+
+    def author(self) -> str:
+        return "Jayden"
+
+    def tags(self):
+        return T.Write.TaoscSql.Stable.Create
+
