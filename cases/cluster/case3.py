993f2668e4c906b570e745cdb5c093fe5acc3cf7
wenzhouwww@live.cn <wenzhouwww@live.cn>
2022-04-11 16:06:32 +0800

    update case and params

--- cases/cluster/case3.py
+++ cases/cluster/case3.py
@@ -16,13 +16,13 @@ import time
 
 from taostest import ClusterCase
 
-class Case2(ClusterCase):
+class Case3(ClusterCase):
 
     def init(self):
         super().init()
        
         self.max_restart_interval = [5, 10]
-        self.restart_times = 5
+        self.restart_times = 50
         self.master_nodes = self.get_masters()
         i = random.randint(0, len(self.master_nodes) - 1)
         self.master_node = self.master_nodes[i]
@@ -33,9 +33,11 @@ class Case2(ClusterCase):
         self.logger.info("slave: %s", self.slave_node)
         
         # set alter schema params
-        self.params = {"_ts" : 1420041600000 , "_ts_step":1 ,"_row_nums":2 ,"_col_nums":16 , "_stables_nums" : 2 , "_table_nums":2 ,
-        "tables_of_per_stable":2 ,"_tags_nums" : 10 , "_replica" :3 ,"_db_nums": 100 ,"_alter_times":100 , "_dbs":0 , "_used_dbs":[] , "_tags":0,
-        "_used_tags":[] , "_stablenames":0 ,"_used_stables":[] }
+        self.params = {"_ts" : 1420041600000 , "_ts_step":1 ,"_row_nums":2 ,"_col_nums":12 ,  "tables_of_per_stable":2 ,"_tags_nums" : 10 , "_replica" :3 }
+        self.db_nums = 100 
+        self.stable_nums = 100
+        self.table_nums = 100
+        self.time_sleep = 0
 
     def cleanup(self):
         pass
@@ -43,7 +45,7 @@ class Case2(ClusterCase):
     def run(self):
         # start alter schema task (self, db_nums , stable_nums,table_nums , time_sleep)
 
-        taskthread = threading.Thread(target=self.basic_alter_shema_task,args=( 2 ,  2 ,2 , 0, self.params ))
+        taskthread = threading.Thread(target=self.basic_alter_shema_task,args=( self.db_nums ,  self.stable_nums ,self.db_nums , self.time_sleep, self.params ))
         taskthread.start()
         # wait thread
         
@@ -56,7 +58,6 @@ class Case2(ClusterCase):
         dthread.join()
        
         
-        return self._status
 
     def cleanup(self):
         pass
