27202c292ff1cfa007114e254f8006f686781655
afwerar <1296468573@qq.com>
2022-04-11 19:17:18 +0800

    [TD-13756]<fix>: test

--- src/os/inc/os.h
+++ src/os/inc/os.h
@@ -11,7 +11,7 @@
  *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
+ */ 
 
 #ifndef TDENGINE_OS_H
 #define TDENGINE_OS_H
