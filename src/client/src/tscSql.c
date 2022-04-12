165b9cf516982ef959868f81a5ca7cbd280a5765
Minglei Jin <mljin@taosdata.com>
2022-04-12 15:39:49 +0800

    fix(shell): print taos_connect failed reason befor exiting

--- src/client/src/tscSql.c
+++ src/client/src/tscSql.c
@@ -224,6 +224,8 @@ TAOS *taos_connect_internal(const char *ip, const char *user, const char *pass, 
     }
   }
 
+  printf("connect failed, reason: %s\n\n", taos_errstr(pSql));
+
   return NULL;
 }
 
