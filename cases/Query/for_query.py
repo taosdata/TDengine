133a2847c7846686876977df4f4b1b303686f0b3
“happyguoxy” <happy_guoxy@163.com>
2022-04-13 17:06:13 +0800

    Query

---
+++ cases/Query/for_query.py
@@ -0,0 +1,33 @@
+###################################################################
+#           Copyright (c) 2016 by TAOS Technologies, Inc.
+#                     All rights reserved.
+#
+#  This file is proprietary and confidential to TAOS Technologies.
+#  No part of this file may be reproduced, stored, transmitted,
+#  disclosed or used in any form or by any means other than as
+#  expressly provided by the written permission from Jianhui Tao
+#
+###################################################################
+
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import time
+import subprocess
+
+class TDFor:
+
+    def run(self):
+
+        n = 1
+        while n < 1000:            
+            print("\n========第 %d 次 程序运行==================\n" % (n))     
+            n += 1
+            
+            subprocess.getoutput("sysctl -p /etc/sysctl.conf")
+            os.system("./query_all.sh  >/root/log/query_all.txt")
+                      
+
+TDFor().run()
+
