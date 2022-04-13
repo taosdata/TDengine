d2fd499b72f2f638f5daef1bdee48c852ffbbb14
xleili <xlli@taosdata.com>
2022-04-13 16:59:17 +0800

    docs:docs-cn-example csharp fix example code && fix reference ident

--- docs-examples/csharp/QueryExample.cs
+++ docs-examples/csharp/QueryExample.cs
@@ -5,13 +5,14 @@ namespace TDengineExample
 {
     internal class QueryExample
     {
-        static void Main() { 
+        static void Main()
+        {
             IntPtr conn = GetConnection();
             // run query
-            IntPtr res = TDengine.Query(conn, "SELECT * FROM meters LIMIT 2");
+            IntPtr res = TDengine.Query(conn, "SELECT * FROM test.meters LIMIT 2");
             if (TDengine.ErrorNo(res) != 0)
             {
-                Console.WriteLine("Failed to query since: " + TDengine.Error(res));  
+                Console.WriteLine("Failed to query since: " + TDengine.Error(res));
                 TDengine.Close(conn);
                 TDengine.Cleanup();
                 return;
@@ -19,7 +20,7 @@ namespace TDengineExample
 
             // get filed count
             int fieldCount = TDengine.FieldCount(res);
-            Console.WriteLine("fieldCount=" + fieldCount); 
+            Console.WriteLine("fieldCount=" + fieldCount);
 
             // print column names
             List<TDengineMeta> metas = TDengine.FetchFields(res);
@@ -31,65 +32,100 @@ namespace TDengineExample
 
             // print values
             IntPtr row;
-            while ((row = TDengine.FetchRows(res)) != IntPtr.Zero) {
-                for (int i = 0; i < fieldCount; i++) { 
-                    var meta = metas[i];
-                    int offset = IntPtr.Size * i;
-                    IntPtr data = Marshal.ReadIntPtr(row, offset);
+            while ((row = TDengine.FetchRows(res)) != IntPtr.Zero)
+            {
+                List<TDengineMeta> metaList = TDengine.FetchFields(res);
+                int numOfFiled = TDengine.FieldCount(res);
+
+                List<String> dataRaw = new List<string>();
+
+                IntPtr colLengthPrt = TDengine.FetchLengths(res);
+                int[] colLengthArr = new int[numOfFiled];
+                Marshal.Copy(colLengthPrt, colLengthArr, 0, numOfFiled);
+
+                for (int i = 0; i < numOfFiled; i++)
+                {
+                    TDengineMeta meta = metaList[i];
+                    IntPtr data = Marshal.ReadIntPtr(row, IntPtr.Size * i);
+
                     if (data == IntPtr.Zero)
                     {
-                        Console.Write("NULL\t");
+                         Console.Write("NULL\t");
                         continue;
                     }
                     switch ((TDengineDataType)meta.type)
                     {
                         case TDengineDataType.TSDB_DATA_TYPE_BOOL:
                             bool v1 = Marshal.ReadByte(data) == 0 ? false : true;
-                            Console.Write(v1 + "\t"); 
+                             Console.Write(v1.ToString()+"\t");
                             break;
                         case TDengineDataType.TSDB_DATA_TYPE_TINYINT:
-                            byte v2 = Marshal.ReadByte(data);
-                            Console.Write(v2 + "\t");
+                            sbyte v2 = (sbyte)Marshal.ReadByte(data);
+                             Console.Write(v2.ToString()+"\t");
                             break;
                         case TDengineDataType.TSDB_DATA_TYPE_SMALLINT:
                             short v3 = Marshal.ReadInt16(data);
-                            Console.Write(v3 + "\t");
+                             Console.Write(v3.ToString()+"\t");
                             break;
                         case TDengineDataType.TSDB_DATA_TYPE_INT:
                             int v4 = Marshal.ReadInt32(data);
-                            Console.Write(v4 + "\t");
+                             Console.Write(v4.ToString()+"\t");
                             break;
                         case TDengineDataType.TSDB_DATA_TYPE_BIGINT:
                             long v5 = Marshal.ReadInt64(data);
-                            Console.Write(v5 + "\t");
+                             Console.Write(v5.ToString()+"\t");
                             break;
                         case TDengineDataType.TSDB_DATA_TYPE_FLOAT:
                             float v6 = (float)Marshal.PtrToStructure(data, typeof(float));
-                            Console.Write(v6 + "\t");
+                             Console.Write(v6.ToString()+"\t");
                             break;
                         case TDengineDataType.TSDB_DATA_TYPE_DOUBLE:
                             double v7 = (double)Marshal.PtrToStructure(data, typeof(double));
-                            Console.WriteLine(v7 + "\t");
+                             Console.Write(v7.ToString()+"\t");
                             break;
                         case TDengineDataType.TSDB_DATA_TYPE_BINARY:
-                            string v8 = Marshal.PtrToStringAnsi(data);
-                            Console.Write(v8 + "\t");
+                            string v8 = Marshal.PtrToStringUTF8(data, colLengthArr[i]);
+                             Console.Write(v8+"\t");
                             break;
                         case TDengineDataType.TSDB_DATA_TYPE_TIMESTAMP:
                             long v9 = Marshal.ReadInt64(data);
-                            Console.Write(v9 + "\t");
+                             Console.Write(v9.ToString()+"\t");
                             break;
                         case TDengineDataType.TSDB_DATA_TYPE_NCHAR:
-                            string v10 = Marshal.PtrToStringAnsi(data);
-                            Console.Write(v10 + "\t");
+                            string v10 = Marshal.PtrToStringUTF8(data, colLengthArr[i]);
+                             Console.Write(v10+"\t");
+                            break;
+                        case TDengineDataType.TSDB_DATA_TYPE_UTINYINT:
+                            byte v12 = Marshal.ReadByte(data);
+                             Console.Write(v12.ToString()+"\t");
+                            break;
+                        case TDengineDataType.TSDB_DATA_TYPE_USMALLINT:
+                            ushort v13 = (ushort)Marshal.ReadInt16(data);
+                             Console.Write(v13.ToString()+"\t");
+                            break;
+                        case TDengineDataType.TSDB_DATA_TYPE_UINT:
+                            uint v14 = (uint)Marshal.ReadInt32(data);
+                             Console.Write(v14.ToString()+"\t");
+                            break;
+                        case TDengineDataType.TSDB_DATA_TYPE_UBIGINT:
+                            ulong v15 = (ulong)Marshal.ReadInt64(data);
+                             Console.Write(v15.ToString()+"\t");
+                            break;
+                        case TDengineDataType.TSDB_DATA_TYPE_JSONTAG:
+                            string v16 = Marshal.PtrToStringUTF8(data, colLengthArr[i]);
+                             Console.Write(v16+"\t");
+                            break;
+                        default:
+                             Console.Write("nonsupport data type value");
                             break;
                     }
+
                 }
                 Console.WriteLine();
             }
             if (TDengine.ErrorNo(res) != 0)
             {
-                Console.WriteLine($"Query is not complete, Error { TDengine.ErrorNo(res)} {TDengine.Error(res)}");
+                Console.WriteLine($"Query is not complete, Error {TDengine.ErrorNo(res)} {TDengine.Error(res)}");
             }
             // exit
             TDengine.FreeResult(res);
