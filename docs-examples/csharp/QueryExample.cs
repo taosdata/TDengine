9f8ba053501c8738a47e6d5394ccc22232575954
Shuduo Sang <sangshuduo@gmail.com>
2022-04-13 10:14:22 +0000

    use 'dotnet format' to correct coding style

--- docs-examples/csharp/QueryExample.cs
+++ docs-examples/csharp/QueryExample.cs
@@ -50,73 +50,73 @@ namespace TDengineExample
 
                     if (data == IntPtr.Zero)
                     {
-                         Console.Write("NULL\t");
+                        Console.Write("NULL\t");
                         continue;
                     }
                     switch ((TDengineDataType)meta.type)
                     {
                         case TDengineDataType.TSDB_DATA_TYPE_BOOL:
                             bool v1 = Marshal.ReadByte(data) == 0 ? false : true;
-                             Console.Write(v1.ToString()+"\t");
+                            Console.Write(v1.ToString() + "\t");
                             break;
                         case TDengineDataType.TSDB_DATA_TYPE_TINYINT:
                             sbyte v2 = (sbyte)Marshal.ReadByte(data);
-                             Console.Write(v2.ToString()+"\t");
+                            Console.Write(v2.ToString() + "\t");
                             break;
                         case TDengineDataType.TSDB_DATA_TYPE_SMALLINT:
                             short v3 = Marshal.ReadInt16(data);
-                             Console.Write(v3.ToString()+"\t");
+                            Console.Write(v3.ToString() + "\t");
                             break;
                         case TDengineDataType.TSDB_DATA_TYPE_INT:
                             int v4 = Marshal.ReadInt32(data);
-                             Console.Write(v4.ToString()+"\t");
+                            Console.Write(v4.ToString() + "\t");
                             break;
                         case TDengineDataType.TSDB_DATA_TYPE_BIGINT:
                             long v5 = Marshal.ReadInt64(data);
-                             Console.Write(v5.ToString()+"\t");
+                            Console.Write(v5.ToString() + "\t");
                             break;
                         case TDengineDataType.TSDB_DATA_TYPE_FLOAT:
                             float v6 = (float)Marshal.PtrToStructure(data, typeof(float));
-                             Console.Write(v6.ToString()+"\t");
+                            Console.Write(v6.ToString() + "\t");
                             break;
                         case TDengineDataType.TSDB_DATA_TYPE_DOUBLE:
                             double v7 = (double)Marshal.PtrToStructure(data, typeof(double));
-                             Console.Write(v7.ToString()+"\t");
+                            Console.Write(v7.ToString() + "\t");
                             break;
                         case TDengineDataType.TSDB_DATA_TYPE_BINARY:
                             string v8 = Marshal.PtrToStringUTF8(data, colLengthArr[i]);
-                             Console.Write(v8+"\t");
+                            Console.Write(v8 + "\t");
                             break;
                         case TDengineDataType.TSDB_DATA_TYPE_TIMESTAMP:
                             long v9 = Marshal.ReadInt64(data);
-                             Console.Write(v9.ToString()+"\t");
+                            Console.Write(v9.ToString() + "\t");
                             break;
                         case TDengineDataType.TSDB_DATA_TYPE_NCHAR:
                             string v10 = Marshal.PtrToStringUTF8(data, colLengthArr[i]);
-                             Console.Write(v10+"\t");
+                            Console.Write(v10 + "\t");
                             break;
                         case TDengineDataType.TSDB_DATA_TYPE_UTINYINT:
                             byte v12 = Marshal.ReadByte(data);
-                             Console.Write(v12.ToString()+"\t");
+                            Console.Write(v12.ToString() + "\t");
                             break;
                         case TDengineDataType.TSDB_DATA_TYPE_USMALLINT:
                             ushort v13 = (ushort)Marshal.ReadInt16(data);
-                             Console.Write(v13.ToString()+"\t");
+                            Console.Write(v13.ToString() + "\t");
                             break;
                         case TDengineDataType.TSDB_DATA_TYPE_UINT:
                             uint v14 = (uint)Marshal.ReadInt32(data);
-                             Console.Write(v14.ToString()+"\t");
+                            Console.Write(v14.ToString() + "\t");
                             break;
                         case TDengineDataType.TSDB_DATA_TYPE_UBIGINT:
                             ulong v15 = (ulong)Marshal.ReadInt64(data);
-                             Console.Write(v15.ToString()+"\t");
+                            Console.Write(v15.ToString() + "\t");
                             break;
                         case TDengineDataType.TSDB_DATA_TYPE_JSONTAG:
                             string v16 = Marshal.PtrToStringUTF8(data, colLengthArr[i]);
-                             Console.Write(v16+"\t");
+                            Console.Write(v16 + "\t");
                             break;
                         default:
-                             Console.Write("nonsupport data type value");
+                            Console.Write("nonsupport data type value");
                             break;
                     }
 
