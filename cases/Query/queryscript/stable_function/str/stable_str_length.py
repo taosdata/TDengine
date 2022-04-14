d7b3c3a4e8b60d0bccd6b7cb2f59272565aa0cc2
“happyguoxy” <happy_guoxy@163.com>
2022-04-14 14:40:57 +0800

    Query

--- cases/Query/queryscript/stable_function/str/stable_str_length.py
+++ cases/Query/queryscript/stable_function/str/stable_str_length.py
@@ -45,7 +45,7 @@ class TDTestQuery(TDCase):
         return case_description
 
     #basic_param
-    db = "stable_fun_str6"
+    db = "stable_length"
     table_list = ['stable_1','stable_2',]
     table = str(random.sample(table_list,1)).replace("[","").replace("]","").replace("'","")
     table_null_list = ['stable_null_data','stable_null_childtable']
