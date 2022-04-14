d5c118307f143e8bfc770982336c751b6c83bd93
“happyguoxy” <happy_guoxy@163.com>
2022-04-14 14:40:57 +0800

    Query

--- cases/Query/queryscript/stable_function/str/stable_str_cast.py
+++ cases/Query/queryscript/stable_function/str/stable_str_cast.py
@@ -46,7 +46,7 @@ class TDTestQuery(TDCase):
         return case_description
 
     #basic_param
-    db = "stable_fun_str9"
+    db = "stable_cast"
     table_list = ['stable_1','stable_2',]
     table = str(random.sample(table_list,1)).replace("[","").replace("]","").replace("'","")
     table_null_list = ['stable_null_data','stable_null_childtable']
