6484aad96242ae6882bc0253db1be5891c38c24a
dingbo <dingbo8128@163.com>
2022-04-14 19:17:08 +0800

    test: fix test_R.sh

--- tests/docs-examples-test/test_R.sh
+++ tests/docs-examples-test/test_R.sh
@@ -7,8 +7,8 @@ pgrep taosadapter || taosadapter >> /dev/null 2>&1 &
 
 cd ../../docs-examples/R
 
-jar_path=`find /home/debug/build -name taos-jdbcdriver-*-dist.jar`
-
+jar_path=`find ../../../debug/build  -name taos-jdbcdriver-*-dist.jar`
+echo jar_path=$jar_path
 R -f connect_native.r --args $jar_path
 # R -f connect_rest.r --args $jar_path # bug 14704
 
