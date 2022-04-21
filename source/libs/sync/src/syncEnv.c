672be6f4ff9125e32b0f051bef07699cbd59af9b
Minghao Li <castermode@gmail.com>
2022-04-21 11:35:22 +0800

    del trace log flag

--- source/libs/sync/src/syncEnv.c
+++ source/libs/sync/src/syncEnv.c
@@ -27,8 +27,6 @@ static void      syncEnvTick(void *param, void *tmrId);
 // --------------------------------
 
 int32_t syncEnvStart() {
-  sDebugFlag = 207;
-
   int32_t ret = 0;
   taosSeedRand(taosGetTimestampSec());
   gSyncEnv = doSyncEnvStart(gSyncEnv);
