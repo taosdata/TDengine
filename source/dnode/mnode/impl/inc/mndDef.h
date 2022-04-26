eedd73323057d139268d5d86770177bc1c7f6fe2
Shengliang Guan <slguan@taosdata.com>
2022-04-26 14:14:28 +0800

    enh: add cb function for transaction

--- source/dnode/mnode/impl/inc/mndDef.h
+++ source/dnode/mnode/impl/inc/mndDef.h
@@ -126,6 +126,8 @@ typedef enum {
   DND_REASON_OTHERS
 } EDndReason;
 
+typedef void (*TransCbFp)(SMnode* pMnode, void* param);
+
 typedef struct {
   int32_t    id;
   ETrnStage  stage;
@@ -148,6 +150,8 @@ typedef struct {
   int64_t    dbUid;
   char       dbname[TSDB_DB_FNAME_LEN];
   char       lastError[TSDB_TRANS_ERROR_LEN];
+  TransCbFp  transCbFp;
+  void*      transCbParam;
 } STrans;
 
 typedef struct {
