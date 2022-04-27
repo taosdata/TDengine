228d36e623921f6a87e659e40168089ac59e1175
Shuduo Sang <sangshuduo@gmail.com>
2022-04-27 11:58:55 +0800

    fix: packing script for connector (#11919)
    [TD-15158]

--- packaging/tools/install.sh
+++ packaging/tools/install.sh
@@ -805,6 +805,11 @@ function updateProduct() {
   install_log
   install_header
   install_lib
+
+  if [ "$verMode" == "cluster" ]; then
+      install_connector
+  fi
+
   install_examples
   if [ -z $1 ]; then
     install_bin
@@ -885,9 +890,9 @@ function installProduct() {
   #install_avro lib
   #install_avro lib64
 
-  #  if [ "$pagMode" != "lite" ]; then
-  #    install_connector
-  #  fi
+  if [ "$verMode" == "cluster" ]; then
+      install_connector
+  fi
   install_examples
 
   if [ -z $1 ]; then # install service and client
