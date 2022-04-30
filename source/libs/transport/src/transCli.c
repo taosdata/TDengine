aa1eace9020728055742d6e4e7ded1b4eab2c1fc
yihaoDeng <luomoxyz@126.com>
2022-04-30 12:14:27 +0800

    fix(rpc):fix rpc fix

--- source/libs/transport/src/transCli.c
+++ source/libs/transport/src/transCli.c
@@ -937,7 +937,7 @@ int cliAppCb(SCliConn* pConn, STransMsg* pResp, SCliMsg* pMsg) {
         tDeserializeSMEpSet(pResp->pCont, pResp->contLen, &emsg);
         pCtx->epSet = emsg.epSet;
       }
-      addConnToPool(pThrd, pConn);
+      addConnToPool(pThrd->pool, pConn);
       tTrace("use remote epset, current in use: %d, retry count%d, try limit: %d", pEpSet->inUse, pCtx->retryCount + 1,
              TRANS_RETRY_COUNT_LIMIT);
 
