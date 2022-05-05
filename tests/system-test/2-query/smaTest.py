154498ddb5068a8383478caeb1387f9e2c6622de
tomchon <haoran920c@163.com>
2022-05-05 22:17:25 +0800

    test: add sma profermance test

--- tests/system-test/2-query/smaTest.py
+++ tests/system-test/2-query/smaTest.py
@@ -27,7 +27,9 @@ class TDTestCase:
     #
     # --------------- main frame -------------------
     #
-    
+    # updatecfgDict = {'debugFlag': 135}
+    # updatecfgDict = {'fqdn': 135}
+
     def caseDescription(self):
         '''
         limit and offset keyword function test cases;
@@ -48,7 +50,7 @@ class TDTestCase:
     # run case  
     def run(self):
         # insert data
-        self.insert_data("t1", self.ts, 1000*10000)
+        self.insert_data1("t1", self.ts, 1000*10000)
         self.insert_data1("t4", self.ts, 1000*10000)        
         # test base case
         # self.test_case1()
@@ -70,11 +72,9 @@ class TDTestCase:
     # create table
     def create_tables(self):
         # super table
-        tdSql.execute("create table st(ts timestamp, i1 int) tags(area int)");
+        tdSql.execute("create table st(ts timestamp, i1 int,i2 int) tags(area int)");
         # child table
         tdSql.execute("create table t1 using st tags(1)");
-        tdSql.execute("create table t2 using st tags(2)");
-        tdSql.execute("create table t3 using st tags(3)");
 
         tdSql.execute("create table st1(ts timestamp, i1 int ,i2 int) tags(area int) sma(i2) ");
         tdSql.execute("create table t4 using st1 tags(1)");
@@ -122,66 +122,7 @@ class TDTestCase:
     #     # base no where
     #     sql = "select * from t1 limit 10"
     #     tdSql.waitedQuery(sql, 10, WAITS)
-    #     tdSql.checkData(0, 1, 0)
-    #     tdSql.checkData(9, 1, 9)
 
-    #     # have where
-    #     sql = "select * from t1 where ts>='2017-07-14 10:40:01' and ts<'2017-07-14 10:40:06' limit 10"
-    #     tdSql.waitedQuery(sql, 5, WAITS)
-    #     tdSql.checkData(0, 1, 1)
-    #     tdSql.checkData(4, 1, 5)
-
-    #     # 
-    #     # offset base function
-    #     #
-    #     # no where
-    #     sql = "select * from t1 limit 10 offset 5"
-    #     tdSql.waitedQuery(sql, 10, WAITS)
-    #     tdSql.checkData(0, 1, 5)
-    #     tdSql.checkData(9, 1, 14)
-
-    #     # have where only ts
-    #     sql = "select * from t1 where ts>='2017-07-14 10:40:10' and ts<'2017-07-14 10:40:20' limit 10 offset 5"
-    #     tdSql.waitedQuery(sql, 5, WAITS)
-    #     tdSql.checkData(0, 1, 15)
-    #     tdSql.checkData(4, 1, 19)
-
-    #     # have where with other column condition
-    #     sql = "select * from t1 where i1>=1 and i1<11 limit 10 offset 5"
-    #     tdSql.waitedQuery(sql, 5, WAITS)
-    #     tdSql.checkData(0, 1, 6)
-    #     tdSql.checkData(4, 1, 10)
-
-    #     # have where with ts and other column condition
-    #     sql = "select * from t1 where ts>='2017-07-14 10:40:10' and ts<'2017-07-14 10:40:50' and i1>=20 and i1<=25 limit 10 offset 5"
-    #     tdSql.waitedQuery(sql, 1, WAITS)
-    #     tdSql.checkData(0, 1, 25)
-
-    #     return 
-
-    # # test advance 
-    # def test_case2(self):
-    #     # 
-    #     # OFFSET merge file data with memory data
-    #     #
-        
-    #     # offset
-    #     sql = "select * from t1 limit 10 offset 72000"
-    #     tdSql.waitedQuery(sql, 10, WAITS)
-    #     tdSql.checkData(0, 1, 72000)
-
-    #     # each insert one row into NO.0 NO.2 NO.7 blocks
-    #     sql = "insert into t1 values (%d, 0) (%d, 2) (%d, 7)"%(self.ts+1, self.ts + 2*3300*1000+1, self.ts + 7*3300*1000+1)
-    #     tdSql.execute(sql)
-    #     # query result
-    #     sql = "select * from t1 limit 10 offset 72000"
-    #     tdSql.waitedQuery(sql, 10, WAITS)
-    #     tdSql.checkData(0, 1, 72000 - 3)
-        
-    #     # have where
-    #     sql = "select * from t1 where ts>='2017-07-14 10:40:10' and ts<'2017-07-22 18:40:10' limit 10 offset 72000"
-    #     tdSql.waitedQuery(sql, 10, WAITS)
-    #     tdSql.checkData(0, 1, 72000 - 3 + 10 + 1)
 
 #
 # add case with filename
