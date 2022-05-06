f4f4bccdbb29c9c7bca06b57727acaa90a7ccdd5
cpwu <cpwu@taosdata.com>
2022-05-06 15:45:16 +0800

    fix case

--- tests/system-test/2-query/join.py
+++ tests/system-test/2-query/join.py
@@ -102,7 +102,7 @@ class TDTestCase:
                 if len(tblist) == 2:
                     if "ct1" in tblist or "t1" in tblist:
                         self.__join_current(sql, checkrows)
-                    elif where_condition or "null" in groups:
+                    elif where_condition or "not null" in groups:
                         self.__join_current(sql, checkrows + 2 )
                     else:
                         self.__join_current(sql, checkrows + 5 )
