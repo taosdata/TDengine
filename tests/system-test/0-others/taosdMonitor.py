4051dadaf185289371c9b4bf7fb74e3319d5e1f0
wenzhouwww@live.cn <wenzhouwww@live.cn>
2022-05-08 12:35:51 +0800

    fix bug for disk_info data_dir

--- tests/system-test/0-others/taosdMonitor.py
+++ tests/system-test/0-others/taosdMonitor.py
@@ -89,13 +89,13 @@ def telemetryInfoCheck(infoDict=''):
     vgroup_infos_nums = len(infoDict["vgroup_infos"])
 
     for  index in range(vgroup_infos_nums):
-        if "vgroup_id" not in infoDict["vgroup_infos"][index] or infoDict["vgroup_infos"][index]["vgroup_id"]< 0:
+        if "vgroup_id" not in infoDict["vgroup_infos"][index] or infoDict["vgroup_infos"][index]["vgroup_id"]<0:
             tdLog.exit("vgroup_id is null!")
         if "database_name" not in infoDict["vgroup_infos"][index] or len(infoDict["vgroup_infos"][index]["database_name"]) < 0:
             tdLog.exit("database_name is null!")
         if "tables_num" not in infoDict["vgroup_infos"][index] or infoDict["vgroup_infos"][index]["tables_num"]!= 0:
             tdLog.exit("tables_num is null!")
-        if "status" not in infoDict["vgroup_infos"][index] or len(infoDict["vgroup_infos"][index]["status"]) > 0 :
+        if "status" not in infoDict["vgroup_infos"][index] or len(infoDict["vgroup_infos"][index]["status"]) < 0 :
             tdLog.exit("status is null!")
         if "vnodes" not in infoDict["vgroup_infos"][index] or infoDict["vgroup_infos"][index]["vnodes"] ==None :
             tdLog.exit("vnodes is null!")
@@ -141,7 +141,7 @@ def telemetryInfoCheck(infoDict=''):
     if "name" not in infoDict["disk_infos"]["datadir"][0] or len(infoDict["disk_infos"]["datadir"][0]["name"]) <= 0:
         tdLog.exit("name is null!")
 
-    if "level" not in infoDict["disk_infos"]["datadir"][0] or infoDict["disk_infos"]["datadir"][0]["level"] <= 0:
+    if "level" not in infoDict["disk_infos"]["datadir"][0] or infoDict["disk_infos"]["datadir"][0]["level"] < 0:
         tdLog.exit("level is null!")
 
     if "avail" not in infoDict["disk_infos"]["datadir"][0] or infoDict["disk_infos"]["datadir"][0]["avail"] <= 0:
