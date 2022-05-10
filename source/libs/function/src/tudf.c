adceacd59b6bd571b70a080ba1e91d0ba99c874f
slzhou <slzhou@taosdata.com>
2022-05-10 20:07:46 +0800

    feature(udf):start udfd can be disabled

--- source/libs/function/src/tudf.c
+++ source/libs/function/src/tudf.c
@@ -18,6 +18,7 @@
 #include "tudf.h"
 #include "tudfInt.h"
 #include "tarray.h"
+#include "tglobal.h"
 #include "tdatablock.h"
 #include "querynodes.h"
 #include "builtinsimpl.h"
@@ -138,6 +139,9 @@ static void udfWatchUdfd(void *args) {
 }
 
 int32_t udfStartUdfd(int32_t startDnodeId) {
+  if (!tsStartUdfd) {
+    fnInfo("start udfd is disabled.")
+  }
   SUdfdData *pData = &udfdGlobal;
   if (pData->startCalled) {
     fnInfo("dnode-mgmt start udfd already called");
