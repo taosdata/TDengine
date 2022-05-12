820156562052dd32e70607638b5643c6fa04bb2f
cpwu <cpwu@taosdata.com>
2022-05-12 18:59:13 +0800

    fix case

--- tests/system-test/0-others/user_control.py
+++ tests/system-test/0-others/user_control.py
@@ -136,10 +136,10 @@ class TDTestCase:
         for count, i in enumerate(range(self.users_count)):
             if self.__init_pass:
                 tdSql.query(self.__alter_pass_sql(self.__user_list[i], f"new{self.__passwd_list[i]}"))
-                self.__init_pass = count != self.user_count - 1
+                self.__init_pass = count != self.users_count - 1
             else:
                 tdSql.query(self.__alter_pass_sql(self.__user_list[i], self.__passwd_list[i] ) )
-                self.__init_pass = count == self.user_count - 1
+                self.__init_pass = count == self.users_count - 1
 
     def alter_pass_err(self):  # sourcery skip: remove-redundant-fstring
         sqls = [
