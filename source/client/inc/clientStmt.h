0815843e94baf90150b749841087db2a173a9507
afwerar <1296468573@qq.com>
2022-05-13 21:42:16 +0800

    fix(os): make taosd.exe taos.exe run on windows.

--- source/client/inc/clientStmt.h
+++ source/client/inc/clientStmt.h
@@ -63,7 +63,7 @@ typedef struct SStmtBindInfo {
   int8_t       tbType;
   bool         tagsCached;
   void*        boundTags;  
-  char         tbName[TSDB_TABLE_FNAME_LEN];;
+  char         tbName[TSDB_TABLE_FNAME_LEN];
   char         tbFName[TSDB_TABLE_FNAME_LEN];
   char         stbFName[TSDB_TABLE_FNAME_LEN];
   SName        sname;
