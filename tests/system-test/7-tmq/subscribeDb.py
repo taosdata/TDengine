d4bafa1225548b616a8449d71740e2b298f63671
plum-lihui <huili@taosdata.com>
2022-05-16 22:01:41 +0800

    test: modify case

--- tests/system-test/7-tmq/subscribeDb.py
+++ tests/system-test/7-tmq/subscribeDb.py
@@ -617,7 +617,7 @@ class TDTestCase:
         tdLog.info("step 1: create database, stb, ctb and insert data")
         # create and start thread
         parameterDict = {'cfg':        '',       \
-                         'dbName':     'db60',    \
+                         'dbName':     'db70',    \
                          'vgroups':    4,        \
                          'stbName':    'stb',    \
                          'ctbNum':     10,       \
@@ -634,7 +634,7 @@ class TDTestCase:
         prepareEnvThread.start()
 
         parameterDict2 = {'cfg':        '',       \
-                         'dbName':     'db61',    \
+                         'dbName':     'db71',    \
                          'vgroups':    4,        \
                          'stbName':    'stb2',    \
                          'ctbNum':     10,       \
