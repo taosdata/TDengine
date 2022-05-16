959fd4336ae5e8e8be2452eedd1de523292749d9
slzhou <slzhou@taosdata.com>
2022-05-16 21:32:52 +0800

    fix: revert the change of qworkder dump flag

--- source/libs/qworker/src/qworker.c
+++ source/libs/qworker/src/qworker.c
@@ -9,7 +9,7 @@
 #include "tmsg.h"
 #include "tname.h"
 
-SQWDebug gQWDebug = {.statusEnable = true, .dumpEnable = false};
+SQWDebug gQWDebug = {.statusEnable = true, .dumpEnable = true};
 SQWorkerMgmt gQwMgmt = {
   .lock  = 0,
   .qwRef = -1, 
