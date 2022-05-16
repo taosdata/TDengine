43a8a55a5bf3dfac00ed3adf1c7ca1d36df2e154
cpwu <cpwu@taosdata.com>
2022-05-16 18:46:48 +0800

    fix case

--- tests/system-test/2-query/union.py
+++ tests/system-test/2-query/union.py
@@ -137,7 +137,7 @@ class TDTestCase:
         return f" group by {col} having {having}" if having else f" group by {col} "
 
     def __single_sql(self, select_clause, from_clause, where_condition="", group_condition=""):
-        if "on" not in from_clause and select_clause.split(".")[0] != from_clause.split(".")[0]:
+        if isinstance(select_clause, str) and "on" not in from_clause and select_clause.split(".")[0] != from_clause.split(".")[0]:
             return
         return f"select {select_clause} from {from_clause} {where_condition} {group_condition}"
 
