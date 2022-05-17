0ac54e6e9bd82570e2272d485351113cf3e1f3b7
cpwu <cpwu@taosdata.com>
2022-05-17 18:17:46 +0800

    fix case

--- tests/system-test/fulltest.sh
+++ tests/system-test/fulltest.sh
@@ -22,7 +22,7 @@ python3 ./test.py -f 2-query/upper.py
 python3 ./test.py -f 2-query/lower.py
 python3 ./test.py -f 2-query/join.py
 python3 ./test.py -f 2-query/cast.py
-python3 ./test.py -f 2-query/concat.py  # after wal ,crash occured
+python3 ./test.py -f 2-query/concat.py
 python3 ./test.py -f 2-query/concat_ws.py
 # python3 ./test.py -f 2-query/union.py
 # python3 ./test.py -f 2-query/union2.py
