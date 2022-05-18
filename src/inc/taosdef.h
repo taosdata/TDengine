cb461b4283329d8563930c4d321703c631614c9d
Alex Duan <417921451@qq.com>
2022-05-18 10:36:50 +0800

    feat(tsdb): column name length modify from 65 to 129 [TS-1465]

--- src/inc/taosdef.h
+++ src/inc/taosdef.h
@@ -202,7 +202,7 @@ do { \
 #define TSDB_FUNC_BUF_SIZE        512
 #define TSDB_TYPE_STR_MAX_LEN     32
 #define TSDB_TABLE_FNAME_LEN      (TSDB_ACCT_ID_LEN + TSDB_DB_NAME_LEN + TSDB_TABLE_NAME_LEN)
-#define TSDB_COL_NAME_LEN         65
+#define TSDB_COL_NAME_LEN         129  // modify from 65 to 129 by alex duan
 #define TSDB_MAX_SAVED_SQL_LEN    TSDB_MAX_COLUMNS * 64
 #define TSDB_MAX_SQL_LEN          TSDB_PAYLOAD_SIZE
 #define TSDB_MAX_SQL_SHOW_LEN     512
