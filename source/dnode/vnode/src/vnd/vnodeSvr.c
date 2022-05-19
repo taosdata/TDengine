8b067c6c3e1d62db3616c565f4061e699b83888f
Liu Jicong <liujicong@qq.com>
2022-05-19 18:00:18 +0800

    fix(stream): fix bad merge

--- source/dnode/vnode/src/vnd/vnodeSvr.c
+++ source/dnode/vnode/src/vnd/vnodeSvr.c
@@ -118,11 +118,6 @@ int vnodeProcessWriteReq(SVnode *pVnode, SRpcMsg *pMsg, int64_t version, SRpcMsg
       break;
   }
 
-  if (tqPushMsg(pVnode->pTq, pMsg->pCont, pMsg->contLen, pMsg->msgType, version) < 0) {
-    vError("vgId:%d failed to push msg to TQ since %s", TD_VID(pVnode), tstrerror(terrno));
-    return -1;
-  }
-
   vDebug("vgId:%d process %s request success, version: %" PRId64, TD_VID(pVnode), TMSG_INFO(pMsg->msgType), version);
 
   if (tqPushMsg(pVnode->pTq, pMsg->pCont, pMsg->contLen, pMsg->msgType, version) < 0) {
