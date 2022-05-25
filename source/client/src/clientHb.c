f6fb34269fc2a92bc3f73dc759c6943c2ff72514
afwerar <1296468573@qq.com>
2022-05-25 19:29:34 +0800

    fix(os): run case on win

--- source/client/src/clientHb.c
+++ source/client/src/clientHb.c
@@ -588,7 +588,9 @@ void hbThreadFuncUnexpectedStopped(void) {
 
 static void *hbThreadFunc(void *param) {
   setThreadName("hb");
+#ifdef WINDOWS
   atexit(hbThreadFuncUnexpectedStopped);
+#endif
   while (1) {
     int8_t threadStop = atomic_val_compare_exchange_8(&clientHbMgr.threadStop, 1, 2);
     if (1 == threadStop) {
