6361e87755ccd14b65b8aedfc4575aaa91989a32
jiacy-jcy <714897623@qq.com>
2022-05-26 11:38:46 +0800

    add case to_iso8601 to ci

--- tests/system-test/fulltest.sh
+++ tests/system-test/fulltest.sh
@@ -40,7 +40,8 @@ python3 ./test.py -f 2-query/max.py
 python3 ./test.py -f 2-query/min.py
 python3 ./test.py -f 2-query/count.py
 python3 ./test.py -f 2-query/last.py
-#python3 ./test.py -f 2-query/To_iso8601.py
+
+python3 ./test.py -f 2-query/To_iso8601.py
 python3 ./test.py -f 2-query/To_unixtimestamp.py
 python3 ./test.py -f 2-query/timetruncate.py
 python3 ./test.py -f 2-query/diff.py
