d0c11c5bba3c68b2a37cecffdfe023721114adce
Xiaoyu Wang <xiaoyuwang@taosdata.com>
2022-05-27 17:00:37 +0800

    feat: parser adapts asynchronous interface

--- source/client/src/clientImpl.c
+++ source/client/src/clientImpl.c
@@ -348,8 +348,8 @@ int32_t validateSversion(SRequestObj* pRequest, void* res) {
       if (NULL == blk->tblFName || 0 == blk->tblFName[0]) {
         continue;
       }
-      
-      STbSVersion    tbSver = {.tbFName = blk->tblFName, .sver = blk->sver};
+
+      STbSVersion tbSver = {.tbFName = blk->tblFName, .sver = blk->sver};
       taosArrayPush(pArray, &tbSver);
     }
   } else if (TDMT_VND_QUERY == pRequest->type) {
@@ -506,12 +506,12 @@ int32_t refreshMeta(STscObj* pTscObj, SRequestObj* pRequest) {
 
 int32_t removeMeta(STscObj* pTscObj, SArray* tbList) {
   SCatalog* pCatalog = NULL;
-  int32_t tbNum = taosArrayGetSize(tbList);
-  int32_t code = catalogGetHandle(pTscObj->pAppInfo->clusterId, &pCatalog);
+  int32_t   tbNum = taosArrayGetSize(tbList);
+  int32_t   code = catalogGetHandle(pTscObj->pAppInfo->clusterId, &pCatalog);
   if (code != TSDB_CODE_SUCCESS) {
     return code;
   }
-  
+
   for (int32_t i = 0; i < tbNum; ++i) {
     SName* pTbName = taosArrayGet(tbList, i);
     catalogRemoveTableMeta(pCatalog, pTbName);
@@ -519,7 +519,6 @@ int32_t removeMeta(STscObj* pTscObj, SArray* tbList) {
 
   return TSDB_CODE_SUCCESS;
 }
-
 
 SRequestObj* execQuery(STscObj* pTscObj, const char* sql, int sqlLen) {
   SRequestObj* pRequest = NULL;
@@ -543,7 +542,7 @@ SRequestObj* execQuery(STscObj* pTscObj, const char* sql, int sqlLen) {
   if (NEED_CLIENT_RM_TBLMETA_REQ(pRequest->type)) {
     removeMeta(pTscObj, pRequest->tableList);
   }
-  
+
   return pRequest;
 }
 
