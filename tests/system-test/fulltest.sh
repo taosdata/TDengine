c85d72e70e070fa2c18fce168bf8205c38775d1a
jiacy-jcy <cyjia@taosdata.com>
2022-05-27 11:46:39 +0800

    add first.py into ci

--- tests/system-test/fulltest.sh
+++ tests/system-test/fulltest.sh
@@ -40,7 +40,7 @@ python3 ./test.py -f 2-query/max.py
 python3 ./test.py -f 2-query/min.py
 python3 ./test.py -f 2-query/count.py
 python3 ./test.py -f 2-query/last.py
-# python3 ./test.py -f 2-query/first.py
+python3 ./test.py -f 2-query/first.py
 python3 ./test.py -f 2-query/To_iso8601.py
 python3 ./test.py -f 2-query/To_unixtimestamp.py
 python3 ./test.py -f 2-query/timetruncate.py
