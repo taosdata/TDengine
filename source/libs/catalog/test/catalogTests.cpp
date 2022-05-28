d4443e649fcd63fe630d097c1dcd4e59133a58d5
afwerar <1296468573@qq.com>
2022-05-28 20:29:52 +0800

    fix(os): add win ctest

--- source/libs/catalog/test/catalogTests.cpp
+++ source/libs/catalog/test/catalogTests.cpp
@@ -41,7 +41,6 @@
 namespace {
 
 extern "C" int32_t ctgdGetClusterCacheNum(struct SCatalog* pCatalog, int32_t type);
-extern "C" int32_t ctgOpUpdateTbMeta(SCtgCacheOperation *action);
 extern "C" int32_t ctgdEnableDebug(char *option);
 extern "C" int32_t ctgdGetStatNum(char *option, void *res);
 
