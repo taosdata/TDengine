8b4d1a4962c4b0abee3924c52fbcf8d38d426d2e
afwerar <1296468573@qq.com>
2022-06-01 16:50:57 +0800

    fix(os): remote fqdn error

--- tests/system-test/0-others/taosShell.py
+++ tests/system-test/0-others/taosShell.py
@@ -84,7 +84,7 @@ class TDTestCase:
     #updatecfgDict = {'clientCfg': {'serverPort': 7080, 'firstEp': 'trd02:7080', 'secondEp':'trd02:7080'},\
     #                 'serverPort': 7080, 'firstEp': 'trd02:7080'}
     hostname = socket.gethostname()
-    if (not tdDnodes.dnodes[0].remoteIP == ""):
+    if (platform.system().lower() == 'windows' and not tdDnodes.dnodes[0].remoteIP == ""):
         try:
             config = eval(tdDnodes.dnodes[0].remoteIP)
             hostname = config["host"]
