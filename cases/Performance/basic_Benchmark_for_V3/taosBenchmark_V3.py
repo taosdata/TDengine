3fa50b4d0ad3fdf69003bb6c7d9a2adea69182b8
wenzhouwww@live.cn <wenzhouwww@live.cn>
2022-06-07 15:54:25 +0800

    update

--- cases/Performance/basic_Benchmark_for_V3/taosBenchmark_V3.py
+++ cases/Performance/basic_Benchmark_for_V3/taosBenchmark_V3.py
@@ -36,26 +36,25 @@ class TestBigintBoundary(TDCase):
         host = self.get_component_by_name(
             "taosd")[0]['fqdn'][0]
         print(host)
-        # ret = self.envMgr._remote.cmd2(host=)
-        ret = os.system(f"taosBenchmark -f  {case_dir}pre_datas_insert.json")
+        ret = self.envMgr._remote.cmd2(host ,[f"taosBenchmark -f  {case_dir}pre_datas_insert.json"])
 
-        if ret !=0:
+        if ret.failed:
             print("prepare data done ! ")
             sys.exit(ret)
 
         for _ in range(10):
             # basic aggregate query for 3.0 branch 
-            ret = os.system(f"taosBenchmark -f  {case_dir}basic_agg_query.json")
+            ret = self.envMgr._remote.cmd2(host ,[f"taosBenchmark -f  {case_dir}basic_agg_query.json"])
 
-            if ret !=0:
+            if ret.failed:
                 print("basic aggregate done ! ")
                 sys.exit(ret)
 
             # basic long query for 3.0 branch 
 
-            ret = os.system(f"taosBenchmark -f  {case_dir}basic_long_query.json")
+            ret = self.envMgr._remote.cmd2(host ,[f"taosBenchmark -f  {case_dir}basic_long_query.json"])
 
-            if ret !=0:
+            if ret.failed:
                 print("basic long done ! ")
                 sys.exit(ret)
 
