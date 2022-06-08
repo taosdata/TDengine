57bd09dd637ded157780cb224584a5707434c6c7
jiajingbin <jiajaybin@126.com>
2022-06-08 12:25:10 +0800

    test: fix error cases

--- cases/taosc_insert/db_param_cachelast.py
+++ cases/taosc_insert/db_param_cachelast.py
@@ -23,13 +23,14 @@ class TestCachelast(TDCase):
         cachelast check
         """
         test_param = "cachelast"
+        get_param = "cache_model"
         # default
         default_value = 0
         dbname = self.tdCom.get_long_name(length=10, mode="letters")
         self.tdSql.execute(f'create database if not exists {dbname}')
         self.tdSql.query('show databases')
         db_field_kv_dict = self.tdSql.get_db_field_kv(0, dbname)
-        self.tdSql.checkEqual(db_field_kv_dict[test_param], default_value)
+        self.tdSql.checkEqual(db_field_kv_dict[get_param], default_value)
         self.tdSql.execute(f'drop database {dbname}')
         # param_list
         param_value_list = [0, 1, 2, 3]
@@ -38,7 +39,7 @@ class TestCachelast(TDCase):
             self.tdSql.execute(f'create database if not exists {dbname} {test_param} {param_value}')
             self.tdSql.query('show databases')
             db_field_kv_dict = self.tdSql.get_db_field_kv(0, dbname)
-            self.tdSql.checkEqual(db_field_kv_dict[test_param], param_value)
+            self.tdSql.checkEqual(db_field_kv_dict[get_param], param_value)
             self.tdSql.execute(f'drop database {dbname}')
         dbname = self.tdCom.get_long_name(length=10, mode="letters")
         self.tdSql.error(f'create database if not exists {dbname} {test_param} {param_value_list[0] - 1}')
