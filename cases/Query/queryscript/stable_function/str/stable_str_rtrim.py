432ad9785349f7f9de866cb5d253c328eed54502
“happyguoxy” <happy_guoxy@163.com>
2022-06-08 19:51:25 +0800

    Query

--- cases/Query/queryscript/stable_function/str/stable_str_rtrim.py
+++ cases/Query/queryscript/stable_function/str/stable_str_rtrim.py
@@ -176,24 +176,6 @@ class TDTestQuery(TDCase):
                         self.tdSql.error(sql2)
                         sql= sql + sql2
 
-                        sql2 = "select %s from %s where tbname in ('%s_1') and %s %s %s; " %(func,self.table,self.table,qt_where,qt_like_match,qt_in_where)
-                        self.tdCreateData.data_matrix_equal('%s' %sql1 ,1,10,1,1,'%s' %sql2 ,1,10,1,1)
-                        self.np_check(sql1,sql2)
-                        cur1.execute(sql2)       
-                        self.explain_sql(sql2)
-                        sql= sql + sql2
-                        
-                        sql2 = "select * from (select %s from %s where tbname in ('%s_1') and %s %s %s );" %(func,self.table,self.table,qt_where,qt_like_match,qt_in_where)
-                        self.tdCreateData.data_matrix_equal('%s' %sql1 ,1,10,1,1,'%s' %sql2 ,1,10,1,1)
-                        self.np_check(sql1,sql2)
-                        cur1.execute(sql2)       
-                        self.explain_sql(sql2)
-                        sql= sql + sql2
-
-                        sql2 = "select %s from (select * from %s) where tbname in ('%s_1') and %s %s %s; " %(func,self.table,self.table,qt_where,qt_like_match,qt_in_where)
-                        self.tdSql.error(sql2)
-                        sql= sql + sql2
-                        
             except Exception as e:
                 raise e   
 
@@ -223,7 +205,7 @@ class TDTestQuery(TDCase):
                 print("\n\n\n=======hanshu num = %d======right case========case1======\n\n\n" %i)
                 
                 stable_where = tdWhere.stable_where()
-                sql1 = 'select %s from %s;'  % (func,self.table)
+                sql1 = "select %s from %s where tbname in ('%s_1');"  % (func,self.table,self.table)
                 
                 for i in range(2,len(stable_where[2])+1):
                     qt_where = list(combinations(stable_where[2],i))
@@ -232,30 +214,24 @@ class TDTestQuery(TDCase):
                         qt_like_match = stable_where[3]
                         qt_in_where = stable_where[4]
 
-                        sql2 = "select %s from %s where  %s %s %s;" %(func,self.table,qt_where,qt_like_match,qt_in_where)
-                        self.tdCreateData.dataequal('%s' %sql1 ,1,1,'%s' %sql2 ,1,1)
+                        sql2 = "select %s from %s where tbname in ('%s_1') and %s %s %s; " %(func,self.table,self.table,qt_where,qt_like_match,qt_in_where)
+                        self.tdCreateData.data_matrix_equal('%s' %sql1 ,1,10,1,1,'%s' %sql2 ,1,10,1,1)
+                        self.np_check(sql1,sql2)
+                        cur1.execute(sql2)       
+                        self.explain_sql(sql2)
+                        sql= sql + sql2
+                        
+                        sql2 = "select * from (select %s from %s where tbname in ('%s_1') and %s %s %s );" %(func,self.table,self.table,qt_where,qt_like_match,qt_in_where)
                         self.tdCreateData.data_matrix_equal('%s' %sql1 ,1,10,1,1,'%s' %sql2 ,1,10,1,1)
                         self.np_check(sql1,sql2)
                         cur1.execute(sql2)       
                         self.explain_sql(sql2)
                         sql= sql + sql2
 
-                        sql2 = "select * from (select %s from %s where %s %s %s);" %(func,self.table,qt_where,qt_like_match,qt_in_where)
-                        self.tdCreateData.dataequal('%s' %sql1 ,1,1,'%s' %sql2 ,1,1)
-                        self.tdCreateData.data_matrix_equal('%s' %sql1 ,1,10,1,1,'%s' %sql2 ,1,10,1,1)
-                        self.np_check(sql1,sql2)
-                        cur1.execute(sql2)       
-                        self.explain_sql(sql2)
+                        sql2 = "select %s from (select * from %s) where tbname in ('%s_1') and %s %s %s; " %(func,self.table,self.table,qt_where,qt_like_match,qt_in_where)
+                        self.tdSql.error(sql2)
                         sql= sql + sql2
-
-                        sql2 = "select %s from (select * from %s) where %s %s %s;" %(func,self.table,qt_where,qt_like_match,qt_in_where)
-                        self.tdCreateData.dataequal('%s' %sql1 ,1,1,'%s' %sql2 ,1,1)
-                        self.tdCreateData.data_matrix_equal('%s' %sql1 ,1,10,1,1,'%s' %sql2 ,1,10,1,1)
-                        self.np_check(sql1,sql2)
-                        cur1.execute(sql2)       
-                        self.explain_sql(sql2)
-                        sql= sql + sql2
-
+                        
             except Exception as e:
                 raise e   
 
