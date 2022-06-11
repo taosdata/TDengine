95ca88432f92e586956ef452ac65e02c38d8a0e5
Shengliang Guan <slguan@taosdata.com>
2022-06-11 10:58:57 +0800

    fix: save sync config in mnode

--- source/dnode/mnode/sdb/src/sdb.c
+++ source/dnode/mnode/sdb/src/sdb.c
@@ -163,7 +163,10 @@ void sdbSetApplyIndex(SSdb *pSdb, int64_t index) { pSdb->curVer = index; }
 
 void sdbSetApplyTerm(SSdb *pSdb, int64_t term) { pSdb->curTerm = term; }
 
-void sdbSetCurConfig(SSdb *pSdb, int64_t config) { pSdb->curConfig = config; }
+void sdbSetCurConfig(SSdb *pSdb, int64_t config) {
+  mInfo("mnode sync config set from %" PRId64 " to %" PRId64, pSdb->curConfig, config);
+  pSdb->curConfig = config;
+}
 
 int64_t sdbGetApplyIndex(SSdb *pSdb) { return pSdb->curVer; }
 
