be3de7b7df7730105dba8c6742e16574f0c50c51
Shengliang Guan <slguan@taosdata.com>
2022-06-13 22:20:53 +0800

    fix(sync): send snapshot to multi nodes at the same time

--- include/common/tmsg.h
+++ include/common/tmsg.h
@@ -1295,6 +1295,14 @@ int32_t tSerializeSDCreateMnodeReq(void* buf, int32_t bufLen, SDCreateMnodeReq* 
 int32_t tDeserializeSDCreateMnodeReq(void* buf, int32_t bufLen, SDCreateMnodeReq* pReq);
 
 typedef struct {
+  int32_t dnodeId;
+  int8_t  standby;
+} SSetStandbyReq;
+
+int32_t tSerializeSSetStandbyReq(void* buf, int32_t bufLen, SSetStandbyReq* pReq);
+int32_t tDeserializeSSetStandbyReq(void* buf, int32_t bufLen, SSetStandbyReq* pReq);
+
+typedef struct {
   int32_t connId;
   int32_t queryId;
 } SKillQueryReq;
