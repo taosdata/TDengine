ec123d14692c128597f1707443193e154c25f8ba
jiacy-jcy <cyjia@taosdata.com>
2022-06-13 02:12:24 +0000

    update test case

--- cases/taosc_insert/db_param_pagesize.py
+++ cases/taosc_insert/db_param_pagesize.py
@@ -56,23 +56,23 @@ class TestPagesize(TDCase):
         self.tdSql.error(f'create database if not exists {dbname} {test_param} {param_value_list[0] - 1}')
         self.tdSql.error(f'create database if not exists {dbname} {test_param} {param_value_list[-1] + 1}')
 
-        #alter database pagesize
-        dbname = self.tdCom.get_long_name(length=10, mode="letters")
-        self.tdSql.execute(f'create database if not exists {dbname}')
-        param_value_list = [1, 16384]
-        for param_value in param_value_list:
-            dbname = self.tdCom.get_long_name(length=10, mode="letters")
-            self.tdSql.execute(f'alter database {dbname} {test_param} {param_value}')
-            self.tdSql.query('show databases')
-            db_field_kv_dict = self.tdSql.get_db_field_kv(0, dbname)
-            self.tdSql.checkEqual(db_field_kv_dict[test_param], param_value)
-            self.tdSql.query(f'show {dbname}.vgroups')
-            db_vnode_kv_dict = self.tdSql.getOneRow(1,dbname)
-            data = json.load(get_data.get_vnode_json(db_vnode_kv_dict))
-            self.tdSql.checkEqual(db_field_kv_dict[test_param],int(data['config']['szPage'])/1024)
-
-        for i in [param_value_list[0]-1,param_value_list[-1]+1,100.5,'abc']:
-            self.tdSql.error(f'alter database  {dbname} {test_param} {i}')
+        #! alter database pagesize  bug:TD-16324
+        # dbname = self.tdCom.get_long_name(length=10, mode="letters")
+        # self.tdSql.execute(f'create database if not exists {dbname}')
+        # param_value_list = [1, 16384]
+        # for param_value in param_value_list:
+        #     dbname = self.tdCom.get_long_name(length=10, mode="letters")
+        #     self.tdSql.execute(f'alter database {dbname} {test_param} {param_value}')
+        #     self.tdSql.query('show databases')
+        #     db_field_kv_dict = self.tdSql.get_db_field_kv(0, dbname)
+        #     self.tdSql.checkEqual(db_field_kv_dict[test_param], param_value)
+        #     self.tdSql.query(f'show {dbname}.vgroups')
+        #     db_vnode_kv_dict = self.tdSql.getOneRow(1,dbname)
+        #     data = json.load(get_data.get_vnode_json(db_vnode_kv_dict))
+        #     self.tdSql.checkEqual(db_field_kv_dict[test_param],int(data['config']['szPage'])/1024)
+        #! bug TD-16166
+        # for i in [param_value_list[0]-1,param_value_list[-1]+1,100.5,'abc']:
+        #     self.tdSql.error(f'alter database  {dbname} {test_param} {i}')
         
         self.tdSql.execute(f'drop database {dbname}')
     def run(self) -> bool:
