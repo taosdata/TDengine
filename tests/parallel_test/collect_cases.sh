63b40fb29c8b0566e5223abffcb75db0f9c064cd
tangfangzhi <fztang@taosdata.com>
2022-06-14 14:03:51 +0800

    enh: backup source code if case fails

--- tests/parallel_test/collect_cases.sh
+++ tests/parallel_test/collect_cases.sh
@@ -41,5 +41,16 @@ fi
 cat ../script/jenkins/basic.txt |grep -v "^#"|grep -v "^$"|sed "s/^/,,script,/" >>$case_file
 grep "^python" ../system-test/fulltest.sh |sed "s/^/,,system-test,/" >>$case_file
 
+# tar source code for run.sh to use
+if [ $ent -eq 0 ]; then
+    cd ../../../
+    rm -rf TDengine.tar.gz
+    tar czf TDengine.tar.gz TDengine taos-connector-python --exclude=TDengine/debug
+else
+    cd ../../../../
+    rm -rf TDinternal.tar.gz
+    tar czf TDinternal.tar.gz TDinternal taos-connector-python --exclude=TDinternal/debug --exclude=TDinternal/community/debug
+fi
+
 exit 0
 
