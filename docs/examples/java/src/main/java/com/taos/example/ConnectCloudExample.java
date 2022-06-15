878ef3e3f7512085a7fcb3121ad0e7d279a61fad
dingbo <dingbo8128@163.com>
2022-06-15 17:53:21 +0800

    docs: java connect

--- docs/examples/java/src/main/java/com/taos/example/ConnectCloudExample.java
+++ docs/examples/java/src/main/java/com/taos/example/ConnectCloudExample.java
@@ -1,16 +1,19 @@
 package com.taos.example;
-import com.taosdata.jdbc.TSDBDriver;
 
 // ANCHOR: connect
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 
 public class ConnectCloudExample {
     public static void main(String[] args) throws SQLException {
         String jdbcUrl = System.getenv("TDENGINE_JDBC_URL");
         Connection conn = DriverManager.getConnection(jdbcUrl);
+        Statement stmt = conn.createStatement();
+        // test the connection by firing a query
+        stmt.executeQuery("select server_version()");
         conn.close();
     }
 }
