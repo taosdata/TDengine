97e59473f62375fa67f8f479c84f7b7da8eee008
jiajingbin <jiajaybin@126.com>
2022-06-17 01:07:26 +0800

    test: improve testcases

--- cases/taosc_insert/db_param_duration.py
+++ cases/taosc_insert/db_param_duration.py
@@ -22,50 +22,45 @@ class TestDuration(TDCase):
     def init(self):
         self.tdCom = TDCom(self.tdSql)
         self._remote: Remote = Remote(self.logger)
-
+        self.cfg = self.tdCom.Boundary.DB_PARAM_DURATION_CONFIG
 
     def duration_check(self):
         """
         duration check
         """
-        test_param = "duration"
+        test_param = self.cfg["create_name"]
         get_data = GetJson(self.logger, self.run_log_dir,self.env_setting)
         # default
-        default_value = 14400
-        dbname = self.tdCom.get_long_name(length=10, mode="letters")
+        dbname = self.tdCom.get_long_name()
         self.tdSql.execute(f'create database if not exists {dbname}')
         self.tdSql.query('show databases')
         db_field_kv_dict = self.tdSql.get_db_field_kv(0, dbname)
-        self.tdSql.checkEqual(db_field_kv_dict[test_param], default_value)
+        self.tdSql.checkEqual(db_field_kv_dict[test_param], self.cfg["default"])
         self.tdSql.query(f'show {dbname}.vgroups')
-        db_vnode_kv_dict = self.tdSql.getOneRow(1,dbname)
+        db_vnode_kv_dict = self.tdSql.getOneRow(1, dbname)
         data = json.load(get_data.get_vnode_json(db_vnode_kv_dict))
-        # print(data['config']['daysPerFile'])
-        self.tdSql.checkEqual(db_field_kv_dict[test_param],int(data['config']['daysPerFile']))
+        self.tdSql.checkEqual(db_field_kv_dict[test_param], int(data['config'][self.cfg["vnode_json_key"]]))
         self.tdSql.execute(f'drop database {dbname}')
         # param_list
         # without unit
-        param_value_list = [1, 3650,'60m','5256000m','1h','87600h','1d','3650d']
-        for param_value in param_value_list:
-            dbname = self.tdCom.get_long_name(length=10, mode="letters")
+        for param_value in self.cfg["boundary"]:
+            dbname = self.tdCom.get_long_name()
             self.tdSql.execute(f'create database if not exists {dbname}  {test_param} {param_value}')
             self.tdSql.query('show databases')
             db_field_kv_dict = self.tdSql.get_db_field_kv(0, dbname)
             if param_value == 1 or param_value == 3650: # days
                 self.tdSql.checkEqual(db_field_kv_dict[test_param], param_value*60*24)
             elif param_value == '60m' or param_value == '5256000m': # minutes
-                self.tdSql.checkEqual(db_field_kv_dict[test_param], int(re.sub('\D','',param_value)))
+                self.tdSql.checkEqual(db_field_kv_dict[test_param], int(re.sub('\D','', param_value)))
             elif param_value == '1h' or param_value =='87600h': # hours
-                self.tdSql.checkEqual(db_field_kv_dict[test_param], int(re.sub('\D','',param_value)) * 60)
+                self.tdSql.checkEqual(db_field_kv_dict[test_param], int(re.sub('\D','', param_value)) * 60)
             elif param_value == '1d' or param_value == '3650d':
-                self.tdSql.checkEqual(db_field_kv_dict[test_param], int(re.sub('\D','',param_value)) * 60 *24)
+                self.tdSql.checkEqual(db_field_kv_dict[test_param], int(re.sub('\D','', param_value)) * 60 *24)
             self.tdSql.query(f'show {dbname}.vgroups')
-            db_vnode_kv_dict = self.tdSql.getOneRow(1,dbname)
+            db_vnode_kv_dict = self.tdSql.getOneRow(1, dbname)
             data = json.load(get_data.get_vnode_json(db_vnode_kv_dict))
-            # print(data['config']['daysPerFile'])
-            self.tdSql.checkEqual(db_field_kv_dict[test_param],int(data['config']['daysPerFile']))
+            self.tdSql.checkEqual(db_field_kv_dict[test_param],int(data['config'][self.cfg["vnode_json_key"]]))
             self.tdSql.execute(f'drop database {dbname}')
-        dbname = self.tdCom.get_long_name(length=10, mode="letters")
         self.tdSql.error(f'create database if not exists {dbname} {test_param} -1')
         self.tdSql.error(f'create database if not exists {dbname} {test_param} 3651')
         self.tdSql.error(f'create database if not exists {dbname} {test_param} 59m')
