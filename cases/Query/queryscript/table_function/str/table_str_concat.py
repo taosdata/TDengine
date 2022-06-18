966db6c64e68b648d48d5287b0993fc392617d9d
“happyguoxy” <happy_guoxy@163.com>
2022-06-18 11:38:17 +0800

    Query

--- cases/Query/queryscript/table_function/str/table_str_concat.py
+++ cases/Query/queryscript/table_function/str/table_str_concat.py
@@ -86,7 +86,7 @@ class TDTestQuery(TDCase):
     def data_create(self,db):
         #os.system("rm -rf %s/%s.sql" % (self.testcasePath,self.testcaseFilename))   
         os.system("touch %s/%s.sql" % (self.testcasePath,self.testcaseFilename))  
-        self.tdCreateData.dropandcreateDB_random_concat("%s" % self.db, 1)  
+        self.tdCreateData.dropandcreateDB_random_concat("%s" % db, 1)  
      
     def right_case_1_groupby(self):
         print("\n==========================right case 1_groupby==========================\n")
