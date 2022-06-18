bf69338d28a04a47b5c3efdad02ec5b0e84c9f48
“happyguoxy” <happy_guoxy@163.com>
2022-06-18 11:38:06 +0800

    Query

--- cases/Query/queryscript/table_function/numeric/table_numeric_state.py
+++ cases/Query/queryscript/table_function/numeric/table_numeric_state.py
@@ -57,7 +57,7 @@ class TDTestQuery(TDCase):
     def data_create(self,db):
         #os.system("rm -rf %s/%s.sql" % (self.testcasePath,self.testcaseFilename))    
         os.system("touch %s/%s.sql" % (self.testcasePath,self.testcaseFilename))  
-        self.tdCreateData.dropandcreateDB_random("%s" % self.db, 10) 
+        self.tdCreateData.dropandcreateDB_random("%s" % db, 10) 
         
     # def case_common(self):
     #     #os.system("rm -rf %s/%s.sql" % (self.testcasePath,self.testcaseFilename))    
