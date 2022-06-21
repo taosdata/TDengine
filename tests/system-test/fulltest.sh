b2486b2a4b96ebe87536818228e80fcbe4e6670f
wenzhouwww@live.cn <wenzhouwww@live.cn>
2022-06-21 14:04:03 +0800

    add case for twa function

--- tests/system-test/fulltest.sh
+++ tests/system-test/fulltest.sh
@@ -105,6 +105,7 @@ python3 ./test.py -f 2-query/distribute_agg_min.py
 python3 ./test.py -f 2-query/distribute_agg_sum.py
 python3 ./test.py -f 2-query/distribute_agg_spread.py
 python3 ./test.py -f 2-query/distribute_agg_apercentile.py
+python3 ./test.py -f 2-query/twa.py
 
 python3 ./test.py -f 6-cluster/5dnode1mnode.py 
 python3 ./test.py -f 6-cluster/5dnode2mnode.py 
