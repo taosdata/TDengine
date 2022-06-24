a533f3497c0ec525dca5d6a5c1f31c9ac7a27e6c
wenzhouwww@live.cn <wenzhouwww@live.cn>
2022-06-24 16:28:56 +0800

    add case for scalar function for null value

--- tests/system-test/fulltest.sh
+++ tests/system-test/fulltest.sh
@@ -110,6 +110,8 @@ python3 ./test.py -f 2-query/distribute_agg_avg.py
 python3 ./test.py -f 2-query/distribute_agg_stddev.py
 python3 ./test.py -f 2-query/twa.py
 
+python3 ./test.py -f 2-query/function_null.py
+
 python3 ./test.py -f 6-cluster/5dnode1mnode.py 
 python3 ./test.py -f 6-cluster/5dnode2mnode.py 
 python3 ./test.py -f 6-cluster/5dnode3mnodeStop.py -N 5 -M 3
