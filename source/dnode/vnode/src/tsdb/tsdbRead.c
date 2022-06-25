27009d5328298e8f06da1d40ef29e7baa6464a09
Cary Xu <klxu@taosdata.com>
2022-06-25 13:40:54 +0800

    fix: table list would be empty when open reader

--- source/dnode/vnode/src/tsdb/tsdbRead.c
+++ source/dnode/vnode/src/tsdb/tsdbRead.c
@@ -506,11 +506,9 @@ int32_t tsdbSetTableList(tsdbReaderT reader, SArray* tableList){
 
 tsdbReaderT tsdbReaderOpen(SVnode* pVnode, SQueryTableDataCond* pCond, SArray* tableList, uint64_t qId,
                             uint64_t taskId) {
-#if 0
   if(taosArrayGetSize(tableList) == 0){
     return NULL;
   }
-#endif
   STsdbReadHandle* pTsdbReadHandle = tsdbQueryTablesImpl(pVnode, pCond, qId, taskId);
   if (pTsdbReadHandle == NULL) {
     return NULL;
