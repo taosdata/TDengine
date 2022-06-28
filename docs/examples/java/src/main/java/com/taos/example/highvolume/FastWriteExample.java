b16a69b54a9d6fd469a38eb243dc5f50701156b5
dingbo <dingbo8128@163.com>
2022-06-28 20:12:43 +0800

    docs: high volumn example for java

--- docs/examples/java/src/main/java/com/taos/example/highvolume/FastWriteExample.java
+++ docs/examples/java/src/main/java/com/taos/example/highvolume/FastWriteExample.java
@@ -1,36 +1,50 @@
 package com.taos.example.highvolume;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingDeque;
 
 // ANCHOR: main
 public class FastWriteExample {
+    final static Logger logger = LoggerFactory.getLogger(FastWriteExample.class);
 
-    final static int readTaskCount = 2;
-    final static int writeTaskCount = 4;
+    final static int readTaskCount = 1;
+    final static int writeTaskCount = 1;
     final static int taskQueueCapacity = 1000;
-    final static List<BlockingQueue> taskQueues = new ArrayList<>();
+    final static List<BlockingQueue<String>> taskQueues = new ArrayList<>();
+    final static List<ReadTask> readTasks = new ArrayList<>();
+    final static List<WriteTask> writeTasks = new ArrayList<>();
+
+    public static void stopAll() {
+        logger.info("stopAll");
+        readTasks.forEach(task -> task.stop());
+        writeTasks.forEach(task -> task.stop());
+    }
 
     public static void main(String[] args) throws InterruptedException {
         // Create task queues, whiting tasks and start writing threads.
         for (int i = 0; i < writeTaskCount; ++i) {
-            BlockingQueue<String> queue = new LinkedBlockingDeque<>(taskQueueCapacity);
+            BlockingQueue<String> queue = new ArrayBlockingQueue<>(taskQueueCapacity);
+            taskQueues.add(queue);
             WriteTask task = new WriteTask(queue);
             Thread t = new Thread(task);
+            t.setName("WriteThread-" + i);
             t.start();
         }
 
         // create reading tasks and start reading threads
         for (int i = 0; i < readTaskCount; ++i) {
-            ReadTask task = new ReadTask(taskQueues);
+            ReadTask task = new ReadTask(i, taskQueues);
             Thread t = new Thread(task);
+            t.setName("ReadThread-" + i);
+            t.start();
         }
 
-        while (true) {
-            Thread.sleep(1000);
-        }
+        Runtime.getRuntime().addShutdownHook(new Thread(FastWriteExample::stopAll));
     }
 }
 
--- docs/examples/java/src/main/java/com/taos/example/highvolume/WriteTask.java
+++ docs/examples/java/src/main/java/com/taos/example/highvolume/WriteTask.java
@@ -1,5 +1,8 @@
 package com.taos.example.highvolume;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -7,60 +10,70 @@ import java.sql.Statement;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingDeque;
 
 // ANCHOR: WriteTask
 class WriteTask implements Runnable {
-    final static int maxBatchSize = 500;
+    private final static Logger logger = LoggerFactory.getLogger(WriteTask.class);
+    private final static int maxBatchSize = 500;
     private final BlockingQueue<String> queue;
+    private boolean active = true;
 
     public WriteTask(BlockingQueue<String> taskQueue) {
         this.queue = taskQueue;
     }
 
-    private static Connection getConnection() throws SQLException {
-        String jdbcUrl = "jdbc:TAOS://localhost:6030?user=root&password=taosdata";
-        return DriverManager.getConnection(jdbcUrl);
+    private static Connection getConnection() throws SQLException, ClassNotFoundException {
+        String jdbcURL = System.getenv("TDENGINE_JDBC_URL");
+        return DriverManager.getConnection(jdbcURL);
     }
 
+    @Override
     public void run() {
-        int count = 0;
-        try {
-            Connection conn = getConnection();
-            Statement stmt = conn.createStatement();
-            Map<String, String> tbValues = new HashMap<>();
-            while (true) {
-                String line = queue.poll();
-                if (line != null) {
-                    processLine(tbValues, line);
-                    count += 1;
-                    if (count == maxBatchSize) {
-                        // trigger writing when count of buffered records reached maxBachSize
+        logger.info("started");
+        int bufferedCount = 0;
+        String line = null;
+        try (Connection conn = getConnection()) {
+            try (Statement stmt = conn.createStatement()) {
+                Map<String, String> tbValues = new HashMap<>();
+                while (active) {
+                    line = queue.poll();
+                    if (line != null) {
+                        processLine(tbValues, line);
+                        bufferedCount += 1;
+                        if (bufferedCount == maxBatchSize) {
+                            // trigger writing when count of buffered records reached maxBachSize
+                            flushValues(stmt, tbValues);
+                            bufferedCount = 0;
+                        }
+                    } else if (bufferedCount == 0) {
+                        // if queue is empty and no buffered records, sleep a while to avoid high CPU usage.
+                        Thread.sleep(500);
+                    } else {
+                        // if queue is empty and there are buffered records then flush immediately
                         flushValues(stmt, tbValues);
-                        count = 0;
+                        bufferedCount = 0;
                     }
-                } else if (count == 0) {
-                    // if queue is empty and no buffered records, sleep a while to avoid high CPU usage.
-                    Thread.sleep(500);
-                } else {
-                    // if queue is empty and there are buffered records then flush immediately
+                }
+                if (bufferedCount > 0) {
                     flushValues(stmt, tbValues);
-                    count = 0;
                 }
             }
         } catch (Exception e) {
-            // handle exception
+            String msg = String.format("line=%s, bufferedCount=%s", line, bufferedCount);
+            logger.error(msg, e);
         }
-
     }
 
     private void processLine(Map<String, String> tbValues, String line) {
-
     }
 
     private void flushValues(Statement stmt, Map<String, String> tbValues) {
-
+        StringBuilder sb = new StringBuilder("INSERT INTO");
     }
 
+    public void stop() {
+        logger.info("stop");
+        this.active = false;
+    }
 }
 // ANCHOR_END: WriteTask
