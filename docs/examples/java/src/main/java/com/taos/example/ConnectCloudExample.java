cce6dc7350380f4ead903c1597e9e1fbf97bea72
BoDing <dingbo8128@163.com>
2022-06-28 21:34:08 +0800

    docs: java connect example free resource

--- docs/examples/java/src/main/java/com/taos/example/ConnectCloudExample.java
+++ docs/examples/java/src/main/java/com/taos/example/ConnectCloudExample.java
@@ -10,12 +10,12 @@ import java.sql.Statement;
 public class ConnectCloudExample {
     public static void main(String[] args) throws SQLException {
         String jdbcUrl = System.getenv("TDENGINE_JDBC_URL");
-        Connection conn = DriverManager.getConnection(jdbcUrl);
-        Statement stmt = conn.createStatement();
-        // test the connection by firing a query
-        stmt.executeQuery("select server_version()");
-        stmt.close();
-        conn.close();
+        try(Connection conn = DriverManager.getConnection(jdbcUrl)) {
+            try(Statement stmt = conn.createStatement()) {
+                // test the connection by firing a query
+                stmt.executeQuery("select server_version()");
+            }
+        }
     }
 }
 // ANCHOR_END: connect
