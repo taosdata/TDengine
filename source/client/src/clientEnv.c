3625a93df1b216dc39f4219c29500405dd104b71
yihaoDeng <luomoxyz@126.com>
2022-06-28 20:32:06 +0800

    fix: fix rpc quit problem

--- source/client/src/clientEnv.c
+++ source/client/src/clientEnv.c
@@ -91,7 +91,7 @@ void closeTransporter(SAppInstInfo *pAppInfo) {
 static bool clientRpcRfp(int32_t code, tmsg_t msgType) {
   if (code == TSDB_CODE_RPC_REDIRECT || code == TSDB_CODE_RPC_NETWORK_UNAVAIL || code == TSDB_CODE_NODE_NOT_DEPLOYED ||
       code == TSDB_CODE_SYN_NOT_LEADER || code == TSDB_CODE_APP_NOT_READY) {
-    if (/*msgType == TDMT_VND_QUERY ||*/ msgType == TDMT_VND_FETCH) {
+    if (msgType == TDMT_VND_QUERY || msgType == TDMT_VND_FETCH) {
       return false;
     }
     return true;
