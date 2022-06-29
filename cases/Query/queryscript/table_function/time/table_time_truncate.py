19e6bafd261d22c60463ec6a3e4c7e17fad22b0e
“happyguoxy” <happy_guoxy@163.com>
2022-06-29 10:31:14 +0800

    test: refine query cases

--- cases/Query/queryscript/table_function/time/table_time_truncate.py
+++ cases/Query/queryscript/table_function/time/table_time_truncate.py
@@ -707,15 +707,24 @@ class TDTestQuery(TDCase):
                         sql= sql + sql2
 
                         sql2 = "select %s from %s where tbname in ('%s') and  %s %s %s order by ts limit 1000" %(func,self.table,self.table,qt_where,qt_like_match,qt_in_where)
-                        self.tdSql.error(sql2)
+                        self.tdCreateData.data_matrix_equal('%s' %sql1 ,1,100,1,1,'%s' %sql2 ,1,100,1,1)
+                        self.time_check(sql1,sql2)
+                        self.np_check(sql1,sql2)
+                        cur1.execute(sql2)
                         sql= sql + sql2
 
                         sql2 = "select * from (select %s from %s where tbname in ('%s') and %s %s %s order by ts limit 1000)" %(func,self.table,self.table,qt_where,qt_like_match,qt_in_where)
-                        self.tdSql.error(sql2)
+                        self.tdCreateData.data_matrix_equal('%s' %sql1 ,1,100,1,1,'%s' %sql2 ,1,100,1,1)
+                        self.time_check(sql1,sql2)
+                        self.np_check(sql1,sql2)
+                        cur1.execute(sql2)
                         sql= sql + sql2
 
                         sql2 = "select %s from (select * from %s) where tbname in ('%s') and %s %s %s order by ts limit 1000" %(func,self.table,self.table,qt_where,qt_like_match,qt_in_where)
-                        self.tdSql.error(sql2)
+                        self.tdCreateData.data_matrix_equal('%s' %sql1 ,1,100,1,1,'%s' %sql2 ,1,100,1,1)
+                        self.time_check(sql1,sql2)
+                        self.np_check(sql1,sql2)
+                        cur1.execute(sql2)
                         sql= sql + sql2
                                    
             except Exception as e:
