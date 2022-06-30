208dee8edc9b74bb9834e6c7da862790fc91816e
Minglei Jin <mljin@taosdata.com>
2022-06-30 21:07:09 +0800

    tsdb/cache: add insert last interface

--- source/dnode/vnode/src/inc/tsdb.h
+++ source/dnode/vnode/src/inc/tsdb.h
@@ -251,6 +251,7 @@ int32_t tsdbReadDelIdx(SDelFReader *pReader, SArray *aDelIdx, uint8_t **ppBuf);
 // tsdbCache
 int32_t tsdbOpenCache(STsdb *pTsdb);
 void    tsdbCloseCache(SLRUCache *pCache);
+int32_t tsdbCacheInsertLast(SLRUCache *pCache, tb_uid_t uid, STSRow *row);
 int32_t tsdbCacheInsertLastrow(SLRUCache *pCache, tb_uid_t uid, STSRow *row);
 
 int32_t tsdbCacheGetLastH(SLRUCache *pCache, tb_uid_t uid, STsdb *pTsdb, LRUHandle **h);
