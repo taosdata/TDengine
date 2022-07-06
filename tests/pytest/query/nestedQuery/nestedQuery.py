51ba0eda46d27b6bf6ca36d7dc54af1b9164bcef
Shuduo Sang <sangshuduo@gmail.com>
2022-07-06 07:32:34 +0000

    fix: query/nestedQuery/nestedQuery.py

--- tests/pytest/query/nestedQuery/nestedQuery.py
+++ tests/pytest/query/nestedQuery/nestedQuery.py
@@ -2222,21 +2222,21 @@ class TDTestCase:
         # regualr-table
         os.system("%staosBenchmark -N -d regular -t 2 -n 1000 -l 4095 -y" % binPath)
         tdSql.execute("use regular")
-        tdSql.query("select * from meters;")
+        tdSql.query("select * from d0;")
         tdSql.checkCols(4096)
-        tdSql.query("describe meters;")
+        tdSql.query("describe d0;")
         tdSql.checkRows(4096)
-        tdSql.query("select * from (select * from meters);")
+        tdSql.query("select * from (select * from d0);")
         tdSql.checkCols(4096)
 
         # select * from (select 4096 columns form d0)
         sql = "select * from ( select ts , "
         for i in range(4094):
             sql += "c%d , " % (i)
-        sql += "c4094 from meters "  
+        sql += "c4094 from d0 "
         sql += " %s )" % random.choice(order_where)
         sql += " %s ;" % random.choice(order_desc_where)
-        tdLog.info(len(sql))      
+        tdLog.info(len(sql))
         tdSql.query(sql)
         tdSql.checkCols(4096)
         tdSql.checkRows(1000)
