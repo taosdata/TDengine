718a7350da03728aa7adc9c2f3e52beed2943ddc
jiacy-jcy <cyjia@taosdata.com>
2022-07-07 13:56:31 +0800

    update test case

--- tests/system-test/2-query/percentile.py
+++ tests/system-test/2-query/percentile.py
@@ -68,43 +68,40 @@ class TDTestCase:
         self.binary_str = 'taosdata'
         self.nchar_str = '涛思数据'
         self.tbnum = 2
+        self.tag_ts = self.ts
+        self.tag_tinyint = 1
+        self.tag_smallint = 2
+        self.tag_int = 3
+        self.tag_bigint = 4
+        self.tag_utint = 5
+        self.tag_usint = 6
+        self.tag_uint = 7
+        self.tag_ubint = 8
+        self.tag_float = 9.1
+        self.tag_double = 10.1
+        self.tag_bool = True
         self.tag_values = [
-            f'1537146000000,1,2,3,4,5,6,7,8,9.1,10.1,"{self.binary_str}","{self.nchar_str}"'
+            f'{self.tag_ts},{self.tag_tinyint},{self.tag_smallint},{self.tag_int},{self.tag_bigint},\
+            {self.tag_utint},{self.tag_usint},{self.tag_uint},{self.tag_ubint},{self.tag_float},{self.tag_double},{self.tag_bool},"{self.binary_str}","{self.nchar_str}"'
             
         ]
         
         self.param = [1,50,100]
+        
     def insert_data(self,column_dict,tbname,row_num):
         intData = []        
         floatData = []
-        sql = ''
-        for k, v in column_dict.items():
-            if v.lower() == 'timestamp' or v.lower() == 'tinyint' or v.lower() == 'smallint' or v.lower() == 'int' or v.lower() == 'bigint' or \
-            v.lower() == 'tinyint unsigned' or v.lower() == 'smallint unsigned' or v.lower() == 'int unsigned' or v.lower() == 'bigint unsigned' or v.lower() == 'bool':
-                sql += '%d,'
-            elif v.lower() == 'float' or v.lower() == 'double':
-                sql += '%f,'
-            elif 'binary' in v.lower():
-                sql += f'"{self.binary_str}%d",'
-            elif 'nchar' in v.lower():
-                sql += f'"{self.nchar_str}%d",'
-        insert_sql = f'insert into {tbname} values({sql[:-1]})'
+        insert_sql = self.setsql.set_insertsql(column_dict,tbname,self.binary_str,self.nchar_str)
         for i in range(row_num):
             insert_list = []
-            for k, v in column_dict.items():
-                if v.lower() in[ 'tinyint' , 'smallint' , 'int', 'bigint' , 'tinyint unsigned' , 'smallint unsigned' , 'int unsigned' , 'bigint unsigned'] or\
-                'binary' in v.lower() or 'nchar' in v.lower():
-                    insert_list.append(1 + i)
-                elif v.lower() == 'float' or v.lower() == 'double':
-                    insert_list.append(0.1 + i)
-                elif v.lower() == 'bool':
-                    insert_list.append(i % 2)
-                elif v.lower() == 'timestamp':
-                    insert_list.append(self.ts + i)
-            tdSql.execute(insert_sql%(tuple(insert_list)))
-            intData.append(i + 1)            
+            self.setsql.insert_values(column_dict,i,insert_sql,insert_list,self.ts)
+            intData.append(i)            
             floatData.append(i + 0.1)
         return intData,floatData
+    def check_tags(self,tags,param,num,value):
+        tdSql.query(f'select percentile({tags}, {param}) from {self.stbname}_{num}')
+        print(tdSql.queryResult)
+        tdSql.checkEqual(tdSql.queryResult[0][0], value)
     def function_check_ntb(self):
         tdSql.prepare()
         tdSql.execute(self.setsql.set_create_normaltable_sql(self.ntbname,self.column_dict))
@@ -120,63 +117,50 @@ class TDTestCase:
                     tdSql.query(f'select percentile({k}, {param}) from {self.ntbname}')
                     tdSql.checkData(0, 0, np.percentile(floatData, param))
     def function_check_ctb(self):
-        
         tdSql.prepare()
         tdSql.execute(self.setsql.set_create_stable_sql(self.stbname,self.column_dict,self.tag_dict))
         for i in range(self.tbnum):
             tdSql.execute(f"create table {self.stbname}_{i} using {self.stbname} tags({self.tag_values[0]})")
-            tdSql.execute(self.insert_data(self.column_dict,f'{self.stbname}_{i}',self.rowNum))
-        
+            intData,floatData = self.insert_data(self.column_dict,f'{self.stbname}_{i}',self.rowNum)
+        for i in range(self.tbnum):
+            for k,v in self.column_dict.items():
+                for param in self.param:
+                    if v.lower() in ['timestamp','bool'] or 'binary' in v.lower() or 'nchar' in v.lower():
+                        tdSql.error(f'select percentile({k},{param}) from {self.stbname}_{i}')
+                    elif v.lower() in ['tinyint','smallint','int','bigint','tinyint unsigned','smallint unsigned','int unsigned','bigint unsigned']:
+                        tdSql.query(f'select percentile({k}, {param}) from {self.stbname}_{i}')
+                        tdSql.checkData(0, 0, np.percentile(intData, param))
+                    else:
+                        tdSql.query(f'select percentile({k}, {param}) from {self.stbname}_{i}')
+                        tdSql.checkData(0, 0, np.percentile(floatData, param))
+            #!bug TD-17119
+            # for k,v in self.tag_dict.items():
+            #     for param in self.param:
+            #         if v.lower() in ['timestamp','bool'] or 'binary' in v.lower() or 'nchar' in v.lower():
+            #             tdSql.error(f'select percentile({k},{param}) from {self.stbname}_{i}')
+            #         elif v.lower() == 'tinyint':
+            #             self.check_tags(k,param,i,self.tag_tinyint)
+            #         elif v.lower() == 'smallint':
+            #             self.check_tags(k,param,i,self.tag_smallint)
+            #         elif v.lower() == 'int':
+            #             self.check_tags(k,param,i,self.tag_int)
+            #         elif v.lower() == 'bigint':
+            #             self.check_tags(k,param,i,self.tag_bigint)
+            #         elif v.lower() == 'tinyint unsigned':
+            #             self.check_tags(k,param,i,self.tag_utint)
+            #         elif v.lower() == 'smallint unsigned':
+            #             self.check_tags(k,param,i,self.tag_usint)   
+            #         elif v.lower() == 'int unsigned':
+            #             self.check_tags(k,param,i,self.tag_uint) 
+            #         elif v.lower() == 'bigint unsigned':
+            #             self.check_tags(k,param,i,self.tag_ubint)
+            #         elif v.lower() == 'float':
+            #             self.check_tags(k,param,i,self.tag_float)
+            #         elif v.lower() == 'double':
+            #             self.check_tags(k,param,i,self.tag_double)
     def run(self):
-        # self.function_check_ntb()
+        self.function_check_ntb()
         self.function_check_ctb()
-        # tdSql.prepare()
-
-        # intData = []        
-        # floatData = []
-
-        # tdSql.execute('''create table test(ts timestamp, col1 tinyint, col2 smallint, col3 int, col4 bigint, col5 float, col6 double, 
-        #             col7 bool, col8 binary(20), col9 nchar(20), col11 tinyint unsigned, col12 smallint unsigned, col13 int unsigned, col14 bigint unsigned)''')
-        # for i in range(self.rowNum):
-        #     tdSql.execute("insert into test values(%d, %d, %d, %d, %d, %f, %f, %d, 'taosdata%d', '涛思数据%d', %d, %d, %d, %d)" 
-        #                 % (self.ts + i, i + 1, i + 1, i + 1, i + 1, i + 0.1, i + 0.1, i % 2, i + 1, i + 1, i + 1, i + 1, i + 1, i + 1))
-        #     intData.append(i + 1)            
-        #     floatData.append(i + 0.1)                        
-
-        # # percentile verifacation 
-        # tdSql.error("select percentile(ts ,20) from test")
-        # tdSql.error("select percentile(col7 ,20) from test")
-        # tdSql.error("select percentile(col8 ,20) from test")        
-        # tdSql.error("select percentile(col9 ,20) from test") 
-        # column_list = [1,2,3,4,11,12,13,14]
-        # percent_list = [0,50,100]  
-        # for i in column_list:  
-        #     for j in percent_list:
-        #         tdSql.query(f"select percentile(col{i}, {j}) from test")        
-        #         tdSql.checkData(0, 0, np.percentile(intData, j)) 
-
-        # for i in [5,6]:
-        #     for j in percent_list:
-        #         tdSql.query(f"select percentile(col{i}, {j}) from test")
-        #         tdSql.checkData(0, 0, np.percentile(floatData, j))
-        
-        # tdSql.execute("create table meters (ts timestamp, voltage int) tags(loc nchar(20))")
-        # tdSql.execute("create table t0 using meters tags('beijing')")
-        # tdSql.execute("create table t1 using meters tags('shanghai')")
-        # for i in range(self.rowNum):
-        #     tdSql.execute("insert into t0 values(%d, %d)" % (self.ts + i, i + 1))            
-        #     tdSql.execute("insert into t1 values(%d, %d)" % (self.ts + i, i + 1))            
-        
-        # # tdSql.error("select percentile(voltage, 20) from meters")
-
-
- 
-        # tdSql.execute("create table st(ts timestamp, k int)")
-        # tdSql.execute("insert into st values(now, -100)(now+1a,-99)")
-        # tdSql.query("select apercentile(k, 20) from st")
-        # tdSql.checkData(0, 0, -100.00)
-
-
         
     def stop(self):
         tdSql.close()
