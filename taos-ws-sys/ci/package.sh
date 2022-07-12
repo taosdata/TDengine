f1d71c4d45d358d952bbafded9357b207241ab51
Huo Linhe <linhehuo@gmail.com>
2022-07-12 13:25:00 +0800

    chore: package ws-run-sql

--- taos-ws-sys/ci/package.sh
+++ taos-ws-sys/ci/package.sh
@@ -5,5 +5,6 @@ wkdir=$bin/../../target/libtaosws
 [ -d "$wkdir" ] || mkdir -p $wkdir
 cp -f $bin/../../target/release/taosws.h $bin/../../target/release/libtaosws.so $bin/../../target/release/libtaosws.a $wkdir/
 cp -f $bin/../examples/Makefile $bin/../examples/show-databases.c $wkdir/
+cp -f $bin/../examples/ws-run-sql.c $wkdir/
 cd $wkdir/../
 tar cavf libtaosws.tar.gz libtaosws/
