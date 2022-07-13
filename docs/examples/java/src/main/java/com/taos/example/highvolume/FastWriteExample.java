b9d7fb56099fcdcd15b53c4d0d23e5fa29be2fb9
dingbo <dingbo8128@163.com>
2022-07-13 12:18:45 +0800

    docs: format code

--- docs/examples/java/src/main/java/com/taos/example/highvolume/FastWriteExample.java
+++ docs/examples/java/src/main/java/com/taos/example/highvolume/FastWriteExample.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingDeque;
 
 /**
  * Prepare target database.
