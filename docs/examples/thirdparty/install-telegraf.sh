3a76613ff217baa4a6f0a26a97f29dda8afd7e28
dingbo <dingbo8128@163.com>
2022-07-15 14:51:50 +0800

    docs: telegraf.md

---
+++ docs/examples/thirdparty/install-telegraf.sh
@@ -0,0 +1,4 @@
+wget -q https://repos.influxdata.com/influxdb.key
+echo '23a1c8836f0afc5ed24e0486339d7cc8f6790b83886c4c96995b88a061c5bb5d influxdb.key' | sha256sum -c && cat influxdb.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/influxdb.gpg > /dev/null
+echo 'deb [signed-by=/etc/apt/trusted.gpg.d/influxdb.gpg] https://repos.influxdata.com/debian stable main' | sudo tee /etc/apt/sources.list.d/influxdata.list
+sudo apt-get update && sudo apt-get install telegraf
