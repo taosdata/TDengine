f82a7c511b530dba562c125896949ad9f953a330
dingbo <dingbo8128@163.com>
2022-07-15 16:21:12 +0800

    test: telegraf

---
+++ cases/cloud/telegraf.py
@@ -0,0 +1,25 @@
+from taostest import TDCase, T
+
+
+class Telegraf(TDCase):
+
+    def run(self):
+        # test run
+        self.logger.info("aaa")
+        self.logger.info("bbb")
+        self.logger.info("bbb")
+        self.logger.info("ccc")
+        self.logger.info("dd")
+        self.logger.info("aaa")
+        self.logger.info("aaa")
+        self.logger.info("aaa")
+        self.logger.info("axa")
+
+    def desc(self) -> str:
+        return "Test connectivity between cloud and telegraf"
+
+    def author(self) -> str:
+        return "DingBo"
+
+    def tags(self):
+        return T.Cloud
