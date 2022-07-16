5c76b493914c9d1dd33331aa814233fd7dd4a1b4
Cary Xu <klxu@taosdata.com>
2022-07-16 15:11:50 +0800

    other: revert the definition

--- source/dnode/mnode/impl/inc/mndGrant.h
+++ source/dnode/mnode/impl/inc/mndGrant.h
@@ -25,8 +25,7 @@
 int32_t mndInitGrant(SMnode *pMnode);
 void    mndCleanupGrant();
 void    grantParseParameter();
-void    grantReset(EGrantType grant, uint64_t value);
-void    grantResetNew(SMnode *pMnode, EGrantType grant, uint64_t value);
+void    grantReset(SMnode *pMnode, EGrantType grant, uint64_t value);
 void    grantAdd(EGrantType grant, uint64_t value);
 void    grantRestore(EGrantType grant, uint64_t value);
 
--- source/dnode/mnode/impl/src/mndMain.c
+++ source/dnode/mnode/impl/src/mndMain.c
@@ -417,7 +417,7 @@ int32_t mndStart(SMnode *pMnode) {
     mndSetRestore(pMnode, true);
   }
   
-  grantResetNew(pMnode, TSDB_GRANT_ALL, 0);
+  grantReset(pMnode, TSDB_GRANT_ALL, 0);
 
   return mndInitTimer(pMnode);
 }
