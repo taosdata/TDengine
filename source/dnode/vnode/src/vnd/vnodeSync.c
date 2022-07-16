fdf7930702c599b0ce3c669815f7890ebaf340b4
Minghao Li <castermode@gmail.com>
2022-07-16 18:09:36 +0800

    refactor(sync): add index/term in snapshot write

--- source/dnode/vnode/src/vnd/vnodeSync.c
+++ source/dnode/vnode/src/vnd/vnodeSync.c
@@ -515,7 +515,7 @@ static int32_t vnodeSnapshotStartWrite(struct SSyncFSM *pFsm, void *pParam, void
 #endif
 }
 
-static int32_t vnodeSnapshotStopWrite(struct SSyncFSM *pFsm, void *pWriter, bool isApply) {
+static int32_t vnodeSnapshotStopWrite(struct SSyncFSM *pFsm, void *pWriter, bool isApply, SSnapshot *pSnapshot) {
 #ifdef USE_TSDB_SNAPSHOT
   SVnode *pVnode = pFsm->data;
   int32_t code = vnodeSnapWriterClose(pWriter, !isApply);
