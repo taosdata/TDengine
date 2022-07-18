7fd6f398daeffa0e1c49f43185d57ac3170aa327
dingbo <dingbo8128@163.com>
2022-07-18 17:41:20 +0800

    test: rust connector for cloud

--- cases/cloud/connector/rust.py
+++ cases/cloud/connector/rust.py
@@ -11,7 +11,10 @@ class Rust(TDCase):
     def run(self):
         cwd = self.env_setting["work_dir"] + "/docs-cloud/docs/examples/rust/cloud-example"
         os.environ.update(self.env_setting["env"])
-        self.lcmd.run("cargo run", cwd=cwd)
+        print("---------------------------------------------")
+        print(os.environ["path"])
+        print("----------------------------------------------")
+        self.lcmd.run("cargo run", cwd=cwd, env=os.environ)
 
     def desc(self) -> str:
         return "rust connector for cloud"
