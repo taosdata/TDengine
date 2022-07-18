3a5b4cfddb194b56238b15fa665201266e53d8a9
dingbo <dingbo8128@163.com>
2022-07-18 17:21:16 +0800

    fix: os object can't be pickled

--- cases/cloud/connector/rust.py
+++ cases/cloud/connector/rust.py
@@ -2,13 +2,16 @@
 测试云服务 Rust 连接器示例代码
 启动命令: tt --case=cloud/connector/rust.py --use=cloud_test.yaml
 """
+import os
+
 from taostest import TDCase, T
 
 
 class Rust(TDCase):
     def run(self):
         cwd = self.env_setting["work_dir"] + "/docs-cloud/docs/examples/rust/cloud-example"
-        self.lcmd.run("cargo run", cwd=cwd, env=self.env_setting["env"])
+        os.environ.update(self.env_setting["env"])
+        self.lcmd.run("cargo run", cwd=cwd)
 
     def desc(self) -> str:
         return "rust connector for cloud"
