ee0704ff22fd635e24f3020230ca8bc4db18d85f
dingbo <dingbo8128@163.com>
2022-07-18 15:50:35 +0800

    test: rust connector for cloud

--- cases/cloud/connector/rust.py
+++ cases/cloud/connector/rust.py
@@ -1,6 +1,6 @@
 """
 测试云服务 Rust 连接器示例代码
-启动命令: tt --case=cloud/connector/python.py --use=cloud_test.yaml
+启动命令: tt --case=cloud/connector/rust.py --use=cloud_test.yaml
 """
 from taostest import TDCase, T
 
