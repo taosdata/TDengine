7c990216d979c1b48d107991645b6b0ca5f3b1fb
plum-lihui <huili@taosdata.com>
2022-07-19 20:17:57 +0800

    test: split test case for timeout

--- tests/system-test/fulltest.sh
+++ tests/system-test/fulltest.sh
@@ -185,7 +185,8 @@ python3 ./test.py -f 7-tmq/tmqConsFromTsdb1-mutilVg-mutilCtb.py
 python3 ./test.py -f 7-tmq/tmqAutoCreateTbl.py
 #python3 ./test.py -f 7-tmq/tmqDnodeRestart.py
 python3 ./test.py -f 7-tmq/tmqUpdate-1ctb.py
-python3 ./test.py -f 7-tmq/tmqUpdate-multiCtb.py
+python3 ./test.py -f 7-tmq/tmqUpdate-multiCtb-snapshot0.py
+python3 ./test.py -f 7-tmq/tmqUpdate-multiCtb-snapshot1.py
 #python3 ./test.py -f 7-tmq/tmqDelete-1ctb.py
 python3 ./test.py -f 7-tmq/tmqUdf.py
 python3 ./test.py -f 7-tmq/tmqUdf-multCtb.py
