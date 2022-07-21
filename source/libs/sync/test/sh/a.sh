da2686ce695686065c6bf630851ed197ec7676be
Minghao Li <castermode@gmail.com>
2022-07-21 16:43:56 +0800

    refactor(sync): add log tools

--- source/libs/sync/test/sh/a.sh
+++ source/libs/sync/test/sh/a.sh
@@ -57,11 +57,28 @@ echo "generate log.leader.term ..."
 cat ${logpath}/*.main | grep "become leader" | grep -v "config change" | awk '{print $5,$0}' | awk -F, '{print $4"_"$0}' | sort -k1 > ${logpath}/log.leader.term
 
 echo ""
-echo "generate log.index ..."
+echo "generate log.index, log.snapshot, log.records, log.actions ..."
 for file in `ls ${logpath}/log.dnode*vgId*`;do
-	destfile=${file}.index
-	echo "generate ${destfile}"
-	cat ${file} | awk '{ if(index($0, "write index:") > 0 || index($0, "wal truncate, from-index") > 0) {print $0} }' > ${destfile}
+	destfile1=${file}.index
+	echo "generate ${destfile1}"
+	cat ${file} | awk '{ if(index($0, "write index:") > 0 || index($0, "wal truncate, from-index") > 0) {print $0} }' > ${destfile1}
+
+	destfile2=${file}.snapshot
+	echo "generate ${destfile2}"
+	cat ${file} | awk '{ if(index($0, "snapshot sender") > 0 || index($0, "snapshot receiver") > 0) {print $0} }' | grep -v "save old" | grep -v "create new" | grep -v "udpate replicaIndex" | grep -v "delete old" | grep -v "reset for" > ${destfile2}
+
+	destfile3=${file}.records
+	echo "generate ${destfile3}"
+	cat ${file} | awk '{ if(index($0, "write index:") > 0 || index($0, "wal truncate, from-index") > 0 || index($0, "snapshot sender") > 0 || index($0, "snapshot receiver") > 0) {print $0} }' | grep -v "save old" | grep -v "create new" | grep -v "udpate replicaIndex" | grep -v "delete old" | grep -v "reset for" > ${destfile3}
+
+	destfile4=${file}.commit
+	echo "generate ${destfile4}"
+	cat ${file} | awk '{ if(index($0, "commit by") > 0) {print $0} }' > ${destfile4}
+
+	destfile5=${file}.actions
+	echo "generate ${destfile5}"
+	cat ${file} | awk '{ if(index($0, "commit by") > 0 || index($0, "sync open") > 0 || index($0, "sync close") > 0 || index($0, "become leader") > 0 || index($0, "write index:") > 0 || index($0, "wal truncate, from-index") > 0 || index($0, "snapshot sender") > 0 || index($0, "snapshot receiver") > 0) {print $0} }' | grep -v "save old" | grep -v "create new" | grep -v "udpate replicaIndex" | grep -v "delete old" | grep -v "reset for" > ${destfile5}
+
 done
 
 exit 0
