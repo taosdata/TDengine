d39efbfe05af9ca18e1b2c03a7e990fd0f029bb4
dingbo <dingbo8128@163.com>
2022-07-22 15:55:32 +0800

    test: taostools.py

--- cases/cloud/taostools.py
+++ cases/cloud/taostools.py
@@ -1,8 +1,9 @@
 """
-taosBenchMark 功能性测试。 因为是测试 taosBenchMark 工具本身，所以尽量不给云服务实例性能压力
+taosBenchmark 和 taosdump 功能性测试。 因为是测试工具本身，所以尽量不给云服务实例性能压力
 1. 测试与云服务的连通性
 2. 建表和插入数据
-启动命令： tt --case=cloud/benchmark.py --use=cloud_test.yaml
+3. 导出数据测试
+启动命令： tt --case=cloud/taostools.py --use=cloud_test.yaml
 """
 
 from subprocess import run
@@ -11,22 +12,37 @@ from taostest import TDCase, T
 from taostest.util.cloud_sql import CloudSql
 
 from .cloud_case import CloudCase
+import os
+import shutil
 
 
-class TaosBenchMark(TDCase, CloudCase):
+class TaosTools(TDCase, CloudCase):
     def init(self):
         self.set_env()
         self.cql = CloudSql()
 
     def run(self):
+        self.test_taos_benchmark()
+        self.test_taos_dump()
+
+    def test_taos_benchmark(self):
         # 创建 1 张表，写 records 条数据
         records = 10
         run(['taosBenchmark', '--database', 'benchmark', '-T', '1', '-t', '1', '--records', str(records), '-y'], check=True, timeout=20)
         count = self.cql.count("benchmark", "meters")
         assert count == records
 
+    def test_taos_dump(self):
+        try:
+            os.mkdir('dumped_data')
+        except:
+            self.cleanup()
+            os.mkdir('dumped_data')
+
+        run(['taosdump', '-D', 'benchmark', '-o', 'dumped_data'], check=True, timeout=20)
+
     def cleanup(self):
-        pass
+        shutil.rmtree("./dumped_data", ignore_errors=True)
 
     def desc(self) -> str:
         return "Test basic function of taosBenchmark"
@@ -35,4 +51,4 @@ class TaosBenchMark(TDCase, CloudCase):
         return "DingBo"
 
     def tags(self):
-        return T.Cloud.Toos.TaosBenchmark
+        return T.Cloud.Toos.TaosBenchmark, T.Cloud.Toos.TaosDump
