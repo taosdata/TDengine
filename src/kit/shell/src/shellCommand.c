6171efdb3569aff5457a2ab115a288414b0571cb
Alex Duan <417921451@qq.com>
2022-07-23 16:54:27 +0800

    feat(shell): autotab calc position changed

--- src/kit/shell/src/shellCommand.c
+++ src/kit/shell/src/shellCommand.c
@@ -79,8 +79,11 @@ void insertChar(Command *cmd, char *c, int size) {
   /* update the values */
   cmd->commandSize += size;
   cmd->cursorOffset += size;
-  cmd->screenOffset += wcwidth(wc);
-  cmd->endOffset += wcwidth(wc);  
+  for (int i = 0; i < size; i++) {
+    mbtowc(&wc, c + i, size);
+    cmd->screenOffset += wcwidth(wc);
+    cmd->endOffset += wcwidth(wc);
+  } 
   showOnScreen(cmd);
 }
 
