3d42a4ec49b06efa4706e975894eeeba3978c9df
“happyguoxy” <happy_guoxy@163.com>
2022-07-28 15:46:37 +0800

    test: refine query cases

--- cases/Query/queryscript/table_query/table_delete.py
+++ cases/Query/queryscript/table_query/table_delete.py
@@ -45,10 +45,23 @@ class TDTestQuery(TDCase):
         #os.system("rm -rf %s/%s.sql" % (self.testcasePath,self.testcaseFilename))    
         os.system("touch %s/%s.sql" % (self.testcasePath,self.testcaseFilename))  
         self.tdCreateData.dropandcreateDB_random("%s" % db, 1) 
-
+        
+    def drop_db(self,database):
+        #delete:
+        table_list = ['stable_1','stable_2','regular_table_1','stable_1_1','regular_table_2']
+        for i in table_list:
+            self.tdSql.execute("delete from {}.{};".format(database, i))
+            self.tdSql.execute("flush database {};".format(database))
+            self.tdSql.execute("reset query cache;")
+            self.tdSql.query("select * from {}.{};".format(database, i))
+            self.tdSql.checkRow(0)
+        
+        #drop:
+        self.tdSql.execute('''drop database if exists %s ;''' %database)
+        
     def rm_sql(self):
         os.system("rm -rf %s/%s.sql" % (self.testcasePath,self.testcaseFilename)) 
-        self.tdCreateData.drop_db("%s" % self.db)  
+        self.drop_db("%s" % self.db)  
                 
     def run(self)-> bool:
         startTime = time.time() 
