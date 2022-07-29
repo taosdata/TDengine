344e821e87e776f8feeaf07fba86f139d70a6568
Ganlin Zhao <ganlinzhao@hotmail.com>
2022-07-29 20:53:45 +0800

    fix: fix rsma writing historical data wal got overwritten by unrelated SSDatablocks got from streamscan
    TD- 17958

--- source/libs/executor/src/timewindowoperator.c
+++ source/libs/executor/src/timewindowoperator.c
@@ -1597,7 +1597,7 @@ static SSDataBlock* doStreamIntervalAgg(SOperatorInfo* pOperator) {
       continue;
     }
 
-    if (pBlock->info.type == STREAM_NORMAL) {
+    if (pBlock->info.type == STREAM_NORMAL && pBlock->info.version != 0) {
       // set input version
       pTaskInfo->version = pBlock->info.version;
     }
