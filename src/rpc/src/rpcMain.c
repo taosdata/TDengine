a21323996dd22b9eba88a714fa73764f42286b1a
Alex Duan <417921451@qq.com>
2022-08-03 18:49:05 +0800

    first build

--- src/rpc/src/rpcMain.c
+++ src/rpc/src/rpcMain.c
@@ -12,7 +12,6 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
 #include "os.h"
 #include "tidpool.h"
 #include "tmd5.h"
