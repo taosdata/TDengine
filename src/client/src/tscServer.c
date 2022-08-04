7ac8dc9e39a67d4a57e512cd725b40daff5c230c
Alex Duan <417921451@qq.com>
2022-08-04 09:53:36 +0800

    feat(rpc): probe msg pass PR test

--- src/client/src/tscServer.c
+++ src/client/src/tscServer.c
@@ -400,7 +400,7 @@ void tscProcessActivityTimer(void *handle, void *tmrId) {
     // call check if have query doing
     if(pObj->sqlList) {
       // have queries executing
-      checkBrokenQueries(pObj);
+      //checkBrokenQueries(pObj);
     }
   }
 
