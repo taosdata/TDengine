e49e9bee6a2465f93c5f355e17e2dc4de3d0d9e8
Cary Xu <klxu@taosdata.com>
2022-08-08 20:59:11 +0800

    other: revert clang format

--- source/libs/parser/src/parInsertData.c
+++ source/libs/parser/src/parInsertData.c
@@ -12,7 +12,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-// clang-format on
+// clang-format off
 #include "parInsertData.h"
 
 #include "catalog.h"
