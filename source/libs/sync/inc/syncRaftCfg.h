8328aae50f2184ce33100f05b3487240959c4944
Minghao Li <castermode@gmail.com>
2022-08-11 15:22:44 +0800

    refactor(sync): add raft config index test

--- source/libs/sync/inc/syncRaftCfg.h
+++ source/libs/sync/inc/syncRaftCfg.h
@@ -31,6 +31,29 @@ extern "C" {
 
 #define MAX_CONFIG_INDEX_COUNT 512
 
+// SRaftCfgIndex ------------------------------------------
+typedef struct SRaftCfgIndex {
+  TdFilePtr pFile;
+  char      path[TSDB_FILENAME_LEN * 2];
+
+  SyncIndex configIndexArr[MAX_CONFIG_INDEX_COUNT];
+  int32_t   configIndexCount;
+} SRaftCfgIndex;
+
+SRaftCfgIndex *raftCfgIndexOpen(const char *path);
+int32_t        raftCfgIndexClose(SRaftCfgIndex *pRaftCfgIndex);
+int32_t        raftCfgIndexPersist(SRaftCfgIndex *pRaftCfgIndex);
+int32_t        raftCfgIndexAddConfigIndex(SRaftCfgIndex *pRaftCfgIndex, SyncIndex configIndex);
+
+cJSON  *raftCfgIndex2Json(SRaftCfgIndex *pRaftCfgIndex);
+char   *raftCfgIndex2Str(SRaftCfgIndex *pRaftCfgIndex);
+int32_t raftCfgIndexFromJson(const cJSON *pRoot, SRaftCfgIndex *pRaftCfgIndex);
+int32_t raftCfgIndexFromStr(const char *s, SRaftCfgIndex *pRaftCfgIndex);
+
+int32_t raftCfgIndexCreateFile(const char *path);
+
+// ---------------------------------------------------------
+
 typedef struct SRaftCfg {
   SSyncCfg  cfg;
   TdFilePtr pFile;
@@ -50,14 +73,14 @@ int32_t   raftCfgClose(SRaftCfg *pRaftCfg);
 int32_t   raftCfgPersist(SRaftCfg *pRaftCfg);
 int32_t   raftCfgAddConfigIndex(SRaftCfg *pRaftCfg, SyncIndex configIndex);
 
-cJSON * syncCfg2Json(SSyncCfg *pSyncCfg);
-char *  syncCfg2Str(SSyncCfg *pSyncCfg);
-char *  syncCfg2SimpleStr(SSyncCfg *pSyncCfg);
+cJSON  *syncCfg2Json(SSyncCfg *pSyncCfg);
+char   *syncCfg2Str(SSyncCfg *pSyncCfg);
+char   *syncCfg2SimpleStr(SSyncCfg *pSyncCfg);
 int32_t syncCfgFromJson(const cJSON *pRoot, SSyncCfg *pSyncCfg);
 int32_t syncCfgFromStr(const char *s, SSyncCfg *pSyncCfg);
 
-cJSON * raftCfg2Json(SRaftCfg *pRaftCfg);
-char *  raftCfg2Str(SRaftCfg *pRaftCfg);
+cJSON  *raftCfg2Json(SRaftCfg *pRaftCfg);
+char   *raftCfg2Str(SRaftCfg *pRaftCfg);
 int32_t raftCfgFromJson(const cJSON *pRoot, SRaftCfg *pRaftCfg);
 int32_t raftCfgFromStr(const char *s, SRaftCfg *pRaftCfg);
 
@@ -81,6 +104,11 @@ void raftCfgPrint(SRaftCfg *pCfg);
 void raftCfgPrint2(char *s, SRaftCfg *pCfg);
 void raftCfgLog(SRaftCfg *pCfg);
 void raftCfgLog2(char *s, SRaftCfg *pCfg);
+
+void raftCfgIndexPrint(SRaftCfgIndex *pCfg);
+void raftCfgIndexPrint2(char *s, SRaftCfgIndex *pCfg);
+void raftCfgIndexLog(SRaftCfgIndex *pCfg);
+void raftCfgIndexLog2(char *s, SRaftCfgIndex *pCfg);
 
 #ifdef __cplusplus
 }
