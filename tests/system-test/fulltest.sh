a270c989d2e51861a2c25def7776b009c246f21c
Ganlin Zhao <ganlinzhao@hotmail.com>
2022-08-11 13:57:23 +0800

    test: add interp test cases

--- tests/system-test/fulltest.sh
+++ tests/system-test/fulltest.sh
@@ -96,6 +96,8 @@ python3 ./test.py -f 2-query/distribute_agg_stddev.py
 python3 ./test.py -f 2-query/distribute_agg_stddev.py -R
 python3 ./test.py -f 2-query/distribute_agg_sum.py
 python3 ./test.py -f 2-query/distribute_agg_sum.py -R
+python3 ./test.py -f 2-query/interp.py
+python3 ./test.py -f 2-query/interp.py -R
 
 
 
@@ -338,6 +340,7 @@ python3 ./test.py -f 2-query/arcsin.py -Q 2
 python3 ./test.py -f 2-query/arccos.py -Q 2
 python3 ./test.py -f 2-query/arctan.py -Q 2
 python3 ./test.py -f 2-query/query_cols_tags_and_or.py  -Q 2
+python3 ./test.py -f 2-query/interp.py  -Q 2
 
 # python3 ./test.py -f 2-query/nestedQuery.py  -Q 2
 # python3 ./test.py -f 2-query/nestedQuery_str.py  -Q 2
@@ -457,3 +460,4 @@ python3 ./test.py -f 2-query/max_partition.py -Q 3
 python3 ./test.py -f 2-query/last_row.py -Q 3
 python3 ./test.py -f 2-query/tsbsQuery.py -Q 3
 python3 ./test.py -f 2-query/sml.py -Q 3
+python3 ./test.py -f 2-query/interp.py -Q 3
