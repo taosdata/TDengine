1dd53e9e415556dde146e92b03f26544e8273b7a
Cary Xu <klxu@taosdata.com>
2022-08-27 23:01:19 +0800

    fix: return data ptr when iterate remove for simple hash

--- source/libs/executor/src/tsimplehash.c
+++ source/libs/executor/src/tsimplehash.c
@@ -295,7 +295,11 @@ int32_t tSimpleHashIterateRemove(SSHashObj *pHashObj, const void *key, size_t ke
       }
 
       if (*pIter == (void *)GET_SHASH_NODE_DATA(pNode)) {
-        *pIter = pPrev;
+        if (!pPrev) {
+          *pIter = NULL;
+        } else {
+          *pIter = GET_SHASH_NODE_DATA(pPrev);
+        }
       }
 
       FREE_HASH_NODE(pNode);
