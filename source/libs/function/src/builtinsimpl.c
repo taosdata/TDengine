e5f5d3710dc86ffae0baa930a6f85b1f51078e5f
Haojun Liao <hjliao@taosdata.com>
2022-08-30 12:48:06 +0800

    fix(query): set  correct value length

--- source/libs/function/src/builtinsimpl.c
+++ source/libs/function/src/builtinsimpl.c
@@ -3627,9 +3627,8 @@ static int32_t doUpdateTupleData(SSerializeDataHandle* pHandle, const void* pBuf
 }
 
 static int32_t updateTupleData(SqlFunctionCtx* pCtx, int32_t rowIndex, const SSDataBlock* pSrcBlock, STuplePos* pPos) {
-  int32_t completeRowSize = pCtx->subsidiaries.rowLen + pCtx->subsidiaries.num * sizeof(bool);
   char* buf = serializeTupleData(pSrcBlock, rowIndex, &pCtx->subsidiaries, pCtx->subsidiaries.buf);
-  doUpdateTupleData(&pCtx->saveHandle, buf, completeRowSize, pPos);
+  doUpdateTupleData(&pCtx->saveHandle, buf, pCtx->subsidiaries.rowLen, pPos);
   return TSDB_CODE_SUCCESS;
 }
 
