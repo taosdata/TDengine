f4ddc6d39365c9963fb47ee4272da5d1626b52be
cpwu <cpwu@taosdata.com>
2022-08-30 14:14:37 +0800

    fix sml case

--- tests/system-test/2-query/sml.py
+++ tests/system-test/2-query/sml.py
@@ -86,7 +86,7 @@ class TDTestCase:
         tdSql.query(f"select * from {dbname}.macylr")
         tdSql.checkRows(2)
 
-        tdSql.query("desc macylr")
+        tdSql.query(f"desc {dbname}.macylr")
         tdSql.checkRows(25)
         return
 
