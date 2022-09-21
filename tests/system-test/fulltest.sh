29042f74eb882e694d435b906ba39532bcbaef27
“happyguoxy” <happy_guoxy@163.com>
2022-09-21 09:52:06 +0800

    test: add pre_suf cases

--- tests/system-test/fulltest.sh
+++ tests/system-test/fulltest.sh
@@ -35,6 +35,7 @@ python3 ./test.py -f 1-insert/mutil_stage.py
 python3 ./test.py -f 1-insert/table_param_ttl.py -R
 python3 ./test.py -f 1-insert/update_data_muti_rows.py
 python3 ./test.py -f 1-insert/db_tb_name_check.py
+python3 ./test.py -f 1-insert/database_pre_suf.py
 
 python3 ./test.py -f 2-query/abs.py
 python3 ./test.py -f 2-query/abs.py -R
