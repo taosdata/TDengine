e9f30db0ab64b2d150e2d97a2da5f9ed7929bef1
Ganlin Zhao <ganlinzhao@hotmail.com>
2022-10-08 18:28:09 +0800

    fix(query): fix infinite interpolation results generated when sliding window is smaller than interval window in descending time order

--- source/libs/executor/src/tfill.c
+++ source/libs/executor/src/tfill.c
@@ -583,7 +583,7 @@ int64_t getNumOfResultsAfterFillGap(SFillInfo* pFillInfo, TSKEY ekey, int32_t ma
     numOfRes += 1;
     assert(numOfRes >= numOfRows);
   } else {  // reach the end of data
-    if ((ekey1 <= pFillInfo->currentKey && FILL_IS_ASC_FILL(pFillInfo)) ||
+    if ((ekey1 < pFillInfo->currentKey && FILL_IS_ASC_FILL(pFillInfo)) ||
         (ekey1 >= pFillInfo->currentKey && !FILL_IS_ASC_FILL(pFillInfo))) {
       return 0;
     }
