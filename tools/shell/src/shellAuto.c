ce382599797bda54254b8d2de76870e125405eda
Alex Duan <417921451@qq.com>
2022-10-08 18:58:07 +0800

    feat(shell): fixed windows build error.

--- tools/shell/src/shellAuto.c
+++ tools/shell/src/shellAuto.c
@@ -1402,7 +1402,7 @@ char * lastWord(char * p) {
   char * p2 = strrchr(p, ',');
 
   if (p1 && p2) {
-    return MAX(p1, p2) + 1;
+    return p1 > p2 ? p1 : p2 + 1;
   } else if (p1) {
     return p1 + 1;
   } else if(p2) {
