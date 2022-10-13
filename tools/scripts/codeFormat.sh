63d9b85631132732c3ae5e984e85f5367515c509
Hongze Cheng <hzcheng@taosdata.com>
2022-10-13 13:59:18 +0800

    more code format

--- tools/scripts/codeFormat.sh
+++ tools/scripts/codeFormat.sh
@@ -8,35 +8,42 @@ ORIGIN_DIR=$(pwd)
 cd ${PRJ_ROOT_DIR}
 
 FORMAT_DIR_LIST=(
-    "include"
-    "source/os"
-    "source/util"
-    "source/common"
-    "source/libs/cache"
-    "source/libs/catalog"
-    "source/libs/command"
-    "source/libs/executor"
-    # "source/libs/function"
-    # "source/libs/index"
-    # "source/libs/monitor"
-    # "source/libs/nodes"
-    # "source/libs/parser"
-    # "source/libs/planner"
-    # "source/libs/qcom"
-    # "source/libs/qworker"
-    # "source/libs/scalar"
-    # "source/libs/stream"
-    # "source/libs/sync"
-    "source/libs/tdb"
-    "source/libs/tfs"
-    # "source/libs/transport"
-    "source/libs/wal"
-    # "source/client"
-    "source/dnode"
+    "${PRJ_ROOT_DIR}/include"
+    "${PRJ_ROOT_DIR}/source/os"
+    "${PRJ_ROOT_DIR}/source/util"
+    "${PRJ_ROOT_DIR}/source/common"
+    "${PRJ_ROOT_DIR}/source/libs/cache"
+    "${PRJ_ROOT_DIR}/source/libs/catalog"
+    "${PRJ_ROOT_DIR}/source/libs/command"
+    "${PRJ_ROOT_DIR}/source/libs/executor"
+    # "${PRJ_ROOT_DIR}/source/libs/function"
+    "${PRJ_ROOT_DIR}/source/libs/index"
+    "${PRJ_ROOT_DIR}/source/libs/monitor"
+    "${PRJ_ROOT_DIR}/source/libs/nodes"
+    # "${PRJ_ROOT_DIR}/source/libs/parser"
+    "${PRJ_ROOT_DIR}/source/libs/planner"
+    # "${PRJ_ROOT_DIR}/source/libs/qcom"
+    # "${PRJ_ROOT_DIR}/source/libs/qworker"
+    # "${PRJ_ROOT_DIR}/source/libs/scalar"
+    "${PRJ_ROOT_DIR}/source/libs/stream"
+    # "${PRJ_ROOT_DIR}/source/libs/sync"
+    "${PRJ_ROOT_DIR}/source/libs/tdb"
+    "${PRJ_ROOT_DIR}/source/libs/tfs"
+    "${PRJ_ROOT_DIR}/source/libs/transport"
+    "${PRJ_ROOT_DIR}/source/libs/wal"
+    # "${PRJ_ROOT_DIR}/source/client"
+    "${PRJ_ROOT_DIR}/source/dnode"
+)
+
+EXCLUDE_DIR_LIST=(
+)
+
+EXCLUDE_FILE_LIST=(
+    "source/libs/parser/sql.c"
 )
 
 for d in ${FORMAT_DIR_LIST[@]}; do
-    for f in $(find ${PRJ_ROOT_DIR}/$d/ -regex '.*\.\(cpp\|hpp\|c\|h\)'); do
+    for f in $(find $d -regex '.*\.\(cpp\|hpp\|c\|h\)'); do
         ${FORMAT_BIN} -i $f
     done
 done
