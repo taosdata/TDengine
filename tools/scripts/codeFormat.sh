5f010843a654d04d9420d496a86ec7a0170dd8f0
Hongze Cheng <hzcheng@taosdata.com>
2022-10-13 16:58:43 +0800

    more code format

--- tools/scripts/codeFormat.sh
+++ tools/scripts/codeFormat.sh
@@ -12,25 +12,7 @@ FORMAT_DIR_LIST=(
     "${PRJ_ROOT_DIR}/source/os"
     "${PRJ_ROOT_DIR}/source/util"
     "${PRJ_ROOT_DIR}/source/common"
-    "${PRJ_ROOT_DIR}/source/libs/cache"
-    "${PRJ_ROOT_DIR}/source/libs/catalog"
-    "${PRJ_ROOT_DIR}/source/libs/command"
-    "${PRJ_ROOT_DIR}/source/libs/executor"
-    "${PRJ_ROOT_DIR}/source/libs/function"
-    "${PRJ_ROOT_DIR}/source/libs/index"
-    "${PRJ_ROOT_DIR}/source/libs/monitor"
-    "${PRJ_ROOT_DIR}/source/libs/nodes"
-    # "${PRJ_ROOT_DIR}/source/libs/parser"
-    "${PRJ_ROOT_DIR}/source/libs/planner"
-    "${PRJ_ROOT_DIR}/source/libs/qcom"
-    "${PRJ_ROOT_DIR}/source/libs/qworker"
-    "${PRJ_ROOT_DIR}/source/libs/scalar"
-    "${PRJ_ROOT_DIR}/source/libs/stream"
-    "${PRJ_ROOT_DIR}/source/libs/sync"
-    "${PRJ_ROOT_DIR}/source/libs/tdb"
-    "${PRJ_ROOT_DIR}/source/libs/tfs"
-    "${PRJ_ROOT_DIR}/source/libs/transport"
-    "${PRJ_ROOT_DIR}/source/libs/wal"
+    "${PRJ_ROOT_DIR}/source/libs"
     "${PRJ_ROOT_DIR}/source/client/inc"
     "${PRJ_ROOT_DIR}/source/client/src"
     "${PRJ_ROOT_DIR}/source/client/test"
@@ -45,7 +27,7 @@ EXCLUDE_FILE_LIST=(
 )
 
 for d in ${FORMAT_DIR_LIST[@]}; do
-    for f in $(find $d -type f -regex '.*\.\(cpp\|hpp\|c\|h\)'); do
+    for f in $(find $d -type f -not -name "*/sql.c" -regex '.*\.\(cpp\|hpp\|c\|h\)'); do
         ${FORMAT_BIN} -i $f
     done
 done
