4cc94968ee09fb7dfa9d32fb7b4e0a346d3e5d81
Hongze Cheng <hzcheng@taosdata.com>
2022-10-13 11:09:43 +0800

    refact: format header file

---
+++ tools/scripts/codeFormat.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+PRJ_ROOT_DIR=$(readlink -f -- "$(dirname $0)/../..")
+FORMAT_BIN=clang-format-14
+
+ORIGIN_DIR=$(pwd)
+
+cd ${PRJ_ROOT_DIR}
+
+FORMAT_DIR_LIST=(
+    "include"
+    # "source"
+)
+
+for d in ${FORMAT_DIR_LIST[@]}; do
+    for f in $(find ${PRJ_ROOT_DIR}/$d/ -regex '.*\.\(cpp\|hpp\|c\|h\)'); do
+        ${FORMAT_BIN} -i $f
+    done
+done
+
+cd ${ORIGIN_DIR}
