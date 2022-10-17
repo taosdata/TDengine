cf7b9646d009bb98d956873b4c0833d93caf68f2
Shuduo Sang <sangshuduo@gmail.com>
2022-10-17 11:22:51 +0800

    docs: remove tb variable after get_table_name removed

--- docs/examples/python/tmq_example.py
+++ docs/examples/python/tmq_example.py
@@ -7,14 +7,18 @@ print("init")
 conn.execute("drop database if exists py_tmq")
 conn.execute("create database if not exists py_tmq vgroups 2")
 conn.select_db("py_tmq")
-conn.execute("create stable if not exists stb1 (ts timestamp, c1 int, c2 float, c3 binary(10)) tags(t1 int)")
+conn.execute(
+    "create stable if not exists stb1 (ts timestamp, c1 int, c2 float, c3 binary(10)) tags(t1 int)"
+)
 conn.execute("create table if not exists tb1 using stb1 tags(1)")
 conn.execute("create table if not exists tb2 using stb1 tags(2)")
 conn.execute("create table if not exists tb3 using stb1 tags(3)")
 
 print("create topic")
 conn.execute("drop topic if exists topic_ctb_column")
-conn.execute("create topic if not exists topic_ctb_column as select ts, c1, c2, c3 from stb1")
+conn.execute(
+    "create topic if not exists topic_ctb_column as select ts, c1, c2, c3 from stb1"
+)
 
 print("build consumer")
 conf = TaosTmqConf()
@@ -23,9 +27,11 @@ conf.set("td.connect.user", "root")
 conf.set("td.connect.pass", "taosdata")
 conf.set("enable.auto.commit", "true")
 
+
 def tmq_commit_cb_print(tmq, resp, offset, param=None):
     print(f"commit: {resp}, tmq: {tmq}, offset: {offset}, param: {param}")
-    
+
+
 conf.set_auto_commit_cb(tmq_commit_cb_print, None)
 tmq = conf.new_consumer()
 
@@ -39,7 +45,7 @@ tmq.subscribe(topic_list)
 
 sub_list = tmq.subscription()
 
-print("subscribed topics: ",sub_list)
+print("subscribed topics: ", sub_list)
 
 while 1:
     res = tmq.poll(1000)
@@ -50,7 +56,3 @@ while 1:
         print(f"topic: {topic}\nvgroup id: {vg}\ndb: {db}")
         for row in res:
             print(row)
-            print(f"from table: {tb}")
-    
-
-
