c6956061847a3fc4297132d20cf811392e37d489
Ganlin Zhao <ganlinzhao@hotmail.com>
2022-10-17 10:59:32 +0800

    fix: coverity issues
    CID: 399357

--- source/common/src/tdataformat.c
+++ source/common/src/tdataformat.c
@@ -602,7 +602,7 @@ _exit:
 }
 
 int32_t tPutTSRow(uint8_t *p, STSRow2 *pRow) {
-  int32_t n;
+  int32_t n = 0;
 
   TSROW_LEN(pRow, n);
   if (p) {
@@ -613,7 +613,7 @@ int32_t tPutTSRow(uint8_t *p, STSRow2 *pRow) {
 }
 
 int32_t tGetTSRow(uint8_t *p, STSRow2 **ppRow) {
-  int32_t n;
+  int32_t n = 0;
 
   *ppRow = (STSRow2 *)p;
   TSROW_LEN(*ppRow, n);
