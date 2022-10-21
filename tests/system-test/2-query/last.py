d4f678f0d65241521939d2182f6e347cab474820
jiajingbin <jiajaybin@126.com>
2022-10-21 19:10:44 +0800

    test: add cachemodel for last

--- tests/system-test/2-query/last.py
+++ tests/system-test/2-query/last.py
@@ -10,7 +10,7 @@ import numpy as np
 class TDTestCase:
     def init(self, conn, logSql):
         tdLog.debug("start to execute %s" % __file__)
-        tdSql.init(conn.cursor(), log=True)
+        tdSql.init(conn.cursor())
 
         self.rowNum = 10
         self.tbnum = 20
