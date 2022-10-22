e5be6663c8faa17a52bac63ef926e6c149dae017
yihaoDeng <luomoxyz@126.com>
2022-10-22 23:06:41 +0800

    avoid invalid read/write

--- source/libs/executor/src/scanoperator.c
+++ source/libs/executor/src/scanoperator.c
@@ -2987,7 +2987,7 @@ static int32_t sysFilte__TableName(void* arg, SNode* pNode, SArray* result) {
   int32_t ret = metaFilterCreateTime(pMeta, &param, result);
   if (ret == 0) return 0;
 
-  return -1;
+  return -2;
 }
 
 static int32_t sysFilte__CreateTime(void* arg, SNode* pNode, SArray* result) {
@@ -3016,8 +3016,8 @@ static int32_t sysFilte__Ncolumn(void* arg, SNode* pNode, SArray* result) {
   bool           reverse = false;
 
   __optSysFilter func = optSysGetFilterFunc(pOper->opType, &reverse);
-  if (func == NULL) return -1;
-  return 0;
+  if (func == NULL) return -2;
+  return -2;
 }
 
 static int32_t sysFilte__Ttl(void* arg, SNode* pNode, SArray* result) {
@@ -3028,8 +3028,8 @@ static int32_t sysFilte__Ttl(void* arg, SNode* pNode, SArray* result) {
   bool           reverse = false;
 
   __optSysFilter func = optSysGetFilterFunc(pOper->opType, &reverse);
-  if (func == NULL) return -1;
-  return 0;
+  if (func == NULL) return -2;
+  return -2;
 }
 static int32_t sysFilte__STableName(void* arg, SNode* pNode, SArray* result) {
   void* pMeta = ((SSTabFltArg*)arg)->pMeta;
@@ -3039,7 +3039,7 @@ static int32_t sysFilte__STableName(void* arg, SNode* pNode, SArray* result) {
   bool           reverse = false;
 
   __optSysFilter func = optSysGetFilterFunc(pOper->opType, &reverse);
-  if (func == NULL) return -1;
+  if (func == NULL) return -2;
   return 0;
 }
 static int32_t sysFilte__Uid(void* arg, SNode* pNode, SArray* result) {
@@ -3050,8 +3050,8 @@ static int32_t sysFilte__Uid(void* arg, SNode* pNode, SArray* result) {
   bool           reverse = false;
 
   __optSysFilter func = optSysGetFilterFunc(pOper->opType, &reverse);
-  if (func == NULL) return -1;
-  return 0;
+  if (func == NULL) return -2;
+  return -2;
 }
 static int32_t sysFilte__Type(void* arg, SNode* pNode, SArray* result) {
   void* pMeta = ((SSTabFltArg*)arg)->pMeta;
@@ -3061,8 +3061,8 @@ static int32_t sysFilte__Type(void* arg, SNode* pNode, SArray* result) {
   bool           reverse = false;
 
   __optSysFilter func = optSysGetFilterFunc(pOper->opType, &reverse);
-  if (func == NULL) return -1;
-  return 0;
+  if (func == NULL) return -2;
+  return -2;
 }
 static int32_t sysChkFilter__Comm(SNode* pNode) {
   // impl
@@ -3070,7 +3070,7 @@ static int32_t sysChkFilter__Comm(SNode* pNode) {
   EOperatorType  opType = pOper->opType;
   if (opType != OP_TYPE_EQUAL && opType != OP_TYPE_LOWER_EQUAL && opType != OP_TYPE_LOWER_THAN &&
       OP_TYPE_GREATER_EQUAL && opType != OP_TYPE_GREATER_THAN) {
-    return -1;
+    return -2;
   }
   return 0;
 }
