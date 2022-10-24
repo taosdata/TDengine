fddd6657bc1ce9731c613f60ee7779a41339dca4
Minghao Li <castermode@gmail.com>
2022-10-24 14:51:45 +0800

    fix(sync): add error log when pState is NULL

--- source/libs/sync/src/syncMain.c
+++ source/libs/sync/src/syncMain.c
@@ -337,6 +337,7 @@ char* syncNodePeerState2Str(const SSyncNode* pSyncNode) {
   for (int32_t i = 0; i < pSyncNode->replicaNum; ++i) {
     SPeerState* pState = syncNodeGetPeerState((SSyncNode*)pSyncNode, &(pSyncNode->replicasId[i]));
     if (pState == NULL) {
+      sError("vgId:%d, replica maybe dropped", pSyncNode->vgId);
       break;
     }
 
@@ -3497,6 +3498,7 @@ SPeerState* syncNodeGetPeerState(SSyncNode* ths, const SRaftId* pDestId) {
 bool syncNodeNeedSendAppendEntries(SSyncNode* ths, const SRaftId* pDestId, const SyncAppendEntries* pMsg) {
   SPeerState* pState = syncNodeGetPeerState(ths, pDestId);
   if (pState == NULL) {
+    sError("vgId:%d, replica maybe dropped", ths->vgId);
     return false;
   }
 
