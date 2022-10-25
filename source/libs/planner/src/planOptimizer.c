c38b2001317a3771652d6d1a28bafd1b31b6a928
Xiaoyu Wang <xiaoyuwang@taosdata.com>
2022-10-25 11:08:56 +0800

    fix: wrong permissions of 'show variables' command

--- source/libs/planner/src/planOptimizer.c
+++ source/libs/planner/src/planOptimizer.c
@@ -2477,7 +2477,7 @@ static const SOptimizeRule optimizeRuleSet[] = {
   {.pName = "RewriteUnique",              .optimizeFunc = rewriteUniqueOptimize},
   {.pName = "LastRowScan",                .optimizeFunc = lastRowScanOptimize},
   {.pName = "TagScan",                    .optimizeFunc = tagScanOptimize},
-  {.pName = "PushDownLimit",              .optimizeFunc = pushDownLimitOptimize}
+  // {.pName = "PushDownLimit",              .optimizeFunc = pushDownLimitOptimize}
 };
 // clang-format on
 
