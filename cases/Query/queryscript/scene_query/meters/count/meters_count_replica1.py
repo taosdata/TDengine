60d359c7b938d748d8b8c37bf24819a4d05140bc
“happyguoxy” <happy_guoxy@163.com>
2022-11-01 11:37:30 +0800

    test: refine query cases

---
+++ cases/Query/queryscript/scene_query/meters/count/meters_count_replica1.py
@@ -0,0 +1,46 @@
+###################################################################
+#           Copyright (c) 2016 by TAOS Technologies, Inc.
+#                     All rights reserved.
+#
+#  This file is proprietary and confidential to TAOS Technologies.
+#  No part of this file may be reproduced, stored, transmitted,
+#  disclosed or used in any form or by any means other than as
+#  expressly provided by the written permission from Jianhui Tao
+#
+###################################################################
+
+# -*- coding: utf-8 -*-
+
+from Query.queryscript.scene_query.meters.meters_bigdata_common import *
+
+class TDTestQuery(TDTestQuery):
+
+    def tags(self) :
+	
+        return ""
+
+    def author(self) -> str:
+
+        return "Guo Xiangyang"
+
+    def desc(self) -> str:
+        case_description = '''
+        case1:# meters all query
+        '''
+        return case_description        
+                                           
+    def run(self):
+        startTime = time.time() 
+        
+        self.countdb_1w_table100_row100(replica=1)
+        self.countdb_2w_table100_row200(replica=1)
+        self.countdb_10w_table100_row1000(replica=1)
+        #self.countdb_1000w_table1w_row1000(replica=1)
+        
+        
+        endTime = time.time()
+        
+    
+        self.logger.info("total time %ds" % (endTime - startTime))
+    
+
