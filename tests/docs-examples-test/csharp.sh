6ea4cc974f9c1b60188fea8774063ffdfa9a8a99
xleili <xlli@taosdata.com>
2022-11-04 15:35:35 +0800

    test(driver)C# CI failed on 3.0

--- tests/docs-examples-test/csharp.sh
+++ tests/docs-examples-test/csharp.sh
@@ -3,32 +3,32 @@
 set -e
 
 pgrep taosd || taosd 2>&1 >/dev/null &	
-pgrep taosadapter || taosadapter 2>&1 >/dev/null &	
+pgrep taosadapter || taosadapter --logLevel=debug 2>&1 >/dev/null &	
 jobs
 
 cd ../../docs/examples/csharp
-jobs
+
 dotnet run --project connect/connect.csproj
-jobs
+
 taos -s "drop database if exists power"
 dotnet run --project sqlInsert/sqlinsert.csproj
 dotnet run --project query/query.csproj
 dotnet run --project asyncQuery/asyncquery.csproj
 dotnet run --project subscribe/subscribe.csproj
-jobs
+
 taos -s "drop topic if exists topic_example"
 taos -s "drop database if exists power"
 dotnet run --project stmtInsert/stmtinsert.csproj
-jobs
+
 taos -s "drop database if exists test"
 dotnet run --project influxdbLine/influxdbline.csproj
-jobs
+
 taos -s "drop database if exists test"
 dotnet run --project optsTelnet/optstelnet.csproj
-jobs
+
 taos -s "drop database if exists test"
 dotnet run --project optsJSON/optsJSON.csproj
-jobs
+
 taos -s "create database if not exists test"
 jobs
 dotnet run --project wsConnect/wsConnect.csproj
