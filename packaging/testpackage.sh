c4f0c340f9d5690ab17d78f5130a816f7abb0775
chenhaoran <haoran920c@163.com>
2022-11-08 17:25:28 +0800

    test:modify two options that downloads the installPackages

--- packaging/testpackage.sh
+++ packaging/testpackage.sh
@@ -248,14 +248,19 @@ cd ${installPath}
 if [[ ${packgeName} =~ "Lite" ]]  ||   ([[ ${packgeName} =~ "x64" ]] && [[ ${packgeName} =~ "client" ]]) ||  ([[ ${packgeName} =~ "deb" ]] && [[ ${packgeName} =~ "server" ]])  || ([[ ${packgeName} =~ "rpm" ]] && [[ ${packgeName} =~ "server" ]]) ;then
     echoColor G "===== install taos-tools when package is lite or client ====="
     cd ${installPath}
-    wgetFile taosTools-2.1.3-Linux-x64.tar.gz v2.1.3 web
-    tar xf taosTools-2.1.3-Linux-x64.tar.gz  
+    if [ ! -f "taosTools-2.1.3-Linux-x64.tar.gz " ];then
+        wgetFile taosTools-2.1.3-Linux-x64.tar.gz v2.1.3 web
+        tar xf taosTools-2.1.3-Linux-x64.tar.gz  
+    fi
     cd taosTools-2.1.3 && bash install-taostools.sh
 elif  ([[ ${packgeName} =~ "arm64" ]] && [[ ${packgeName} =~ "client" ]]);then
     echoColor G "===== install taos-tools arm when package is arm64-client ====="
     cd ${installPath}
-    wgetFile taosTools-2.1.3-Linux-arm64.tar.gz  v2.1.3 web
-    tar xf taosTools-2.1.3-Linux-arm64.tar.gz
+    if [ ! -f "taosTools-2.1.3-Linux-x64.tar.gz " ];then
+        wgetFile taosTools-2.1.3-Linux-x64.tar.gz v2.1.3 web
+        tar xf taosTools-2.1.3-Linux-arm64.tar.gz
+    fi    
+    
     cd taosTools-2.1.3 && bash install-taostools.sh
 fi
 
