8475aaaaf1efd95b8cdc4928848ff3778e053ce2
Benguang Zhao <bgzhao@taosdata.com>
2022-11-28 13:48:57 +0800

    enh: add ASSERT info msg in tsdbStartCommit

--- source/dnode/vnode/src/tsdb/tsdbCommit.c
+++ source/dnode/vnode/src/tsdb/tsdbCommit.c
@@ -811,7 +811,7 @@ static int32_t tsdbStartCommit(STsdb *pTsdb, SCommitter *pCommitter) {
   int32_t lino = 0;
 
   memset(pCommitter, 0, sizeof(*pCommitter));
-  ASSERT(pTsdb->mem && pTsdb->imem == NULL);
+  ASSERT(pTsdb->mem && pTsdb->imem == NULL && "last tsdb commit incomplete");
 
   taosThreadRwlockWrlock(&pTsdb->rwLock);
   pTsdb->imem = pTsdb->mem;
