0b91b5b2b3d41b36754841575552b341de33476a
54liuyao <54liuyao@163.com>
2022-11-29 14:25:31 +0800

    fix:calculate the correct deletion range

--- source/libs/executor/src/tfill.c
+++ source/libs/executor/src/tfill.c
@@ -918,8 +918,11 @@ void setDeleteFillValueInfo(TSKEY start, TSKEY end, SStreamFillSupporter* pFillS
     return;
   }
 
+  TSKEY realStart = taosTimeAdd(pFillSup->prev.key, pFillSup->interval.sliding, pFillSup->interval.slidingUnit,
+                                pFillSup->interval.precision);
+
   pFillInfo->needFill = true;
-  pFillInfo->start = start;
+  pFillInfo->start = realStart;
   pFillInfo->current = pFillInfo->start;
   pFillInfo->end = end;
   pFillInfo->pos = FILL_POS_INVALID;
@@ -1418,9 +1421,13 @@ static void doDeleteFillResult(SOperatorInfo* pOperator) {
         if (code == TSDB_CODE_SUCCESS) {
           code = streamStateGetGroupKVByCur(pCur, &nextKey, (const void**)&nextVal, &nextLen);
         }
+        // ts will be deleted later
         if (delTs != ts) {
           streamStateFillDel(pOperator->pTaskInfo->streamInfo.pState, &delKey);
+          streamStateFreeCur(pCur);
+          pCur = streamStateGetAndCheckCur(pOperator->pTaskInfo->streamInfo.pState, &nextKey);
         }
+        endTs = nextKey.ts - 1;
         if (code != TSDB_CODE_SUCCESS) {
           break;
         }
