50883689e7bdaf579d75615bd61a03985bc456a7
Benguang Zhao <bgzhao@taosdata.com>
2022-12-07 15:16:05 +0800

    enh: check contiguousness of indexes applied to vnode tsdb

--- include/common/tmsg.h
+++ include/common/tmsg.h
@@ -70,6 +70,11 @@ static inline bool vnodeIsMsgBlock(tmsg_t type) {
   return (type == TDMT_VND_CREATE_TABLE) || (type == TDMT_VND_ALTER_TABLE) || (type == TDMT_VND_DROP_TABLE) ||
          (type == TDMT_VND_UPDATE_TAG_VAL);
 }
+
+static inline bool syncUtilUserCommit(tmsg_t msgType) {
+  return msgType != TDMT_SYNC_NOOP && msgType != TDMT_SYNC_LEADER_TRANSFER;
+}
+
 /* ------------------------ OTHER DEFINITIONS ------------------------ */
 // IE type
 #define TSDB_IE_TYPE_SEC         1
