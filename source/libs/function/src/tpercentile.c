ad69067a012a2336d591bdc6abcb84129e711c9d
Ganlin Zhao <ganlinzhao@hotmail.com>
2022-12-08 10:57:21 +0800

    fix asan stack-scope-after-use errror

--- source/libs/function/src/tpercentile.c
+++ source/libs/function/src/tpercentile.c
@@ -367,16 +367,18 @@ int32_t tMemBucketPut(tMemBucket *pBucket, const void *data, size_t size) {
         pSlot->info.data = NULL;
       }
 
-      SArray **pPageIdList = taosHashGet(pBucket->groupPagesMap, &groupId, sizeof(groupId));
-      if (pPageIdList == NULL) {
-        SArray *pList = taosArrayInit(4, sizeof(int32_t));
-        taosHashPut(pBucket->groupPagesMap, &groupId, sizeof(groupId), &pList, POINTER_BYTES);
-        pPageIdList = &pList;
+      SArray *pPageIdList;
+      void *p = taosHashGet(pBucket->groupPagesMap, &groupId, sizeof(groupId));
+      if (p == NULL) {
+        pPageIdList = taosArrayInit(4, sizeof(int32_t));
+        taosHashPut(pBucket->groupPagesMap, &groupId, sizeof(groupId), &pPageIdList, POINTER_BYTES);
+      } else {
+        pPageIdList = *(SArray **)p;
       }
 
       pSlot->info.data = getNewBufPage(pBucket->pBuffer, &pageId);
       pSlot->info.pageId = pageId;
-      taosArrayPush(*pPageIdList, &pageId);
+      taosArrayPush(pPageIdList, &pageId);
     }
 
     memcpy(pSlot->info.data->data + pSlot->info.data->num * pBucket->bytes, d, pBucket->bytes);
