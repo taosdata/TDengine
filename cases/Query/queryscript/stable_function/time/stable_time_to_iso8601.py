b50efb65200c2936db393f40bbf91dfcc80be0a0
“happyguoxy” <happy_guoxy@163.com>
2022-12-21 09:42:46 +0800

    test: refine query cases

--- cases/Query/queryscript/stable_function/time/stable_time_to_iso8601.py
+++ cases/Query/queryscript/stable_function/time/stable_time_to_iso8601.py
@@ -171,7 +171,9 @@ class TDTestQuery(TDCase):
         # self.tdSql.execute('''drop database if exists %s ;''' %self.db)
         
         num1 = sql.count('where')
-        self.logger.info("sqlnum1 %d" % num1) 
+        self.logger.info("sqlnum1 %d" % num1)  
+        cur1.close()
+        conn1.close() 
 
     def right_case_1_tbname(self):
         self.logger.info("\n==========================right case 1_tbname==========================\n")
@@ -242,7 +244,9 @@ class TDTestQuery(TDCase):
         # self.tdSql.execute('''drop database if exists %s ;''' %self.db)
         
         num1 = sql.count('where')
-        self.logger.info("sqlnum1_tbname %d" % num1)
+        self.logger.info("sqlnum1_tbname %d" % num1) 
+        cur1.close()
+        conn1.close() 
 
     def right_case_1(self):
         self.logger.info("\n==========================right case 1==========================\n")
@@ -298,7 +302,9 @@ class TDTestQuery(TDCase):
         # self.tdSql.execute('''drop database if exists %s ;''' %self.db)
         
         num1 = sql.count('where')
-        self.logger.info("sqlnum1_right %d" % num1) 
+        self.logger.info("sqlnum1_right %d" % num1)  
+        cur1.close()
+        conn1.close() 
                 
     def right_case_2_groupby(self):
         self.logger.info("\n==========================right case 2_groupby==========================\n")
@@ -389,7 +395,9 @@ class TDTestQuery(TDCase):
         # self.tdSql.execute('''drop database if exists %s ;''' %self.db)
         
         num2 = sql.count('where')
-        self.logger.info("sqlnum2 %d" % num2) 
+        self.logger.info("sqlnum2 %d" % num2)  
+        cur1.close()
+        conn1.close() 
         
     def right_case_2_tbname(self):
         self.logger.info("\n==========================right case 2_tbname==========================\n")
@@ -559,7 +567,9 @@ class TDTestQuery(TDCase):
         # self.tdSql.execute('''drop database if exists %s ;''' %self.db)
         
         num2 = sql.count('where')
-        self.logger.info("sqlnum2_tbname %d" % num2) 
+        self.logger.info("sqlnum2_tbname %d" % num2)  
+        cur1.close()
+        conn1.close() 
 
     def right_case_2(self):
         self.logger.info("\n==========================right case 2==========================\n")
@@ -683,7 +693,9 @@ class TDTestQuery(TDCase):
         # self.tdSql.execute('''drop database if exists %s ;''' %self.db)
         
         num2 = sql.count('where')
-        self.logger.info("sqlnum2_right %d" % num2) 
+        self.logger.info("sqlnum2_right %d" % num2)  
+        cur1.close()
+        conn1.close() 
         
                                
     def right_case_3_groupby(self):
@@ -732,7 +744,9 @@ class TDTestQuery(TDCase):
         # self.tdSql.execute('''drop database if exists %s ;''' %self.db)
         
         num3 = sql.count('where')
-        self.logger.info("sqlnum3 %d" % num3) 
+        self.logger.info("sqlnum3 %d" % num3)  
+        cur1.close()
+        conn1.close() 
  
  
     def right_case_3_tbname(self):
@@ -798,7 +812,9 @@ class TDTestQuery(TDCase):
         # self.tdSql.execute('''drop database if exists %s ;''' %self.db)
         
         num3 = sql.count('where')
-        self.logger.info("sqlnum3_tbname %d" % num3)         
+        self.logger.info("sqlnum3_tbname %d" % num3)   
+        cur1.close()
+        conn1.close()        
 
     def right_case_3(self):
         self.logger.info("\n==========================right case 3==========================\n")
@@ -855,7 +871,9 @@ class TDTestQuery(TDCase):
         # self.tdSql.execute('''drop database if exists %s ;''' %self.db)
         
         num3 = sql.count('where')
-        self.logger.info("sqlnum3_right %d" % num3) 
+        self.logger.info("sqlnum3_right %d" % num3)  
+        cur1.close()
+        conn1.close() 
         
         
     def rm_sql(self):
