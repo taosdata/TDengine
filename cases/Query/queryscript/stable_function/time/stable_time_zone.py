5b5ed76979cf41fd4f1077bbe8048422c1095e93
“happyguoxy” <happy_guoxy@163.com>
2022-12-21 09:42:47 +0800

    test: refine query cases

--- cases/Query/queryscript/stable_function/time/stable_time_zone.py
+++ cases/Query/queryscript/stable_function/time/stable_time_zone.py
@@ -175,7 +175,9 @@ class TDTestQuery(TDCase):
         # self.tdSql.execute('''drop database if exists %s ;''' %self.db)
         
         num1 = sql.count('where')
-        self.logger.info("sqlnum1 %d" % num1) 
+        self.logger.info("sqlnum1 %d" % num1)  
+        cur1.close()
+        conn1.close() 
 
     def right_case_1_tbname(self):
         self.logger.info("\n==========================right case 1_tbname==========================\n")
@@ -248,7 +250,9 @@ class TDTestQuery(TDCase):
         # self.tdSql.execute('''drop database if exists %s ;''' %self.db)
         
         num1 = sql.count('where')
-        self.logger.info("sqlnum1_tbname %d" % num1)
+        self.logger.info("sqlnum1_tbname %d" % num1) 
+        cur1.close()
+        conn1.close() 
 
     def right_case_1(self):
         self.logger.info("\n==========================right case 1==========================\n")
@@ -304,7 +308,9 @@ class TDTestQuery(TDCase):
         # self.tdSql.execute('''drop database if exists %s ;''' %self.db)
         
         num1 = sql.count('where')
-        self.logger.info("sqlnum1_right %d" % num1) 
+        self.logger.info("sqlnum1_right %d" % num1)  
+        cur1.close()
+        conn1.close() 
                 
     def right_case_2_groupby(self):
         self.logger.info("\n==========================right case 2_groupby==========================\n")
@@ -395,7 +401,9 @@ class TDTestQuery(TDCase):
         # self.tdSql.execute('''drop database if exists %s ;''' %self.db)
         
         num2 = sql.count('where')
-        self.logger.info("sqlnum2 %d" % num2) 
+        self.logger.info("sqlnum2 %d" % num2)  
+        cur1.close()
+        conn1.close() 
         
     def right_case_2_tbname(self):
         self.logger.info("\n==========================right case 2_tbname==========================\n")
@@ -565,7 +573,9 @@ class TDTestQuery(TDCase):
         # self.tdSql.execute('''drop database if exists %s ;''' %self.db)
         
         num2 = sql.count('where')
-        self.logger.info("sqlnum2_tbname %d" % num2) 
+        self.logger.info("sqlnum2_tbname %d" % num2)  
+        cur1.close()
+        conn1.close() 
 
     def right_case_2(self):
         self.logger.info("\n==========================right case 2==========================\n")
@@ -688,7 +698,9 @@ class TDTestQuery(TDCase):
         # self.tdSql.execute('''drop database if exists %s ;''' %self.db)
         
         num2 = sql.count('where')
-        self.logger.info("sqlnum2_right %d" % num2) 
+        self.logger.info("sqlnum2_right %d" % num2)  
+        cur1.close()
+        conn1.close() 
         
                                
     def right_case_3_groupby(self):
@@ -737,6 +749,8 @@ class TDTestQuery(TDCase):
         
         num3 = sql.count('where')
         self.logger.info("sqlnum3 %d" % num3) 
+        cur1.close()
+        conn1.close()  
  
  
     def right_case_3_tbname(self):
@@ -801,7 +815,9 @@ class TDTestQuery(TDCase):
         # self.tdSql.execute('''drop database if exists %s ;''' %self.db)
         
         num3 = sql.count('where')
-        self.logger.info("sqlnum3_tbname %d" % num3)         
+        self.logger.info("sqlnum3_tbname %d" % num3)   
+        cur1.close()
+        conn1.close()        
 
     def right_case_3(self):
         self.logger.info("\n==========================right case 3==========================\n")
@@ -857,7 +873,9 @@ class TDTestQuery(TDCase):
         # self.tdSql.execute('''drop database if exists %s ;''' %self.db)
         
         num3 = sql.count('where')
-        self.logger.info("sqlnum3_right %d" % num3) 
+        self.logger.info("sqlnum3_right %d" % num3)  
+        cur1.close()
+        conn1.close() 
  
     def rm_sql(self):
         os.system("rm -rf %s/%s.sql" % (self.testcasePath,self.testcaseFilename)) 
