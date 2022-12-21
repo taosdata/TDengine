36a4959372c4014ffff7c84acbeb1631327302ac
“happyguoxy” <happy_guoxy@163.com>
2022-12-21 09:42:48 +0800

    test: refine query cases

--- cases/Query/queryscript/stable_function/str/stable_str_lower.py
+++ cases/Query/queryscript/stable_function/str/stable_str_lower.py
@@ -140,7 +140,9 @@ class TDTestQuery(TDCase):
         # self.tdSql.execute('''drop database if exists %s ;''' %self.db)
         
         num1 = sql.count('where')
-        self.logger.info("sqlnum1 %d" % num1) 
+        self.logger.info("sqlnum1 %d" % num1)  
+        cur1.close()
+        conn1.close() 
 
     def right_case_1_tbname(self):
         self.logger.info("\n==========================right case 1_tbname==========================\n")
@@ -186,7 +188,9 @@ class TDTestQuery(TDCase):
         # self.tdSql.execute('''drop database if exists %s ;''' %self.db)
         
         num1 = sql.count('where')
-        self.logger.info("sqlnum1_tbname %d" % num1)
+        self.logger.info("sqlnum1_tbname %d" % num1) 
+        cur1.close()
+        conn1.close() 
 
     def right_case_1(self):
         self.logger.info("\n==========================right case 1==========================\n")
@@ -245,7 +249,9 @@ class TDTestQuery(TDCase):
         # self.tdSql.execute('''drop database if exists %s ;''' %self.db)
         
         num1 = sql.count('where')
-        self.logger.info("sqlnum1_right %d" % num1) 
+        self.logger.info("sqlnum1_right %d" % num1)  
+        cur1.close()
+        conn1.close() 
                 
     def right_case_2_groupby(self):
         self.logger.info("\n==========================right case 2_groupby==========================\n")
@@ -335,7 +341,9 @@ class TDTestQuery(TDCase):
         # self.tdSql.execute('''drop database if exists %s ;''' %self.db)
         
         num2 = sql.count('where')
-        self.logger.info("sqlnum2 %d" % num2) 
+        self.logger.info("sqlnum2 %d" % num2)  
+        cur1.close()
+        conn1.close() 
         
     def right_case_2_tbname(self):
         self.logger.info("\n==========================right case 2_tbname==========================\n")
@@ -541,6 +549,8 @@ class TDTestQuery(TDCase):
         
         num2 = sql.count('where')
         self.logger.info("sqlnum2_tbname %d" % num2) 
+        cur1.close()
+        conn1.close()  
 
     def right_case_2(self):
         self.logger.info("\n==========================right case 2==========================\n")
@@ -672,7 +682,9 @@ class TDTestQuery(TDCase):
         # self.tdSql.execute('''drop database if exists %s ;''' %self.db)
         
         num2 = sql.count('where')
-        self.logger.info("sqlnum2_right %d" % num2) 
+        self.logger.info("sqlnum2_right %d" % num2)  
+        cur1.close()
+        conn1.close() 
         
                                
     def right_case_3_groupby(self):
@@ -720,7 +732,9 @@ class TDTestQuery(TDCase):
         # self.tdSql.execute('''drop database if exists %s ;''' %self.db)
         
         num3 = sql.count('where')
-        self.logger.info("sqlnum3 %d" % num3) 
+        self.logger.info("sqlnum3 %d" % num3)  
+        cur1.close()
+        conn1.close() 
  
  
     def right_case_3_tbname(self):
@@ -793,7 +807,9 @@ class TDTestQuery(TDCase):
         # self.tdSql.execute('''drop database if exists %s ;''' %self.db)
         
         num3 = sql.count('where')
-        self.logger.info("sqlnum3_tbname %d" % num3)         
+        self.logger.info("sqlnum3_tbname %d" % num3)   
+        cur1.close()
+        conn1.close()        
 
     def right_case_3(self):
         self.logger.info("\n==========================right case 3==========================\n")
@@ -852,7 +868,9 @@ class TDTestQuery(TDCase):
         # self.tdSql.execute('''drop database if exists %s ;''' %self.db)
         
         num3 = sql.count('where')
-        self.logger.info("sqlnum3_right %d" % num3) 
+        self.logger.info("sqlnum3_right %d" % num3)  
+        cur1.close()
+        conn1.close() 
 
     def rm_sql(self):
         os.system("rm -rf %s/%s.sql" % (self.testcasePath,self.testcaseFilename))  
