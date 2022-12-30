ad51482da1fdec47426e2ca9213b930957c3dba5
xsren <285808407@qq.com>
2022-12-30 09:59:02 +0800

    fix/check calloc failed

--- include/os/osSystem.h
+++ include/os/osSystem.h
@@ -66,29 +66,31 @@ void    taosResetTerminalMode();
 #include <dbghelp.h>
 #define taosPrintTrace(flags, level, dflag)                                                                \
   {                                                                                                        \
-    unsigned int   i;                                                                                             \
-    void*          stack[100];                                                                                    \
-    unsigned short frames;                                                                                        \
-    SYMBOL_INFO*   symbol;                                                                                        \
-    HANDLE         process;                                                                                       \
-                                                                                                                  \
-    process = GetCurrentProcess();                                                                                \
-                                                                                                                  \
-    SymInitialize(process, NULL, TRUE);                                                                           \
-                                                                                                                  \
-    frames = CaptureStackBackTrace(0, 100, stack, NULL);                                                          \
-    symbol = (SYMBOL_INFO*)calloc(sizeof(SYMBOL_INFO) + 256 * sizeof(char), 1);                                   \
-    symbol->MaxNameLen = 255;                                                                                     \
-    symbol->SizeOfStruct = sizeof(SYMBOL_INFO);                                                                   \
-                                                                                                                  \
-    if (frames > 0) {                                                                                             \
-      taosPrintLog(flags, level, dflag, "obtained %d stack frames", frames);                                      \
-      for (i = 0; i < frames; i++) {                                                                              \
-        SymFromAddr(process, (DWORD64)(stack[i]), 0, symbol);                                                     \
-        taosPrintLog(flags, level, dflag, "frame:%i: %s - 0x%0X", frames - i - 1, symbol->Name, symbol->Address); \
-      }                                                                                                           \
-    }                                                                                                             \
-    free(symbol);                                                                                                 \
+    unsigned int   i;                                                                                                 \
+    void*          stack[100];                                                                                        \
+    unsigned short frames;                                                                                            \
+    SYMBOL_INFO*   symbol;                                                                                            \
+    HANDLE         process;                                                                                           \
+                                                                                                                      \
+    process = GetCurrentProcess();                                                                                    \
+                                                                                                                      \
+    SymInitialize(process, NULL, TRUE);                                                                               \
+                                                                                                                      \
+    frames = CaptureStackBackTrace(0, 100, stack, NULL);                                                              \
+    symbol = (SYMBOL_INFO*)calloc(sizeof(SYMBOL_INFO) + 256 * sizeof(char), 1);                                       \
+    if (symbol != NULL) {                                                                                             \
+        symbol->MaxNameLen = 255;                                                                                     \
+        symbol->SizeOfStruct = sizeof(SYMBOL_INFO);                                                                   \
+                                                                                                                      \
+        if (frames > 0) {                                                                                             \
+          taosPrintLog(flags, level, dflag, "obtained %d stack frames", frames);                                      \
+          for (i = 0; i < frames; i++) {                                                                              \
+            SymFromAddr(process, (DWORD64)(stack[i]), 0, symbol);                                                     \
+            taosPrintLog(flags, level, dflag, "frame:%i: %s - 0x%0X", frames - i - 1, symbol->Name, symbol->Address); \
+          }                                                                                                           \
+        }                                                                                                             \
+        free(symbol);                                                                                                 \
+    }                                                                                                                 \
   }
 #endif
 
