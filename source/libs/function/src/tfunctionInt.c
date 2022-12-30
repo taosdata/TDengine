c08ff7dd3518759847ea4f8e7818085f885c783a
Ganlin Zhao <ganlinzhao@hotmail.com>
2022-12-30 13:58:38 +0800

    fix assert in tfunctionInt.c

--- source/libs/function/src/tfunctionInt.c
+++ source/libs/function/src/tfunctionInt.c
@@ -41,8 +41,6 @@ int32_t getNumOfResult(SqlFunctionCtx* pCtx, int32_t num, SSDataBlock* pResBlock
     }
   }
 
-  assert(maxRows >= 0);
-
   blockDataEnsureCapacity(pResBlock, maxRows);
   for (int32_t i = 0; i < num; ++i) {
     SColumnInfoData* pCol = taosArrayGet(pResBlock->pDataBlock, i);
@@ -64,7 +62,6 @@ int32_t getNumOfResult(SqlFunctionCtx* pCtx, int32_t num, SSDataBlock* pResBlock
 }
 
 bool isRowEntryCompleted(struct SResultRowEntryInfo* pEntry) {
-  assert(pEntry != NULL);
   return pEntry->complete;
 }
 
