555ee2b556b582c5609b9f56db96437b240f66a1
Haojun Liao <hjliao@taosdata.com>
2023-01-29 09:43:03 +0800

    fix(query): add qsort for alpine.

--- source/os/src/osMath.c
+++ source/os/src/osMath.c
@@ -15,7 +15,6 @@
 
 #define ALLOW_FORBID_FUNC
 #define _DEFAULT_SOURCE
-#include "os.h"
 #include <stdlib.h>
 
 #ifdef WINDOWS
@@ -32,7 +31,18 @@ void swapStr(char* j, char* J, int width) {
 }
 #endif
 
-// todo refactor: 1) move away; 2) use merge sort instead; 3) qsort is not a stable sort actually.
-void taosSort(void* arr, int64_t sz, int64_t width, __compar_fn_t compar) {
-  qsort(arr, sz, width, compar);
+int32_t qsortHelper(const void* p1, const void* p2, const void* param) {
+  __compar_fn_t comparFn = param;
+  return comparFn(p1, p2);
 }
+
+// todo refactor: 1) move away; 2) use merge sort instead; 3) qsort is not a stable sort actually.
+void taosSort(void* base, int64_t sz, int64_t width, __compar_fn_t compar) {
+#ifdef _ALPINE
+  void* param = compar;
+  taosqsort(base, width, sz, param, qsortHelper);
+#else
+  qsort(base, sz, width, compar);
+#endif
+}
+
