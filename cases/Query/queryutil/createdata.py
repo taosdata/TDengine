a81b81829ff2b1ba657ac047883bce5503457d6b
“happyguoxy” <happy_guoxy@163.com>
2023-02-17 18:00:46 +0800

    test: refine query cases

--- cases/Query/queryutil/createdata.py
+++ cases/Query/queryutil/createdata.py
@@ -852,10 +852,20 @@ class TDCreateData():
         self.tdSql.query(sql)
         self.tdSql.checkRow(0)
         
+    def value_check(self,base_value,check_value,sql1,sql2):
+        #两个sql及执行数据检查
+        self.logger.debug(f"sql1={sql1},sql2={sql2}")
+        if (base_value == check_value) :
+            self.logger.info(("sql1:'%s' result '%s' = sql2:'%s' result '%s' ") %(sql1,base_value,sql2,check_value))
+        else:
+            self.logger.info(("sql1:'%s' result '%s' != sql2:'%s' result '%s'") %(sql1,base_value,sql2,check_value))
+            return self.tdSql.checkEqual(base_value,check_value)
+        
     def dataequal(self, sql1,row1,col1, sql2,row2,col2):
         self.sql1 = sql1
         list1 =[]
         self.tdSql.query(sql1)
+        sql1_row = self.tdSql.query(sql1).row_count
         for i1 in range(row1):
             for j1 in range(col1):
                 list1.append(self.tdSql.getData(i1,j1))
@@ -864,9 +874,12 @@ class TDCreateData():
         self.sql2 = sql2  
         list2 =[]
         self.tdSql.query(sql2)
+        sql2_row = self.tdSql.query(sql2).row_count
         for i2 in range(row2):
             for j2 in range(col2):
                 list2.append(self.tdSql.getData(i2,j2))
+                
+        self.value_check(sql1_row,sql2_row,sql1,sql2)
        
         if  (list1 == list2) and len(list2)>0:
             self.logger.info(("===list=_===sql1:'%s' result = sql2:'%s' result") %(sql1,sql2))
