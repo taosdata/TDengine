cea32f2e3ce4e8f941564e2b97a678a10dd49386
wangmm0220 <wangmm0220@gmail.com>
2023-03-04 18:18:20 +0800

    fix:give error if col is same in schemless & fix json parse error in TD-22903

--- utils/test/c/sml_test.c
+++ utils/test/c/sml_test.c
@@ -1191,8 +1191,8 @@ int main(int argc, char *argv[]) {
   }
 
   int ret = 0;
-//  ret = sml_ts2385_Test();    // this test case need config sml table name using ./sml_test config_file
-//  ASSERT(!ret);
+  ret = sml_ts2385_Test();    // this test case need config sml table name using ./sml_test config_file
+  ASSERT(!ret);
   //  for(int i = 0; i < sizeof(str)/sizeof(str[0]); i++){
   //    printf("str:%s \t %d\n", str[i], smlCalTypeSum(str[i], strlen(str[i])));
   //  }
