d3c9e0800378ffb2ddd759a87dd6ba67445ae811
yihaoDeng <luomoxyz@126.com>
2023-03-14 06:15:47 +0000

    fix: fix log invalid err msg

--- source/dnode/mnode/impl/src/mndMain.c
+++ source/dnode/mnode/impl/src/mndMain.c
@@ -706,6 +706,7 @@ int32_t mndProcessRpcMsg(SRpcMsg *pMsg) {
   } else if (code == 0) {
     mGTrace("msg:%p, successfully processed", pMsg);
   } else {
+    code = terrno;
     mGError("msg:%p, failed to process since %s, app:%p type:%s", pMsg, tstrerror(code), pMsg->info.ahandle,
             TMSG_INFO(pMsg->msgType));
   }
