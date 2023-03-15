6b3fa3f5afd9858706db19c2db0a334be7e7eeb5
gccgdb1234 <wxzhang@taosdata.com>
2023-03-16 01:41:44 +0800

    test:fix that timestamps can be validated correctly in testcase

--- tests/pytest/util/sql.py
+++ tests/pytest/util/sql.py
@@ -23,13 +23,36 @@ import pandas as pd
 from util.log import *
 from util.constant import *
 
+from datetime import datetime, timedelta,timezone
+from tzlocal import get_localzone
+import pytz
+import time
+
+def _locaTzTimeStamp(utctimestamp):
+    tz = get_localzone()
+    temptz = str(tz)
+    localtz = pytz.timezone(temptz)
+    defUtctimestamp=1035640800
+    local_dt = datetime(2002, 10, 27, 6, 0, 0, tzinfo=localtz)
+    defLocaltimestamp = time.mktime(local_dt.timetuple())
+    deltaTzTime = int(defLocaltimestamp)-int(defUtctimestamp)
+    temp = int(str(utctimestamp)[0:10])
+    tempOther = str(utctimestamp)[10-len(str(utctimestamp)):]
+    localtemp = deltaTzTime + temp
+    localAll = str(localtemp) + str(tempOther)
+    localtimestamp =  int(localAll)
+    
+    print(f"local timezone is {localtimestamp}, Deltel time is {deltaTzTime}s")
+    return localtimestamp
+
+
 def _parse_datetime(timestr):
     try:
-        return datetime.datetime.strptime(timestr, '%Y-%m-%d %H:%M:%S.%f')
+        return datetime.strptime(timestr, '%Y-%m-%d %H:%M:%S.%f')
     except ValueError:
         pass
     try:
-        return datetime.datetime.strptime(timestr, '%Y-%m-%d %H:%M:%S')
+        return datetime.strptime(timestr, '%Y-%m-%d %H:%M:%S')
     except ValueError:
         pass
 
@@ -227,18 +250,51 @@ class TDSql:
         self.checkRowCol(row, col)
         return self.cursor.istype(col, dataType)
 
+
     def checkData(self, row, col, data):
         self.checkRowCol(row, col)
         if self.queryResult[row][col] != data:
             if self.cursor.istype(col, "TIMESTAMP"):
-                # suppose user want to check nanosecond timestamp if a longer data passed
-                if (len(data) >= 28):
-                    if pd.to_datetime(self.queryResult[row][col]) == pd.to_datetime(data):
-                        tdLog.info(f"sql:{self.sql}, row:{row} col:{col} data:{self.queryResult[row][col]} == expect:{data}")
+                # suppose user want to check nanosecond timestamp if a longer data passed`` 
+                if isinstance(data,str) :
+                    if (len(data) >= 28):
+                        resultData = _locaTzTimeStamp(self.queryResult[row][col])
+                        if pd.to_datetime(resultData) == pd.to_datetime(data):
+                            tdLog.info(f"sql:{self.sql}, row:{row} col:{col} data:{pd.to_datetime(resultData)} == expect:{data}")
+                        else:
+                            caller = inspect.getframeinfo(inspect.stack()[1][0])
+                            args = (caller.filename, caller.lineno, self.sql, row, col, self.queryResult[row][col], data)
+                            tdLog.exit("%s(%d) failed: sql:%s row:%d col:%d data:%s != expect:%s" % args)                        
+                    else:
+                        if self.queryResult[row][col] == _parse_datetime(data):
+                            tdLog.info(f"sql:{self.sql}, row:{row} col:{col} data:{self.queryResult[row][col]} == expect:{data}")
+                        else:
+                            caller = inspect.getframeinfo(inspect.stack()[1][0])
+                            args = (caller.filename, caller.lineno, self.sql, row, col, self.queryResult[row][col], data)
+                            tdLog.exit("%s(%d) failed: sql:%s row:%d col:%d data:%s != expect:%s" % args)
+                    return
+                elif isinstance(data,int) :
+                    if len(str(data)) == 16 :
+                        unitTime = 'us'
+                    elif len(str(data)) == 13 :
+                        unitTime = 'ms'    
+                    elif len(str(data)) == 19 :
+                        unitTime = 'ns'   
+                    else:
+                        tdLog.exit("expect timeStamp type is wrong")
+                    resultData = pd.to_datetime(_locaTzTimeStamp(data),unit=unitTime) 
+                    if resultData == self.queryResult[row][col] :
+                        tdLog.info(f"sql:{self.sql}, row:{row} col:{col} data:{self.queryResult[row][col]} == expect:{resultData}")
+                    else:
+                        caller = inspect.getframeinfo(inspect.stack()[1][0])
+                        args = (caller.filename, caller.lineno, self.sql, row, col, self.queryResult[row][col], data)
+                        tdLog.exit("%s(%d) failed: sql:%s row:%d col:%d data:%s != expect:%s" % args)
+                    return
                 else:
-                    if self.queryResult[row][col] == _parse_datetime(data):
-                        tdLog.info(f"sql:{self.sql}, row:{row} col:{col} data:{self.queryResult[row][col]} == expect:{data}")
-                return
+                    caller = inspect.getframeinfo(inspect.stack()[1][0])
+                    args = (caller.filename, caller.lineno, self.sql, row, col, self.queryResult[row][col], data)
+                    tdLog.exit("%s(%d) failed: sql:%s row:%d col:%d data:%s != expect:%s" % args)
+                  
 
             if str(self.queryResult[row][col]) == str(data):
                 tdLog.info(f"sql:{self.sql}, row:{row} col:{col} data:{self.queryResult[row][col]} == expect:{data}")
