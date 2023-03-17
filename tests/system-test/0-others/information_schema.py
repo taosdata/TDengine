31d38dfd2331447637000f80fc5d01797a152b98
kailixu <klxu@taosdata.com>
2023-03-17 14:51:47 +0800

    fix: select ins_columns from stb/ctb

--- tests/system-test/0-others/information_schema.py
+++ tests/system-test/0-others/information_schema.py
@@ -22,7 +22,7 @@ class TDTestCase:
     def init(self, conn, logSql, replicaVar=1):
         self.replicaVar = int(replicaVar)
         tdLog.debug("start to execute %s" % __file__)
-        tdSql.init(conn.cursor(), True)
+        tdSql.init(conn.cursor())
         self.setsql = TDSetSql()
         self.dbname = 'db'
         self.stbname = 'stb'
