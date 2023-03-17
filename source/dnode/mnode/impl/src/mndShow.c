0c4e9f6646b4852dd98009d52c5651ad54b993a4
kailixu <klxu@taosdata.com>
2023-03-17 16:53:28 +0800

    chore: more code

--- source/dnode/mnode/impl/src/mndShow.c
+++ source/dnode/mnode/impl/src/mndShow.c
@@ -324,8 +324,7 @@ static int32_t mndProcessRetrieveSysTableReq(SRpcMsg *pReq) {
   pReq->info.rsp = pRsp;
   pReq->info.rspLen = size;
 
-  // if (rowsRead == 0 || ((rowsRead < rowsToRead) && !pShow->resume)) {
-  if (rowsRead == 0 || rowsRead < rowsToRead) {
+  if (rowsRead == 0 || ((rowsRead < rowsToRead) && !pShow->resume)) {
     pRsp->completed = 1;
     mDebug("show:0x%" PRIx64 ", retrieve completed", pShow->id);
     mndReleaseShowObj(pShow, true);
