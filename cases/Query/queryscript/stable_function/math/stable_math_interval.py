37b9be7e5fe5666573076df21b3366a9a16b266f
“happyguoxy” <happy_guoxy@163.com>
2023-03-18 10:49:36 +0800

    test: refine query cases

--- cases/Query/queryscript/stable_function/math/stable_math_interval.py
+++ cases/Query/queryscript/stable_function/math/stable_math_interval.py
@@ -78,7 +78,7 @@ class TDTestQuery(TDCase):
     list_maths = [1,21,3,4,61,71,91]
     list_math = random.sample(list_maths,3) 
     list_intervals = [1,2,3,4,6,7,8,9,11,12,13,14,15,16,17,18,19,20,21,22,31,32,33,34,35,50,51,52,53,54,55,56,57,58,59,61,71,81,91,62,72,82,92,]
-    list_interval = random.sample(list_intervals,9) 
+    list_interval = random.sample(list_intervals,8) 
     
 
     # def case_common(self):
