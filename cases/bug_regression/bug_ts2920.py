f1f35856eff11b120e8a4af12c69a67bf2441c00
jiajingbin <jiajaybin@126.com>
2023-03-18 11:09:01 +0800

    test: add cases for TS-2920

---
+++ cases/bug_regression/bug_ts2920.py
@@ -0,0 +1,50 @@
+###################################################################
+#           Copyright (c) 2020 by TAOS Technologies, Inc.
+#                     All rights reserved.
+#
+#  This file is proprietary and confidential to TAOS Technologies.
+#  No part of this file may be reproduced, stored, transmitted,
+#  disclosed or used in any form or by any means other than as
+#  expressly provided by the written permission from Jianhui Tao
+#
+###################################################################
+
+# -*- coding: utf-8 -*-
+
+from taostest import TDCase, T
+from taostest.util.common import TDCom
+
+class TestTs2920(TDCase):
+    def init(self):
+        self.tdCom = TDCom(self.tdSql)
+        self.rows_count = 10
+
+    def run(self):
+        self.tdCom.createDb("test_ts2920")
+        self.tdSql.execute('create stable test_ts2920.nginx (ts timestamp, short_request1 varchar(256), upstream_response double) tags (short_request varchar(256));')
+        self.tdSql.execute(f'create table test_ts2920.ctb using test_ts2920.nginx tags ("/gdmall/auction_syn/{self.tdCom.get_long_name(200)}");')
+        self.tdSql.execute('create stream nginx_avg into test_ts2920.nginx_avg_output subtable(short_request) as select _wstart as start_time, _wend as end_time, short_request, short_request1, avg(upstream_response) as avg_response from test_ts2920.nginx partition by short_request,short_request1 interval(1m);')
+        for i in range(self.rows_count):
+            self.tdSql.execute(f'insert into test_ts2920.ctb values (now+{i}m, "/gdmall/auction_syn/{self.tdCom.get_long_name(200)}", {i}.{i});')
+        self.tdSql.execute('drop stream nginx_avg')
+        self.tdSql.execute('alter stable test_ts2920.nginx_avg_output add column sum_response double')
+        self.tdSql.execute('create stream nginx_avg into test_ts2920.nginx_avg_output subtable(short_request) as select _wstart as start_time, _wend as end_time, short_request, short_request1, avg(upstream_response) as avg_response, sum(upstream_response) as sum_response from test_ts2920.nginx partition by short_request,short_request1 interval(1m);')
+        for i in range(self.rows_count, self.rows_count*2):
+            self.tdSql.execute(f'insert into test_ts2920.ctb values (now+{i}m, "/gdmall/auction_syn/{self.tdCom.get_long_name(200)}", {i}.{i});')
+        self.tdSql.query(f'select count(*) from test_ts2920.nginx_avg_output')
+        self.tdSql.checkEqual(self.tdSql.query_data[0][0], self.rows_count*2-2)
+
+    def cleanup(self):
+        pass
+
+    def desc(self) -> str:
+        case_description = """
+            bug-ts2920
+        """
+        return case_description
+
+    def author(self) -> str:
+        return "Jayden"
+
+    def tags(self):
+        return T.Write
