8bf49fdbd2f9fa0a60f3c532f3d049f22bfc19d3
dapan1121 <wpan@taosdata.com>
2023-03-24 17:16:06 +0800

    fix: skiplist level

--- source/dnode/vnode/src/tsdb/tsdbMemTable.c
+++ source/dnode/vnode/src/tsdb/tsdbMemTable.c
@@ -16,7 +16,7 @@
 #include "tsdb.h"
 
 #define MEM_MIN_HASH 1024
-#define SL_MAX_LEVEL 5
+#define SL_MAX_LEVEL 1
 
 // sizeof(SMemSkipListNode) + sizeof(SMemSkipListNode *) * (l) * 2
 #define SL_NODE_SIZE(l)               (sizeof(SMemSkipListNode) + ((l) << 4))
