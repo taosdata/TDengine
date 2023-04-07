e375e4c47f2c785b22069540cf5360c46776acab
shenglian zhou <slzhou@taosdata.com>
2023-04-07 23:01:20 +0800

    fix: math operator shall be applied only to mathable type

--- source/libs/scalar/src/scalar.c
+++ source/libs/scalar/src/scalar.c
@@ -1574,6 +1574,10 @@ static int32_t sclGetMathOperatorResType(SOperatorNode *pOp) {
   SDataType ldt = ((SExprNode *)(pOp->pLeft))->resType;
   SDataType rdt = ((SExprNode *)(pOp->pRight))->resType;
 
+  if (!IS_MATHABLE_TYPE(ldt.type) || !IS_MATHABLE_TYPE(rdt.type)) {
+    return TSDB_CODE_TSC_INVALID_OPERATION;
+  }
+
   if ((TSDB_DATA_TYPE_TIMESTAMP == ldt.type && TSDB_DATA_TYPE_TIMESTAMP == rdt.type) ||
       (TSDB_DATA_TYPE_TIMESTAMP == ldt.type && (IS_VAR_DATA_TYPE(rdt.type) || IS_FLOAT_TYPE(rdt.type))) ||
       (TSDB_DATA_TYPE_TIMESTAMP == rdt.type && (IS_VAR_DATA_TYPE(ldt.type) || IS_FLOAT_TYPE(ldt.type)))) {
