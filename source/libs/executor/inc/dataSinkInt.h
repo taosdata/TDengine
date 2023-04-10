166e8181c04421424a3e5b78b3dd82e06190c3ad
facetosea <25808407@qq.com>
2023-04-10 09:43:23 +0800

    fix: handle leak

--- source/libs/executor/inc/dataSinkInt.h
+++ source/libs/executor/inc/dataSinkInt.h
@@ -29,7 +29,7 @@ struct SDataSinkHandle;
 
 typedef struct SDataSinkManager {
   SDataSinkMgtCfg cfg;
-  TdThreadMutex   mutex;
+  //TdThreadMutex   mutex;
 } SDataSinkManager;
 
 typedef int32_t (*FPutDataBlock)(struct SDataSinkHandle* pHandle, const SInputData* pInput, bool* pContinue);
