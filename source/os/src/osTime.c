bf53345f9ddae3e3d84ab34d8feb408c8f0dc8be
xinsheng Ren <285808407@qq.com>
2023-04-11 08:47:31 +0800

    fix: windows gettime negative value (#20800)

--- source/os/src/osTime.c
+++ source/os/src/osTime.c
@@ -37,6 +37,9 @@
 // This magic number is the number of 100 nanosecond intervals since January 1, 1601 (UTC)
 // until 00:00:00 January 1, 1970
 static const uint64_t TIMEEPOCH = ((uint64_t)116444736000000000ULL);
+// This magic number is the number of 100 nanosecond intervals since January 1, 1601 (UTC)
+// until 00:00:00 January 1, 1900
+static const uint64_t TIMEEPOCH1900 = ((uint64_t)116445024000000000ULL);
 
 /*
  * We do not implement alternate representations. However, we always
@@ -409,7 +412,9 @@ time_t taosMktime(struct tm *timep) {
 
 struct tm *taosLocalTime(const time_t *timep, struct tm *result, char *buf) {
   struct tm *res = NULL;
-
+  if (timep == NULL) {
+    return NULL;
+  }
   if (result == NULL) {
     res = localtime(timep);
     if (res == NULL && buf != NULL) {
@@ -419,13 +424,15 @@ struct tm *taosLocalTime(const time_t *timep, struct tm *result, char *buf) {
   }
 #ifdef WINDOWS
   if (*timep < 0) {
-    if (buf != NULL) {
-      sprintf(buf, "NaN");
+    if (*timep < -2208988800LL) {
+      if (buf != NULL) {
+        sprintf(buf, "NaN");
+      }
+      return NULL;
     }
-    return NULL;
-    // TODO: bugs in following code
-    SYSTEMTIME    ss, s;
-    FILETIME      ff, f;
+
+    SYSTEMTIME    s;
+    FILETIME      f;
     LARGE_INTEGER offset;
     struct tm     tm1;
     time_t        tt = 0;
@@ -435,17 +442,7 @@ struct tm *taosLocalTime(const time_t *timep, struct tm *result, char *buf) {
       }
       return NULL;
     }
-    ss.wYear = tm1.tm_year + 1900;
-    ss.wMonth = tm1.tm_mon + 1;
-    ss.wDay = tm1.tm_mday;
-    ss.wHour = tm1.tm_hour;
-    ss.wMinute = tm1.tm_min;
-    ss.wSecond = tm1.tm_sec;
-    ss.wMilliseconds = 0;
-    SystemTimeToFileTime(&ss, &ff);
-    offset.QuadPart = ff.dwHighDateTime;
-    offset.QuadPart <<= 32;
-    offset.QuadPart |= ff.dwLowDateTime;
+    offset.QuadPart = TIMEEPOCH1900;
     offset.QuadPart += *timep * 10000000;
     f.dwLowDateTime = offset.QuadPart & 0xffffffff;
     f.dwHighDateTime = (offset.QuadPart >> 32) & 0xffffffff;
