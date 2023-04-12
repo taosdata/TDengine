698fe12efa65328a121bee829cfe99adc217c104
jiacy-jcy <714897623@qq.com>
2023-04-12 14:15:14 +0800

    test:update test case

--- tests/system-test/0-others/information_schema.py
+++ tests/system-test/0-others/information_schema.py
@@ -102,19 +102,6 @@ class TDTestCase:
                 tdSql.checkEqual(i[1],self.tbnum+1)
     
 
-        
-    def ins_col_check_4096(self):
-        tdSql.execute('create database db3 vgroups 2 replica 1')
-        col_str = tdCom.gen_tag_col_str("col", "int",4094)
-        tdSql.execute(f'create stable if not exists db3.stb (col_ts timestamp, {col_str}) tags (t1 int)')
-        for i in range(100):
-            tdLog.info(f"create table db3.ctb{i} using db3.stb tags({i})")
-            tdSql.execute(f"create table db3.ctb{i} using db3.stb tags({i})")
-            col_value_str = '1, ' * 4093 + '1'
-            tdSql.execute(f"insert into db3.ctb{i} values(now,{col_value_str})(now+1s,{col_value_str})(now+2s,{col_value_str})(now+3s,{col_value_str})")
-        tdSql.query("select * from information_schema.ins_columns")
-        
-        tdSql.execute('drop database db3')
     def ins_stable_check(self):
         tdSql.execute('create database db3 vgroups 2 replica 1')
         tbnum = 10
@@ -168,7 +155,7 @@ class TDTestCase:
         self.prepare_data()
         self.count_check()
         self.ins_columns_check()
-        # self.ins_col_check_4096()
+        
         self.ins_stable_check()
 
 
