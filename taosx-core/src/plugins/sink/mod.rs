69eb75bb759053f1d3ddefb9dce0a12ac2480143
lispQin <cqin@taosdata.com>
2023-05-08 16:53:54 +0800

    fix: fix stable not create for lush message when use agent

--- taosx-core/src/plugins/sink/mod.rs
+++ taosx-core/src/plugins/sink/mod.rs
@@ -479,7 +479,7 @@ async fn ipc_process<R: Read, W: Write>(
         loop {
             info!("[{client}] {sql}");
             // rt.block_on(taos.exec(&sql))?;
-            let res = taos.exec(&sql).await;
+            let res: Result<usize, taos::Error> = taos.exec(&sql).await;
             if let Err(err) = res {
                 tracing::error!("Query error with {sql}: {err:?}");
                 i += 1;
@@ -551,6 +551,26 @@ impl<'a> IpcStreamWorker<'a> {
         stmt: &mut Stmt,
         record: RecordBatch,
     ) -> anyhow::Result<usize> {
+        if let Some(sql) = self.parser.metadata().init_sql_string() {
+            let guard = self.lock.lock().await;
+            let max_retries = 10;
+            let mut i = 0;
+            loop {
+                info!("metadata sql: {sql}");
+                // rt.block_on(taos.exec(&sql))?;
+                let res = self.taos.exec(&sql).await;
+                if let Err(err) = res {
+                    tracing::error!("Query error with {sql}: {err:?}");
+                    i += 1;
+                    if i > max_retries {
+                        break;
+                    }
+                } else {
+                    break;
+                }
+            }
+            drop(guard);
+        }
         // let stmt = unsafe { &mut *self.stmt.get() };
         match self.parser.metadata().stream_type() {
             StreamType::Line => {
@@ -744,7 +764,7 @@ pub fn listen_tcp_socket_with_agent(
 
                     runtime.spawn(async move {
                         let client = addr.as_socket_ipv4().unwrap().to_string();
-                        let res =
+                        let res: Result<(), anyhow::Error> =
                             ipc_tcp_forward(client.clone(), stream, cancel, remote, token, id)
                                 .await;
                         if let Err(err) = res {
