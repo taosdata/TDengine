bc7f8e6513c4ca7861a171ef39f57a9b8fff71e5
kailixu <klxu@taosdata.com>
2023-05-15 10:15:45 +0800

    chore: revert

--- enterprise/src/plugins/grant/inc/machine.h
+++ enterprise/src/plugins/grant/inc/machine.h
@@ -28,8 +28,8 @@
 
 #if 1
 #define GRANT_TOLERENCE      86400  //86400
-#define GRANT_CHECK_INTERVAL 30   //3600seconds
-#define GRANT_HEART_BEAT_MSG 10     //60seconds
+#define GRANT_CHECK_INTERVAL 3600   //3600seconds
+#define GRANT_HEART_BEAT_MSG 60     //60seconds
 #else
 #define GRANT_DEFAULT        60
 #define GRANT_TOLERENCE      60
