f8de38e5327ca92025c6f7a00fa6e02c08b6e710
wangmm0220 <wangmm0220@gmail.com>
2023-05-19 22:52:58 +0800

    fix:core dump in tsdbreader is null when tsdbreader is closed by subscribe msg and current offset equal to saved

--- source/libs/executor/src/executor.c
+++ source/libs/executor/src/executor.c
@@ -1080,7 +1080,7 @@ int32_t qStreamPrepareScan(qTaskInfo_t tinfo, STqOffsetVal* pOffset, int8_t subT
   const char*    id = GET_TASKID(pTaskInfo);
 
   // if pOffset equal to current offset, means continue consume
-  if (tOffsetEqual(pOffset, &pTaskInfo->streamInfo.currentOffset)) {
+  if (tOffsetEqual(pOffset, &pTaskInfo->streamInfo.currentOffset) && pOffset->type != TMQ_OFFSET__SNAPSHOT_DATA) {
     return 0;
   }
 
