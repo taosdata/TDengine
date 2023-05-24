ef899a0cc3cb9ff0366dc902678d109c57d4972d
kailixu <klxu@taosdata.com>
2023-05-24 11:28:45 +0800

    fix: delete index filter for desc query

--- source/dnode/vnode/src/tsdb/tsdbRead.c
+++ source/dnode/vnode/src/tsdb/tsdbRead.c
@@ -1767,8 +1767,8 @@ static bool overlapWithDelSkyline(STableBlockScanInfo* pBlockScanInfo, const SDa
       if (p->ts > pBlock->minKey.ts && index > 0) {
         index -= 1;
       } else {  // find the first point that is smaller than the minKey.ts of dataBlock.
-        if (p->version == 0 && p->ts == pBlock->minKey.ts) {  
-          index -= 1; // index always larger than 0 if p->version equals 0
+        if (p->ts == pBlock->minKey.ts && p->version < pBlock->maxVer && index > 0) {
+          index -= 1;
         }
         break;
       }
