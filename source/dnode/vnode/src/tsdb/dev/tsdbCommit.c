0c09d6f0d2e11944bb4377aa31d19cf8fa064cf9
Hongze Cheng <hzcheng@taosdata.com>
2023-05-24 09:30:14 +0800

    more code

--- source/dnode/vnode/src/tsdb/dev/tsdbCommit.c
+++ source/dnode/vnode/src/tsdb/dev/tsdbCommit.c
@@ -326,7 +326,7 @@ static int32_t open_committer(STsdb *pTsdb, SCommitInfo *pInfo, SCommitter *pCom
   pCommitter->minRow = pInfo->info.config.tsdbCfg.minRows;
   pCommitter->maxRow = pInfo->info.config.tsdbCfg.maxRows;
   pCommitter->cmprAlg = pInfo->info.config.tsdbCfg.compression;
-  pCommitter->sttTrigger = 4;  // TODO
+  pCommitter->sttTrigger = pInfo->info.config.sttTrigger;
 
   pCommitter->aTbDataP = tsdbMemTableGetTbDataArray(pTsdb->imem);
   if (pCommitter->aTbDataP == NULL) {
@@ -441,6 +441,8 @@ int32_t tsdbCommitCommit(STsdb *pTsdb) {
   pTsdb->imem = NULL;
   taosThreadRwlockUnlock(&pTsdb->rwLock);
   tsdbUnrefMemTable(pMemTable, NULL, true);
+
+  // check merge (TODO: remove here)
 
 _exit:
   if (code) {
