10035dc7943548edbbc877eb53d4312b8bd23004
tjuzyp <zhangyuanpai@gmail.com>
2023-05-26 18:13:51 +0800

    fix: rename the name of the stable to 'bucket_measurement'. #TD-24417

--- taosx-influxdb/src/main/java/com/taosdata/utils/arrow/ArrowUtils.java
+++ taosx-influxdb/src/main/java/com/taosdata/utils/arrow/ArrowUtils.java
@@ -70,7 +70,7 @@ public class ArrowUtils {
     private ArrowInitDto getArrowInit(InfluxdbMeasurementEntity influxdbMeasurementEntity) {
         ArrowInitDto arrowInitDto = new ArrowInitDto();
         // name
-        arrowInitDto.setName(influxdbMeasurementEntity.getMeasurement());
+        arrowInitDto.setName(influxdbMeasurementEntity.getBucket() + "_" + influxdbMeasurementEntity.getMeasurement());
         // columns
         List<ArrowInitDto.Column> columns = new ArrayList<>();
         columns.add(arrowInitDto.new Column("time", "timestamp"));
