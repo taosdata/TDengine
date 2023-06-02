6dda2e94f403e87888c77f095bf87fedb3743b2e
dapan1121 <wpan@taosdata.com>
2023-06-02 11:42:25 +0800

    fix: distinct primary timestamp issue

--- source/libs/parser/src/parAstCreater.c
+++ source/libs/parser/src/parAstCreater.c
@@ -828,7 +828,7 @@ SNode* createSelectStmt(SAstCreateContext* pCxt, bool isDistinct, SNodeList* pPr
   select->pProjectionList = pProjectionList;
   select->pFromTable = pTable;
   sprintf(select->stmtName, "%p", select);
-  select->timeLineResMode = TIME_LINE_GLOBAL;
+  select->timeLineResMode = select->isDistinct ? TIME_LINE_NONE : TIME_LINE_GLOBAL;
   select->onlyHasKeepOrderFunc = true;
   select->timeRange = TSWINDOW_INITIALIZER;
   return (SNode*)select;
