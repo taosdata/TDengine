7381209618f625386b049149e5dc7effb3a7d4a9
dapan1121 <wpan@taosdata.com>
2023-06-05 16:08:11 +0800

    enh: optimize filter performance

--- source/libs/executor/src/executorInt.c
+++ source/libs/executor/src/executorInt.c
@@ -573,18 +573,8 @@ void extractQualifiedTupleByFilterResult(SSDataBlock* pBlock, const SColumnInfoD
           if (colDataIsNull_var(pDst, j)) {
             colDataSetNull_var(pDst, numOfRows);
           } else {
-            // fix address sanitizer error. p1 may point to memory that will change during realloc of colDataSetVal, first copy it to p2
             char* p1 = colDataGetVarData(pDst, j);
-            int32_t len = 0;
-            if (pDst->info.type == TSDB_DATA_TYPE_JSON) {
-              len = getJsonValueLen(p1);
-            } else {
-              len = varDataTLen(p1);
-            }
-            char* p2 = taosMemoryMalloc(len);
-            memcpy(p2, p1, len);
-            colDataSetVal(pDst, numOfRows, p2, false);
-            taosMemoryFree(p2);
+            colDataReassignVal(pDst, numOfRows, j, p1);
           }
           numOfRows += 1;
           j += 1;
