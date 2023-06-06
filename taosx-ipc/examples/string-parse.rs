c1060b6ebd13a5b04f2fe2ee651fd5904769c2d9
lispQin <cqin@taosdata.com>
2023-06-06 10:36:54 +0800

    fix: fix example error

--- taosx-ipc/examples/string-parse.rs
+++ taosx-ipc/examples/string-parse.rs
@@ -20,7 +20,7 @@ async fn main() -> Result<()> {
         .with_stable(
             "meters",
             vec![
-                IpcField::new("ts", false, timestamp_type, IpcDataType::Timestamp),
+                IpcField::new("ts", false, timestamp_type, IpcDataType::Timestamp(arrow::datatypes::TimeUnit::Millisecond)),
                 IpcField::new("c1", false, ArrowDataType::Utf8, IpcDataType::Int32),
                 IpcField::new("c2", false, ArrowDataType::Utf8, IpcDataType::Float64),
                 IpcField::new("c3", false, ArrowDataType::Utf8, IpcDataType::VarChar(100)),
