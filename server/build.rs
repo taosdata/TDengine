754577a5bd5b17d16e1e0a066f9ffac4a0dc289f
Huo Linhe <linhehuo@gmail.com>
2023-06-19 19:05:19 +0800

    fix: follow latest packaging rule

--- server/build.rs
+++ server/build.rs
@@ -1,5 +1,7 @@
 use std::path::Path;
 
+const DEFAULT_CUS_NAME: &str = "TDengine";
+const DEFAULT_CUS_PROMPT: &str = "taos";
 fn main() -> shadow_rs::SdResult<()> {
     let out_dir = std::env::var("OUT_DIR").unwrap();
     let out_dir = Path::new(&out_dir);
@@ -8,34 +10,31 @@ fn main() -> shadow_rs::SdResult<()> {
     let readme = manifest_dir.join("README.md");
     let target_dir = manifest_dir.join("..").join("target");
 
-    let cus_name = std::env::var("CUS_NAME").unwrap_or("TDengine".to_string());
-    let cus_prompt = std::env::var("CUS_PROMPT").unwrap_or("taosX".to_string());
+    let cus_name = std::env::var("CUS_NAME").unwrap_or(DEFAULT_CUS_NAME.to_string());
+    let cus_prompt = std::env::var("CUS_PROMPT").unwrap_or(DEFAULT_CUS_PROMPT.to_string());
     let cus_name = if cus_name.trim().is_empty() {
-        "TDengine"
+        DEFAULT_CUS_NAME
     } else {
         cus_name.trim()
     };
     let cus_prompt = if cus_prompt.trim().is_empty() {
-        "taosX"
+        DEFAULT_CUS_PROMPT
     } else {
         cus_prompt.trim()
     };
     let content = std::fs::read_to_string(readme)
         .unwrap()
-        .replace("taos", cus_prompt)
-        .replace("TDengine", cus_name);
+        .replace(DEFAULT_CUS_PROMPT, cus_prompt)
+        .replace(DEFAULT_CUS_NAME, cus_name);
     let readme_out = out_dir.join("README.md");
     std::fs::write(&readme_out, content).unwrap();
 
     let service = std::fs::read_to_string(manifest_dir.join("examples").join("explorer.service"))
         .unwrap()
-        .replace("taos", cus_prompt)
-        .replace("TDengine", cus_name);
-    std::fs::write(
-        target_dir.join(format!("{cus_prompt}-explorer.service")),
-        service,
-    )
-    .unwrap();
+        .replace(DEFAULT_CUS_PROMPT, cus_prompt)
+        .replace(DEFAULT_CUS_NAME, cus_name);
+    let service_path = target_dir.join(format!("{cus_prompt}-explorer.service"));
+    std::fs::write(&service_path, service).unwrap();
     println!("cargo:rustc-env=CUS_NAME={cus_name}");
     println!("cargo:rustc-env=CUS_PROMPT={cus_prompt}");
     println!("cargo:rustc-env=CUS_CLI_NAME={cus_prompt}-explorer");
@@ -45,5 +44,6 @@ fn main() -> shadow_rs::SdResult<()> {
     println!("cargo:rerun-if-changed=README.md");
     println!("cargo:rerun-if-changed=../dist/");
     println!("cargo:rerun-if-changed=examples/explorer.service");
+    println!("cargo:rerun-if-changed={}", service_path.display());
     shadow_rs::new()
 }
