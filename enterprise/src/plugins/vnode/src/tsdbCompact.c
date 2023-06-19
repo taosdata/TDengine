1fa08d1d958977aeecefc1fda660237f5331bc7b
Hongze Cheng <hzcheng@taosdata.com>
2023-06-19 18:31:17 +0800

    more code

--- enterprise/src/plugins/vnode/src/tsdbCompact.c
+++ enterprise/src/plugins/vnode/src/tsdbCompact.c
@@ -13,6 +13,11 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "../../../../../source/dnode/vnode/src/tsdb/tsdbDataFileRW.h"
+#include "../../../../../source/dnode/vnode/src/tsdb/tsdbFS2.h"
+#include "../../../../../source/dnode/vnode/src/tsdb/tsdbFSetRW.h"
+#include "../../../../../source/dnode/vnode/src/tsdb/tsdbIter.h"
+#include "../../../../../source/dnode/vnode/src/tsdb/tsdbSttFileRW.h"
 #include "tsdb.h"
 
 extern int32_t tsdbUpdateTableSchema(SMeta *pMeta, int64_t suid, int64_t uid, SSkmInfo *pSkmInfo);
@@ -668,6 +673,500 @@ _exit:
     tsdbError("vgId:%d %s failed at line %d since %s", TD_VID(pTsdb->pVnode), __func__, lino, tstrerror(code));
   } else {
     tsdbInfo("vgId:%d %s done", TD_VID(pTsdb->pVnode), __func__);
+  }
+  return code;
+}
+
+// new code ====================================================================================
+typedef struct {
+  STsdb  *tsdb;
+  int32_t szPage;
+  int32_t minRow;
+  int32_t maxRow;
+  int8_t  cmprAlg;
+  int64_t cid;
+  int64_t compactVersion;
+
+  TFileSetArray *fsetArr;
+  TFileOpArray  *fopArr;
+
+  struct {
+    STFileSet *fset;
+    SDiskID    did;
+
+    // reader
+    SDataFileReader    *dataReader;
+    TSttFileReaderArray sttReaderArr[1];
+
+    // iter & merger
+    TTsdbIterArray dataIterArr[1];
+    SIterMerger   *dataIterMerger;
+    TTsdbIterArray tombIterArr[1];
+    SIterMerger   *tombIterMerger;
+
+    // writer
+    SFSetWriter *writer;
+
+    TABLEID tbid[1];
+
+    // skyline
+    SArray  *aSkyLine;
+    int32_t  iSkyLine;
+    TSDBKEY *pDKey;
+    TSDBKEY  dKey;
+  } ctx[1];
+} SCompactor2;
+
+static int32_t tsdbCompactBegin(STsdb *tsdb, SCompactInfo *info, SCompactor2 *compactor) {
+  int32_t code = 0;
+  int32_t lino = 0;
+
+  compactor->tsdb = tsdb;
+  compactor->szPage = tsdb->pVnode->config.tsdbPageSize;
+  compactor->minRow = tsdb->pVnode->config.tsdbCfg.minRows;
+  compactor->maxRow = tsdb->pVnode->config.tsdbCfg.maxRows;
+  compactor->cmprAlg = tsdb->pVnode->config.tsdbCfg.compression;
+  compactor->cid = tsdbFSAllocEid(tsdb->pFS);
+  compactor->compactVersion = INT64_MAX;
+
+  code = tsdbFSCreateCopySnapshot(tsdb->pFS, &compactor->fsetArr);
+  TSDB_CHECK_CODE(code, lino, _exit);
+
+_exit:
+  if (code) {
+    TSDB_ERROR_LOG(TD_VID(compactor->tsdb->pVnode), lino, code);
+  }
+  return code;
+}
+
+static int32_t tsdbCompactEnd(SCompactor2 *compactor) {
+  int32_t code = 0;
+  int32_t lino = 0;
+
+  code = tsdbFSEditBegin(compactor->tsdb->pFS, compactor->fopArr, TSDB_FEDIT_MERGE);
+  TSDB_CHECK_CODE(code, lino, _exit);
+
+  taosThreadRwlockWrlock(&compactor->tsdb->rwLock);
+  code = tsdbFSEditCommit(compactor->tsdb->pFS);
+  if (code) {
+    taosThreadRwlockUnlock(&compactor->tsdb->rwLock);
+    TSDB_CHECK_CODE(code, lino, _exit);
+  }
+  taosThreadRwlockUnlock(&compactor->tsdb->rwLock);
+
+  taosArrayDestroy(compactor->ctx->aSkyLine);
+
+  TARRAY2_DESTROY(compactor->ctx->tombIterArr, NULL);
+  TARRAY2_DESTROY(compactor->ctx->dataIterArr, NULL);
+  TARRAY2_DESTROY(compactor->ctx->sttReaderArr, NULL);
+  TARRAY2_DESTROY(compactor->fopArr, NULL);
+
+  tsdbFSDestroyCopySnapshot(&compactor->fsetArr);
+
+_exit:
+  if (code) {
+    TSDB_ERROR_LOG(TD_VID(compactor->tsdb->pVnode), lino, code);
+  }
+  return code;
+}
+
+static int32_t tsdbCompactFSetOpenReader(SCompactor2 *compactor) {
+  int32_t    code = 0;
+  int32_t    lino = 0;
+  STFileObj *fobj;
+
+  ASSERT(compactor->ctx->dataReader == NULL);
+  ASSERT(TARRAY2_SIZE(compactor->ctx->sttReaderArr) == 0);
+
+  // data
+  SDataFileReaderConfig dataFileReaderConfig = {
+      .tsdb = compactor->tsdb,
+      .szPage = compactor->szPage,
+  };
+  for (int32_t ftype = 0; ftype < TSDB_FTYPE_MAX && (fobj = compactor->ctx->fset->farr[ftype], 1); ftype++) {
+    if (fobj == NULL) continue;
+    dataFileReaderConfig.files[ftype].exist = true;
+    dataFileReaderConfig.files[ftype].file = fobj->f[0];
+
+    STFileOp op = {
+        .optype = TSDB_FOP_REMOVE,
+        .fid = compactor->ctx->fset->fid,
+        .of = fobj->f[0],
+    };
+
+    code = TARRAY2_APPEND(compactor->fopArr, op);
+    TSDB_CHECK_CODE(code, lino, _exit);
+  }
+  code = tsdbDataFileReaderOpen(NULL, &dataFileReaderConfig, &compactor->ctx->dataReader);
+  TSDB_CHECK_CODE(code, lino, _exit);
+
+  // stt
+  SSttLvl *lvl;
+  TARRAY2_FOREACH(compactor->ctx->fset->lvlArr, lvl) {
+    TARRAY2_FOREACH(lvl->fobjArr, fobj) {
+      SSttFileReader      *sttReader;
+      SSttFileReaderConfig sttFileReaderConfig = {
+          .tsdb = compactor->tsdb,
+          .szPage = compactor->szPage,
+          .file = fobj->f[0],
+      };
+
+      code = tsdbSttFileReaderOpen(fobj->fname, &sttFileReaderConfig, &sttReader);
+      TSDB_CHECK_CODE(code, lino, _exit);
+
+      code = TARRAY2_APPEND(compactor->ctx->sttReaderArr, sttReader);
+      TSDB_CHECK_CODE(code, lino, _exit);
+
+      STFileOp op = {
+          .optype = TSDB_FOP_REMOVE,
+          .fid = compactor->ctx->fset->fid,
+          .of = fobj->f[0],
+      };
+
+      code = TARRAY2_APPEND(compactor->fopArr, op);
+      TSDB_CHECK_CODE(code, lino, _exit);
+    }
+  }
+
+_exit:
+  if (code) {
+    TSDB_ERROR_LOG(TD_VID(compactor->tsdb->pVnode), lino, code);
+  }
+  return code;
+}
+
+static int32_t tsdbCompactFSetCloseReader(SCompactor2 *compactor) {
+  TARRAY2_CLEAR(compactor->ctx->sttReaderArr, tsdbSttFileReaderClose);
+  tsdbDataFileReaderClose(&compactor->ctx->dataReader);
+  return 0;
+}
+
+static int32_t tsdbCompactFSetOpenIter(SCompactor2 *compactor) {
+  int32_t code = 0;
+  int32_t lino = 0;
+
+  STsdbIter      *iter;
+  STsdbIterConfig iterConfig = {0};
+
+  ASSERT(compactor->ctx->dataIterMerger == NULL);
+  ASSERT(compactor->ctx->tombIterMerger == NULL);
+  ASSERT(TARRAY2_SIZE(compactor->ctx->dataIterArr) == 0);
+  ASSERT(TARRAY2_SIZE(compactor->ctx->tombIterArr) == 0);
+
+  // data
+  if (compactor->ctx->dataReader != NULL) {
+    // data
+    iterConfig.type = TSDB_ITER_TYPE_DATA;
+    iterConfig.dataReader = compactor->ctx->dataReader;
+
+    code = tsdbIterOpen(&iterConfig, &iter);
+    TSDB_CHECK_CODE(code, lino, _exit);
+
+    code = TARRAY2_APPEND(compactor->ctx->dataIterArr, iter);
+    TSDB_CHECK_CODE(code, lino, _exit);
+
+    // tomb
+    iterConfig.type = TSDB_ITER_TYPE_DATA_TOMB;
+    iterConfig.dataReader = compactor->ctx->dataReader;
+
+    code = tsdbIterOpen(&iterConfig, &iter);
+    TSDB_CHECK_CODE(code, lino, _exit);
+
+    code = TARRAY2_APPEND(compactor->ctx->tombIterArr, iter);
+    TSDB_CHECK_CODE(code, lino, _exit);
+  }
+
+  // stt
+  SSttFileReader *sttReader;
+  TARRAY2_FOREACH(compactor->ctx->sttReaderArr, sttReader) {
+    // data
+    iterConfig.type = TSDB_ITER_TYPE_STT;
+    iterConfig.sttReader = sttReader;
+
+    code = tsdbIterOpen(&iterConfig, &iter);
+    TSDB_CHECK_CODE(code, lino, _exit);
+
+    code = TARRAY2_APPEND(compactor->ctx->dataIterArr, iter);
+    TSDB_CHECK_CODE(code, lino, _exit);
+
+    // tomb
+    iterConfig.type = TSDB_ITER_TYPE_STT_TOMB;
+    iterConfig.sttReader = sttReader;
+
+    code = tsdbIterOpen(&iterConfig, &iter);
+    TSDB_CHECK_CODE(code, lino, _exit);
+
+    code = TARRAY2_APPEND(compactor->ctx->tombIterArr, iter);
+    TSDB_CHECK_CODE(code, lino, _exit);
+  }
+
+  // merger
+  code = tsdbIterMergerOpen(compactor->ctx->dataIterArr, &compactor->ctx->dataIterMerger, false);
+  TSDB_CHECK_CODE(code, lino, _exit);
+
+  code = tsdbIterMergerOpen(compactor->ctx->tombIterArr, &compactor->ctx->tombIterMerger, true);
+  TSDB_CHECK_CODE(code, lino, _exit);
+
+_exit:
+  if (code) {
+    TSDB_ERROR_LOG(TD_VID(compactor->tsdb->pVnode), lino, code);
+  }
+  return code;
+}
+
+static int32_t tsdbCompactFSetCloseIter(SCompactor2 *compactor) {
+  tsdbIterMergerClose(&compactor->ctx->dataIterMerger);
+  tsdbIterMergerClose(&compactor->ctx->tombIterMerger);
+  TARRAY2_CLEAR(compactor->ctx->tombIterArr, tsdbIterClose);
+  TARRAY2_CLEAR(compactor->ctx->dataIterArr, tsdbIterClose);
+  return 0;
+}
+
+static int32_t tsdbCompactFSetOpenWriter(SCompactor2 *compactor) {
+  int32_t code = 0;
+  int32_t lino = 0;
+
+  ASSERT(compactor->ctx->writer == NULL);
+  SFSetWriterConfig config = {
+      .tsdb = compactor->tsdb,
+      .toSttOnly = false,
+      .compactVersion = compactor->compactVersion,
+      .minRow = compactor->minRow,
+      .maxRow = compactor->maxRow,
+      .szPage = compactor->szPage,
+      .cmprAlg = compactor->cmprAlg,
+      .fid = compactor->ctx->fset->fid,
+      .cid = compactor->cid,
+      .did = compactor->ctx->did,
+      .level = 0,
+  };
+
+  code = tsdbFSetWriterOpen(&config, &compactor->ctx->writer);
+  TSDB_CHECK_CODE(code, lino, _exit);
+
+_exit:
+  if (code) {
+    TSDB_ERROR_LOG(TD_VID(compactor->tsdb->pVnode), lino, code);
+  }
+  return code;
+}
+
+static int32_t tsdbCompactFSetCloseWriter(SCompactor2 *compactor) {
+  return tsdbFSetWriterClose(&compactor->ctx->writer, 0, compactor->fopArr);
+}
+
+static int32_t tsdbCompactFSetBegin(SCompactor2 *compactor) {
+  int32_t code = 0;
+  int32_t lino = 0;
+
+  code = tsdbCompactFSetOpenReader(compactor);
+  TSDB_CHECK_CODE(code, lino, _exit);
+
+  code = tsdbCompactFSetOpenIter(compactor);
+  TSDB_CHECK_CODE(code, lino, _exit);
+
+  code = tsdbCompactFSetOpenWriter(compactor);
+  TSDB_CHECK_CODE(code, lino, _exit);
+
+  compactor->ctx->tbid->suid = 0;
+  compactor->ctx->tbid->uid = 0;
+
+_exit:
+  if (code) {
+    TSDB_ERROR_LOG(TD_VID(compactor->tsdb->pVnode), lino, code);
+  }
+  return code;
+}
+
+static int32_t tsdbCompactFSetEnd(SCompactor2 *compactor) {
+  int32_t code = 0;
+  int32_t lino = 0;
+
+  code = tsdbCompactFSetCloseWriter(compactor);
+  TSDB_CHECK_CODE(code, lino, _exit);
+
+  code = tsdbCompactFSetCloseIter(compactor);
+  TSDB_CHECK_CODE(code, lino, _exit);
+
+  code = tsdbCompactFSetCloseReader(compactor);
+  TSDB_CHECK_CODE(code, lino, _exit);
+
+_exit:
+  if (code) {
+    TSDB_ERROR_LOG(TD_VID(compactor->tsdb->pVnode), lino, code);
+  }
+  return code;
+}
+
+static int32_t tsdbCompactFSetTableDataEnd(SCompactor2 *compactor) {
+  // do nothing
+  return 0;
+}
+
+static int32_t tsdbCompactFSetTableDataBegin(SCompactor2 *compactor, const TABLEID *tbid) {
+  int32_t code = 0;
+  int32_t lino = 0;
+
+  compactor->ctx->tbid->suid = tbid->suid;
+  compactor->ctx->tbid->uid = tbid->uid;
+
+  SArray *delDataArr = NULL;
+
+  for (STombRecord *record; (record = tsdbIterMergerGetTombRecord(compactor->ctx->tombIterMerger)) != NULL;) {
+    if (record->suid > tbid->uid || (record->suid == tbid->uid && record->uid > tbid->uid)) {
+      break;
+    } else {
+      if (record->uid == tbid->uid) {
+        SDelData delData = {
+            .version = record->version,
+            .sKey = record->skey,
+            .eKey = record->ekey,
+        };
+
+        if (delDataArr == NULL && (delDataArr = taosArrayInit(0, sizeof(SDelData))) == NULL) {
+          code = TSDB_CODE_OUT_OF_MEMORY;
+          TSDB_CHECK_CODE(code, lino, _exit);
+        }
+
+        if (taosArrayPush(delDataArr, &delData) == NULL) {
+          code = TSDB_CODE_OUT_OF_MEMORY;
+          TSDB_CHECK_CODE(code, lino, _exit);
+        }
+      }
+
+      code = tsdbIterMergerNext(compactor->ctx->tombIterMerger);
+      TSDB_CHECK_CODE(code, lino, _exit);
+    }
+  }
+
+  if (delDataArr) {
+    if (compactor->ctx->aSkyLine == NULL && (compactor->ctx->aSkyLine = taosArrayInit(0, sizeof(TSDBKEY))) == NULL) {
+      code = TSDB_CODE_OUT_OF_MEMORY;
+      TSDB_CHECK_CODE(code, lino, _exit);
+    }
+
+    code = tsdbBuildDeleteSkyline(delDataArr, 0, taosArrayGetSize(delDataArr) - 1, compactor->ctx->aSkyLine);
+    TSDB_CHECK_CODE(code, lino, _exit);
+
+    TSDBKEY *pKey = (TSDBKEY *)taosArrayGet(compactor->ctx->aSkyLine, compactor->ctx->iSkyLine);
+    compactor->ctx->dKey.version = 0;
+    compactor->ctx->dKey.ts = pKey->ts;
+    compactor->ctx->pDKey = &compactor->ctx->dKey;
+
+    taosArrayDestroy(delDataArr);
+  } else {
+    compactor->ctx->pDKey = NULL;
+  }
+
+_exit:
+  if (code) {
+    TSDB_ERROR_LOG(TD_VID(compactor->tsdb->pVnode), lino, code);
+  }
+  return code;
+}
+
+static bool tsdbRowIsDeleted(SCompactor2 *compactor, TSDBROW *row) {
+  TSDBKEY  tKey = TSDBROW_KEY(row);
+  TSDBKEY *aKey = (TSDBKEY *)TARRAY_DATA(compactor->ctx->aSkyLine);
+  int32_t  nKey = TARRAY_SIZE(compactor->ctx->aSkyLine);
+
+  if (tKey.ts > compactor->ctx->pDKey->ts) {
+    do {
+      compactor->ctx->pDKey->version = aKey[compactor->ctx->iSkyLine].version;
+      compactor->ctx->iSkyLine++;
+      if (compactor->ctx->iSkyLine < nKey) {
+        compactor->ctx->dKey.ts = aKey[compactor->ctx->iSkyLine].ts;
+      } else {
+        if (compactor->ctx->pDKey->version == 0) {
+          compactor->ctx->pDKey = NULL;
+          return false;
+        } else {
+          compactor->ctx->pDKey->ts = INT64_MAX;
+        }
+      }
+    } while (tKey.ts > compactor->ctx->pDKey->ts);
+  }
+
+  if (tKey.ts < compactor->ctx->pDKey->ts) {
+    if (tKey.version > compactor->ctx->pDKey->version) {
+      return false;
+    } else {
+      return true;
+    }
+  } else if (tKey.ts == compactor->ctx->pDKey->ts) {
+    ASSERT(compactor->ctx->iSkyLine < nKey);
+    if (tKey.version > TMAX(compactor->ctx->pDKey->version, aKey[compactor->ctx->iSkyLine].version)) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+static int32_t tsdbCompactFSet(SCompactor2 *compactor) {
+  int32_t code = 0;
+  int32_t lino = 0;
+
+  SMetaInfo info;
+  for (SRowInfo *row; (row = tsdbIterMergerGetData(compactor->ctx->dataIterMerger)) != NULL;) {
+    if (row->uid != compactor->ctx->tbid->uid) {
+      code = tsdbCompactFSetTableDataEnd(compactor);
+      TSDB_CHECK_CODE(code, lino, _exit);
+
+      if (metaGetInfo(compactor->tsdb->pVnode->pMeta, row->uid, &info, NULL) != 0) {
+        code = tsdbIterMergerSkipTableData(compactor->ctx->dataIterMerger, (TABLEID *)row);
+        TSDB_CHECK_CODE(code, lino, _exit);
+        continue;
+      }
+
+      code = tsdbCompactFSetTableDataBegin(compactor, (TABLEID *)row);
+      TSDB_CHECK_CODE(code, lino, _exit);
+    }
+
+    if (compactor->ctx->pDKey == NULL || !tsdbRowIsDeleted(compactor, &row->row)) {
+      code = tsdbFSetWriteRow(compactor->ctx->writer, row);
+      TSDB_CHECK_CODE(code, lino, _exit);
+    }
+    code = tsdbIterMergerNext(compactor->ctx->dataIterMerger);
+    TSDB_CHECK_CODE(code, lino, _exit);
+  }
+
+_exit:
+  if (code) {
+    TSDB_ERROR_LOG(TD_VID(compactor->tsdb->pVnode), lino, code);
+  }
+  return code;
+}
+
+int32_t tsdbCompact2(STsdb *tsdb, SCompactInfo *info) {
+  int32_t code = 0;
+  int32_t lino = 0;
+
+  SCompactor2 compactor[1] = {0};
+
+  code = tsdbCompactBegin(tsdb, info, compactor);
+  TSDB_CHECK_CODE(code, lino, _exit);
+
+  TARRAY2_FOREACH(compactor->fsetArr, compactor->ctx->fset) {
+    // check if the file set should be compacted
+    code = tsdbCompactFSetBegin(compactor);
+    TSDB_CHECK_CODE(code, lino, _exit);
+
+    code = tsdbCompactFSet(compactor);
+    TSDB_CHECK_CODE(code, lino, _exit);
+
+    code = tsdbCompactFSetEnd(compactor);
+    TSDB_CHECK_CODE(code, lino, _exit);
+  }
+
+  code = tsdbCompactEnd(compactor);
+  TSDB_CHECK_CODE(code, lino, _exit);
+
+_exit:
+  if (code) {
+    TSDB_ERROR_LOG(TD_VID(tsdb->pVnode), lino, code);
   }
   return code;
 }
