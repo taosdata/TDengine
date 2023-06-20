2d4eed4234c52c46dbf734884bdfd4ee21c44755
Haojun Liao <hjliao@taosdata.com>
2023-06-20 17:12:31 +0800

    other: merge other branch.

--- source/dnode/vnode/src/tsdb/tsdbRead2.c
+++ source/dnode/vnode/src/tsdb/tsdbRead2.c
@@ -1074,7 +1074,7 @@ static int32_t doLoadFileBlock(STsdbReader* pReader, SArray* pIndexList, SBlockN
 
   double el = (taosGetTimestampUs() - st) / 1000.0;
   tsdbDebug(
-      "load block of %ld tables completed, blocks:%d in %d tables, last-files:%d, block-info-size:%.2f Kb, elapsed "
+      "load block of %d tables completed, blocks:%d in %d tables, last-files:%d, block-info-size:%.2f Kb, elapsed "
       "time:%.2f ms %s",
       numOfTables, pBlockNum->numOfBlocks, (int32_t)taosArrayGetSize(pTableScanInfoList), pBlockNum->numOfLastFiles,
       sizeInDisk / 1000.0, el, pReader->idStr);
@@ -4746,6 +4746,7 @@ void tsdbReaderClose2(STsdbReader* pReader) {
   if (pReader->status.pTableMap != NULL) {
     destroyAllBlockScanInfo(pReader->status.pTableMap);
     clearBlockScanInfoBuf(&pReader->blockInfoBuf);
+    pReader->status.pTableMap = NULL;
   }
 
   if (pReader->pFileReader != NULL) {
