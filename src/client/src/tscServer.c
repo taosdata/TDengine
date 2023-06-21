a8d2f7ff53917d4114395a83eb670c74a30d2764
kailixu <klxu@taosdata.com>
2023-06-21 13:04:46 +0800

    chore: more check

--- src/client/src/tscServer.c
+++ src/client/src/tscServer.c
@@ -663,7 +663,7 @@ void tscProcessMsgFromServer(SRpcMsg *rpcMsg, SRpcEpSet *pEpSet) {
         memcpy(pRes->pRsp, rpcMsg->pCont, pRes->rspLen);
       }
     } else {
-      if (pRes->code == TSDB_CODE_SUCCESS) {
+      if (!rpcMsg->pCont && TSDB_CODE_SUCCESS == pRes->code) {
         pRes->code = TSDB_CODE_RPC_UNEXPECTED_RESPONSE;
       }
       tfree(pRes->pRsp);
