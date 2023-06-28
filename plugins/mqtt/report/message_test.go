4470d59fca0b9fc6ee710dc9de62b6110a8b736f
t_max <1172915550@qq.com>
2023-06-28 16:13:47 +0800

    enh(mqtt): reduce CPU usage
    (cherry picked from commit c08fa4c7323bb416fb1393c14c314e0f53166894)

--- plugins/mqtt/report/message_test.go
+++ plugins/mqtt/report/message_test.go
@@ -2,6 +2,7 @@ package report
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -11,13 +12,17 @@ func TestMessageList(t *testing.T) {
 
 	messages.Add(&Message{Topic: "test", Payload: []byte("hello")})
 	messages.Add(&Message{Topic: "test", Payload: []byte("world")})
-
-	allMessages := messages.GetAll()
-
-	assert.Equal(t, 2, len(allMessages))
-	assert.Equal(t, "test", allMessages[0].Topic)
-	assert.Equal(t, "test", allMessages[1].Topic)
-	assert.Equal(t, []byte("hello"), allMessages[0].Payload)
-	assert.Equal(t, []byte("world"), allMessages[1].Payload)
-	assert.Equal(t, 0, messages.list.Len())
+	timer := time.NewTimer(time.Second)
+	select {
+	case <-messages.C():
+		allMessages := messages.GetAll()
+		assert.Equal(t, 2, len(allMessages))
+		assert.Equal(t, "test", allMessages[0].Topic)
+		assert.Equal(t, "test", allMessages[1].Topic)
+		assert.Equal(t, []byte("hello"), allMessages[0].Payload)
+		assert.Equal(t, []byte("world"), allMessages[1].Payload)
+		assert.Equal(t, 0, messages.list.Len())
+	case <-timer.C:
+		t.Fatal("get message channel timeout")
+	}
 }
