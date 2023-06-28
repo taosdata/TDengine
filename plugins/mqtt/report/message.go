c08fa4c7323bb416fb1393c14c314e0f53166894
t_max <1172915550@qq.com>
2023-06-28 16:13:47 +0800

    enh(mqtt): reduce CPU usage

--- plugins/mqtt/report/message.go
+++ plugins/mqtt/report/message.go
@@ -15,21 +15,28 @@ type Message struct {
 type MessageList struct {
 	list *list.List
 	lock sync.RWMutex
+	c    chan struct{}
+	sent bool
 }
 
 func NewMessageList() *MessageList {
-	return &MessageList{list: list.New()}
+	return &MessageList{list: list.New(), c: make(chan struct{}, 1)}
 }
 
 func (m *MessageList) Add(message *Message) {
 	m.lock.Lock()
 	defer m.lock.Unlock()
 	m.list.PushBack(message)
+	if !m.sent {
+		m.c <- struct{}{}
+		m.sent = true
+	}
 }
 
 func (m *MessageList) GetAll() []*Message {
 	m.lock.Lock()
 	defer m.lock.Unlock()
+	m.sent = false
 	if m.list.Len() > 0 {
 		values := make([]*Message, 0, m.list.Len())
 		for e := m.list.Front(); e != nil; e = e.Next() {
@@ -39,4 +46,8 @@ func (m *MessageList) GetAll() []*Message {
 		return values
 	}
 	return nil
+}
+
+func (m *MessageList) C() <-chan struct{} {
+	return m.c
 }
