c99b619d184913aefb2d2bdf5bcf606c8bfd85f0
“happyguoxy” <happy_guoxy@163.com>
2023-07-03 11:05:16 +0800

    test: refine query cases

---
+++ cases/Query/queryscript/stable_function/numeric/split/table_numeric_interp_2.py
@@ -0,0 +1,45 @@
+###################################################################
+#           Copyright (c) 2016 by TAOS Technologies, Inc.
+#                     All rights reserved.
+#
+#  This file is proprietary and confidential to TAOS Technologies.
+#  No part of this file may be reproduced, stored, transmitted,
+#  disclosed or used in any form or by any means other than as
+#  expressly provided by the written permission from Jianhui Tao
+#
+###################################################################
+
+# -*- coding: utf-8 -*-
+
+from Query.queryscript.stable_function.numeric.table_numeric_interp import *
+
+class TDTestQuery(TDTestQuery):
+        
+    def tags(self) :
+	
+        return ""
+
+    def author(self) -> str:
+
+        return "Guo Xiangyang"
+
+    def desc(self) -> str:
+        case_description = '''
+        case1:# support INTERP function [hanshu = 'INTERP']
+        case2:
+        '''
+        return case_description
+                
+    def run(self):
+        startTime = time.time() 
+        self.tdSql.query("alter local 'schedulePolicy' '2';") 
+        
+        self.data_create(self.db_2)
+        
+        self.right_case_2_range()    
+
+        endTime = time.time()
+        self.rm_sql(self.db_2)
+        self.logger.info("total time %ds" % (endTime - startTime))
+
+
