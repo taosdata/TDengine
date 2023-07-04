a57412f8fcbe26beb5711816f236fa83548a7333
sunpeng <sunpeng.dev@gmail.com>
2023-07-04 09:27:01 +0800

    build: upgrade taospy version

--- tests/parallel_test/run_case.sh
+++ tests/parallel_test/run_case.sh
@@ -76,10 +76,10 @@ ulimit -c unlimited
 md5sum /usr/lib/libtaos.so.1
 md5sum /home/TDinternal/debug/build/lib/libtaos.so
 
-#define taospy 2.7.6
+#define taospy 2.7.10
 pip3 list|grep taospy
 pip3 uninstall taospy -y
-pip3 install  --default-timeout=120 taospy==2.7.6 
+pip3 install  --default-timeout=120 taospy==2.7.10
 
 $TIMEOUT_CMD $cmd
 RET=$?
