35168ff08e3c58528d1f3b9b33950df2c45b3e67
Huo Linhe <linhehuo@gmail.com>
2023-07-07 11:29:47 +0800

    enh(serve): add short_description field in params/groups
    Close [TD-25111](https://jira.taosdata.com:18080/browse/TD-25111)

--- src/serve/controller/mod.rs
+++ src/serve/controller/mod.rs
@@ -1843,6 +1843,9 @@ lazy_static::lazy_static! {
         def.push(serde_yaml::from_str(include_str!("../data_sources/en/opcda.yaml")).unwrap());
         def.push(serde_yaml::from_str(include_str!("../data_sources/en/influxdb.yaml")).unwrap());
         def.push(serde_yaml::from_str(include_str!("../data_sources/en/mqtt.yaml")).unwrap());
+        for ds in &mut def {
+            ds.compute();
+        }
         def
     };
     pub static ref DATA_SOURCE_DEFINITIONS_VEC_CN: Vec<DataSourceDefinition> = {
@@ -1854,6 +1857,9 @@ lazy_static::lazy_static! {
         def.push(serde_yaml::from_str(include_str!("../data_sources/cn/opcda.yaml")).unwrap());
         def.push(serde_yaml::from_str(include_str!("../data_sources/cn/influxdb.yaml")).unwrap());
         def.push(serde_yaml::from_str(include_str!("../data_sources/cn/mqtt.yaml")).unwrap());
+        for ds in &mut def {
+            ds.compute();
+        }
         def
     };
     /// This is an example for using doc comment attributes
@@ -1866,6 +1872,9 @@ lazy_static::lazy_static! {
         def.push(serde_yaml::from_str(include_str!("../data_sources/en/opcda.yaml")).unwrap());
         def.push(serde_yaml::from_str(include_str!("../data_sources/en/influxdb.yaml")).unwrap());
         def.push(serde_yaml::from_str(include_str!("../data_sources/en/mqtt.yaml")).unwrap());
+        for ds in &mut def {
+            ds.compute();
+        }
         def.into_iter().map(|ds| (ds.id.to_string(), ds)).collect()
     };
     pub static ref DATA_SOURCE_DEFINITIONS_CN: BTreeMap<String, DataSourceDefinition> = {
@@ -1877,6 +1886,9 @@ lazy_static::lazy_static! {
         def.push(serde_yaml::from_str(include_str!("../data_sources/cn/opcda.yaml")).unwrap());
         def.push(serde_yaml::from_str(include_str!("../data_sources/cn/influxdb.yaml")).unwrap());
         def.push(serde_yaml::from_str(include_str!("../data_sources/cn/mqtt.yaml")).unwrap());
+        for ds in &mut def {
+            ds.compute();
+        }
         def.into_iter().map(|ds| (ds.id.to_string(), ds)).collect()
     };
 }
