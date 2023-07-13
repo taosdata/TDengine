5a09c257128aeb08627696c682350956ae25d8ca
jiajingbin <jiajaybin@126.com>
2023-07-13 20:09:53 +0800

    test: update

--- cases/Caict/performance/10billion_timelines.py
+++ cases/Caict/performance/10billion_timelines.py
@@ -40,17 +40,18 @@ class Timeline_100B(TDCase):
         self.childtable_count1 = 33333333
         self.childtable_count2 = 33333333
         self.childtable_count3 = 33333334
-        self.interlace_rows = 5000
+        self.interlace_rows = 10000
         # self.childtable_count1 = 100
         # self.childtable_count2 = 100
         # self.childtable_count3 = 100
+        self.insert_mode = "stmt"
         self.childtable_prefix1 = "ctb1_"
         self.childtable_prefix2 = "ctb2_"
         self.childtable_prefix3 = "ctb3_"
-        self.insert_rows = 10
+        self.insert_rows = 1
         self.num_of_records_per_req = 10000
         self.batch_create_tbl_num = 10000
-        self.dbname = "test"
+        self.dbname = "dbt"
         self.stbname = "stb"
 
 
@@ -74,7 +75,7 @@ class Timeline_100B(TDCase):
         column_info_list = [
           {
             "type": "INT",
-            "count": 99
+            "count": 100
           }
         ]
         tag_info_list = [
@@ -91,7 +92,7 @@ class Timeline_100B(TDCase):
 
         json_filename_list.append(self.file_name1)
         dbinfo = self.tdCom.setDBinfo(name=self.dbname, replica=self.replica, vgroups=self.vgroups, drop="no")
-        stb_into = [self.tdCom.setStbinfo(columns=column_info_list, tags=tag_info_list, childtable_count=self.childtable_count1, childtable_prefix=self.childtable_prefix1, insert_rows=self.insert_rows, interlace_rows=self.interlace_rows, batch_create_tbl_num=self.batch_create_tbl_num)]
+        stb_into = [self.tdCom.setStbinfo(columns=column_info_list, tags=tag_info_list, childtable_count=self.childtable_count1, childtable_prefix=self.childtable_prefix1, insert_rows=self.insert_rows, interlace_rows=self.interlace_rows, batch_create_tbl_num=self.batch_create_tbl_num, insert_mode=self.insert_mode)]
         database_info = [self.tdCom.setDatabases(dbinfo=dbinfo, super_tables=stb_into)]
         host = self.get_fqdn("taosd")[0]
         json_info1 = self.tdCom.setJsoninfo(host=host, databases=database_info, create_table_thread_count=self.create_table_thread_count, thread_count=self.thread_count, num_of_records_per_req=self.num_of_records_per_req)
@@ -100,7 +101,7 @@ class Timeline_100B(TDCase):
 
         json_filename_list.append(self.file_name2)
         dbinfo = self.tdCom.setDBinfo(name=self.dbname, replica=self.replica, vgroups=self.vgroups, drop="no")
-        stb_into = [self.tdCom.setStbinfo(columns=column_info_list, tags=tag_info_list, childtable_count=self.childtable_count2, childtable_prefix=self.childtable_prefix2, insert_rows=self.insert_rows, interlace_rows=self.interlace_rows, batch_create_tbl_num=self.batch_create_tbl_num)]
+        stb_into = [self.tdCom.setStbinfo(columns=column_info_list, tags=tag_info_list, childtable_count=self.childtable_count2, childtable_prefix=self.childtable_prefix2, insert_rows=self.insert_rows, interlace_rows=self.interlace_rows, batch_create_tbl_num=self.batch_create_tbl_num, insert_mode=self.insert_mode)]
         database_info = [self.tdCom.setDatabases(dbinfo=dbinfo, super_tables=stb_into)]
         json_info2 = self.tdCom.setJsoninfo(host=host, databases=database_info, create_table_thread_count=self.create_table_thread_count, thread_count=self.thread_count, num_of_records_per_req=self.num_of_records_per_req)
         self.tdCom.genBenchmarkJson(self.run_log_dir, self.file_name2, json_info2)
@@ -108,7 +109,7 @@ class Timeline_100B(TDCase):
 
         json_filename_list.append(self.file_name3)
         dbinfo = self.tdCom.setDBinfo(name=self.dbname, replica=self.replica, vgroups=self.vgroups, drop="no")
-        stb_into = [self.tdCom.setStbinfo(columns=column_info_list, tags=tag_info_list, childtable_count=self.childtable_count3, childtable_prefix=self.childtable_prefix3, insert_rows=self.insert_rows, interlace_rows=self.interlace_rows, batch_create_tbl_num=self.batch_create_tbl_num)]
+        stb_into = [self.tdCom.setStbinfo(columns=column_info_list, tags=tag_info_list, childtable_count=self.childtable_count3, childtable_prefix=self.childtable_prefix3, insert_rows=self.insert_rows, interlace_rows=self.interlace_rows, batch_create_tbl_num=self.batch_create_tbl_num, insert_mode=self.insert_mode)]
         database_info = [self.tdCom.setDatabases(dbinfo=dbinfo, super_tables=stb_into)]
         json_info3 = self.tdCom.setJsoninfo(host=host, databases=database_info, create_table_thread_count=self.create_table_thread_count, thread_count=self.thread_count, num_of_records_per_req=self.num_of_records_per_req)
         self.tdCom.genBenchmarkJson(self.run_log_dir, self.file_name3, json_info3)
@@ -117,9 +118,9 @@ class Timeline_100B(TDCase):
         timestamp_start = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
         result_file_list = self.tdCom.threads_run_taosBenchmark(self._remote, taosBenchmark_iplist, json_data_list, json_filename_list, taosBenchmark_env_setting, self.run_log_dir)
         timestamp_end = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
-        env_setting = self.get_component_by_name("prometheus")
-        Insert_file = Perf_Base_func(self._remote._logger, self.run_log_dir)
-        Insert_file.taosBenchmark_insert_summary_result(result_file_list, version="3.0")
-        Insert_file.get_process_exporter_info(env_setting, 30, timestamp_start, timestamp_end)
-        Insert_file.get_node_exporter_info(env_setting, 30, timestamp_start, timestamp_end)
+        # env_setting = self.get_component_by_name("prometheus")
+        # Insert_file = Perf_Base_func(self._remote._logger, self.run_log_dir)
+        # Insert_file.taosBenchmark_insert_summary_result(result_file_list, version="3.0")
+        # Insert_file.get_process_exporter_info(env_setting, 30, timestamp_start, timestamp_end)
+        # Insert_file.get_node_exporter_info(env_setting, 30, timestamp_start, timestamp_end)
         print(self.run_log_dir + '/perf_report.txt')
