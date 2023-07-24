1d23b0215c2f32505cbc847856b65030b0ecfbdc
kailixu <klxu@taosdata.com>
2023-07-24 10:07:15 +0800

    fix: exclude sys/sysinfo.h for mac

--- include/os/os.h
+++ include/os/os.h
@@ -43,7 +43,6 @@ extern "C" {
 #include <sys/shm.h>
 #include <sys/stat.h>
 #include <sys/statvfs.h>
-#include <sys/sysinfo.h>
 #include <sys/time.h>
 #include <sys/types.h>
 #include <sys/utsname.h>
@@ -54,6 +53,7 @@ extern "C" {
 #else
 #include <argp.h>
 #include <sys/prctl.h>
+#include <sys/sysinfo.h>
 #if defined(_TD_X86_)
 #include <cpuid.h>
 #endif
