be4e47e9a708f0571375dabe0809b0aeb23200c6
wangmm0220 <wangmm0220@gmail.com>
2023-07-25 16:15:33 +0800

    fix:add macro ASSERT_NOT_CORE to control ASSERT not core in release version

--- source/dnode/mnode/impl/src/mndSubscribe.c
+++ source/dnode/mnode/impl/src/mndSubscribe.c
@@ -1168,7 +1168,7 @@ static int32_t buildResult(SSDataBlock *pBlock, int32_t* numOfRows, int64_t cons
     pColInfo = taosArrayGet(pBlock->pDataBlock, cols++);
     colDataSetVal(pColInfo, *numOfRows, (const char *)consumerIdHex, consumerId == -1);
     
-    mDebug("mnd show subscriptions: topic %s, consumer:0x%" PRIx64 " cgroup %s vgid %d", varDataVal(topic),
+    mInfo("mnd show subscriptions: topic %s, consumer:0x%" PRIx64 " cgroup %s vgid %d", varDataVal(topic),
            consumerId, varDataVal(cgroup), pVgEp->vgId);
 
     // offset
