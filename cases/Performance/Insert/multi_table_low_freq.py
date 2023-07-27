488214dd997506c0db4aed2c81757710a79a6e5e
jiajingbin <jiajaybin@126.com>
2023-07-27 14:27:00 +0800

    test: update

--- cases/Performance/Insert/multi_table_low_freq.py
+++ cases/Performance/Insert/multi_table_low_freq.py
@@ -75,6 +75,7 @@ class MultiTableLowFreq(TDCase):
         )
         self.result_file_name = self.run_log_dir + '/perf_report.txt'
         self.file_name1 = "insert0.json"
+        self.file_name2 = "insert1.json"
         self._tmp_dir: str = os.path.join(self.run_log_dir, "tmp")
         self.replica = 1
         # self.vgroups = 10
@@ -83,6 +84,7 @@ class MultiTableLowFreq(TDCase):
         self.interlace_rows = 1
         # self.interlace_rows = 0
         self.childtable_prefix = "ctb1_"
+        self.childtable_prefix2 = "ct2_"
         self.disorder_ratio = 10
         self.disorder_range = 864000000
         self.timestamp_step = 86400000
@@ -133,7 +135,38 @@ class MultiTableLowFreq(TDCase):
         env_setting = self.get_component_by_name("prometheus")
         Insert_file.get_process_exporter_info(env_setting, 30, timestamp_start, timestamp_end)
         Insert_file.get_node_exporter_info(env_setting, 10, timestamp_start, timestamp_end)
-    
+
+    def order_insert_2b(self, vgroups, column_info_list, childtable_count):
+        taosBenchmark_iplist = self.get_fqdn("taosBenchmark")
+        taosBenchmark_env_setting = self.get_component_by_name("taosBenchmark")
+        self.tdCom.createDb(dbname=self.dbname1, replica=self.replica, vgroups=vgroups, buffer=self.buffer, stt_trigger=self.stt_trigger)
+
+        self.json_filename_list.append(self.file_name1)
+        dbinfo = self.tdCom.setDBinfo(name=self.dbname1, replica=self.replica, vgroups=vgroups, buffer=self.buffer, stt_trigger=self.stt_trigger, drop="no")
+        stb_into = [self.tdCom.setStbinfo(columns=column_info_list, tags=self.tag_info_list, childtable_count=int(childtable_count/2), childtable_prefix=self.childtable_prefix, insert_rows=int(self.insert_rows/2), batch_create_tbl_num=self.batch_create_tbl_num, insert_mode=self.insert_mode, interlace_rows=self.interlace_rows, timestamp_step=self.timestamp_step)]
+        database_info = [self.tdCom.setDatabases(dbinfo=dbinfo, super_tables=stb_into)]
+
+        json_info1 = self.tdCom.setJsoninfo(host=self.taosd_host, databases=database_info, create_table_thread_count=self.create_table_thread_count, thread_count=self.thread_count, num_of_records_per_req=self.num_of_records_per_req)
+        self.tdCom.genBenchmarkJson(self.run_log_dir, self.file_name1, json_info1)
+        self.json_data_list.append(json_info1)
+
+        self.json_filename_list.append(self.file_name2)
+        stb_into = [self.tdCom.setStbinfo(columns=column_info_list, tags=self.tag_info_list, childtable_count=int(childtable_count/2), childtable_prefix=self.childtable_prefix2, insert_rows=int(self.insert_rows/2), batch_create_tbl_num=self.batch_create_tbl_num, insert_mode=self.insert_mode, interlace_rows=self.interlace_rows, timestamp_step=self.timestamp_step)]
+        database_info = [self.tdCom.setDatabases(dbinfo=dbinfo, super_tables=stb_into)]
+        json_info2 = self.tdCom.setJsoninfo(host=self.taosd_host, databases=database_info, create_table_thread_count=self.create_table_thread_count, thread_count=self.thread_count, num_of_records_per_req=self.num_of_records_per_req)
+        self.tdCom.genBenchmarkJson(self.run_log_dir, self.file_name2, json_info2)
+        self.json_data_list.append(json_info2)
+
+        self.tdCom.put_file(self._remote, taosBenchmark_iplist, self.json_data_list, self.json_filename_list, self.run_log_dir)
+        timestamp_start = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+        result_file_list = self.tdCom.threads_run_taosBenchmark(self._remote, taosBenchmark_iplist, self.json_data_list, self.json_filename_list, taosBenchmark_env_setting, self.run_log_dir)
+        timestamp_end = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+        Insert_file = Perf_Base_func(self._remote._logger, self.run_log_dir)
+        Insert_file.taosBenchmark_insert_summary_result(result_file_list, version="3.0")
+        env_setting = self.get_component_by_name("prometheus")
+        Insert_file.get_process_exporter_info(env_setting, 30, timestamp_start, timestamp_end)
+        Insert_file.get_node_exporter_info(env_setting, 10, timestamp_start, timestamp_end)
+
     def disorder_insert(self, vgroups, column_info_list, childtable_count):
         self.write_log(f'\n****************************** disorder ratio: {self.disorder_ratio} ******************************\n\n')
         taosBenchmark_iplist = self.get_fqdn("taosBenchmark")
@@ -157,7 +190,7 @@ class MultiTableLowFreq(TDCase):
         env_setting = self.get_component_by_name("prometheus")
         Insert_file.get_process_exporter_info(env_setting, 30, timestamp_start, timestamp_end)
         Insert_file.get_node_exporter_info(env_setting, 10, timestamp_start, timestamp_end)
-        
+
     def diff_disorder_insert(self, vgroups, column_info_list, childtable_count, disorder_ratio):
         self.write_log(f'\n****************************** disorder ratio: {disorder_ratio} ******************************\n\n')
         taosBenchmark_iplist = self.get_fqdn("taosBenchmark")
@@ -221,6 +254,14 @@ class MultiTableLowFreq(TDCase):
                 # self.order_insert(vgroups, column_info_list, childtable_count)
                 # self.disorder_insert(vgroups, column_info_list, childtable_count)
         # self.order_insert(40, self.column_info_list1, 10000)
-        for disorder_ratio in [20, 50, 100]:
-            self.diff_disorder_insert(vgroups=10, column_info_list=self.column_info_list1, childtable_count=10000000, disorder_ratio=disorder_ratio)
+        # for disorder_ratio in [20, 50, 100]:
+        #     self.diff_disorder_insert(vgroups=10, column_info_list=self.column_info_list1, childtable_count=10000000, disorder_ratio=disorder_ratio)
+        # self.order_insert_2b(self.vgroups_list[0], self.column_info_list2, self.childtable_count_list[0])
+        self.replica = 3
+        for column_info_list in [self.column_info_list1, self.column_info_list2]:
+            self.write_log(f'\n****************************** column_info_list: {column_info_list} ******************************\n\n')
+            for vgroups, childtable_count in zip(self.vgroups_list, self.childtable_count_list):
+                self.write_log(f'\n\n****************************** vgroups: {vgroups} childtable_count: {childtable_count} ******************************\n\n')
+                self.order_insert(vgroups, column_info_list, childtable_count)
+                self.disorder_insert(vgroups, column_info_list, childtable_count)
         print(self.result_file_name)
