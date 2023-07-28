0e0baa5928f07ba3a32a9fccb3711821451e5c34
lizjiang <ginger_jyl@qq.com>
2023-07-28 17:50:20 +0800

    fix: add timezone

--- src/api/gateway/console.js
+++ src/api/gateway/console.js
@@ -4,7 +4,7 @@ import { compHeadAndData, getLocalTimezone } from "@/utils";
 export function sendSQLReq(sqlStr, composeData = false, appId = store.getters.appId) {
   return request({
     baseURL:'',
-    url: '/rest/sql',
+    url: `/rest/sql?tz=${getLocalTimezone()}`,
     method: 'post',
     headers: {
       "Content-Type": "text/plain"
@@ -22,7 +22,7 @@ export function sendSQLReq(sqlStr, composeData = false, appId = store.getters.ap
 export function executeDBOperations(sql, appId = store.getters.appId) {
   return request({
     baseURL:'',
-    url: '/rest/sql',
+    url: `/rest/sql?tz=${getLocalTimezone()}`,
     method: "post",
     headers: {
       "Content-Type": "text/plain"
