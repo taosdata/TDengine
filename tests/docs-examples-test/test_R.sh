f2728a65d26a4054b7c247f9c74d8ac58d38e6d7
Shuduo Sang <sangshuduo@gmail.com>
2023-07-31 21:34:10 +0800

    test: wget jdbc dist.jar

--- tests/docs-examples-test/test_R.sh
+++ tests/docs-examples-test/test_R.sh
@@ -6,8 +6,9 @@ pgrep taosd || taosd >> /dev/null 2>&1 &
 pgrep taosadapter || taosadapter >> /dev/null 2>&1 &
 
 cd ../../docs/examples/R
+wget https://repo1.maven.org/maven2/com/taosdata/jdbc/taos-jdbcdriver/3.2.4/taos-jdbcdriver-3.2.4-dist.jar
 
-jar_path=`find ../../../../debug/build  -name taos-jdbcdriver-*-dist.jar`
+jar_path=`find . -name taos-jdbcdriver-*-dist.jar`
 echo jar_path=$jar_path
 R -f connect_native.r --args $jar_path
 # R -f connect_rest.r --args $jar_path # bug 14704
