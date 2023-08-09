2403db06b0241aa40874c5e3a0447d3d699d562b
chenhaoran <haoran920c@163.com>
2023-08-09 11:19:56 +0800

    test:fix comatibility case

--- tests/system-test/0-others/compatibility.py
+++ tests/system-test/0-others/compatibility.py
@@ -30,7 +30,15 @@ class TDTestCase:
         self.replicaVar = int(replicaVar)
         tdLog.debug(f"start to excute {__file__}")
         tdSql.init(conn.cursor())
-    
+        self.deletedDataSql= '''drop database if exists deldata;create database deldata duration 300;use deldata;
+                            create table deldata.stb1 (ts timestamp, c1 int, c2 bigint, c3 smallint, c4 tinyint, c5 float, c6 double, c7 bool, c8 binary(16),c9 nchar(32), c10 timestamp) tags (t1 int);
+                            create table deldata.ct1 using deldata.stb1 tags ( 1 );
+                            insert into deldata.ct1 values ( now()-0s, 0, 0, 0, 0, 0.0, 0.0, 0, 'binary0', 'nchar0', now()+0a ) ( now()-10s, 1, 11111, 111, 11, 1.11, 11.11, 1, 'binary1', 'nchar1', now()+1a ) ( now()-20s, 2, 22222, 222, 22, 2.22, 22.22, 0, 'binary2', 'nchar2', now()+2a ) ( now()-30s, 3, 33333, 333, 33, 3.33, 33.33, 1, 'binary3', 'nchar3', now()+3a );
+                            select avg(c1) from deldata.ct1;
+                            delete from deldata.stb1;
+                            flush database deldata;
+                            insert into deldata.ct1 values ( now()-0s, 0, 0, 0, 0, 0.0, 0.0, 0, 'binary0', 'nchar0', now()+0a ) ( now()-10s, 1, 11111, 111, 11, 1.11, 11.11, 1, 'binary1', 'nchar1', now()+1a ) ( now()-20s, 2, 22222, 222, 22, 2.22, 22.22, 0, 'binary2', 'nchar2', now()+2a ) ( now()-30s, 3, 33333, 333, 33, 3.33, 33.33, 1, 'binary3', 'nchar3', now()+3a );
+                            delete from deldata.ct1;'''   
     def checkProcessPid(self,processName):
         i=0
         while i<60:
@@ -154,7 +162,7 @@ class TDTestCase:
         os.system("LD_LIBRARY_PATH=/usr/lib  taos -f 0-others/TS-3131.tsql")
 
         # add deleted  data
-        os.system("LD_LIBRARY_PATH=/usr/lib taos -f  0-others/deletedData.sql")
+        os.system(f'LD_LIBRARY_PATH=/usr/lib taos -s "{self.deletedDataSql}" ')
 
 
         cmd = f" LD_LIBRARY_PATH={bPath}/build/lib  {bPath}/build/bin/taos -h localhost ;"
