b1775a04d79ce6cd43b7e97c1844f479aff5ae29
jiajingbin <jiajaybin@126.com>
2023-08-11 09:33:05 +0800

    test: update

--- cases/stability/insert/scenario_test/stream_stability_test.py
+++ cases/stability/insert/scenario_test/stream_stability_test.py
@@ -190,10 +190,14 @@ class StreamStabilityTest(TDCase):
         Insert_file.get_process_exporter_info(env_setting, 30, timestamp_start, timestamp_end)
         Insert_file.get_node_exporter_info(env_setting, 30, timestamp_start, timestamp_end)
         if self.trigger_mode.lower() == "at_once" and "ignore_expired 1" not in self.stream_sql and int(self.stream_json_info["streams"][0]["fill_history"]) == 1:
-            self.tdSql.query(self.stream_sql)
-            expected_res = self.tdSql.query_row
-            self.tdSql.query(f'select count(*) from {self.dbname}.{self.stream_stbname}')
-            self.tdSql.checkEqual(self.tdSql.query_data[0][0], expected_res)
+            if "partition by" not in self.stream_sql
+                self.tdSql.query(self.stream_sql)
+                expected_res = self.tdSql.query_row
+                self.tdSql.query(f'select count(*) from {self.dbname}.{self.stream_stbname}')
+                self.tdSql.checkEqual(self.tdSql.query_data[0][0], expected_res)
+            else:
+                self.tdSql.query(f'select count(*) from {self.dbname}.{self.stream_stbname}')
+                self.tdSql.checkEqual(self.tdSql.query_data[0][0] > 0, True)
         end_time = datetime.now()
         for host in self.taosBenchmark_iplist:
             self._remote.get(host, json_info["result_file"], self.run_log_dir)
