c60d4c5760430ef62c5f4fc3b287d7bd72d82b9b
suzumiya <jrgwyz@163.com>
2023-08-25 09:25:52 +0800

    fix: update retry wait time and add log info

--- taosx-core/src/legacy/scheduler.rs
+++ taosx-core/src/legacy/scheduler.rs
@@ -123,8 +123,10 @@ async fn worker(
                                         to = target.get().await?;
                                         retries -= 1;
                                         log::warn!(
-                                            "[worker:{worker}] sync stable {stable} error: {err}, retrying"
+                                            "[worker:{worker}] sync stable {stable} error: {err}, retrying ... {retries} times left"
                                         );
+                                        // wait 5 seconds to avoid too many retries
+                                        tokio::time::sleep(std::time::Duration::from_secs(5)).await;
                                         continue;
                                     }
 
@@ -220,7 +222,7 @@ async fn worker(
                                 to = target.get().await?;
                                 retries -= 1;
                                 log::warn!(
-                                    "[worker:{worker}] sync table {table} error: {err}, retrying"
+                                    "[worker:{worker}] sync table {table} error: {err}, retrying ... {retries} times left"
                                 );
                                 continue;
                             }
