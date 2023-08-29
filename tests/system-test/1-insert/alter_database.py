1e4e2549a44979adaaaf03fa81f35b79314a3380
kailixu <klxu@taosdata.com>
2023-08-29 19:53:59 +0800

    chore: add test cases

--- tests/system-test/1-insert/alter_database.py
+++ tests/system-test/1-insert/alter_database.py
@@ -18,7 +18,7 @@ class TDTestCase:
         tdSql.init(conn.cursor(), logSql)
         self.buffer_boundary = [3, 4097, 8193, 12289, 16384]
         self.buffer_error = [self.buffer_boundary[0] -
-                             1, self.buffer_boundary[-1]+1, 256]
+                             1, self.buffer_boundary[-1]+1]
         # pages_boundary >= 64
         self.pages_boundary = [64, 128, 512]
         self.pages_error = [self.pages_boundary[0]-1]
@@ -47,15 +47,40 @@ class TDTestCase:
         tdSql.execute('create database db')
         tdSql.query(
             'select * from information_schema.ins_databases where name = "db"')
-        self.pages_error.append(tdSql.queryResult[0][10])
+        # self.pages_error.append(tdSql.queryResult[0][10])
         for pages in self.pages_error:
             tdSql.error(f'alter database db pages {pages}')
         tdSql.execute('drop database db')
 
+    def alter_same_options(self):
+        tdSql.execute('drop database if exists db')
+        tdSql.execute('create database db')
+        tdSql.query('select * from information_schema.ins_databases where name = "db"')
+        
+        db_options_items = ["replica","keep","buffer","pages","minrows","cachemodel","cachesize","wal_level","wal_fsync_period",
+                      "wal_retention_period","wal_retention_size","stt_trigger"]
+        db_options_result_idx = [4,7,8,10,11,18,19,20,21,22,23,24]
+        
+        self.option_result = []
+        for idx in db_options_result_idx:
+            self.option_result.append(tdSql.queryResult[0][idx])
+        
+        index = 0
+        for option in db_options_items:
+            if option == "cachemodel":
+                option_sql = "alter database db %s '%s'" % (option, self.option_result[index] )
+            else:
+                option_sql = "alter database db %s %s" % (option, self.option_result[index] )
+            tdLog.debug(option_sql)
+            tdSql.query(option_sql)
+            index += 1
+        tdSql.execute('drop database db')
+        
     def run(self):
         
         self.alter_buffer()
         self.alter_pages()
+        self.alter_same_options()
 
     def stop(self):
         tdSql.close()
