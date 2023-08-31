a1e2be8e559b74aa45d51c5212b4adba00ba0c4d
Benguang Zhao <bgzhao@taosdata.com>
2023-08-31 10:25:53 +0800

    fixup: remove minVer and maxVer in FSetWriterConfig

--- source/dnode/vnode/src/inc/vnodeInt.h
+++ source/dnode/vnode/src/inc/vnodeInt.h
@@ -515,16 +515,10 @@ struct SSnapDataHdr {
   uint8_t data[];
 };
 
-typedef struct SRange {
-  int64_t start;
-  int64_t end;
-} SRange;
-
 struct SCommitInfo {
   SVnodeInfo info;
   SVnode*    pVnode;
   TXN*       txn;
-  SRange     vers;
 };
 
 struct SCompactInfo {
