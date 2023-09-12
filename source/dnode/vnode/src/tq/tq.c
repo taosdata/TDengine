8a7d04a306c43d29a4a4b8f0336c8d540b6cb7e0
Haojun Liao <hjliao@taosdata.com>
2023-09-12 18:09:02 +0800

    refactor: do some internal refactor.

--- source/dnode/vnode/src/tq/tq.c
+++ source/dnode/vnode/src/tq/tq.c
@@ -1048,7 +1048,7 @@ int32_t tqProcessTaskScanHistory(STQ* pTq, SRpcMsg* pMsg) {
     ASSERT(pTask->status.pauseAllowed == true);
   }
 
-  streamSourceScanHistoryData(pTask);
+  streamScanHistoryData(pTask);
   if (pTask->status.taskStatus == TASK_STATUS__PAUSE) {
     double el = (taosGetTimestampMs() - pTask->tsInfo.step1Start) / 1000.0;
     tqDebug("s-task:%s is paused in the step1, elapsed time:%.2fs, sched-status:%d", pTask->id.idStr, el,
