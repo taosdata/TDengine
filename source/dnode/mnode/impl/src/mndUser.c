a7d4547a2d72fd681a1293c5b00cb968e23e02be
yihaoDeng <cyang@taosdata.com>
2023-09-14 17:01:06 +0800

    Merge branch 'enh/ipWhiteList' of https://github.com/taosdata/TDengine into enh/ipWhiteList

--- source/dnode/mnode/impl/src/mndUser.c
+++ source/dnode/mnode/impl/src/mndUser.c
@@ -432,8 +432,8 @@ static void ipRangeToStr(SIpV4Range *range, char *buf) {
   addr.s_addr = range->ip;
 
   uv_inet_ntop(AF_INET, &addr, buf, 32);
-  if (range->mask == 32) {
-   sprintf(buf + strlen(buf), "/%d", range->mask);
+  if (range->mask != 32) {
+    sprintf(buf + strlen(buf), "/%d", range->mask);
   }
   return;
 }
