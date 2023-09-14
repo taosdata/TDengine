7ec31029e6fe34bca94385fa571e7f374d5d66c9
yihaoDeng <cyang@taosdata.com>
2023-09-14 08:45:55 +0800

    update test case

--- source/dnode/mnode/impl/src/mndUser.c
+++ source/dnode/mnode/impl/src/mndUser.c
@@ -197,9 +197,9 @@ int64_t mndGetIpWhiteVer(SMnode *pMnode) {
   taosThreadRwlockUnlock(&ipWhiteMgt.rw);
   mInfo("ip-white-mnode ver: %" PRId64 "", ver);
 
-  // if (mndEnableIpWhiteList(pMnode) == 0 || tsEnableWhiteList == false) {
-  //   return 0;
-  // }
+  if (mndEnableIpWhiteList(pMnode) == 0 || tsEnableWhiteList == false) {
+    return 0;
+  }
   return ver;
 }
 
