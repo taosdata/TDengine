c2cfeeea53a46f6e058203770ad3b2ca55c8a026
dmchen <cademfly@hotmail.com>
2023-09-18 14:15:14 +0800

    ci break

--- source/common/src/tmsg.c
+++ source/common/src/tmsg.c
@@ -44,8 +44,8 @@
 
 #define ENCODESQL()                                              \
   do {                                                           \
-    if (tEncodeI32(&encoder, pReq->sqlLen) < 0) return -1;       \
-    if (pReq->sqlLen > 0){                                       \
+    if (pReq->sqlLen > 0 && pReq->sql != NULL){                  \
+      if (tEncodeI32(&encoder, pReq->sqlLen) < 0) return -1;     \
       if (tEncodeCStr(&encoder, pReq->sql) < 0) return -1;       \
     }                                                            \
   } while (0)
