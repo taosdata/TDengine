900aacd428d6316d52986bae2dcc4f2c2bba4256
slzhou <shenglian_zhou@163.com>
2023-09-19 09:40:00 +0800

    fix: fix sanitizer error

--- source/dnode/vnode/src/vnd/vnodeSvr.c
+++ source/dnode/vnode/src/vnd/vnodeSvr.c
@@ -974,6 +974,7 @@ static int32_t vnodeProcessCreateTbReq(SVnode *pVnode, int64_t ver, void *pReq, 
 _exit:
   for (int32_t iReq = 0; iReq < req.nReqs; iReq++) {
     pCreateReq = req.pReqs + iReq;
+    taosMemoryFree(pCreateReq->sql);
     taosMemoryFree(pCreateReq->comment);
     taosArrayDestroy(pCreateReq->ctb.tagName);
   }
