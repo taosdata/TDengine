9445fafe474ab984cc9acad97f63068ee69f5fa7
chenhaoran <haoran920c@163.com>
2023-09-20 14:54:28 +0800

    test: add insertFromCsvOfLength65500 case for TS-3957

--- tests/system-test/1-insert/rowlength64k.py
+++ tests/system-test/1-insert/rowlength64k.py
@@ -32,6 +32,7 @@ class TDTestCase:
         tdSql.init(conn.cursor(), logSql)
 
         self.testcasePath = os.path.split(__file__)[0]
+        self.testcasePath = self.testcasePath.replace('\\', '//')
         self.testcaseFilename = os.path.split(__file__)[-1]
         os.system("rm -rf %s/%s.sql" % (self.testcasePath,self.testcaseFilename))
         
@@ -1263,20 +1264,32 @@ class TDTestCase:
 
         self.ins_query()
 
+    def insertFromCsvOfLength65500(self):  
+        
+        tdLog.info('test insert from csv of length 65500')
+        os.system(f"taosBenchmark -f {self.testcasePath}//tableColumn4096.json")
+        
+        tdSql.execute(f"insert into db4096.ctb00 file '{self.testcasePath}//tableColumn4096csvLength64k.csv'")
+        tdSql.query("select count(*) from db4096.ctb00")
+        tdSql.checkData(0, 0, 1)
+        tdSql.query("select length(c4092) from db4096.ctb00")
+        tdSql.checkData(0, 0, 16375)
+
 
     def run(self):
         tdSql.prepare()
         
         startTime_all = time.time() 
-        self.run_8() 
-        self.run_9() 
-        self.run_1() 
-        self.run_2() 
-        # self.run_3() 
-        # self.run_4() 
-        # self.run_5() 
-        # self.run_6() 
-        # self.run_7() 
+        # self.run_8() 
+        # self.run_9() 
+        # self.run_1() 
+        # self.run_2() 
+        # # self.run_3() 
+        # # self.run_4() 
+        # # self.run_5() 
+        # # self.run_6() 
+        # # self.run_7() 
+        self.insertFromCsvOfLength65500()
         
         endTime_all = time.time()
         print("total time %ds" % (endTime_all - startTime_all))        
