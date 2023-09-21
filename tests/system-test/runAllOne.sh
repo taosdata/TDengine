995cd971d5aff2785df31f9891207d97487e4d71
facetosea <25808407@qq.com>
2023-09-21 15:03:21 +0800

    rest does not support varbinary

--- tests/system-test/runAllOne.sh
+++ tests/system-test/runAllOne.sh
@@ -211,7 +211,6 @@ python3 ./test.py -f 2-query/hyperloglog.py -P -R
 python3 ./test.py -f 2-query/interp.py -P
 python3 ./test.py -f 2-query/interp.py -P -R
 python3 ./test.py -f 2-query/fill.py -P
-python3 ./test.py -f 2-query/fill.py -P -R
 python3 ./test.py -f 2-query/irate.py -P
 python3 ./test.py -f 2-query/irate.py -P -R
 python3 ./test.py -f 2-query/join.py -P
