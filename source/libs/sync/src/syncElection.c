cd2bef9981c93fae346cc91fb9b3ae3d16803f89
Benguang Zhao <bgzhao@taosdata.com>
2023-09-21 18:38:10 +0800

    enh: use waitTime to prevent from starting snapshot too frequently

--- source/libs/sync/src/syncElection.c
+++ source/libs/sync/src/syncElection.c
@@ -72,7 +72,7 @@ static int32_t syncNodeRequestVotePeers(SSyncNode* pNode) {
 
 int32_t syncNodeElect(SSyncNode* pSyncNode) {
   if (pSyncNode->fsmState == SYNC_FSM_STATE_INCOMPLETE) {
-    sNError(pSyncNode, "ignore leader hb timeout due to incomplete fsm state");
+    sNError(pSyncNode, "skip leader election due to incomplete fsm state");
     return -1;
   }
 
