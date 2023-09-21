ac258d20435913b0773ba846db2d09f94bebe247
Benguang Zhao <bgzhao@taosdata.com>
2023-09-21 09:43:57 +0800

    enh: replicate a fset if it is corrupted without valid minVer

--- source/dnode/vnode/src/tsdb/tsdbFS2.c
+++ source/dnode/vnode/src/tsdb/tsdbFS2.c
@@ -482,7 +482,7 @@ static int32_t tsdbFSDoSanAndFix(STFileSystem *fs) {
         STFileObj *fobj = fset->farr[ftype];
         code = tsdbFSDoScanAndFixFile(fs, fobj);
         if (code) {
-          fset->maxVerValid = TMIN(fset->maxVerValid, fobj->f->minVer - 1);
+          fset->maxVerValid = (fobj->f->minVer <= fobj->f->maxVer) ? TMIN(fset->maxVerValid, fobj->f->minVer - 1) : -1;
           corrupt = true;
         }
       }
@@ -494,7 +494,7 @@ static int32_t tsdbFSDoSanAndFix(STFileSystem *fs) {
         TARRAY2_FOREACH(lvl->fobjArr, fobj) {
           code = tsdbFSDoScanAndFixFile(fs, fobj);
           if (code) {
-            fset->maxVerValid = TMIN(fset->maxVerValid, fobj->f->minVer - 1);
+            fset->maxVerValid = (fobj->f->minVer <= fobj->f->maxVer) ? TMIN(fset->maxVerValid, fobj->f->minVer - 1) : -1;
             corrupt = true;
           }
         }
