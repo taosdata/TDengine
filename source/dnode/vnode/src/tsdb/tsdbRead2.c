0a8f4530f7b9d1c1c2671f8d3d54e605ea612cf6
Haojun Liao <hjliao@taosdata.com>
2023-09-28 11:42:19 +0800

    fix(tsdb): replace destroy with reset.

--- source/dnode/vnode/src/tsdb/tsdbRead2.c
+++ source/dnode/vnode/src/tsdb/tsdbRead2.c
@@ -4605,14 +4605,14 @@ static SSDataBlock* doRetrieveDataBlock(STsdbReader* pReader) {
 
   code = doLoadFileBlockData(pReader, &pStatus->blockIter, &pStatus->fileBlockData, pBlockScanInfo->uid);
   if (code != TSDB_CODE_SUCCESS) {
-    tBlockDataDestroy(&pStatus->fileBlockData);
+    tBlockDataReset(&pStatus->fileBlockData);
     terrno = code;
     return NULL;
   }
 
   code = copyBlockDataToSDataBlock(pReader);
   if (code != TSDB_CODE_SUCCESS) {
-    tBlockDataDestroy(&pStatus->fileBlockData);
+    tBlockDataReset(&pStatus->fileBlockData);
     terrno = code;
     return NULL;
   }
