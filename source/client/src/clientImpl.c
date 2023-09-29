25a871104567888dd7b0ca2362e093f9e69dd6b0
kailixu <klxu@taosdata.com>
2023-09-30 05:36:25 +0800

    chore: log optimization

--- source/client/src/clientImpl.c
+++ source/client/src/clientImpl.c
@@ -158,7 +158,7 @@ STscObj* taos_connect_internal(const char* ip, const char* user, const char* pas
 
     pInst = &p;
   } else {
-    ASSERTS((*pInst) && (*pInst)->pAppHbMgr, "ppInst:%p, pAppHgMgr:%p", *pInst, (*pInst) ? (*pInst)->pAppHbMgr : NULL);
+    ASSERTS((*pInst) && (*pInst)->pAppHbMgr, "*pInst:%p, pAppHgMgr:%p", *pInst, (*pInst) ? (*pInst)->pAppHbMgr : NULL);
     // reset to -1 in case of conn with duplicated user key with its user has ever been dropped.
     atomic_store_8(&(*pInst)->pAppHbMgr->connHbFlag, -1); 
   }
