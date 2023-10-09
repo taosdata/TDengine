b12c734f096b348143a475969c138ee841b9c2a9
wangmm0220 <wangmm0220@gmail.com>
2023-10-09 21:14:32 +0800

    feat:[TD-26056] add replay logic

--- utils/test/c/tmqSim.c
+++ utils/test/c/tmqSim.c
@@ -621,7 +621,7 @@ static int32_t data_msg_process(TAOS_RES* msg, SThreadInfo* pInfo, int32_t msgIn
     taos_print_row(buf, row, fields, numOfFields);
 
     if (0 != g_stConfInfo.showRowFlag) {
-      taosFprintfFile(g_fp, "%lld tbname:%s, rows[%d]: %s\n", taosGetTimestampMs(), (tbName != NULL ? tbName : "null table"), totalRows, buf);
+      taosFprintfFile(g_fp, "time:%" PRId64 " tbname:%s, rows[%d]: %s\n", taosGetTimestampMs(), (tbName != NULL ? tbName : "null table"), totalRows, buf);
       // if (0 != g_stConfInfo.saveRowFlag) {
       //   saveConsumeContentToTbl(pInfo, buf);
       // }
