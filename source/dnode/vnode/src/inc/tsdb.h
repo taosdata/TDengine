6b86d050f211ce8dac31d596096f27d844030ac7
Haojun Liao <hjliao@taosdata.com>
2023-10-16 11:48:57 +0800

    fix(tsdb): record the pinned block iter.

--- source/dnode/vnode/src/inc/tsdb.h
+++ source/dnode/vnode/src/inc/tsdb.h
@@ -746,13 +746,12 @@ typedef struct SSttBlockLoadInfo {
 } SSttBlockLoadInfo;
 
 typedef struct SMergeTree {
-  int8_t             backward;
-  SRBTree            rbt;
-  SLDataIter        *pIter;
-  bool               destroyLoadInfo;
-  SSttBlockLoadInfo *pLoadInfo;
-  const char        *idStr;
-  bool               ignoreEarlierTs;
+  int8_t      backward;
+  SRBTree     rbt;
+  SLDataIter *pIter;
+  SLDataIter *pPinnedBlockIter;
+  const char *idStr;
+  bool        ignoreEarlierTs;
 } SMergeTree;
 
 typedef struct {
