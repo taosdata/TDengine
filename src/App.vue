78dd2108b10a942266c38d7bd3893e25bedee294
lizjiang <ginger_jyl@qq.com>
2023-10-18 19:45:48 +0800

    fix: clear locastorage

--- src/App.vue
+++ src/App.vue
@@ -56,6 +56,10 @@ export default {
         link.remove();
       }
     });
+    window.onbeforeunload=function(){
+      localStorage.clear()
+      sessionStorage.clear()
+    }
   }
 };
 </script>
