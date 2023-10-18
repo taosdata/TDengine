e7d9de06cb0f73f9fb5148f30694f675dfcfee03
chenhaoran <haoran920c@163.com>
2023-10-18 15:59:04 +0800

    test: support replica3 in splited/transform tmqvnode

--- tests/system-test/7-tmq/tmqVnodeTransform-db.py
+++ tests/system-test/7-tmq/tmqVnodeTransform-db.py
@@ -123,24 +123,25 @@ class TDTestCase:
         tdSql.query(redistributeSql)
         tdLog.debug("redistributeSql ok")
 
-    def tmqCaseStable(self):
-        tdLog.printNoPrefix("======== test case 1: ")
+
+    def tmqCaseStableSelect(self):
+        tdLog.printNoPrefix("======== test case 3 subscrib column start : ")
         paraDict = {'dbName':     'dbt',
                     'dropFlag':   1,
                     'event':      '',
                     'vgroups':    1,
-                    'stbName':    'stb',
+                    'stbName':    'stbn',
                     'colPrefix':  'c',
                     'tagPrefix':  't',
                     'colSchema':   [{'type': 'INT', 'count':1},{'type': 'BIGINT', 'count':1},{'type': 'DOUBLE', 'count':1},{'type': 'BINARY', 'len':32, 'count':1},{'type': 'NCHAR', 'len':32, 'count':1},{'type': 'TIMESTAMP', 'count':1}],
                     'tagSchema':   [{'type': 'INT', 'count':1},{'type': 'BIGINT', 'count':1},{'type': 'DOUBLE', 'count':1},{'type': 'BINARY', 'len':32, 'count':1},{'type': 'NCHAR', 'len':32, 'count':1}],
-                    'ctbPrefix':  'ctb1',
+                    'ctbPrefix':  'ctb',
                     'ctbStartIdx': 0,
                     'ctbNum':     10,
                     'rowsPerTbl': 10000,
                     'batchNum':   10,
                     'startTs':    1640966400000,  # 2022-01-01 00:00:00.000
-                    'pollDelay':  30,
+                    'pollDelay':  10,
                     'showMsg':    1,
                     'showRow':    1,
                     'snapshot':   0}
@@ -149,23 +150,30 @@ class TDTestCase:
         paraDict['ctbNum'] = self.ctbNum
         paraDict['rowsPerTbl'] = self.rowsPerTbl
 
-        topicNameList = ['topic1']
-        # expectRowsList = []
+        topicNameList = ['topic3']
         tmqCom.initConsumerTable()
 
+        tdLog.info("create stb")
+        tmqCom.create_stable(tdSql, dbName=paraDict["dbName"],stbName=paraDict["stbName"])
+
+        tdLog.info("create ctb")
+        tmqCom.create_ctable(tdSql, dbName=paraDict["dbName"],stbName=paraDict["stbName"],ctbPrefix=paraDict['ctbPrefix'],
+                             ctbNum=paraDict["ctbNum"],ctbStartIdx=paraDict['ctbStartIdx'])
+        tdLog.info("insert data")
+        tmqCom.insert_data_interlaceByMultiTbl(tsql=tdSql,dbName=paraDict["dbName"],ctbPrefix=paraDict["ctbPrefix"],
+                                               ctbNum=paraDict["ctbNum"],rowsPerTbl=paraDict["rowsPerTbl"],batchNum=paraDict["batchNum"],
+                                               startTs=paraDict["startTs"],ctbStartIdx=paraDict['ctbStartIdx'])
+
         tdLog.info("create topics from stb with filter")
-        queryString = "select * from %s.%s"%(paraDict['dbName'], paraDict['stbName'])
-        # sqlString = "create topic %s as stable %s" %(topicNameList[0], paraDict['stbName'])
+        queryString = "select * from %s.%s where c2 > 0 "%(paraDict['dbName'], paraDict['stbName'])
         sqlString = "create topic %s as %s" %(topicNameList[0], queryString)
         tdLog.info("create topic sql: %s"%sqlString)
         tdSql.execute(sqlString)
-        # tdSql.query(queryString)
-        # expectRowsList.append(tdSql.getRows())
 
         # init consume info, and start tmq_sim, then check consume result
         tdLog.info("insert consume info to consume processor")
         consumerId   = 0
-        expectrowcnt = paraDict["rowsPerTbl"] * paraDict["ctbNum"] * 2
+        expectrowcnt = paraDict["rowsPerTbl"] * paraDict["ctbNum"]
         topicList    = topicNameList[0]
         ifcheckdata  = 1
         ifManualCommit = 1
@@ -176,91 +184,109 @@ class TDTestCase:
         tmqCom.startTmqSimProcess(pollDelay=paraDict['pollDelay'],dbName=paraDict["dbName"],showMsg=paraDict['showMsg'], showRow=paraDict['showRow'],snapshot=paraDict['snapshot'])
         tdLog.info("wait the consume result")
 
-        tdLog.info("create ctb1")
-        tmqCom.create_ctable(tdSql, dbName=paraDict["dbName"],stbName=paraDict["stbName"],ctbPrefix=paraDict['ctbPrefix'],
-                             ctbNum=paraDict["ctbNum"],ctbStartIdx=paraDict['ctbStartIdx'])
-        
-        tdLog.info("create ctb2")
-        paraDict2 = paraDict.copy()
-        paraDict2['ctbPrefix'] = "ctb2"
-        tmqCom.create_ctable(tdSql, dbName=paraDict["dbName"],stbName=paraDict["stbName"],ctbPrefix=paraDict2['ctbPrefix'],
-                             ctbNum=paraDict["ctbNum"],ctbStartIdx=paraDict['ctbStartIdx'])
-        
-        tdLog.info("insert data")
-        pInsertThread = tmqCom.asyncInsertDataByInterlace(paraDict)
-
-        tmqCom.getStartConsumeNotifyFromTmqsim()
-        tmqCom.getStartCommitNotifyFromTmqsim()
-
+        time.sleep(1)
         #restart dnode & remove wal
         # self.restartAndRemoveWal()
 
         # redistribute vgroup
         self.redistributeVgroups()
 
-
-        tdLog.info("insert data")
-        pInsertThread1 = tmqCom.asyncInsertDataByInterlace(paraDict2)
-        pInsertThread.join()
-        pInsertThread1.join()
-
-        expectRows = 1
+        tdLog.info("start consume processor")
+        tmqCom.startTmqSimProcess(pollDelay=paraDict['pollDelay'],dbName=paraDict["dbName"],showMsg=paraDict['showMsg'], showRow=paraDict['showRow'],snapshot=paraDict['snapshot'])
+        tdLog.info("wait the consume result")
+        expectRows = 2
         resultList = tmqCom.selectConsumeResult(expectRows)
-
-        if expectrowcnt / 2 >= resultList[0]:
-            tdLog.info("expect consume rows: %d, act consume rows: %d"%(expectrowcnt / 2, resultList[0]))
-            tdLog.exit("%d tmq consume rows error!"%consumerId)
-
-        # tmqCom.checkFileContent(consumerId, queryString)
-
-        time.sleep(5)
+        
+        time.sleep(6)
         for i in range(len(topicNameList)):
             tdSql.query("drop topic %s"%topicNameList[i])
 
-        tdLog.printNoPrefix("======== test case 1 end ...... ")
+        tdLog.printNoPrefix("======== test case 3 subscrib column end ...... ")
 
-    def tmqCaseNtable(self):
-        tdLog.printNoPrefix("======== test case 2: ")
-        paraDict = {'dbName':'dbt'}
+    def tmqCaseDbname(self):
+        tdLog.printNoPrefix("======== test case  4 subscrib Dbname start: ")
+        paraDict = {'dbName':     'dbt',
+                    'dropFlag':   1,
+                    'event':      '',
+                    'vgroups':    1,
+                    'stbName':    'stbn',
+                    'colPrefix':  'c',
+                    'tagPrefix':  't',
+                    'colSchema':   [{'type': 'INT', 'count':1},{'type': 'BIGINT', 'count':1},{'type': 'DOUBLE', 'count':1},{'type': 'BINARY', 'len':32, 'count':1},{'type': 'NCHAR', 'len':32, 'count':1},{'type': 'TIMESTAMP', 'count':1}],
+                    'tagSchema':   [{'type': 'INT', 'count':1},{'type': 'BIGINT', 'count':1},{'type': 'DOUBLE', 'count':1},{'type': 'BINARY', 'len':32, 'count':1},{'type': 'NCHAR', 'len':32, 'count':1}],
+                    'ctbPrefix':  'ctb',
+                    'ctbStartIdx': 0,
+                    'ctbNum':     10,
+                    'rowsPerTbl': 10000,
+                    'batchNum':   10,
+                    'startTs':    1640966400000,  # 2022-01-01 00:00:00.000
+                    'pollDelay':  10,
+                    'showMsg':    1,
+                    'showRow':    1,
+                    'snapshot':   0}
 
-        ntbName = "ntb"
+        paraDict['vgroups'] = self.vgroups
+        paraDict['ctbNum'] = self.ctbNum
+        paraDict['rowsPerTbl'] = self.rowsPerTbl
 
-        topicNameList = ['topic2']
+        topicNameList = ['topic4']
         tmqCom.initConsumerTable()
 
-        sqlString = "create table %s.%s(ts timestamp, i nchar(8))" %(paraDict['dbName'], ntbName)
-        tdLog.info("create nomal table sql: %s"%sqlString)
-        tdSql.execute(sqlString)
+        tdLog.info("create stb")
+        tmqCom.create_stable(tdSql, dbName=paraDict["dbName"],stbName=paraDict["stbName"])
 
-        tdLog.info("create topics from nomal table")
-        queryString = "select * from %s.%s"%(paraDict['dbName'], ntbName)
+        tdLog.info("create ctb")
+        tmqCom.create_ctable(tdSql, dbName=paraDict["dbName"],stbName=paraDict["stbName"],ctbPrefix=paraDict['ctbPrefix'],
+                             ctbNum=paraDict["ctbNum"],ctbStartIdx=paraDict['ctbStartIdx'])
+        tdLog.info("insert data")
+        tmqCom.insert_data_interlaceByMultiTbl(tsql=tdSql,dbName=paraDict["dbName"],ctbPrefix=paraDict["ctbPrefix"],
+                                               ctbNum=paraDict["ctbNum"],rowsPerTbl=paraDict["rowsPerTbl"],batchNum=paraDict["batchNum"],
+                                               startTs=paraDict["startTs"],ctbStartIdx=paraDict['ctbStartIdx'])
+
+        tdLog.info("create topics from database ")
+        queryString = "database  %s "%(paraDict['dbName'])
         sqlString = "create topic %s as %s" %(topicNameList[0], queryString)
         tdLog.info("create topic sql: %s"%sqlString)
         tdSql.execute(sqlString)
-        tdSql.query("flush database %s"%(paraDict['dbName']))
+
+        # init consume info, and start tmq_sim, then check consume result
+        tdLog.info("insert consume info to consume processor")
+        consumerId   = 0
+        expectrowcnt = paraDict["rowsPerTbl"] * paraDict["ctbNum"]
+        topicList    = topicNameList[0]
+        ifcheckdata  = 1
+        ifManualCommit = 1
+        keyList      = 'group.id:cgrp1, enable.auto.commit:true, auto.commit.interval.ms:200, auto.offset.reset:earliest'
+        tmqCom.insertConsumerInfo(consumerId, expectrowcnt,topicList,keyList,ifcheckdata,ifManualCommit)
+
+        tdLog.info("start consume processor")
+        tmqCom.startTmqSimProcess(pollDelay=paraDict['pollDelay'],dbName=paraDict["dbName"],showMsg=paraDict['showMsg'], showRow=paraDict['showRow'],snapshot=paraDict['snapshot'])
+        tdLog.info("wait the consume result")
+
+        time.sleep(1)
         #restart dnode & remove wal
         # self.restartAndRemoveWal()
 
         # redistribute vgroup
         self.redistributeVgroups()
 
-        sqlString = "alter table %s.%s modify column i nchar(16)" %(paraDict['dbName'], ntbName)
-        tdLog.info("alter table sql: %s"%sqlString)
-        tdSql.error(sqlString)
-        expectRows = 0
+        tdLog.info("start consume processor")
+        tmqCom.startTmqSimProcess(pollDelay=paraDict['pollDelay'],dbName=paraDict["dbName"],showMsg=paraDict['showMsg'], showRow=paraDict['showRow'],snapshot=paraDict['snapshot'])
+        tdLog.info("wait the consume result")
+        expectRows = 2
         resultList = tmqCom.selectConsumeResult(expectRows)
-        time.sleep(5)
+        
+        time.sleep(6)
         for i in range(len(topicNameList)):
             tdSql.query("drop topic %s"%topicNameList[i])
-
-        tdLog.printNoPrefix("======== test case 2 end ...... ")
-
+ 
+        tdLog.printNoPrefix("======== test case 4 subscrib Dbname end ...... ")
 
     def run(self):
         self.prepareTestEnv()
-        self.tmqCaseStable()
+        self.tmqCaseStableSelect()
         self.prepareTestEnv()
-        self.tmqCaseNtable()
+        self.tmqCaseDbname()
         
     def stop(self):
         tdSql.close()
