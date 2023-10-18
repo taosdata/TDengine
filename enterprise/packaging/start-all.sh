a180a41c73791f44ad1cb238082210e401b688af
Ping Xiao <pxiao@taosdata.com>
2023-10-18 19:30:39 +0800

    TD-26785: update start-all.sh and stop-all.sh

--- enterprise/packaging/start-all.sh
+++ enterprise/packaging/start-all.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-sudo systemctl start taosd
-sudo systemctl start taosadapter
-sudo systemctl start taosx
-sudo systemctl start taos-explorer
-sudo systemctl start taoskeeper
+csudo=""
+if command -v sudo >/dev/null; then
+  csudo="sudo "
+fi
+
+prefix="taos"
+
+services=(${prefix}"d" ${prefix}"adapter" ${prefix}"x" ${prefix}"-explorer" ${prefix}"keeper")
+
+
+for service in "${services[@]}"; do    
+    ${csudo}systemctl start $service
+    if systemctl is-active $service >/dev/null; then
+        echo "$service has started"
+    else
+        echo "$service failed to start"
+    fi
+done
--- enterprise/packaging/stop-all.sh
+++ enterprise/packaging/stop-all.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-sudo systemctl stop taosd
-sudo systemctl stop taosadapter
-sudo systemctl stop taosx
-sudo systemctl stop taos-explorer
-sudo systemctl stop taoskeeper
+csudo=""
+if command -v sudo >/dev/null; then
+  csudo="sudo "
+fi
+
+prefix="taos"
+
+services=(${prefix}"d" ${prefix}"adapter" ${prefix}"x" ${prefix}"-explorer" ${prefix}"keeper")
+
+
+for service in "${services[@]}"; do    
+    ${csudo}systemctl stop $service
+    if systemctl is-active $service >/dev/null; then
+        echo "$service failed to stop"
+    else        
+        echo "$service has stoped"
+    fi
+done
