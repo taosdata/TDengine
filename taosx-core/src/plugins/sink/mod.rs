454ad000281618592a4b345fa0f81e7235333e28
suzumiya <jrgwyz@163.com>
2023-10-19 10:23:48 +0800

    chore: update log

--- taosx-core/src/plugins/sink/mod.rs
+++ taosx-core/src/plugins/sink/mod.rs
@@ -448,7 +448,6 @@ async fn consume_lush_record(
                 {
                     for sql in sqls {
                         if let Some(ts) = get_ts_from_sql(sql) {
-                            // tracing::info!("task: {} stable: {} ts: {}", &task, &stable, &ts);
                             let task_clone = task.to_string();
                             let stable_clone = stable.to_string();
                             let ts_clone = ts.clone();
@@ -2259,7 +2258,7 @@ impl IpcStreamWorker {
                 let mut taos = Some(self.pool.get().await?);
                 // let stmt = unsafe { &mut *self.stmt.get() };
                 let task = self.task;
-                tracing::info!("consume lush record task: {task:?}");
+                tracing::debug!("consume lush record task: {task:?}");
                 consume_lush_record(
                     &self.pool,
                     &mut taos,
