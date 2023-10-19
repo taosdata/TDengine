b9b62a9a005982aa908fe7636cd8bb0fcc4ef183
yihaoDeng <yhdeng@taosdata.com>
2023-10-19 10:38:45 +0800

    add rpc sync read timeout

--- source/dnode/mgmt/mgmt_dnode/src/dmHandle.c
+++ source/dnode/mgmt/mgmt_dnode/src/dmHandle.c
@@ -160,7 +160,7 @@ void dmSendStatusReq(SDnodeMgmt *pMgmt) {
 
   SEpSet epSet = {0};
   dmGetMnodeEpSet(pMgmt->pData, &epSet);
-  rpcSendRecv(pMgmt->msgCb.statusClientRpc, &epSet, &rpcMsg, &rpcRsp);
+  rpcSendRecvWithTimeout(pMgmt->msgCb.statusClientRpc, &epSet, &rpcMsg, &rpcRsp, 1000);
   if (rpcRsp.code != 0) {
     dmRotateMnodeEpSet(pMgmt->pData);
     char tbuf[256];
