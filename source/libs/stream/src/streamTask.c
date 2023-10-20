0d4892f25b01c8a8ffed6b6b00917994ae846612
Haojun Liao <hjliao@taosdata.com>
2023-10-20 16:27:39 +0800

    fix(stream): fix bugs caused by refactor sm.

--- source/libs/stream/src/streamTask.c
+++ source/libs/stream/src/streamTask.c
@@ -318,7 +318,7 @@ void tFreeStreamTask(SStreamTask* pTask) {
   // remove the ref by timer
   while (pTask->status.timerActive > 0) {
     stDebug("s-task:%s wait for task stop timer activities, ref:%d", pTask->id.idStr, pTask->status.timerActive);
-    taosMsleep(10);
+    taosMsleep(100);
   }
 
   if (pTask->schedInfo.pTimer != NULL) {
