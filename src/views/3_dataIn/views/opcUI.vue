81c010ee13f1595182c1e5057ee96459b889dc51
lizjiang <ginger_jyl@qq.com>
2023-10-20 16:01:02 +0800

    fix: remove editable status sourcename in datain

--- src/views/3_dataIn/views/opcUI.vue
+++ src/views/3_dataIn/views/opcUI.vue
@@ -11,20 +11,7 @@
         <DataTarget></DataTarget>
       </section>
 
-      <div class="source-name" v-if="isEditable">
-        <div class="block-title">
-          <span>{{ $t("datasource.sourcename") }}</span>
-        </div>
-        <div class="name">
-          <span class="label">{{ $t("name") }}</span>
-          <el-input
-            size="small"
-            v-model="sourceName"
-            placeholder="请输入数据源名称"
-            style="width: 200px"
-          ></el-input>
-        </div>
-      </div>
+      
       <section class="basics" v-if="tagName !== 'csv'">
         <div class="block-title">
           <span>{{ $t("dataIn.connectionConfiguration") }}</span>
@@ -1008,7 +995,6 @@ export default {
       opcfileList: [],
       fileurl: "",
       uploadUrl: process.env.VUE_APP_X_API + `/upload`,
-      sourceName: this.$store.state.app.currentDSName,
       openSSL: false,
       constmqttCols: [],
       textareas: ["ca", "cert", "cert_key"],
