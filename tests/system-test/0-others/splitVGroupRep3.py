33b696dcc5aace2df83f14ebe8688779dd234fa5
Alex Duan <417921451@qq.com>
2023-10-26 14:14:12 +0800

    fix: reduce split vgroup loop to 2

--- tests/system-test/0-others/splitVGroupRep3.py
+++ tests/system-test/0-others/splitVGroupRep3.py
@@ -233,6 +233,7 @@ class TDTestCase:
 
         rowlen1 = len(res1)
         rowlen2 = len(res2)
+        errCnt = 0
 
         if rowlen1 != rowlen2:
             tdLog.exit(f"both row count not equal. rowlen1={rowlen1} rowlen2={rowlen2} ")
@@ -248,8 +249,11 @@ class TDTestCase:
                 return False
             for j in range(collen1):
                 if row1[j] != row2[j]:
-                    tdLog.exit(f"both col not equal. row={i} col={j} col1={row1[j]} col2={row2[j]} .")
-                    return False
+                    tdLog.info(f"error both column value not equal. row={i} col={j} col1={row1[j]} col2={row2[j]} .")
+                    errCnt += 1
+
+        if errCnt > 0:
+            tdLog.exit(f" db2 column value  different with db2. different count ={errCnt} ")
 
         # warning performance
         diff = (spend2 - spend1)*100/spend1
@@ -383,6 +387,7 @@ class TDTestCase:
         self.expectSplitError("streamdb")
         tdSql.execute("drop stream ma;")
         self.expectSplitOk("streamdb")
+
         # topic
         tdLog.info("check forbid split having topic...")
         tdSql.execute("create database topicdb wal_retention_period 10;")
