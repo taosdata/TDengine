82b42b956dae81874a2b6e54665d8eb3a2bfa2b4
slzhou <shenglian_zhou@163.com>
2023-10-26 16:32:28 +0800

    fix: add more back

--- source/libs/parser/src/parUtil.c
+++ source/libs/parser/src/parUtil.c
@@ -186,6 +186,8 @@ static char* getSyntaxErrFormat(int32_t errCode) {
       return "Invalid usage of expr: %s";
     case TSDB_CODE_PAR_INVALID_VARBINARY:
       return "Invalid varbinary type";
+    case TSDB_CODE_PAR_INVALID_IP_RANGE:
+      return "invalid ip range";      
     case TSDB_CODE_OUT_OF_MEMORY:
       return "Out of memory";
     case TSDB_CODE_PAR_ORDERBY_AMBIGUOUS:
