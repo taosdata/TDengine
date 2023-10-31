21b5ea6915daa783f374446cd12349822ed792a7
yihaoDeng <yhdeng@taosdata.com>
2023-10-31 20:49:15 +0800

    fix:print log if assert & check epoch when update epoch

--- source/client/src/clientTmq.c
+++ source/client/src/clientTmq.c
@@ -1442,7 +1442,7 @@ static bool doUpdateLocalEp(tmq_t* tmq, int32_t epoch, const SMqAskEpRsp* pRsp) 
   bool set = false;
 
   int32_t topicNumGet = taosArrayGetSize(pRsp->topics);
-  if (epoch < tmq->epoch) {
+  if (epoch < tmq->epoch || (epoch == tmq->epoch && topicNumGet == 0)) { 
     tscInfo("consumer:0x%" PRIx64 " no update ep epoch from %d to epoch %d, incoming topics:%d",
             tmq->consumerId, tmq->epoch, epoch, topicNumGet);
     return false;
