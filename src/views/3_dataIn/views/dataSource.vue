c35a7579664230fa567d521d82a40b18d564200e
guxiang02 <guxiang_0211@163.com>
2023-11-01 16:37:09 +0800

    fix: 更改任务状态开始停止交互

--- src/views/3_dataIn/views/dataSource.vue
+++ src/views/3_dataIn/views/dataSource.vue
@@ -154,7 +154,7 @@
             >
               <el-tooltip
                 v-if="
-                  ['stopped', 'finished', 'failed'].includes(
+                  showErrStatus.includes(
                     scope.row.status.toLowerCase()
                   )
                 "
@@ -175,7 +175,7 @@
               <span style="width: 80px; display: inline-block" v-else>{{
                 scope.row.status
               }}</span>
-              <template v-if="scope.row.status.toLowerCase() !== 'running'">
+              <template v-if="permitStartStatus.includes(scope.row.status.toLowerCase())">
                 <el-tooltip
                   placement="bottom"
                   effect="light"
@@ -189,7 +189,7 @@
                   ></el-button>
                 </el-tooltip>
               </template>
-              <template v-else>
+              <template v-if="permitStopStatus.includes(scope.row.status.toLowerCase())">
                 <el-tooltip
                   placement="bottom"
                   effect="light"
@@ -342,6 +342,10 @@ export default {
       taskActivities: [],
       expandRowKeys: [],
       metricDisable: false,
+      // 不允许 start/stop 的状态 sopping, suspending
+      permitStartStatus: ['created','failed','stopped','suspended','completed'],
+      permitStopStatus: ['queued','running','interrupted','waiting','resumed'],
+      showErrStatus: ['waiting','suspending','suspended','failed','interupted']
     };
   },
   computed: {
