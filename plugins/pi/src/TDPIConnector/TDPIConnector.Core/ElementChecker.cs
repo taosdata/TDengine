a0820a82bdec8e1da58995bf9daa5b2ce22ea866
facetosea <285808407@qq.com>
2023-11-05 19:39:30 +0800

    fix error when af database is null

--- plugins/pi/src/TDPIConnector/TDPIConnector.Core/ElementChecker.cs
+++ plugins/pi/src/TDPIConnector/TDPIConnector.Core/ElementChecker.cs
@@ -28,7 +28,7 @@ namespace TDPIConnector.Core
         {
             var checkResult = new CheckReslut();
             checkResult.version = PISystemManager.GetPISDKInfo();
-            if (AppSettings.tomlConfig.AFDatabaseName != "" && !DBValid(AppSettings.tomlConfig.AFDatabaseName))
+            if (AppSettings.tomlConfig.AFDatabaseName != null && AppSettings.tomlConfig.AFDatabaseName != "" && !DBValid(AppSettings.tomlConfig.AFDatabaseName))
             {
                 checkResult.avaliable = false;
                 checkResult.since = "AF Database not found";
@@ -41,7 +41,7 @@ namespace TDPIConnector.Core
                 }
 
                 if ((pISystemManager == null || AppSettings.tomlConfig.AFDatabaseName == "") &&
-                    AppSettings.tomlConfig.PointList.Count() > 0)
+                    ( AppSettings.tomlConfig.TemplateForPIPoint.Count() > 0 || AppSettings.tomlConfig.TemplateForAFElement.Count() > 0 ) )
                 {
                     checkResult.items.Add($"AF Server not config correct, template can't used.");
                 }
