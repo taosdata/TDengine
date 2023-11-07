f2b3166e41add0965af807bf510cc2f1f7bad7b0
dapan1121 <wpan@taosdata.com>
2023-11-07 11:20:08 +0800

    fix: stmt query execution mode issue

--- source/client/src/clientImpl.c
+++ source/client/src/clientImpl.c
@@ -1051,6 +1051,7 @@ SRequestObj* launchQueryImpl(SRequestObj* pRequest, SQuery* pQuery, bool keepQue
     }
   }
 
+  pRequest->body.execMode = pQuery->execMode;
   switch (pQuery->execMode) {
     case QUERY_EXEC_MODE_LOCAL:
       if (!pRequest->validateOnly) {
