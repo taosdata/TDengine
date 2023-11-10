5937bdddf58249ee65166d5caacc6da6beffcfaf
Haojun Liao <hjliao@taosdata.com>
2023-11-10 09:46:20 +0800

    refactor:disable test.

--- source/dnode/vnode/src/tq/tq.c
+++ source/dnode/vnode/src/tq/tq.c
@@ -1940,7 +1940,7 @@ int32_t tqProcessTaskUpdateReq(STQ* pTq, SRpcMsg* pMsg) {
       tqDebug("vgId:%d all %d task(s) nodeEp updated and closed", vgId, numOfTasks);
 
       streamMetaWUnLock(pMeta);
-#if 0
+#if 1
       tqStartStreamTaskAsync(pTq, true);
 #else
       // For debug purpose.
