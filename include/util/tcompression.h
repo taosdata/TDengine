65aef15be63fae8c2aa15ef59706e37671b475a3
Haojun Liao <hjliao@taosdata.com>
2023-11-13 00:50:26 +0800

    refactor: do some internal refactor.

--- include/util/tcompression.h
+++ include/util/tcompression.h
@@ -139,7 +139,6 @@ int32_t getWordLength(char type);
 int32_t tsDecompressIntImpl_Hw(const char *const input, const int32_t nelements, char *const output, const char type);
 int32_t tsDecompressFloatImplAvx512(const char *const input, const int32_t nelements, char *const output);
 int32_t tsDecompressFloatImplAvx2(const char *const input, const int32_t nelements, char *const output);
-void tsDecompressFloatHelper(const char *const input, const int32_t nelements, float* ostream);
 
 /*************************************************************************
  *                  STREAM COMPRESSION
