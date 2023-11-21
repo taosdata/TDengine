59038dbf59aa5ddb63a60ef23f6d68f4cfe6bae5
dingbo8128 <dingbo8128@163.com>
2023-11-21 20:23:54 +0800

    feat: add metric ipc.stream.received_batches

--- taosx-core/src/plugins/sink/mod.rs
+++ taosx-core/src/plugins/sink/mod.rs
@@ -2105,7 +2105,7 @@ async fn ipc_flat_stream_reader<R: Read, W: Write>(
         batches += 1;
         let data_trace_id = create_data_trace_id(stream_trace_id, batches);
         let data_trace_id_str: String = get_data_trace_id_str(data_trace_id);
-        info!("Start writing batch {}", data_trace_id_str);
+        info!("Writing batch {}", data_trace_id_str);
         let record = *Box::<dyn Any>::downcast::<FlatMessage>(unsafe {
             std::mem::transmute::<Box<dyn IpcMessage>, Box<dyn Any>>(record)
         })
