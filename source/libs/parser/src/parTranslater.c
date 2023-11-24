08fc9170f59d1af5423e5d4e197100dd1935e750
wangjiaming0909 <604227650@qq.com>
2023-11-24 17:16:59 +0800

    refine

--- source/libs/parser/src/parTranslater.c
+++ source/libs/parser/src/parTranslater.c
@@ -7456,6 +7456,7 @@ static int32_t addWstartToSampleProjects(SNodeList* pProjectionList) {
     return TSDB_CODE_OUT_OF_MEMORY;
   }
   strcpy(pFunc->functionName, "_wstart");
+  strcpy(pFunc->node.userAlias, "_wstart");
   return nodesListPushFront(pProjectionList, (SNode*)pFunc);
 }
 
@@ -7465,6 +7466,7 @@ static int32_t addWendToSampleProjects(SNodeList* pProjectionList) {
     return TSDB_CODE_OUT_OF_MEMORY;
   }
   strcpy(pFunc->functionName, "_wend");
+  strcpy(pFunc->node.userAlias, "_wend");
   return nodesListAppend(pProjectionList, (SNode*)pFunc);
 }
 
@@ -7474,6 +7476,7 @@ static int32_t addWdurationToSampleProjects(SNodeList* pProjectionList) {
     return TSDB_CODE_OUT_OF_MEMORY;
   }
   strcpy(pFunc->functionName, "_wduration");
+  strcpy(pFunc->node.userAlias, "_wduration");
   return nodesListAppend(pProjectionList, (SNode*)pFunc);
 }
 
@@ -10377,9 +10380,10 @@ static int32_t buildTSMAAst(STranslateContext* pCxt, SCreateTSMAStmt* pStmt, SMC
 
   if (TSDB_CODE_SUCCESS == code) {
     // append partition by tbname
-    SNode* pTbnameFunc = createTbnameFunction();
+    SFunctionNode* pTbnameFunc = (SFunctionNode*)createTbnameFunction();
     if (pTbnameFunc) {
-      nodesListMakeAppend(&info.pPartitionByList, pTbnameFunc);
+      sprintf(pTbnameFunc->node.userAlias, "tbname");
+      nodesListMakeAppend(&info.pPartitionByList, (SNode*)pTbnameFunc);
     } else {
       code = TSDB_CODE_OUT_OF_MEMORY;
     }
@@ -10405,7 +10409,7 @@ static int32_t buildTSMAAst(STranslateContext* pCxt, SCreateTSMAStmt* pStmt, SMC
   return code;
 }
 
-static char* defaultTSMAFuncs[4] = {"MAX", "MIN", "SUM", "COUNT"};
+static char* defaultTSMAFuncs[4] = {"max", "min", "sum", "count"};
 
 static int32_t
 translateTSMAFuncs(STranslateContext * pCxt, SCreateTSMAStmt* pStmt, STableMeta* pTableMeta) {
@@ -10456,6 +10460,9 @@ translateTSMAFuncs(STranslateContext * pCxt, SCreateTSMAStmt* pStmt, STableMeta*
           nodesDestroyList(pTSMAFuncs);
           return TSDB_CODE_OUT_OF_MEMORY;
         }
+        // TODO what if exceeds the max size
+        snprintf(pFunc->node.userAlias, TSDB_COL_NAME_LEN, "%s(%s)", pFunc->functionName,
+                 ((SColumnNode*)pNode2)->colName);
       }
     }
     nodesDestroyList(pStmt->pOptions->pFuncs);
