5341d7ad8f9591232799b065b8e87e48e9eb7821
Alex Duan <417921451@qq.com>
2023-12-04 20:39:33 +0800

    fix: memcpy replace strncpy on main branch

--- source/client/src/clientEnv.c
+++ source/client/src/clientEnv.c
@@ -729,7 +729,7 @@ int taos_options_imp(TSDB_OPTION option, const char *str) {
         return -1;
       }
       newstr[0] = '"';
-      strncpy(newstr+1, str, len);
+      memcpy(newstr+1, str, len);
       newstr[len + 1] = '"';
       newstr[len + 2] = '\0';
       str = newstr;
