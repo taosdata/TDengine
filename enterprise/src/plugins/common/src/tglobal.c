7bf440caff51098900592fafb4215158462e9eee
chenhaoran <haoran920c@163.com>
2023-12-06 16:17:00 +0800

    test:replace  strncpy with memcpy

--- enterprise/src/plugins/common/src/tglobal.c
+++ enterprise/src/plugins/common/src/tglobal.c
@@ -18,9 +18,9 @@
 
 int32_t taosSetReleaseCfg(SConfig *pCfg) {
 #ifndef GRANTS_CFG
-  memcpy(tsVersionName, "trial", 16);
+  strncpy(tsVersionName, "trial", 16);
 #else
-  memcpy(tsVersionName, "official", 16);
+  strncpy(tsVersionName, "official", 16);
 #endif
   return 0;
 }
