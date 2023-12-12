d17eda7ad7de166b63dd88558ba9c8b3a23de0ef
Alex Duan <417921451@qq.com>
2023-12-12 10:41:21 +0800

    fix: fix mistaken

--- tests/system-test/1-insert/alter_replica.py
+++ tests/system-test/1-insert/alter_replica.py
@@ -12,7 +12,7 @@ from util.cases import *
 from util.dnodes import *
 
 
-class TDTestCase(TBaseCase):
+class TDTestCase:
     def init(self, conn, logSql, replicaVar=1):
         self.replicaVar = int(replicaVar)
         tdLog.debug("start to execute %s" % __file__)
