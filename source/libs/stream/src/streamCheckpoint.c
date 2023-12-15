5189f3d15b1aea01bd5426a327d72e6db4848f46
yihaoDeng <yhdeng@taosdata.com>
2023-12-15 09:18:37 +0800

    not do chkp when pbackend not inited

--- source/libs/stream/src/streamCheckpoint.c
+++ source/libs/stream/src/streamCheckpoint.c
@@ -425,6 +425,9 @@ int32_t streamTaskUploadChkp(SStreamTask* pTask, int64_t chkpId, char* taskId) {
   if (type == UPLOAD_DISABLE) {
     return 0;
   }
+  if (pTask->pBackend == NULL) {
+    return 0;
+  }
   SAsyncUploadArg* arg = taosMemoryCalloc(1, sizeof(SAsyncUploadArg));
   arg->type = type;
   arg->taskId = taosStrdup(taskId);
