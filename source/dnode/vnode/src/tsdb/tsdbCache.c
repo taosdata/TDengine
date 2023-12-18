5d847830646728e2cd9336c1b917f08bf1b672d2
Minglei Jin <mljin@taosdata.com>
2023-12-18 18:06:40 +0800

    fix(tsdb/cache): fix buffer-overflow with column adding

--- source/dnode/vnode/src/tsdb/tsdbCache.c
+++ source/dnode/vnode/src/tsdb/tsdbCache.c
@@ -2651,6 +2651,13 @@ static int32_t mergeLastCid(tb_uid_t uid, STsdb *pTsdb, SArray **ppLastArray, SC
           break;
         }
         SLastCol *pCol = taosArrayGet(pColArray, iCol);
+        if (slotIds[iCol] > pTSchema->numOfCols - 1) {
+          if (!setNoneCol) {
+            noneCol = iCol;
+            setNoneCol = true;
+          }
+          continue;
+        }
         if (pCol->colVal.cid != pTSchema->columns[slotIds[iCol]].colId) {
           continue;
         }
@@ -2825,6 +2832,9 @@ static int32_t mergeLastRowCid(tb_uid_t uid, STsdb *pTsdb, SArray **ppLastArray,
         break;
       }
       SLastCol *pCol = taosArrayGet(pColArray, iCol);
+      if (slotIds[iCol] > pTSchema->numOfCols - 1) {
+        continue;
+      }
       if (pCol->colVal.cid != pTSchema->columns[slotIds[iCol]].colId) {
         continue;
       }
