49718e6246c818e8f72492c4072d1f732ce1f670
kailixu <klxu@taosdata.com>
2024-01-06 18:19:39 +0800

    enh: support parse integer in str for timestamp

--- source/libs/parser/src/parInsertSql.c
+++ source/libs/parser/src/parInsertSql.c
@@ -229,14 +229,9 @@ static int parseTimestampOrInterval(const char** end, SToken* pToken, int16_t ti
     }
   } else {  // parse the RFC-3339/ISO-8601 timestamp format string
     *isTs = true;
-    if (taosParseTime(pToken->z, ts, pToken->n, timePrec, tsDaylight) != TSDB_CODE_SUCCESS) {
-      if (pToken->type == TK_NK_STRING) {
-        if (TSDB_CODE_SUCCESS != toInteger(pToken->z, pToken->n, 10, ts)) {
-          return buildSyntaxErrMsg(pMsgBuf, "invalid timestamp format", pToken->z);
-        }
-      } else {
-        return buildSyntaxErrMsg(pMsgBuf, "invalid timestamp format", pToken->z);
-      }
+    if (taosParseTime(pToken->z, ts, pToken->n, timePrec, tsDaylight) != TSDB_CODE_SUCCESS ||
+        (pToken->type == TK_NK_STRING && TSDB_CODE_SUCCESS != toInteger(pToken->z, pToken->n, 10, ts))) {
+      return buildSyntaxErrMsg(pMsgBuf, "invalid timestamp format", pToken->z);
     }
   }
 
