b5e5167b8fb845ffc0bd266e457c4abda533222e
Yihao Deng <luomoxyz@126.com>
2024-01-10 09:57:08 +0000

    split sync/status channel

--- source/dnode/mgmt/node_mgmt/src/dmMgmt.c
+++ source/dnode/mgmt/node_mgmt/src/dmMgmt.c
@@ -93,6 +93,9 @@ int32_t dmInitDnode(SDnode *pDnode) {
   indexInit(tsNumOfCommitThreads);
   streamMetaInit();
 
+  dmInitStatusClient(pDnode);
+  dmInitSyncClient(pDnode);  
+
   dmReportStartup("dnode-transport", "initialized");
   dDebug("dnode is created, ptr:%p", pDnode);
   code = 0;
@@ -112,7 +115,9 @@ void dmCleanupDnode(SDnode *pDnode) {
 
   dmCleanupClient(pDnode);
   dmCleanupStatusClient(pDnode);
+  dmCleanupSyncClient(pDnode);
   dmCleanupServer(pDnode);
+
   dmClearVars(pDnode);
   rpcCleanup();
   streamMetaCleanup();
