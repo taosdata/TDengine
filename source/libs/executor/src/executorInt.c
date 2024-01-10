f3ba97f1013d5de53f7d8c04beb4f273a3593f26
slzhou <shenglian_zhou@163.com>
2024-01-10 21:36:09 +0800

    fix: recalcuate window ekey

--- source/libs/executor/src/executorInt.c
+++ source/libs/executor/src/executorInt.c
@@ -449,7 +449,13 @@ STimeWindow getAlignQueryTimeWindow(const SInterval* pInterval, int64_t key) {
    * if the realSkey > INT64_MAX - pInterval->interval, the query duration between
    * realSkey and realEkey must be less than one interval.Therefore, no need to adjust the query ranges.
    */
-  win.ekey = taosTimeAdd(win.skey, pInterval->interval, pInterval->intervalUnit, pInterval->precision) - 1;
+  if (pInterval->offset > 0) {
+    int64_t slideStart = taosTimeAdd(win.skey, -1 * pInterval->offset, pInterval->offsetUnit, pInterval->precision);
+    int64_t slideEnd = taosTimeAdd(slideStart, pInterval->interval, pInterval->intervalUnit, pInterval->precision) - 1;
+    win.ekey = taosTimeAdd(slideEnd, pInterval->offset, pInterval->offsetUnit, pInterval->precision);
+  } else {
+    win.ekey = taosTimeAdd(win.skey, pInterval->interval, pInterval->intervalUnit, pInterval->precision) - 1;
+  }
   if (win.ekey < win.skey) {
     win.ekey = INT64_MAX;
   }
