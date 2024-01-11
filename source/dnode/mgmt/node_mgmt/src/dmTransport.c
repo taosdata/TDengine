5a1510aa2fd7769d6916cac544455d20268fc4d2
Yihao Deng <luomoxyz@126.com>
2024-01-11 07:30:03 +0000

    split sync/status channel

--- source/dnode/mgmt/node_mgmt/src/dmTransport.c
+++ source/dnode/mgmt/node_mgmt/src/dmTransport.c
@@ -407,7 +407,7 @@ int32_t dmInitStatusClient(SDnode *pDnode) {
   SDnodeTrans *pTrans = &pDnode->trans;
 
   SRpcInit rpcInit = {0};
-  rpcInit.label = "DND-STATUS-CLI";
+  rpcInit.label = "DNODE-STA-CLI";
   rpcInit.numOfThreads = 1;
   rpcInit.cfp = (RpcCfp)dmProcessRpcMsg;
   rpcInit.sessions = 1024;
@@ -452,7 +452,7 @@ int32_t dmInitSyncClient(SDnode *pDnode) {
   SDnodeTrans *pTrans = &pDnode->trans;
 
   SRpcInit rpcInit = {0};
-  rpcInit.label = "DND-SYNC-CLI";
+  rpcInit.label = "DNODE-SYNC-CLI";
   rpcInit.numOfThreads = tsNumOfRpcThreads / 2;
   rpcInit.cfp = (RpcCfp)dmProcessRpcMsg;
   rpcInit.sessions = 1024;
