162c7be2915a92519d73fe185ba138cfe4d52620
dapan1121 <wpan@taosdata.com>
2024-01-22 17:07:47 +0800

    fix: mac compile issue

--- source/libs/planner/src/planOptimizer.c
+++ source/libs/planner/src/planOptimizer.c
@@ -337,7 +337,7 @@ static void scanPathOptSetScanOrder(EScanOrder scanOrder, SScanLogicNode* pScan)
   if (pScan->sortPrimaryKey || pScan->scanSeq[0] > 1 || pScan->scanSeq[1] > 1) {
     return;
   }
-  pScan->node.outputTsOrder = scanOrder;
+  pScan->node.outputTsOrder = (SCAN_ORDER_ASC == scanOrder) ? ORDER_ASC : ORDER_DESC;
   switch (scanOrder) {
     case SCAN_ORDER_ASC:
       pScan->scanSeq[0] = 1;
@@ -1465,12 +1465,12 @@ static int32_t sortForJoinOptimizeImpl(SOptimizeContext* pCxt, SLogicSubplan* pL
       case SCAN_ORDER_ASC:
         pScan->scanSeq[0] = 0;
         pScan->scanSeq[1] = 1;
-        pScan->node.outputTsOrder = SCAN_ORDER_DESC;
+        pScan->node.outputTsOrder = ORDER_DESC;
         goto _return;
       case SCAN_ORDER_DESC:
         pScan->scanSeq[0] = 1;
         pScan->scanSeq[1] = 0;
-        pScan->node.outputTsOrder = SCAN_ORDER_ASC;
+        pScan->node.outputTsOrder = ORDER_ASC;
         goto _return;
       default:
         break;
