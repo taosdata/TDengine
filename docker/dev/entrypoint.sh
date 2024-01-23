7e73044978768fc9eccc48a0b75fc934f77e2438
Huo Linhe <linhehuo@gmail.com>
2024-01-23 17:07:39 +0800

    ci: add .dockerignore to speedup docker build

--- docker/dev/entrypoint.sh
+++ docker/dev/entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-source /opt/rh/devtoolset-11/enable
+. /opt/rh/devtoolset-11/enable
+ln /opt/rh/devtoolset-11/root/usr/lib/gcc/x86_64-redhat-linux/11/libgcc.a /usr/lib64/
+ln /opt/rh/devtoolset-11/root/usr/lib/gcc/x86_64-redhat-linux/11/libgcc_s.so /usr/lib64/
+ln /opt/rh/devtoolset-11/root/usr/lib/gcc/x86_64-redhat-linux/11/crtendS.o /usr/lib64/
+ln /opt/rh/devtoolset-11/root/usr/lib/gcc/x86_64-redhat-linux/11/crtbeginS.o /usr/lib64/
+
+set -x
 $@
+sccache -s
