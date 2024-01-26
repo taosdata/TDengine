775e5f2372a1d07158d5315e9c28e9b92e683005
haoranchen <haoran920c@163.com>
2024-01-26 12:40:27 +0800

    Update sigint_stop_dnodes.sh

--- tests/script/sh/sigint_stop_dnodes.sh
+++ tests/script/sh/sigint_stop_dnodes.sh
@@ -4,13 +4,14 @@ set +e
 #set -x
 
 unset LD_PRELOAD
-UNAME_BIN=`which uname`
-OS_TYPE=`$UNAME_BIN`
+# UNAME_BIN=`which uname`
+# OS_TYPE=$($UNAME_BIN)
 
-PID=`ps -ef|grep -w taosd | grep -v grep | awk '{print $2}'`
-echo "Killing taosd processes " $PID
+PID=$(pgrep |grep -w taosd  | awk '{print $2}')
+echo "Killing taosd processes " "$PID"
 while [ -n "$PID" ]; do
   #echo "Killing taosd processes " $PID
-  kill $PID
-  PID=`ps -ef|grep -w taosd | grep -v grep | awk '{print $2}'`
+  kill "$PID"
+  PID=$(pgrep -ef|grep -w taosd | awk '{print $2}')
+  echo "taosd processes" "$PID"
 done
