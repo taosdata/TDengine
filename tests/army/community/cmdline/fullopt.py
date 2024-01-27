9536a1ccf916831ae6efafca771ffbaa11923308
Alex Duan <51781608+DuanKuanJun@users.noreply.github.com>
2024-01-27 09:43:45 +0800

    modify runBinFile to exeBinFile

--- tests/army/community/cmdline/fullopt.py
+++ tests/army/community/cmdline/fullopt.py
@@ -91,7 +91,7 @@ class TDTestCase(TBase):
         # -C
         etool.exeBinFile("taosd", "-C")
         # -k 
-        etool.runBinFile("taosd", "-k", wait=False)
+        etool.exeBinFile("taosd", "-k", False)
         # -V
         rets = etool.runBinFile("taosd", "-V")
         self.checkListNotEmpty(rets)
