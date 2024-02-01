1e30ac9408631cadc098a627a3c43fa5f7b0c00e
slzhou <shenglian_zhou@163.com>
2024-02-01 10:30:07 +0800

    fix: compilation error

--- source/libs/planner/src/planLogicCreater.c
+++ source/libs/planner/src/planLogicCreater.c
@@ -501,7 +501,7 @@ static int32_t createScanLogicNode(SLogicPlanContext* pCxt, SSelectStmt* pSelect
   } else {
     nodesDestroyNode((SNode*)pScan);
   }
-  pScan->seqBlocksSort = getSeqBlocksSortHint(pSelect->pHint);
+  pScan->seqBlocksSort = getSeqBlocksSortOptHint(pSelect->pHint);
   pCxt->hasScan = true;
 
   return code;
