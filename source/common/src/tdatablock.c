3cd158f681c9952091832159e5d7b1c7f586a000
Haojun Liao <hjliao@taosdata.com>
2024-02-24 02:19:40 +0800

    fix(stream): fix the invalid check for subtable name.

--- source/common/src/tdatablock.c
+++ source/common/src/tdatablock.c
@@ -2139,7 +2139,7 @@ bool alreadyAddGroupId(char* ctbName) {
     _location--;
   }
 
-  return ctbName[_location] == '_' && len - 1 - _location > 15;  // 15 means the min length of groupid
+  return ctbName[_location] == '_' && len - 1 - _location >= 15;  // 15 means the min length of groupid
 }
 
 char* buildCtbNameByGroupId(const char* stbFullName, uint64_t groupId) {
