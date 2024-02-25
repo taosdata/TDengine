820c61211c979faa68fc6aa413209094d6633eb4
yingzhao <zacharyzhou@taosdata.com>
2024-02-25 16:52:32 +0800

    enh(opc): csv template title, sn, varchar

--- src/serve/data_sources/point_loader/mod.rs
+++ src/serve/data_sources/point_loader/mod.rs
@@ -395,8 +395,8 @@ async fn get_all_points(
 
 fn get_opcua_csv_header(lang: &str, demo: bool) -> String {
     let header = match lang {
-        "zh" => "\u{FEFF}序号,数据点位id,\"是否启用(可选,1 - 启动, 0 - 停用。配置为0,将删除数据点位对应的子表)\",超级表名,子表名,采集值列名,采集值转换规则(可选),\"采集值类型(可选,默认根据实际类型自动填充,可选值有int, double, float, string)\",数据质量列名,OPC原始时间列名(默认作为时间戳主键),\"TD 服务端接收时间列名(将本列剪切到 ts_col 之前,将会使用本列作为时间戳主键)\", ts_col 的时间戳转换规则, received_ts_col 的时间戳转换规则,\" 标签列(不需要可删除,需要多个,可以在右侧添加新列,可指定列名和类型）\"\n",
-        _ => "\u{FEFF}No,OPC Point Id (Required),\"Enable point?(1-Enable,0-Disable.if set to 0, will delete the sub table)\",Stable Name,sub table name,value column name,value transform rule(Optional),\"value data type(Optional, candidate values:int, float, double, string)\",\"Quality Column Name(Optional, default is quality)\",OPC original time column name(default to be the primary key),\"TDengine received time column name (Optionally, if you want to use this column as the primary key, move it to the left of ts_col.)\",ts_col transform rule,receive_ts_col transform rule,\"Tag column(Optional, if need more, add new column to the right)\"\n",
+        "zh" => "\u{FEFF}序号,数据点位id,\"是否启用(可选,1 - 启动, 0 - 停用。配置为0,将删除数据点位对应的子表)\",超级表名,子表名,采集值列名,采集值转换规则(可选),\"采集值类型(可选,默认根据实际类型自动填充,可选值有int, double, float, varchar)\",数据质量列名,OPC原始时间列名(默认作为时间戳主键),\"TD 服务端接收时间列名(将本列剪切到 ts_col 之前,将会使用本列作为时间戳主键)\", ts_col 的时间戳转换规则, received_ts_col 的时间戳转换规则,\" 标签列(不需要可删除,需要多个,可以在右侧添加新列,可指定列名和类型）\"\n",
+        _ => "\u{FEFF}S/N,OPC Point Id (Required),\"Enable point?(1-Enable,0-Disable.if set to 0, will delete the sub table)\",Stable Name,sub table name,value column name,value transform rule(Optional),\"value data type(Optional, candidate values:int, float, double, string)\",\"Quality Column Name(Optional, default is quality)\",OPC original time column name(default to be the primary key),\"TDengine received time column name (Optionally, if you want to use this column as the primary key, move it to the left of ts_col.)\",ts_col transform rule,receive_ts_col transform rule,\"Tag column(Optional, if need more, add new column to the right)\"\n",
     };
 
     let mut header = header.to_string();
