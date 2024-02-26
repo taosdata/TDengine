26cf2f8a9bd2039bde15965c47c6459729c49d06
factosea <285808407@qq.com>
2024-02-26 17:00:19 +0800

    use cache

--- source/libs/executor/src/executor.c
+++ source/libs/executor/src/executor.c
@@ -636,7 +636,7 @@ int32_t qExecTaskOpt(qTaskInfo_t tinfo, SArray* pResList, uint64_t* useconds, bo
     ASSERT(p->info.rows > 0);
     taosArrayPush(pResList, &p);
 
-    if (current >= rowsThreshold || current >= pRes->info.rows) {
+    if (current >= rowsThreshold) {
       break;
     }
 
