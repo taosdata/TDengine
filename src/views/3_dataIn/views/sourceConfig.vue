189a6ae91baca57142c6d1b00f2d229a71e117e6
guxiang02 <guxiang_0211@163.com>
2024-03-25 19:56:39 +0800

    fix: 点击取消时修改数据源按钮不能点击 TD-29109

--- src/views/3_dataIn/views/sourceConfig.vue
+++ src/views/3_dataIn/views/sourceConfig.vue
@@ -431,7 +431,7 @@ export default {
           .then(() => {
             this.submit(true);
           })
-          .catch(() => {});
+          .catch(() => {this.loading = false});
       } else {
         this.submit(true);
       }
