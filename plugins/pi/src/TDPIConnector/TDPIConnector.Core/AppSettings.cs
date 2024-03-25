9e6bd5a4cbc5d663bb23cea637f3b8dc338cdc52
xinsheng Ren <285808407@qq.com>
2024-03-25 14:41:23 +0800

    fix(pi): fix pip numbers, duplicate value one time (#1663)
    * fix pip numbers
    
    * duplicate value one time
    
    ---------
    
    Co-authored-by: facetosea <25808407@qq.com>

--- plugins/pi/src/TDPIConnector/TDPIConnector.Core/AppSettings.cs
+++ plugins/pi/src/TDPIConnector/TDPIConnector.Core/AppSettings.cs
@@ -20,7 +20,7 @@ namespace TDPIConnector.Core
             public string PISystemName { get; set; }
             public string AFDatabaseName { get; set; }
             public int PIDataPipesInstances { get; set; } = 1;
-            public int AFDataPipesInstances { get; set; } = 1;
+            public int AFDataPipesInstances { get; set; } = 10;
             public string IPCStream { get; set; }
             public string SQLAPI { get; set; }
             public string AFTreeTagName { get; set; } = "location";
@@ -151,7 +151,7 @@ namespace TDPIConnector.Core
             WebMaxTDEngineHttpResponses = GetIntegerFromAppSettings("WebMaxTDEngineHttpResponses", 5);
             WebMonitoringEventsEnabled = GetBooleanFromAppSettings("WebMonitoringEventsEnabled", false);
             BackfillQuitWait = GetIntegerFromAppSettings("BackfillQuitWait", 60);
-            MaxEventCountObserverFetchOnce = GetIntegerFromAppSettings("MaxEventCountObserverFetchOnce", 20000);
+            MaxEventCountObserverFetchOnce = GetIntegerFromAppSettings("MaxEventCountObserverFetchOnce", 10000);
 
             if (TDEnginePITablesPrefix == null)
             {
