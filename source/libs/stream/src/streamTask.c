ce2cbd4ab5940fd29a44f60cd0875c70adafbc51
wangjiaming0909 <604227650@qq.com>
2024-04-09 11:37:08 +0800

    fix stream fill history not finished

--- source/libs/stream/src/streamTask.c
+++ source/libs/stream/src/streamTask.c
@@ -733,6 +733,7 @@ bool streamTaskIsAllUpstreamClosed(SStreamTask* pTask) {
 bool streamTaskSetSchedStatusWait(SStreamTask* pTask) {
   bool ret = false;
 
+  // double check
   taosThreadMutexLock(&pTask->lock);
   if (pTask->status.schedStatus == TASK_SCHED_STATUS__INACTIVE) {
     pTask->status.schedStatus = TASK_SCHED_STATUS__WAITING;
