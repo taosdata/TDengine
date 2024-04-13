f65ba95b6ab8111d28e8762a2cc1180d456bc243
Haojun Liao <hjliao@taosdata.com>
2024-04-13 18:27:38 +0800

    fix(stream): fix init error.

--- source/dnode/vnode/src/tqCommon/tqCommon.c
+++ source/dnode/vnode/src/tqCommon/tqCommon.c
@@ -74,13 +74,15 @@ int32_t tqExpandStreamTask(SStreamTask* pTask, SStreamMeta* pMeta, void* pVnode)
 
   initStorageAPI(&handle.api);
 
-  pTask->exec.pExecutor = qCreateStreamExecTaskInfo(pTask->exec.qmsg, &handle, vgId, pTask->id.taskId);
-  if (pTask->exec.pExecutor == NULL) {
-    tqError("s-task:%s failed to create exec taskInfo, failed to expand task", pTask->id.idStr);
-    return -1;
+  if (pTask->info.taskLevel == TASK_LEVEL__SOURCE || pTask->info.taskLevel == TASK_LEVEL__AGG) {
+    pTask->exec.pExecutor = qCreateStreamExecTaskInfo(pTask->exec.qmsg, &handle, vgId, pTask->id.taskId);
+    if (pTask->exec.pExecutor == NULL) {
+      tqError("s-task:%s failed to create exec taskInfo, failed to expand task", pTask->id.idStr);
+      return -1;
+    }
+    qSetTaskId(pTask->exec.pExecutor, pTask->id.taskId, pTask->id.streamId);
   }
 
-  qSetTaskId(pTask->exec.pExecutor, pTask->id.taskId, pTask->id.streamId);
   return TSDB_CODE_SUCCESS;
 }
 
