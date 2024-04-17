13e14d0f55b52b5ccdacd94d1d072c8c5ab356ae
Haojun Liao <hjliao@taosdata.com>
2024-04-17 10:48:22 +0800

    fix(stream): add return.

--- source/libs/stream/src/streamExec.c
+++ source/libs/stream/src/streamExec.c
@@ -591,6 +591,7 @@ int32_t doStreamExecTask(SStreamTask* pTask) {
     if (ret == EXEC_AFTER_IDLE) {
       ASSERT(pInput == NULL && numOfBlocks == 0);
       setTaskSchedInfo(pTask, MIN_INVOKE_INTERVAL);
+      return 0;
     } else {
       if (pInput == NULL) {
         ASSERT(numOfBlocks == 0);
