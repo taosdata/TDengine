83950bb0a7a28c63d8f3425a8f051d6ddba0e1bb
Shungang Li <sgli@taosdata.com>
2024-04-24 11:39:16 +0800

    feat: add pcre2 lib to precheck geometry wkt

--- include/util/tpcre2.h
+++ include/util/tpcre2.h
@@ -13,36 +13,22 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef _TD_UTIL_GEOS_CTX_H_
-#define _TD_UTIL_GEOS_CTX_H_
+#ifndef _TD_ULIT_PCRE2_H_
+#define _TD_ULIT_PCRE2_H_
+
+#define PCRE2_CODE_UNIT_WIDTH 8
+#include "pcre2.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#include <geos_c.h>
-#include <tpcre2.h>
-
-typedef struct SGeosContext {
-  GEOSContextHandle_t handle;
-
-  GEOSWKTReader *WKTReader;
-  GEOSWKTWriter *WKTWriter;
-
-  GEOSWKBReader *WKBReader;
-  GEOSWKBWriter *WKBWriter;
-
-  pcre2_code *WKTRegex;
-  pcre2_match_data *WKTMatchData;
-
-  char errMsg[512];
-} SGeosContext;
-
-SGeosContext* getThreadLocalGeosCtx();
-void destroyThreadLocalGeosCtx();
+int32_t doRegComp(pcre2_code** ppRegex, pcre2_match_data** ppMatchData, const char* pattern);
+int32_t doRegExec(const char* pString, pcre2_code* pRegex, pcre2_match_data* pMatchData);
+void    destroyRegexes(pcre2_code* pWktRegex, pcre2_match_data* pWktMatchData);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /*_TD_UTIL_GEOS_CTX_H_*/
+#endif  // _TD_UTIL_PAGEDBUF_H_
