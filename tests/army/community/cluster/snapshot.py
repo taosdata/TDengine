1804c12ee8fc7893f04e66a168e18c62e8eaf6e1
Alex Duan <417921451@qq.com>
2024-04-25 15:42:30 +0800

    fix:solve snapshot.py failed

--- tests/army/community/cluster/snapshot.py
+++ tests/army/community/cluster/snapshot.py
@@ -30,7 +30,7 @@ from frame.srvCtl import *
 
 class TDTestCase(TBase):
     updatecfgDict = {
-        "countAlwaysReturnValue" : "0",
+        "countAlwaysReturnValue" : "1",
         "lossyColumns"           : "float,double",
         "fPrecision"             : "0.000000001",
         "dPrecision"             : "0.00000000000000001",
