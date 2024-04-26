222aabac68eb5f8a303e9db16e93052caf784c1b
dingbo8128 <dingbo8128@163.com>
2024-04-26 11:16:42 +0800

    debug

--- src/serve/rpc/mod.rs
+++ src/serve/rpc/mod.rs
@@ -1030,7 +1030,7 @@ async fn modify_task_dsn_params(task: &mut Task) -> anyhow::Result<()> {
                 new_value.pop();
             }
         } else if k == "transform_config_file" {
-            let (point_list, element_id_list) =
+            let (element_id_list, point_list) =
                 PiConfig::get_points_from_transform_config_file(v.as_str()).with_context(|| {
                     anyhow::format_err!("Modify task dsn error, failed to parse file:{}", v)
                 })?;
