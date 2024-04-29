145837ab676b98beaf9b046f3547809e5033537b
Hongze Cheng <hzcheng@taosdata.com>
2024-04-29 15:21:09 +0800

    fix memory leak

--- source/libs/tdb/src/db/tdbBtree.c
+++ source/libs/tdb/src/db/tdbBtree.c
@@ -2492,6 +2492,10 @@ int tdbBtcClose(SBTC *pBtc) {
     pBtc->idx = pBtc->idxStack[pBtc->iPage];
   }
 
+  if (TDB_CELLDECODER_FREE_KEY(&pBtc->coder)) {
+    tdbFree(pBtc->coder.pKey);
+  }
+
   if (TDB_CELLDECODER_FREE_VAL(&pBtc->coder)) {
     tdbDebug("tdb btc/close decoder: %p pVal free: %p", &pBtc->coder, pBtc->coder.pVal);
 
