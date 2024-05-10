72726aafe31441275ec4a66389bae9ff84721713
dingbo8128 <dingbo8128@163.com>
2024-05-10 18:13:31 +0800

    fix(pi): can't find transform_config_file in dsn at agent side

--- src/serve/rpc/mod.rs
+++ src/serve/rpc/mod.rs
@@ -1075,17 +1075,7 @@ async fn modify_task_dsn_params(task: &mut Task) -> anyhow::Result<()> {
                 new_value.pop();
             }
         } else if k == "transform_config_file" {
-            // let (element_id_list, point_list) =
-            //     PiConfig::get_points_from_transform_config_file(v.as_str()).with_context(|| {
-            //         anyhow::format_err!("Modify task dsn error, failed to parse file:{}", v)
-            //     })?;
-            // if !element_id_list.is_empty() {
-            //     map.insert("element_id_list".to_string(), element_id_list.join(","));
-            // }
-            // if !point_list.is_empty() {
-            //     map.insert("point_list".to_string(), point_list.join(","));
-            // }
-            continue;
+            // do nothing
         } else if v.contains("@") {
             new_value.push_str(
                 get_string_content_from_param_value(&v, false, false)?
