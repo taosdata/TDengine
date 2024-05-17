15181eb14bbed37cd1fd6db0c7caaa5e064e48c9
Haojun Liao <hjliao@taosdata.com>
2024-05-17 09:51:56 +0800

    enh(tsdb): increase the capacity for return data.

--- source/dnode/vnode/src/tsdb/tsdbRead2.c
+++ source/dnode/vnode/src/tsdb/tsdbRead2.c
@@ -4242,7 +4242,7 @@ int32_t tsdbReaderOpen2(void* pVnode, SQueryTableDataCond* pCond, void* pTableLi
   SVnodeCfg*  pConf = &(((SVnode*)pVnode)->config);
 
   int32_t capacity = pConf->tsdbCfg.maxRows;
-  capacity = 8192;  // for debug purpose
+  capacity = 16384;  // for debug purpose
   if (pResBlock != NULL) {
     blockDataEnsureCapacity(pResBlock, capacity);
   }
