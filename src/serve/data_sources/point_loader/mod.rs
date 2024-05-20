cc2142045b533e5705ea96ee9c7011fc1070db13
zyyang <zyyang@taosdata.com>
2024-05-20 10:48:11 +0800

    fix: remove csv_config_file when download points#td-30151

--- src/serve/data_sources/point_loader/mod.rs
+++ src/serve/data_sources/point_loader/mod.rs
@@ -332,6 +332,9 @@ async fn get_all_points(
         categories
     );
     let mut from = from.into_dsn()?;
+    // remove csv_config_file param for opc
+    from.params.remove("csv_config_file");
+
     let pattern;
     match from.driver.as_str() {
         "pi" | "pibackfill" => {
