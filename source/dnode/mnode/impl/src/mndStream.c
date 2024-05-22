5c56a3d3066ff92d2066ca2b4b8e4b894baf51e4
Haojun Liao <hjliao@taosdata.com>
2024-05-22 13:24:11 +0800

    fix(stream): adjust buf size.

--- source/dnode/mnode/impl/src/mndStream.c
+++ source/dnode/mnode/impl/src/mndStream.c
@@ -1620,10 +1620,10 @@ static int32_t setTaskAttrInResBlock(SStreamObj *pStream, SStreamTask *pTask, SS
   colDataSetVal(pColInfo, numOfRows, (const char *)&pe->stage, false);
 
   // input queue
-  char        vbuf[30] = {0};
-  char        buf[25] = {0};
+  char        vbuf[35] = {0};
+  char        buf[32] = {0};
   const char *queueInfoStr = "%4.2fMiB (%5.2f%)";
-  sprintf(buf, queueInfoStr, pe->inputQUsed, pe->inputRate);
+  snprintf(buf, tListLen(buf), queueInfoStr, pe->inputQUsed, pe->inputRate);
   STR_TO_VARSTR(vbuf, buf);
 
   pColInfo = taosArrayGet(pBlock->pDataBlock, cols++);
