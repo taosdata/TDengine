2cdaa8c96246b765135eeb12b74e3d7fa1d309c9
Yihao Deng <luomoxyz@126.com>
2024-05-29 09:19:27 +0000

    opt transport

--- include/libs/transport/trpc.h
+++ include/libs/transport/trpc.h
@@ -126,6 +126,7 @@ typedef struct SRpcInit {
   int32_t timeToGetConn;
   int8_t  supportBatch;  // 0: no batch, 1. batch
   int32_t batchSize;
+  int8_t  shareConn;  // 0: no share, 1. share
   void   *parent;
 } SRpcInit;
 
