d28abdf837b4407ed184db538df33517da87dc07
Hongze Cheng <hzcheng@taosdata.com>
2024-05-30 16:11:53 +0800

    fix coverity scan problem

--- source/dnode/vnode/src/tsdb/tsdbCommit2.c
+++ source/dnode/vnode/src/tsdb/tsdbCommit2.c
@@ -212,6 +212,11 @@ _exit:
   return code;
 }
 
+static int32_t tsdbCommitCloseReader(SCommitter2 *committer) {
+  TARRAY2_CLEAR(committer->sttReaderArray, tsdbSttFileReaderClose);
+  return 0;
+}
+
 static int32_t tsdbCommitOpenReader(SCommitter2 *committer) {
   int32_t code = 0;
   int32_t lino = 0;
@@ -256,13 +261,17 @@ static int32_t tsdbCommitOpenReader(SCommitter2 *committer) {
 
 _exit:
   if (code) {
+    tsdbCommitCloseReader(committer);
     TSDB_ERROR_LOG(TD_VID(committer->tsdb->pVnode), lino, code);
   }
   return code;
 }
 
-static int32_t tsdbCommitCloseReader(SCommitter2 *committer) {
-  TARRAY2_CLEAR(committer->sttReaderArray, tsdbSttFileReaderClose);
+static int32_t tsdbCommitCloseIter(SCommitter2 *committer) {
+  tsdbIterMergerClose(&committer->tombIterMerger);
+  tsdbIterMergerClose(&committer->dataIterMerger);
+  TARRAY2_CLEAR(committer->tombIterArray, tsdbIterClose);
+  TARRAY2_CLEAR(committer->dataIterArray, tsdbIterClose);
   return 0;
 }
 
@@ -336,17 +345,10 @@ static int32_t tsdbCommitOpenIter(SCommitter2 *committer) {
 
 _exit:
   if (code) {
+    tsdbCommitCloseIter(committer);
     TSDB_ERROR_LOG(TD_VID(committer->tsdb->pVnode), lino, code);
   }
   return code;
-}
-
-static int32_t tsdbCommitCloseIter(SCommitter2 *committer) {
-  tsdbIterMergerClose(&committer->tombIterMerger);
-  tsdbIterMergerClose(&committer->dataIterMerger);
-  TARRAY2_CLEAR(committer->tombIterArray, tsdbIterClose);
-  TARRAY2_CLEAR(committer->dataIterArray, tsdbIterClose);
-  return 0;
 }
 
 static int32_t tsdbCommitFileSetBegin(SCommitter2 *committer) {
