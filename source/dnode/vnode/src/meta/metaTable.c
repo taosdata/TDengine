c6407277dc27e92dee64af1abb6524d482991b23
Yihao Deng <luomoxyz@126.com>
2024-05-30 11:41:22 +0000

    support add column compress

--- source/dnode/vnode/src/meta/metaTable.c
+++ source/dnode/vnode/src/meta/metaTable.c
@@ -1491,6 +1491,7 @@ static int metaAlterTableColumn(SMeta *pMeta, int64_t version, SVAlterTbReq *pAl
   SSchema  tScheam;
   switch (pAlterTbReq->action) {
     case TSDB_ALTER_TABLE_ADD_COLUMN:
+    case TSDB_ALTER_TABLE_ADD_COLUMN_WITH_COMPRESS_OPTION:
       if (pColumn) {
         terrno = TSDB_CODE_VND_COL_ALREADY_EXISTS;
         goto _err;
@@ -1522,10 +1523,21 @@ static int metaAlterTableColumn(SMeta *pMeta, int64_t version, SVAlterTbReq *pAl
         (void)tsdbCacheNewNTableColumn(pMeta->pVnode->pTsdb, entry.uid, cid, col_type);
       }
       SSchema *pCol = &pSchema->pSchema[entry.ntbEntry.schemaRow.nCols - 1];
-      updataTableColCmpr(&entry.colCmpr, pCol, 1);
-      freeColCmpr = true;
-      ASSERT(entry.colCmpr.nCols == pSchema->nCols);
-      break;
+      if (pAlterTbReq->action == TSDB_ALTER_TABLE_ADD_COLUMN_WITH_COMPRESS_OPTION) {
+        
+        // if (pAlterTbReq->colCmpr.nCols != pSchema->nCols) {
+        //   terrno = TSDB_CODE_VND_INVALID_TABLE_ACTION;
+        //   goto _err;
+        // }
+        // updataTableColCmpr(&entry.colCmpr, pCol, 1);
+        // freeColCmpr = true;
+        // ASSERT(entry.colCmpr.nCols == pSchema->nCols);
+      } else {
+        updataTableColCmpr(&entry.colCmpr, pCol, 1);
+        freeColCmpr = true;
+        ASSERT(entry.colCmpr.nCols == pSchema->nCols);
+        break;
+      }
     case TSDB_ALTER_TABLE_DROP_COLUMN:
       if (pColumn == NULL) {
         terrno = TSDB_CODE_VND_COL_NOT_EXISTS;
