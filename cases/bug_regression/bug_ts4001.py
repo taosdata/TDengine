2d46981e885fd3f4e7b04802d7bc0ea0dfdebad2
jiajingbin <jiajaybin@126.com>
2024-06-12 20:00:12 +0800

    test: refactor cases/bug_regression/bug_ts4001.py

--- cases/bug_regression/bug_ts4001.py
+++ cases/bug_regression/bug_ts4001.py
@@ -14,14 +14,11 @@
 from taostest import TDCase, T
 from taostest.util.common import TDCom
 from taostest.components import TaosD
-from taostest.util.remote import Remote
 import time
 
 class TestTs4001(TDCase):
     def init(self):
         self.tdCom = TDCom(self.tdSql)
-        self._remote: Remote = Remote(self.logger)
-        self.taosd = TaosD(self._remote)
 
     def run(self):
         self.tdCom.createDb(dbname="bug4001_test")
@@ -29,9 +26,7 @@ class TestTs4001(TDCase):
         self.tdSql.execute('CREATE STABLE IF NOT EXISTS bug4001_test.test(create_time timestamp, record_time timestamp, s1 int) TAGS (t1 nchar(50));')
         self.tdSql.execute('CREATE STREAM IF NOT EXISTS test_s FILL_HISTORY 1 IGNORE EXPIRED 0 TRIGGER MAX_DELAY 3s INTO test_s1 SUBTABLE(CONCAT(tbname, "_s1")) AS SELECT _wstart AS alarm_begin_time, _wend AS alarm_end_time, _wduration AS alarm_duration, count(*) AS record_count,t1, s1 FROM test PARTITION BY tbname, t1, s1 STATE_WINDOW(s1);')
         self.tdSql.execute('INSERT INTO test_001 USING test TAGS("001") VALUES("2023-09-15 09:00:00", "2023-09-15 09:00:00", 1) ("2023-09-15 09:00:01", "2023-09-15 09:00:01", 2);')
-        time.sleep(3)
-        self.tdSql.query('SELECT * FROM test_s1;')
-        self.tdSql.checkEqual(self.tdSql.query_row, 2)
+        self.tdCom.check_query_data("SELECT _wstart AS alarm_begin_time, _wend AS alarm_end_time, _wduration AS alarm_duration, count(*) AS record_count,t1, s1 FROM test PARTITION BY tbname, t1, s1 STATE_WINDOW(s1);", "select alarm_begin_time, alarm_end_time, alarm_duration, record_count, t1, s1 from test_s1")
 
     def cleanup(self):
         pass
