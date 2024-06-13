1d698afb5576c1e084799a877066818f2284201e
facetosea <25808407@qq.com>
2024-06-13 14:13:44 +0800

    fix: code review

--- source/libs/executor/src/groupoperator.c
+++ source/libs/executor/src/groupoperator.c
@@ -581,45 +581,39 @@ SSDataBlock* createBlockDataNotLoaded(const SOperatorInfo* pOperator, SSDataBloc
 
   SSDataBlock* pDstBlock = createDataBlock();
   pDstBlock->info = pDataBlock->info;
+  pDstBlock->info.id = pOperator->resultDataBlockId;
   pDstBlock->info.capacity = 0;
   pDstBlock->info.rowSize = 0;
 
-  size_t numOfCols = taosArrayGetSize(pDataBlock->pDataBlock);
-  for (int32_t i = 0; i < pOperator->exprSupp.numOfExprs; ++i) {
-    SExprInfo*       pExpr = &pOperator->exprSupp.pExprInfo[i];
-    int32_t          slotId = pExpr->base.pParam[0].pCol->slotId;
-    SColumnInfoData* pSrc = taosArrayGet(pDataBlock->pDataBlock, slotId);
-    SColumnInfoData colInfo = {.hasNull = true, .info = pSrc->info};
-    blockDataAppendColInfo(pDstBlock, &colInfo);
-  }
-
-  int32_t code = blockDataEnsureCapacity(pDstBlock, pDataBlock->info.rows);
-  if (code != TSDB_CODE_SUCCESS) {
-    terrno = code;
-    blockDataDestroy(pDstBlock);
-    return NULL;
-  }
-
-  if (pDataBlock->pBlockAgg != NULL) {
+  size_t numOfCols = pOperator->exprSupp.numOfExprs;
+  if (pDataBlock->pBlockAgg) {
     pDstBlock->pBlockAgg = taosMemoryCalloc(numOfCols, sizeof(SColumnDataAgg));
     if (pDstBlock->pBlockAgg == NULL) {
       terrno = TSDB_CODE_OUT_OF_MEMORY;
       blockDataDestroy(pDstBlock);
       return NULL;
     }
-    for (int32_t i = 0; i < pOperator->exprSupp.numOfExprs; ++i) {
-      SExprInfo* pExpr = &pOperator->exprSupp.pExprInfo[i];
-      int32_t    slotId = pExpr->base.pParam[0].pCol->slotId;
-      pDstBlock->pBlockAgg[i] = pDataBlock->pBlockAgg[slotId];
-    }
   }
 
   for (int32_t i = 0; i < pOperator->exprSupp.numOfExprs; ++i) {
     SExprInfo*       pExpr = &pOperator->exprSupp.pExprInfo[i];
     int32_t          slotId = pExpr->base.pParam[0].pCol->slotId;
     SColumnInfoData* pSrc = taosArrayGet(pDataBlock->pDataBlock, slotId);
+    SColumnInfoData  colInfo = {.hasNull = true, .info = pSrc->info};
+    blockDataAppendColInfo(pDstBlock, &colInfo);
+
     SColumnInfoData* pDst = taosArrayGet(pDstBlock->pDataBlock, i);
+    int32_t code = doEnsureCapacity(pDst, &pDstBlock->info, pDataBlock->info.rows, false);
+    if (code != TSDB_CODE_SUCCESS) {
+      terrno = code;
+      blockDataDestroy(pDstBlock);
+      return NULL;
+    }
     colDataAssign(pDst, pSrc, pDataBlock->info.rows, &pDataBlock->info);
+
+    if (pDataBlock->pBlockAgg) {
+      pDstBlock->pBlockAgg[i] = pDataBlock->pBlockAgg[slotId];
+    }
   }
 
   return pDstBlock;
@@ -718,13 +712,13 @@ static void doHashPartition(SOperatorInfo* pOperator, SSDataBlock* pBlock) {
         T_LONG_JMP(pTaskInfo->env, terrno);
       }
       if (pGroupInfo->blockForNotLoaded == NULL) {
-        pGroupInfo->blockForNotLoaded = taosArrayInit(1, sizeof(SSDataBlock));
+        pGroupInfo->blockForNotLoaded = taosArrayInit(0, sizeof(SSDataBlock*));
         pGroupInfo->offsetForNotLoaded = 0;
       }
       dataNotLoadBlock->info.id.groupId = pGroupInfo->groupId;
       dataNotLoadBlock->info.dataLoad = 0;
       pInfo->binfo.pRes->info.rows = pBlock->info.rows;
-      taosArrayInsert(pGroupInfo->blockForNotLoaded, pGroupInfo->blockForNotLoaded->size, &dataNotLoadBlock);
+      taosArrayPush(pGroupInfo->blockForNotLoaded, &dataNotLoadBlock);
       break;
     }
   }
@@ -879,28 +873,27 @@ static SSDataBlock* buildPartitionResult(SOperatorInfo* pOperator) {
     pInfo->pageIndex = 0;
   }
 
-  int32_t* pageId = taosArrayGet(pGroupInfo->pPageList, pInfo->pageIndex);
-  void*    page = getBufPage(pInfo->pBuf, *pageId);
-  if (page == NULL) {
-    qError("failed to get buffer, code:%s, %s", tstrerror(terrno), GET_TASKID(pTaskInfo));
-    T_LONG_JMP(pTaskInfo->env, terrno);
-  }
-  if (*(int32_t*)page == 0) {
-    SSDataBlock* ret = buildPartitionResultForNotLoadBlock(pGroupInfo);
-    if (ret != NULL) return ret;
-    releaseBufPage(pInfo->pBuf, page);
-    if (pInfo->pageIndex < taosArrayGetSize(pGroupInfo->pPageList)) {
-      pInfo->pageIndex += 1;
-    } else if (pInfo->groupIndex + 1 < taosArrayGetSize(pInfo->sortedGroupArray)) {
-      pInfo->groupIndex++;
-      pInfo->pageIndex = 0;
-    } else {
-      setOperatorCompleted(pOperator);
-      clearPartitionOperator(pInfo);
-      return NULL;
+    int32_t* pageId = taosArrayGet(pGroupInfo->pPageList, pInfo->pageIndex);
+    void*    page = getBufPage(pInfo->pBuf, *pageId);
+    if (page == NULL) {
+      qError("failed to get buffer, code:%s, %s", tstrerror(terrno), GET_TASKID(pTaskInfo));
+      T_LONG_JMP(pTaskInfo->env, terrno);
     }
-    return buildPartitionResult(pOperator);
-  }
+    if (*(int32_t*)page == 0) {
+      releaseBufPage(pInfo->pBuf, page);
+      SSDataBlock* ret = buildPartitionResultForNotLoadBlock(pGroupInfo);
+      if (ret != NULL) return ret;
+      releaseBufPage(pInfo->pBuf, page);
+      if (pInfo->groupIndex + 1 < taosArrayGetSize(pInfo->sortedGroupArray)) {
+        pInfo->groupIndex++;
+        pInfo->pageIndex = 0;
+      } else {
+        setOperatorCompleted(pOperator);
+        clearPartitionOperator(pInfo);
+        return NULL;
+      }
+      return buildPartitionResult(pOperator);
+    }
 
   blockDataEnsureCapacity(pInfo->binfo.pRes, pInfo->rowCapacity);
   blockDataFromBuf1(pInfo->binfo.pRes, page, pInfo->rowCapacity);
@@ -963,8 +956,6 @@ static SSDataBlock* hashPartition(SOperatorInfo* pOperator) {
   while (pGroupIter != NULL) {
     SDataGroupInfo* pGroupInfo = pGroupIter;
     taosArrayPush(groupArray, pGroupInfo);
-    static int i = 0;
-    qInfo("groupArray push  %p %p %d times", pGroupInfo, pGroupInfo->blockForNotLoaded, ++i);
     pGroupIter = taosHashIterate(pInfo->pGroupSet, pGroupIter);
   }
 
