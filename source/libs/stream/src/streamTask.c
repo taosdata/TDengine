4490f40c961de428d95d8441d92700e8faa27f3a
Haojun Liao <hjliao@taosdata.com>
2024-06-14 14:43:57 +0800

    fix(stream): do some internal refactor.

--- source/libs/stream/src/streamTask.c
+++ source/libs/stream/src/streamTask.c
@@ -944,32 +944,36 @@ char* createStreamTaskIdStr(int64_t streamId, int32_t taskId) {
 
 static int32_t streamTaskEnqueueRetrieve(SStreamTask* pTask, SStreamRetrieveReq* pReq) {
   SStreamDataBlock* pData = taosAllocateQitem(sizeof(SStreamDataBlock), DEF_QITEM, sizeof(SStreamDataBlock));
-  int8_t            status = TASK_INPUT_STATUS__NORMAL;
-
-  // enqueue
-  if (pData != NULL) {
-    stDebug("s-task:%s (child %d) recv retrieve req from task:0x%x(vgId:%d), reqId:0x%" PRIx64, pTask->id.idStr,
-            pTask->info.selfChildId, pReq->srcTaskId, pReq->srcNodeId, pReq->reqId);
-
-    pData->type = STREAM_INPUT__DATA_RETRIEVE;
-    pData->srcVgId = 0;
-    streamRetrieveReqToData(pReq, pData);
-    if (streamTaskPutDataIntoInputQ(pTask, (SStreamQueueItem*)pData) == 0) {
-      status = TASK_INPUT_STATUS__NORMAL;
-    } else {
-      status = TASK_INPUT_STATUS__FAILED;
-    }
-  } else {  // todo handle oom
-    /*streamTaskInputFail(pTask);*/
-    /*status = TASK_INPUT_STATUS__FAILED;*/
+  if (pData == NULL) {
+    stError("s-task:%s failed to allocated retrieve-block", pTask->id.idStr);
+    return terrno;
   }
 
-  return status == TASK_INPUT_STATUS__NORMAL ? 0 : -1;
+  // enqueue
+  stDebug("s-task:%s (vgId:%d level:%d) recv retrieve req from task:0x%x(vgId:%d), reqId:0x%" PRIx64, pTask->id.idStr,
+          pTask->pMeta->vgId, pTask->info.taskLevel, pReq->srcTaskId, pReq->srcNodeId, pReq->reqId);
+
+  pData->type = STREAM_INPUT__DATA_RETRIEVE;
+  pData->srcVgId = 0;
+
+  int32_t code = streamRetrieveReqToData(pReq, pData, pTask->id.idStr);
+  if (code != TSDB_CODE_SUCCESS) {
+    taosFreeQitem(pData);
+    return code;
+  }
+
+  code = streamTaskPutDataIntoInputQ(pTask, (SStreamQueueItem*)pData);
+  if (code != TSDB_CODE_SUCCESS) {
+    stError("s-task:%s failed to put retrieve-block into inputQ, inputQ is full, discard the retrieve msg",
+            pTask->id.idStr);
+  }
+
+  return code;
 }
 
 int32_t streamProcessRetrieveReq(SStreamTask* pTask, SStreamRetrieveReq* pReq) {
   int32_t code = streamTaskEnqueueRetrieve(pTask, pReq);
-  if(code != 0){
+  if (code != 0) {
     return code;
   }
   return streamTrySchedExec(pTask);
