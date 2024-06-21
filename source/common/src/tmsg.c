4e1ea52d07ec06fdf6af2e5650de97975b946488
Yihao Deng <luomoxyz@126.com>
2024-06-21 11:12:58 +0000

    fix invalid ip white

--- source/common/src/tmsg.c
+++ source/common/src/tmsg.c
@@ -467,6 +467,8 @@ int32_t tSerializeSClientHbBatchReq(void *buf, int32_t bufLen, const SClientHbBa
     SClientHbReq *pReq = taosArrayGet(pBatchReq->reqs, i);
     if (tSerializeSClientHbReq(&encoder, pReq) < 0) return -1;
   }
+
+  if (tEncodeI64(&encoder, pBatchReq->ipWhiteList) < 0) return -1;
   tEndEncode(&encoder);
 
   int32_t tlen = encoder.pos;
@@ -491,6 +493,10 @@ int32_t tDeserializeSClientHbBatchReq(void *buf, int32_t bufLen, SClientHbBatchR
     SClientHbReq req = {0};
     tDeserializeSClientHbReq(&decoder, &req);
     taosArrayPush(pBatchReq->reqs, &req);
+  }
+
+  if (!tDecodeIsEnd(&decoder)) {
+    tDecodeI64(&decoder, &pBatchReq->ipWhiteList);
   }
 
   tEndDecode(&decoder);
