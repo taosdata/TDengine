708a44441ee287b2256b722beacf5867175ed276
Yihao Deng <luomoxyz@126.com>
2024-06-22 00:16:37 +0000

    fix UAF

--- source/client/src/clientHb.c
+++ source/client/src/clientHb.c
@@ -892,10 +892,10 @@ SClientHbBatchReq *hbGatherAllInfo(SAppHbMgr *pAppHbMgr) {
       }
     }
 
-    releaseTscObj(connKey->tscRid);
-
     int64_t ver = atomic_load_64(&pTscObj->whiteListInfo.ver);
     maxIpWhiteVer = MAX(maxIpWhiteVer, ver);
+
+    releaseTscObj(connKey->tscRid);
   }
   pBatchReq->ipWhiteList = maxIpWhiteVer;
 
