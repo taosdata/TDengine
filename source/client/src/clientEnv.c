d4149c95919cc72ed0d7920030ab3875c36cac47
wangmm0220 <wangmm0220@gmail.com>
2024-06-24 12:56:28 +0800

    fix:remove comma in db list

--- source/client/src/clientEnv.c
+++ source/client/src/clientEnv.c
@@ -86,7 +86,7 @@ static void concatStrings(SArray *list, char* buf, int size){
       strcat(buf, ",");
       len += 1;
     }
-    int ret = snprintf(buf + len, size - len, "%s,", db);
+    int ret = snprintf(buf + len, size - len, "%s", db);
     if (ret < 0)  {
       tscError("snprintf failed, buf:%s, ret:%d", buf, ret);
       break;
