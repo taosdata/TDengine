ec7460554db0d35ae35eb00b10b43a9779ff80af
Shungang Li <sgli@taosdata.com>
2024-06-28 10:50:13 +0800

    fix: parseOneStbRow fill tag name

--- source/libs/parser/src/parTranslater.c
+++ source/libs/parser/src/parTranslater.c
@@ -12807,8 +12807,7 @@ static int32_t parseOneStbRow(SMsgBuf* pMsgBuf, SParseFileContext* pParFileCtx) 
       if (TSDB_CODE_SUCCESS == code) {
         SArray* aTagNames = pParFileCtx->tagNameFilled ? NULL : pParFileCtx->aTagNames;
         code = parseTagValue(pMsgBuf, &pParFileCtx->pSql, precision, (SSchema*)pTagSchema, &token,
-                             pParFileCtx->aTagNames, pParFileCtx->aTagVals, &pParFileCtx->pTag);
-        pParFileCtx->tagNameFilled = true;
+                             aTagNames, pParFileCtx->aTagVals, &pParFileCtx->pTag);
       }
     } else {
       // parse tbname
@@ -12826,7 +12825,8 @@ static int32_t parseOneStbRow(SMsgBuf* pMsgBuf, SParseFileContext* pParFileCtx) 
     if (TSDB_CODE_SUCCESS != code) break;
   }
 
-  if (TSDB_CODE_SUCCESS == code) {  // may fail to handle json
+  if (TSDB_CODE_SUCCESS == code) {
+    pParFileCtx->tagNameFilled = true;
     code = tTagNew(pParFileCtx->aTagVals, 1, false, &pParFileCtx->pTag);
   }
 
