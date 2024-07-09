3ed458f4250abaef7649695cf49d3a36a4b5a067
zhipz <zhipzhang@foxmail.com>
2024-07-09 11:08:10 +0800

    Adding database parameter in the common.py:new_con, common.py:new_cur Changing some content of  TS-5130.py

--- tests/pytest/util/common.py
+++ tests/pytest/util/common.py
@@ -524,16 +524,16 @@ class TDCom:
         # print(con)
         return con
 
-    def newcur(self,host='localhost',port=6030,user='root',password='taosdata'):
+    def newcur(self,host='localhost',port=6030,user='root',password='taosdata', database=None):
         cfgPath = self.getClientCfgPath()
-        con=taos.connect(host=host, user=user, password=password, config=cfgPath, port=port)
+        con=taos.connect(host=host, user=user, password=password, config=cfgPath, port=port, database=None)
         cur=con.cursor()
         # print(cur)
         return cur
 
-    def newTdSql(self, host='localhost',port=6030,user='root',password='taosdata'):
+    def newTdSql(self, host='localhost',port=6030,user='root',password='taosdata', database=None):
         newTdSql = TDSql()
-        cur = self.newcur(host=host,port=port,user=user,password=password)
+        cur = self.newcur(host=host,port=port,user=user,password=password, database=None)
         newTdSql.init(cur, False)
         return newTdSql
 
