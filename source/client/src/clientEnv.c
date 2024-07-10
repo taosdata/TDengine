f12cb30e5ba0439f48d3dfc48da3e2dffbf79a9c
Yihao Deng <luomoxyz@126.com>
2024-07-10 11:30:10 +0000

    fix error while client quit

--- source/client/src/clientEnv.c
+++ source/client/src/clientEnv.c
@@ -44,6 +44,7 @@ int64_t  lastClusterId = 0;
 int32_t  clientReqRefPool = -1;
 int32_t  clientConnRefPool = -1;
 int32_t  clientStop = -1;
+int8_t   clientTransportCount = 0;
 
 int32_t timestampDeltaLimit = 900;  // s
 
@@ -137,6 +138,7 @@ void closeTransporter(SAppInstInfo *pAppInfo) {
 
   tscDebug("free transporter:%p in app inst %p", pAppInfo->pTransporter, pAppInfo);
   rpcClose(pAppInfo->pTransporter);
+  atomic_sub_fetch_8(&clientTransportCount, 1);
 }
 
 static bool clientRpcRfp(int32_t code, tmsg_t msgType) {
