1d7080eb89e2db03e1d97a9d9d7b9e016fa62c9e
jiajingbin <jiajaybin@126.com>
2024-07-11 16:38:33 +0800

    test: update

--- cases/stream_computing/stream_computing_test.py
+++ cases/stream_computing/stream_computing_test.py
@@ -4845,6 +4845,7 @@ class StreamComputingTest(TDCase):
         #     self.tdSql.error(f'create stream if not exists {stream_name} into {dbname2}.{tbname} as select ts,c100 from {self.dbname}.{self.case_name}_{tbname}')
 
     def wait_checkpoint_ready(self, stream_name):
+        time.sleep(3)
         cnt = 0
         cmd = f'select distinct status from information_schema.ins_stream_tasks where stream_name = "{stream_name}"'
         self.tdSql.query(cmd)
@@ -4896,8 +4897,6 @@ class StreamComputingTest(TDCase):
         self.tdCom.create_stream(stream_name=f'{self.stb_name}{self.stream_suffix}', des_table=self.stb_stream_des_table, source_sql=f'select _wstart AS wstart, {self.stb_source_select_str}  from {self.stb_name} {partition_elm} interval({self.dataDict["interval"]}s)', trigger_mode="at_once", subtable_value=stb_subtable_value, fill_history_value=fill_history_value)
         self.tdCom.create_stream(stream_name=f'{self.ctb_name}{self.stream_suffix}', des_table=self.ctb_stream_des_table, source_sql=f'select _wstart AS wstart, {self.stb_source_select_str}  from {self.ctb_name} {partition_elm} interval({self.dataDict["interval"]}s)', trigger_mode="at_once", subtable_value=ctb_subtable_value, fill_history_value=fill_history_value)
         self.tdCom.create_stream(stream_name=f'{self.tb_name}{self.stream_suffix}', des_table=self.tb_stream_des_table, source_sql=f'select _wstart AS wstart, {self.tb_source_select_str}  from {self.tb_name} {partition_elm} interval({self.dataDict["interval"]}s)', trigger_mode="at_once", subtable_value=tb_subtable_value, fill_history_value=fill_history_value)
-        if ignore_untreated:
-            self.wait_checkpoint_ready(stream_name)
         start_time = self.date_time
         for i in range(range_count):
             ts_value = str(self.date_time+self.dataDict["interval"])+f'+{i*10}s'
