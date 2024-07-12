b32fe096845fbba6585e7f48e89ea3f623046ed7
huohong <346479823@qq.com>
2024-07-12 15:45:21 +0800

    smoke Test scripts

---
+++ packaging/smokeTest/getAndRunInstaller.bat
@@ -0,0 +1,11 @@
+set baseVersion=%1%
+set version=%2%
+set verMode=%3%
+set fileType=%4%
+set installer=TDengine-%fileType%-%version%-Windows-x64.exe
+echo %installer%
+scp root@192.168.1.213:/nas/TDengine/"%baseVersion%/v%version%/%verMode%/%installer% C:\workspace
+
+echo "***************Finish installer transfer!***************"
+C:\workspace\%installer% /silent
+echo "***************Finish install!***************"
