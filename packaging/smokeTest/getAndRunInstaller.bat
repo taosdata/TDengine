27ddaf0bdb4a8381bae1996b0e6270497cc16971
huohong <346479823@qq.com>
2024-07-12 15:53:53 +0800

    smoke Test scripts

--- packaging/smokeTest/getAndRunInstaller.bat
+++ packaging/smokeTest/getAndRunInstaller.bat
@@ -4,7 +4,7 @@ set verMode=%3%
 set fileType=%4%
 set installer=TDengine-%fileType%-%version%-Windows-x64.exe
 echo %installer%
-scp root@192.168.1.213:/nas/TDengine/"%baseVersion%/v%version%/%verMode%/%installer% C:\workspace
+scp root@192.168.1.213:/nas/TDengine/%baseVersion%/v%version%/%verMode%/%installer% C:\workspace
 
 echo "***************Finish installer transfer!***************"
 C:\workspace\%installer% /silent
