4a2a596e8f95943584a26e6e25e97b884bd50cb4
huohong <346479823@qq.com>
2024-07-15 11:03:02 +0800

    smoke test scripts

--- packaging/smokeTest/getAndRunInstaller.bat
+++ packaging/smokeTest/getAndRunInstaller.bat
@@ -2,6 +2,7 @@ set baseVersion=%1%
 set version=%2%
 set verMode=%3%
 set fileType=%4%
+if %verMode% == "enterprise" ( fileType = enterprise-client)
 set installer=TDengine-%fileType%-%version%-Windows-x64.exe
 echo %installer%
 scp root@192.168.1.213:/nas/TDengine/%baseVersion%/v%version%/%verMode%/%installer% C:\workspace
