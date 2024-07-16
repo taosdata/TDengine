a200dc2b122f475837d23adb10d3bce8cea0fba6
huohong <346479823@qq.com>
2024-07-16 11:59:52 +0800

    smoke test scripts

--- packaging/smokeTest/smokeTestClient.py
+++ packaging/smokeTest/smokeTestClient.py
@@ -76,9 +76,11 @@ else:
     subprocess.getoutput("pip3 install taospy")
 
 # prepare data by taosBenchmark
-
+cmd = ("taosBenchmark -y -a 3 -n 100 -t 100 -d %s -h %s -P %d" % (databaseName, serverHost, serverPort)).split(' ')
+process = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
 print("run taosBenchmark -y -a 3 -n 100 -t 100 -d %s -h %s -P %d" % (databaseName, serverHost, serverPort))
-os.system("taosBenchmark -y -a 3 -n 100 -t 100 -d %s -h %s -P %d" % (databaseName, serverHost, serverPort))
+#os.system("taosBenchmark -y -a 3 -n 100 -t 100 -d %s -h %s -P %d" % (databaseName, serverHost, serverPort))
+process.wait()
 taosBenchmark_test_result = True
 import taos
 
