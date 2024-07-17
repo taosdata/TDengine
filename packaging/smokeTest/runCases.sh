ad582d592ad662b468c34674e895c61b87af87ff
huohong <346479823@qq.com>
2024-07-17 08:52:18 +0800

    smoke test scripts

--- packaging/smokeTest/runCases.sh
+++ packaging/smokeTest/runCases.sh
@@ -3,3 +3,11 @@ ulimit -c unlimited
 
 cd ${TDENGINE_ROOT_DIR}/tests/army
 python3 ./test.py -f query/query_basic.py -N 3
+
+cd ${TDENGINE_ROOT_DIR}/tests/system-test
+python3 ./test.py -f 1-insert/insert_column_value.py
+python3 ./test.py -f 2-query/primary_ts_base_5.py
+python3 ./test.py -f 2-query/case_when.py
+python3 ./test.py -f 2-query/partition_limit_interval.py
+python3 ./test.py -f 2-query/join.py
+python3 ./test.py -f 2-query/fill.py
