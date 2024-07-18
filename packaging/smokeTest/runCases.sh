f7f4db5300ca1b2949066f6274e6c4ebdd4b9125
huohong <346479823@qq.com>
2024-07-18 09:18:49 +0800

    smoke test scripts

--- packaging/smokeTest/runCases.sh
+++ packaging/smokeTest/runCases.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 ulimit -c unlimited
-
+case_out_file=`pwd`/case.out
 cd ${TDENGINE_ROOT_DIR}/tests/army
-python3 ./test.py -f query/query_basic.py -N 3
+python3 ./test.py -f query/query_basic.py -N 3 >> $case_out_file
 
 cd ${TDENGINE_ROOT_DIR}/tests/system-test
-python3 ./test.py -f 1-insert/insert_column_value.py
-python3 ./test.py -f 2-query/primary_ts_base_5.py
-python3 ./test.py -f 2-query/case_when.py
-python3 ./test.py -f 2-query/partition_limit_interval.py
-python3 ./test.py -f 2-query/join.py
-python3 ./test.py -f 2-query/fill.py
+python3 ./test.py -f 1-insert/insert_column_value.py >> $case_out_file
+python3 ./test.py -f 2-query/primary_ts_base_5.py >> $case_out_file
+python3 ./test.py -f 2-query/case_when.py >> $case_out_file
+python3 ./test.py -f 2-query/partition_limit_interval.py >> $case_out_file
+python3 ./test.py -f 2-query/join.py >> $case_out_file
+python3 ./test.py -f 2-query/fill.py >> $case_out_file
