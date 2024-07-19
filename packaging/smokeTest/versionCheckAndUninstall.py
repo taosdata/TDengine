9233026841dec1059efb5afaf158e25c212e9704
huohong <346479823@qq.com>
2024-07-19 11:08:28 +0800

    smoke test scripts

--- packaging/smokeTest/versionCheckAndUninstall.py
+++ packaging/smokeTest/versionCheckAndUninstall.py
@@ -23,12 +23,12 @@ import time
 
 # input for server
 
-opts, args = getopt.gnu_getopt(sys.argv[1:], 'v:u:e', ['version='])
+opts, args = getopt.gnu_getopt(sys.argv[1:], 'v:m:u', ['version=', 'verMode='])
 serverHost = ""
 serverPort = 0
 version = ""
 uninstall = False
-isEnterprise = False
+verMode = ""
 for key, value in opts:
     if key in ['--help']:
         print('A collection of test cases written using Python')
@@ -40,8 +40,8 @@ for key, value in opts:
         version = value
     if key in ['-u']:
         uninstall = True
-    if key in ['-e']:
-        isEnterprise = True
+    if key in ['-m']:
+        verMode = value
 if not version:
     print("No version specified, will not run version check.")
 
@@ -107,7 +107,7 @@ if version:
         taosadapter_V_output = subprocess.getoutput("taosadapter -V | findstr version")
         taoskeeper_V_output = subprocess.getoutput("taoskeeper -V | findstr version")
         taosB_V_output = subprocess.getoutput("taosBenchmark -V | findstr version")
-        if isEnterprise:
+        if verMode == "Enterprise":
             taosx_V_output = subprocess.getoutput("taosx -V | findstr version")
     else:
         taos_V_output = subprocess.getoutput("taos -V | grep version | awk -F ' ' '{print $3}'")
@@ -115,7 +115,7 @@ if version:
         taosadapter_V_output = subprocess.getoutput("taosadapter -V | grep version | awk -F ' ' '{print $3}'")
         taoskeeper_V_output = subprocess.getoutput("taoskeeper -V | grep version | awk -F ' ' '{print $3}'")
         taosB_V_output = subprocess.getoutput("taosBenchmark -V | grep version | awk -F ' ' '{print $3}'")
-        if isEnterprise:
+        if verMode == "Enterprise":
             taosx_V_output = subprocess.getoutput("taosx -V | grep version | awk -F ' ' '{print $3}'")
 
     print("taos -V output is: %s" % taos_V_output)
@@ -123,7 +123,7 @@ if version:
     print("taosadapter -V output is: %s" % taosadapter_V_output)
     print("taoskeeper -V output is: %s" % taoskeeper_V_output)
     print("taosBenchmark -V output is: %s" % taosB_V_output)
-    if isEnterprise:
+    if verMode == "Enterprise":
         print("taosx -V output is: %s" % taosx_V_output)
         taosxVersion = version in taosx_V_output
     else:
