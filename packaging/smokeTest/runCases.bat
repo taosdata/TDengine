c9f9bad931d8afd55fb793001de79a29d4dc13db
huohong <346479823@qq.com>
2024-07-19 09:24:12 +0800

    smoke test scripts

--- packaging/smokeTest/runCases.bat
+++ packaging/smokeTest/runCases.bat
@@ -4,11 +4,11 @@ mkdir %WIN_TDENGINE_ROOT_DIR%\debug\build
 mkdir %WIN_TDENGINE_ROOT_DIR%\debug\build\bin
 xcopy C:\TDengine\taos*.exe %WIN_TDENGINE_ROOT_DIR%\debug\build\bin
 
-case_out_file=`pwd`\case.out
+case_out_file=%cd%\case.out
 cd %WIN_TDENGINE_ROOT_DIR%\tests\army
 python3 test.py -f query\query_basic.py -N 3 >> %case_out_file%
 
-cd ${WIN_TDENGINE_ROOT_DIR}\tests\system-test
+cd %WIN_TDENGINE_ROOT_DIR%\tests\system-test
 python3 test.py -f 1-insert\insert_column_value.py >> %case_out_file%
 python3 test.py -f 2-query\primary_ts_base_5.py >> %case_out_file%
 python3 test.py -f 2-query\case_when.py >> %case_out_file%
