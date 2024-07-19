06234f18a2b90c05d2a2a4e9104654bd5e3a57eb
huohong <346479823@qq.com>
2024-07-19 10:48:26 +0800

    smoke test scripts

--- packaging/smokeTest/getAndRunInstaller.bat
+++ packaging/smokeTest/getAndRunInstaller.bat
@@ -3,6 +3,18 @@ set version=%2%
 set verMode=%3%
 set sType=%4%
 echo %fileType%
+rem stop services
+if EXIST C:\TDengine (
+	if EXIST C:\TDengine\stop-all.bat (
+		call C:\TDengine\stop-all.bat /silent
+		echo "***************Stop taos services***************"
+	)
+	if exist C:\TDengine\unins000.exe (
+		call C:\TDengine\unins000.exe /silent
+		echo "***************uninstall TDengine***************"
+	)
+	rd /S /q C:\TDengine
+)
 if "%verMode%"=="enterprise" (
     if "%sType%"=="client" (
         set fileType=enterprise-client
