8e9b81539209f467238e38db7e44aec1948ae5c6
wangmm0220 <wangmm0220@gmail.com>
2024-07-25 23:27:32 +0800

    fix:[TD-31017]process return value in client

--- source/client/src/clientImpl.c
+++ source/client/src/clientImpl.c
@@ -1427,7 +1427,7 @@ STscObj* taosConnectImpl(const char* user, const char* auth, const char* db, __t
   int64_t transporterId = 0;
   asyncSendMsgToServer(pTscObj->pAppInfo->pTransporter, &pTscObj->pAppInfo->mgmtEp.epSet, &transporterId, body);
 
-  (void)tsem_wait(&pRequest->body.rspSem);
+  (void)tsem2_wait(&pRequest->body.rspSem);
   if (pRequest->code != TSDB_CODE_SUCCESS) {
     const char* errorMsg =
         (pRequest->code == TSDB_CODE_RPC_FQDN_ERROR) ? taos_errstr(pRequest) : tstrerror(pRequest->code);
