e9ef530a59e48b0c8d81bb1ad218bd23397794c8
xsren <285808407@qq.com>
2024-07-29 18:20:50 +0800

    fix: regex cache destroy

--- source/client/src/clientMain.c
+++ source/client/src/clientMain.c
@@ -85,13 +85,14 @@ void taos_cleanup(void) {
     tscWarn("failed to close clientReqRefPool");
   }
 
-  DestroyRegexCache();
   rpcCleanup();
   tscDebug("rpc cleanup");
 
   taosConvDestroy();
 
   tmqMgmtClose();
+
+  DestroyRegexCache();
 
   tscInfo("all local resources released");
   taosCleanupCfg();
