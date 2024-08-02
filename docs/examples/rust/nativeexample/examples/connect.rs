5aa94ea90c844b6724f7bd5b2ce93298fdf47d8d
sheyanjie-qq <249478495@qq.com>
2024-08-02 14:02:22 +0800

    mod rust dev guide

--- docs/examples/rust/nativeexample/examples/connect.rs
+++ docs/examples/rust/nativeexample/examples/connect.rs
@@ -3,7 +3,7 @@ use taos::*;
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     #[allow(unused_variables)]
-    let taos = TaosBuilder::from_dsn("taos://")?.build()?;
-    println!("Connected");
+    let taos = TaosBuilder::from_dsn("taos://localhost:6030")?.build()?;
+    println!("Connected to localhost with native connection successfully.");
     Ok(())
 }
