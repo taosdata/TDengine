a476dd4f6663cd8dad7799095e92ca53810b39be
Yaming Pei <sanwenyuv@sina.com>
2024-08-02 20:51:28 +0800

    The Connector section of the official website reconstructs the C language interface description

--- docs/examples/c/CCreateDBDemo.c
+++ docs/examples/c/CCreateDBDemo.c
@@ -25,49 +25,51 @@
 
 static int DemoCreateDB() {
 // ANCHOR: create_db_and_table
-  int         ret_code  = -1;
-  const char *ip        = "localhost";
-  const char *user      = "root";
-  const char *password  = "taosdata";
+const char *ip        = "localhost";
+const char *user      = "root";
+const char *password  = "taosdata";
 
-  // connect
-  TAOS *taos = taos_connect(ip, user, password, NULL, 0);
-  if (taos == NULL) {
-    printf("failed to connect to server, reason: %s\n", taos_errstr(NULL));
-    goto end;
-  }
+// connect
+TAOS *taos = taos_connect(ip, user, password, NULL, 0);
+if (taos == NULL) {
+  printf("failed to connect to server, reason: %s\n", taos_errstr(NULL));
+  taos_cleanup();
+  return -1;
+}
 
-  // create database
-  TAOS_RES *result = taos_query(taos, "CREATE DATABASE IF NOT EXISTS power");
-  int code = taos_errno(result);
-  if (code != 0) {
-    printf("failed to create database, reason: %s\n", taos_errstr(result));
-    goto end;
-  }
-  taos_free_result(result);
-  printf("success to create database\n");
-
-  // use database
-  result = taos_query(taos, "USE power");
-  taos_free_result(result);
-
-  // create table
-  const char* sql = "CREATE STABLE IF NOT EXISTS meters (ts TIMESTAMP, current FLOAT, voltage INT, phase FLOAT) TAGS (groupId INT, location BINARY(24))";
-  result = taos_query(taos, sql);
-  code = taos_errno(result);
-  if (code != 0) {
-    printf("failed to create table, reason: %s\n", taos_errstr(result));
-    goto end;
-  }
-  taos_free_result(result);
-  printf("success to create table\n");
-  ret_code = 0;
-
-end:
-  // close & clean
+// create database
+TAOS_RES *result = taos_query(taos, "CREATE DATABASE IF NOT EXISTS power");
+int code = taos_errno(result);
+if (code != 0) {
+  printf("failed to create database, reason: %s\n", taos_errstr(result));
   taos_close(taos);
   taos_cleanup();
-  return ret_code;
+  return -1;
+}
+taos_free_result(result);
+printf("success to create database\n");
+
+// use database
+result = taos_query(taos, "USE power");
+taos_free_result(result);
+
+// create table
+const char* sql = "CREATE STABLE IF NOT EXISTS meters (ts TIMESTAMP, current FLOAT, voltage INT, phase FLOAT) TAGS (groupId INT, location BINARY(24))";
+result = taos_query(taos, sql);
+code = taos_errno(result);
+if (code != 0) {
+  printf("failed to create table, reason: %s\n", taos_errstr(result));
+  taos_close(taos);
+  taos_cleanup();
+  return -1;
+}
+taos_free_result(result);
+printf("success to create table\n");
+
+// close & clean
+taos_close(taos);
+taos_cleanup();
+return 0;
 // ANCHOR_END: create_db_and_table
 }
 
--- docs/examples/c/CWithReqIdDemo.c
+++ docs/examples/c/CWithReqIdDemo.c
@@ -25,66 +25,68 @@
 
 static int DemoWithReqId() {
 // ANCHOR: with_reqid
-  int         ret_code  = -1;
-  const char *ip        = "localhost";
-  const char *user      = "root";
-  const char *password  = "taosdata";
+const char *ip        = "localhost";
+const char *user      = "root";
+const char *password  = "taosdata";
 
-  // connect
-  TAOS *taos = taos_connect(ip, user, password, NULL, 0);
-  if (taos == NULL) {
-    printf("failed to connect to server, reason: %s\n", taos_errstr(NULL));
-    goto end;
-  }
+// connect
+TAOS *taos = taos_connect(ip, user, password, NULL, 0);
+if (taos == NULL) {
+  printf("failed to connect to server, reason: %s\n", taos_errstr(NULL));
+  taos_cleanup();
+  return -1;
+}
 
-  // create database
-  TAOS_RES *result = taos_query_with_reqid(taos, "CREATE DATABASE IF NOT EXISTS power", 1L);
-  int code = taos_errno(result);
-  if (code != 0) {
-    printf("failed to create database, reason: %s\n", taos_errstr(result));
-    taos_free_result(result);
-    goto end;
-  }
+// create database
+TAOS_RES *result = taos_query_with_reqid(taos, "CREATE DATABASE IF NOT EXISTS power", 1L);
+int code = taos_errno(result);
+if (code != 0) {
+  printf("failed to create database, reason: %s\n", taos_errstr(result));
   taos_free_result(result);
-  printf("success to create database\n");
-
-  // use database
-  result = taos_query_with_reqid(taos, "USE power", 2L);
-  taos_free_result(result);
-
-  // query data
-  const char* sql = "SELECT * FROM power.meters";
-  result = taos_query_with_reqid(taos, sql, 3L);
-  code = taos_errno(result);
-  if (code != 0) {
-    printf("failed to select, reason: %s\n", taos_errstr(result));
-    goto end;
-  }
-
-  TAOS_ROW    row         = NULL;
-  int         rows        = 0;
-  int         num_fields  = taos_field_count(result);
-  TAOS_FIELD *fields      = taos_fetch_fields(result);
-
-  printf("fields: %d\n", num_fields);
-  printf("sql: %s, result:\n", sql);
-  
-  // fetch the records row by row
-  while ((row = taos_fetch_row(result))) {
-    char temp[1024] = {0};
-    rows++;
-    taos_print_row(temp, row, fields, num_fields);
-    printf("%s\n", temp);
-  }
-  printf("total rows: %d\n", rows);
-  taos_free_result(result);
-  ret_code = 0;
-
-end:
-  // close & clean
   taos_close(taos);
   taos_cleanup();
-  return ret_code;
+  return -1;
+}
+taos_free_result(result);
+printf("success to create database\n");
+
+// use database
+result = taos_query_with_reqid(taos, "USE power", 2L);
+taos_free_result(result);
+
+// query data
+const char* sql = "SELECT * FROM power.meters";
+result = taos_query_with_reqid(taos, sql, 3L);
+code = taos_errno(result);
+if (code != 0) {
+  printf("failed to select, reason: %s\n", taos_errstr(result));
+  taos_close(taos);
+  taos_cleanup();
+  return -1;
+}
+
+TAOS_ROW    row         = NULL;
+int         rows        = 0;
+int         num_fields  = taos_field_count(result);
+TAOS_FIELD *fields      = taos_fetch_fields(result);
+
+printf("fields: %d\n", num_fields);
+printf("sql: %s, result:\n", sql);
+
+// fetch the records row by row
+while ((row = taos_fetch_row(result))) {
+  char temp[1024] = {0};
+  rows++;
+  taos_print_row(temp, row, fields, num_fields);
+  printf("%s\n", temp);
+}
+printf("total rows: %d\n", rows);
+taos_free_result(result);
+
+// close & clean
+taos_close(taos);
+taos_cleanup();
+return 0;
 // ANCHOR_END: with_reqid
 }
 
