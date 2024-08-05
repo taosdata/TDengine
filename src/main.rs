e0556eab9334aa8ba38fdeb4911c91735e965ad4
dingbo8128 <dingbo8128@163.com>
2024-08-05 10:45:08 +0800

    enh(tracing): trace sql execution with trace ID

--- src/main.rs
+++ src/main.rs
@@ -410,12 +410,11 @@ where
                 .add_directive("tokio_tungstenite=warn".parse()?)
                 .add_directive("mio=warn".parse()?)
                 .add_directive("h2=warn".parse()?)
-                .add_directive("sqlx=warn".parse()?)
                 .add_directive("hyper=warn".parse()?)
                 .add_directive("reqwest=warn".parse()?)
                 .add_directive("sled=warn".parse()?)
         } else {
-            event_filter.add_directive("sqlx::query=warn".parse()?)
+            event_filter
         };
         Ok(event_filter)
     }
