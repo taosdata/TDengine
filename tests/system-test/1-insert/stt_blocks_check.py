8077bb3eb8cca15522e6b1d79509d276aa12422a
54liuyao <54liuyao>
2024-08-06 13:50:32 +0800

    adj log

--- tests/system-test/1-insert/stt_blocks_check.py
+++ tests/system-test/1-insert/stt_blocks_check.py
@@ -20,6 +20,7 @@ from util.sqlset import *
 from util.boundary import *
 
 class TDTestCase:
+    updatecfgDict = {'debugFlag': 143}
     def init(self, conn, logSql, replicaVar=1):
         self.replicaVar = int(replicaVar)
         tdLog.debug("start to execute %s" % __file__)
