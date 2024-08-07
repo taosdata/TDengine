621bc6750db80f1bfd19036430fdff064f1a43c7
wangmm0220 <wangmm0220@gmail.com>
2024-08-07 13:59:47 +0800

    fix:[TD-31047] change poll delay bigger to avoid consumer null if split too long

--- tests/system-test/7-tmq/tmqVnodeSplit-stb.py
+++ tests/system-test/7-tmq/tmqVnodeSplit-stb.py
@@ -54,7 +54,7 @@ class TDTestCase:
                     'rowsPerTbl': 1000,
                     'batchNum':   10,
                     'startTs':    1640966400000,  # 2022-01-01 00:00:00.000
-                    'pollDelay':  60,
+                    'pollDelay':  120,
                     'showMsg':    1,
                     'showRow':    1,
                     'snapshot':   0}
@@ -123,7 +123,7 @@ class TDTestCase:
                     'rowsPerTbl': 1000,
                     'batchNum':   10,
                     'startTs':    1640966400000,  # 2022-01-01 00:00:00.000
-                    'pollDelay':  60,
+                    'pollDelay':  120,
                     'showMsg':    1,
                     'showRow':    1,
                     'snapshot':   0}
