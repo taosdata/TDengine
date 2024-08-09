b330edf535ec3f91aae49abfa0bafa81011be557
charles <flyingangel_2008@163.com>
2024-08-09 09:26:54 +0800

    add test case test_passwd by charles

---
+++ tests/army/user/test_passwd.py
@@ -0,0 +1,20 @@
+import subprocess
+from frame.log import *
+from frame.cases import *
+from frame.sql import *
+from frame.caseBase import *
+from frame.epath import *
+from frame import *
+
+
+class TDTestCase(TBase):
+    def run(self):
+        c_file_path = os.sep.join([binPath(), "passwdTest localhost"])
+        p = subprocess.Popen(c_file_path, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        if 0 != p.returncode:
+            tdLog.error("Failed to run passwd test with output: %s \n error: %s" % (out, err))
+        tdLog.success(f"{__file__} successfully executed")
+
+tdCases.addLinux(__file__, TDTestCase())
+tdCases.addWindows(__file__, TDTestCase())
