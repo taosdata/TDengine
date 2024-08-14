ac7c12390e82b22f8fbc864030f3570f828357f9
xuyunlong <xuyunlong@taosdatadeMacBook-Pro.local>
2024-08-14 19:47:50 +0800

    add check_version

--- packaging/smokeTest/test_server_linux.py
+++ packaging/smokeTest/test_server_linux.py
@@ -3,38 +3,54 @@ import subprocess
 import os
 from versionCheckAndUninstallforPytest import UninstallTaos
 import platform
+import re
+import time
 
 current_path = os.path.abspath(os.path.dirname(__file__))
 with open("%s/test_server_linux.txt" % current_path) as f:
     cases = f.read().splitlines()
 
-system = platform.system()
-arch = platform.machine()
+
+@pytest.fixture(scope="module")
+def get_config(request):
+    verMode = request.config.getoption("--verMode")
+    taosVersion = request.config.getoption("--tVersion")
+    baseVersion = request.config.getoption("--baseVersion")
+    sourcePath = request.config.getoption("--sourcePath")
+    config = {
+        "verMode": verMode,
+        "taosVersion": taosVersion,
+        "baseVersion": baseVersion,
+        "sourcePath": sourcePath,
+        "system": platform.system(),
+        "arch": platform.machine()
+    }
+    return config
 
 
 @pytest.fixture(scope="module")
-def setup_module(request):
+def setup_module(get_config):
     def run_cmd(command):
         print("CMD:", command)
         result = subprocess.run(command, capture_output=True, text=True, shell=True)
         print("STDOUT:", result.stdout)
         print("STDERR:", result.stderr)
         print("Return Code:", result.returncode)
-        # assert result.returncode == 0
+        assert result.returncode == 0
         return result
 
     # setup before module tests
+    config = get_config
     # bash getAndRunInstaller.sh -m ${verMode} -f server -l false -c x64  -v ${version} -o ${baseVersion} -s ${sourcePath}  -t tar
-    verMode = request.config.getoption("--verMode")
-    taosVersion = request.config.getoption("--tVersion")
-    baseVersion = request.config.getoption("--baseVersion")
-    sourcePath = request.config.getoption("--sourcePath")
-    cmd = "bash getAndRunInstaller.sh -m %s -f server -l false -c x64 -v %s -o %s -s %s -t tar" % (
-        verMode, taosVersion, baseVersion, sourcePath)
+    t = "tar"
+    if config["system"] == "Darwin":
+        t = "pkg"
+    cmd = "bash getAndRunInstaller.sh -m %s -f server -l false -c x64 -v %s -o %s -s %s -t %s" % (
+        config["verMode"], config["taosVersion"], config["baseVersion"], config["sourcePath"], t)
     run_cmd(cmd)
     cmd = "mkdir -p ../../debug/build/bin/"
     run_cmd(cmd)
-    if system == "Darwin":
+    if config["system"] == "Darwin":
         cmd = "cp /usr/local/bin/taos*  ../../debug/build/bin/"
     else:
         cmd = "cp /usr/bin/taos*  ../../debug/build/bin/"
@@ -42,7 +58,7 @@ def setup_module(request):
 
     yield
 
-    UninstallTaos(taosVersion, verMode, True)
+    UninstallTaos(config["taosVersion"], config["verMode"], True)
 
 
 # use pytest fixture to exec case
@@ -64,7 +80,7 @@ def run_command(request):
 
 
 class TestServerLinux:
-    @pytest.mark.main
+    @pytest.mark.all
     def test_execute_cases(self, setup_module, run_command):
         # assert the result
         if run_command['returncode'] != 0:
@@ -83,3 +99,93 @@ class TestServerLinux:
 
         assert run_command[
                    'returncode'] == 0, f"Command '{run_command['command']}' failed with return code {run_command['returncode']}"
+
+    @pytest.mark.all
+    def test_check_version(self, get_config, setup_module):
+        config = get_config
+        databaseName = re.sub(r'[^a-zA-Z0-9]', '', subprocess.getoutput("hostname")).lower()
+        # install taospy
+        taospy_version = ""
+        system = config["system"]
+        version = config["taosVersion"]
+        verMode = config["verMode"]
+        if system == 'Windows':
+            taospy_version = subprocess.getoutput("pip3 show taospy|findstr Version")
+        else:
+            taospy_version = subprocess.getoutput("pip3 show taospy|grep Version| awk -F ':' '{print $2}' ")
+
+        print("taospy version %s " % taospy_version)
+        if taospy_version == "":
+            subprocess.getoutput("pip3 install git+https://github.com/taosdata/taos-connector-python.git")
+            print("install taos python connector")
+        else:
+            subprocess.getoutput("pip3 install taospy")
+
+        # start taosd server
+        if system == 'Windows':
+            cmd = ["C:\\TDengine\\start-all.bat"]
+        elif system == 'Linux':
+            cmd = "systemctl start taosd".split(' ')
+        else:
+            cmd = "sudo launchctl start com.tdengine.taosd".split(' ')
+        process_out = subprocess.Popen(cmd,
+                                       stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
+        print(cmd)
+        time.sleep(5)
+
+        import taos
+        conn = taos.connect()
+        server_version = conn.server_info
+        print("server_version", server_version)
+        client_version = conn.client_info
+        print("client_version", client_version)
+        # Execute sql get version info
+        result: taos.TaosResult = conn.query("SELECT server_version()")
+        select_server = result.fetch_all()[0][0]
+        print("SELECT server_version():" + select_server)
+        result: taos.TaosResult = conn.query("SELECT client_version()")
+        select_client = result.fetch_all()[0][0]
+        print("SELECT client_version():" + select_client)
+        conn.close()
+
+        taos_V_output = ""
+        taosd_V_output = ""
+        taosadapter_V_output = ""
+        taoskeeper_V_output = ""
+        taosx_V_output = ""
+        taosB_V_output = ""
+        taosxVersion = False
+        if system == "Windows":
+            taos_V_output = subprocess.getoutput("taos -V | findstr version")
+            taosd_V_output = subprocess.getoutput("taosd -V | findstr version")
+            taosadapter_V_output = subprocess.getoutput("taosadapter -V | findstr version")
+            taoskeeper_V_output = subprocess.getoutput("taoskeeper -V | findstr version")
+            taosB_V_output = subprocess.getoutput("taosBenchmark -V | findstr version")
+            if verMode == "Enterprise":
+                taosx_V_output = subprocess.getoutput("taosx -V | findstr version")
+        else:
+            taos_V_output = subprocess.getoutput("taos -V | grep version | awk -F ' ' '{print $3}'")
+            taosd_V_output = subprocess.getoutput("taosd -V | grep version | awk -F ' ' '{print $3}'")
+            taosadapter_V_output = subprocess.getoutput("taosadapter -V | grep version | awk -F ' ' '{print $3}'")
+            taoskeeper_V_output = subprocess.getoutput("taoskeeper -V | grep version | awk -F ' ' '{print $3}'")
+            taosB_V_output = subprocess.getoutput("taosBenchmark -V | grep version | awk -F ' ' '{print $3}'")
+            if verMode == "Enterprise":
+                taosx_V_output = subprocess.getoutput("taosx -V | grep version | awk -F ' ' '{print $3}'")
+
+        print("taos -V output is: %s" % taos_V_output)
+        print("taosd -V output is: %s" % taosd_V_output)
+        print("taosadapter -V output is: %s" % taosadapter_V_output)
+        print("taoskeeper -V output is: %s" % taoskeeper_V_output)
+        print("taosBenchmark -V output is: %s" % taosB_V_output)
+        assert version in client_version
+        assert version in server_version
+        assert version in select_server
+        assert version in select_client
+        assert version in taos_V_output
+        assert version in taosd_V_output
+        assert version in taosadapter_V_output
+        assert version in taoskeeper_V_output
+        assert version in taosB_V_output
+        if verMode == "Enterprise":
+            print("taosx -V output is: %s" % taosx_V_output)
+            assert version in taosx_V_output
--- packaging/smokeTest/versionCheckAndUninstallforPytest.py
+++ packaging/smokeTest/versionCheckAndUninstallforPytest.py
@@ -25,122 +25,11 @@ from lib import run_cmd
 
 # input for server
 def UninstallTaos(version, verMode, uninstall):
-    # opts, args = getopt.gnu_getopt(sys.argv[1:], 'v:m:u', ['version=', 'verMode='])
-    # serverHost = ""
-    # serverPort = 0
-    # version = ""
-    # uninstall = False
-    # verMode = ""
-    # for key, value in opts:
-    #     if key in ['--help']:
-    #         print('A collection of test cases written using Python')
-    #         print('-v test client version')
-    #         print('-u test uninstall process, will uninstall TDengine')
-    #         sys.exit(0)
-    #
-    #     if key in ['-v']:
-    #         version = value
-    #     if key in ['-u']:
-    #         uninstall = True
-    #     if key in ['-m']:
-    #         verMode = value
     if not version:
         raise "No version specified, will not run version check."
 
     system = platform.system()
     arch = platform.machine()
-
-    databaseName = re.sub(r'[^a-zA-Z0-9]', '', subprocess.getoutput("hostname")).lower()
-    # install taospy
-    taospy_version = ""
-    if system == 'Windows':
-        taospy_version = subprocess.getoutput("pip3 show taospy|findstr Version")
-    else:
-        taospy_version = subprocess.getoutput("pip3 show taospy|grep Version| awk -F ':' '{print $2}' ")
-
-    print("taospy version %s " % taospy_version)
-    if taospy_version == "":
-        subprocess.getoutput("pip3 install git+https://github.com/taosdata/taos-connector-python.git")
-        print("install taos python connector")
-    else:
-        subprocess.getoutput("pip3 install taospy")
-
-    # start taosd server
-    if system == 'Windows':
-        cmd = ["C:\\TDengine\\start-all.bat"]
-    elif system == 'Linux':
-        cmd = "systemctl start taosd".split(' ')
-    else:
-        cmd = "sudo launchctl start com.tdengine.taosd".split(' ')
-    process_out = subprocess.Popen(cmd,
-                                   stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
-    print(cmd)
-    time.sleep(5)
-
-    # get taosc version info
-    version_test_result = False
-    if version:
-        import taos
-        conn = taos.connect()
-        server_version = conn.server_info
-        print("server_version", server_version)
-        client_version = conn.client_info
-        print("client_version", client_version)
-        # Execute sql get version info
-        result: taos.TaosResult = conn.query("SELECT server_version()")
-        select_server = result.fetch_all()[0][0]
-        print("SELECT server_version():" + select_server)
-        result: taos.TaosResult = conn.query("SELECT client_version()")
-        select_client = result.fetch_all()[0][0]
-        print("SELECT client_version():" + select_client)
-        conn.close()
-
-        taos_V_output = ""
-        taosd_V_output = ""
-        taosadapter_V_output = ""
-        taoskeeper_V_output = ""
-        taosx_V_output = ""
-        taosB_V_output = ""
-        taosxVersion = False
-        if system == "Windows":
-            taos_V_output = subprocess.getoutput("taos -V | findstr version")
-            taosd_V_output = subprocess.getoutput("taosd -V | findstr version")
-            taosadapter_V_output = subprocess.getoutput("taosadapter -V | findstr version")
-            taoskeeper_V_output = subprocess.getoutput("taoskeeper -V | findstr version")
-            taosB_V_output = subprocess.getoutput("taosBenchmark -V | findstr version")
-            if verMode == "Enterprise":
-                taosx_V_output = subprocess.getoutput("taosx -V | findstr version")
-        else:
-            taos_V_output = subprocess.getoutput("taos -V | grep version | awk -F ' ' '{print $3}'")
-            taosd_V_output = subprocess.getoutput("taosd -V | grep version | awk -F ' ' '{print $3}'")
-            taosadapter_V_output = subprocess.getoutput("taosadapter -V | grep version | awk -F ' ' '{print $3}'")
-            taoskeeper_V_output = subprocess.getoutput("taoskeeper -V | grep version | awk -F ' ' '{print $3}'")
-            taosB_V_output = subprocess.getoutput("taosBenchmark -V | grep version | awk -F ' ' '{print $3}'")
-            if verMode == "Enterprise":
-                taosx_V_output = subprocess.getoutput("taosx -V | grep version | awk -F ' ' '{print $3}'")
-
-        print("taos -V output is: %s" % taos_V_output)
-        print("taosd -V output is: %s" % taosd_V_output)
-        print("taosadapter -V output is: %s" % taosadapter_V_output)
-        print("taoskeeper -V output is: %s" % taoskeeper_V_output)
-        print("taosBenchmark -V output is: %s" % taosB_V_output)
-        if verMode == "Enterprise":
-            print("taosx -V output is: %s" % taosx_V_output)
-            taosxVersion = version in taosx_V_output
-        else:
-            taosxVersion = True
-        if (version in client_version
-                and version in server_version
-                and version in select_server
-                and version in select_client
-                and version in taos_V_output
-                and version in taosd_V_output
-                and version in taosadapter_V_output
-                and version in taoskeeper_V_output
-                and version in taosB_V_output
-                and taosxVersion
-        ):
-            version_test_result = True
     leftFile = False
     if uninstall:
         print("Start to run rmtaos")
@@ -202,17 +91,18 @@ def UninstallTaos(version, verMode, uninstall):
                 print("*******Test Result: uninstall test passed ************")
 
         elif system == "Darwin":
-            # 启动命令
-            process = subprocess.Popen(['rmtaos'], stdin=subprocess.PIPE, stdout=subprocess.PIPE,
-                                       stderr=subprocess.PIPE, text=True)
-
-            # 发送交互输入
-            stdout, stderr = process.communicate(
-                input="y\nI confirm that I would like to delete all data, log and configuration files.\n")
-
-            # 打印输出（可选）
-            print(stdout)
-            print(stderr)
+            # 创建一个subprocess.Popen对象，并使用stdin和stdout进行交互
+            process = subprocess.Popen(['sudo', 'rmtaos'],
+                                       stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
+            # 向子进程发送输入
+            process.stdin.write("y\n")
+            process.stdin.flush()  # 确保输入被发送到子进程
+            process.stdin.write("I confirm that I would like to delete all data, log and configuration files\n")
+            process.stdin.flush()  # 确保输入被发送到子进程
+            # 关闭子进程的stdin，防止它无限期等待更多输入
+            process.stdin.close()
+            # 等待子进程结束
+            process.wait()
             # 检查目录清除情况
             out = subprocess.getoutput("ls /usr/local/bin/taos*")
             if "No such file or directory" not in out:
@@ -245,15 +135,9 @@ def UninstallTaos(version, verMode, uninstall):
                 leftFile = True
                 print("Uninstall left some files: %s" % out)
 
-    if version_test_result:
-        print("**********Test Result: version test passed! **********")
-    else:
-        print("!!!!!!!!!!!Test Result: version test failed! !!!!!!!!!!")
     if not leftFile:
         print("**********Test Result: uninstall test passed! **********")
     else:
         print("!!!!!!!!!!!Test Result: uninstall test failed! !!!!!!!!!!")
-    if version_test_result and not leftFile:
-        pass
-    else:
+    if leftFile:
         raise "uninstall taos fail, please check"
