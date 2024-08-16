eac8c856c5f52cf8b9b4ebc52b6712cc55baffbc
sheyanjie-qq <249478495@qq.com>
2024-08-16 11:28:03 +0800

    update java sample code

--- docs/examples/java/src/main/java/com/taos/example/JdbcInsertDataDemo.java
+++ docs/examples/java/src/main/java/com/taos/example/JdbcInsertDataDemo.java
@@ -25,25 +25,26 @@ public class JdbcInsertDataDemo {
         properties.setProperty("timezone", "UTC-8");
         System.out.println("get connection starting...");
 // ANCHOR: insert_data
+        // insert data, please make sure the database and table are created before
+        String insertQuery = "INSERT INTO " +
+                "power.d1001 USING power.meters TAGS(2,'California.SanFrancisco') " +
+                "VALUES " +
+                "(NOW + 1a, 10.30000, 219, 0.31000) " +
+                "(NOW + 2a, 12.60000, 218, 0.33000) " +
+                "(NOW + 3a, 12.30000, 221, 0.31000) " +
+                "power.d1002 USING power.meters TAGS(3, 'California.SanFrancisco') " +
+                "VALUES " +
+                "(NOW + 1a, 10.30000, 218, 0.25000) ";
         try (Connection connection = DriverManager.getConnection(jdbcUrl, properties);
              Statement stmt = connection.createStatement()) {
 
-            // insert data, please make sure the database and table are created before
-            String insertQuery = "INSERT INTO " +
-                    "power.d1001 USING power.meters TAGS(2,'California.SanFrancisco') " +
-                    "VALUES " +
-                    "(NOW + 1a, 10.30000, 219, 0.31000) " +
-                    "(NOW + 2a, 12.60000, 218, 0.33000) " +
-                    "(NOW + 3a, 12.30000, 221, 0.31000) " +
-                    "power.d1002 USING power.meters TAGS(3, 'California.SanFrancisco') " +
-                    "VALUES " +
-                    "(NOW + 1a, 10.30000, 218, 0.25000) ";
             int affectedRows = stmt.executeUpdate(insertQuery);
             // you can check affectedRows here
             System.out.println("Successfully inserted " + affectedRows + " rows to power.meters.");
         } catch (Exception ex) {
             // please refer to the JDBC specifications for detailed exceptions info
-            System.out.printf("Failed to insert data to power.meters, %sErrMessage: %s%n",
+            System.out.printf("Failed to insert data to power.meters, sql: %s, %sErrMessage: %s%n",
+                    insertQuery,
                     ex instanceof SQLException ? "ErrCode: " + ((SQLException) ex).getErrorCode() + ", " : "",
                     ex.getMessage());
             // Print stack trace for context in examples. Use logging in production.
