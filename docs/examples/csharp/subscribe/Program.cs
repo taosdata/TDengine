e8fcd42dfa7261a0aecc5ff25827711231725892
t_max <1172915550@qq.com>
2024-08-16 15:22:18 +0800

    docs: update the output of go and c# examples

--- docs/examples/csharp/subscribe/Program.cs
+++ docs/examples/csharp/subscribe/Program.cs
@@ -111,11 +111,12 @@ namespace TMQExample
             catch (Exception e)
             {
                 // handle other exceptions
-                Console.WriteLine($"Failed to create native consumer, " +
-                                  $"host: {_host}, " +
-                                  $"groupId: {_groupId}, " +
-                                  $"clientId: {_clientId}, " +
-                                  $"ErrMessage: {e.Message}");
+                Console.WriteLine(
+                    $"Failed to create native consumer, " +
+                    $"host: {_host}, " +
+                    $"groupId: {_groupId}, " +
+                    $"clientId: {_clientId}, " +
+                    $"ErrMessage: {e.Message}");
                 throw;
             }
 
@@ -163,11 +164,12 @@ namespace TMQExample
             catch (Exception e)
             {
                 // handle other exceptions
-                Console.WriteLine($"Failed to poll data, " +
-                                  $"topic: {_topic}, " +
-                                  $"groupId: {_groupId}, " +
-                                  $"clientId: {_clientId}, " +
-                                  $"ErrMessage: {e.Message}");
+                Console.WriteLine(
+                    $"Failed to poll data, " +
+                    $"topic: {_topic}, " +
+                    $"groupId: {_groupId}, " +
+                    $"clientId: {_clientId}, " +
+                    $"ErrMessage: {e.Message}");
                 throw;
             }
             // ANCHOR_END: subscribe
@@ -186,13 +188,14 @@ namespace TMQExample
                 {
                     consumer.Seek(new TopicPartitionOffset(topicPartition.Topic, topicPartition.Partition, 0));
                 }
+
                 Console.WriteLine("Assignment seek to beginning successfully");
             }
             catch (TDengineError e)
             {
                 // handle TDengine error
                 Console.WriteLine(
-                    $"Failed to execute seek example, " +
+                    $"Failed to seek offset, " +
                     $"topic: {_topic}, " +
                     $"groupId: {_groupId}, " +
                     $"clientId: {_clientId}, " +
@@ -204,12 +207,13 @@ namespace TMQExample
             catch (Exception e)
             {
                 // handle other exceptions
-                Console.WriteLine($"Failed to execute seek example, " +
-                                  $"topic: {_topic}, " +
-                                  $"groupId: {_groupId}, " +
-                                  $"clientId: {_clientId}, " +
-                                  $"offset: 0, " +
-                                  $"ErrMessage: {e.Message}");
+                Console.WriteLine(
+                    $"Failed to seek offset, " +
+                    $"topic: {_topic}, " +
+                    $"groupId: {_groupId}, " +
+                    $"clientId: {_clientId}, " +
+                    $"offset: 0, " +
+                    $"ErrMessage: {e.Message}");
                 throw;
             }
             // ANCHOR_END: seek
@@ -240,7 +244,7 @@ namespace TMQExample
                 {
                     // handle TDengine error
                     Console.WriteLine(
-                        $"Failed to execute commit example, " +
+                        $"Failed to commit offset, " +
                         $"topic: {_topic}, " +
                         $"groupId: {_groupId}, " +
                         $"clientId: {_clientId}, " +
@@ -253,7 +257,7 @@ namespace TMQExample
                 {
                     // handle other exceptions
                     Console.WriteLine(
-                        $"Failed to execute commit example, " +
+                        $"Failed to commit offset, " +
                         $"topic: {_topic}, " +
                         $"groupId: {_groupId}, " +
                         $"clientId: {_clientId}, " +
