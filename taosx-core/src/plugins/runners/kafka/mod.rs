5d59ed6a327beded10ae679fb529f645b37ee6d3
Huo Linhe <linhehuo@gmail.com>
2024-08-20 14:53:18 +0800

    refactor(kafka): use offsets cache in context for rebalance-compatible

--- taosx-core/src/plugins/runners/kafka/mod.rs
+++ taosx-core/src/plugins/runners/kafka/mod.rs
@@ -12,6 +12,7 @@ use arrow::ipc::writer::StreamWriter;
 use arrow::record_batch::RecordBatch;
 use arrow_schema::SchemaRef;
 use chrono::Utc;
+use futures_ext::TryReadyChunksError;
 use futures_util::StreamExt;
 use itertools::Itertools;
 use linked_hash_map::LinkedHashMap;
@@ -19,15 +20,17 @@ use rdkafka::client::ClientContext;
 use rdkafka::config::{ClientConfig, RDKafkaLogLevel};
 use rdkafka::consumer::stream_consumer::StreamConsumer;
 use rdkafka::consumer::{BaseConsumer, CommitMode, Consumer, ConsumerContext, Rebalance};
-use rdkafka::error::KafkaResult;
+use rdkafka::error::{KafkaError, KafkaResult};
 use rdkafka::message::{BorrowedMessage, Message};
 use rdkafka::topic_partition_list::TopicPartitionList;
+use rdkafka::types::RDKafkaErrorCode;
 use rdkafka::Offset;
+use scc::HashIndex;
 use serde_json::json;
 use taos::Dsn;
 use tokio::task::JoinSet;
 use tokio_util::sync::CancellationToken;
-use tracing::{instrument, Instrument, Span};
+use tracing::{instrument, warn, Instrument, Span};
 
 use taosx_ipc::ack::AckReaderBuilder;
 use taosx_ipc::prelude::ArrowDataType;
@@ -43,6 +46,7 @@ use crate::{build_ipc, Action, Parser, Transferred};
 mod config;
 
 pub const KAFKA_ID: &'static str = "kafka";
+const FETCH_METADATA_TIMEOUT: Duration = Duration::from_secs(30);
 
 pub async fn is_valid(dsn: &Dsn) -> DataSourceValidation {
     match is_valid_impl(dsn) {
@@ -61,8 +65,8 @@ fn is_valid_impl(dsn: &Dsn) -> anyhow::Result<()> {
         .map_err(|err| anyhow::anyhow!("failed to create client, cause: {:#}", err))?;
 
     let metadata = consumer
-        .fetch_metadata(None, Duration::from_secs(5))
-        .map_err(|err| anyhow::anyhow!("failed to load meta data, cause: {:#}", err))?;
+        .fetch_metadata(None, FETCH_METADATA_TIMEOUT)
+        .context("failed to load meta data while checking kafka data source")?;
 
     tracing::info!(
         brokers = metadata
@@ -426,6 +430,12 @@ async fn execute(
 
     let batch_size = config.advanced_options.batch_size.unwrap_or(1000);
     let batch_timeout_ms = config.advanced_options.batch_timeout.unwrap_or(1000) as i64;
+    tracing::info!(
+        timeout = config.timeout,
+        batch.size = batch_size,
+        batch.timeout.ms = batch_timeout_ms,
+        "Kafka consumer configuration"
+    );
 
     // split into sub tasks
     let sub_tasks: Vec<SubTask> = SubTask::build_tasks(config, notify.clone())?;
@@ -453,13 +463,7 @@ async fn execute(
                 .in_current_span()
                 .await
             }
-            .instrument(tracing::info_span!(
-                "kafka_consumer",
-                id = idx,
-                timeout = timeout,
-                batch_size = batch_size,
-                batch_timeout = batch_timeout_ms,
-            )),
+            .instrument(tracing::info_span!("consumer", id = idx)),
         );
     }
 
@@ -483,12 +487,12 @@ impl SubTask {
         // create a base consumer
         let consumer: BaseConsumer = client_config
             .create()
-            .map_err(|err| anyhow::anyhow!("failed to create consumer, cause: {:#}", err))?;
+            .context("failed to create consumer")?;
 
         // fetch metadata
         let metadata = consumer
-            .fetch_metadata(None, Duration::from_secs(5))
-            .map_err(|err| anyhow::anyhow!("failed to load meta data, cause: {:#}", err))?;
+            .fetch_metadata(None, FETCH_METADATA_TIMEOUT)
+            .context("failed to load meta data")?;
 
         tracing::info!(
             brokers = metadata
@@ -534,12 +538,12 @@ impl SubTask {
 
         let mut sub_tasks = Vec::new();
         let concurrency = match config.advanced_options.read_concurrency {
-            Some(n) if n > 0 => n,
+            Some(n) if n > 0 => n.min(topic_partitions.values().sum()),
             _ => topic_partitions.values().sum(),
         };
 
         for _ in 0..concurrency {
-            let consumer = consumer_builder(config.clone())?;
+            let consumer = consumer_builder(&config)?;
             let topics = topic_partitions
                 .keys()
                 .into_iter()
@@ -587,8 +591,6 @@ struct MessagesSender<'a> {
     last_polling: i64,
     last_sent: i64,
 
-    offsets_cache: HashMap<String, HashMap<i32, i64>>,
-
     // Builders
     timestamp: TimestampNanosecondBuilder,
     topic: StringBuilder,
@@ -610,17 +612,52 @@ impl<'a> MessagesSender<'a> {
         }
 
         let now = std::time::Instant::now();
-        for msg in chunk {
-            if let Some(s) = msg.payload() {
-                self.timestamp
-                    .append_value(Utc::now().timestamp_nanos_opt().unwrap());
-                self.topic.append_value(msg.topic());
-                self.partition.append_value(msg.partition());
-                self.offset.append_value(msg.offset());
-                self.key.append_value(msg.key().unwrap_or(&[]));
-                self.value.append_value(s);
-            };
+        let context = self.consumer.context();
+        let chunks = chunk.iter().chunk_by(|msg| (msg.topic(), msg.partition()));
+
+        for ((topic, partition), iter) in &chunks {
+            let mut offset = None;
+            for msg in iter {
+                offset.replace(msg.offset());
+                if let Some(s) = msg.payload() {
+                    self.timestamp
+                        .append_value(Utc::now().timestamp_nanos_opt().unwrap());
+                    self.topic.append_value(msg.topic());
+                    self.partition.append_value(msg.partition());
+                    self.offset.append_value(msg.offset());
+                    self.key.append_value(msg.key().unwrap_or(&[]));
+                    self.value.append_value(s);
+                }
+            }
+            let offset = offset.expect("offset should always exist");
+
+            if let Some(map) = context.offsets_cache.get(topic) {
+                unsafe {
+                    map.entry(partition)
+                        .and_modify(|v| *v = offset)
+                        .or_insert(offset);
+                }
+            } else {
+                let map = HashIndex::with_capacity(1);
+                let _ = map
+                    .insert(partition, offset)
+                    .inspect_err(|(partition, offset)| {
+                        tracing::warn!(topic, partition, offset, "Push offset error")
+                    });
+                let _ = context
+                    .offsets_cache
+                    .insert(topic.to_string(), map)
+                    .inspect_err(|_| {
+                        tracing::warn!(
+                            topic,
+                            partition,
+                            offset,
+                            "Push offset error for topic `{topic}`"
+                        )
+                    });
+            }
         }
+
         tracing::debug!(
             elapsed = ?now.elapsed(),
             cache.len = self.value.len(),
@@ -642,11 +679,7 @@ impl<'a> MessagesSender<'a> {
                 "Batch size reached, send directly"
             );
             // Reaches batch size, send directly.
-            return unsafe {
-                self.send_unchecked_with_messages(chunk)
-                    .in_current_span()
-                    .await
-            };
+            return unsafe { self.send_unchecked().in_current_span().await };
         }
 
         let now = chrono::Utc::now().timestamp_millis();
@@ -656,95 +689,17 @@ impl<'a> MessagesSender<'a> {
                 "Batch timeout reached, send directly"
             );
             // Reaches batch timeout, send directly.
-            return unsafe {
-                self.send_unchecked_with_messages(chunk)
-                    .in_current_span()
-                    .await
-            };
+            return unsafe { self.send_unchecked().in_current_span().await };
         }
 
         tracing::trace!(
             cache.len = self.value.len(),
             "Stay in cache, go next polling"
         );
-
-        for msg in chunk {
-            let topic = msg.topic().to_string();
-            if let Some(map) = self.offsets_cache.get_mut(&topic) {
-                map.insert(msg.partition(), msg.offset());
-            } else {
-                let mut map = HashMap::with_capacity(1);
-                map.insert(msg.partition(), msg.offset());
-                self.offsets_cache.insert(topic, map);
-            }
-        }
         // Partially in cache, go next polling.
         anyhow::Ok(ExitStatus::None)
     }
 
-    async unsafe fn send_unchecked_with_messages(
-        &mut self,
-        chunk: &[BorrowedMessage<'a>],
-    ) -> anyhow::Result<ExitStatus> {
-        debug_assert!(
-            self.value.len() > 0,
-            "value length should be greater than 0"
-        );
-        let batch = RecordBatch::try_new(
-            self.schema.clone(),
-            vec![
-                Arc::new(self.timestamp.finish()),
-                Arc::new(self.topic.finish()),
-                Arc::new(self.partition.finish()),
-                Arc::new(self.offset.finish()),
-                Arc::new(self.key.finish()),
-                Arc::new(self.value.finish()),
-            ],
-        )?;
-
-        let batch_size = batch.num_rows();
-        let now = std::time::Instant::now();
-        self.tx.send_async(batch).await?;
-        let sending_elapsed = now.elapsed();
-        if sending_elapsed > Duration::from_secs(2) {
-            tracing::warn!(elapsed = ?now.elapsed(), batch.size = batch_size, "Sending batch to IPC may be slow");
-        } else {
-            tracing::warn!(elapsed = ?now.elapsed(), batch.size = batch_size, "Sending batch to IPC");
-        }
-        self.last_sent = chrono::Utc::now().timestamp_millis();
-
-        tracing::debug!(
-            "Kafka consumer send batch to IPC Writer, batch size: {}",
-            batch_size
-        );
-        for msg in chunk {
-            self.consumer
-                .store_offset_from_message(msg)
-                .with_context(|| {
-                    format!(
-                        "Store offset error in partition {} of `{}`",
-                        msg.partition(),
-                        msg.topic()
-                    )
-                })?;
-        }
-        let now = std::time::Instant::now();
-        if let Err(err) = self.consumer.commit_consumer_state(CommitMode::Sync) {
-            let err_str = format!("{:#}", err);
-            tracing::warn!("failed to commit consumer state, cause: {}", err_str);
-            if err_str.contains("NoOffset") {
-                return Ok(ExitStatus::Finished);
-            }
-            bail!("failed to commit consumer state, cause: {}", err_str);
-        }
-        if now.elapsed() > Duration::from_secs(2) {
-            tracing::warn!(elapsed = ?now.elapsed(), batch.size = batch_size, "committing may be slow");
-        } else {
-            tracing::warn!(elapsed = ?now.elapsed(), batch.size = batch_size, "committing done");
-        }
-
-        anyhow::Ok(ExitStatus::Finished)
-    }
     async unsafe fn send_unchecked(&mut self) -> anyhow::Result<ExitStatus> {
         debug_assert!(
             self.value.len() > 0,
@@ -771,17 +726,55 @@ impl<'a> MessagesSender<'a> {
             batch_size
         );
 
-        for (topic, map) in self.offsets_cache.iter() {
-            for (partition, offset) in map.iter() {
-                self.consumer
-                    .store_offset(&topic, *partition, *offset)
-                    .with_context(|| {
-                        format!(
-                            "Store offset error in partition {} of `{}`",
-                            partition, offset
-                        )
-                    })?;
+        let context = self.consumer.context();
+        let guard = scc::ebr::Guard::new();
+
+        let mut assignment = None;
+        let mut no_offsets = true;
+        for (topic, map) in context.offsets_cache.iter(&guard) {
+            for (partition, offset) in map.iter(&guard) {
+                if let Err(err) = self.consumer.store_offset(&topic, *partition, *offset) {
+                    warn!(
+                        cause = %err,
+                        topic,
+                        partition,
+                        offset,
+                        "Store offset error in partition {}",
+                        partition,
+                    );
+                    if assignment.is_none() {
+                        assignment = self.consumer.assignment().inspect_err(|err| {
+                            tracing::error!(cause  = %err, "Get consumer assignment error")
+                        }).ok();
+                    }
+                    if assignment.is_none() {
+                        bail!("Store offset error in partition {partition} of topic `{topic}`, seems assignment lost");
+                    }
+                    if assignment
+                        .as_ref()
+                        .unwrap()
+                        .elements_for_topic(topic)
+                        .iter()
+                        .all(|item| item.partition() != *partition)
+                    {
+                        tracing::warn!("Rebalanced, partition {partition} is no longer assigned to this consumer");
+                    } else {
+                        Err(err).with_context(|| {
+                            format!(
+                                "Store offset error in partition {partition} of topic `{topic}`"
+                            )
+                        })?;
+                    }
+                } else {
+                    no_offsets = false;
+                }
             }
+        }
+        drop(guard);
+
+        if no_offsets {
+            tracing::warn!(batch.size = batch_size, "No offsets stored, skip commit");
+            return anyhow::Ok(ExitStatus::Finished);
         }
 
         if let Err(err) = self.consumer.commit_consumer_state(CommitMode::Sync) {
@@ -854,7 +847,6 @@ async fn poll_message<'a>(
         polling_timeout_ms: timeout,
         last_polling: chrono::Utc::now().timestamp_millis(),
         last_sent: chrono::Utc::now().timestamp_millis(),
-        offsets_cache: HashMap::with_capacity(1),
         timestamp,
         topic,
         partition,
@@ -896,12 +888,23 @@ async fn poll_message<'a>(
                             }
                         }
                     }
-                    Some(Err(err)) => {
+                    Some(Err(TryReadyChunksError(_, e))) => {
+                        if e == KafkaError::MessageConsumption(RDKafkaErrorCode::OperationTimedOut) {
+                            tracing::warn!("Kafka polling timeout, continue");
+                            tokio::time::sleep(Duration::from_millis(500)).await;
+                            continue;
+                        }
+                        if e == KafkaError::MessageConsumption(RDKafkaErrorCode::PollExceeded) {
+                            tracing::warn!("Maximum application poll interval (max.poll.interval.ms) exceeded, try continue");
+                            tokio::time::sleep(Duration::from_millis(500)).await;
+                            continue;
+                        }
+
                         // Ready chunks may still contains some messages, but we just skip them.
                         // It will be handled by next consuming.
-                        let _ = notify.send(crate::TaskNotify::error(format!("failed to polling from kafka, cause: {:#}", err)));
-                        tracing::error!("failed to polling from kafka, cause: {:#}", err);
-                        bail!("failed to polling from kafka, cause: {:#}", err);
+                        let _ = notify.send(crate::TaskNotify::error(format!("failed to polling from kafka, cause: {:#}", e)));
+                        tracing::error!("failed to polling from kafka, cause: {:#}", e);
+                        bail!("failed to polling from kafka, cause: {:#}", e);
                     }
                     None => {
                         tracing::trace!("Kafka polling return None, continue");
@@ -952,7 +955,17 @@ fn build_schema() -> Schema {
 /// due to this issue: https://github.com/fede1024/rust-rdkafka/issues/681
 /// do not use `{:?}` for `TopicPartitionList` struct, or we will meet a panic
 /// we use a temporary workaround for now
-struct CustomContext;
+struct CustomContext {
+    offsets_cache: scc::HashIndex<String, scc::HashIndex<i32, i64>>,
+}
+
+impl Default for CustomContext {
+    fn default() -> Self {
+        Self {
+            offsets_cache: scc::HashIndex::with_capacity(1),
+        }
+    }
+}
 
 impl ClientContext for CustomContext {}
 
@@ -968,7 +981,8 @@ impl ConsumerContext for CustomContext {
         if is_rebalance_empty(rebalance) {
             return;
         }
-        tracing::info!("Post rebalance {:?}", rebalance);
+        tracing::info!("Post rebalance {:?}, clear offsets", rebalance);
+        self.offsets_cache.clear();
     }
 
     fn commit_callback(&self, result: KafkaResult<()>, tpl: &TopicPartitionList) {
@@ -998,17 +1012,17 @@ fn is_tplist_empty(tpl: &TopicPartitionList) -> bool {
 // A type alias with your custom consumer can be created for convenience.
 type LoggingConsumer = StreamConsumer<CustomContext>;
 
-fn consumer_builder(config: KafkaTaskConfig) -> anyhow::Result<LoggingConsumer> {
+fn consumer_builder(config: &KafkaTaskConfig) -> anyhow::Result<LoggingConsumer> {
     let mut client = build_client_config(config.connect.clone()).unwrap();
     // Client identifier, default "rdkafka".
-    if let Some(client_id) = config.client_id {
+    if let Some(client_id) = &config.client_id {
         client.set("client.id", client_id);
     }
     // All clients sharing the same group.id belong to the same group.
-    client.set("group.id", config.group);
+    client.set("group.id", &config.group);
     // Action to take when there is no initial offset in offset store or the desired offset is out of range.
     // smallest, earliest, beginning, largest, latest, end, error
-    client.set("auto.offset.reset", config.fallback_offset);
+    client.set("auto.offset.reset", &config.fallback_offset);
 
     // Refer to [rdkafka configuration](https://github.com/confluentinc/librdkafka/blob/master/CONFIGURATION.md).
     // > Note: It is recommended to set `enable.auto.offset.store=false`
@@ -1061,7 +1075,7 @@ fn consumer_builder(config: KafkaTaskConfig) -> anyhow::Result<LoggingConsumer> 
     // Set log level and create consumer
     let consumer = client
         .set_log_level(RDKafkaLogLevel::Info)
-        .create_with_context(CustomContext)
+        .create_with_context(CustomContext::default())
         .context("Consumer creation failed")?;
     Ok(consumer)
 }
