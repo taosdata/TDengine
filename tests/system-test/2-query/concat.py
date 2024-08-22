f84da0f2ec8c63a50def5ed66f1166bab857f9f1
Leo Xu <381899826@qq.com>
2024-08-22 22:04:34 +0800

    add assert

--- tests/system-test/2-query/concat.py
+++ tests/system-test/2-query/concat.py
@@ -146,7 +146,7 @@ class TDTestCase:
         ]
         tdSql.query("select concat(null,null)")  # TD-31571
         tdSql.checkRows(1)
-        tdSql.getData(0, 0) == "NULL"
+        assert tdSql.getData(0, 0) == None
         for tb in tbname:
             for i in range(2,8):
                 self.__concat_check(tb,i)
