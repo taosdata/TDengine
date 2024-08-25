4af3ab50825590cab90521577f954359d22339a4
sheyanjie-qq <249478495@qq.com>
2024-08-25 10:45:34 +0800

    update java code example

--- docs/examples/JDBC/SpringJdbcTemplate/src/main/java/com/taosdata/example/jdbcTemplate/App.java
+++ docs/examples/JDBC/SpringJdbcTemplate/src/main/java/com/taosdata/example/jdbcTemplate/App.java
@@ -6,8 +6,6 @@ import com.taosdata.example.jdbcTemplate.dao.WeatherDao;
 import com.taosdata.example.jdbcTemplate.domain.Weather;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import java.sql.Timestamp;
 import java.util.Date;
@@ -20,41 +18,30 @@ public class App {
 
     public static void main(String[] args) {
 
-//        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
-//
-//        ExecuteAsStatement executor = ctx.getBean(ExecuteAsStatement.class);
-//        // drop database
-//        executor.doExecute("drop database if exists test");
-//        // create database
-//        executor.doExecute("create database if not exists test");
-//        //use database
-//        executor.doExecute("use test");
-//        // create table
-//        executor.doExecute("create table if not exists test.weather (ts timestamp, temperature float, humidity int)");
-//
-//        WeatherDao weatherDao = ctx.getBean(WeatherDao.class);
-//        Weather weather = new Weather(new Timestamp(new Date().getTime()), random.nextFloat() * 50.0f, random.nextInt(100));
-//        // insert rows
-//        int affectedRows = weatherDao.add(weather);
-//        System.out.println("insert success " + affectedRows + " rows.");
-//
-//        // query for list
-//        int limit = 10, offset = 0;
-//        List<Weather> weatherList = weatherDao.queryForList(limit, offset);
-//        for (Weather w : weatherList) {
-//            System.out.println(w);
-//        }
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.taosdata.jdbc.rs.RestfulDriver");
-        dataSource.setUrl("jdbc:TAOS-RS://vm98:6041/?batchfetch=true&user=root&password=taosdata");
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
 
+        ExecuteAsStatement executor = ctx.getBean(ExecuteAsStatement.class);
+        // drop database
+        executor.doExecute("drop database if exists test");
+        // create database
+        executor.doExecute("create database if not exists test");
+        //use database
+        executor.doExecute("use test");
+        // create table
+        executor.doExecute("create table if not exists test.weather (ts timestamp, temperature float, humidity int)");
 
+        WeatherDao weatherDao = ctx.getBean(WeatherDao.class);
+        Weather weather = new Weather(new Timestamp(new Date().getTime()), random.nextFloat() * 50.0f, random.nextInt(100));
+        // insert rows
+        int affectedRows = weatherDao.add(weather);
+        System.out.println("insert success " + affectedRows + " rows.");
 
-        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-
-        jdbcTemplate.query("select * from test.meters limit ?, ?",
-                        (rs, rowNum) -> rs.getString("ts"), 1, 10)
-                .forEach(System.out::println) ;
+        // query for list
+        int limit = 10, offset = 0;
+        List<Weather> weatherList = weatherDao.queryForList(limit, offset);
+        for (Weather w : weatherList) {
+            System.out.println(w);
+        }
 
     }
 
--- docs/examples/JDBC/mybatisplus-demo/src/main/java/com/taosdata/example/mybatisplusdemo/config/MybatisPlusConfig.java
+++ docs/examples/JDBC/mybatisplus-demo/src/main/java/com/taosdata/example/mybatisplusdemo/config/MybatisPlusConfig.java
@@ -1,7 +1,6 @@
 package com.taosdata.example.mybatisplusdemo.config;
 
-import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -23,10 +22,13 @@ public class MybatisPlusConfig {
 //    }
 
     @Bean
-    public MybatisPlusInterceptor mybatisPlusInterceptor() {
-        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
-        return interceptor;
+    public PaginationInterceptor paginationInterceptor() {
+//        return new PaginationInterceptor();
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        //TODO: mybatis-plus do not support TDengine, use postgresql Dialect
+        paginationInterceptor.setDialectType("postgresql");
+
+        return paginationInterceptor;
     }
 
 }
