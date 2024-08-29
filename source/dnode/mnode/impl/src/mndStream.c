b2af572bc2d1fe098c07777cc0d141e795ef4878
dmchen <cademfly@hotmail.com>
2024-08-29 04:03:33 +0000

    fix/TD-31686-remove-avoid-stream

--- source/dnode/mnode/impl/src/mndStream.c
+++ source/dnode/mnode/impl/src/mndStream.c
@@ -2167,8 +2167,7 @@ static int32_t refreshNodeListFromExistedStreams(SMnode *pMnode, SArray *pNodeLi
 
       SNodeEntry entry = {.hbTimestamp = -1, .nodeId = pTask->info.nodeId, .lastHbMsgId = -1};
       epsetAssign(&entry.epset, &pTask->info.epSet);
-      (void)taosHashPut(pHash, &entry.nodeId, sizeof(entry.nodeId), &entry, sizeof(entry));
-      /*
+      //(void)taosHashPut(pHash, &entry.nodeId, sizeof(entry.nodeId), &entry, sizeof(entry));
       if (taosHashPut(pHash, &entry.nodeId, sizeof(entry.nodeId), &entry, sizeof(entry)) == 0) {
         code = terrno;
         taosWUnLockLatch(&pStream->lock);
@@ -2177,7 +2176,6 @@ static int32_t refreshNodeListFromExistedStreams(SMnode *pMnode, SArray *pNodeLi
         taosArrayClear(pNodeList);
         return code;
       }
-      */
     }
 
     destroyStreamTaskIter(pTaskIter);
