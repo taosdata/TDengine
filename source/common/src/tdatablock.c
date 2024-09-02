4e6781d7531ba5ac9648a2cb9e3f66aff0c2ba04
dapan1121 <wpan@taosdata.com>
2024-09-02 16:50:30 +0800

    fix: add encode debug info

--- source/common/src/tdatablock.c
+++ source/common/src/tdatablock.c
@@ -3009,6 +3009,13 @@ int32_t blockEncode(const SSDataBlock* pBlock, char* data, int32_t numOfCols) {
       data += colSizes[col];
     }
 
+    if (colSizes[col] <= 0) {
+      uError("Invalid colSize:%d while encoding block", colSizes[col]);
+      ASSERT(0);
+      terrno = TSDB_CODE_QRY_EXECUTOR_INTERNAL_ERROR;
+      return -1;
+    }
+    
     colSizes[col] = htonl(colSizes[col]);
     //    uError("blockEncode col bytes:%d, type:%d, size:%d, htonl size:%d", pColRes->info.bytes, pColRes->info.type,
     //    htonl(colSizes[col]), colSizes[col]);
