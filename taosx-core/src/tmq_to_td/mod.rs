39f386ed623e3f30a9252a630bc246662951e3d7
Linhe Huo <linhehuo@gmail.com>
2024-09-03 08:55:36 +0800

    chore: do not set jobs to 1 when input 0
    Deal with jobs = 0 after we got vgroups number.

--- taosx-core/src/tmq_to_td/mod.rs
+++ taosx-core/src/tmq_to_td/mod.rs
@@ -1314,7 +1314,7 @@ pub async fn tmq_to_td(
         .remove("read_concurrency")
         .or(from.remove("num.of.consumers"))
         .and_then(|s| s.parse().ok())
-        .unwrap_or_else(|| if jobs > 0 { jobs } else { 1 });
+        .unwrap_or(0); // 0 means auto
     let strategy = from
         .remove("prefer")
         .map(|s| s.into())
@@ -1324,7 +1324,7 @@ pub async fn tmq_to_td(
         .or(from.remove("num.of.writers"))
         .or(std::env::var("TMQ_WRITE_CONCURRENCY").ok())
         .and_then(|s| s.parse().ok())
-        .unwrap_or(0);
+        .unwrap_or(0); // 0 means auto, should be set after.
     let commit_chunk_size = from
         .remove("commit.chunk.size")
         .or(std::env::var("TMQ_COMMIT_CHUNK_SIZE").ok())
@@ -1466,11 +1466,14 @@ pub async fn tmq_to_td(
             &topic.database
         };
 
+        // Jobs should be less than or equal to vgroups and greater than 0.
         let jobs = if jobs == 0 || jobs >= topic.vgroups {
             topic.vgroups
         } else {
             jobs
         };
+
+        // If concurrency is 0, use available_parallelism * 2 / jobs
         if options.concurrency == 0 {
             options.concurrency = std::thread::available_parallelism()
                 .map_or_else(|_| 8, |n| n.get() * 2 / jobs)
