484fc895b37697d54c2c84dc29e92bca1a0c02a3
Linhe Huo <linhehuo@gmail.com>
2024-09-06 17:48:01 +0800

    fix(kafka): kafka should no lost data when target unreachable
    Close
    - [TD-31888](https://jira.taosdata.com:18080/browse/TD-31888)
    - [TD-31887](https://jira.taosdata.com:18080/browse/TD-31887)

--- src/serve/rpc/put.rs
+++ src/serve/rpc/put.rs
@@ -215,6 +215,7 @@ async fn ipc_stream_writer(
                             metrics,
                             metrics_arc,
                             tables_messages_in_progress,
+                            None,
                         )
                         .in_current_span()
                         .await
@@ -733,7 +734,7 @@ impl PutStream {
                 async move {
                     match message.payload {
                         arrow_flight::decode::DecodedPayload::RecordBatch(batch) => {
-                            // tracing::trace!("Enqueue batch {}\nschema=\n{:?} \ncolumns=\n{:?}", trace_id, batch.schema(), batch.columns());
+                            tracing::trace!("Enqueue batch {}\nschema=\n{:?} \ncolumns=\n{:?}", trace_id, batch.schema(), batch.columns());
                             if let Err(err) = tx.send_async((batch, trace_id)).await {
                                 tracing::warn!(
                                     trace_id = %trace_id,
