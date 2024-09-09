43c100b0261c7e6ce880dfaf1024bbab6fc907b5
Leo Xu <381899826@qq.com>
2024-09-09 18:11:05 +0800

    add flexify and S3azure case

--- tests/army/storage/s3/s3azure.py
+++ tests/army/storage/s3/s3azure.py
@@ -20,6 +20,7 @@ import frame
 import frame.etool
 import frame.eos
 import frame.eutil
+import requests
 
 from frame.log import *
 from frame.cases import *
@@ -28,10 +29,10 @@ from frame.caseBase import *
 from frame.srvCtl import *
 from frame import *
 from frame.eos import *
+from azure import Azure
 
-
-#  
-# 192.168.1.52 MINIO S3 
+#
+# 192.168.1.52 MINIO S3
 #
 
 '''
@@ -47,12 +48,23 @@ for test:
 
 
 class TDTestCase(TBase):
-    index = eutil.cpuRand(20) + 1
-    bucketName = f"ci-bucket{index}"
+    def __init__(self):
+        self.fileName = ""  # track the upload of S3 file
+
+        account_name = 'fd2d01cd892f844eeaa2273'
+        url = "http://192.168.0.21/azure_account_key.txt"
+        response = requests.get(url)
+        account_key = response.text.strip()
+        container_name = 'td-test'
+
+        self.azure_class = Azure(account_name, account_key, container_name)
+
+    # index = eutil.cpuRand(20) + 1
+    bucketName = "td-test"
     updatecfgDict = {
-        "supportVnodes":"1000",
-        's3EndPoint': 'http://192.168.1.52:9000', 
-        's3AccessKey': 'zOgllR6bSnw2Ah3mCNel:cdO7oXAu3Cqdb1rUdevFgJMi0LtRwCXdWKQx4bhX', 
+        "supportVnodes": "1000",
+        's3EndPoint': 'http://192.168.1.49',
+        's3AccessKey': 'FlIOwdr5HAnsMyEZ6FBwSPE5:87x1tZJll1SaK4hoiglC8zPRhDgbMeW6ufQqEt8',
         's3BucketName': f'{bucketName}',
         's3PageCacheSize': '10240',
         "s3UploadDelaySec": "10",
@@ -61,7 +73,7 @@ class TDTestCase(TBase):
     }
 
     tdLog.info(f"assign bucketName is {bucketName}\n")
-    maxFileSize = (128 + 10) * 1014 * 1024 # add 10M buffer
+    maxFileSize = (128 + 10) * 1014 * 1024  # add 10M buffer
 
     def insertData(self):
         tdLog.info(f"insert data.")
@@ -91,13 +103,14 @@ class TDTestCase(TBase):
             cols = line.split()
             fileSize = int(cols[4])
             fileName = cols[8]
-            #print(f" filesize={fileSize} fileName={fileName}  line={line}")
+            # print(f" filesize={fileSize} fileName={fileName}  line={line}")
             if fileSize > maxFileSize:
                 tdLog.info(f"error, {fileSize} over max size({maxFileSize}) {fileName}\n")
                 overCnt += 1
+                self.fileName = fileName
             else:
                 tdLog.info(f"{fileName}({fileSize}) check size passed.")
-                
+
         return overCnt
 
     def checkUploadToS3(self):
@@ -131,23 +144,45 @@ class TDTestCase(TBase):
             loop += 1
             # migrate
             self.migrateDbS3()
-                
+
         # check can pass
         if overCnt > 0:
             tdLog.exit(f"s3 have {overCnt} files over size.")
-
 
     def doAction(self):
         tdLog.info(f"do action.")
 
         self.flushDb(show=True)
-        #self.compactDb(show=True)
+        # self.compactDb(show=True)
 
         # sleep 70s
         self.migrateDbS3()
 
         # check upload to s3
         self.checkUploadToS3()
+
+    def check_azure_exist(self):
+        result = self.azure_class.blob_list()
+        if not self.fileName:
+            tdLog.exit("cannot find S3 file")
+        datafile = self.fileName.split("/")[-1].split(".")[0]
+        for blob in result["Blobs"]["Blob"]:
+            if datafile in blob["Name"]:
+                tdLog.info("Successfully found the file %s in Azure" % self.fileName)
+                break
+        else:
+            tdLog.exit("failed found the file %s in Azure" % self.fileName)
+
+    def check_azure_not_exist(self):
+        datafile = self.fileName.split("/")[-1].split(".")[0]
+
+        result = self.azure_class.blob_list()
+        for blob in result["Blobs"]["Blob"]:
+            if datafile in blob["Name"]:
+                tdLog.exit("failed delete the file %s in Azure" % self.fileName)
+                break
+        else:
+            tdLog.info("Successfully delete the file %s in Azure" % self.fileName)
 
     def checkStreamCorrect(self):
         sql = f"select count(*) from {self.db}.stm1"
@@ -158,23 +193,22 @@ class TDTestCase(TBase):
             if count == 100000 or count == 100001:
                 return True
             time.sleep(1)
-            
-        tdLog.exit(f"stream count is not expect . expect = 100000 or 100001 real={count} . sql={sql}")
 
+        tdLog.exit(f"stream count is not expect . expect = 100000 or 100001 real={count} . sql={sql}")
 
     def checkCreateDb(self, keepLocal, chunkSize, compact):
         # keyword
-        kw1 = kw2 = kw3 = "" 
+        kw1 = kw2 = kw3 = ""
         if keepLocal is not None:
             kw1 = f"s3_keeplocal {keepLocal}"
         if chunkSize is not None:
             kw2 = f"s3_chunksize {chunkSize}"
         if compact is not None:
-            kw3 = f"s3_compact {compact}"    
+            kw3 = f"s3_compact {compact}"
 
         sql = f" create database db1 vgroups 1 duration 1h {kw1} {kw2} {kw3}"
         tdSql.execute(sql, show=True)
-        #sql = f"select name,s3_keeplocal,s3_chunksize,s3_compact from information_schema.ins_databases where name='db1';"
+        # sql = f"select name,s3_keeplocal,s3_chunksize,s3_compact from information_schema.ins_databases where name='db1';"
         sql = f"select * from information_schema.ins_databases where name='db1';"
         tdSql.query(sql)
         # 29 30 31 -> chunksize keeplocal compact
@@ -183,7 +217,7 @@ class TDTestCase(TBase):
         if keepLocal is not None:
             keepLocalm = keepLocal * 24 * 60
             tdSql.checkData(0, 30, f"{keepLocalm}m")
-        if compact is not None:    
+        if compact is not None:
             tdSql.checkData(0, 31, compact)
         sql = "drop database db1"
         tdSql.execute(sql)
@@ -203,28 +237,26 @@ class TDTestCase(TBase):
         ]
         tdSql.errors(sqls)
 
-
     def checkBasic(self):
         # create db
-        keeps  = [1, 256, 1024, 365000, None]
+        keeps = [1, 256, 1024, 365000, None]
         chunks = [131072, 600000, 820000, 1048576, None]
-        comps  = [0, 1, None]
+        comps = [0, 1, None]
 
         for keep in keeps:
             for chunk in chunks:
                 for comp in comps:
                     self.checkCreateDb(keep, chunk, comp)
 
-        
         # --checks3
         idx = 1
         taosd = sc.taosdFile(idx)
-        cfg   = sc.dnodeCfgPath(idx)
+        cfg = sc.dnodeCfgPath(idx)
         cmd = f"{taosd} -c {cfg} --checks3"
 
         eos.exe(cmd)
-        #output, error = eos.run(cmd)
-        #print(lines)
+        # output, error = eos.run(cmd)
+        # print(lines)
 
         '''
         tips = [
@@ -239,19 +271,19 @@ class TDTestCase(TBase):
             #if pos == -1:
             #    tdLog.exit(f"checks3 failed not found {tip}. cmd={cmd} output={output}")
         '''
-        
+
         # except
         self.checkExcept()
 
     #
     def preDb(self, vgroups):
-        cnt = int(time.time())%2 + 1
+        cnt = int(time.time()) % 2 + 1
         for i in range(cnt):
-             vg = eutil.cpuRand(9) + 1
-             sql = f"create database predb vgroups {vg}"
-             tdSql.execute(sql, show=True)
-             sql = "drop database predb"
-             tdSql.execute(sql, show=True)
+            vg = eutil.cpuRand(9) + 1
+            sql = f"create database predb vgroups {vg}"
+            tdSql.execute(sql, show=True)
+            sql = "drop database predb"
+            tdSql.execute(sql, show=True)
 
     # history
     def insertHistory(self):
@@ -261,7 +293,7 @@ class TDTestCase(TBase):
         etool.benchMark(json=json)
 
         # come from s3_basic.json
-        self.insert_rows += self.insert_rows/4
+        self.insert_rows += self.insert_rows / 4
         self.timestamp_step = 50
 
     # delete
@@ -270,15 +302,15 @@ class TDTestCase(TBase):
         start = 1600000000000
         drows = 200
         for i in range(1, drows, 2):
-            sql = f"from {self.db}.{self.stb} where ts = {start + i*500}"
+            sql = f"from {self.db}.{self.stb} where ts = {start + i * 500}"
             tdSql.execute("delete " + sql, show=True)
             tdSql.query("select * " + sql)
             tdSql.checkRows(0)
-        
+
         # delete all 500 step
         self.flushDb()
         self.compactDb()
-        self.insert_rows   -= drows/2
+        self.insert_rows -= drows / 2
         sql = f"select count(*) from {self.db}.{self.stb}"
         tdSql.checkAgg(sql, self.insert_rows * self.childtable_count)
 
@@ -291,10 +323,9 @@ class TDTestCase(TBase):
         tdSql.query("select * " + sql)
         tdSql.checkRows(0)
 
-        self.insert_rows   -= drows
+        self.insert_rows -= drows
         sql = f"select count(*) from {self.db}.{self.stb}"
         tdSql.checkAgg(sql, self.insert_rows * self.childtable_count)
-        
 
     # run
     def run(self):
@@ -303,7 +334,7 @@ class TDTestCase(TBase):
         if eos.isArm64Cpu():
             tdLog.success(f"{__file__} arm64 ignore executed")
         else:
-            
+
             self.preDb(10)
 
             # insert data
@@ -313,48 +344,48 @@ class TDTestCase(TBase):
             self.createStream(self.sname)
 
             # check insert data correct
-            #self.checkInsertCorrect()
+            # self.checkInsertCorrect()
 
             # save
             self.snapshotAgg()
 
             # do action
             self.doAction()
-
+            # check azure data exist
+            self.check_azure_exist()
             # check save agg result correct
             self.checkAggCorrect()
 
             # check insert correct again
             self.checkInsertCorrect()
 
-
             # check stream correct and drop stream
-            #self.checkStreamCorrect()
+            # self.checkStreamCorrect()
 
             # drop stream
             self.dropStream(self.sname)
 
-            # insert history  disorder data
-            self.insertHistory()
+            # # insert history  disorder data
+            # self.insertHistory()
+            #
+            # # checkBasic
+            # self.checkBasic()
 
-            # checkBasic
-            self.checkBasic()
-
-            #self.checkInsertCorrect()
-            self.snapshotAgg()
-            self.doAction()
-            self.checkAggCorrect()
-            self.checkInsertCorrect(difCnt=self.childtable_count*1499999)
-            self.checkDelete()
-            self.doAction()
+            # self.checkInsertCorrect()
+            # self.snapshotAgg()
+            # self.doAction()
+            # self.checkAggCorrect()
+            # self.checkInsertCorrect(difCnt=self.childtable_count * 1499999)
+            # self.checkDelete()
+            # self.doAction()
 
             # drop database and free s3 file
             self.dropDb()
-
+            # check azure data not exist
+            self.check_azure_not_exist()
 
             tdLog.success(f"{__file__} successfully executed")
 
-        
 
 tdCases.addLinux(__file__, TDTestCase())
 tdCases.addWindows(__file__, TDTestCase())
