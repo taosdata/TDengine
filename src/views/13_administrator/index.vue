b7112aed60675e57bc85f34439a531d69474b07d
guxiang02 <guxiang_0211@163.com>
2024-09-11 14:45:47 +0800

    feat(license): csv&data_sync授权适配版本3.3.2.12

--- src/views/13_administrator/index.vue
+++ src/views/13_administrator/index.vue
@@ -9,8 +9,8 @@
         <el-tab-pane name="backup" :label="$t('taosuser.backup')" :disabled='taosxDisabled' lazy v-if="getMetaShow('backup_restore')">
           <AdBackup ></AdBackup>
         </el-tab-pane>
-        <el-tab-pane name="replication" :label="$t('taosuser.datareplication')" :disabled='taosxDisabled' lazy v-if="isLessThen3_3_3_0 ? getMetaShow('td3.0') : getMetaShow('data_sync')">
-          <AdReplication :isLessThen3_3_3_0="isLessThen3_3_3_0"></AdReplication>
+        <el-tab-pane name="replication" :label="$t('taosuser.datareplication')" :disabled='taosxDisabled' lazy v-if="isLessThan3_3_2_12 ? getMetaShow('td3.0') : getMetaShow('data_sync')">
+          <AdReplication ></AdReplication>
         </el-tab-pane>
         <el-tab-pane name="cluster" :label="$t('route.cluster')" lazy v-if="getMetaShow('dnodes')">
           <Cluster></Cluster>
@@ -66,8 +66,8 @@ export default {
     TDengineVersion() {
       return localStorage.getItem("agent_version");
     },
-    isLessThen3_3_3_0() {
-      return compareVersion(this.TDengineVersion, '<3.3.3.0')
+    isLessThan3_3_2_12() {
+      return compareVersion(this.TDengineVersion, '<3.3.2.12')
     }
   },
   created() {
