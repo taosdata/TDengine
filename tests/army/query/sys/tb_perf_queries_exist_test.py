ffcab144370a22354a1aa2077350c805cbed5190
wangmeng <shuizhilong2008@163.com>
2024-09-11 09:46:01 +0800

    fix[TS-5349]:fix the problem that the table exist in the information schema.

--- tests/army/query/sys/tb_perf_queries_exist_test.py
+++ tests/army/query/sys/tb_perf_queries_exist_test.py
@@ -1,26 +1,21 @@
-# from asyncio.windows_events import NULL
-from util.log import *
-from util.sql import *
-from util.cases import *
+# -*- coding: utf-8 -*-
 
-class TDTestCase:
+from frame.log import *
+from frame.cases import *
+from frame.sql import *
+from frame.caseBase import *
+from frame import *
+from frame.autogen import *
 
-    def init(self, conn, logSql, replicaVar=1):
-        tdLog.debug(f"start to excute {__file__}")
-
-
+class TDTestCase(TBase):
 
     def run(self):
         tdSql.query("select count(*) from performance_schema.perf_queries limit 1;")
-        tdSql.checkData(0, 0, "None")
-        tdSql.info("-----111-----performance_schema.perf_queries ---- success")
+        tdSql.checkData(0, 0, 0)
+        tdLog.info("Table [perf_queries] exist in schema [performance_schema]")
 
-
-        tdSql.query("select count(*) from  information_schema.perf_queries limit 1;")
-        tdSql.checkData(0, 0, "None")
-        tdSql.info("-----222-----information_schema.perf_queries ---- success")
-
-
-    def stop(self):
-        #tdSql.close()
+        tdSql.error("select count(*) from  information_schema.perf_queries limit 1;")
         tdLog.success(f"{__file__} successfully executed")
+
+tdCases.addLinux(__file__, TDTestCase())
+tdCases.addWindows(__file__, TDTestCase())
