fce82c96a6afe564350573ff8b371a1f1a4011ff
wangmeng <shuizhilong2008@163.com>
2024-09-11 11:05:25 +0800

    fix the problem that the table exist in the information schema.

--- tests/army/query/sys/tb_perf_queries_exist_test.py
+++ tests/army/query/sys/tb_perf_queries_exist_test.py
@@ -7,14 +7,19 @@ from frame.caseBase import *
 from frame import *
 from frame.autogen import *
 
+'''
+    TS-5349: https://jira.taosdata.com:18080/browse/TS-5349
+    查询 performance_schema.perf_queries 后, 再查询 information_schema.perf_queries,
+    正常情况下在 information_schema 中不存在表 perf_queries
+'''
+
 class TDTestCase(TBase):
 
     def run(self):
-        tdSql.query("select count(*) from performance_schema.perf_queries limit 1;")
-        tdSql.checkData(0, 0, 0)
+        tdSql.query("select * from performance_schema.perf_queries;")
         tdLog.info("Table [perf_queries] exist in schema [performance_schema]")
 
-        tdSql.error("select count(*) from  information_schema.perf_queries limit 1;")
+        tdSql.error("select * from  information_schema.perf_queries;")
         tdLog.success(f"{__file__} successfully executed")
 
 tdCases.addLinux(__file__, TDTestCase())
