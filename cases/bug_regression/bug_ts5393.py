8273aa6ca1e54e7ea7f562a25664d64566bf47ff
jiajingbin <jiajaybin@126.com>
2024-09-14 19:06:34 +0800

    test: add case for TS-5393

--- cases/bug_regression/bug_ts5393.py
+++ cases/bug_regression/bug_ts5393.py
@@ -23,6 +23,10 @@ from datetime import datetime
 from copy import deepcopy
 import pandas as pd
 import copy
+import random
+import threading
+import concurrent.futures
+import multiprocessing
 
 class BugTs5393(TDCase):
     def init(self):
@@ -39,28 +43,34 @@ class BugTs5393(TDCase):
         self.replica = int(os.environ["DATABASE_REPLICAS"]) if "DATABASE_REPLICAS" in os.environ else 1
         self.vgroups = 3
         self.create_table_thread_count = 40
-        self.thread_count = 200
+        self.thread_count = 80
         # self.thread_count = 10
-        self.num_of_records_per_req = 100
+        self.num_of_records_per_req = 10000
         # self.num_of_records_per_req = 100
-        self.childtable_count = 2000000
-        self.insert_rows = 1000
+        self.childtable_count = 20000
+        self.insert_rows = 10
         self.start_timestamp = "2020-01-01 00:00:00"
-        self.stbname = "stb"
-        self.dbname1 = "test1"
-        self.dbname2 = "test2"
-        self.dbname3 = "test3"
-        self.dbname4 = "test4"
-        self.dbname5 = "test5"
-        self.dbname6 = "test6"
-        self.dbname_list = [self.dbname1, self.dbname2, self.dbname3, self.dbname4, self.dbname5, self.dbname6]
+        self.dbname = "test"
+        self.stbname1 = "stb1"
+        self.stbname2 = "stb2"
+        self.stbname3 = "stb3"
+        self.stbname4 = "stb4"
+        self.stbname5 = "stb5"
+        self.stbname6 = "stb6"
+        self.childtable_prefix1 = "ctb1_"
+        self.childtable_prefix2 = "ctb2_"
+        self.childtable_prefix3 = "ctb3_"
+        self.childtable_prefix4 = "ctb4_"
+        self.childtable_prefix5 = "ctb5_"
+        self.childtable_prefix6 = "ctb6_"
+        self.stbname_list = [self.stbname1, self.stbname2, self.stbname3, self.stbname4, self.stbname5, self.stbname6]
         self.child_table_exists = "no"
         self.auto_create_table = "yes"
-        self.db_drop = "yes"
+        self.db_drop = "no"
         self.wal_retention_period = 1800
         self.keep_trying = -1
         self.trying_interval = 10000
-        self.interlace_rows = 1
+        self.interlace_rows = 0
 
         self.primary_key = 0
 
@@ -84,16 +94,18 @@ class BugTs5393(TDCase):
               "fillNull": "false"
             },
             {
-              "type": "INT",
+              "type": "BIGINT",
               "count": 2
             }
         ]
         self.tag_info_list = [
             {
-              "type": "INT",
+              "type": "BIGINT",
               "count": 1
             }
         ]
+        self.tag_field = "t0"
+        self.col_field = "ts,c0,c1,c2"
 
     def desc(self):
         pass
@@ -107,9 +119,9 @@ class BugTs5393(TDCase):
     def cleanup(self):
         pass
 
-    # def get_dnode_id_list(self):
-    #     self.tdSql.query('show dnodes')
-    #     self.dnode_id_list = list(map(lambda x:x[0], self.tdSql.query_data))
+    def get_dnode_id_list(self):
+        self.tdSql.query('show dnodes')
+        self.dnode_id_list = list(map(lambda x:x[0], self.tdSql.query_data))
 
     def check_restored_true(self):
         self.tdCom.check_restored_true(self._remote)
@@ -117,19 +129,19 @@ class BugTs5393(TDCase):
 
     def insert_data(self):
         self.json_filename_list = [self.json_file_name1, self.json_file_name2, self.json_file_name3, self.json_file_name4, self.json_file_name5, self.json_file_name6]
-        dbinfo1 = self.tdCom.setDBinfo(name=self.dbname1, replica=self.replica, vgroups=self.vgroups, drop=self.db_drop, wal_retention_period=self.wal_retention_period)
-        dbinfo2 = self.tdCom.setDBinfo(name=self.dbname2, replica=self.replica, vgroups=self.vgroups, drop=self.db_drop, wal_retention_period=self.wal_retention_period)
-        dbinfo3 = self.tdCom.setDBinfo(name=self.dbname3, replica=self.replica, vgroups=self.vgroups, drop=self.db_drop, wal_retention_period=self.wal_retention_period)
-        dbinfo4 = self.tdCom.setDBinfo(name=self.dbname4, replica=self.replica, vgroups=self.vgroups, drop=self.db_drop, wal_retention_period=self.wal_retention_period)
-        dbinfo5 = self.tdCom.setDBinfo(name=self.dbname5, replica=self.replica, vgroups=self.vgroups, drop=self.db_drop, wal_retention_period=self.wal_retention_period)
-        dbinfo6 = self.tdCom.setDBinfo(name=self.dbname6, replica=self.replica, vgroups=self.vgroups, drop=self.db_drop, wal_retention_period=self.wal_retention_period)
-        stb_into = [self.tdCom.setStbinfo(columns=self.column_info_list, tags=self.tag_info_list, childtable_count=self.childtable_count, insert_rows=self.insert_rows, start_timestamp=self.start_timestamp, child_table_exists=self.child_table_exists, name=self.stbname, keep_trying=self.keep_trying, trying_interval=self.trying_interval, interlace_rows=self.interlace_rows, primary_key=self.primary_key, auto_create_table=self.auto_create_table)]
-        database_info1 = [self.tdCom.setDatabases(dbinfo=dbinfo1, super_tables=stb_into)]
-        database_info2 = [self.tdCom.setDatabases(dbinfo=dbinfo2, super_tables=stb_into)]
-        database_info3 = [self.tdCom.setDatabases(dbinfo=dbinfo3, super_tables=stb_into)]
-        database_info4 = [self.tdCom.setDatabases(dbinfo=dbinfo4, super_tables=stb_into)]
-        database_info5 = [self.tdCom.setDatabases(dbinfo=dbinfo5, super_tables=stb_into)]
-        database_info6 = [self.tdCom.setDatabases(dbinfo=dbinfo6, super_tables=stb_into)]
+        dbinfo1 = self.tdCom.setDBinfo(name=self.dbname, replica=self.replica, vgroups=self.vgroups, drop=self.db_drop, wal_retention_period=self.wal_retention_period)
+        stb_info1 = [self.tdCom.setStbinfo(columns=self.column_info_list, tags=self.tag_info_list, childtable_count=self.childtable_count, insert_rows=self.insert_rows, start_timestamp=self.start_timestamp, child_table_exists=self.child_table_exists, name=self.stbname1, keep_trying=self.keep_trying, trying_interval=self.trying_interval, interlace_rows=self.interlace_rows, primary_key=self.primary_key, auto_create_table=self.auto_create_table, childtable_prefix=self.childtable_prefix1)]
+        stb_info2 = [self.tdCom.setStbinfo(columns=self.column_info_list, tags=self.tag_info_list, childtable_count=self.childtable_count, insert_rows=self.insert_rows, start_timestamp=self.start_timestamp, child_table_exists=self.child_table_exists, name=self.stbname2, keep_trying=self.keep_trying, trying_interval=self.trying_interval, interlace_rows=self.interlace_rows, primary_key=self.primary_key, auto_create_table=self.auto_create_table, childtable_prefix=self.childtable_prefix2)]
+        stb_info3 = [self.tdCom.setStbinfo(columns=self.column_info_list, tags=self.tag_info_list, childtable_count=self.childtable_count, insert_rows=self.insert_rows, start_timestamp=self.start_timestamp, child_table_exists=self.child_table_exists, name=self.stbname3, keep_trying=self.keep_trying, trying_interval=self.trying_interval, interlace_rows=self.interlace_rows, primary_key=self.primary_key, auto_create_table=self.auto_create_table, childtable_prefix=self.childtable_prefix3)]
+        stb_info4 = [self.tdCom.setStbinfo(columns=self.column_info_list, tags=self.tag_info_list, childtable_count=self.childtable_count, insert_rows=self.insert_rows, start_timestamp=self.start_timestamp, child_table_exists=self.child_table_exists, name=self.stbname4, keep_trying=self.keep_trying, trying_interval=self.trying_interval, interlace_rows=self.interlace_rows, primary_key=self.primary_key, auto_create_table=self.auto_create_table, childtable_prefix=self.childtable_prefix4)]
+        stb_info5 = [self.tdCom.setStbinfo(columns=self.column_info_list, tags=self.tag_info_list, childtable_count=self.childtable_count, insert_rows=self.insert_rows, start_timestamp=self.start_timestamp, child_table_exists=self.child_table_exists, name=self.stbname5, keep_trying=self.keep_trying, trying_interval=self.trying_interval, interlace_rows=self.interlace_rows, primary_key=self.primary_key, auto_create_table=self.auto_create_table, childtable_prefix=self.childtable_prefix5)]
+        stb_info6 = [self.tdCom.setStbinfo(columns=self.column_info_list, tags=self.tag_info_list, childtable_count=self.childtable_count, insert_rows=self.insert_rows, start_timestamp=self.start_timestamp, child_table_exists=self.child_table_exists, name=self.stbname6, keep_trying=self.keep_trying, trying_interval=self.trying_interval, interlace_rows=self.interlace_rows, primary_key=self.primary_key, auto_create_table=self.auto_create_table, childtable_prefix=self.childtable_prefix6)]
+        database_info1 = [self.tdCom.setDatabases(dbinfo=dbinfo1, super_tables=stb_info1)]
+        database_info2 = [self.tdCom.setDatabases(dbinfo=dbinfo1, super_tables=stb_info2)]
+        database_info3 = [self.tdCom.setDatabases(dbinfo=dbinfo1, super_tables=stb_info3)]
+        database_info4 = [self.tdCom.setDatabases(dbinfo=dbinfo1, super_tables=stb_info4)]
+        database_info5 = [self.tdCom.setDatabases(dbinfo=dbinfo1, super_tables=stb_info5)]
+        database_info6 = [self.tdCom.setDatabases(dbinfo=dbinfo1, super_tables=stb_info6)]
         host = self.get_fqdn("taosd")[0]
         json_info1 = self.tdCom.setJsoninfo(host=host, databases=database_info1, create_table_thread_count=self.create_table_thread_count, thread_count=self.thread_count, num_of_records_per_req=self.pre_num_of_records_per_req)
         json_info2 = self.tdCom.setJsoninfo(host=host, databases=database_info2, create_table_thread_count=self.create_table_thread_count, thread_count=self.thread_count, num_of_records_per_req=self.pre_num_of_records_per_req)
@@ -146,19 +158,20 @@ class BugTs5393(TDCase):
         self.json_data_list = [json_info1, json_info2, json_info3, json_info4, json_info5, json_info6]
         self.tdCom.put_file(self._remote, self.taosBenchmark_iplist, self.json_data_list, self.json_filename_list, self.run_log_dir)
         self.tdCom.threads_run_taosBenchmark(self._remote, self.taosBenchmark_iplist, self.json_data_list, self.json_filename_list, self.taosBenchmark_env_setting, self.run_log_dir)
-
+        self.tdSql.execute(f'flush database {self.dbname}')
 
     def reinsert(self):
         self.json_filename_list = [self.json_file_name1]
-        dbinfo1 = self.tdCom.setDBinfo(name=self.dbname1, replica=self.replica, vgroups=self.vgroups, drop=self.db_drop, wal_retention_period=self.wal_retention_period)
-        stb_into = [self.tdCom.setStbinfo(columns=self.column_info_list, tags=self.tag_info_list, childtable_count=self.childtable_count, insert_rows=self.insert_rows, start_timestamp=self.start_timestamp, child_table_exists=self.child_table_exists, name=self.stbname, keep_trying=self.keep_trying, trying_interval=self.trying_interval, interlace_rows=self.interlace_rows, primary_key=self.primary_key, auto_create_table=self.auto_create_table)]
-        database_info1 = [self.tdCom.setDatabases(dbinfo=dbinfo1, super_tables=stb_into)]
+        dbinfo = self.tdCom.setDBinfo(name=self.dbname, replica=self.replica, vgroups=self.vgroups, drop=self.db_drop, wal_retention_period=self.wal_retention_period)
+        stb_into = [self.tdCom.setStbinfo(columns=self.column_info_list, tags=self.tag_info_list, childtable_count=self.childtable_count, insert_rows=self.insert_rows, start_timestamp=self.start_timestamp, child_table_exists=self.child_table_exists, name=self.stbname1, keep_trying=self.keep_trying, trying_interval=self.trying_interval, interlace_rows=self.interlace_rows, primary_key=self.primary_key, auto_create_table=self.auto_create_table)]
+        database_info1 = [self.tdCom.setDatabases(dbinfo=dbinfo, super_tables=stb_into)]
         host = self.get_fqdn("taosd")[0]
         json_info1 = self.tdCom.setJsoninfo(host=host, databases=database_info1, create_table_thread_count=self.create_table_thread_count, thread_count=self.thread_count, num_of_records_per_req=self.pre_num_of_records_per_req)
         self.tdCom.genBenchmarkJson(self.run_log_dir, self.json_file_name1, json_info1)
         self.json_data_list = [json_info1]
-        self.tdCom.put_file(self._remote, self.taosBenchmark_iplist, self.json_data_list, self.json_filename_list, self.run_log_dir)
-        self.tdCom.threads_run_taosBenchmark(self._remote, self.taosBenchmark_iplist, self.json_data_list, self.json_filename_list, self.taosBenchmark_env_setting, self.run_log_dir)
+        self.tdCom.put_file(self._remote, [self.taosBenchmark_iplist[0]], self.json_data_list, self.json_filename_list, self.run_log_dir)
+        self.tdCom.threads_run_taosBenchmark(self._remote, [self.taosBenchmark_iplist[0]], self.json_data_list, self.json_filename_list, self.taosBenchmark_env_setting, self.run_log_dir)
+        self.tdSql.execute(f'flush database {self.dbname}')
 
     def show_transactions(self):
         self.tdSql.query('show transactions')
@@ -167,23 +180,84 @@ class BugTs5393(TDCase):
     def restart_dnodes(self):
         # dnodes_out_mnodes = self.tdSql.get_dnodes_out_mnodes()[0]
         # print("----dnodes_out_mnodes: ", dnodes_out_mnodes)
-        restart_endpoint_list = self.tdCom.get_fqdn_by_dnode_id(self.dnode_id_list)
+        self.get_dnode_id_list()
+        restart_endpoint_list = sorted(self.tdCom.get_fqdn_by_dnode_id(self.dnode_id_list))
+        print(restart_endpoint_list)
         # print("-----restart_endpoint_list: ", restart_endpoint_list)
-        for endpoint in restart_endpoint_list:
-            taosd_setting = copy.deepcopy(self.taosd_setting)
-            self.taosd.update_cfg('/tmp',taosd_setting , {"supportVnodes": self.cfg["boundary"][-1]}, endpoint, True)
+        taosd_setting = copy.deepcopy(self.taosd_setting)
+        self.taosd.update_cfg('/tmp',taosd_setting , {"supportVnodes": self.cfg["boundary"][-1]}, restart_endpoint_list[1], True)
         self.check_restored_true()
 
     def deleteDnodeDataDir(self):
         dnode = self.base_dnode_list[1]
-        self._remote.cmd(dnode["endpoint"], [f'rm -rf {dnode["config"]["dataDir"]}'])
+        dnode_fqdn = dnode["endpoint"].split(":")[0]
+        self._remote.cmd(dnode_fqdn, [f'rm -rf {dnode["config"]["dataDir"]}'])
+
+    def auto_create_table_insert(self, args):
+        # ctbname, stbname, tag_field, tag_values, col_field, col_values
+        ctbname, stbname, tag_value = args
+        ts = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+        c0, c1, c2 = random.randint(0, 100), random.randint(0, 100), random.randint(0, 100)
+        self.tdSql.execute(f'insert into {self.dbname}.{ctbname} using {self.dbname}.{stbname} ({self.tag_field}) tags ({tag_value}) ({self.col_field}) values ("{ts}", {c0}, {c1}, {c2})')
+
+    def thread_insert(self, ctb_prefix, stbname):
+        self._remote._logger.info(f"thread_insert with {self.thread_count} threads")
+        with concurrent.futures.ThreadPoolExecutor(max_workers=self.thread_count) as executor:
+            tasks = [(f"{ctb_prefix}{i}", stbname, i) for i in range(self.childtable_count)]
+            executor.map(self.auto_create_table_insert, tasks)
 
     def run(self):
+        
+        # for stbname in self.stbname_list:
+        #     self.tdCom.create_stable(dbname=self.dbname, stbname=stbname, column_elm_list=self.column_info_list, tag_elm_list=self.tag_info_list, default_column_index_start_num=0, default_tag_index_start_num=0)
+        # self.thread_insert(self.childtable_prefix1, self.stbname1)
+        # self.thread_insert(self.childtable_prefix2, self.stbname2)
+        # self.thread_insert(self.childtable_prefix3, self.stbname3)
+        # self.thread_insert(self.childtable_prefix4, self.stbname4)
+        # self.thread_insert(self.childtable_prefix5, self.stbname5)
+        # self.thread_insert(self.childtable_prefix6, self.stbname6)
+        self.tdCom.createDb(dbname=self.dbname, replica=self.replica, vgroups=self.vgroups, wal_retention_period=self.wal_retention_period)
         self.insert_data()
+        
         self.deleteDnodeDataDir()
-        self.tdSql.execute(f'drop table {self.dbname1}.{self.stbname}')
+        # self.restart_dnodes()
+        self.tdSql.execute(f'use {self.dbname}')
+        self.tdSql.execute(f'drop table {self.dbname}.{self.stbname1}')
+        self.tdCom.create_stable(dbname=self.dbname, stbname=self.stbname1, column_elm_list=self.column_info_list, tag_elm_list=self.tag_info_list, default_column_index_start_num=0, default_tag_index_start_num=0)
         self.reinsert()
-        for i in self.dbname_list:
-            self.tdSql.execute(f'compact database {i}')
+        # self.thread_insert(self.childtable_prefix1, self.stbname1)
+        self.tdSql.execute(f'compact database {self.dbname}')
         self.tdSql.execute(f'restore dnode 2')
 
+
+
+
+        """
+        
+        
+        
+        process_count = len(self.stbname_list)
+        processes = []
+        for i in range(process_count):
+            process = multiprocessing.Process(target=self.thread_insert, args=(self.childtable_prefix1, self.stbname1))
+            processes.append(process)
+            process.start()
+            process = multiprocessing.Process(target=self.thread_insert, args=(self.childtable_prefix2, self.stbname2))
+            processes.append(process)
+            process.start()
+            process = multiprocessing.Process(target=self.thread_insert, args=(self.childtable_prefix3, self.stbname3))
+            processes.append(process)
+            process.start()
+            process = multiprocessing.Process(target=self.thread_insert, args=(self.childtable_prefix4, self.stbname4))
+            processes.append(process)
+            process.start()
+            process = multiprocessing.Process(target=self.thread_insert, args=(self.childtable_prefix5, self.stbname5))
+            processes.append(process)
+            process.start()
+            process = multiprocessing.Process(target=self.thread_insert, args=(self.childtable_prefix6, self.stbname6))
+            processes.append(process)
+            process.start()
+
+        for process in processes:
+            process.join()
+        """
