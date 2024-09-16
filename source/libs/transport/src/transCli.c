b447c620e5d8b0175b0af7130e7a1d36c64537bb
yihaoDeng <luomoxyz@126.com>
2024-09-16 18:35:45 +0800

    Merge branch '3.0' into enh/opt-transport

--- source/libs/transport/src/transCli.c
+++ source/libs/transport/src/transCli.c
@@ -3192,8 +3192,12 @@ static int32_t getOrCreateHeap(SHashObj* pConnHeapCache, char* key, SHeap** pHea
 }
 
 static FORCE_INLINE int8_t shouldSWitchToOtherConn(int32_t reqNum, int32_t sentNum, int32_t stateNum) {
-  int32_t total = reqNum + sentNum + stateNum;
+  int32_t total = reqNum + sentNum;
   if (total >= BUFFER_LIMIT) {
+    return 1;
+  }
+
+  if (stateNum >= BUFFER_LIMIT * 2) {
     return 1;
   }
 
