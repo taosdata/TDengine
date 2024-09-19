1bee113c8efe4b4d0a65be8d0a9767a5ef8825a4
dmchen <cademfly@hotmail.com>
2024-09-19 02:26:14 +0000

    fix/TD-31891-remove-void-mnode2-fix-case

--- source/dnode/mnode/impl/src/mndAcct.c
+++ source/dnode/mnode/impl/src/mndAcct.c
@@ -81,7 +81,7 @@ static int32_t mndCreateDefaultAcct(SMnode *pMnode) {
     code = terrno;
     TAOS_RETURN(code);
   }
-  TAOS_RETURN(sdbSetRawStatus(pRaw, SDB_STATUS_READY));
+  TAOS_CHECK_RETURN(sdbSetRawStatus(pRaw, SDB_STATUS_READY));
 
   mInfo("acct:%s, will be created when deploying, raw:%p", acctObj.acct, pRaw);
 
