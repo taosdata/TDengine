396945f57c074fc46a9439b17ef7fc1e5a47caa4
menshibin <men_shi_bin@163.com>
2024-09-25 21:07:10 +0800

    modify node ci example

--- tests/docs-examples-test/node.sh
+++ tests/docs-examples-test/node.sh
@@ -2,6 +2,33 @@
 
 set -e
 
+check_transactions() {
+    for i in {1..30}
+    do
+        output=$(taos -s "show transactions;")
+        if [[ $output == *"Query OK, 0 row(s)"* ]]; then
+            echo "Success: No transactions are in progress."
+            return 0
+        fi
+        sleep 1
+    done
+
+    echo "Error: Transactions are still in progress after 30 attempts."
+    return 1
+}
+
+reset_cache() {
+  response=$(curl --location -uroot:taosdata 'http://127.0.0.1:6041/rest/sql' --data 'reset query cache')
+
+  if [[ $response == \{\"code\":0* ]]; then
+    echo "Success: Query cache reset successfully."
+  else
+    echo "Error: Failed to reset query cache. Response: $response"
+    return 1
+  fi
+}
+
+
 pgrep taosd || taosd >> /dev/null 2>&1 &
 pgrep taosadapter || taosadapter >> /dev/null 2>&1 &
 
@@ -11,21 +38,44 @@ cd ../../docs/examples/node
 
 npm install
 
+taos -s "drop database if exists power"
+check_transactions || exit 1
+reset_cache || exit 1
 cd websocketexample
 
+taos -s "drop database if exists power"
+check_transactions || exit 1
+reset_cache || exit 1
 node all_type_query.js
 
+taos -s "drop database if exists power"
+check_transactions || exit 1
+reset_cache || exit 1
 node all_type_stmt.js
 
 taos -s "drop database if exists power"
+check_transactions || exit 1
+reset_cache || exit 1
 node line_example.js
 
+taos -s "drop database if exists power"
+check_transactions || exit 1
+reset_cache || exit 1
 node nodejsChecker.js
 
+taos -s "drop database if exists power"
+check_transactions || exit 1
+reset_cache || exit 1
 node sql_example.js
 
+taos -s "drop database if exists power"
+check_transactions || exit 1
+reset_cache || exit 1
 node stmt_example.js
 
+taos -s "drop database if exists power"
+check_transactions || exit 1
+reset_cache || exit 1
 node tmq_example.js
 
 node tmq_seek_example.js
