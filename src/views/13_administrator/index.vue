0564774290ea642e2615d8038c1a1a4dc97673c0
guxiang02 <guxiang_0211@163.com>
2024-09-25 16:15:12 +0800

    feat(slowsql): td version 3.3.3.0 开始支持

--- src/views/13_administrator/index.vue
+++ src/views/13_administrator/index.vue
@@ -21,7 +21,7 @@
         <el-tab-pane name="audit" :label="$t('topic.audit')" lazy v-if="getMetaShow('audit')">
           <Audit :activeName="activeName"></Audit>
         </el-tab-pane>
-        <el-tab-pane name="slowSql" :label="$t('topic.slowSql')" lazy v-if="version_gt_equ_3330">
+        <el-tab-pane name="slowSql" :label="$t('topic.slowSql')" lazy v-if="!isLessThan3_3_3_0">
           <SlowSql :activeName="activeName"></SlowSql>
         </el-tab-pane>
       </el-tabs>
@@ -59,7 +59,6 @@ export default {
       message: "这是Admin页面",
       taosxDisabled:false,
       activeName: 'user',
-      version_gt_equ_3330: false
     };
   },
   computed: {
@@ -68,15 +67,12 @@ export default {
     },
     isLessThan3_3_2_12() {
       return compareVersion(this.TDengineVersion, '<3.3.2.12')
+    },
+    isLessThan3_3_3_0() {
+      return compareVersion(this.TDengineVersion, '<3.3.3.0')
     }
   },
-  created() {
-    let version = localStorage.getItem("agent_version");
-    let [a, b, c, d] = version.split(".");
-    if (a > 3 || (a == 3 && b >= 1 && c >= 2)) {
-      this.version_gt_equ_3330 = true;
-    }
-  }
+  created() {}
 };
 </script>
 <style scoped lang="scss">
