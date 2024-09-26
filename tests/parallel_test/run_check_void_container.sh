c6e2935c4774a983c80cff6b75e2494fa0d2eb27
chenhaoran <haoran920c@163.com>
2024-09-26 17:50:20 +0800

    test: add checks for void function invocations to the CI pipeline

--- tests/parallel_test/run_check_void_container.sh
+++ tests/parallel_test/run_check_void_container.sh
@@ -36,7 +36,7 @@ REP_MOUNT_PARAM="$INTERNAL_REPDIR:/home/TDinternal"
 
 CONTAINER_TESTDIR=/home/TDinternal/community
 
-check_assert_scripts="$CONTAINER_TESTDIR/tests/ci/check_void.sh  " 
+check_assert_scripts="$CONTAINER_TESTDIR/tests/ci/check_void.sh" 
 func_list_file="$CONTAINER_TESTDIR/tests/ci/func.txt"
 source_file_path="$CONTAINER_TESTDIR/source/"
 
@@ -49,7 +49,7 @@ EOF
 
 docker run \
     -v "$REP_MOUNT_PARAM" \
-    --rm --ulimit core=-1 taos_test:v1.0  "$check_assert_scripts" -c $func_list_file -f $source_file_path
+    --rm --ulimit core=-1 taos_test:v1.0  $check_assert_scripts -c $func_list_file -f $source_file_path
 
 ret=$?
 exit $ret
