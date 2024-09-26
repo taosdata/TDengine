05006939055a6d2280184462ccef679e6ab2eeb9
wangmm0220 <wangmm0220@gmail.com>
2024-09-26 18:26:55 +0800

    fix:[TS-5466] tag rewrite error

--- tests/system-test/7-tmq/tmq_ts5466.py
+++ tests/system-test/7-tmq/tmq_ts5466.py
@@ -25,7 +25,7 @@ class TDTestCase:
     def run(self):
         tdSql.execute(f'create database if not exists db_taosx')
         tdSql.execute(f'create database if not exists db_5466')
-        tdSql.execute(f'use db_5466;')
+        tdSql.execute(f'use db_5466')
         tdSql.execute(f'create stable if not exists s5466 (ts timestamp, c1 int, c2 int) tags (t int)')
         tdSql.execute(f'insert into t1 using s5466 tags(1) values(1669092069068, 0, 1)')
         for i in range(80):
@@ -35,7 +35,7 @@ class TDTestCase:
         tdSql.execute(f'insert into t1(ts, c1, c2) values(1669092069067, 0, 1)')
         tdSql.execute(f'flush database db_5466')
 
-        tdSql.execute("create topic db_5466_topic as database db_5466")
+        tdSql.execute("create topic db_5466_topic with meta as database db_5466")
         buildPath = tdCom.getBuildPath()
         cmdStr = '%s/build/bin/tmq_ts5466'%(buildPath)
         tdLog.info(cmdStr)
