7cdb54033baf73d3971bd91d11697029824a6838
Hongze Cheng <hzcheng@taosdata.com>
2024-09-29 11:38:43 +0800

    enh: make CI happy

--- source/dnode/vnode/src/tsdb/tsdbFSet2.c
+++ source/dnode/vnode/src/tsdb/tsdbFSet2.c
@@ -71,7 +71,9 @@ static int32_t tsdbSttLvlInitRef(STsdb *pTsdb, const SSttLvl *lvl1, SSttLvl **lv
     }
     code = TARRAY2_APPEND(lvl[0]->fobjArr, fobj1);
     if (code) {
-      tsdbTFileObjUnref(fobj1);
+      if (tsdbTFileObjUnref(fobj1) != 0) {
+        tsdbError("failed to unref file obj, fobj:%p", fobj1);
+      }
       tsdbSttLvlClear(lvl);
       return code;
     }
