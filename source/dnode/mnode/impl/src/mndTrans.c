639799be2723d3a33810fa3068e0cc9875237369
dmchen <cademfly@hotmail.com>
2024-09-29 03:25:24 +0000

    fix/TD-32316-fix-compile

--- source/dnode/mnode/impl/src/mndTrans.c
+++ source/dnode/mnode/impl/src/mndTrans.c
@@ -12,8 +12,6 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
-#include "osSleep.h"
 #define _DEFAULT_SOURCE
 #include "mndTrans.h"
 #include "mndDb.h"
