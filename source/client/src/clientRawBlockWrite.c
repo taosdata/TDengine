676f8759e23395d0ed5bbd6dabc2a18c6c08d197
lyh250-666 <lyh666@bupt.edu.cn>
2024-10-09 15:37:00 +0800

    enh:modify error code passing

--- source/client/src/clientRawBlockWrite.c
+++ source/client/src/clientRawBlockWrite.c
@@ -1918,7 +1918,7 @@ static int32_t tmqWriteRawMetaDataImpl(TAOS* taos, void* data, int32_t dataLen) 
     const char* tbName = (const char*)taosArrayGetP(rspObj.dataRsp.blockTbName, rspObj.resIter);
     if (!tbName) {
       SET_ERROR_MSG("block tbname is null");
-      code = TSDB_CODE_TMQ_INVALID_MSG;
+      code = terrno;
       goto end;
     }
 
