ea9ac5f3b42f0768ba4113331952160b7e511f17
minhuinie <nminhui@163.com>
2024-10-14 19:01:23 +0800

    修改mqtt不稳定用例，修改mongodb分库分表用例

--- tests/e2e/test_function/mongodb_test.py
+++ tests/e2e/test_function/mongodb_test.py
@@ -71,29 +71,69 @@ def test_case_base(input_data):
 
 
 @pytest.mark.sanity
-def test_case_base_1(input_data):
+@pytest.mark.parametrize(
+    "database, collection, start, end, expected",
+    [
+        (
+            "test_db1_${Y}",
+            "tb_${M}",
+            "2020-01-01T00:00:00+08:00",
+            "2024-07-31T00:00:00+08:00",
+            360,
+        ),
+        (
+            "test_db2_${Y}",
+            "tb_${M}_${D}",
+            "2020-01-01T00:00:00+08:00",
+            "2024-07-31T00:00:00+08:00",
+            1793,
+        ),
+        (
+            "test_db4_${Y}",
+            "tb_${m}_${d}",
+            "2021-12-01T00:00:00+08:00",
+            "2023-05-01T00:00:00+08:00",
+            810,
+        ),
+        (
+            "test_db5_${y}",
+            "tb_${j}",
+            "2020-01-01T00:00:00+08:00",
+            "2020-03-01T00:00:00+08:00",
+            24,
+        ),
+        (
+            "test_db5_${y}",
+            "tb_${J}",
+            "2020-01-02T00:00:00+08:00",
+            "2020-03-01T00:00:00+08:00",
+            21,
+        ),
+    ],
+)
+def test_case_base_0(database, collection, start, end, expected, input_data):
     """
-    用例概述: mongodb 用例, 测试分库分表，${Y}, ${M}
+    用例概述: mongodb 用例, 测试分库分表，${y}, ${j}
     数据源信息: 192.168.1.45:27017, 用户名密码:admin/tbase125!
     用例步骤：
     1. 使用 agent
-    2. 配置数据库名: test_db1_${Y}, 表名: tb_${M}
+    2. 配置数据库名,表名,数据起始时间
     3. 查询模版: {"createtime":{"$gte":${start_datetime},"$lt":${end_datetime}}}
-    4. 数据起始时间: '2020-01-01 00:00:00+08:00' - '2024-07-31 00:00:00+08:00'
+    4. 数据起始时间: '2020-01-01 00:00:00+08:00' - '2020-03-01 00:00:00+08:00'
 
     验证点：
-    1. 360条数据正常写入
+    1. 数据正常写入
     """
     mongodb_test_logger.info("start test...sanity case: basic")
     env_data, case_data_orig = input_data
     case_data = copy.deepcopy(case_data_orig)
-    case_data["from"]["database"] = "test_db1_${Y}"
-    case_data["from"]["collection"] = "tb_${M}"
+    case_data["from"]["database"] = database
+    case_data["from"]["collection"] = collection
     case_data["from"][
         "sql"
     ] = '{"createtime":{"$gte":${start_datetime},"$lt":${end_datetime}}}'
-    case_data["from"]["start"] = "2020-01-01T00:00:00+08:00"
-    case_data["from"]["end"] = "2024-07-31T00:00:00+08:00"
+    case_data["from"]["start"] = start
+    case_data["from"]["end"] = end
     mongodb_payload = Util.read_yaml("mongodb/payload-basic.yaml")["parser"]
     case_data["parser"] = mongodb_payload
 
@@ -111,52 +151,7 @@ def test_case_base_1(input_data):
     rows_count = TaosAdapter.check_db_count(
         env_data["taosadapter_host"], case_data["to"]["target_dbname"]
     )
-    assert rows_count == 360, "入库的数据量应等于 360"
-    TaosAdapter.drop_db(ENV.taosd_host, case_data["to"]["target_dbname"])
-
-
-@pytest.mark.sanity
-def test_case_base_2(input_data):
-    """
-    用例概述: mongodb 用例, 测试分库分表，${Y}, ${M}, ${D}
-    数据源信息: 192.168.1.45:27017, 用户名密码:admin/tbase125!
-    用例步骤：
-    1. 使用 agent
-    2. 配置数据库名: test_db2_${Y}, 表名: tb_${M}_${D}
-    3. 查询模版: {"createtime":{"$gte":${start_datetime},"$lt":${end_datetime}}}
-    4. 数据起始时间: '2020-01-01 00:00:00+08:00' - '2024-07-31 00:00:00+08:00'
-
-    验证点：
-    1. 1793条数据正常写入
-    """
-    mongodb_test_logger.info("start test...sanity case: basic")
-    env_data, case_data_orig = input_data
-    case_data = copy.deepcopy(case_data_orig)
-    case_data["from"]["database"] = "test_db2_${Y}"
-    case_data["from"]["collection"] = "tb_${M}_${D}"
-    case_data["from"][
-        "sql"
-    ] = '{"createtime":{"$gte":${start_datetime},"$lt":${end_datetime}}}'
-    case_data["from"]["start"] = "2020-01-01T00:00:00+08:00"
-    case_data["from"]["end"] = "2024-07-31T00:00:00+08:00"
-    mongodb_payload = Util.read_yaml("mongodb/payload-basic.yaml")["parser"]
-    case_data["parser"] = mongodb_payload
-
-    Util.create_stable(case_data, mongodb_payload["parser"])
-    payload = Util.get_task_payload(case_data, env_data, EnvType.LOCAL)
-    task = Task(env_data, case_data)
-    task_info = task.create_task(payload)
-    # 等待任务结束
-    while True:
-        task_status = task.get_task_status(task_info["id"])
-        if task_status["status"] == "completed":
-            break
-        else:
-            time.sleep(5)
-    rows_count = TaosAdapter.check_db_count(
-        env_data["taosadapter_host"], case_data["to"]["target_dbname"]
-    )
-    assert rows_count == 1793, "入库的数据量应等于 1793"
+    assert rows_count == expected, f"入库的数据量应等于 {expected}"
     TaosAdapter.drop_db(ENV.taosd_host, case_data["to"]["target_dbname"])
 
 
