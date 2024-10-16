5e14f65fb9071239b0c40fba5a731fcb54c70ebb
Minglei Jin <mljin@taosdata.com>
2024-10-16 19:01:39 +0800

    tcs/test: new blob test for checkcfg

--- source/libs/tcs/test/tcsTest.cpp
+++ source/libs/tcs/test/tcsTest.cpp
@@ -6,8 +6,10 @@
 #include "tcs.h"
 #include "tcsInt.h"
 
-int32_t tcsInitEnv() {
+int32_t tcsInitEnv(int8_t isBlob) {
   int32_t code = 0;
+
+  extern int8_t tsS3EpNum;
 
   extern char tsS3Hostname[][TSDB_FQDN_LEN];
   extern char tsS3AccessKeyId[][TSDB_FQDN_LEN];
@@ -21,20 +23,38 @@ int32_t tcsInitEnv() {
   tsS3BucketName = "<bucket/container-name>";
   */
 
-  const char *hostname = "endpoint/<account-name>.blob.core.windows.net";
-  const char *accessKeyId = "<access-key-id/account-name>";
-  const char *accessKeySecret = "<access-key-secret/account-key>";
-  const char *bucketName = "<bucket/container-name>";
+  tsS3Ablob = isBlob;
+  if (isBlob) {
+    const char *hostname = "endpoint/<account-name>.blob.core.windows.net";
+    const char *accessKeyId = "<access-key-id/account-name>";
+    const char *accessKeySecret = "<access-key-secret/account-key>";
+    const char *bucketName = "<bucket/container-name>";
 
-  tstrncpy(&tsS3Hostname[0][0], hostname, TSDB_FQDN_LEN);
-  tstrncpy(&tsS3AccessKeyId[0][0], accessKeyId, TSDB_FQDN_LEN);
-  tstrncpy(&tsS3AccessKeySecret[0][0], accessKeySecret, TSDB_FQDN_LEN);
-  tstrncpy(tsS3BucketName, bucketName, TSDB_FQDN_LEN);
+    tstrncpy(&tsS3Hostname[0][0], hostname, TSDB_FQDN_LEN);
+    tstrncpy(&tsS3AccessKeyId[0][0], accessKeyId, TSDB_FQDN_LEN);
+    tstrncpy(&tsS3AccessKeySecret[0][0], accessKeySecret, TSDB_FQDN_LEN);
+    tstrncpy(tsS3BucketName, bucketName, TSDB_FQDN_LEN);
+
+  } else {
+    const char *hostname = "endpoint/<account-name>.blob.core.windows.net";
+    const char *accessKeyId = "<access-key-id/account-name>";
+    const char *accessKeySecret = "<access-key-secret/account-key>";
+    const char *bucketName = "<bucket/container-name>";
+
+    tstrncpy(&tsS3Hostname[0][0], hostname, TSDB_FQDN_LEN);
+    tstrncpy(&tsS3AccessKeyId[0][0], accessKeyId, TSDB_FQDN_LEN);
+    tstrncpy(&tsS3AccessKeySecret[0][0], accessKeySecret, TSDB_FQDN_LEN);
+    tstrncpy(tsS3BucketName, bucketName, TSDB_FQDN_LEN);
+
+    // setup s3 env
+    tsS3EpNum = 1;
+  }
 
   tstrncpy(tsTempDir, "/tmp/", PATH_MAX);
 
   tsS3Enabled = true;
-  tsS3Ablob = true;
+  if (!tsS3Ablob) {
+  }
 
   return code;
 }
@@ -43,10 +63,48 @@ int32_t tcsInitEnv() {
 TEST(TcsTest, InterfaceTest) {
   int code = 0;
 
-  code = tcsInitEnv();
+  code = tcsInitEnv(true);
   GTEST_ASSERT_EQ(code, 0);
   GTEST_ASSERT_EQ(tsS3Enabled, 1);
   GTEST_ASSERT_EQ(tsS3Ablob, 1);
+
+  code = tcsInit();
+  GTEST_ASSERT_EQ(code, 0);
+
+  code = tcsCheckCfg();
+  GTEST_ASSERT_EQ(code, 0);
+  /*
+  code = tcsPutObjectFromFileOffset(file, object_name, offset, size);
+  GTEST_ASSERT_EQ(code, 0);
+  code = tcsGetObjectBlock(object_name, offset, size, check, ppBlock);
+  GTEST_ASSERT_EQ(code, 0);
+
+  tcsDeleteObjectsByPrefix(prefix);
+  // list object to check
+
+  code = tcsPutObjectFromFile2(file, object, withcp);
+  GTEST_ASSERT_EQ(code, 0);
+  code = tcsGetObjectsByPrefix(prefix, path);
+  GTEST_ASSERT_EQ(code, 0);
+  code = tcsDeleteObjects(object_name, nobject);
+  GTEST_ASSERT_EQ(code, 0);
+  code = tcsGetObjectToFile(object_name, fileName);
+  GTEST_ASSERT_EQ(code, 0);
+
+  // GTEST_ASSERT_NE(pEnv, nullptr);
+  */
+
+  tcsUninit();
+}
+
+// TEST(TcsTest, DISABLE_InterfaceNonBlobTest) {
+TEST(TcsTest, InterfaceNonBlobTest) {
+  int code = 0;
+
+  code = tcsInitEnv(false);
+  GTEST_ASSERT_EQ(code, 0);
+  GTEST_ASSERT_EQ(tsS3Enabled, 1);
+  GTEST_ASSERT_EQ(tsS3Ablob, 0);
 
   code = tcsInit();
   GTEST_ASSERT_EQ(code, 0);
