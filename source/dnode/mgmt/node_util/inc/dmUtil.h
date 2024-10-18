295e4462d5da3ea526a52f4016da0df1f1b7c9df
dmchen <cademfly@hotmail.com>
2024-10-18 07:23:25 +0000

    fix/TS-5532-set-seperate-thread-update-status-fix-case

--- source/dnode/mgmt/node_util/inc/dmUtil.h
+++ source/dnode/mgmt/node_util/inc/dmUtil.h
@@ -144,6 +144,7 @@ typedef struct {
   char           machineId[TSDB_MACHINE_ID_LEN + 1];
   EEncryptAlgor  encryptAlgorigthm;
   EEncryptScope  encryptScope;
+  TdThreadRwlock statusInfolock;
 } SDnodeData;
 
 typedef struct {
