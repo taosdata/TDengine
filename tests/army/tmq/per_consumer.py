e4547fd5a7f225c03c029e3a0be0f6f8b8153e23
chenhaoran <haoran920c@163.com>
2024-10-23 16:56:11 +0800

    tetst:add testecase for clear consumer  with session and poll timout

---
+++ tests/army/tmq/per_consumer.py
@@ -0,0 +1,144 @@
+import os
+import taos
+import time
+from datetime import datetime
+import subprocess
+from multiprocessing import Process
+import threading
+from taos.tmq import Consumer
+import click
+
+try:
+    conn = taos.connect()
+except Exception as e:
+    print(str(e))
+    
+@click.command()
+@click.option('-c', '--consumer-groups-num', "consumer_group_num", default=1, help='Number of consumer group.')
+@click.option('-s', '--session-timeout-ms', "session_timeout_ms",  default=60000, help='session timeout:ms')
+@click.option('-p', '--max-poll-interval-ms',"max_poll_interval_ms",  default=180000, help='max poll interval timeout:ms')
+
+def test_timeout_sub(consumer_group_num,session_timeout_ms,max_poll_interval_ms):
+    threads = []
+    print(consumer_group_num,session_timeout_ms,max_poll_interval_ms)
+    for id in range(consumer_group_num):
+        conf = set_conf(group_id=id,session_timeout_ms=session_timeout_ms,max_poll_interval_ms=max_poll_interval_ms)
+        print(conf)
+        threads.append(threading.Thread(target=taosc_consumer, args=(conf,)))
+    for tr in threads:
+        tr.start()
+    for tr in threads:
+        tr.join()
+
+def sub_consumer(consumer,group_id):
+    group_id = int(group_id)
+    if group_id < 100 :
+        try:
+            consumer.subscribe(["select_d1"])
+        except Exception as e:
+            print(f"subscribe error")  
+            exit(1)       
+
+    nrows = 0
+    while True:
+        start = datetime.now()
+        print(f"time:{start},consumer:{group_id}, start to consume")
+        message = consumer.poll(timeout=10.0)
+        
+        if message:
+            id = message.offset()
+            topic = message.topic()
+            database = message.database()
+            
+            for block in message:
+                addrows = block.nrows()
+                nrows += block.nrows()
+                ncols = block.ncols()
+                values = block.fetchall
+            end = datetime.now()
+            elapsed_time = end -start
+            print(f"time:{end},consumer:{group_id}, elapsed time:{elapsed_time},consumer_nrows:{nrows},consumer_addrows:{addrows}, consumer_ncols:{ncols},offset:{id}")
+        consumer.commit()
+        print(f"consumer:{group_id},consumer_nrows:{nrows}")
+            # consumer.unsubscribe()
+            # consumer.close()
+            # break
+        # if nrows >= 1000000:
+        #     break
+def sub_consumer_once(consumer,group_id):
+    group_id = int(group_id)
+    if group_id < 100 :
+        consumer.subscribe(["select_d1"])
+    nrows = 0
+    consumer_nrows = 0
+    while True:
+        start = datetime.now()
+        print(f"time:{start},consumer:{group_id}, start to consume")
+        #start = datetime.now()
+        #print(f"time:{start},consumer:{group_id}, start to consume")
+        print(f"consumer_nrows:{consumer_nrows}")
+        if consumer_nrows < 1000000:
+            message = consumer.poll(timeout=10.0)
+        else:
+            print(" stop consumer when consumer all rows")
+
+        if message:
+            id = message.offset()
+            topic = message.topic()
+            database = message.database()
+            
+            for block in message:
+                addrows = block.nrows()
+                nrows += block.nrows()
+                ncols = block.ncols()
+                values = block.fetchall
+            end = datetime.now()
+            elapsed_time = end -start
+            # print(f"time:{end},consumer:{group_id}, elapsed time:{elapsed_time},consumer_nrows:{nrows},consumer_addrows:{addrows}, consumer_ncols:{ncols},offset:{id}")
+        consumer.commit()
+        # print(f"consumer:{group_id},consumer_nrows:{nrows}")
+        consumer_nrows = nrows
+            # consumer.unsubscribe()
+            # consumer.close()
+            # break
+
+def set_conf(td_connect_ip="localhost",group_id=1,client_id="test_consumer_py",enable_auto_commit="false",auto_commit_interval_ms="1000",auto_offset_reset="earliest",msg_with_table_name="true",session_timeout_ms=10000,max_poll_interval_ms=20000,experimental_snapshot_enable="false"):
+    conf = {
+        # auth options
+        # consume options
+        "td.connect.ip": f"{td_connect_ip}",
+        "group.id": f"{group_id}", 
+        "client.id": f"{client_id}",
+        "enable.auto.commit": f"{enable_auto_commit}",
+        "auto.commit.interval.ms": f"{auto_commit_interval_ms}",
+        "auto.offset.reset": f"{auto_offset_reset}",
+        "msg.with.table.name": f"{msg_with_table_name}",
+        "session.timeout.ms": f"{session_timeout_ms}",
+        "max.poll.interval.ms": f"{max_poll_interval_ms}",
+        "experimental.snapshot.enable" :f"{experimental_snapshot_enable}",
+    }
+    return conf
+
+def taosc_consumer(conf):
+    consumer = Consumer(conf)
+    group_id = int(conf["group.id"])
+    print(f"{consumer},{group_id}")
+    #counsmer sub:
+    # while True:
+    #     try:
+    #         self.sub_consumer_once(consumer,group_id)
+    #     except Exception as e:
+    #         print(str(e))
+    #         time.sleep(1)
+    #         break
+    # only consumer once
+    try:
+        sub_consumer_once(consumer,group_id)
+    except Exception as e:
+        print(str(e))
+            
+    #consumer.close()
+
+
+if __name__ == '__main__':
+    test_timeout_sub()
