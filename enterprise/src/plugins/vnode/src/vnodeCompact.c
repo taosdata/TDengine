34761cb8804879b46aa7028092cd4671367a9c52
dmchen <cademfly@hotmail.com>
2024-10-23 02:21:23 +0000

    fix/TD-32627-add-log

--- enterprise/src/plugins/vnode/src/vnodeCompact.c
+++ enterprise/src/plugins/vnode/src/vnodeCompact.c
@@ -34,11 +34,12 @@ int32_t vnodeAsyncCompact(SVnode *pVnode, int64_t version, void *pReq, int32_t l
 int32_t vnodeProcessKillCompactReq(SVnode *pVnode, int64_t ver, void *pReq, int32_t len, SRpcMsg *pRsp) {
   SVKillCompactReq req = {0};
 
+  vInfo("vgId:%d, kill compact msg will be processed, compactId:%d, pReq:%p, len:%d", TD_VID(pVnode), req.compactId,
+        pReq, len);
   int32_t code = tDeserializeSVKillCompactReq(pReq, len, &req);
   if (code) {
     return TSDB_CODE_INVALID_MSG;
   }
-  vInfo("vgId:%d, kill compact msg will be processed, compactId:%d", TD_VID(pVnode), req.compactId);
 
   tsdbStopAllCompTask(pVnode->pTsdb);
 
