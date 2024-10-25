0a8b3714f9496276f6af5e67b1935c8ba2ed7b40
xsren <285808407@qq.com>
2024-10-25 18:45:29 +0800

    fix: reserve

--- source/libs/planner/src/planPhysiCreater.c
+++ source/libs/planner/src/planPhysiCreater.c
@@ -233,7 +233,7 @@ static int32_t buildDataBlockSlots(SPhysiPlanContext* pCxt, SNodeList* pList, SD
     int32_t len = 0;
     code = getSlotKey(pNode, NULL, &name, &len, 16);
     if (TSDB_CODE_SUCCESS == code) {
-      code = nodesListStrictAppend(pDataBlockDesc->pSlots, createSlotDesc(pCxt, name, pNode, slotId, true, false));
+      code = nodesListStrictAppend(pDataBlockDesc->pSlots, createSlotDesc(pCxt, name, pNode, slotId, true, true));
     }
     code = putSlotToHash(name, len, pDataBlockDesc->dataBlockId, slotId, pNode, pHash);
     if (TSDB_CODE_SUCCESS == code) {
@@ -367,7 +367,7 @@ static int32_t addDataBlockSlotsForProject(SPhysiPlanContext* pCxt, const char* 
 }
 
 static int32_t pushdownDataBlockSlots(SPhysiPlanContext* pCxt, SNodeList* pList, SDataBlockDescNode* pDataBlockDesc) {
-  return addDataBlockSlotsImpl(pCxt, pList, pDataBlockDesc, NULL, true, true);
+  return addDataBlockSlotsImpl(pCxt, pList, pDataBlockDesc, NULL, true, false);
 }
 
 typedef struct SSetSlotIdCxt {
