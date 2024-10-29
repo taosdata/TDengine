6b67c2ebf0be26e47810ce2719f3320b6a61f937
Minglei Jin <mljin@taosdata.com>
2024-10-29 15:07:13 +0800

    uncomment tsdb cache commit

--- source/dnode/vnode/src/tsdb/tsdbCache.c
+++ source/dnode/vnode/src/tsdb/tsdbCache.c
@@ -493,7 +493,7 @@ int tsdbCacheFlushDirty(const void *key, size_t klen, void *value, void *ud) {
 int32_t tsdbCacheCommit(STsdb *pTsdb) {
   int32_t code = 0;
   char   *err = NULL;
-  /*
+
   SLRUCache            *pCache = pTsdb->lruCache;
   rocksdb_writebatch_t *wb = pTsdb->rCache.writebatch;
 
@@ -511,7 +511,7 @@ int32_t tsdbCacheCommit(STsdb *pTsdb) {
     rocksdb_free(err);
     code = TSDB_CODE_FAILED;
   }
-  */
+
   TAOS_RETURN(code);
 }
 
