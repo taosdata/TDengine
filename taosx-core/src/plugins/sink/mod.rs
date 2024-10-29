eb88b4c916fd6c2cc830d417a454ca47f4206af8
zyyang <zyyang@taosdata.com>
2024-10-29 10:09:29 +0800

    fix(opc): escape tbname when enabled is zero#td-32735

--- taosx-core/src/plugins/sink/mod.rs
+++ taosx-core/src/plugins/sink/mod.rs
@@ -3533,7 +3533,7 @@ pub async fn handle_point_message_init(config: &OpcModelConfig, taos: &Taos) -> 
                 ))?
                 .code
                 .clone();
-            let drop_sql = format!("DROP TABLE IF EXISTS {}", tbname);
+            let drop_sql = format!("DROP TABLE IF EXISTS `{}`", tbname);
             qid.add_sub_batch_id();
             tracing::info!("drop table sql: {drop_sql}");
             taos.exec_with_req_id(&drop_sql, qid.get())
