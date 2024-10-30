0513612b382f8125a9abede06203fefe256f7ec7
54liuyao <54liuyao@163.com>
2024-10-30 10:33:05 +0800

    add log

--- tests/system-test/7-tmq/tmqConsFromTsdb1.py
+++ tests/system-test/7-tmq/tmqConsFromTsdb1.py
@@ -16,6 +16,7 @@ sys.path.append("./7-tmq")
 from tmqCommon import *
 
 class TDTestCase:
+    updatecfgDict = {'debugFlag': 143}
     def __init__(self):
         self.vgroups    = 1
         self.ctbNum     = 10
