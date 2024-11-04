faa267da0415346dc101206c3d30ffd634b5ef65
tjuzyp <zhangyuanpai@gmail.com>
2024-11-04 14:31:47 +0800

    fix(packaging): remove stackoverflow link from the bat script

--- enterprise/packaging/windows/stop-all.bat
+++ enterprise/packaging/windows/stop-all.bat
@@ -1,5 +1,4 @@
-@REM start as administrator by default 
-@REM ref: https://stackoverflow.com/questions/6811372/how-to-code-a-bat-file-to-always-run-as-admin-mode
+@REM start as administrator by default
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/c cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 
