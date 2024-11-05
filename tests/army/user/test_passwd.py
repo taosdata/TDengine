7ff0fb497a7a924a1665c4dfd966b51457b81a64
Feng Chao <flyingangel_2008@163.com>
2024-11-05 15:46:36 +0800

    Update test_passwd.py to replace tdLog.error with tdLog.exit

--- tests/army/user/test_passwd.py
+++ tests/army/user/test_passwd.py
@@ -13,7 +13,7 @@ class TDTestCase(TBase):
         p = subprocess.Popen(c_file_path, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, err = p.communicate()
         if 0 != p.returncode:
-            tdLog.error("Failed to run passwd test with output: %s \n error: %s" % (out, err))
+            tdLog.exit("Failed to run passwd test with output: %s \n error: %s" % (out, err))
         tdLog.success(f"{__file__} successfully executed")
 
 tdCases.addLinux(__file__, TDTestCase())
