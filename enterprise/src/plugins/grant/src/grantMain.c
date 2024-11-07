6c65659a334b874b948f021e8e8c23a9d3e990a0
kailixu <klxu@taosdata.com>
2024-11-07 15:06:11 +0800

    enh: show grants logs

--- enterprise/src/plugins/grant/src/grantMain.c
+++ enterprise/src/plugins/grant/src/grantMain.c
@@ -2646,14 +2646,14 @@ static int32_t mndRetrieveGrantLogs(SRpcMsg *pReq, SShowObj *pShow, SSDataBlock 
     pColInfo = taosArrayGet(pBlock->pDataBlock, cols);
     if (pColInfo) {
       qBuf = POINTER_SHIFT(pBuf, VARSTR_HEADER_SIZE);
-      if (gStatus.grantState == GRANT_STATE_INIT || gStatus.grantState == GRANT_STATE_UNGRANTED) {
-        tmpLen = 0;
-      } else {
+      if (grantObj.granted) {
         tmpLen = tsnprintf(qBuf, bufLen,
                            "checkUpTime:%" PRIi8 ",checkMachineCode:%" PRIi8 ",checkHistoricalActive:%" PRIi8
                            ",skipOldActiveIfParseFail:%" PRIi8,
                            gStatus.checkUpTime ? 1 : 0, gStatus.checkMachineCode ? 1 : 0,
                            gStatus.checkHistoricalActive ? 1 : 0, gStatus.skipOldActiveIfParseFail ? 1 : 0);
+      } else {
+        tmpLen = 0;
       }
       qBuf += tmpLen;
       qBuf[0] = 0;
