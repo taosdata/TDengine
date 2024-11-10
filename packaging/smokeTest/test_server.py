2ef1e81e58b64f3e75f5e2b5629eda7f060947a9
chenhaoran <haoran920c@163.com>
2024-11-10 20:29:52 +0800

    test:modify testcase for 3.1 branch

--- packaging/smokeTest/test_server.py
+++ packaging/smokeTest/test_server.py
@@ -40,7 +40,7 @@ def get_config(request):
 def setup_module(get_config):
     def run_cmd(command):
         print("CMD:", command)
-        result = subprocess.run(command, capture_output=True, text=False, shell=False)
+        result = subprocess.run(command, capture_output=True, text=True, shell=True)
         print("STDOUT:", result.stdout)
         print("STDERR:", result.stderr)
         print("Return Code:", result.returncode)
