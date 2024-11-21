246a375cc278458d5a5b6762e976aa46f4f9d984
dmchen <cademfly@hotmail.com>
2024-11-21 18:28:37 +0800

    ehn/vnode-open-log-fix-review

--- source/libs/sync/src/syncReplication.c
+++ source/libs/sync/src/syncReplication.c
@@ -90,16 +90,16 @@ int32_t syncNodeSendAppendEntries(SSyncNode* pSyncNode, const SRaftId* destRaftI
 
   int32_t nRef = 0;
   if (pSyncNode != NULL) {
-    nRef = atomic_fetch_add_32(&pSyncNode->sendCount, 1);
-    if (nRef < 0) {
+    nRef = atomic_add_fetch_32(&pSyncNode->sendCount, 1);
+    if (nRef <= 0) {
       sError("vgId:%d, send count is %d", pSyncNode->vgId, nRef);
     }
   }
 
   SSyncLogReplMgr* mgr = syncNodeGetLogReplMgr(pSyncNode, (SRaftId*)destRaftId);
   if (mgr != NULL) {
-    nRef = atomic_fetch_add_32(&mgr->sendCount, 1);
-    if (nRef < 0) {
+    nRef = atomic_add_fetch_32(&mgr->sendCount, 1);
+    if (nRef <= 0) {
       sError("vgId:%d, send count is %d", pSyncNode->vgId, nRef);
     }
   }
