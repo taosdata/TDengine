601d2f1c088a04f3cb471cd5d19d920e824869f3
chenhaoran <haoran920c@163.com>
2024-11-25 20:04:30 +0800

    ci: set test step in ci when tdgpt file changed

---
+++ tests/army/tmq/a.py
@@ -0,0 +1,13 @@
+import click
+
+@click.command()
+@click.option('--count', default=1, help='Number of greetings.')
+@click.option('--name', prompt='Your name',
+              help='The person to greet.')
+def hello(count, name):
+    """Simple program that greets NAME for a total of COUNT times."""
+    for x in range(count):
+        click.echo('Hello %s!' % name)
+
+if __name__ == '__main__':
+    hello()
