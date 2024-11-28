8629a63c6b76122217ac9f22eb7ccbf9883c8a59
wangmm0220 <wangmm0220@gmail.com>
2024-11-29 01:34:20 +0800

    feat:[TD-32642] add timezone for connection support

--- source/common/src/ttime.c
+++ source/common/src/ttime.c
@@ -163,6 +163,8 @@ int32_t parseTimezone(char* str, int64_t* tzOffset) {
     PARSE(&str[i], hourSize, hour);
 
     i += hourSize + 1;
+    size_t minSize = strlen(&str[i]);
+    PARSE(&str[i], minSize, minute);
   } else {
     size_t hourSize = strlen(&str[i]);
     if (hourSize > 2){
@@ -170,14 +172,15 @@ int32_t parseTimezone(char* str, int64_t* tzOffset) {
     }
     PARSE(&str[i], hourSize, hour)
     i += hourSize;
+    size_t minSize = strlen(&str[i]);
+    if (minSize > 0){
+      PARSE(&str[i], minSize, minute);
+    }
   }
 
   if (hour > 13 || hour < 0) {
     TAOS_RETURN(TSDB_CODE_INVALID_PARA);
   }
-
-  size_t minSize = strlen(&str[i]);
-  PARSE(&str[i], minSize, minute);
   if (minute > 59 || minute < 0) {
     TAOS_RETURN(TSDB_CODE_INVALID_PARA);
   }
