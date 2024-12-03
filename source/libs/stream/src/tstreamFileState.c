2a8263efea092ffcddce91147e77259277c050da
54liuyao <54liuyao@163.com>
2024-12-03 09:11:46 +0800

    opt force window close memory

--- source/libs/stream/src/tstreamFileState.c
+++ source/libs/stream/src/tstreamFileState.c
@@ -1228,6 +1228,8 @@ void setFillInfo(SStreamFileState* pFileState) {
 }
 
 void clearExpiredState(SStreamFileState* pFileState) {
+  int32_t    code = TSDB_CODE_SUCCESS;
+  int32_t    lino = 0;
   SSHashObj* pSearchBuff = pFileState->searchBuff;
   void*      pIte = NULL;
   int32_t    iter = 0;
@@ -1245,6 +1247,13 @@ void clearExpiredState(SStreamFileState* pFileState) {
       }
     }
     taosArrayRemoveBatch(pWinStates, 0, size - 1, NULL);
+  }
+  code = clearRowBuff(pFileState);
+  QUERY_CHECK_CODE(code, lino, _end);
+
+_end:
+  if (code != TSDB_CODE_SUCCESS) {
+    qError("%s failed at line %d since %s", __func__, lino, tstrerror(code));
   }
 }
 
