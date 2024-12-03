f4aedcf6fd86b21e4b236651fe5d249f3aa0f463
dmchen <cademfly@hotmail.com>
2024-12-03 09:58:14 +0800

    fix/exit-fail-retry-when-follower

--- source/dnode/mnode/impl/src/mndTrans.c
+++ source/dnode/mnode/impl/src/mndTrans.c
@@ -1671,9 +1671,6 @@ static bool mndTransPerformRedoActionStage(SMnode *pMnode, STrans *pTrans, bool 
       return false;
     }
   }
-
-  // if (mndCannotExecuteTrans(pMnode, topHalf)) return false;
-
   terrno = code;
 
   if (code == 0) {
