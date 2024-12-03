f23633e83de509ae25b10eb168a7ca16a3dbfce8
wangmm0220 <wangmm0220@gmail.com>
2024-12-03 22:28:18 +0800

    feat:[TD-32642] add timezone support in windows

--- source/os/test/osTimeTests.cpp
+++ source/os/test/osTimeTests.cpp
@@ -49,7 +49,7 @@ TEST(osTimeTests, taosLocalTime) {
   // Test 4: Test when timep is negative on Windows
 #ifdef WINDOWS
   time_t pos_timep = 1609459200;  // 2021-01-01 08:00:00
-  local_time = taosLocalTime(&pos_timep, &result, NULL, 0);
+  local_time = taosLocalTime(&pos_timep, &result, NULL, 0, NULL);
   ASSERT_NE(local_time, nullptr);
   ASSERT_EQ(local_time->tm_year, 121);
   ASSERT_EQ(local_time->tm_mon, 0);
@@ -59,7 +59,7 @@ TEST(osTimeTests, taosLocalTime) {
   ASSERT_EQ(local_time->tm_sec, 0);
 
   time_t neg_timep = -1617531000;  // 1918-09-29 21:50:00
-  local_time = taosLocalTime(&neg_timep, &result, NULL, 0);
+  local_time = taosLocalTime(&neg_timep, &result, NULL, 0, NULL);
   ASSERT_NE(local_time, nullptr);
   ASSERT_EQ(local_time->tm_year, 18);
   ASSERT_EQ(local_time->tm_mon, 8);
@@ -69,7 +69,7 @@ TEST(osTimeTests, taosLocalTime) {
   ASSERT_EQ(local_time->tm_sec, 0);
 
   time_t neg_timep2 = -315619200;  // 1960-01-01 08:00:00
-  local_time = taosLocalTime(&neg_timep2, &result, NULL, 0);
+  local_time = taosLocalTime(&neg_timep2, &result, NULL, 0, NULL);
   ASSERT_NE(local_time, nullptr);
   ASSERT_EQ(local_time->tm_year, 60);
   ASSERT_EQ(local_time->tm_mon, 0);
@@ -79,7 +79,7 @@ TEST(osTimeTests, taosLocalTime) {
   ASSERT_EQ(local_time->tm_sec, 0);
 
   time_t zero_timep = 0;  // 1970-01-01 08:00:00
-  local_time = taosLocalTime(&zero_timep, &result, NULL, 0);
+  local_time = taosLocalTime(&zero_timep, &result, NULL, 0, NULL);
   ASSERT_NE(local_time, nullptr);
   ASSERT_EQ(local_time->tm_year, 70);
   ASSERT_EQ(local_time->tm_mon, 0);
@@ -89,7 +89,7 @@ TEST(osTimeTests, taosLocalTime) {
   ASSERT_EQ(local_time->tm_sec, 0);
 
   time_t neg_timep3 = -78115158887;
-  local_time = taosLocalTime(&neg_timep3, &result, NULL, 0);
+  local_time = taosLocalTime(&neg_timep3, &result, NULL, 0, NULL);
   ASSERT_EQ(local_time, nullptr);
 #endif
 }
