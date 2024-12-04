f859d2fbb059589b25f98841ef77b01f92627564
Jing Sima <simondominic9997@outlook.com>
2024-12-04 18:54:32 +0800

    feat: [TS-4897] Support create/drop/alter/show/describe vtable

--- source/dnode/mnode/impl/inc/mndDef.h
+++ source/dnode/mnode/impl/inc/mndDef.h
@@ -561,6 +561,7 @@ typedef struct {
   col_id_t colId;
   int32_t  cmprAlg;
 } SCmprObj;
+
 typedef struct {
   char      name[TSDB_TABLE_FNAME_LEN];
   char      db[TSDB_DB_FNAME_LEN];
@@ -590,6 +591,7 @@ typedef struct {
   SRWLatch  lock;
   int8_t    source;
   SColCmpr* pCmpr;
+  int8_t    virtualStb;
 } SStbObj;
 
 typedef struct {
