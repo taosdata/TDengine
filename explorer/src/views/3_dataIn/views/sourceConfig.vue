78450ec9a0a76b1d71ed5c99f75ef7f8e61c0bff
guxiang02 <guxiang_0211@163.com>
2024-12-10 19:12:06 +0800

    fix(mqtt): 清除s_model数据

--- explorer/src/views/3_dataIn/views/sourceConfig.vue
+++ explorer/src/views/3_dataIn/views/sourceConfig.vue
@@ -393,6 +393,7 @@ export default {
           this.$store.commit('app/SET_HISTORIAN_ECHODATA',null)
           this.$store.commit('app/SET_HISTORIAN_DSN','')
           this.$store.commit("app/SET_STB_DEFAULT_COLUMNS",[]);
+          this.$store.state.app.s_model = {}
         }
         if (val == "kafka" || val == "mqtt") {
           // this.$set(this, "constmqttCols", []);
