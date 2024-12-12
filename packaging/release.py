9cf58220c681003e64f4fe221e09f9625f77e803
WANG Xu <feici02@outlook.com>
2024-12-12 11:51:57 +0800

    refactor: move oem to the else part

--- packaging/release.py
+++ packaging/release.py
@@ -576,9 +576,7 @@ def update_docs_zip_file(explorer_path):
         zh_doc_zip = os.path.join(local_doc_zip_path, "docs-zh.zip")
         print("update docs zip file")
         doc_zip_path = os.path.join(explorer_path, "..")
-        if release_info.CustomPrompt != 'taos' or release_info.CustomName != 'TDengine':
-            subprocess.run(f"scp root@192.168.0.30:{remote_doc_zip_path}/docs-{release_info.CustomPrompt}.zip {doc_zip_path}", shell=True)
-        else:
+        if release_info.CustomPrompt == 'taos' or release_info.CustomName == 'TDengine':
             if release_info.build_with_selfhost:
                 if os.path.exists(en_doc_zip) and os.path.exists(zh_doc_zip):
                     cmd1 = f"cp {en_doc_zip} {doc_zip_path}"
@@ -591,6 +589,8 @@ def update_docs_zip_file(explorer_path):
                 cmd2 = f"scp root@192.168.0.30:{remote_doc_zip_path}/docs-zh.zip {doc_zip_path}"
             subprocess.run(cmd1, shell=True)
             subprocess.run(cmd2, shell=True)
+        else: # for oem
+            subprocess.run(f"scp root@192.168.0.30:{remote_doc_zip_path}/docs-{release_info.CustomPrompt}.zip {doc_zip_path}", shell=True)
     except Exception as e:
         print(f"ERROR: update docs zip file failed: {e}")
         sys.exit(1)
