1c7cf241f65337d426c5e0f635da7ed0e9861656
chenhaoran <haoran920c@163.com>
2024-12-12 10:11:09 +0800

    Add an option to fetch docs zip from local

--- packaging/release.py
+++ packaging/release.py
@@ -197,7 +197,7 @@ def init_build_info():
         release_info.build_without_docs = True
     if args.build_with_selfhost:
         release_info.build_with_selfhost = True
-        
+
     release_info.InstallPath = get_install_path()
     release_info.ReleasePath = os.path.abspath(os.path.join(script_dir, "..", "release"))
     release_info.CpuType = GetCpuType()
@@ -572,15 +572,20 @@ def update_docs_zip_file(explorer_path):
     try:
         remote_doc_zip_path = "/root/enterprise_build_zip_work/enterprise-docs"
         local_doc_zip_path = os.path.join(explorer_path, "../../../../packaging/")
+        en_doc_zip = os.path.join(local_doc_zip_path, "docs-en.zip")
+        zh_doc_zip = os.path.join(local_doc_zip_path, "docs-zh.zip")
         print("update docs zip file")
         doc_zip_path = os.path.join(explorer_path, "..")
         if release_info.CustomPrompt != 'taos' or release_info.CustomName != 'TDengine':
             subprocess.run(f"scp root@192.168.0.30:{remote_doc_zip_path}/docs-{release_info.CustomPrompt}.zip {doc_zip_path}", shell=True)
         else:
             if release_info.build_with_selfhost:
-                if os.path.exists(local_doc_zip_path):
+                if os.path.exists(en_doc_zip) and os.path.exists(zh_doc_zip):
                     cmd1 = f"cp {local_doc_zip_path}/docs-en.zip {doc_zip_path}"
                     cmd2 = f"cp {local_doc_zip_path}/docs-zh.zip {doc_zip_path}"
+                else:
+                    print(f"ERROR: not found docs-en.zip or docs-zh.zip at {en_doc_zip}" )
+                    sys.exit(1)
             else:
                 cmd1 = f"scp root@192.168.0.30:{remote_doc_zip_path}/docs-en.zip {doc_zip_path}"
                 cmd2 = f"scp root@192.168.0.30:{remote_doc_zip_path}/docs-zh.zip {doc_zip_path}"
