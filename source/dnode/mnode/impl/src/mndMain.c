d586026e258ffef7c7b4d85be18b11c79ca5a3fa
yihaoDeng <luomoxyz@126.com>
2024-12-15 18:01:21 +0800

    telemetry refactor

--- source/dnode/mnode/impl/src/mndMain.c
+++ source/dnode/mnode/impl/src/mndMain.c
@@ -410,11 +410,9 @@ void mndDoTimerPullupTask(SMnode *pMnode, int64_t sec) {
     mndStreamConsensusChkpt(pMnode);
   }
 
-#ifndef TD_ENTERPRISE
-  if (sec % tsTelemInterval == (TMIN(60, (tsTelemInterval - 1)))) {
+  if (sec % tsTelemInterval == (TMIN(86400, (tsTelemInterval - 1)))) {
     mndPullupTelem(pMnode);
   }
-#endif
 
   if (sec % tsGrantHBInterval == 0) {
     mndPullupGrant(pMnode);
