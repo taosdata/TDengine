073748be902ca08f0267966e266bfe1114e1b2a4
wangjiaming0909 <wangjiaming0909@sina.com>
2024-12-27 18:20:42 +0800

    define wide integer, support decimal128

---
+++ source/libs/decimal/inc/wideInteger.h
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2019 TAOS Data, Inc. <jhtao@taosdata.com>
+ *
+ * This program is free software: you can use, redistribute, and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3
+ * or later ("AGPL"), as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _TD_WIDE_INTEGER_H_
+#define _TD_WIDE_INTEGER_H_
+
+#include <stdint.h>
+#include "tdef.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct _UInt128 {
+  uint64_t low;
+  uint64_t high;
+} _UInt128;
+
+// TODO wjm use cmake to check if the compiler supports __int128_t
+#if defined(__GNUC__) || defined(__clang__)
+// #if 0
+typedef __uint128_t UInt128;
+#else
+typedef _UInt128 UInt128;
+#define Int128 UInt128
+#endif
+
+#define SAFE_SIGNED_OP(a, b, SIGNED_TYPE, UNSIGNED_TYPE, OP) (SIGNED_TYPE)((UNSIGNED_TYPE)(a)OP(UNSIGNED_TYPE)(b))
+#define SAFE_INT64_ADD(a, b)                                 SAFE_SIGNED_OP(a, b, int64_t, uint64_t, +)
+#define SAFE_INT64_SUBTRACT(a, b)                            SAFE_SIGNED_OP(a, b, int64_t, uint64_t, -)
+
+void     makeUInt128(UInt128* pInt, DecimalWord hi, DecimalWord lo);
+uint64_t uInt128Hi(const UInt128* pInt);
+uint64_t uInt128Lo(const UInt128* pInt);
+
+void uInt128Abs(UInt128* pInt);
+void uInt128Add(UInt128* pLeft, const UInt128* pRight);
+void uInt128Subtract(UInt128* pLeft, const UInt128* pRight);
+void uInt128Multiply(UInt128* pLeft, const UInt128* pRight);
+void uInt128Divide(UInt128* pLeft, const UInt128* pRight);
+void uInt128Mod(UInt128* pLeft, const UInt128* pRight);
+bool uInt128Lt(const UInt128* pLeft, const UInt128* pRight);
+bool uInt128Gt(const UInt128* pLeft, const UInt128* pRight);
+bool uInt128Eq(const UInt128* pLeft, const UInt128* pRight);
+
+extern UInt128 uInt128_1e18;
+extern UInt128 uInt128Zero;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _TD_WIDE_INTEGER_H_ */
---
+++ source/libs/decimal/src/wideInteger.c
@@ -0,0 +1,25 @@
+#include "wideInteger.h"
+
+#if defined(__GNUC__) || defined(__clang__)
+// #if 0
+void     makeUInt128(UInt128* pInt, DecimalWord hi, DecimalWord lo) { *pInt = ((UInt128)hi) << 64 | lo; }
+uint64_t uInt128Hi(const UInt128* pInt) { return *pInt >> 64; }
+uint64_t uInt128Lo(const UInt128* pInt) { return *pInt & 0xFFFFFFFFFFFFFFFF; }
+
+void uInt128Abs(UInt128* pInt);
+void uInt128Add(UInt128* pLeft, const UInt128* pRight) { *pLeft += *pRight; }
+void uInt128Subtract(UInt128* pLeft, const UInt128* pRight);
+void uInt128Multiply(UInt128* pLeft, const UInt128* pRight) { *pLeft = *pLeft * *pRight; }
+void uInt128Divide(UInt128* pLeft, const UInt128* pRight) { *pLeft /= *pRight; }
+void uInt128Mod(UInt128* pLeft, const UInt128* pRight) { *pLeft %= *pRight; }
+bool uInt128Lt(const UInt128* pLeft, const UInt128* pRight);
+bool uInt128Gt(const UInt128* pLeft, const UInt128* pRight);
+bool uInt128Eq(const UInt128* pLeft, const UInt128* pRight) { return *pLeft == *pRight; }
+
+UInt128 uInt128Zero = 0;
+UInt128 uInt128_1e18 = 1000000000000000000;
+#else
+
+void uInt128Multiply(UInt128* pLeft, const UInt128* pRight) {}
+
+#endif
