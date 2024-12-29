63e3e33f141a3b93d589c9ee578c7170e431e28f
Shengliang Guan <guanshengliang@qq.com>
2024-12-29 19:37:58 +0800

    Revert "Revert "Revert "Enh:[td 29974]improve trans"""

--- include/libs/nodes/cmdnodes.h
+++ include/libs/nodes/cmdnodes.h
@@ -439,11 +439,6 @@ typedef struct SShowCompactDetailsStmt {
   SNode*    pCompactId;
 } SShowCompactDetailsStmt;
 
-typedef struct SShowTransactionDetailsStmt {
-  ENodeType type;
-  SNode*    pTransactionId;
-} SShowTransactionDetailsStmt;
-
 typedef enum EIndexType { INDEX_TYPE_SMA = 1, INDEX_TYPE_FULLTEXT, INDEX_TYPE_NORMAL } EIndexType;
 
 typedef struct SIndexOptions {
