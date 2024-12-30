f6b180ff569aab9ba859a6ec84be019a7fc5c4fd
Alex Duan <417921451@qq.com>
2024-12-30 16:57:16 +0800

    fix: use old case model

--- docs/examples/python/stmt2_native.py
+++ docs/examples/python/stmt2_native.py
@@ -2,6 +2,9 @@ import taos
 from datetime import datetime
 import random
 
+numOfSubTable = 10
+numOfRow = 10
+
 conn = None
 stmt2 = None
 host="localhost"        
@@ -26,38 +29,28 @@ try:
     sql = "INSERT INTO ? USING meters (groupid, location) TAGS(?,?) VALUES (?,?,?,?)"
     stmt2 = conn.statement2(sql)
 
-    # table name array
-    tbnames = ["d0","d1","d2"]
-    # tag data array
-    tags    = [
-        [1, "BeiJing"],
-        [2, None],
-        [3, "ShangHai"]
-    ]
-    # column data array
-    datas   = [
-        # d0 tabled
-        [
-            [1601481600000,1601481600001,1601481600002,1601481600003,1601481600004,1601481600005],
-            [10.1,         10.2,         10.3,         10.4,         10.5         ,None         ],
-            [98,           None,           60,           100,          99           ,128          ],
-            [0,            1,            0,            0,            1           ,0             ]
-        ],
-        # d1 tabled
-        [
-            [1601481700000,1601481700001,1601481700002,1601481700003,1601481700004,1601481700005],
-            [10.1,         10.2,         10.3,         10.4,         10.5         ,11.2         ],
-            [98,           80,           60,           100,          99           ,128          ],
-            [0,            1,            0,            0,            1           ,0             ]
-        ],
-        # d2 tabled
-        [
-            [1601481800000,1601481800001,1601481800002,1601481800003,1601481800004,1601481800005],
-            [10.1,         10.2,         10.3,         10.4,         10.5         ,13.4         ],
-            [98,           80,           60,           100,          99           ,128          ],
-            [0,            1,            0,            None,            1           ,0          ]
-        ],
-    ]
+    tbnames = []
+    tags    = []
+    datas   = []
+    
+    for i in range(numOfSubTable):
+        # tbnames
+        tbnames.append(f"d_bind_{i}")
+        # tags
+        tags.append([i, f"location_{i}"])
+        # datas
+        current = int(datetime.now().timestamp() * 1000)
+        timestamps = []
+        currents = []
+        voltages = []
+        phases = []
+        for j in range (numOfRow):
+            timestamps.append(current + i*1000 + j)
+            currents.append(float(random.random() * 30))
+            voltages.append(random.randint(100, 300))
+            phases.append(float(random.random()))
+        data = [timestamps, currents, voltages, phases]
+        datas.append(data)
 
     # 4 bind param
     stmt2.bind_param(tbnames, tags, datas)
@@ -66,7 +59,7 @@ try:
     stmt2.execute()
 
     # show 
-    print(f"Successfully inserted with stmt2 to power.meters.")
+    print(f"Successfully inserted with stmt2 to power.meters. child={numOfSubTable} rows={numOfRow} \n")
 
 except Exception as err:
     print(f"Failed to insert to table meters using stmt2, ErrMessage:{err}") 
