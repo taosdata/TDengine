9634a3867d25fdb8af92e6847ba0abe1647ccb29
wangjiaming0909 <wangjiaming0909@sina.com>
2025-01-08 16:24:20 +0800

    fix decimal from str and decimal to str

--- include/libs/decimal/decimal.h
+++ include/libs/decimal/decimal.h
@@ -24,13 +24,18 @@ extern "C" {
 #include "ttypes.h"
 typedef struct SDataType SDataType;
 typedef struct SValue    SValue;
+typedef void             DecimalType;
 
 typedef struct Decimal64 {
-  DecimalWord words[1];
+  DecimalWord words[1]; // do not touch it directly, use DECIMAL64_GET_VALUE MACRO
 } Decimal64;
 
+#define DECIMAL64_GET_VALUE(pDec) (int64_t)((pDec)->words[0])
+#define DECIMAL64_SET_VALUE(pDec, val) (*(int64_t*)((pDec)->words)) = (int64_t)(val)
+#define DECIMAL64_CLONE(pDst, pFrom) ((Decimal64*)(pDst))->words[0] = ((Decimal64*)(pFrom))->words[0]
+
 typedef struct Decimal128 {
-  DecimalWord words[2];
+  DecimalWord words[2]; // do not touch it directly, use DECIMAL128_HIGH_WORD/DECIMAL128_LOW_WORD
 } Decimal128;
 
 #define Decimal        Decimal128
@@ -38,7 +43,7 @@ typedef struct Decimal128 {
 #define makeDecimal    makeDecimal128
 
 // TODO wjm check if we need to expose these functions in decimal.h
-void makeDecimal64(Decimal64* pDec64, DecimalWord w);
+void makeDecimal64(Decimal64* pDec64, int64_t w);
 void makeDecimal128(Decimal128* pDec128, int64_t hi, uint64_t low);
 
 #define DECIMAL_WORD_NUM(TYPE) sizeof(TYPE) / sizeof(DecimalWord)
@@ -53,7 +58,7 @@ int32_t decimal128FromStr(const char* str, int32_t len, uint8_t expectPrecision,
 int32_t decimal64ToDataVal(const Decimal64* dec, SValue* pVal);
 int32_t decimal128ToDataVal(Decimal128* dec, SValue* pVal);
 
-int32_t decimalToStr(const DecimalWord* pDec, int8_t type, int8_t precision, int8_t scale, char* pBuf, int32_t bufLen);
+int32_t decimalToStr(const DecimalType* pDec, int8_t type, int8_t precision, int8_t scale, char* pBuf, int32_t bufLen);
 
 int32_t decimalGetRetType(const SDataType* pLeftT, const SDataType* pRightT, EOperatorType opType, SDataType* pOutType);
 int32_t decimalOp(EOperatorType op, const SDataType* pLeftT, const SDataType* pRightT, const SDataType* pOutT,
@@ -62,17 +67,17 @@ int32_t convertToDecimal(const void* pData, const SDataType* pInputType, void* p
 
 // TODO wjm change rightWordNum to DecimalType??
 typedef struct SDecimalOps {
-  void (*negate)(DecimalWord* pWord);
-  void (*abs)(DecimalWord* pWord);
-  void (*add)(DecimalWord* pLeft, const DecimalWord* pRight, uint8_t rightWordNum);
-  void (*subtract)(DecimalWord* pLeft, const DecimalWord* pRight, uint8_t rightWordNum);
-  void (*multiply)(DecimalWord* pLeft, const DecimalWord* pRight, uint8_t rightWordNum);
-  void (*divide)(DecimalWord* pLeft, const DecimalWord* pRight, uint8_t rightWordNum, DecimalWord* pRemainder);
-  void (*mod)(DecimalWord* pLeft, const DecimalWord* pRight, uint8_t rightWordNum);
-  bool (*lt)(const DecimalWord* pLeft, const DecimalWord* pRight, uint8_t rightWordNum);
-  bool (*gt)(const DecimalWord* pLeft, const DecimalWord* pRight, uint8_t rightWordNum);
-  bool (*eq)(const DecimalWord* pLeft, const DecimalWord* pRight, uint8_t rightWordNum);
-  int32_t (*toStr)(const DecimalWord* pInt, uint8_t scale, char* pBuf, int32_t bufLen);
+  void (*negate)(DecimalType* pWord);
+  void (*abs)(DecimalType* pWord);
+  void (*add)(DecimalType* pLeft, const DecimalType* pRight, uint8_t rightWordNum);
+  void (*subtract)(DecimalType* pLeft, const DecimalType* pRight, uint8_t rightWordNum);
+  void (*multiply)(DecimalType* pLeft, const DecimalType* pRight, uint8_t rightWordNum);
+  void (*divide)(DecimalType* pLeft, const DecimalType* pRight, uint8_t rightWordNum, DecimalType* pRemainder);
+  void (*mod)(DecimalType* pLeft, const DecimalType* pRight, uint8_t rightWordNum);
+  bool (*lt)(const DecimalType* pLeft, const DecimalType* pRight, uint8_t rightWordNum);
+  bool (*gt)(const DecimalType* pLeft, const DecimalType* pRight, uint8_t rightWordNum);
+  bool (*eq)(const DecimalType* pLeft, const DecimalType* pRight, uint8_t rightWordNum);
+  int32_t (*toStr)(const DecimalType* pInt, uint8_t scale, char* pBuf, int32_t bufLen);
 } SDecimalOps;
 
 SDecimalOps* getDecimalOps(int8_t dataType);
