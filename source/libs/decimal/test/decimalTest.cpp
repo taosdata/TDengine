474d526dfe6e5b36cf3087a65329e45bd6f16b24
wangjiaming0909 <wangjiaming0909@sina.com>
2025-01-14 09:08:46 +0800

    decimal + - * /

--- source/libs/decimal/test/decimalTest.cpp
+++ source/libs/decimal/test/decimalTest.cpp
@@ -42,7 +42,7 @@ void extractWideInteger(__int128 a) {
 }
 
 void printDecimal(const DecimalType* pDec, uint8_t type, uint8_t prec, uint8_t scale) {
-  char buf[64] = {0};
+  char    buf[64] = {0};
   int32_t code = decimalToStr(pDec, type, prec, scale, buf, 64);
   ASSERT_EQ(code, 0);
   cout << buf;
@@ -60,11 +60,219 @@ __int128 generate_big_int128(uint32_t digitNum) {
 void checkDecimal(const DecimalType* pDec, uint8_t t, uint8_t prec, uint8_t scale, const char* valExpect) {
   ASSERT_TRUE(t == prec > 18 ? TSDB_DATA_TYPE_DECIMAL : TSDB_DATA_TYPE_DECIMAL64);
   ASSERT_TRUE(scale <= prec);
-  char buf[64] = {0};
+  char    buf[64] = {0};
   int32_t code = decimalToStr(pDec, t, prec, scale, buf, 64);
   ASSERT_EQ(code, 0);
   ASSERT_STREQ(buf, valExpect);
   cout << "decimal" << (prec > 18 ? 128 : 64) << " " << (int32_t)prec << ":" << (int32_t)scale << " -> " << buf << endl;
+}
+
+class Numeric128;
+class Numeric64 {
+  Decimal64                dec_;
+  static constexpr uint8_t WORD_NUM = WORD_NUM(Decimal64);
+
+ public:
+  friend class Numeric128;
+  Numeric64() { dec_ = {0}; }
+  int32_t fromStr(const string& str, uint8_t prec, uint8_t scale) {
+    return decimal64FromStr(str.c_str(), str.size(), prec, scale, &dec_);
+  }
+  Numeric64& operator+=(const Numeric64& r) {
+    getOps()->add(&dec_, &r.dec_, WORD_NUM);
+    return *this;
+  }
+  // Numeric64& operator+=(const Numeric128& r);
+
+  bool       operator==(const Numeric64& r) const { return getOps()->eq(&dec_, &r.dec_, WORD_NUM); }
+  Numeric64& operator=(const Numeric64& r);
+  Numeric64& operator=(const Numeric128& r);
+
+  static SDecimalOps* getOps() { return getDecimalOps(TSDB_DATA_TYPE_DECIMAL64); }
+};
+
+class Numeric128 {
+  Decimal128               dec_;
+  static constexpr uint8_t WORD_NUM = WORD_NUM(Decimal128);
+
+ public:
+  friend Numeric64;
+  Numeric128() { dec_ = {0}; }
+  Numeric128(const Numeric128& r) = default;
+  int32_t fromStr(const string& str, uint8_t prec, uint8_t scale) {
+    return decimal128FromStr(str.c_str(), str.size(), prec, scale, &dec_);
+  }
+  Numeric128& operator+=(const Numeric128& r) { return *this; }
+  Numeric128& operator+=(const Numeric64& r) {
+    getOps()->add(&dec_, &r.dec_, Numeric64::WORD_NUM);
+    return *this;
+  }
+
+  static SDecimalOps* getOps() { return getDecimalOps(TSDB_DATA_TYPE_DECIMAL); }
+};
+
+Numeric64& Numeric64::operator=(const Numeric64& r) {
+  dec_ = r.dec_;
+  return *this;
+}
+
+template <int ByteNum>
+struct NumericType {};
+
+template <>
+struct NumericType<64> {
+  using Type = Numeric64;
+  static constexpr int8_t dataType = TSDB_DATA_TYPE_DECIMAL64;
+  static constexpr int8_t maxPrec = TSDB_DECIMAL64_MAX_PRECISION;
+};
+
+template <>
+struct NumericType<128> {
+  using Type = Numeric128;
+  static constexpr int8_t dataType = TSDB_DATA_TYPE_DECIMAL;
+  static constexpr int8_t maxPrec = TSDB_DECIMAL_MAX_PRECISION;
+};
+
+template <int ByteNum>
+class Numeric {
+  using Type = typename NumericType<ByteNum>::Type;
+  Type    dec_;
+  uint8_t prec_;
+  uint8_t scale_;
+
+ public:
+  Numeric(uint8_t prec, uint8_t scale, const std::string& str) : prec_(prec), scale_(scale) {
+    if (prec > NumericType<ByteNum>::maxPrec) throw std::string("prec too big") + std::to_string(prec);
+    int32_t code = dec_.fromStr(str, prec, scale) != 0;
+    if (code != 0) {
+      cout << "failed to init decimal from str: " << str << endl;
+      throw std::string(tstrerror(code));
+    }
+  }
+  Numeric(const Numeric& o) = default;
+  ~Numeric() = default;
+
+  SDataType getRetType(EOperatorType op, const SDataType& lt, const SDataType& rt) const {
+    SDataType ot = {0};
+    decimalGetRetType(&lt, &rt, op, &ot);
+    return ot;
+  }
+  uint8_t     prec() const { return prec_; }
+  uint8_t     scale() const { return scale_; }
+  const Type& dec() const { return dec_; }
+
+  template <int ByteNum2>
+  Numeric& binaryOp(const Numeric<ByteNum2>& r, EOperatorType op) {
+    auto out = binaryOp<ByteNum2, ByteNum>(r, op);
+    return *this = out;
+  }
+
+  template <int ByteNum2, int ByteNumO>
+  Numeric<ByteNumO> binaryOp(const Numeric<ByteNum2>& r, EOperatorType op) {
+    SDataType lt{.type = NumericType<ByteNum>::dataType, .precision = prec_, .scale = scale_, .bytes = ByteNum};
+    SDataType rt{.type = NumericType<ByteNum2>::dataType, .precision = r.prec(), .scale = r.scale(), .bytes = ByteNum2};
+    SDataType ot = getRetType(op, lt, rt);
+    Numeric<ByteNumO> out{ot.precision, ot.scale, "0"};
+    int32_t           code = decimalOp(op, &lt, &rt, &ot, &dec_, &r.dec(), &out);
+    if (code != 0) throw std::overflow_error(tstrerror(code));
+    return out;
+  }
+
+  template <int ByteNum2, int ByteNumO = 128>
+  Numeric<ByteNumO> operator+(const Numeric<ByteNum2>& r) {
+    return binaryOp<ByteNum2, ByteNumO>(r, OP_TYPE_ADD);
+  }
+
+  template <int ByteNum2, int ByteNumO = 128>
+  Numeric<ByteNumO> operator-(const Numeric<ByteNum2>& r) {
+    return binaryOp<ByteNum2, ByteNumO>(r, OP_TYPE_SUB);
+  }
+
+  template <int ByteNum2, int ByteNumO = 128>
+  Numeric<ByteNumO> operator*(const Numeric<ByteNum2>& r) {
+    return binaryOp<ByteNum2, ByteNumO>(r, OP_TYPE_MULTI);
+  }
+
+  template <int ByteNum2, int ByteNumO = 128>
+  Numeric<ByteNumO> operator/(const Numeric<ByteNum2>& r) {
+    return binaryOp<ByteNum2, ByteNumO>(r, OP_TYPE_DIV);
+  }
+
+  template <int ByteNum2>
+  Numeric& operator+=(const Numeric<ByteNum2>& r) {
+    return binaryOp(r, OP_TYPE_ADD);
+  }
+
+  template <int ByteNum2>
+  bool operator==(const Numeric<ByteNum2>& r) {
+    return binaryOp(r, OP_TYPE_EQUAL);
+  }
+  std::string toString() const {
+    char    buf[64] = {0};
+    int32_t code = decimalToStr(&dec_, NumericType<ByteNum>::dataType, prec(), scale(), buf, 64);
+    if (code != 0) throw std::string(tstrerror(code));
+    return {buf};
+  }
+
+  std::string toStringTrimTailingZeros() const {
+    auto    ret = toString();
+    int32_t sizeToRemove = 0;
+    auto    it = ret.rbegin();
+    for (; it != ret.rend(); ++it) {
+      if (*it == '0') {
+        ++sizeToRemove;
+        continue;
+      }
+      break;
+    }
+    if (ret.size() - sizeToRemove > 0) ret.resize(ret.size() - sizeToRemove);
+    return ret;
+  }
+};
+
+template <int ByteNum>
+ostream& operator<<(ostream& os, const Numeric<ByteNum>& n) {
+  os << n.toString() << "(" << (int32_t)n.prec() << ":" << (int32_t)n.scale() << ")";
+  return os;
+}
+
+TEST(decimal, numeric) {
+  Numeric<64> dec{10, 4, "123.456"};
+  Numeric<64> dec2{18, 10, "123456.123123"};
+  auto        o = dec + dec2;
+  cout << dec << " + " << dec2 << " = " << o << endl;
+  ASSERT_EQ(o.toString(), "123579.5791230000");
+
+  Numeric<128> dec128{37, 10, "123456789012300.09876543"};
+  o = dec + dec128;
+  cout << dec << " + " << dec128 << " = " << o << endl;
+  ASSERT_EQ(o.toStringTrimTailingZeros(), "123456789012423.55476543");
+  ASSERT_EQ(o.toString(), "123456789012423.5547654300");
+
+  auto os = o - dec;
+  ASSERT_EQ(os.toStringTrimTailingZeros(), dec128.toStringTrimTailingZeros());
+
+  auto os2 = o - dec128;
+  ASSERT_EQ(os2.toStringTrimTailingZeros(), dec.toStringTrimTailingZeros());
+
+  os = dec * dec2;
+  cout << dec << " * " << dec2 << " = " << os << endl;
+  ASSERT_EQ(os.toStringTrimTailingZeros(), "15241399.136273088");
+  ASSERT_EQ(os.toString(), "15241399.13627308800000");
+
+  os = dec * dec128;
+  cout << dec << " * " << dec128 << " = " << os << endl;
+  ASSERT_EQ(os.toStringTrimTailingZeros(), "15241481344302520.993184");
+  ASSERT_EQ(os.toString(), "15241481344302520.993184");
+
+  os2 = os / dec128;
+  cout << os << " / " << dec128 << " = " << os2 << endl;
+  ASSERT_EQ(os2.toStringTrimTailingZeros(), "123.456");
+  ASSERT_EQ(os2.toString(), "123.456000");
+
+  os = dec2 / dec;
+  cout << dec2 << " / " << dec << " = " << os;
+  ASSERT_EQ(os.toString(), "1000.000997302682737169518");
 }
 
 TEST(decimal, a) {
@@ -118,7 +326,7 @@ TEST(decimal128, divide) {
 }
 
 TEST(decimal, cpi_taos_fetch_rows) {
-  GTEST_SKIP();
+  //GTEST_SKIP();
   const char* host = "127.0.0.1";
   const char* user = "root";
   const char* passwd = "taosdata";
@@ -169,7 +377,6 @@ TEST(decimal, cpi_taos_fetch_rows) {
   taos_cleanup();
 }
 
-
 TEST(decimal, conversion) {
   // convert uint8 to decimal
   char      buf[64] = {0};
@@ -209,9 +416,9 @@ TEST(decimal, conversion) {
 static constexpr uint64_t k1E16 = 10000000000000000LL;
 
 TEST(decimal, decimalFromStr) {
-  char inputBuf[64] = "123.000000000000000000000000000000001";
+  char       inputBuf[64] = "123.000000000000000000000000000000001";
   Decimal128 dec128 = {0};
-  int32_t code = decimal128FromStr(inputBuf, strlen(inputBuf), 38, 35, &dec128);
+  int32_t    code = decimal128FromStr(inputBuf, strlen(inputBuf), 38, 35, &dec128);
   ASSERT_EQ(code, 0);
   __int128 res = decimal128ToInt128(&dec128);
   __int128 resExpect = 123;
@@ -222,7 +429,7 @@ TEST(decimal, decimalFromStr) {
   resExpect *= 100;
   ASSERT_EQ(res, resExpect);
 
-  char buf[64] = "999.999";
+  char      buf[64] = "999.999";
   Decimal64 dec64 = {0};
   code = decimal64FromStr(buf, strlen(buf), 6, 3, &dec64);
   ASSERT_EQ(code, 0);
@@ -231,8 +438,8 @@ TEST(decimal, decimalFromStr) {
 
 TEST(decimal, toStr) {
   Decimal64 dec = {0};
-  char buf[64] = {0};
-  int32_t code = decimalToStr(&dec, TSDB_DATA_TYPE_DECIMAL64, 10, 2, buf, 64);
+  char      buf[64] = {0};
+  int32_t   code = decimalToStr(&dec, TSDB_DATA_TYPE_DECIMAL64, 10, 2, buf, 64);
   ASSERT_EQ(code, 0);
   ASSERT_STREQ(buf, "0");
 
@@ -257,10 +464,10 @@ bool operator==(const SDataType& lt, const SDataType& rt) {
 
 TEST(decimal, decimalOpRetType) {
   EOperatorType op = OP_TYPE_ADD;
-  auto ta = getDecimalType(10, 2);
-  auto tb = getDecimalType(10, 2);
+  auto          ta = getDecimalType(10, 2);
+  auto          tb = getDecimalType(10, 2);
   SDataType tc{}, tExpect = {.type = TSDB_DATA_TYPE_DECIMAL64, .precision = 11, .scale = 2, .bytes = sizeof(Decimal64)};
-  int32_t code = decimalGetRetType(&ta, &tb, op, &tc);
+  int32_t   code = decimalGetRetType(&ta, &tb, op, &tc);
   ASSERT_EQ(code, 0);
   ASSERT_EQ(tExpect, tc);
 
@@ -288,14 +495,14 @@ TEST(decimal, decimalOpRetType) {
 }
 
 TEST(decimal, op) {
-  const char* stra = "123.99", * strb = "456.12";
+  const char *  stra = "123.99", *strb = "456.12";
   EOperatorType op = OP_TYPE_ADD;
-  auto ta = getDecimalType(10, 2);
-  Decimal64 a = {0};
-  int32_t code = decimal64FromStr(stra, strlen(stra), ta.precision, ta.scale, &a);
+  auto          ta = getDecimalType(10, 2);
+  Decimal64     a = {0};
+  int32_t       code = decimal64FromStr(stra, strlen(stra), ta.precision, ta.scale, &a);
   ASSERT_EQ(code, 0);
 
-  auto tb = getDecimalType(10, 2);
+  auto      tb = getDecimalType(10, 2);
   Decimal64 b{0};
   code = decimal64FromStr(strb, strlen(strb), tb.precision, tb.scale, &b);
   ASSERT_EQ(code, 0);
