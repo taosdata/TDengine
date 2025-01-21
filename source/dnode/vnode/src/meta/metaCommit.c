699f385c8990dfcb7d712c3643c3be75a3fbfb7a
Minglei Jin <mljin@taosdata.com>
2025-01-21 10:08:59 +0800

    meta/alloc: restrict tdb allocation from buffer pool

--- source/dnode/vnode/src/meta/metaCommit.c
+++ source/dnode/vnode/src/meta/metaCommit.c
@@ -14,8 +14,19 @@
  */
 
 #include "meta.h"
+#include "vnd.h"
 
 static FORCE_INLINE void *metaMalloc(void *pPool, size_t size) {
+  SVBufPool *pool = (SVBufPool *)pPool;
+  SVnode    *pVnode = pool->pVnode;
+
+  if (pVnode->inUse && pVnode->inUse->size > pVnode->inUse->node.size) {
+    return NULL;
+  }
+
+  return vnodeBufPoolMallocAligned((SVBufPool *)pPool, size);
+}
+static FORCE_INLINE void *metaMalloc2(void *pPool, size_t size) {
   return vnodeBufPoolMallocAligned((SVBufPool *)pPool, size);
 }
 static FORCE_INLINE void metaFree(void *pPool, void *p) { vnodeBufPoolFree((SVBufPool *)pPool, p); }
