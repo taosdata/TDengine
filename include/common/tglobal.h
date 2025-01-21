eb26a1ef7674ad74cf951b194696c012d217c0a4
xiao-77 <berylbao@taosdata.com>
2025-01-21 09:50:28 +0800

    Fix review errors.

--- include/common/tglobal.h
+++ include/common/tglobal.h
@@ -29,6 +29,10 @@ extern "C" {
 #define SLOW_LOG_TYPE_OTHERS 0x4
 #define SLOW_LOG_TYPE_ALL    0xFFFFFFFF
 
+#define RPC_MEMORY_USAGE_RATIO   0.1
+#define QUEUE_MEMORY_USAGE_RATIO 0.06
+#define APPLY_MEMORY_USAGE_RATIO 0.04
+
 // cluster
 extern char     tsFirst[];
 extern char     tsSecond[];
