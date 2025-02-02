a3db589cb2f72e1a82d3a36b41cbab58368d1b30
Alex Duan <417921451@qq.com>
2025-02-02 18:05:27 +0800

    fix: no use for CUnit

--- tools/taos-tools/deps/CUnit/CUnit/Simple.h
+++
@@ -1,8 +0,0 @@
-/*
- * Simple Informative/Automated Test interface
- */
-
-#ifndef CU_SIMPLE_H
-#define CU_SIMPLE_H
-
-#endif //CU_SIMPLE_H
--- tools/taos-tools/deps/CUnit/CUnitConfig.cmake
+++
@@ -1,4 +0,0 @@
-set (CUNIT_LIBRARIES cunit)
-set (CUNIT_INCLUDE_DIRS ${CMAKE_CURRENT_LIST_DIR}/../../../include)
-
-include(${CMAKE_CURRENT_LIST_DIR}/CUnitTargets.cmake)
