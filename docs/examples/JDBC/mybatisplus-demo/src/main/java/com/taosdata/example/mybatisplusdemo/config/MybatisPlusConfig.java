e540ff93a217608397ef0ebfd77d162e3a6d9ee9
sheyanjie-qq <249478495@qq.com>
2025-02-05 17:29:50 +0800

    resolve jdbc demo trivy issues

--- docs/examples/JDBC/mybatisplus-demo/src/main/java/com/taosdata/example/mybatisplusdemo/config/MybatisPlusConfig.java
+++ docs/examples/JDBC/mybatisplus-demo/src/main/java/com/taosdata/example/mybatisplusdemo/config/MybatisPlusConfig.java
@@ -1,34 +1,30 @@
 package com.taosdata.example.mybatisplusdemo.config;
 
-import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import java.util.Collections;
+
+@EnableTransactionManagement
 @Configuration
+@MapperScan("com.taosdata.example.mybatisplusdemo.mapper")
 public class MybatisPlusConfig {
 
-
-    /** mybatis 3.4.1 pagination config start ***/
-//    @Bean
-//    public MybatisPlusInterceptor mybatisPlusInterceptor() {
-//        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-//        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
-//        return interceptor;
-//    }
-
-//    @Bean
-//    public ConfigurationCustomizer configurationCustomizer() {
-//        return configuration -> configuration.setUseDeprecatedExecutor(false);
-//    }
-
+    /**
+     * 添加分页插件
+     */
     @Bean
-    public PaginationInterceptor paginationInterceptor() {
-//        return new PaginationInterceptor();
-        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
-        //TODO: mybatis-plus do not support TDengine, use postgresql Dialect
-        paginationInterceptor.setDialectType("postgresql");
-
-        return paginationInterceptor;
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL)); // 如果配置多个插件, 切记分页最后添加
+        // 如果有多数据源可以不配具体类型, 否则都建议配上具体的 DbType
+        return interceptor;
     }
-
 }
