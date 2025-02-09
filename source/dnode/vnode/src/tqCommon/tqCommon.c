694b6385d55a3cac8a59c116f8b850b4bcf51c8d
Haojun Liao <hjliao@taosdata.com>
2025-02-09 21:40:33 +0800

    refactor(stream): adjust reset and mark failed Id.

--- source/dnode/vnode/src/tqCommon/tqCommon.c
+++ source/dnode/vnode/src/tqCommon/tqCommon.c
@@ -960,9 +960,9 @@ int32_t tqStreamTaskProcessTaskResetReq(SStreamMeta* pMeta, char* pMsg) {
   tqDebug("s-task:%s receive task-reset msg from mnode, reset status and ready for data processing", pTask->id.idStr);
 
   streamMutexLock(&pTask->lock);
-  streamTaskClearCheckInfo(pTask, true);
 
   streamTaskSetFailedCheckpointId(pTask, pReq->chkptId);
+  streamTaskClearCheckInfo(pTask, true);
 
   // clear flag set during do checkpoint, and open inputQ for all upstream tasks
   SStreamTaskState pState = streamTaskGetStatus(pTask);
