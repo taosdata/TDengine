7aa8ad22cef1ef18f35077cc3ab94ad63d080bf4
dapan1121 <wpan@taosdata.com>
2025-02-10 13:31:41 +0800

    fix: optimize case

--- tests/system-test/2-query/join_cond.py
+++ tests/system-test/2-query/join_cond.py
@@ -14,9 +14,9 @@ class TDTestCase:
         os.system(f'taos -f {sql_file}')
         tdSql.query('use csv_import')
         tdSql.query('SELECT * FROM trans,candle WHERE trans.tradeTime BETWEEN to_timestamp("2010-01-01", "yyyy-mm-dd") AND to_timestamp("2010-01-21", "yyyy-mm-dd")+1d-1a AND candle.date BETWEEN to_timestamp("2010-01-01", "yyyy-mm-dd") AND to_timestamp("2010-01-21", "yyyy-mm-dd") AND timetruncate(trans.tradeTime, 1d) = candle.date AND trans.securityId = cast(substring_index(candle.order_book_id, ".", 1) as INT) AND trans.securityid = 600884;', queryTimes=1)
-        tdSql.checkRows(570)
+        tdSql.checkRows(460)
         tdSql.query('SELECT * FROM (SELECT * FROM trans WHERE tradeTime BETWEEN to_timestamp("2010-01-01", "yyyy-mm-dd") AND to_timestamp("2010-01-21", "yyyy-mm-dd")+1d-1a AND securityid = 600884) as t INNER JOIN(SELECT * FROM candle WHERE date BETWEEN to_timestamp("2010-01-01", "yyyy-mm-dd") AND to_timestamp("2010-01-21", "yyyy-mm-dd") ) as c ON timetruncate(t.tradeTime, 1d) = c.date AND t.securityId = cast(substring_index(c.order_book_id, ".", 1) as INT);', queryTimes=1)
-        tdSql.checkRows(570)
+        tdSql.checkRows(460)
 
     def run(self):
         self.test_join()
