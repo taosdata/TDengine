2227f3e7f519a8bbf7de06c6e4033587038bf78a
Alex Duan <417921451@qq.com>
2025-02-11 21:31:43 +0800

    fix: taosdump taosbenchmark --version commit is none

--- tests/army/tools/taosdump/ws/taosdumpEscapedDb.py
+++ tests/army/tools/taosdump/ws/taosdumpEscapedDb.py
@@ -27,10 +27,23 @@ class TDTestCase(TBase):
         case1<sdsang>: [TS-3072] taosdump dump escaped db name test
         """
 
+    def checkVersion(self):
+        # run
+        outputs = etool.runBinFile("taosdump", "-V")
+        print(outputs)
+        if len(outputs) != 3:
+            tdLog.exit(f"checkVersion return lines count {len(outputs) != 3}")
+        # version string len
+        assert len(outputs[0]) > 22
+        assert len(outputs[1]) > 43
+        assert len(outputs[2]) > 36
 
-
+        tdLog.info("check taosdump version successfully.")
 
     def run(self):
+        # check version
+        self.checkVersion()
+
         tdSql.prepare()
 
         tdSql.execute("drop database if exists db")
