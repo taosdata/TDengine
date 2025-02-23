8c0ead7c6113800c532b0b55a6148c100ce01e99
Haojun Liao <hjliao@taosdata.com>
2025-02-23 11:03:49 +0800

    refactor: add some comments.

--- source/dnode/vnode/src/vnd/vnodeSvr.c
+++ source/dnode/vnode/src/vnd/vnodeSvr.c
@@ -931,6 +931,8 @@ int32_t vnodeProcessFetchMsg(SVnode *pVnode, SRpcMsg *pMsg, SQueueInfo *pInfo) {
 
 int32_t vnodeProcessStreamMsg(SVnode *pVnode, SRpcMsg *pMsg, SQueueInfo *pInfo) {
   vTrace("vgId:%d, msg:%p in stream queue is processing", pVnode->config.vgId, pMsg);
+
+  // todo: NOTE: some command needs to run on follower, such as, stop_all_tasks
   if ((pMsg->msgType == TDMT_SCH_FETCH || pMsg->msgType == TDMT_VND_TABLE_META || pMsg->msgType == TDMT_VND_TABLE_CFG ||
        pMsg->msgType == TDMT_VND_BATCH_META) &&
       !syncIsReadyForRead(pVnode->sync)) {
