cc7e06f30398946e8e6adcf75a2d29529480e988
jiajingbin <jiajaybin@126.com>
2025-02-25 00:56:45 +0800

    feat: add grafana url export

--- cases/customer_scenarios/fractal/start.py
+++ cases/customer_scenarios/fractal/start.py
@@ -12,9 +12,25 @@
 # -*- coding: utf-8 -*-
 
 from taostest import TDCase, T
-
+from taostest.util.common import TDCom
+from taostest.util.remote import Remote
+import os
+import time
+from datetime import datetime, timedelta
 class Start(TDCase):
     def init(self):
+        start_time = datetime.utcnow()
+        self.tdCom = TDCom(self.tdSql)
+        self._remote: Remote = Remote(self.logger)
+        workflow_config = self.tdCom.load_workflow_json(self._remote, f'{os.environ["TEST_ROOT"]}/env/workflow_config.json')
+        print(workflow_config)
+        end_time = start_time + timedelta(seconds=int(workflow_config["exec_time"]))
+        url = (
+        f"http://192.168.2.190:3000/d/dedq3n2zhlypsd/named-processes"
+        f"?var-interval=10m&orgId=1&from={start_time.isoformat(timespec='milliseconds')}Z&to={end_time.isoformat(timespec='milliseconds')}Z"
+        f"&timezone=browser&var-processes=$__all&refresh=5s"
+        )
+        print(url)
         pass
 
     def run(self) -> bool:
