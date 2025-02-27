be47425691e97b2ddd59d086a646aa9172185c57
jiacy-jcy <714897623@qq.com>
2025-02-27 15:37:20 +0800

    add start taosx service

--- cases/customer_scenarios/fractal/start.py
+++ cases/customer_scenarios/fractal/start.py
@@ -42,10 +42,16 @@ class Start(TDCase):
         mqtt_pub_interval= mqtt_client_config["spec"]["interval"]
         self._remote.cmd(mqtt_host,f"nohup mqtt_pub --schema {mqtt_pub_path} --host {edge_host} --interval {mqtt_pub_interval}s > mqtt_pub.log 2>&1 &")
         
+    def start_taosx_service(self,host):
+        self._remote_cmd(host,"systemctl start taosx")
     def run(self) -> bool:
+        
         # start mqtt simulator
         self.start_mqtt_simulator()
-        pass
+        # start taosx service
+        taosd_setting = self.tdCom.get_components_setting(self.env_setting["settings"], "taosd")
+        taosx_host = taosd_setting["fqdn"][0]
+        self.start_taosx_service(taosx_host)
 
     def cleanup(self):
         pass
