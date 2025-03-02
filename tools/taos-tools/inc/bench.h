8687f08393dbf58aa79b8c702aa29480ab7fe9d4
Alex Duan <417921451@qq.com>
2025-03-02 17:39:26 +0800

    fix: native and websocket with string

--- tools/taos-tools/inc/bench.h
+++ tools/taos-tools/inc/bench.h
@@ -261,7 +261,7 @@ typedef unsigned __int32 uint32_t;
 #define STR_WEBSOCKET "WebSocket"
 
 #define DRIVER_OPT     "driver"
-#define DRIVER_DESC    "Connect mode , value can be \"" CONN_NATIVE "\" or \"" CONN_WEBSOCKET "\""
+#define DRIVER_DESC    "Connect mode , value can be \" Native \" or \" WebSocket \""
 
 // connect mode type define
 #define CONN_MODE_INVALID   -1
