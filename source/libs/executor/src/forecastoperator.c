9139f3cb47e3156848d93c087bf0c855c6fa2bdf
Haojun Liao <hjliao@taosdata.com>
2025-03-04 13:44:56 +0800

    fix(analytics): fix syntax error.

--- source/libs/executor/src/forecastoperator.c
+++ source/libs/executor/src/forecastoperator.c
@@ -158,7 +158,8 @@ static int32_t forecastCloseBuf(SForecastSupp* pSupp, const char* id) {
   }
 
   if (pSupp->optRows > ANALY_MAX_FC_ROWS) {
-    qError("%s required too many forecast rows, max allowed:%d, required:%d", id, ANALY_MAX_FC_ROWS, pSupp->optRows);
+    qError("%s required too many forecast rows, max allowed:%d, required:%" PRId64, id, ANALY_MAX_FC_ROWS,
+           pSupp->optRows);
     return TSDB_CODE_ANA_ANODE_TOO_MANY_ROWS;
   }
 
