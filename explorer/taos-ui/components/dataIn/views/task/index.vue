48147b2b9f7259e450cdcb8422c1540e374c61ee
yingzhao <zacharyzhou@taosdata.com>
2025-03-11 16:29:38 +0800

    fix(explorer): 'disableEdit' for csv is not compatible

--- explorer/taos-ui/components/dataIn/views/task/index.vue
+++ explorer/taos-ui/components/dataIn/views/task/index.vue
@@ -405,7 +405,7 @@ async function getList() {
       item['localtype'] = dataSourceMap[item.from.type] ? dataSourceMap[item.from.type] : '';
       item['target'] = item.to_expand?.subject || '';
       item['created_at'] = item.created_at ? item.created_at.replace(/(?<=\.)\S+$/, '').replace('.', '') + 'Z' : '';
-      item['disableEdit'] = item.from.type === 'csv' && item.from.data.csvData.currentTab === 'upload_csv_file';
+      // item['disableEdit'] = item.from.type === 'csv' && item.from.data.csvData.currentTab === 'upload_csv_file';
       return item;
     });
   }
