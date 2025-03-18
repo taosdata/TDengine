c25981cb0d58349556a02f882a302497086a6895
Haojun Liao <hjliao@taosdata.com>
2025-03-18 09:27:04 +0800

    feat(gpt): support lstm and do some internal refactor, add sample autoencoder model.

--- tools/tdgpt/taosanalytics/test/anomaly_test.py
+++ tools/tdgpt/taosanalytics/test/anomaly_test.py
@@ -141,25 +141,23 @@ class AnomalyDetectionTest(unittest.TestCase):
 
     def test_autoencoder_ad(self):
         """for local test only, disabled it in github action"""
-        pass
+        data = self.__load_remote_data_for_ad()
 
-        # data = self.__load_remote_data_for_ad()
-        #
-        # s = loader.get_service("ad_encoder")
-        # s.set_input_list(data)
-        #
-        # try:
-        #     s.set_params({"model": "ad_encoder_"})
-        # except ValueError as e:
-        #     app_logger.log_inst.error(f"failed to set the param for auto_encoder algorithm, reason:{e}")
-        #     return
-        #
-        # r = s.execute()
-        #
-        # num_of_error = -(sum(filter(lambda x: x == -1, r)))
-        # self.assertEqual(num_of_error, 109)
-        #
-        # draw_ad_results(data, r, "autoencoder")
+        s = loader.get_service("sample_ad_model")
+        s.set_input_list(data)
+
+        try:
+            s.set_params({"model": "sample-ad-autoencoder"})
+        except ValueError as e:
+            app_logger.log_inst.error(f"failed to set the param for auto_encoder algorithm, reason:{e}")
+            return
+
+        r = s.execute()
+
+        num_of_error = -(sum(filter(lambda x: x == -1, r)))
+        draw_ad_results(data, r, "autoencoder")
+
+        self.assertEqual(num_of_error, 109)
 
     def test_get_all_services(self):
         """Test get all services"""
