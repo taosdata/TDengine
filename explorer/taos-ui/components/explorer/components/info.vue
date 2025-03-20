56fcbb279ac2f2dbe8803d614f3f5d3197cb9931
ZacharyZhou <13466397075@163.com>
2025-03-20 20:07:08 +0800

    fix(explorer): optimize stable & table schema view page (#2827)
    Co-authored-by: yingzhao <zacharyzhou@taosdata.com>

--- explorer/taos-ui/components/explorer/components/info.vue
+++ explorer/taos-ui/components/explorer/components/info.vue
@@ -2,38 +2,32 @@
   <div class="info">
     <el-form label-width="180px" label-position="left">
       <section class="info-content">
-        <section class="left">
-          <el-form-item v-for="item in leftField" :key="item" :label="item + ':'">
-            {{ infoData[item] }}
-          </el-form-item>
-          <el-form-item v-if="infoType !== 'db'" label="tags:"></el-form-item>
-          <el-table v-if="infoType !== 'db'" tooltip-effect="light" size="small" border :data="tags">
-            <el-table-column :show-overflow-tooltip="true" min-width="100" label="name" prop="name"> </el-table-column>
-            <el-table-column
-              :show-overflow-tooltip="true"
-              :label="infoType == 'stb' ? 'type' : 'value'"
-              prop="value"
-              :min-width="infoType == 'stb' ? 100 : 150"
-            >
-            </el-table-column>
-          </el-table>
-        </section>
-        <section class="right">
-          <el-form-item v-for="item in rightField" :key="item" :label="item + ':'">
-            {{ infoData[item] }}
-          </el-form-item>
-          <el-form-item v-if="infoType == 'stb' || infoType == 'db'" label="columns:"></el-form-item>
-          <el-table
-            v-if="infoType !== 'db' && columns?.length > 0"
-            tooltip-effect="light"
-            size="small"
-            border
-            :data="columns"
+        <el-form-item v-for="item in infoField" :key="item" :label="item + ':'">
+          {{ infoData[item] }}
+        </el-form-item>
+        <el-form-item v-if="infoType !== 'db'" label="tags:"></el-form-item>
+        <el-table v-if="infoType !== 'db'" style="width:800px; margin-bottom: 40px;" tooltip-effect="light" size="small" border :data="tags">
+          <el-table-column :show-overflow-tooltip="true" width="300" label="name" prop="name"> </el-table-column>
+          <el-table-column
+            :show-overflow-tooltip="true"
+            :label="'type'"
+            prop="type"
           >
-            <el-table-column :show-overflow-tooltip="true" min-width="100" label="name" prop="name"> </el-table-column>
-            <el-table-column :show-overflow-tooltip="true" width="150" label="type" prop="value"> </el-table-column>
-          </el-table>
-        </section>
+          </el-table-column>
+          <el-table-column v-if="infoType === 'tb'" :show-overflow-tooltip="true" label="value" prop="value"> </el-table-column>
+        </el-table>
+        <el-form-item v-if="infoType == 'stb' || infoType == 'tb'" label="columns:"></el-form-item>
+        <el-table
+          v-if="infoType !== 'db' && columns?.length > 0"
+          tooltip-effect="light"
+          size="small"
+          style="width:800px; margin-bottom: 40px;"
+          border
+          :data="columns"
+        >
+          <el-table-column :show-overflow-tooltip="true" width="300" label="name" prop="name"> </el-table-column>
+          <el-table-column :show-overflow-tooltip="true" label="type" prop="type"> </el-table-column>
+        </el-table>
       </section>
       <el-form-item v-if="infoType === 'db'" :label="'DSN:'" class="tmp-label">
         <div class="nowrap tmp-dns">{{ tmqDNS }}</div>
@@ -71,8 +65,8 @@ const infoField = computed(() =>
     ? Object.keys(infoData.value).filter((item: string) => !DBCustomedFiled.includes(item))
     : displayMap[infoType.value]
 );
-const leftField = computed(() => infoField.value.filter((_, index: number) => index % 2 == 0));
-const rightField = computed(() => infoField.value.filter((_, index: number) => index % 2));
+// const leftField = computed(() => infoField.value.filter((_, index: number) => index % 2 == 0));
+// const rightField = computed(() => infoField.value.filter((_, index: number) => index % 2));
 const tmqDNS = computed(() => {
   // return this.$store.state.app.current_cluster.urlPath;
   const gatewayURL = instance.gatewayUrl;
@@ -87,12 +81,12 @@ const tmqDNS = computed(() => {
   }
   return `taos+${wsPrefix}://${user}:${password}@${uri}/${dbName}`;
 });
-watch(
-  () => infoData.value.name,
-  () => {
-    getStruct();
-  }
-);
+// watch(
+//   () => infoData.value.name,
+//   () => {
+//     getStruct();
+//   }
+// );
 
 function getStruct() {
   switch (infoType.value) {
@@ -106,6 +100,11 @@ function getStruct() {
       break;
   }
 }
+
+onMounted(() => {
+  getStruct();
+});
+
 async function getStableStruct() {
   // 当为超级表的时候只需要获取结构的类型就可以了
   const data = await getStableStructReq(infoData.value.parent, infoData.value.name).catch(() => ({
@@ -120,12 +119,13 @@ async function getTableStruct() {
   const data = await getSubtbCurrentStruct(currentInfoData.db.name, '', infoData.value.name);
   tags.value = processTagAndColumnData(data.tags);
   columns.value = processTagAndColumnData(data.columns);
-}
+  }
 
 function processTagAndColumnData(data: Recordable[]) {
   return data.map((item: Recordable) => ({
     name: item.field,
-    value: item.type
+    type: item.type,
+    value: item.value
   }));
 }
 function copyDNS() {
@@ -137,49 +137,7 @@ function copyDNS() {
 .info {
   height: 100%;
 
-  .info-content {
-    display: flex;
-    justify-content: space-between;
-
-    .left,
-    .right {
-      width: 50%;
-      padding-left: 10px;
-    }
-
-    &:deep(.el-form-item) {
-      margin-bottom: 8px;
-    }
-  }
-
-  &:deep(.el-form-item__label) {
-    font-size: 16px;
-    line-height: 20px !important;
-  }
-
-  &:deep(.el-form-item__content) {
-    font-size: 16px;
-    line-height: 22px;
-  }
-
-  &:deep(.el-table) {
-    margin-top: -6px;
-
-    & th.el-table__cell {
-      cursor: unset;
-    }
-
-    & th.el-table__cell > .cell {
-      padding-left: 6px;
-      font-size: 16px;
-      font-weight: 500;
-    }
-
-    & td.el-table__cell > .cell {
-      padding-left: 6px;
-      font-size: 16px;
-    }
-  }
+  
 }
 
 .tmp-label {
