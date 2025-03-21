88c487cdeba4d34d931ec6a9734c8a17adf71522
Minglei Jin <mljin@taosdata.com>
2025-03-21 17:11:54 +0800

    chore: move uv top

--- source/libs/tmqtt/src/xnode_mgmt_mqtt/mgmtMqtt.c
+++ source/libs/tmqtt/src/xnode_mgmt_mqtt/mgmtMqtt.c
@@ -13,10 +13,10 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "uv.h"
 #include "os.h"
 #include "tarray.h"
 #include "tglobal.h"
-#include "uv.h"
 #include "xnode_mgmt_mqtt.h"
 
 #ifdef _TD_DARWIN_64
