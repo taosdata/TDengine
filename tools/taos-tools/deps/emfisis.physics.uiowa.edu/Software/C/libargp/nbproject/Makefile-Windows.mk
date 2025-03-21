6723f47f9d40ce69a29f361d5c88f6f856db6594
freemine <freemine@yeah.net>
2025-03-21 09:21:00 +0800

    refactor(cmake): adding source code found on http://emfisis.physics.uiowa.edu/Software/C/libargp/

---
+++ tools/taos-tools/deps/emfisis.physics.uiowa.edu/Software/C/libargp/nbproject/Makefile-Windows.mk
@@ -0,0 +1,54 @@
+#
+# Gererated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc.exe
+CCC=g++.exe
+CXX=g++.exe
+FC=
+
+# Include project Makefile
+include libargp-Makefile.mk
+
+# Object Directory
+OBJECTDIR=build/Windows/MinGW-Windows
+
+# Object Files
+OBJECTFILES=
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=
+CXXFLAGS=
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS} 
+	cd . && make -f Makefile
+
+# Subprojects
+.build-subprojects:
+
+# Clean Targets
+.clean-conf:
+	cd . && make -f Makefile distclean
+
+# Subprojects
+.clean-subprojects:
