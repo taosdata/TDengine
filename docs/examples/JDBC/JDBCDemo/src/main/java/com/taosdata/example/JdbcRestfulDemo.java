9f9420151dc12fc334f4485fc07ef114e27cfbf4
sheyanjie-qq <249478495@qq.com>
2025-03-22 21:21:40 +0800

    test ci

--- docs/examples/JDBC/JDBCDemo/src/main/java/com/taosdata/example/JdbcRestfulDemo.java
+++ docs/examples/JDBC/JDBCDemo/src/main/java/com/taosdata/example/JdbcRestfulDemo.java
@@ -11,7 +11,7 @@ public class JdbcRestfulDemo {
 
     public static void main(String[] args) {
         try {
-            // use port 6041 in url when use JDBC-restful
+            // use port 6041 in url when using JDBC-restful
             String url = "jdbc:TAOS-RS://" + host + ":6041/?user=" + user + "&password=" + password;
 
             Properties properties = new Properties();
@@ -23,7 +23,8 @@ public class JdbcRestfulDemo {
             stmt.execute("drop database if exists " + dbname);
             stmt.execute("create database if not exists " + dbname);
             stmt.execute("use " + dbname);
-            stmt.execute("create table " + dbname + ".weather(ts timestamp, temperature float) tags(location nchar(64))");
+            stmt.execute(
+                    "create table " + dbname + ".weather(ts timestamp, temperature float) tags(location nchar(64))");
             stmt.executeUpdate("insert into t1 using " + dbname + ".weather tags('北京') values(now, 18.2)");
             ResultSet rs = stmt.executeQuery("select * from " + dbname + ".weather");
             ResultSetMetaData meta = rs.getMetaData();
