b8e4c3c2790c978f452d5580fec5bb4c049115ac
chenhaoran <haoran920c@163.com>
2025-03-26 03:55:30 +0800

    enh: add PID logging for tdtsfm and timer-moe service start scripts

--- tools/tdgpt/script/start-tdtsfm.sh
+++ tools/tdgpt/script/start-tdtsfm.sh
@@ -7,6 +7,8 @@ if [ -d "/var/lib/taos/taosanode/model/tdtsfm" ]; then
     source /usr/local/taos/taosanode/venv/bin/activate
     nohup python3 taos_ts_server.py  --action server &
     echo "check the pid of the taos_ts_server.py to confirm it is running"
+    pid=$(pgrep -f "taos_ts_server.py")
+    echo "PID of the taos_ts_server.py is $pid"
 else
     echo "Directory /var/lib/taos/taosanode/model/tdtsfm does not exist."
     exit 1
