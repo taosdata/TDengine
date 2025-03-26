2e8dd9f736a1d0d697965a703c27d99b6f62f31c
Feng Chao <flyingangel_2008@163.com>
2025-03-26 11:24:56 +0800

    test: Remove unnecessary blank line in test_5dnode_3mnode_stop.py for improved readability

--- test/cases/high_availability/cluster_expansion_reduction/test_5dnode_3mnode_stop.py
+++ test/cases/high_availability/cluster_expansion_reduction/test_5dnode_3mnode_stop.py
@@ -130,7 +130,6 @@ class Test5dnode3mnodeStop:
         tdDnodes[1].starttaosd()
         clusterComCheck.checkMnodeStatus(3)
 
-
         tdDnodes[2].stoptaosd()
         clusterComCheck.check3mnodeoff(3,3)
         tdDnodes[2].starttaosd()
