d6186b32cc1453897fa33c54263c84da038add99
happyguoxy <happy_guoxy@163.com>
2025-03-26 12:47:02 +0800

    test:add gcda show

--- tests/parallel_test/container_build_coverage.sh
+++ tests/parallel_test/container_build_coverage.sh
@@ -78,7 +78,7 @@ docker run \
     -v ${REP_REAL_PATH}/community/contrib/pcre2/:${REP_DIR}/community/contrib/pcre2 \
     -v ${REP_REAL_PATH}/community/contrib/zlib/:${REP_DIR}/community/contrib/zlib \
     -v ${REP_REAL_PATH}/community/contrib/zstd/:${REP_DIR}/community/contrib/zstd \
-    --rm --ulimit core=-1 taos_test:v1.0 sh -c "cd $REP_DIR; rm -rf debug; mkdir -p debug; cd debug; cmake .. -DCOVER=true -DBUILD_TEST=true -DBUILD_HTTP=false -DBUILD_TOOLS=true -DWEBSOCKET=true -DBUILD_GEOS=true ; make -j install ; ctest  -E "cunit_test" -j20|| exit 1"
+    --rm --ulimit core=-1 taos_test:v1.0 sh -c "cd $REP_DIR; rm -rf debug; mkdir -p debug; cd debug; cmake .. -DCOVER=true -DBUILD_TEST=true -DBUILD_HTTP=false -DBUILD_TOOLS=true -DWEBSOCKET=true -DBUILD_GEOS=true ; make -j install || exit 1"
 
 
 if [[ -d ${WORKDIR}/debugNoSan  ]] ;then
@@ -90,7 +90,7 @@ if [[ -d ${WORKDIR}/debugRelease ]] ;then
     rm -rf  ${WORKDIR}/debugRelease
 fi
 
-mv  ${REP_REAL_PATH}/debug  ${WORKDIR}/debugNoSan
+mv  ${REP_REAL_PATH}/debug  ${WORKDIR}/debugNoSan|| true
 cd ${WORKDIR}/debugNoSan
 # 检查是否存在 .gcda 文件
 if ls -lR ${WORKDIR}/debugNoSan | grep '\.gcda$'; then
