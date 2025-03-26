7d298f3d5f062b2b9a85e646c1d6f67a5e6aa31a
happyguoxy <happy_guoxy@163.com>
2025-03-26 16:02:53 +0800

    test:alter sh

--- tests/parallel_test/container_build_coverage.sh
+++ tests/parallel_test/container_build_coverage.sh
@@ -79,7 +79,7 @@ docker run \
     -v ${REP_REAL_PATH}/community/contrib/pcre2/:${REP_DIR}/community/contrib/pcre2 \
     -v ${REP_REAL_PATH}/community/contrib/zlib/:${REP_DIR}/community/contrib/zlib \
     -v ${REP_REAL_PATH}/community/contrib/zstd/:${REP_DIR}/community/contrib/zstd \
-    --rm --ulimit core=-1 taos_test:v1.0 sh -c "cd $REP_DIR; rm -rf debug; mkdir -p debug; cd debug; cmake .. -DCOVER=true -DBUILD_TEST=true -DBUILD_HTTP=false -DBUILD_TOOLS=true -DWEBSOCKET=true -DBUILD_GEOS=true ; make -j ; make install ||  exit 1" || true
+    --rm --ulimit core=-1 taos_test:v1.0 sh -c "cd $REP_DIR; rm -rf debug; mkdir -p debug; cd debug; cmake .. -DCOVER=true -DBUILD_TEST=true -DBUILD_HTTP=false -DBUILD_TOOLS=true -DWEBSOCKET=true -DBUILD_GEOS=true ; make -j ||  exit 1" || true
 
 
 if [[ -d ${WORKDIR}/debugNoSan  ]] ;then
@@ -91,19 +91,12 @@ if [[ -d ${WORKDIR}/debugRelease ]] ;then
     rm -rf  ${WORKDIR}/debugRelease
 fi
 
-cd ${REP_REAL_PATH}/debug
-if ls -lR ${REP_REAL_PATH}/debug | grep '\.gcda$'; then
-    echo "real path .gcda files found."
-else
-    echo "No .gcda files in real path. Continuing without errors."
-fi
-
-cp -r ${REP_REAL_PATH}/debug  ${WORKDIR}/debugNoSan|| true
+mv  ${REP_REAL_PATH}/debug  ${WORKDIR}/debugNoSan|| true
 cd ${WORKDIR}/debugNoSan
 if ls -lR ${WORKDIR}/debugNoSan | grep '\.gcda$'; then
-    echo "mv path .gcda files found."
+    echo ".gcda files found."
 else
-    echo "No .gcda files in mv path. Continuing without errors."
+    echo "No .gcda files found. Continuing without errors."
 fi
 
 # 始终返回成功退出码
