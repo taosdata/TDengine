ed4e35f836321b9c46208e0aed4cc1b01deda76a
Yuanpai Zhang <ypzhang@taosdata.com>
2025-03-28 14:31:22 +0800

    fix(explorer): remove unused parameter (#2867)

--- explorer/taos-ui/components/dataIn/config/en/02-taos.ts
+++ explorer/taos-ui/components/dataIn/config/en/02-taos.ts
@@ -69,14 +69,10 @@ export default {
       label: 'Authentication',
       description: 'Use username/password plain authentication.',
       field: 'authentication',
-      type: 'tabs',
-      valueField: 'currentTab',
-      defaultValue: 'plain',
-      multiple: false,
       children: [
         {
           label: 'Plain',
-          name: 'plain',
+          name: '',
           field: 'plain',
           children: [
             {
