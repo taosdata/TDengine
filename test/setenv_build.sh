0705837c2725e8d1868f8235221b6112bd2ec018
freemine <freemine@yeah.net>
2025-04-08 20:33:39 +0800

    refactor(cmake): revert test/* stuffs back to main branch

--- test/setenv_build.sh
+++ test/setenv_build.sh
@@ -1,14 +1,11 @@
 export TEST_DIR=`pwd`
-# for debug set
+
 IN_TDINTERNAL="community"
 if [[ "$TEST_DIR" == *"$IN_TDINTERNAL"* ]]; then
   export TAOS_DIR=$TEST_DIR/../..
 else
   export TAOS_DIR=$TEST_DIR/..
 fi
+
 export TAOS_BIN_PATH=$TAOS_DIR/debug/build/bin
 export WORK_DIR=$TAOS_DIR/sim
-
-# for install set
-#export TAOS_BIN_PATH=/usr/bin
-#export WORK_DIR=`pwd`
