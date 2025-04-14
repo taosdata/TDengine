360f5c7fca2594e8ec8101e12c6c4ef23b10a73b
wangmm0220 <wangmm0220@gmail.com>
2025-04-14 09:58:37 +0800

    fix(taosx): invalid para error for zhongshihua

--- utils/test/c/write_raw_block_test.c
+++ utils/test/c/write_raw_block_test.c
@@ -39,32 +39,36 @@ int32_t test_write_raw_block(char* query, char* dst) {
 }
 
 int32_t test_write_raw_block_with_fields(char* query, char* dst) {
-  TAOS_RES* pRes = taos_query(pConn, query);
-  ASSERT(taos_errno(pRes) == 0);
-  void*   data = NULL;
-  int32_t numOfRows = 0;
-  int     error_code = taos_fetch_raw_block(pRes, &numOfRows, &data);
-  ASSERT(error_code == 0);
+//  TAOS_RES* pRes = taos_query(pConn, query);
+//  ASSERT(taos_errno(pRes) == 0);
+//  void*   data = NULL;
+//  int32_t numOfRows = 0;
+//  int     error_code = taos_fetch_raw_block(pRes, &numOfRows, &data);
+//  ASSERT(error_code == 0);
+//
+//  int         numFields = taos_num_fields(pRes);
+//  TAOS_FIELD* fields = taos_fetch_fields(pRes);
 
-  int         numFields = taos_num_fields(pRes);
-  TAOS_FIELD* fields = taos_fetch_fields(pRes);
 
-
-  TdFilePtr pFile = taosOpenFile("../../../utils/test/c/data_3460", TD_FILE_READ);
+  TdFilePtr pFile = taosOpenFile("../../../utils/test/c/data.bin", TD_FILE_READ);
   ASSERT(pFile != NULL);
-  int32_t size = 3460;
+  int32_t size = 5735607;
   void*   pMemBuf = taosMemoryCalloc(1, size);
   ASSERT (pMemBuf != NULL);
   int32_t readSize = taosReadFile(pFile, pMemBuf, size);
   ASSERT (readSize == size);
 
   taosCloseFile(&pFile);
-
-  error_code = taos_write_raw_block_with_fields(pConn, numOfRows, pMemBuf, dst, fields, numFields);
-  taos_free_result(pRes);
+  tmq_raw_data raw = {
+    .raw = pMemBuf,
+    .raw_len = size,
+    .raw_type = 2
+  };
+  int code = tmq_write_raw(pConn, raw);
+//  taos_free_result(pRes);
   taosMemoryFree(pMemBuf);
 
-  return error_code;
+  return 0;
 }
 
 void init_env() {
@@ -75,9 +79,11 @@ void init_env() {
   action("create database if not exists db_raw vgroups 2");
   action("use db_raw");
 
-  action("CREATE STABLE `rt_logging` (`time` TIMESTAMP, `depth` DOUBLE, `tvb` DOUBLE, `lagdepth` DOUBLE, `bitdep` DOUBLE, `blockpos` DOUBLE, `timedist_inst` DOUBLE, `hkload` DOUBLE, `wob` DOUBLE, `spp` DOUBLE, `cup` DOUBLE, `rpm` DOUBLE, `torque` DOUBLE, `tdrpm` DOUBLE, `tdtorque` DOUBLE, `spm1` DOUBLE, `spm2` DOUBLE, `spm3` DOUBLE, `zbc` DOUBLE, `pit1` DOUBLE, `pit2` DOUBLE, `pit3` DOUBLE, `pit4` DOUBLE, `pit5` DOUBLE, `pit6` DOUBLE, `tpit` DOUBLE, `mwi` DOUBLE, `mwo` DOUBLE, `mci` DOUBLE, `mco` DOUBLE, `mti` DOUBLE, `mto` DOUBLE, `mfi` DOUBLE, `flowout` DOUBLE, `gas` DOUBLE, `c1` DOUBLE, `c2` DOUBLE, `c3` DOUBLE, `ic4` DOUBLE, `nc4` DOUBLE, `ic5` DOUBLE, `nc5` DOUBLE, `co2` DOUBLE, `h2` DOUBLE, `h2s1` DOUBLE, `h2s2` DOUBLE, `h2s3` DOUBLE, `h2s4` DOUBLE, `dcexp` DOUBLE, `dexp` DOUBLE, `gainlosstot` DOUBLE, `bitspeed` DOUBLE) TAGS (`jth` VARCHAR(80), `bzjh` VARCHAR(20), `etloilfield` VARCHAR(20))");
-  action("CREATE TABLE `t_lj_JS2025012601` USING `rt_logging` (`jth`, `bzjh`, `etloilfield`) TAGS (\"JS2025012601\", \"天105斜\", \"207\")");
-  action("insert into `t_lj_JS2025012601`(time, depth) values(now,1)");
+  system("taos -f ../../../utils/test/c/stb1_sql");
+  system("taos -f ../../../utils/test/c/tb1_sql");
+//  action("CREATE STABLE `rt_logging` (`time` TIMESTAMP, `depth` DOUBLE, `tvb` DOUBLE, `lagdepth` DOUBLE, `bitdep` DOUBLE, `blockpos` DOUBLE, `timedist_inst` DOUBLE, `hkload` DOUBLE, `wob` DOUBLE, `spp` DOUBLE, `cup` DOUBLE, `rpm` DOUBLE, `torque` DOUBLE, `tdrpm` DOUBLE, `tdtorque` DOUBLE, `spm1` DOUBLE, `spm2` DOUBLE, `spm3` DOUBLE, `zbc` DOUBLE, `pit1` DOUBLE, `pit2` DOUBLE, `pit3` DOUBLE, `pit4` DOUBLE, `pit5` DOUBLE, `pit6` DOUBLE, `tpit` DOUBLE, `mwi` DOUBLE, `mwo` DOUBLE, `mci` DOUBLE, `mco` DOUBLE, `mti` DOUBLE, `mto` DOUBLE, `mfi` DOUBLE, `flowout` DOUBLE, `gas` DOUBLE, `c1` DOUBLE, `c2` DOUBLE, `c3` DOUBLE, `ic4` DOUBLE, `nc4` DOUBLE, `ic5` DOUBLE, `nc5` DOUBLE, `co2` DOUBLE, `h2` DOUBLE, `h2s1` DOUBLE, `h2s2` DOUBLE, `h2s3` DOUBLE, `h2s4` DOUBLE, `dcexp` DOUBLE, `dexp` DOUBLE, `gainlosstot` DOUBLE, `bitspeed` DOUBLE) TAGS (`jth` VARCHAR(80), `bzjh` VARCHAR(20), `etloilfield` VARCHAR(20))");
+//  action("CREATE TABLE `t_lj_JS2025012601` USING `rt_logging` (`jth`, `bzjh`, `etloilfield`) TAGS (\"JS2025012601\", \"天105斜\", \"207\")");
+//  action("insert into `t_lj_JS2025012601`(time, depth) values(now,1)");
 }
 
 int main(int argc, char* argv[]) {
