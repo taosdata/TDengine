138d296d312bc8e8c65e4aaadf012385668828ff
haoranchen <haoran920c@163.com>
2025-04-14 15:22:17 +0800

    debug: add log in run_case.sh

--- tests/parallel_test/run_case.sh
+++ tests/parallel_test/run_case.sh
@@ -46,6 +46,7 @@ if [ -z "$cmd" ]; then
 fi
 
 if [ $ent -eq 0 ]; then
+    echo "community edition init env"
     export PATH=$PATH:/home/TDengine/debug/build/bin
     export LD_LIBRARY_PATH=/home/TDengine/debug/build/lib
     ln -s /home/TDengine/debug/build/lib/libtaos.so /usr/lib/libtaos.so 2>/dev/null
@@ -57,7 +58,11 @@ if [ $ent -eq 0 ]; then
     ln -s /home/TDengine/include/util/taoserror.h /usr/include/taoserror.h 2>/dev/null
     ln -s /home/TDengine/include/libs/function/taosudf.h /usr/include/taosudf.h 2>/dev/null
     CONTAINER_TESTDIR=/home/TDengine
+    echo "print include file for udftest"
+    ls -l /usr/include/taos* 
+    ls -l 
 else
+    echo "enterprise edition init env"
     export PATH=$PATH:/home/TDinternal/debug/build/bin
     export LD_LIBRARY_PATH=/home/TDinternal/debug/build/lib
     ln -s /home/TDinternal/debug/build/lib/libtaos.so /usr/lib/libtaos.so 2>/dev/null
@@ -69,7 +74,11 @@ else
     ln -s /home/TDinternal/community/include/util/taoserror.h /usr/include/taoserror.h 2>/dev/null
     ln -s /home/TDinternal/community/include/libs/function/taosudf.h /usr/include/taosudf.h 2>/dev/null
     CONTAINER_TESTDIR=/home/TDinternal/community
+    echo "print include file for udftest"
+    ls -l /usr/include/taos* 
+    ls -l 
 fi
+
 mkdir -p /var/lib/taos/subscribe
 mkdir -p /var/log/taos
 mkdir -p /var/lib/taos
