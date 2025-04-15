d9c4ab6ac38664f8abfe7a50787b9afdc9389506
freemine <freemine@yeah.net>
2025-04-15 21:05:45 +0800

    debug(ci): debug udf.sim for the moment

--- tests/script/sh/compile_udf.sh
+++ tests/script/sh/compile_udf.sh
@@ -1,5 +1,9 @@
 set +e
 
+# NOTE: debug for the moment
+date
+ll /tmp/udf/*.so
+
 rm -rf /tmp/udf/libbitand.so /tmp/udf/libsqrsum.so /tmp/udf/libgpd.so
 mkdir -p /tmp/udf
 echo "compile udf bit_and and sqr_sum"
@@ -8,4 +12,10 @@ gcc -fPIC -shared sh/l2norm.c -I../../include/libs/function/ -I../../include/cli
 gcc -fPIC -shared sh/gpd.c -I../../include/libs/function/ -I../../include/client -I../../include/util   -o /tmp/udf/libgpd.so
 echo "debug show /tmp/udf/*.so"
 ls /tmp/udf/*.so
+
+# NOTE: debug for the moment
+
+date
+ll /tmp/udf/*.so
+ldd /tmp/udf/libbitand.so
 
