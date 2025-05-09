ede0acfe5be669ea16609ca647a0ce9223a628e4
dapan1121 <wpan@taosdata.com>
2025-05-09 11:19:32 +0800

    fix: ignore hb send failed issue

--- source/client/src/clientImpl.c
+++ source/client/src/clientImpl.c
@@ -81,6 +81,7 @@ bool chkRequestKilled(void* param) {
 void cleanupAppInfo() {
   taosHashCleanup(appInfo.pInstMap);
   taosHashCleanup(appInfo.pInstMapByClusterId);
+  tscInfo("cluster instance map cleaned");
 }
 
 static int32_t taosConnectImpl(const char* user, const char* auth, const char* db, __taos_async_fn_t fp, void* param,
