0158ad8a54f5b3c3a1c827030e7ada020ea34370
freemine <freemine@yeah.net>
2025-05-10 15:52:35 +0800

    debug(ci): debugging tdengine-build.yml, check hostname and printf ip in hex format

--- source/libs/transport/src/transSvr.c
+++ source/libs/transport/src/transSvr.c
@@ -1536,7 +1536,7 @@ void* transInitServer(uint32_t ip, uint32_t port, char* label, int numOfThreads,
 
   if (false == taosValidIpAndPort(srv->ip, srv->port)) {
     code = TAOS_SYSTEM_ERROR(ERRNO);
-    tError("invalid ip/port, %d:%d since %s", srv->ip, srv->port, terrstr());
+    tError("invalid ip/port, %" PRIx32 ":%d since %s", srv->ip, srv->port, terrstr());
     goto End;
   }
   char pipeName[PATH_MAX];
