ac757656a108a8f760ff67ec5b4f204e28dd0b61
Kaili Xu <klxu@taosdata.com>
2025-05-12 14:26:22 +0800

    enh: grant support data source SparkplugB (#2234)

--- enterprise/src/plugins/grant/src/grantMain.c
+++ enterprise/src/plugins/grant/src/grantMain.c
@@ -196,12 +196,13 @@
 #define GRANT_LOG_MAX_MACHINE 300
 
 static const char gConnName[CONN_TYPE_DYN_MAX][GRANT_ITEM_NAME_LEN] = {
-    "opc_da", "opc_ua", "pi",    "kafka",    "influxdb", "mqtt",  "avevahistorian", "opentsdb",
-    "td2.6",  "td3.0",  "mysql", "postgres", "oracle",   "mssql", "mongodb",        "csv"};
+    "opc_da", "opc_ua",   "pi",     "kafka", "influxdb", "mqtt", "avevahistorian", "opentsdb", "td2.6", "td3.0",
+    "mysql",  "postgres", "oracle", "mssql", "mongodb",  "csv",  "sparkplugb"};
 
 static const char *gConnDisplay[CONN_TYPE_DYN_MAX] = {
-    "OPC_DA",      "OPC_UA",      "Pi",    "Kafka",      "InfluxDB", "MQTT",      "avevaHistorian", "OpenTSDB",
-    "TDengine2.6", "TDengine3.0", "MySQL", "PostgreSQL", "Oracle",   "SqlServer", "MongoDB",        "CSV"};
+    "OPC_DA",         "OPC_UA",    "Pi",          "Kafka",       "InfluxDB",  "MQTT",
+    "avevaHistorian", "OpenTSDB",  "TDengine2.6", "TDengine3.0", "MySQL",     "PostgreSQL",
+    "Oracle",         "SqlServer", "MongoDB",     "CSV",         "SparkplugB"};
 
 static const char gGrantName[GRANT_OPT_DYN_MAX][GRANT_ITEM_NAME_LEN] = {
     "basic",         "service",      "stream",        "subscription",   "audit",
@@ -482,6 +483,9 @@ static void grantInitShowFlags() {
 #endif
 #if !defined(TD_INDUSTRY) || defined(TD_DATAIN_CSV)
   grantHandle.showDataIns[CONN_TYPE_CSV] = 1;
+#endif
+#if !defined(TD_INDUSTRY) || defined(TD_DATAIN_SPARKPLUGB)
+  grantHandle.showDataIns[CONN_TYPE_SPARKPLUGB] = 1;
 #endif
 
   // add future datains here ...
