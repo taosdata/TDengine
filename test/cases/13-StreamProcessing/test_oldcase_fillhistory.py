70fd1a8005727848619f5db7540cbbf98cc2ea06
Simon Guan <guanshengliang@qq.com>
2025-05-15 20:54:40 +0800

    test: create null files

---
+++ test/cases/13-StreamProcessing/test_oldcase_fillhistory.py
@@ -0,0 +1,81 @@
+import time
+from new_test_framework.utils import tdLog, tdSql, sc, clusterComCheck
+
+
+class TestStreamOldCaseBasic:
+
+    def setup_class(cls):
+        tdLog.debug(f"start to execute {__file__}")
+
+    def test_stream_oldcase_basic(self):
+        """Stream basic test
+
+        1. basic test
+        2. out of order data
+
+        Catalog:
+            - Streams:OldCase
+        Since: v3.0.0.0
+        Labels: common,ci
+        Jira: None
+        History:
+            - 2025-5-15 Simon Guan Migrated from tsim/stream/fillHistoryBasic1.sim
+            - 2025-5-15 Simon Guan Migrated from tsim/stream/fillHistoryBasic2.sim
+            - 2025-5-15 Simon Guan Migrated from tsim/stream/fillHistoryBasic3.sim
+            - 2025-5-15 Simon Guan Migrated from tsim/stream/fillHistoryBasic4.sim
+            - 2025-5-15 Simon Guan Migrated from tsim/stream/fillHistoryTransform.sim
+            - 2025-5-15 Simon Guan Migrated from tsim/stream/fillIntervalDelete0.sim
+            - 2025-5-15 Simon Guan Migrated from tsim/stream/fillIntervalDelete1.sim
+            - 2025-5-15 Simon Guan Migrated from tsim/stream/fillIntervalLinear.sim
+            - 2025-5-15 Simon Guan Migrated from tsim/stream/fillIntervalPartitionBy.sim
+            - 2025-5-15 Simon Guan Migrated from tsim/stream/fillIntervalPrevNext.sim
+            - 2025-5-15 Simon Guan Migrated from tsim/stream/fillIntervalPrevNext1.sim
+            - 2025-5-15 Simon Guan Migrated from tsim/stream/fillIntervalRange.sim
+            - 2025-5-15 Simon Guan Migrated from tsim/stream/fillIntervalValue.sim
+        """
+
+        # self.stream_basic_0()
+        # self.stream_basic_1()
+        # self.stream_basic_2()
+        # self.stream_basic_3()
+        self.stream_basic_4()
+        # self.stream_basic_5()
+
+
+
+def check_stream_status(stream_name=""):
+    for loop in range(60):
+        if stream_name == "":
+            tdSql.query(f"select * from information_schema.ins_stream_tasks")
+            if tdSql.getRows() == 0:
+                continue
+            tdSql.query(
+                f'select * from information_schema.ins_stream_tasks where status != "ready"'
+            )
+            if tdSql.getRows() == 0:
+                return
+        else:
+            tdSql.query(
+                f'select stream_name, status from information_schema.ins_stream_tasks where stream_name = "{stream_name}" and status == "ready"'
+            )
+            if tdSql.getRows() == 1:
+                return
+        time.sleep(1)
+
+    tdLog.exit(f"stream task status not ready in {loop} seconds")
+
+
+def drop_all_streams_and_dbs():
+    dbList = tdSql.query("show databases", row_tag=True)
+    for r in range(len(dbList)):
+        if (
+            dbList[r][0] != "information_schema"
+            and dbList[r][0] != "performance_schema"
+        ):
+            tdSql.execute(f"drop database {dbList[r][0]}")
+
+    streamList = tdSql.query("show streams", row_tag=True)
+    for r in range(len(streamList)):
+        tdSql.execute(f"drop stream {streamList[r][0]}")
+
+    tdLog.info(f"drop {len(dbList)} databases, {len(streamList)} streams")
---
+++ test/cases/13-StreamProcessing/test_oldcase_interp.py
@@ -0,0 +1,82 @@
+import time
+from new_test_framework.utils import tdLog, tdSql, sc, clusterComCheck
+
+
+class TestStreamOldCaseBasic:
+
+    def setup_class(cls):
+        tdLog.debug(f"start to execute {__file__}")
+
+    def test_stream_oldcase_basic(self):
+        """Stream basic test
+
+        1. basic test
+        2. out of order data
+
+        Catalog:
+            - Streams:OldCase
+        Since: v3.0.0.0
+        Labels: common,ci
+        Jira: None
+        History:
+            - 2025-5-15 Simon Guan Migrated from tsim/stream/streamInterpHistory.sim
+            - 2025-5-15 Simon Guan Migrated from tsim/stream/streamInterpHistory1.sim
+            - 2025-5-15 Simon Guan Migrated from tsim/stream/streamInterpLarge.sim
+            - 2025-5-15 Simon Guan Migrated from tsim/stream/streamInterpLinear0.sim
+            - 2025-5-15 Simon Guan Migrated from tsim/stream/streamInterpLinear1.sim
+            - 2025-5-15 Simon Guan Migrated from tsim/stream/streamInterpNext0.sim
+            - 2025-5-15 Simon Guan Migrated from tsim/stream/streamInterpNext1.sim
+            - 2025-5-15 Simon Guan Migrated from tsim/stream/streamInterpOther.sim
+            - 2025-5-15 Simon Guan Migrated from tsim/stream/streamInterpOther1.sim
+            - 2025-5-15 Simon Guan Migrated from tsim/stream/streamInterpOther2.sim
+            - 2025-5-15 Simon Guan Migrated from tsim/stream/streamInterpPrev0.sim
+            - 2025-5-15 Simon Guan Migrated from tsim/stream/streamInterpPrev1.sim
+            - 2025-5-15 Simon Guan Migrated from tsim/stream/streamInterpScalar.sim
+            - 2025-5-15 Simon Guan Migrated from tsim/stream/streamInterpValue0.sim
+            - 2025-5-15 Simon Guan Migrated from tsim/stream/streamInterpValue1.sim
+        """
+
+        # self.stream_basic_0()
+        # self.stream_basic_1()
+        # self.stream_basic_2()
+        # self.stream_basic_3()
+        self.stream_basic_4()
+        # self.stream_basic_5()
+
+
+def check_stream_status(stream_name=""):
+    for loop in range(60):
+        if stream_name == "":
+            tdSql.query(f"select * from information_schema.ins_stream_tasks")
+            if tdSql.getRows() == 0:
+                continue
+            tdSql.query(
+                f'select * from information_schema.ins_stream_tasks where status != "ready"'
+            )
+            if tdSql.getRows() == 0:
+                return
+        else:
+            tdSql.query(
+                f'select stream_name, status from information_schema.ins_stream_tasks where stream_name = "{stream_name}" and status == "ready"'
+            )
+            if tdSql.getRows() == 1:
+                return
+        time.sleep(1)
+
+    tdLog.exit(f"stream task status not ready in {loop} seconds")
+
+
+def drop_all_streams_and_dbs():
+    dbList = tdSql.query("show databases", row_tag=True)
+    for r in range(len(dbList)):
+        if (
+            dbList[r][0] != "information_schema"
+            and dbList[r][0] != "performance_schema"
+        ):
+            tdSql.execute(f"drop database {dbList[r][0]}")
+
+    streamList = tdSql.query("show streams", row_tag=True)
+    for r in range(len(streamList)):
+        tdSql.execute(f"drop stream {streamList[r][0]}")
+
+    tdLog.info(f"drop {len(dbList)} databases, {len(streamList)} streams")
