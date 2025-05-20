2b4c67c653d2beb3fe6c38e7c294ced8835c4cba
yihaoDeng <luomoxyz@126.com>
2025-05-20 10:05:00 +0800

    add test

--- source/libs/scheduler/src/schRemote.c
+++ source/libs/scheduler/src/schRemote.c
@@ -519,7 +519,7 @@ int32_t schHandleDropCallback(void *param, SDataBuf *pMsg, int32_t code) {
 
   if (pMsg->handle == NULL) {
     qError("sch handle is NULL, may be already released and mem lea");
-    ASSERT(0);
+    ASSERT_CORE(0, "pMsg->handle is NULL");
   }
   if (pMsg) {
     taosMemoryFree(pMsg->pData);
