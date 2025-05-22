e93e0f3222bf51a7594255b13b0ca44f76fcee29
yihaoDeng <luomoxyz@126.com>
2025-05-22 15:53:34 +0800

    add feat

--- source/dnode/mnode/impl/src/mndEncKey.c
+++ source/dnode/mnode/impl/src/mndEncKey.c
@@ -573,6 +573,11 @@ int32_t mndRetrieveEncLog(SRpcMsg *pReq, SShowObj *pShow, SSDataBlock *pBlock, i
   return numOfRows;
 }
 
+int32_t mndCheckAKEncPrivilege(SMnode *pMnode, const char *user, EOperType operType, const char *pViewFName) {
+  int32_t code = 0;
+  return code;
+}
+
 int32_t mndProcessAKEncReq(SRpcMsg *pReq) {
   int32_t code = -1;
   SMnode *pMnode = pReq->info.node;
