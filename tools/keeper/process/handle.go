3b54cabc77da5eebc190e8539a028a2682bf688f
sheyanjie-qq <249478495@qq.com>
2025-06-06 17:10:20 +0800

    feat: support forever retry when error in program start

--- tools/keeper/process/handle.go
+++ tools/keeper/process/handle.go
@@ -267,7 +267,7 @@ type Value struct {
 
 func NewProcessor(conf *config.Config) *Processor {
 
-	conn, err := db.NewConnector(conf.TDengine.Username, conf.TDengine.Password, conf.TDengine.Host, conf.TDengine.Port, conf.TDengine.Usessl)
+	conn, err := db.NewConnectorWithRetryForever(conf.TDengine.Username, conf.TDengine.Password, conf.TDengine.Host, conf.TDengine.Port, conf.TDengine.Usessl)
 	if err != nil {
 		panic(err)
 	}
@@ -307,7 +307,7 @@ func (p *Processor) Prepare() {
 
 		err := pool.GoroutinePool.Submit(func() {
 			defer wg.Done()
-			data, err := p.dbConn.Query(p.ctx, fmt.Sprintf("describe %s", p.withDBName(tableName)), util.GetQidOwn(config.Conf.InstanceID))
+			data, err := p.dbConn.QueryWithRetryForever(p.ctx, fmt.Sprintf("describe %s", p.withDBName(tableName)), util.GetQidOwn(config.Conf.InstanceID))
 			if err != nil {
 				var tdEngineError *taosError.TaosError
 				if errors.As(err, &tdEngineError) {
@@ -465,7 +465,7 @@ func (p *Processor) Process() {
 		}
 		sql := b.String()
 		pool.BytesPoolPut(b)
-		data, err := p.dbConn.Query(p.ctx, sql, util.GetQidOwn(config.Conf.InstanceID))
+		data, err := p.dbConn.QueryWithRetryForever(p.ctx, sql, util.GetQidOwn(config.Conf.InstanceID))
 		logger.Debug(sql)
 		if err != nil {
 			logger.WithError(err).Errorln("select data sql:", sql)
