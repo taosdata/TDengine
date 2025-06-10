207dc16c48359f0123736f89bc452490505806f0
menshibin <men_shi_bin@163.com>
2025-06-10 11:37:26 +0800

    docs: update python websocket connector version

--- tests/parallel_test/run_case.sh
+++ tests/parallel_test/run_case.sh
@@ -84,7 +84,7 @@ md5sum /home/TDinternal/debug/build/lib/libtaosnative.so
 
 #get python connector and update: taospy and  taos-ws-py to latest
 pip3 install taospy==2.8.1
-pip3 install taos-ws-py==0.5.1
+pip3 install taos-ws-py==0.5.2
 $TIMEOUT_CMD $cmd
 RET=$?
 echo "cmd exit code: $RET"
