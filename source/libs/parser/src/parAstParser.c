8ac2d095f796fb290e72021783deb16a982997da
Jing Sima <simondominic9997@outlook.com>
2025-06-16 15:47:17 +0800

    feat: [TS-6100] Parse create stream sql: Add more error case and fix bugs.

--- source/libs/parser/src/parAstParser.c
+++ source/libs/parser/src/parAstParser.c
@@ -628,6 +628,7 @@ static int32_t collectMetaKeyFromCreateStream(SCollectMetaKeyCxt* pCxt, SCreateS
   if (strcmp(pStmt->targetTabName, "") != 0) {
     PAR_ERR_RET(reserveTableMetaInCache(pCxt->pParseCxt->acctId, pStmt->targetDbName, pStmt->targetTabName, pCxt->pMetaCache));
     reserveTableVgroupInCache(pCxt->pParseCxt->acctId, pStmt->targetDbName, pStmt->targetTabName, pCxt->pMetaCache);
+    PAR_ERR_RET(reserveDbCfgInCache(pCxt->pParseCxt->acctId, pStmt->targetDbName, pCxt->pMetaCache));
   }
   PAR_ERR_RET(reserveDbCfgInCache(pCxt->pParseCxt->acctId, pStmt->streamDbName, pCxt->pMetaCache));
   SRealTableNode *pTriggerTable = (SRealTableNode*)((SStreamTriggerNode*)pStmt->pTrigger)->pTrigerTable;
