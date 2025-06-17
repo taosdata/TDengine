d1da3b84c89f7b420daef842dfaee7925df3418f
Simon Guan <guanshengliang@qq.com>
2025-06-17 11:36:39 +0800

    test: more

--- test/cases/13-StreamProcessing/31-OldTsimCases/test_oldcase_twa.py
+++ test/cases/13-StreamProcessing/31-OldTsimCases/test_oldcase_twa.py
@@ -99,44 +99,40 @@ class TestStreamOldCaseTwa:
         tdLog.info(f"end")
 
     def streamTwaFwcFill(self):
-        tdLog.info(f"streamTwaFwcFill")
-        tdStream.dropAllStreamsAndDbs()
+        # tdLog.info(f"streamTwaFwcFill")
+        # tdStream.dropAllStreamsAndDbs()
 
-        tdLog.info(f"step1")
-        tdLog.info(f"=============== create database")
-        tdSql.execute(f"create database test vgroups 1;")
-        tdSql.execute(f"use test;")
+        # tdLog.info(f"step1")
+        # tdLog.info(f"=============== create database")
+        # tdSql.execute(f"create database test vgroups 1;")
+        # tdSql.execute(f"use test;")
 
-        tdSql.execute(
-            f"create stable st(ts timestamp, a int, b int, c int) tags(ta int, tb int, tc int);"
-        )
-        tdSql.execute(f"create table t1 using st tags(1, 1, 1);")
-        tdSql.execute(f"create table t2 using st tags(2, 2, 2);")
+        # tdSql.execute(
+        #     f"create stable st(ts timestamp, a int, b int, c int) tags(ta int, tb int, tc int);"
+        # )
+        # tdSql.execute(f"create table t1 using st tags(1, 1, 1);")
+        # tdSql.execute(f"create table t2 using st tags(2, 2, 2);")
 
-        tdSql.execute(
-            f"create stream streams1 interval(2s) sliding(2s) from st partition by tbname, ta options(expired_time(0s)|ignore_disorder) into streamt as select _twstart, twa(a), twa(b), elapsed(ts), now, timezone() from %%trows;"
-        )
+        # tdSql.execute(
+        #     f"create stream streams1 interval(2s) sliding(2s) from st partition by tbname, ta options(expired_time(0s)|ignore_disorder) into streamt as select _twstart, twa(a), twa(b), elapsed(ts), now, timezone() from %%trows;"
+        # )
 
-        tdSql.execute(
-            f"insert into t1 values(now +  3s, 1, 1, 1) (now +  4s, 10, 1, 1)  (now + 7s, 20, 2, 2) (now + 8s, 30, 3, 3);"
-        )
-        tdSql.execute(
-            f"insert into t2 values(now +  5s, 1, 1, 1) (now +  6s, 10, 1, 1)  (now + 9s, 20, 2, 2) (now + 10s, 30, 3, 3);"
-        )
+        # tdSql.execute(
+        #     f"insert into t1 values('2025-06-01 00:00:03', 1, 1, 1) ('2025-06-01 00:00:04', 10, 1, 1)  ('2025-06-01 00:00:07', 20, 2, 2) ('2025-06-01 00:00:08', 30, 3, 3) ('2025-06-01 00:00:11', 40, 4, 4) ('2025-06-01 00:00:12', 50, 5, 5);"
+        # )
+        # tdSql.execute(
+        #     f"insert into t2 values('2025-06-01 00:00:05', 1, 1, 1) ('2025-06-01 00:00:06', 10, 1, 1)  ('2025-06-01 00:00:09', 20, 2, 2) ('2025-06-01 00:00:10', 30, 3, 3) ('2025-06-01 00:00:13', 40, 4, 4) ('2025-06-01 00:00:14', 50, 5, 5);"
+        # )
 
-        tdLog.info(f"1 sql select * from streamt where ta == 1;")
-        tdSql.checkResultsByFunc(
-            f"select * from streamt where ta == 1;",
-            lambda: tdSql.getRows() > 0,
-        )
+        # tdSql.checkResultsByFunc(
+        #     f"select * from test.streamt where ta == 1;",
+        #     lambda: tdSql.getRows() == 5,
+        # )
 
-        tdLog.info(f"2 sql select * from streamt where ta == 2;")
-        tdSql.checkResultsByFunc(
-            f"select * from streamt where ta == 2;",
-            lambda: tdSql.getRows() > 0,
-        )
-        
-        return
+        # tdSql.checkResultsByFunc(
+        #     f"select * from test.streamt where ta == 2;",
+        #     lambda: tdSql.getRows() == 5,
+        # )
 
         tdLog.info(f"step2")
         tdStream.dropAllStreamsAndDbs()
@@ -150,38 +146,31 @@ class TestStreamOldCaseTwa:
         )
         tdSql.execute(f"create table t1 using st tags(1, 1, 1);")
         tdSql.execute(f"create table t2 using st tags(2, 2, 2);")
+
         tdSql.execute(
-            f"create stream streams2 trigger force_window_close IGNORE EXPIRED 1 IGNORE UPDATE 1 into streamt as select _wstart, twa(a), twa(b), elapsed(ts), now, timezone(), ta from st partition by tbname interval(2s) fill(NULL);"
+            f"create stream streams2 period(2s) from st partition by tbname options(expired_time(0s)|ignore_disorder) into streamt as select _tlocaltime, twa(a), twa(b), elapsed(ts), now, timezone(), ta from %%trows;"
         )
 
         tdStream.checkStreamStatus()
 
         tdSql.execute(
-            f"insert into t1 values(now +  3s, 1, 1, 1) (now +  4s, 10, 1, 1)  (now + 7s, 20, 2, 2) (now + 8s, 30, 3, 3);"
+            f"insert into t1 values(now +  1s, 1, 1, 1)(now +  2s, 10, 1, 1)(now + 3s, 20, 2, 2)(now + 4s, 30, 3, 3)(now + 5s, 30, 3, 3)(now + 6s, 30, 3, 3)(now + 6s, 30, 3, 3)(now + 8s, 30, 3, 3)(now + 9s, 30, 3, 3)(now + 10s, 30, 3, 3);"
         )
         tdSql.execute(
-            f"insert into t2 values(now +  4s, 1, 1, 1) (now +  5s, 10, 1, 1)  (now + 8s, 20, 2, 2) (now + 9s, 30, 3, 3);"
+            f"insert into t2 values(now +  1s, 1, 1, 1)(now +  2s, 10, 1, 1)(now + 3s, 20, 2, 2)(now + 4s, 30, 3, 3)(now + 5s, 30, 3, 3)(now + 6s, 30, 3, 3)(now + 6s, 30, 3, 3)(now + 8s, 30, 3, 3)(now + 9s, 30, 3, 3)(now + 10s, 30, 3, 3);"
         )
 
-        tdLog.info(f"sql select * from t1;")
-        tdSql.query(f"select * from t1;")
-
-        tdLog.info(f"sql select * from t2;")
-        tdSql.query(f"select * from t2;")
-
-        time.sleep(2)
-
-        tdLog.info(f"2 sql select * from streamt where ta == 1;")
         tdSql.checkResultsByFunc(
-            f"select * from streamt where ta == 1;",
-            lambda: tdSql.getRows() < 5,
+            f"select * from test2.streamt where ta == 1;",
+            lambda: tdSql.getRows() > 0,
         )
 
-        tdLog.info(f"2 sql select * from streamt where ta == 2;")
         tdSql.checkResultsByFunc(
-            f"select * from streamt where ta == 2;",
-            lambda: tdSql.getRows() < 5,
+            f"select * from test2.streamt where ta == 2;",
+            lambda: tdSql.getRows() > 0,
         )
+
+        return
 
         tdLog.info(f"step3")
         tdStream.dropAllStreamsAndDbs()
