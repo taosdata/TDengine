118b9afa1b3111f0bdcef7a62c294da1c642abd3
yihaoDeng <luomoxyz@126.com>
2025-06-17 10:05:51 +0800

    fix return resp

--- enterprise/src/plugins/privilege/src/privilege.c
+++ enterprise/src/plugins/privilege/src/privilege.c
@@ -339,6 +339,8 @@ int32_t mndSetUserWhiteListDualRsp(SMnode *pMnode, SUserObj *pUser, SGetUserWhit
       TAOS_RETURN(TSDB_CODE_OUT_OF_MEMORY);
     }
     (void)memset(pWhiteListRsp->pWhiteListsDual, 0, pWhiteListRsp->numWhiteLists * sizeof(SIpRange));
+    pWhiteListRsp->pWhiteListsDual[0].type = 1;
+    pWhiteListRsp->pWhiteListsDual[1].type = 1;
   }
   TAOS_RETURN(0);
 
