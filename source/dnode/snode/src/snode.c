703a345cc54ec4269b716cc8ba0cc6514db4c224
dapan1121 <wpan@taosdata.com>
2025-06-27 15:43:01 +0800

    fix: remove assert

--- source/dnode/snode/src/snode.c
+++ source/dnode/snode/src/snode.c
@@ -258,9 +258,6 @@ static int32_t handleStreamFetchFromCache(SSnode* pSnode, SRpcMsg* pRpcMsg) {
 _exit:
 
   stsDebug("task %" PRIx64 " TDMT_STREAM_FETCH_FROM_CACHE_RSP with code:%d size:%d", req.taskId, code, (int32_t)size);  
-  if (0 == code)  {
-    assert(size > 0);
-  }
   SRpcMsg rsp = {.code = code, .msgType = TDMT_STREAM_FETCH_FROM_CACHE_RSP, .contLen = size, .pCont = buf, .info = pRpcMsg->info};
   tmsgSendRsp(&rsp);
 
