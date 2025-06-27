eca0fb4d78679f54713636b123be68fd98a18dc9
dmchen <cademfly@hotmail.com>
2025-06-27 10:51:54 +0800

    fix case

--- test/cases/23-Users/test_user_crypted_pass.py
+++ test/cases/23-Users/test_user_crypted_pass.py
@@ -3,34 +3,33 @@ from new_test_framework.utils import tdLog, tdSql, sc, clusterComCheck, clusterC
 
 
 class TestUserPassword:
+    updatecfgDict = {'encryptPassAlgorithm':1}
 
     def setup_class(cls):
         tdLog.debug(f"start to execute {__file__}")
 
     def test_user_password(self):
-        """user password
+        """user crypted password
 
         1. -
 
         Catalog:
             - User
 
-        Since: v3.0.0.0
+        Since: v3.3.7.0
 
         Labels: common,ci
 
-        Jira: None
+        Jira: TS-5619
 
         History:
-            - 2025-4-30 Simon Guan Migrated from tsim/user/password.sim
+            - 2025-6-27 init 
 
         """
         tdLog.info(f"============= step1")
-        tdSql.execute("create encrypt_key '12345678'")
-        time.sleep(3)
-
-        tdLog.info(f"============= step2")
-        tdSql.execute("alter all dnode 'encryptPassAlgorithm' '1'")
+        tdSql.execute("create encrypt_key '12345678'", queryTimes = 0)
+        tdSql.query(f"show encryptions")
+        tdSql.checkRows(1)
 
         tdLog.info(f"============= step2")
         tdSql.execute(f"create user u_test pass 'taosdata'")
@@ -38,6 +37,6 @@ class TestUserPassword:
         tdSql.query(f"show users")
         tdSql.checkRows(2)
 
-        tdLog.info(f"============= step2")
+        tdLog.info(f"============= step3")
         tdLog.info(f"user u_test login")
         tdSql.connect("u_test")
