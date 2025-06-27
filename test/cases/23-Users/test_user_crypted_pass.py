9c1fa79437244ecd4aeb0f1a4cf007d53902c10f
dmchen <cademfly@hotmail.com>
2025-06-27 14:51:07 +0800

    fix case

--- test/cases/23-Users/test_user_crypted_pass.py
+++ test/cases/23-Users/test_user_crypted_pass.py
@@ -26,16 +26,23 @@ class TestUserPassword:
             - 2025-6-27 init 
 
         """
+        time.sleep(3)
+        tdSql.query(f"show dnodes")
+        tdSql.checkRows(1, True)
+        tdSql.checkData(0, 4, "ready")
+
         tdLog.info(f"============= step1")
-        tdSql.execute("create encrypt_key '12345678'", queryTimes = 0)
+        tdSql.execute("create encrypt_key '12345678'", queryTimes = 1, show = True)
+        time.sleep(3)
         tdSql.query(f"show encryptions")
-        tdSql.checkRows(1)
+        tdSql.checkRows(1, True)
+        tdSql.checkData(0, 1, "loaded")
 
         tdLog.info(f"============= step2")
-        tdSql.execute(f"create user u_test pass 'taosdata'", queryTimes = 0)
+        tdSql.execute(f"create user u_test pass 'taosdata'", queryTimes = 1, show = True)
 
         tdSql.query(f"show users")
-        tdSql.checkRows(2)
+        tdSql.checkRows(2, True)
 
         tdLog.info(f"============= step3")
         tdLog.info(f"user u_test login")
