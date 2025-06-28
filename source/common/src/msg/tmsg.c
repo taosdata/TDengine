92692acbaa9c0fe1006be0fad748a7a2b442f673
yihaoDeng <luomoxyz@126.com>
2025-06-28 15:24:40 +0800

    make ci happy

--- source/common/src/msg/tmsg.c
+++ source/common/src/msg/tmsg.c
@@ -600,7 +600,6 @@ int32_t tDeserializeSClientHbBatchReq(void *buf, int32_t bufLen, SClientHbBatchR
       TAOS_CHECK_EXIT(tDeserializeIpRange(&decoder, (SIpRange *)&pReq->userDualIp));
     }
   }
-
   tEndDecode(&decoder);
 
 _exit:
