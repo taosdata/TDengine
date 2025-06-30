05aa896353dfa022cfc0b980a6ab0e82f91eda61
minhuinie <nminhui@163.com>
2025-06-30 18:38:10 +0800

    stop taosd process if using --clean

--- test/new_test_framework/taostest/components/taosd.py
+++ test/new_test_framework/taostest/components/taosd.py
@@ -349,6 +349,27 @@ class TaosD:
                             killCmd = [
                                 f"ps -ef | grep -wi {nodeDict['name']} | grep {i['config_dir']} | grep -v grep | awk '{{print $2}}' | xargs kill -9 > /dev/null 2>&1"]
                             self._remote.cmd(fqdn, killCmd)
+
+                    if "system" in i.keys() and i["system"].lower() == "darwin":
+                        stop_service_cmd = "launchctl unload /Library/LaunchDaemons/com.taosdata.taosd.plist"
+                    else:
+                        stop_service_cmd = "systemctl is-active taosd && systemctl stop taosd || true"
+                    self.logger.debug(f"Executing stop command on {fqdn}: {stop_service_cmd}")
+                    self._remote.cmd(fqdn, [stop_service_cmd])
+
+                    get_and_kill_cmd = (
+                        "ps -ef | grep '[t]aosd' | grep -v grep | awk '{print $2}' | "
+                        "while read pid; do "
+                        "if kill -0 $pid 2>/dev/null; then "
+                        "echo Killing taosd process with PID: $pid; "
+                        "kill -9 $pid; "
+                        "fi; "
+                        "done"
+                    )
+                    self.logger.debug(f"Executing get and kill command on {fqdn}: {get_and_kill_cmd}")
+                    output=self._remote.cmd(fqdn, [get_and_kill_cmd])
+                    self.logger.info(f"Kill log on {fqdn}:{output}")
+                    
                     if self.taosd_valgrind and not self.taosc_valgrind:
                         killCmd = [
                             "ps -ef|grep -wi valgrind.bin | grep -v grep | awk '{print $2}' | xargs kill -9 > /dev/null 2>&1"]
