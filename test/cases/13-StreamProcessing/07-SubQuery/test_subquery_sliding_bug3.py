dde1f32cce6ee95b8e5be2fab958ec4a3477fdaf
Simon Guan <slguan@taosdata.com>
2025-06-30 12:51:42 +0800

    test: report bugs (#31541)

--- test/cases/13-StreamProcessing/07-SubQuery/test_subquery_sliding_bug3.py
+++ test/cases/13-StreamProcessing/07-SubQuery/test_subquery_sliding_bug3.py
@@ -106,11 +106,11 @@ class TestStreamDevBasic:
     def createStreams(self):
         self.streams = []
         stream = StreamItem(
-            id=2,
-            stream="create stream rdb.s2 interval(5m) sliding(5m) from tdb.triggers partition by tbname into rdb.r2 as select _twstart ts, _twend te, _twduration td, _twrownum tw, _tgrpid tg, _tlocaltime tl, tbname tb, count(cint) c1, avg(cint) c2 from qdb.meters where cts >= _twstart and cts < _twend and _twduration is not null and _twrownum is not null and _tgrpid is not null and _tlocaltime is not null partition by tbname",
-            res_query="select ts, te, td, c1, tag_tbname from rdb.r2 where tag_tbname='t1';",
-            exp_query="select _wstart ts, _wend te, _wduration td, count(cint) c1, 't1' from qdb.t1 where cts >= '2025-01-01 00:00:00' and cts < '2025-01-01 00:35:00' interval(5m);",
-            check_func=self.check2,
+            id=46,
+            stream="create stream rdb.s46 interval(5m) sliding(5m) from tdb.triggers into rdb.r46 as select _twstart ts, count(c1) ccnt, sum(c2) csum, first(id) cfirst from %%trows",
+            res_query="select ts, ccnt, csum, cfirst from rdb.r46 limit 1",
+            exp_query="select ts, 1, 0, 1 from tdb.t1 where ts='2025-01-01 00:00:00.000'",
+            check_func=self.check46,
         )
         self.streams.append(stream)
 
@@ -118,39 +118,17 @@ class TestStreamDevBasic:
         for stream in self.streams:
             stream.createStream()
 
-    def check2(self):
-        tdSql.checkTableType(
-            dbname="rdb",
-            stbname="r2",
-            columns=9,
-            tags=1,
-        )
-        tdSql.checkTableSchema(
-            dbname="rdb",
-            tbname="r2",
-            schema=[
-                ["ts", "TIMESTAMP", 8, ""],
-                ["te", "TIMESTAMP", 8, ""],
-                ["td", "BIGINT", 8, ""],
-                ["tw", "BIGINT", 8, ""],
-                ["tg", "BIGINT", 8, ""],
-                ["tl", "TIMESTAMP", 8, ""],
-                ["tb", "VARCHAR", 270, ""],
-                ["c1", "BIGINT", 8, ""],
-                ["c2", "DOUBLE", 8, ""],
-                ["tag_tbname", "VARCHAR", 270, "TAG"],
-            ],
-        )
+    def check46(self):
         tdSql.checkResultsByFunc(
-            sql="select * from information_schema.ins_tags where db_name='rdb' and stable_name='r2' and tag_name='tag_tbname' and (tag_value='t1' or tag_value='t2');",
-            func=lambda: tdSql.getRows() == 2,
-        )
-        tdSql.checkResultsByFunc(
-            sql="select ts, te, td, c1, tag_tbname from rdb.r2 where tag_tbname='t2'",
-            func=lambda: tdSql.getRows() == 1
-            and tdSql.compareData(0, 0, "2025-01-01 00:10:00.000")
-            and tdSql.compareData(0, 1, "2025-01-01 00:15:00.000")
-            and tdSql.compareData(0, 2, 300000)
-            and tdSql.compareData(0, 3, 10)
-            and tdSql.compareData(0, 4, "t2"),
+            sql="select ts, ccnt, csum from rdb.r46;",
+            func=lambda: tdSql.getRows() == 7
+            and tdSql.compareData(0, 0, "2025-01-01 00:00:00.000")
+            and tdSql.compareData(0, 1, 1)
+            and tdSql.compareData(0, 2, 0)
+            and tdSql.compareData(1, 0, "2025-01-01 00:05:00.000")
+            and tdSql.compareData(1, 1, 1)
+            and tdSql.compareData(1, 2, 50)
+            and tdSql.compareData(6, 0, "2025-01-01 00:30:00.000")
+            and tdSql.compareData(6, 1, 2)
+            and tdSql.compareData(6, 2, 620),
         )
