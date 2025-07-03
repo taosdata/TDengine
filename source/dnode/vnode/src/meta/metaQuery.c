e63026f58cf0427be86b40ec01bef2c92e56bf4b
yihaoDeng <luomoxyz@126.com>
2025-07-03 14:41:04 +0800

    use no-default-index

--- source/dnode/vnode/src/meta/metaQuery.c
+++ source/dnode/vnode/src/meta/metaQuery.c
@@ -16,7 +16,6 @@
 #include "meta.h"
 #include "osMemory.h"
 #include "tencode.h"
-#include "tmsg.h"
 
 void _metaReaderInit(SMetaReader *pReader, void *pVnode, int32_t flags, SStoreMeta *pAPI) {
   SMeta *pMeta = ((SVnode *)pVnode)->pMeta;
