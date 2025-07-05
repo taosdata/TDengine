3e58874cf136f1fc9fc27bde0d12d748b3e29936
Simon Guan <guanshengliang@qq.com>
2025-07-05 15:30:53 +0800

    test: more

--- test/cases/13-StreamProcessing/07-SubQuery/test_subquery_sliding.py
+++ test/cases/13-StreamProcessing/07-SubQuery/test_subquery_sliding.py
@@ -506,7 +506,7 @@ class TestStreamSubquerySliding:
             res_query="select ts, cts, tt, cint, cuint, cbigint, cubigint, cfloat, cdouble, cvarchar, csmallint, cusmallint, ctinyint, cutinyint, cbool, cnchar, cvarbinary from rdb.r43 where id = 1 limit 1 offset 1",
             exp_query="select cts, cts, 1, cint, cuint, cbigint, cubigint, cfloat, cdouble, cvarchar, csmallint, cusmallint, ctinyint, cutinyint, cbool, cnchar, cvarbinary from qdb.v1 where cts = '2025-01-01 00:05:00.000';",
         )
-        # self.streams.append(stream) cases/13-StreamProcessing/07-SubQuery/test_subquery_sliding_bug6.py
+        self.streams.append(stream)
 
         stream = StreamItem(
             id=44,
@@ -1138,7 +1138,7 @@ class TestStreamSubquerySliding:
             res_query="select * from rdb.r122 where tag_tbname='t1';",
             exp_query="select first(tats), last(tbts), count(tat1), sum(tat1), first(tbt1), last(tbt1), 't1' from (select ta.cts tats, tb.cts tbts, ta.cint tat1, tb.cint tbt1 from qdb.t1 ta left join qdb.t2 tb on ta.cts=tb.cts and (ta.cint >= tb.cint) order by ta.cts) where tats >= '2025-01-01 00:00:00.000' and tats < '2025-01-01 00:35:00.000' interval(5m);",
         )
-        self.streams.append(stream)
+         # self.streams.append(stream) cases/13-StreamProcessing/07-SubQuery/test_subquery_sliding_bug7 and bug5.py
 
         stream = StreamItem(
             id=123,
