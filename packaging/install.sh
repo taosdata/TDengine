1f97c636a50c3bc8ddcfe93049905afa7027a7ad
huohong <346479823@qq.com>
2025-07-08 15:07:17 +0800

    Revert "dev(packaging): support TSDB and explorer package (#3043) (#3063)" (#3070)
    This reverts commit e63d3cf95512060bade351409d9157deb8513f5c.

--- packaging/install.sh
+++ packaging/install.sh
@@ -13,7 +13,6 @@ csudo=""
 explorerEndpoint=""
 
 target=""
-verNumber=3.0
 
 if command -v sudo >/dev/null; then
   csudo="sudo "
@@ -70,17 +69,6 @@ elif echo $osinfo | grep -qwi "Linx"; then
   service_mod=0
   initd_mod=0
   SERVICE_CONFIG_DIR="/etc/systemd/system"
-elif echo `uname` | grep -qwi "Darwin"; then
-  os_type=3
-  if [ -d "/usr/local/Cellar/" ]; then
-    TAOSX_ROOT_DIR="/usr/local/Cellar/tdengine/${verNumber}"
-  elif [ -d "/opt/homebrew/Cellar/" ]; then
-    TAOSX_ROOT_DIR="/opt/homebrew/Cellar/tdengine/${verNumber}"
-  else
-    TAOSX_ROOT_DIR="/usr/local/taos"
-  fi
-  INSTALL_DIR="/usr/local/bin"
-  SERVICE_CONFIG_DIR="/Library/LaunchDaemons"
 else
   echo " osinfo: ${osinfo}"
   echo " This is an officially unverified linux system,"
@@ -118,17 +106,7 @@ stop_taosx_agent_service(){
     fi
 }
 
-stop_explorer_service(){
-  if echo `uname` | grep -qwi "Darwin"; then
-    service=com.tdengine.taos-explorer
-    explore_service_config=${SERVICE_CONFIG_DIR}/${service}.plist
-    if [ -e "$explore_service_config" ]; then
-      ${csudo}launchctl stop ${service}
-      ${csudo}launchctl unload -w ${explore_service_config} || :
-      ${csudo}launchctl remove ${service} || :
-      ${csudo}rm ${explore_service_config} >/dev/null 2>&1 || :
-    fi
-  else
+stop_explore_service(){
     explore_service_config="${SERVICE_CONFIG_DIR}/${explorerName}.service"
     if [ -e "$explore_service_config" ]; then
       if systemctl is-active --quiet ${explorerName}; then
@@ -138,7 +116,6 @@ stop_explorer_service(){
       ${csudo}systemctl disable ${explorerName} &>/dev/null || echo &>/dev/null
       ${csudo}rm -f ${explore_service_config}
     fi
-  fi
 }
 
 
@@ -149,7 +126,7 @@ remove_taosx() {
         ${csudo}rm -rf ${INSTALL_DIR}/${xName}
     fi
     if [ -f ./bin/${explorerName} ]; then
-        stop_explorer_service
+        stop_explore_service
         ${csudo}rm -rf ${INSTALL_DIR}/${explorerName}
     fi
     if [ -f ./bin/${agentname} ]; then
@@ -158,13 +135,6 @@ remove_taosx() {
     fi
     ${csudo}rm -rf ${TAOSX_ROOT_DIR}/plugins
     ${csudo}rm -rf ${TAOSX_ROOT_DIR}/uninstall.sh
-}
-
-remove_explorer() {
-    if [ -f ./bin/${explorerName} ]; then
-        stop_explorer_service
-        ${csudo}rm -rf ${INSTALL_DIR}/${explorerName}
-    fi
 }
 
 # remove taosx-agent
@@ -180,8 +150,6 @@ remove_taos_agent() {
 remove_target() {
     if [ "$target" = ${xName} ]; then
       remove_taosx
-    elif  [ "$target" = ${explorerName} ]; then
-      remove_explorer
     else
       remove_taos_agent
     fi
@@ -197,14 +165,8 @@ print_tips(){
       echo -e "\033[32mTo start ${explorerName}      \033[0m: sudo systemctl start ${explorerName}"
 
       echo -e "\n\033[32mtaosX and taosExplorer are installed successfully!\033[0m"
-      echo -e "\033[32mTo access the TDengine TSDB management system: http://`hostname`:6060\033[0m"
-      echo -e "\033[32mTo read the TDengine TSDB user manual: http://`hostname`:6060/docs-en\033[0m"
-    elif [ "$target" = ${explorerName} ]; then
-      echo -e "\033[32mTo configure ${explorerName}  \033[0m: edit /etc/${PREFIX}/explorer.toml"
-      echo -e "\033[32mTo start ${explorerName}      \033[0m: sudo systemctl start ${explorerName}"
-      echo -e "\n\033[32mtaosExplorer are installed successfully!\033[0m"
-      echo -e "\033[32mTo access the TDengine TSDB management system: http://`hostname`:6060\033[0m"
-      echo -e "\033[32mTo read the TDengine TSDB user manual: http://`hostname`:6060/docs-en\033[0m"
+      echo -e "\033[32mTo access the TDengine management system: http://`hostname`:6060\033[0m"
+      echo -e "\033[32mTo read the TDengine user manual: http://`hostname`:6060/docs-en\033[0m"
     else
       # echo -e "\033[32mTo configure taosx         \033[0m: edit /etc/taos/taosx.toml"
       echo -e "\033[32mTo configure ${agentname}   \033[0m: edit /etc/${PREFIX}/agent.toml"
@@ -225,10 +187,7 @@ getUserInputEndpoint() {
     echo -n "Please enter fqdn or ip: "
     read endpoint
     if [ -z "$endpoint" ]; then
-      #echo "You need to enter explorer‘s fqdn or IP address!"
-      explorerEndpoint="localhost"
-      echo "You have set explorer's fqdn or ip:${explorerEndpoint}"
-      return
+      echo "You need to enter explorer‘s fqdn or IP address!"
     else
       explorerEndpoint="$endpoint"
       echo "You have set explorer's fqdn or ip:${explorerEndpoint}"
@@ -240,11 +199,16 @@ getUserInputEndpoint() {
 function replaceExplorerEndpoint() {
   local FileName=$1
   if [ -f "$FileName" ]; then
-    if echo `uname` | grep -qwi "Darwin"; then
-      ${csudo}sed -i '' "s/localhost/${explorerEndpoint}/g" $FileName
-    else
       ${csudo}sed -i "s/localhost/${explorerEndpoint}/g" $FileName
-    fi
+  fi
+}
+
+# install new taosx and taosx-agent
+install_taosx() {
+  if [ "$target" = ${xName} ]; then
+    install_taosx_only
+  else
+    install_agent_only
   fi
 }
 
@@ -294,48 +258,7 @@ install_taosx_only() {
     print_tips
 }
 
-install_explorer_only(){
-    echo "install starting..."
-    echo "install binary files to ${INSTALL_DIR}..."
-    ${csudo}cp -fr bin/* ${INSTALL_DIR}
-    check_and_create_directory ${TAOSX_ROOT_DIR}
-    ${csudo}cp uninstall_explorer.sh ${TAOSX_ROOT_DIR}
-    echo "install services to ${SERVICE_CONFIG_DIR}..."
-
-    if echo `uname` | grep -qwi "Darwin"; then
-      if [ -f etc/launchd/com.tdengine.taos-explorer.plist ]; then
-        ${csudo}launchctl unload -w ${SERVICE_CONFIG_DIR}/com.tdengine.taos-explorer.plist
-        ${csudo}cp -fr etc/launchd/* ${SERVICE_CONFIG_DIR}
-        ${csudo}launchctl load -w ${SERVICE_CONFIG_DIR}/com.tdengine.taos-explorer.plist || :
-      fi
-    else
-      ${csudo}cp -fr etc/systemd/system/* ${SERVICE_CONFIG_DIR}
-      ${csudo}systemctl daemon-reload
-      explore_service_config="${SERVICE_CONFIG_DIR}/${explorerName}.service"
-      if [ -e "$explore_service_config" ]; then
-        ${csudo}systemctl enable ${explorerName}
-      fi
-
-      ${csudo}systemctl daemon-reload
-    fi
-
-    check_and_create_directory "${CONFIG_DIR}"
-    getUserInputEndpoint
-    # copy config to /etc/taos
-    echo "install toml file to ${CONFIG_DIR}..."
-    if [ -f ./etc/taos/explorer.toml ]; then
-        if [ -f ${CONFIG_DIR}/explorer.toml ]; then
-            ${csudo}cp -f ./etc/taos/explorer.toml ${CONFIG_DIR}/explorer.toml.new
-            replaceExplorerEndpoint ${CONFIG_DIR}/explorer.toml.new
-        else
-            ${csudo}cp -f ./etc/taos/explorer.toml ${CONFIG_DIR}/
-            replaceExplorerEndpoint ${CONFIG_DIR}/explorer.toml
-        fi
-    fi
-    print_tips
-}
-
-install_agent_only(){
+install_agent_only() {
   echo "install starting..."
   echo "install binary files to ${INSTALL_DIR}..."
   ${csudo}cp -fr bin/* ${INSTALL_DIR}
@@ -354,17 +277,6 @@ install_agent_only(){
       ${csudo}cp -f ./etc/taos/agent.toml ${CONFIG_DIR}/agent.toml.new
   else
       [ -e ./etc/taos/agent.toml ] && ${csudo}cp -f ./etc/taos/agent.toml ${CONFIG_DIR}/
-  fi
-}
-
-# install new taosx and taosx-agent
-install_target() {
-  if [ "$target" = ${xName} ]; then
-      install_taosx_only
-  elif  [ "$target" = ${explorerName} ]; then
-      install_explorer_only
-  else
-      install_agent_only
   fi
 }
 
@@ -403,10 +315,8 @@ while getopts "e:" arg; do
 done
 
 check_install_env(){
-  if  [ "$target" != ${explorerName} ]; then
     echo "Check Java env for InfluxDB/OpenTSDB Connector"
     check_java_env
-  fi
 }
 check_install_env
 
@@ -416,12 +326,12 @@ if [ -x ${INSTALL_DIR}/${target} ]; then
     read answer
     if [ X$answer == X"y" ]; then
         remove_target
-        install_target
+        install_taosx
     else
         echo "${target} installation is cancelled"
         exit 1
     fi
 else
     check_and_create_directory "${INSTALL_DIR}"
-    install_target
+    install_taosx
 fi
