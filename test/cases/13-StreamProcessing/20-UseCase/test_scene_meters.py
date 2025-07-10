ddf37a4321b1a3e194caeda0a2c645fcca03cb5d
Alex Duan <417921451@qq.com>
2025-07-10 19:46:48 +0800

    fix: rename case name test_scene_asset01

--- test/cases/13-StreamProcessing/20-UseCase/test_scene_meters.py
+++ test/cases/13-StreamProcessing/20-UseCase/test_scene_meters.py
@@ -1,5 +1,6 @@
 import time
 import math
+import random
 from new_test_framework.utils import tdLog, tdSql, tdStream, etool
 from datetime import datetime
 from datetime import date
@@ -61,7 +62,9 @@ class Test_Scene_Asset01:
     def prepare(self):
         # name
         self.db = "assert01"
+        self.vdb = "tdasset"
         self.stb = "electricity_meters"
+        self.start = 1752563000000
 
         # import data
         etool.taosdump(f"-i cases/13-StreamProcessing/20-UseCase/asset01/data/")
@@ -90,16 +93,66 @@ class Test_Scene_Asset01:
     # 3. insert trigger data
     #
     def insertTriggerData(self):
-        pass
+        # em1-strem1
+        self.trigger_em1_stream1()
+        # em2-stream1
+        self.trigger_em2_stream1()
 
     # 
     # 4. wait stream processing
     #
     def waitStreamProcessing(self):
-        pass
+        tdLog.info("wait for check result sleep 3s ...")
+        time.sleep(3)
 
     # 
     # 5. verify results
     #
     def verifyResults(self):
+        self.verify_em1_stream1()
+        self.verify_em2_stream1()    
+
+
+    # em1-stream1 trigger voltage > 250 start and voltage <= 250 end
+    def trigger_em1_stream1(self):
+
+        # 1~20 minutes no trigger
+        ts = self.start
+        for i in range(20):
+            ts += 1*60*1000
+            sql = f"insert into asset01.`em-1`(ts,voltage) values({ts}, 250);"
+            tdSql.execute(sql, show=True)
+
+        # 20~40 minutes trigger
+        voltage = 251
+        for i in range(20):
+            ts += 1*60*1000
+            voltage += 1
+            sql = f"insert into asset01.`em-1`(ts,voltage) values({ts}, {voltage});"
+            tdSql.execute(sql, show=True)
+
+
+        # 40~60 minutes no triiger with high lower data 
+        for i in range(20):
+            ts += 1*60*1000
+            if i % 2 == 0:
+                voltage = 250 - i
+            else:
+                voltage = 260 + i 
+            sql = f"insert into asset01.`em-1`(ts,voltage) values({ts}, {voltage});"
+            tdSql.execute(sql, show=True)
+
+    # em2-stream1 trigger
+    def trigger_em2_stream1(self):
         pass
+
+
+    # verify em1_stream1
+    def verify_em1_stream1(self):
+        sql = f"select * from {self.vdb}.`result-em1-stream1` "
+        tdSql.waitedQuery(sql, 1, 100)
+        tdLog.info("verify em1_stream1 successfully.")
+
+    # verify em2_stream1
+    def verify_em2_stream1(self):
+        tdLog.info("verify em2_stream1 successfully.")
