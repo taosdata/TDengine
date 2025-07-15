7258a768dd57b9ea945b3bc213ea637f8bcf55c5
kailixu <klxu@taosdata.com>
2025-07-15 20:08:03 +0800

    mounted vnode may have no tq

--- source/dnode/vnode/src/tq/tqRead.c
+++ source/dnode/vnode/src/tq/tqRead.c
@@ -1252,8 +1252,11 @@ void tqReaderRemoveTbUidList(STqReader* pReader, const SArray* tbUidList) {
 }
 
 int32_t tqUpdateTbUidList(STQ* pTq, const SArray* tbUidList, bool isAdd) {
-  if (pTq == NULL || tbUidList == NULL) {
-    return pTq->pVnode->mounted ? 0 : TSDB_CODE_INVALID_PARA;  // mounted vnode may have no tq
+  if (pTq == NULL) {
+    return 0;  // mounted vnode may have no tq
+  }
+  if (tbUidList == NULL) {
+    return TSDB_CODE_INVALID_PARA;
   }
   void*   pIter = NULL;
   int32_t vgId = TD_VID(pTq->pVnode);
