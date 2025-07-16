0e0af2c96b2e89ec518ffea975a70766349df2ab
zyyang90 <zyyang@taosdata.com>
2025-07-16 20:44:46 +0800

    test(stream): tobacco scene e testing #TD-36514

--- test/cases/13-StreamProcessing/20-UseCase/test_idmp_tobacco_bug5.py
+++ test/cases/13-StreamProcessing/20-UseCase/test_idmp_tobacco_bug5.py
@@ -4,5 +4,6 @@ from test_idmp_tobacco import TestIdmpTobaccoImpl
 class TestIdmpTobaccoBug5:
     def test_idmp_tobacco(self):
         tobac = TestIdmpTobaccoImpl()
+        tobac.init()
         tobac.stream_ids = [4]
         tobac.run()
