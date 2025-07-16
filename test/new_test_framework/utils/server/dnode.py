6526ebb1c034d0ef311f9dd3408c2f6d8aac91f2
huohong <sallyhuo@taosdata.com>
2025-07-16 14:07:10 +0800

    fix failed cases

--- test/new_test_framework/utils/server/dnode.py
+++ test/new_test_framework/utils/server/dnode.py
@@ -562,7 +562,7 @@ class TDDnode:
 
         if self.running != 0:
             psCmd = (
-                "ps -ef|grep -w %s| grep -v grep | awk '{print $2}' | xargs"
+                "ps -efww |grep -w %s| grep -v grep | awk '{print $2}' | xargs"
                 % toBeKilled
             )
             processID = (
