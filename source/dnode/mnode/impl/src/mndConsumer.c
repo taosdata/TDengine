7d381d6f6ebf455bb6ee255b7f4aa812e5b49dfc
hongzhenliu335 <ethanliu@taosdata.com>
2025-07-17 16:33:27 +0800

    delete debug log

--- source/dnode/mnode/impl/src/mndConsumer.c
+++ source/dnode/mnode/impl/src/mndConsumer.c
@@ -946,7 +946,7 @@ int32_t mndAcquireConsumer(SMnode *pMnode, int64_t consumerId, SMqConsumerObj** 
   SSdb           *pSdb = pMnode->pSdb;
   *pConsumer = sdbAcquire(pSdb, SDB_CONSUMER, &consumerId);
   if (*pConsumer == NULL) {
-    mError("Can't find the consumer id {%lld}, error: {%s}", consumerId, terrstr());
+    //mError("Can't find the consumer id {%lld}, error: {%s}", consumerId, terrstr());
     mndDumpSdb();
     return TSDB_CODE_MND_CONSUMER_NOT_EXIST;  //TODO::EthanLiu
   }
