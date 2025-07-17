8eb2e55df0bebb4b6608f888fa7b60e6ceae6e58
wangmm0220 <wangmm0220@gmail.com>
2025-07-17 09:16:42 +0800

    fix(stream): add log

--- source/dnode/vnode/src/vnd/vnodeStream.c
+++ source/dnode/vnode/src/vnd/vnodeStream.c
@@ -1898,7 +1898,8 @@ static int32_t vnodeProcessStreamFetchMsg(SVnode* pVnode, SRpcMsg* pMsg) {
   SStreamTriggerReaderCalcInfo* sStreamReaderCalcInfo = taosArrayGetP(calcInfoList, req.execId);
   STREAM_CHECK_NULL_GOTO(sStreamReaderCalcInfo, terrno);
   void* pTask = sStreamReaderCalcInfo->pTask;
-  ST_TASK_DLOG("vgId:%d %s start", TD_VID(pVnode), __func__);
+  ST_TASK_DLOG("vgId:%d %s start, execId:%d, reset:%d, pTaskInfo:%p", TD_VID(pVnode), __func__, req.execId, req.reset,
+               sStreamReaderCalcInfo->pTaskInfo);
 
   if (req.reset || sStreamReaderCalcInfo->pTaskInfo == NULL) {
     qDestroyTask(sStreamReaderCalcInfo->pTaskInfo);
