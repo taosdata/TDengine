1c34138614789e782236f3e9834442ae8af1bd44
zyyang90 <zyyang@taosdata.com>
2025-07-19 17:17:40 +0800

    test(stream): pv scene #TD-36783

--- test/cases/13-StreamProcessing/20-UseCase/test_idmp_pv.py
+++ test/cases/13-StreamProcessing/20-UseCase/test_idmp_pv.py
@@ -4,7 +4,18 @@ from test_idmp_tobacco import TestIdmpScene
 class TestIdmpPhotovoltaic:
     def test_pv(self):
         """
-        Refer:
+        Refer: https://taosdata.feishu.cn/wiki/Zkb2wNkHDihARVkGHYEcbNhmnxb#share-Ygqld907hoMESmx04GBcRlaVnZz
+        1. 测试 AI 推荐生成的分析, 创建 Stream, 验证流的正确性
+        2. 测试不同的触发类型
+        1. 滑动窗口: 每 n 分钟计算一次 m 小时内的聚合
+        2. 事件窗口: field 从 start_condition 开始，到 stop_condition 结束
+        3. 会话窗口: 超过 n 分钟没有上报数据
+        4. 计数窗口: 连续 n 次采集数据
+        3. 不同类型的聚合函数：
+        1. AVG: 平均值
+        2. LAST: 最新值
+        3. SUM: 求和
+        4. MAX: 最大值
         Catalog:
             - Streams:UseCases
         Since: v3.3.6.14
