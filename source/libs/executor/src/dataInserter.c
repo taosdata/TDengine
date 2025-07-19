521490f832767f5087c08c9dd2d4d3cbc51ed986
Tony Zhang <tonyzhang@taosdata.com>
2025-07-19 19:06:25 +0800

    fix: stream result row data inconsistency error

--- source/libs/executor/src/dataInserter.c
+++ source/libs/executor/src/dataInserter.c
@@ -889,7 +889,7 @@ int32_t buildSubmitReqFromBlock(SDataInserterHandle* pInserter, SSubmitReq2** pp
 
   if (needSortMerge) {
     if ((tRowSort(tbData.aRowP) != TSDB_CODE_SUCCESS) ||
-        (terrno = tRowMerge(tbData.aRowP, (STSchema*)pTSchema, 0)) != 0) {
+        (terrno = tRowMerge(tbData.aRowP, (STSchema*)pTSchema, PREFER_NON_NULL)) != 0) {
       goto _end;
     }
   }
