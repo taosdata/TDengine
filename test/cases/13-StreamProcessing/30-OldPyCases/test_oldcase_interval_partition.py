20177f78ede9300a51c368c86e67df25d8e0a49e
zyyang90 <zyyang@taosdata.com>
2025-07-24 10:05:11 +0800

    test(stream): migrate old case #TD-36887

--- test/cases/13-StreamProcessing/30-OldPyCases/test_oldcase_interval_partition.py
+++ test/cases/13-StreamProcessing/30-OldPyCases/test_oldcase_interval_partition.py
@@ -4,26 +4,26 @@ import random
 import time
 
 
-class TestSlindingPartition:
+class TestIntervalPartition:
 
     @pytest.mark.parametrize(
-        "sliding,partition_by",
+        "interval,partition_by",
         [(10, "tbname"), (10, "t1"), (10, "t2"), (10, "t1,t2")],
     )
-    def test_sliding_partition(self, sliding, partition_by):
+    def test_interval_partition(self, interval, partition_by):
         """迁移老用例
 
         老用例 tests/system-test/8-stream/partition_interval.py
         老的建流语句
         CREATE STREAM xxx INTO xxx AS SELECT _wstart,count(val) FROM stb PARTITION BY tbname INTERVAL(10s)
         新的建流语句
-        CREATE STREAM xxx SLINDING(10s) FROM stb PARTITON BY tbname INTO xxx AS SELECT _tcurrent_ts as ts,count(val) FROM %%trows;
+        CREATE STREAM xxx INTERVAL(10s) SLIDING(10s) FROM stb PARTITON BY tbname INTO xxx AS SELECT _tcurrent_ts as ts,count(val) FROM %%trows;
 
         Since: v3.3.7.0
 
         Labels: common,ci
 
-        Jira: https://jira.taosdata.com:18080/browse/TD-36995
+        Jira: https://jira.taosdata.com:18080/browse/TD-36887
 
         History:
             - 2025-07-22: Created by zyyang90
@@ -61,8 +61,7 @@ class TestSlindingPartition:
 
         # create stream
         tdSql.execute(
-            f"CREATE STREAM `{self.stream}` SLIDING({sliding}s) FROM stb PARTITION BY {partition_by} INTO `{self.stream}` AS SELECT _tcurrent_ts as wstart, _tnext_ts as wend, count(val) FROM %%trows;",
-            # f"CREATE STREAM `{self.stream}` INTERVAL({interval}s) SLIDING({interval}s) FROM stb PARTITION BY {partition_by} INTO `{self.stream}` AS SELECT _tcurrent_ts as wstart, _tnext_ts as wend, count(val) FROM %%trows;",
+            f"CREATE STREAM `{self.stream}` INTERVAL({interval}s) SLIDING({interval}s) FROM stb PARTITION BY {partition_by} INTO `{self.stream}` AS SELECT _tcurrent_ts as wstart, _tnext_ts as wend, count(val) FROM %%trows;",
             queryTimes=1,
         )
         tdStream.checkStreamStatus()
@@ -71,9 +70,9 @@ class TestSlindingPartition:
         ts = (int(time.time()) // 60) * 60 * 1000  # 当前时间取整分钟的时间戳
         for t1 in range(5):
             for t2 in ["a", "b", "c", "d"]:
-                for i in range(sliding + 1):
+                for i in range(interval + 1):
                     val = random.randint(1, 100)
-                    sql = f"INSERT INTO t_{t1}_{t2} VALUES ({ts - (sliding - i) * 1000}, {val});"
+                    sql = f"INSERT INTO t_{t1}_{t2} VALUES ({ts - (interval - i) * 1000}, {val});"
                     tdLog.info(f"INSERT SQL: {sql}")
                     tdSql.execute(
                         sql,
@@ -93,31 +92,27 @@ class TestSlindingPartition:
             # 按照 t1 分区，会产生 5 个分区，每个分区会计算 2 次：[0s, 10s] 和 [10s, 20s]
             tdSql.checkResultsByFunc(
                 sql=f"select * from {self.stream} order by t1, wstart;",
-                func=lambda: tdSql.getRows() == 2 * self.t1
-                and tdSql.compareData(0, 2, len(self.t2))
-                and tdSql.compareData(1, 2, len(self.t2) * sliding),
+                func=lambda: tdSql.getRows() == self.t1
+                and tdSql.compareData(0, 2, len(self.t2) * interval),
             )
         if partition_by == "t2":
             # 按照 t2 分区，会产生 4 个分区，每个分区会计算 2 次：[0s, 10s] 和 [10s, 20s]
             tdSql.checkResultsByFunc(
                 sql=f"select * from {self.stream} order by t2, wstart;",
-                func=lambda: tdSql.getRows() == 2 * len(self.t2)
-                and tdSql.compareData(0, 2, self.t1)
-                and tdSql.compareData(1, 2, self.t1 * sliding),
+                func=lambda: tdSql.getRows() == len(self.t2)
+                and tdSql.compareData(0, 2, self.t1 * interval),
             )
         if partition_by == "tbname":
             # 按照 tbname 分区，会产生 5 * 4 个分区，每个分区会计算 2 次：[0s, 10s] 和 [10s, 20s]
             tdSql.checkResultsByFunc(
                 sql=f"select * from {self.stream} order by tag_tbname, wstart;",
-                func=lambda: tdSql.getRows() == 2 * self.t1 * len(self.t2)
-                and tdSql.compareData(0, 2, 1)
-                and tdSql.compareData(1, 2, sliding),
+                func=lambda: tdSql.getRows() == self.t1 * len(self.t2)
+                and tdSql.compareData(0, 2, interval),
             )
         if partition_by == "t1,t2":
             # 按照 t1, t2 分区，会产生 5 * 4 个分区，每个分区会计算 2 次：[0s, 10s] 和 [10s, 20s]
             tdSql.checkResultsByFunc(
                 sql=f"select * from {self.stream} order by t1, t2, wstart;",
-                func=lambda: tdSql.getRows() == 2 * self.t1 * len(self.t2)
-                and tdSql.compareData(0, 2, 1)
-                and tdSql.compareData(1, 2, sliding),
+                func=lambda: tdSql.getRows() == self.t1 * len(self.t2)
+                and tdSql.compareData(0, 2, interval),
             )
