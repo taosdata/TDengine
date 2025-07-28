f1566107211840b5e8431b7c1a9761c95b3a38c6
chenhaoran <haoran920c@163.com>
2025-07-28 20:11:43 +0800

    fix(entrypoint): move snode creation command after serverPort readiness check

--- packaging/docker/bin/entrypoint.sh
+++ packaging/docker/bin/entrypoint.sh
@@ -105,9 +105,6 @@ else
         taosd &
     fi
 fi
-# wait for serverPort ready
-sleep 1s
-sh -c "taos -p'$TAOS_ROOT_PASSWORD' -h $FIRST_EP_HOST -P $FIRST_EP_PORT -s 'create snode on dnode 1;'"
 
 if [ "$DISABLE_ADAPTER" = "0" ]; then
     which taosadapter >/dev/null && taosadapter &
@@ -163,6 +160,8 @@ if [ "$NEEDS_INITDB" = "1" ]; then
 
     touch "${DATA_DIR}/.docker-entrypoint-inited"
 fi
+
+sh -c "taos -p'$TAOS_ROOT_PASSWORD' -h $FIRST_EP_HOST -P $FIRST_EP_PORT -s 'create snode on dnode 1;'"
 
 tail -f /dev/null
 # while true; do sleep 1000; done
