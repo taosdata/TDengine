4df972c5e7281bb58efe44b4667bdf5b1ca29256
Kaili Xu <klxu@taosdata.com>
2025-07-30 14:03:31 +0800

    enh: grant support for data source ORC (#2329)

--- enterprise/src/plugins/grant/inc/machine.h
+++ enterprise/src/plugins/grant/inc/machine.h
@@ -228,9 +228,10 @@ typedef enum {
   CONN_TYPE_MONGODB = 14,
   CONN_TYPE_CSV = 15,
   CONN_TYPE_SPARKPLUGB = 16,
+  CONN_TYPE_ORC = 17,
   // add future data ins here
   // CONN_TYPE_FUTURE_DATA_IN = XX,
-  CONN_TYPE_DYN_MAX = 17,
+  CONN_TYPE_DYN_MAX = 18,
 } EGrantConnType;
 
 #define CONN_TYPE_MAX_V1 6
