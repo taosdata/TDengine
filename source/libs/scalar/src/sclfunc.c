3ac8b66d2b4ab3452b8b3d9a3c891349042e86d9
Tony Zhang <tonyzhang@taosdata.com>
2025-08-12 14:48:02 +0800

    implement date() function

--- source/libs/scalar/src/sclfunc.c
+++ source/libs/scalar/src/sclfunc.c
@@ -2683,9 +2683,54 @@ _return:
   return code;
 }
 
-int32_t toCharFunction(SScalarParam *pInput, int32_t inputNum, SScalarParam *pOutput) {
-  char   *format = taosMemoryMalloc(TS_FORMAT_MAX_LEN);
-  char   *out = taosMemoryCalloc(1, TS_FORMAT_MAX_LEN + VARSTR_HEADER_SIZE);
+int32_t dateFunction(SScalarParam* pInput, int32_t inputNum, SScalarParam* pOutput) {
+  int32_t code = 0;
+  char* format = "yyyy-mm-dd";
+  char* out = taosMemoryCalloc(1, TS_FORMAT_MAX_LEN + VARSTR_HEADER_SIZE);
+  SArray *formats = NULL;
+  if (out == NULL) {
+    SCL_ERR_JRET(terrno);
+  }
+  int64_t precision = 0;
+  GET_TYPED_DATA(precision, int64_t, GET_PARAM_TYPE(&pInput[1]), pInput[1].columnData->pData,
+                typeGetTypeModFromColInfo(&pInput[1].columnData->info));
+
+  for (int32_t rowIdx = 0; rowIdx < pInput[0].numOfRows; ++rowIdx) {
+    if (colDataIsNull_s(pInput[0].columnData, rowIdx)) {
+      colDataSetNULL(pOutput->columnData, rowIdx);
+      continue;
+    }
+
+    int32_t type = GET_PARAM_TYPE(&pInput[0]);
+    char *data = colDataGetData(pInput[0].columnData, rowIdx);
+    int64_t ts = 0;
+    if (IS_VAR_DATA_TYPE(type)) { // datetime format strings
+      int32_t ret = convertStringToTimestamp(type, data, precision, &ts, pInput->tz, pInput->charsetCxt);
+      if (ret != TSDB_CODE_SUCCESS) {
+        colDataSetNULL(pOutput->columnData, rowIdx);
+        continue;
+      }
+    } else if (type == TSDB_DATA_TYPE_TIMESTAMP || type == TSDB_DATA_TYPE_BIGINT || type == TSDB_DATA_TYPE_INT) {
+      GET_TYPED_DATA(ts, int64_t, type, data, typeGetTypeModFromColInfo(&pInput[0].columnData->info));
+    }
+
+    SCL_ERR_JRET(taosTs2Char(format, &formats, ts, precision, varDataVal(out), TS_FORMAT_MAX_LEN, pInput->tz));
+    varDataSetLen(out, strlen(varDataVal(out)));
+    SCL_ERR_JRET(colDataSetVal(pOutput->columnData, rowIdx, out, false));
+  }
+
+_return:
+  if (code) {
+    qError("dateFunction failed since %s", tstrerror(code));
+  }
+  if (formats) taosArrayDestroy(formats);
+  taosMemoryFree(out);
+  return code;
+}
+
+int32_t toCharFunction(SScalarParam* pInput, int32_t inputNum, SScalarParam* pOutput) {
+  char *  format = taosMemoryMalloc(TS_FORMAT_MAX_LEN);
+  char *  out = taosMemoryCalloc(1, TS_FORMAT_MAX_LEN + VARSTR_HEADER_SIZE);
   int32_t len;
   SArray *formats = NULL;
   int32_t code = 0;
