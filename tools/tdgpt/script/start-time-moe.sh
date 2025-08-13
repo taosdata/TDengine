a5de65b69ac96efbfe973a93a2ceae1c071c13ec
huohong <346479823@qq.com>
2025-08-13 11:25:51 +0800

    test: change tdgpt model timer-moe to time-moe

--- tools/tdgpt/script/start-time-moe.sh
+++ tools/tdgpt/script/start-time-moe.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-if [ -d "/var/lib/taos/taosanode/model/time-moe" ]; then
-    echo "Directory /var/lib/taos/taosanode/model/time-moe exists. "
-    echo "Starting time-moe service"
-    cd /var/lib/taos/taosanode/model/time-moe
+if [ -d "/var/lib/taos/taosanode/model/timer-moe" ]; then
+    echo "Directory /var/lib/taos/taosanode/model/timer-moe exists. "
+    echo "Starting timer-moe service"
+    cd /var/lib/taos/taosanode/model/timer-moe
     source /usr/local/taos/taosanode/venv/bin/activate
-    nohup python3 time-moe_server.py --action server &
-    echo "check the pid of the time-moe_server.py to confirm it is running"
-    pid=$(pgrep -f "time-moe_server.py")
-    echo "PID of the time-moe_server.py is $pid"
+    nohup python3 timer-moe_server.py --action server &  
+    echo "check the pid of the timer-moe_server.py to confirm it is running"
+    pid=$(pgrep -f "timer-moe_server.py")
+    echo "PID of the timer-moe_server.py is $pid"
 else
-    echo "Directory /var/lib/taos/taosanode/model/time-moe does not exist."
+    echo "Directory /var/lib/taos/taosanode/model/timer-moe does not exist."
     exit 1
 fi
