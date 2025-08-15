b5cfb4d70e38f6edb14e6e306635433402ff0f07
xiao-77 <berylbao@taosdata.com>
2025-08-15 15:17:34 +0800

    fix: metaGetTableSchema.

--- source/dnode/vnode/src/meta/metaQuery.c
+++ source/dnode/vnode/src/meta/metaQuery.c
@@ -394,11 +394,7 @@ SSchemaWrapper *metaGetTableSchema(SMeta *pMeta, tb_uid_t uid, int32_t sver, int
   if (code) {
     goto _err;
   }
-  if (info.suid == info.uid) {
-    key = (SSkmDbKey){.uid = uid, .sver = info.version};
-  } else {
-    key = (SSkmDbKey){.uid = uid, .sver = info.skmVer};
-  }
+  key = (SSkmDbKey){.uid = uid, .sver = info.skmVer};
   // query from skm db
   if (tdbTbGet(pMeta->pSkmDb, &key, sizeof(SSkmDbKey), &pData, &nData) < 0) {
     goto _err;
