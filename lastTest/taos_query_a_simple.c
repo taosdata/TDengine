268b66a7566997067d8e49dd20f2d89f45e4c488
xiao-77 <berylbao@taosdata.com>
2025-08-20 18:40:56 +0800

    enh: where tag in use tag index.

--- lastTest/taos_query_a_simple.c
+++ lastTest/taos_query_a_simple.c
@@ -184,7 +184,7 @@ int main(int argc, char* argv[]) {
   }
 
   int         query_mode = atoi(argv[1]);
-  const char* lib_path = "../../debug/build/lib/libtaos.so";
+  const char* lib_path = "/root/workspace/TDinternal/debug/build/lib/libtaos.so";
   const char* host = "127.0.0.1";
   const char* user = "root";
   const char* pass = "taosdata";
@@ -229,11 +229,15 @@ int main(int argc, char* argv[]) {
     max_query_nums = 1;
     qps_rate = 1000000;
   } else if (query_mode == 4) {
-    sql = "select channel_id,last(*) from meters where channel_id in ('338068841') partition by channel_id;";
+    sql =
+        "select channel_id,last(*) from meters where channel_id in "
+        "(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,"
+        "40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,"
+        "76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100) partition by channel_id;";
     thread_count = 16;
     queries_per_thread = 100;
     max_query_nums = 5;
-    qps_rate = 1;
+    qps_rate = 100;
   } else {
     printf("Usage: %s <query_mode>\n", argv[0]);
     return 1;
