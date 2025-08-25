d310923f94ea27ba7909699850be1c41fc37e1ca
haoranchen <haoran920c@163.com>
2025-08-25 11:17:37 +0800

    fix(test): remove unnecessary call to sxny_data2 in test_three_gorges_second_case18 (#32718)

--- test/cases/13-StreamProcessing/20-UseCase/test_three_gorges_second_case18.py
+++ test/cases/13-StreamProcessing/20-UseCase/test_three_gorges_second_case18.py
@@ -58,7 +58,6 @@ class Test_ThreeGorges:
         self.createSnodeTest()
         self.createStream()
         self.checkStreamRunning()
-        self.sxny_data2()
         self.dataIn()
         tdSql.checkRowsLoop(500,f"select val,tablename,point, ps_code, cnstationno, index_code from {self.dbname}.{self.outTbname} order by tablename;",200,1)
         self.checkResultWithResultFile()
