d058192807a78ee5ef65615a49b4b81c534ae830
Simon Guan <slguan@taosdata.com>
2025-08-26 15:28:28 +0800

    test: add function case desc (#32755)

--- test/cases/22-Functions/02-Aggregate/test_agg_sum.py
+++ test/cases/22-Functions/02-Aggregate/test_agg_sum.py
@@ -1,21 +1,18 @@
 from new_test_framework.utils import tdLog, tdSql, sc, clusterComCheck
 
 
-class TestFuncMax:
+class TestFuncSum:
 
     def setup_class(cls):
         tdLog.debug(f"start to execute {__file__}")
 
-    def test_func_max(self):
-        """Max 函数
+    def test_func_sum(self):
+        """Agg-basic: Sum
 
-        1. 创建包含一个 Int 普通数据列的超级表
-        2. 创建子表并写入数据
-        3. 对子表执行 Max 查询，包括时间窗口、普通数据列筛选
-        4. 对超级表执行 Max 查询，包括时间窗口、普通数据列筛选、标签列筛选、Group By、Partition By
+        Test the SUM function, including time windows, filtering on ordinary data columns, filtering on tag columns, GROUP BY, and PARTITION BY.
 
         Catalog:
-            - Function:Selection
+            - Function:Aggregate
 
         Since: v3.0.0.0
 
@@ -24,13 +21,13 @@ class TestFuncMax:
         Jira: None
 
         History:
-            - 2025-4-28 Simon Guan Migrated from tsim/compute/max.sim
+            - 2025-4-28 Simon Guan Migrated from tsim/compute/sum.sim
 
         """
 
-        dbPrefix = "m_ma_db"
-        tbPrefix = "m_ma_tb"
-        mtPrefix = "m_ma_mt"
+        dbPrefix = "m_su_db"
+        tbPrefix = "m_su_tb"
+        mtPrefix = "m_su_mt"
         tbNum = 10
         rowNum = 20
         totalNum = 200
@@ -60,78 +57,79 @@ class TestFuncMax:
         i = 1
         tb = tbPrefix + str(i)
 
-        tdSql.query(f"select max(tbcol) from {tb}")
+        tdSql.query(f"select sum(tbcol) from {tb}")
         tdLog.info(f"===> {tdSql.getData(0,0)}")
-        tdSql.checkData(0, 0, 19)
+        tdSql.checkData(0, 0, 190)
 
         tdLog.info(f"=============== step3")
         cc = 4 * 60000
         ms = 1601481600000 + cc
 
-        tdSql.query(f"select max(tbcol) from {tb} where ts <= {ms}")
+        tdSql.query(f"select sum(tbcol) from {tb} where ts <= {ms}")
         tdLog.info(f"===> {tdSql.getData(0,0)}")
-        tdSql.checkData(0, 0, 4)
+        tdSql.checkData(0, 0, 10)
 
         tdLog.info(f"=============== step4")
-        tdSql.query(f"select max(tbcol) as b from {tb}")
+        tdSql.query(f"select sum(tbcol) as b from {tb}")
         tdLog.info(f"===> {tdSql.getData(0,0)}")
-        tdSql.checkData(0, 0, 19)
+        tdSql.checkData(0, 0, 190)
 
         tdLog.info(f"=============== step5")
-        tdSql.query(f"select max(tbcol) as b from {tb} interval(1m)")
+        tdSql.query(f"select sum(tbcol) as b from {tb} interval(1m)")
         tdLog.info(f"===> {tdSql.getData(1,0)}")
         tdSql.checkData(1, 0, 1)
 
-        tdSql.query(f"select max(tbcol) as b from {tb} interval(1d)")
+        tdSql.query(f"select sum(tbcol) as b from {tb} interval(1d)")
         tdLog.info(f"===> {tdSql.getData(0,0)}")
-        tdSql.checkData(0, 0, 19)
+        tdSql.checkData(0, 0, 190)
 
         tdLog.info(f"=============== step6")
         cc = 4 * 60000
         ms = 1601481600000 + cc
 
-        tdSql.query(f"select max(tbcol) as b from {tb} where ts <= {ms} interval(1m)")
+        tdSql.query(f"select sum(tbcol) as b from {tb} where ts <= {ms} interval(1m)")
         tdLog.info(f"===> {tdSql.getData(1,0)}")
         tdSql.checkData(1, 0, 1)
+
         tdSql.checkRows(5)
 
         tdLog.info(f"=============== step7")
-        tdSql.query(f"select max(tbcol) from {mt}")
+        tdSql.query(f"select sum(tbcol) from {mt}")
         tdLog.info(f"===> {tdSql.getData(0,0)}")
-        tdSql.checkData(0, 0, 19)
+        tdSql.checkData(0, 0, 1900)
 
         tdLog.info(f"=============== step8")
         cc = 4 * 60000
         ms = 1601481600000 + cc
 
-        tdSql.query(f"select max(tbcol) as c from {mt} where ts <= {ms}")
+        tdSql.query(f"select sum(tbcol) as c from {mt} where ts <= {ms}")
         tdLog.info(f"===> {tdSql.getData(0,0)}")
-        tdSql.checkData(0, 0, 4)
+        tdSql.checkData(0, 0, 100)
 
-        tdSql.query(f"select max(tbcol) as c from {mt} where tgcol < 5")
+        tdSql.query(f"select sum(tbcol) as c from {mt} where tgcol < 5")
         tdLog.info(f"===> {tdSql.getData(0,0)}")
-        tdSql.checkData(0, 0, 19)
+        tdSql.checkData(0, 0, 950)
 
         cc = 4 * 60000
         ms = 1601481600000 + cc
 
-        tdSql.query(f"select max(tbcol) as c from {mt} where tgcol < 5 and ts <= {ms}")
+        tdSql.query(f"select sum(tbcol) as c from {mt} where tgcol < 5 and ts <= {ms}")
         tdLog.info(f"===> {tdSql.getData(0,0)}")
-        tdSql.checkData(0, 0, 4)
+        tdSql.checkData(0, 0, 50)
 
         tdLog.info(f"=============== step9")
-        tdSql.query(f"select max(tbcol) as b from {mt} interval(1m)")
+        tdSql.query(f"select sum(tbcol) as b from {mt} interval(1m)")
         tdLog.info(f"===> {tdSql.getData(1,0)}")
-        tdSql.checkData(1, 0, 1)
+        tdSql.checkAssert(tdSql.getData(1, 0) >= 5)
 
-        tdSql.query(f"select max(tbcol) as b from {mt} interval(1d)")
+        tdSql.query(f"select sum(tbcol) as b from {mt} interval(1d)")
         tdLog.info(f"===> {tdSql.getData(0,0)}")
-        tdSql.checkData(0, 0, 19)
+        tdSql.checkData(0, 0, 1900)
 
         tdLog.info(f"=============== step10")
-        tdSql.query(f"select max(tbcol) as b from {mt} group by tgcol")
+        tdSql.query(f"select sum(tbcol) as b from {mt} group by tgcol")
         tdLog.info(f"===> {tdSql.getData(0,0)}")
-        tdSql.checkData(0, 0, 19)
+        tdSql.checkData(0, 0, 190)
         tdSql.checkRows(tbNum)
 
         tdLog.info(f"=============== step11")
@@ -139,11 +137,13 @@ class TestFuncMax:
         ms = 1601481600000 + cc
 
         tdSql.query(
-            f"select max(tbcol) as b from {mt}  where ts <= {ms} partition by tgcol interval(1m)"
+            f"select sum(tbcol) as b from {mt}  where ts <= {ms} partition by tgcol interval(1d)"
         )
-        tdLog.info(f"===> {tdSql.getData(1,0)}")
-        tdSql.checkData(1, 0, 1)
-        tdSql.checkRows(50)
+        tdLog.info(
+            f"select sum(tbcol) as b from {mt}  where ts <= {ms} partition by tgcol interval(1d)"
+        )
+        tdSql.checkData(0, 0, 10)
+        tdSql.checkRows(10)
 
         tdLog.info(f"=============== clear")
         tdSql.execute(f"drop database {db}")
