ca6eb0b4ec9c3fb141c9ca4e91f2651a7cb34537
Jinqing Kuang <kuangjinqingcn@gmail.com>
2025-09-04 09:55:16 +0800

    feat(stream): impl state extend in stream trigger

--- source/libs/parser/src/parTranslater.c
+++ source/libs/parser/src/parTranslater.c
@@ -14609,13 +14609,6 @@ static int32_t createStreamReqBulidTriggerExtractCondFromWindow(STranslateContex
       }
       break;
     }
-    case QUERY_NODE_STATE_WINDOW: {
-      PAR_ERR_JRET(extractCondFromStateWindow(pCxt, (SStateWindowNode*)pTriggerWindow, &pLogicCond));
-      if (pLogicCond) {
-        PAR_ERR_JRET(nodesMergeNode(pTriggerFilter, &pLogicCond));
-      }
-      break;
-    }
     default:
       break;
   }
