f0b05e41b76c3dc2baa985e258cb57cd9ded129e
wangmm0220 <wangmm0220@gmail.com>
2025-09-23 19:02:35 +0800

    fix(stream): suid not equal when delete data for child table

--- source/dnode/vnode/src/vnd/vnodeStream.c
+++ source/dnode/vnode/src/vnd/vnodeStream.c
@@ -20,6 +20,7 @@
 #include "osMemory.h"
 #include "scalar.h"
 #include "streamReader.h"
+#include "taosdef.h"
 #include "tarray.h"
 #include "tcommon.h"
 #include "tdatablock.h"
@@ -318,7 +319,7 @@ static int32_t scanDeleteDataNew(SStreamTriggerReaderInfo* sStreamReaderInfo, SS
   req.uidList = taosArrayInit(0, sizeof(tb_uid_t));
   tDecoderInit(&decoder, data, len);
   STREAM_CHECK_RET_GOTO(tDecodeDeleteRes(&decoder, &req));
-  STREAM_CHECK_CONDITION_GOTO(req.suid != sStreamReaderInfo->suid, TDB_CODE_SUCCESS);
+  STREAM_CHECK_CONDITION_GOTO((sStreamReaderInfo->tableType == TSDB_SUPER_TABLE && req.suid != sStreamReaderInfo->suid), TDB_CODE_SUCCESS);
   
   for (int32_t i = 0; i < taosArrayGetSize(req.uidList); i++) {
     uint64_t* uid = taosArrayGet(req.uidList, i);
