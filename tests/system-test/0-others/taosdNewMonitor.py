894459526337fa4b69208363b9189c99c36af81e
Haojun Liao <hjliao@taosdata.com>
2025-09-23 10:52:50 +0800

    fix(test): update test case.

--- tests/system-test/0-others/taosdNewMonitor.py
+++ tests/system-test/0-others/taosdNewMonitor.py
@@ -41,7 +41,7 @@ class RequestHandlerImpl(http.server.BaseHTTPRequestHandler):
         
         # dnode_info  ====================================
 
-        dnode_infos =  ['disk_engine', 'system_net_in', 'vnodes_num', 'system_net_out', 'uptime', 'has_mnode', 'io_read_disk', 'error_log_count',
+        dnode_infos =  ['disk_engine', 'mem_cache_buffer', 'system_net_in', 'vnodes_num', 'system_net_out', 'uptime', 'has_mnode', 'io_read_disk', 'error_log_count',
         'io_read', 'cpu_cores', 'has_qnode', 'has_snode', 'disk_total', 'mem_engine', 'info_log_count', 'cpu_engine', 'io_write_disk',
         'debug_log_count', 'disk_used', 'mem_total', 'io_write', 'masters', 'cpu_system',
         'trace_log_count', 'mem_free']
