181b621e1681f0bfcd2494d354d9230634a4dc29
Alex Duan <417921451@qq.com>
2025-09-24 19:09:03 +0800

    fix: indent for test_fun_agg_histogram.py

--- test/cases/22-Functions/02-Aggregate/test_fun_agg_histogram.py
+++ test/cases/22-Functions/02-Aggregate/test_fun_agg_histogram.py
@@ -584,7 +584,7 @@ class TestHistogram:
             f"insert into {dbname}.{NTBNAME} values ( {NOW - self.rows * int(TIME_STEP * 0.59)}, {null_data} )")
 
     def test_fun_agg_histogram(self):
-       """ Function HISTOGRAM
+        """ Function HISTOGRAM
 
         1. Query on super/child/normal table
         2. Query with bin_type parameter as user_input/linear_bin/log_bin
