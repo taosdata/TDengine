4e882b218b1ca0b4b666eb0ddfb31b3661937dfc
xiao-77 <berylbao@taosdata.com>
2025-09-25 09:25:32 +0800

    feat: add status check on compatibility cases.

--- test/new_test_framework/utils/compatibilityUtil.py
+++ test/new_test_framework/utils/compatibilityUtil.py
@@ -69,18 +69,19 @@ class CompatibilityBase:
     # Modified installTaosd to accept version parameter
     def installTaosdForRollingUpgrade(self, dnodePaths, base_version):
         packagePath = "/usr/local/src/"
-        packageType = "server"
-            
+        
+        # New download URL format
         if platform.system() == "Linux" and platform.machine() == "aarch64":
-            packageName = "TDengine-"+ packageType + "-" + base_version + "-Linux-arm64.tar.gz"
+            packageName = f"tdengine-tsdb-oss-{base_version}-linux-arm64.tar.gz"
+            download_url = f"https://downloads.taosdata.com/tdengine-tsdb-oss/{base_version}/{packageName}"
         else:
-            packageName = "TDengine-"+ packageType + "-" + base_version + "-Linux-x64.tar.gz"
+            packageName = f"tdengine-tsdb-oss-{base_version}-linux-x64.tar.gz"
+            download_url = f"https://downloads.taosdata.com/tdengine-tsdb-oss/{base_version}/{packageName}"
             
-        # Determine download URL
-        download_url = f"https://www.taosdata.com/assets-download/3.0/{packageName}"
         tdLog.info(f"wget {download_url}")
         
-        packageTPath = packageName.split("-Linux-")[0]
+        # Extract package name without extension for installation
+        packageTPath = packageName.replace("-linux-x64.tar.gz", "")
         my_file = Path(f"{packagePath}/{packageName}")
         if not my_file.exists():
             print(f"{packageName} is not exists")
@@ -340,6 +341,7 @@ class CompatibilityBase:
             self.buildTaosd(bPath)
             tdLog.info(f"nohup {bPath}/build/bin/taosd -c {cPaths[0]} > /dev/null 2>&1 &")
             os.system(f"nohup {bPath}/build/bin/taosd -c {cPaths[0]} > /dev/null 2>&1 &")
+        self.checkstatus()
 
     def checkTagSizeAndAlterStb(self,tdsql):
         tdsql.query("select * from information_schema.ins_tags where db_name = 'db_all_insert_mode'")
@@ -606,6 +608,63 @@ class CompatibilityBase:
         os.system(f" LD_LIBRARY_PATH={bPath}/build/lib  {bPath}/build/bin/taosBenchmark -t {tableNumbers} -n {recordNumbers2} -y  ")
         tdsql.query(f"select count(*) from {stb}")
         tdsql.checkData(0,0,tableNumbers*recordNumbers2)
+
+    def checkstatus(self,retry_times=30):
+        tdsql=tdCom.newTdSql()
+        dnodes_ready = False
+        for i in range(retry_times):
+            tdsql.query("show dnodes;")
+            dnode_nums = tdsql.queryRows
+            ready_nums = 0
+
+            for j in range(tdsql.queryRows):
+                if tdsql.queryResult[j][4] == "ready":
+                    ready_nums += 1
+            if ready_nums == dnode_nums:
+                dnodes_ready = True
+                break
+
+            time.sleep(1)
+
+        if not dnodes_ready:
+            tdLog.exit(f"dnodes are not ready in {retry_times}s")
+        tdLog.info(f"dnodes are ready in {retry_times}s")
+
+        modes_ready = False
+        for i in range(retry_times):
+            tdsql.query("show mnodes;")
+            mnode_nums = tdsql.queryRows
+            ready_nums = 0
+            for j in range(tdsql.queryRows):
+                if tdsql.queryResult[j][3] == "ready":
+                    ready_nums += 1
+            if ready_nums == mnode_nums:
+                modes_ready = True
+                break
+            time.sleep(1)
+
+        if not modes_ready:
+            tdLog.exit(f"mnodes are not ready in {retry_times}s")
+        tdLog.info(f"mnodes are ready in {retry_times}s")
+
+    
+        vnodes_ready = False
+        for i in range(retry_times):
+            tdsql.query("show vnodes;")
+            vnode_nums = tdsql.queryRows
+            ready_nums = 0
+            for j in range(tdsql.queryRows):
+                if str(tdsql.queryResult[j][6]).lower() == "true":
+                    ready_nums += 1
+            if ready_nums == vnode_nums:
+                vnodes_ready = True
+                break
+            time.sleep(1)
+    
+        if not vnodes_ready:
+            tdLog.exit(f"vnodes are not ready in {retry_times}s") 
+        tdLog.info(f"vnodes are ready in {retry_times}s")
+
 
 
 # Create instance for compatibility
