0f2f307d87bf7fac5dc86a45dd0aabb79aaa44e5
Yihao Deng <luomoxyz@126.com>
2025-09-29 13:52:49 +0800

    fix possible mem leak while driver quit (#33093)

--- source/libs/transport/src/transComm.c
+++ source/libs/transport/src/transComm.c
@@ -513,6 +513,8 @@ void transCtxMerge(STransCtx* dst, STransCtx* src) {
   }
 
   taosHashCleanup(src->args);
+  src->args = NULL;
+  src->brokenVal.val = NULL;
 }
 void* transCtxDumpVal(STransCtx* ctx, int32_t key) {
   if (ctx->args == NULL) {
