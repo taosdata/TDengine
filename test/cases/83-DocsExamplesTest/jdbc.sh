3707c83f6570d6d6a1e250d8d05a48d8aa54ffa9
chenhaoran <haoran920c@163.com>
2025-09-29 17:31:29 +0800

    test:move docs tests and unit tests to new framework

---
+++ test/cases/83-DocsExamplesTest/jdbc.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+pgrep taosd || taosd >> /dev/null 2>&1 &
+pgrep taosadapter || taosadapter >> /dev/null 2>&1 &
+cd ../../docs/examples/java
+
+mvn clean test > jdbc-out.log 2>&1
+tail -n 20 jdbc-out.log
+
+totalJDBCCases=`grep 'Tests run' jdbc-out.log | awk -F"[:,]" 'END{ print $2 }'`
+failed=`grep 'Tests run' jdbc-out.log | awk -F"[:,]" 'END{ print $4 }'`
+error=`grep 'Tests run' jdbc-out.log | awk -F"[:,]" 'END{ print $6 }'`
+totalJDBCFailed=$((failed + error))
+totalJDBCSuccess=$((totalJDBCCases - totalJDBCFailed))
+
+if [ "$totalJDBCSuccess" -gt "0" ]; then
+  echo -e "\n${GREEN} ### Total $totalJDBCSuccess JDBC case(s) succeed! ### ${NC}"
+fi
+
+if [ "$totalJDBCFailed" -ne "0" ]; then
+  echo -e "\n${RED} ### Total $totalJDBCFailed JDBC case(s) failed! ### ${NC}"
+  exit 8
+fi
