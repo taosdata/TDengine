b62a807a2b2764099d56688cd9335907939ab869
facetosea <285808407@qq.com>
2025-09-29 11:39:47 +0000

    fix: last error

--- source/libs/executor/src/scanoperator.c
+++ source/libs/executor/src/scanoperator.c
@@ -530,7 +530,7 @@ static void prepareForDescendingScan(STableScanBase* pTableScanInfo, SqlFunction
   switchCtxOrder(pCtx, numOfOutput);
   pTableScanInfo->cond.order = TSDB_ORDER_DESC;
   STimeWindow* pTWindow = &pTableScanInfo->cond.twindows;
-  TSWAP(pTWindow->skey, pTWindow->ekey);
+  // TSWAP(pTWindow->skey, pTWindow->ekey);
 }
 
 typedef struct STableCachedVal {
