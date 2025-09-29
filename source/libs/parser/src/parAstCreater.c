da32f297a2c972e2b5b047873d07bfe99cf4a53e
Kaili Xu <klxu@taosdata.com>
2025-09-29 17:04:05 +0800

    enh: support rollup vgroups and show create rsma (#33128)

--- source/libs/parser/src/parAstCreater.c
+++ source/libs/parser/src/parAstCreater.c
@@ -5288,8 +5288,8 @@ SNode* createShowCreateRsmaStmt(SAstCreateContext* pCxt, ENodeType type, SNode* 
   SShowCreateRsmaStmt* pStmt = NULL;
   pCxt->errCode = nodesMakeNode(type, (SNode**)&pStmt);
   CHECK_MAKE_NODE(pStmt);
-  tstrncpy(pStmt->dbName, ((SRealTableNode*)pRealTable)->table.dbName, TSDB_DB_NAME_LEN);
-  tstrncpy(pStmt->rsmaName, ((SRealTableNode*)pRealTable)->table.tableName, TSDB_TABLE_NAME_LEN);
+  tstrncpy(pStmt->dbName, ((SRealTableNode*)pRealTable)->table.dbName, sizeof(pStmt->dbName));
+  tstrncpy(pStmt->rsmaName, ((SRealTableNode*)pRealTable)->table.tableName, sizeof(pStmt->rsmaName));
   nodesDestroyNode(pRealTable);
   return (SNode*)pStmt;
 _err:
@@ -5321,8 +5321,8 @@ SNode* createRollupVgroupsStmt(SAstCreateContext* pCxt, SNode* pDbName, SNodeLis
     pCxt->errCode = TSDB_CODE_PAR_DB_NOT_SPECIFIED;
     CHECK_PARSER_STATUS(pCxt);
   }
-  SCompactVgroupsStmt* pStmt = NULL;
-  pCxt->errCode = nodesMakeNode(QUERY_NODE_COMPACT_VGROUPS_STMT, (SNode**)&pStmt);
+  SRollupVgroupsStmt* pStmt = NULL;
+  pCxt->errCode = nodesMakeNode(QUERY_NODE_ROLLUP_VGROUPS_STMT, (SNode**)&pStmt);
   CHECK_MAKE_NODE(pStmt);
   pStmt->pDbName = pDbName;
   pStmt->vgidList = vgidList;
