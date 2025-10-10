86e0847ac11de573b65d6e01abba179d99b69e82
menshibin <men_shi_bin@163.com>
2025-10-10 19:08:12 +0800

    feat: set version number arrary length 40

--- tools/taos-tools/deps/toolscJson/src/toolscJson.c
+++ tools/taos-tools/deps/toolscJson/src/toolscJson.c
@@ -63,7 +63,7 @@ CJSON_PUBLIC(const char *) tools_cJSON_GetErrorPtr(void)
 
 CJSON_PUBLIC(const char*) tools_cJSON_Version(void)
 {
-    static char version[15];
+    static char version[40];
     snprintf(version, sizeof(version), "%i.%i.%i", CJSON_VERSION_MAJOR, CJSON_VERSION_MINOR, CJSON_VERSION_PATCH);
 
     return version;
