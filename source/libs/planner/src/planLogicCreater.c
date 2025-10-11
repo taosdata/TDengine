ab467873c06b640475ccc81114a2ac6343f6cf50
Jing Sima <simondominic9997@outlook.com>
2025-10-11 16:13:53 +0800

    enh: Add timerange expr type check and Relax the criteria for determining external windows.

--- source/libs/planner/src/planLogicCreater.c
+++ source/libs/planner/src/planLogicCreater.c
@@ -2190,8 +2190,7 @@ static bool filterHasPlaceHolderRange(SOperatorNode *pStart, SOperatorNode *pEnd
   nodesWalkExpr(pStartRight, conditionOnlyPhAndConstImpl, &startCxt);
   nodesWalkExpr(pEndRight, conditionOnlyPhAndConstImpl, &endCxt);
   if (startCxt.hasNotBasicOp || startCxt.hasNegativeConst || startCxt.hasOtherFunc || startCxt.placeholderAtRight ||
-    endCxt.hasNotBasicOp || endCxt.hasNegativeConst || endCxt.hasOtherFunc || endCxt.placeholderAtRight ||
-    !placeHolderCanMakeExternalWindow(startCxt.placeholderType, endCxt.placeholderType)) {
+    endCxt.hasNotBasicOp || endCxt.hasNegativeConst || endCxt.hasOtherFunc || endCxt.placeholderAtRight) {
     return false;
   }
   return true;
