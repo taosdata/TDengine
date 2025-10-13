b7dc5a60d3bbf0305f1767e9ec02884f4a9a42ab
yihaoDeng <luomoxyz@126.com>
2025-10-13 13:37:51 +0800

    fix clear userip error

--- docs/examples/c/connect_example.c
+++ docs/examples/c/connect_example.c
@@ -8,13 +8,15 @@
 #include <unistd.h>  // for sleep()
 #include "taos.h"
 int main() {
+  int32_t     code = 0;
   const char *host = "localhost";
   const char *user = "root";
   const char *passwd = "taosdata";
   const char *db = NULL;      // if don't want to connect to a default db, set it to NULL or ""
   uint16_t    port = 6030;    // 0 means use the default port
   TAOS       *taos = taos_connect(host, user, passwd, db, port);
-  taos_options_connection(taos, TSDB_OPTION_CONNECTION_USER_IP, "192.168.1.1");
+
+  code = taos_options_connection(taos, TSDB_OPTION_CONNECTION_USER_IP, "192.168.1.1");
   sleep(5);
   {
     TAOS_RES *res = taos_query(taos, "show connections");
@@ -26,59 +28,6 @@ int main() {
     }
 
     taos_free_result(res);
-  }
-
-  taos_options_connection(taos, TSDB_OPTION_CONNECTION_USER_IP, NULL);
-  sleep(5);
-  {
-    {
-      TAOS_RES *res = taos_query(taos, "show connections");
-
-      sleep(5);
-      if (taos == NULL) {
-        fprintf(stderr, "Failed to connect to %s:%hu, ErrCode: 0x%x, ErrMessage: %s.\n", host, port, taos_errno(NULL),
-                taos_errstr(NULL));
-        taos_cleanup();
-        return -1;
-      }
-
-      taos_free_result(res);
-    }
-  }
-
-  taos_options_connection(taos, TSDB_OPTION_CONNECTION_USER_IP, "192.168.1.3");
-  sleep(5);
-  {
-    {
-      TAOS_RES *res = taos_query(taos, "show connections");
-
-      sleep(5);
-      if (taos == NULL) {
-        fprintf(stderr, "Failed to connect to %s:%hu, ErrCode: 0x%x, ErrMessage: %s.\n", host, port, taos_errno(NULL),
-                taos_errstr(NULL));
-        taos_cleanup();
-        return -1;
-      }
-
-      taos_free_result(res);
-    }
-  }
-  taos_options_connection(taos, TSDB_OPTION_CONNECTION_CLEAR, "192.168.1.3");
-  sleep(5);
-  {
-    {
-      TAOS_RES *res = taos_query(taos, "show connections");
-
-      sleep(5);
-      if (taos == NULL) {
-        fprintf(stderr, "Failed to connect to %s:%hu, ErrCode: 0x%x, ErrMessage: %s.\n", host, port, taos_errno(NULL),
-                taos_errstr(NULL));
-        taos_cleanup();
-        return -1;
-      }
-
-      taos_free_result(res);
-    }
   }
 
   fprintf(stdout, "Connected to %s:%hu successfully.\n", host, port);
