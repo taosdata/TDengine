5757f93aa25519c952c11e13eacc9fcd9177b5bb
yihaoDeng <luomoxyz@126.com>
2025-10-13 11:08:18 +0800

    fix clear userip error

--- docs/examples/c/connect_example.c
+++ docs/examples/c/connect_example.c
@@ -164,6 +164,43 @@ int main() {
     }
   }
 
+  taos_options_connection(taos, TSDB_OPTION_CONNECTION_USER_IP, "192.168.1.3");
+  sleep(5);
+  {
+    {
+      TAOS_RES *res = taos_query(taos, "show connections");
+
+      printResult(res);
+      sleep(5);
+      if (taos == NULL) {
+        fprintf(stderr, "Failed to connect to %s:%hu, ErrCode: 0x%x, ErrMessage: %s.\n", host, port, taos_errno(NULL),
+                taos_errstr(NULL));
+        taos_cleanup();
+        return -1;
+      }
+
+      taos_free_result(res);
+    }
+  }
+  taos_options_connection(taos, TSDB_OPTION_CONNECTION_CLEAR, "192.168.1.3");
+  sleep(5);
+  {
+    {
+      TAOS_RES *res = taos_query(taos, "show connections");
+
+      printResult(res);
+      sleep(5);
+      if (taos == NULL) {
+        fprintf(stderr, "Failed to connect to %s:%hu, ErrCode: 0x%x, ErrMessage: %s.\n", host, port, taos_errno(NULL),
+                taos_errstr(NULL));
+        taos_cleanup();
+        return -1;
+      }
+
+      taos_free_result(res);
+    }
+  }
+
   fprintf(stdout, "Connected to %s:%hu successfully.\n", host, port);
   
   /* put your code here for read and write */
