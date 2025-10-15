2da7e1c5edb1375c3277f360fa9d9a8df5b0bc1a
Alex Duan <417921451@qq.com>
2025-10-15 11:40:11 +0800

    fix: update copyright to 2025

--- source/client/wrapper/inc/wrapperHint.h
+++ source/client/wrapper/inc/wrapperHint.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 TAOS Data, Inc. <jhtao@taosdata.com>
+ * Copyright (c) 2025 TAOS Data, Inc. <jhtao@taosdata.com>
  *
  * This program is free software: you can use, redistribute, and/or modify
  * it under the terms of the GNU Affero General Public License, version 3
