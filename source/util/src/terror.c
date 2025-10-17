4ce52650cfb7b4bec72de95037469905c900b187
Minglei Jin <mljin@taosdata.com>
2025-10-17 17:32:20 +0800

    use strerror_s for windows

--- source/util/src/terror.c
+++ source/util/src/terror.c
@@ -1009,7 +1009,12 @@ const char* tstrerror2(int32_t err, char *errstr, int len) {
 
     // strerror can handle any invalid code
     // invalid code return Unknown error
+    #ifdef WINDOWS
+    strerror_s(code, errbuf, sizeof(errbuf));
+    #else
     strerror_r(code, errbuf, sizeof(errbuf));
+    #endif
+
     snprintf(errstr, len, "System error(%d): %s", code, errbuf);
 
     return errstr;
