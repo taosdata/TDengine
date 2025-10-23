5c755c94cc62f9cc4fff64f50d004ea73e22061e
Tony Zhang <tonyzhang@taosdata.com>
2025-10-23 09:10:35 +0800

    fix typo

--- source/libs/function/src/builtinsimpl.c
+++ source/libs/function/src/builtinsimpl.c
@@ -7311,7 +7311,6 @@ int32_t groupConstValueFunction(SqlFunctionCtx* pCtx) {
     pInfo->isNull = false;
   } else {
     // all values are null or first value is null for group by
-    pInfo->hasResult = true;
     pInfo->isNull = true;
   }
 
