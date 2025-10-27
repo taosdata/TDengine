0ea8ec28bb7ff13c84b80a325a5e1388699cc6ef
Kaili Xu <klxu@taosdata.com>
2025-10-27 09:26:09 +0800

    enh: grant support KingHistorian (#2383)

--- enterprise/src/plugins/grant/inc/machine.h
+++ enterprise/src/plugins/grant/inc/machine.h
@@ -229,9 +229,10 @@ typedef enum {
   CONN_TYPE_CSV = 15,
   CONN_TYPE_SPARKPLUGB = 16,
   CONN_TYPE_ORC = 17,
+  CONN_TYPE_KINGHISTORIAN = 18,
   // add future data ins here
   // CONN_TYPE_FUTURE_DATA_IN = XX,
-  CONN_TYPE_DYN_MAX = 18,
+  CONN_TYPE_DYN_MAX = 19,
 } EGrantConnType;
 
 #define CONN_TYPE_MAX_V1 6
--- enterprise/src/plugins/grant/src/grantMain.c
+++ enterprise/src/plugins/grant/src/grantMain.c
@@ -223,13 +223,13 @@
 #define GRANT_LOG_MAX_MACHINE 300
 
 static const char gConnName[CONN_TYPE_DYN_MAX][GRANT_ITEM_NAME_LEN] = {
-    "opc_da", "opc_ua",   "pi",     "kafka", "influxdb", "mqtt", "avevahistorian", "opentsdb", "td2.6", "td3.0",
-    "mysql",  "postgres", "oracle", "mssql", "mongodb",  "csv",  "sparkplugb",     "orc"};
+    "opc_da", "opc_ua",   "pi",     "kafka", "influxdb", "mqtt", "avevahistorian", "opentsdb", "td2.6",   "td3.0",
+    "mysql",  "postgres", "oracle", "mssql", "mongodb",  "csv",  "sparkplugb",     "orc",      "kinghist"};
 
 static const char *gConnDisplay[CONN_TYPE_DYN_MAX] = {
-    "OPC_DA",         "OPC_UA",    "Pi",          "Kafka",       "InfluxDB",   "MQTT",
-    "avevaHistorian", "OpenTSDB",  "TDengine2.6", "TDengine3.0", "MySQL",      "PostgreSQL",
-    "Oracle",         "SqlServer", "MongoDB",     "CSV",         "SparkplugB", "ORC"};
+    "OPC_DA",   "OPC_UA",      "Pi",          "Kafka", "InfluxDB",     "MQTT",   "avevaHistorian",
+    "OpenTSDB", "TDengine2.6", "TDengine3.0", "MySQL", "PostgreSQL",   "Oracle", "SqlServer",
+    "MongoDB",  "CSV",         "SparkplugB",  "ORC",   "KingHistorian"};
 
 static const char gGrantName[GRANT_OPT_DYN_MAX][GRANT_ITEM_NAME_LEN] = {
     "basic",         "service",      "stream",        "subscription",   "audit",
@@ -547,6 +547,9 @@ static void grantInitShowFlags() {
 #endif
 #if !defined(TD_INDUSTRY) || defined(TD_DATAIN_ORC)
   grantHandle.showDataIns[CONN_TYPE_ORC] = 1;
+#endif
+#if !defined(TD_INDUSTRY) || defined(TD_DATAIN_KINGHISTORIAN)
+  grantHandle.showDataIns[CONN_TYPE_KINGHISTORIAN] = 1;
 #endif
 
   // add future datains here ...
