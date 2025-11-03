6c8e3d5e807dc421d4f307121dbe4fa220686cd9
Minglei Jin <mljin@taosdata.com>
2025-11-03 16:40:34 +0800

    fix format string

--- include/util/tutil.h
+++ include/util/tutil.h
@@ -275,7 +275,7 @@ static FORCE_INLINE int32_t taosCreateSHA2Hash(char *pBuf, int32_t len, uint32_t
       sha512((const uint8_t *)pBuf, len, result);
       return sprintf(pBuf,
                      "%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%"
-                     "02x%02x%02x%02x%x02x%02x%02x%02x%x02x%02x%02x%02x%x02x%02x%02x%02x%x02x%02x%02x%02x%x02x%02x%02x%"
+                     "02x%02x%02x%02x%02x%02x%02x%02x%x02x%02x%02x%02x%x02x%02x%02x%02x%x02x%02x%02x%02x%x02x%02x%02x%"
                      "02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
                      result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7], result[8],
                      result[9], result[10], result[11], result[12], result[13], result[14], result[15], result[16],
