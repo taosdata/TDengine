fd77b1b4eff54973ad226ed60118103c5ea084b6
dongming chen <cademfly@hotmail.com>
2025-11-06 09:17:27 +0800

    fix/TD-38490-reset-heartbeat-after-mnode-leader-restart (#33473)

--- source/dnode/mnode/impl/inc/mndDef.h
+++ source/dnode/mnode/impl/inc/mndDef.h
@@ -302,9 +302,9 @@ typedef struct {
 } SSnodeObj;
 
 typedef struct {
-  int32_t dnodeId;
+  int32_t assignedDnodeId;
   char    token[TSDB_ARB_TOKEN_SIZE];
-  int8_t  acked;
+  int8_t  assignAcked;
 } SArbAssignedLeader;
 
 typedef struct {
