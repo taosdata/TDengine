0f4da56f93b8e1d71f8d04c3a3120cb74228adf8
Jing Sima <simondominic9997@outlook.com>
2025-11-12 15:50:46 +0800

    fix:[TD-38550] Fix mem leak in exchagne operator.

--- source/libs/executor/src/exchangeoperator.c
+++ source/libs/executor/src/exchangeoperator.c
@@ -1559,6 +1559,12 @@ int32_t addSingleExchangeSource(SOperatorInfo* pOperator, SExchangeOperatorBasic
         return code;
       }
 
+      SExchangePhysiNode* pExchange = (SExchangePhysiNode*)pOperator->pPhyNode;
+      code = nodesListMakeStrictAppend(&pExchange->pSrcEndPoints, (SNode*)pNode);
+      if (code != TSDB_CODE_SUCCESS) {
+        qError("%s failed at line %d since %s", __func__, __LINE__, tstrerror(code));
+        return code;
+      }
       void* tmp = taosArrayPush(pExchangeInfo->pSources, pNode);
       if (!tmp) {
         qError("%s failed at line %d since %s", __func__, __LINE__, tstrerror(terrno));
