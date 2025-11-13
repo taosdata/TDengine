4e4c48f29447af93af13516bace8954a6ea4f2a1
Jinqing Kuang <kuangjinqingcn@gmail.com>
2025-11-13 19:12:22 +0800

    fix(stream): upgrade some trigger debug log for state window trigger

--- source/libs/new-stream/src/streamTriggerTask.c
+++ source/libs/new-stream/src/streamTriggerTask.c
@@ -3494,12 +3494,22 @@ static int32_t stRealtimeContextSendCalcReq(SSTriggerRealtimeContext *pContext) 
         pParam->prevTs--;
       }
     }
-    ST_TASK_ILOG("[calc param %d]: gid=%" PRId64 ", wstart=%" PRId64 ", wend=%" PRId64 ", nrows=%" PRId64
-                 ", prevTs=%" PRId64 ", currentTs=%" PRId64 ", nextTs=%" PRId64 ", prevLocalTime=%" PRId64
-                 ", nextLocalTime=%" PRId64 ", localTime=%" PRId64 ", create=%d",
-                 i, pCalcReq->gid, pParam->wstart, pParam->wend, pParam->wrownum, pParam->prevTs, pParam->currentTs,
-                 pParam->nextTs, pParam->prevLocalTime, pParam->nextLocalTime, pParam->triggerTime,
-                 pCalcReq->createTable);
+    if (pTask->triggerType == STREAM_TRIGGER_STATE) {
+      ST_TASK_ILOG("[calc param %d]: gid=%" PRId64 ", wstart=%" PRId64 ", wend=%" PRId64 ", nrows=%" PRId64
+                   ", prevTs=%" PRId64 ", currentTs=%" PRId64 ", nextTs=%" PRId64 ", prevLocalTime=%" PRId64
+                   ", nextLocalTime=%" PRId64 ", localTime=%" PRId64 ", create=%d",
+                   i, pCalcReq->gid, pParam->wstart, pParam->wend, pParam->wrownum, pParam->prevTs, pParam->currentTs,
+                   pParam->nextTs, pParam->prevLocalTime, pParam->nextLocalTime, pParam->triggerTime,
+                   pCalcReq->createTable);
+
+    } else {
+      ST_TASK_DLOG("[calc param %d]: gid=%" PRId64 ", wstart=%" PRId64 ", wend=%" PRId64 ", nrows=%" PRId64
+                   ", prevTs=%" PRId64 ", currentTs=%" PRId64 ", nextTs=%" PRId64 ", prevLocalTime=%" PRId64
+                   ", nextLocalTime=%" PRId64 ", localTime=%" PRId64 ", create=%d",
+                   i, pCalcReq->gid, pParam->wstart, pParam->wend, pParam->wrownum, pParam->prevTs, pParam->currentTs,
+                   pParam->nextTs, pParam->prevLocalTime, pParam->nextLocalTime, pParam->triggerTime,
+                   pCalcReq->createTable);
+    }
   }
 
 #ifdef SKIP_SEND_CALC_REQUEST
@@ -5909,12 +5919,21 @@ static int32_t stHistoryContextSendCalcReq(SSTriggerHistoryContext *pContext) {
         pParam->prevTs--;
       }
     }
-    ST_TASK_ILOG("[calc param %d]: gid=%" PRId64 ", wstart=%" PRId64 ", wend=%" PRId64 ", nrows=%" PRId64
-                 ", prevTs=%" PRId64 ", currentTs=%" PRId64 ", nextTs=%" PRId64 ", prevLocalTime=%" PRId64
-                 ", nextLocalTime=%" PRId64 ", localTime=%" PRId64 ", create=%d",
-                 i, pCalcReq->gid, pParam->wstart, pParam->wend, pParam->wrownum, pParam->prevTs, pParam->currentTs,
-                 pParam->nextTs, pParam->prevLocalTime, pParam->nextLocalTime, pParam->triggerTime,
-                 pCalcReq->createTable);
+    if (pTask->triggerType == STREAM_TRIGGER_STATE && !pContext->isHistory) {
+      ST_TASK_ILOG("[calc param %d]: gid=%" PRId64 ", wstart=%" PRId64 ", wend=%" PRId64 ", nrows=%" PRId64
+                   ", prevTs=%" PRId64 ", currentTs=%" PRId64 ", nextTs=%" PRId64 ", prevLocalTime=%" PRId64
+                   ", nextLocalTime=%" PRId64 ", localTime=%" PRId64 ", create=%d",
+                   i, pCalcReq->gid, pParam->wstart, pParam->wend, pParam->wrownum, pParam->prevTs, pParam->currentTs,
+                   pParam->nextTs, pParam->prevLocalTime, pParam->nextLocalTime, pParam->triggerTime,
+                   pCalcReq->createTable);
+    } else {
+      ST_TASK_DLOG("[calc param %d]: gid=%" PRId64 ", wstart=%" PRId64 ", wend=%" PRId64 ", nrows=%" PRId64
+                   ", prevTs=%" PRId64 ", currentTs=%" PRId64 ", nextTs=%" PRId64 ", prevLocalTime=%" PRId64
+                   ", nextLocalTime=%" PRId64 ", localTime=%" PRId64 ", create=%d",
+                   i, pCalcReq->gid, pParam->wstart, pParam->wend, pParam->wrownum, pParam->prevTs, pParam->currentTs,
+                   pParam->nextTs, pParam->prevLocalTime, pParam->nextLocalTime, pParam->triggerTime,
+                   pCalcReq->createTable);
+    }
   }
 
   // serialize and send request
