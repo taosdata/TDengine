03ee32918b97acb9c1dd5388c9249ed0fba73c49
Alex Duan <417921451@qq.com>
2025-11-18 15:33:41 +0800

    feat: add F6 F7 scenes

--- tools/tsbs-tdengine/resource/config/readings.sql
+++ tools/tsbs-tdengine/resource/config/readings.sql
@@ -1,3 +1,3 @@
 DROP DATABASE IF EXISTS db1;
-CREATE DATABASE db1;
+CREATE DATABASE db1 vgroups 8;
 CREATE TABLE db1.`readings` (`ts` TIMESTAMP, `latitude` DOUBLE, `longitude` DOUBLE, `elevation` DOUBLE, `velocity` DOUBLE, `heading` DOUBLE, `grade` DOUBLE, `fuel_consumption` DOUBLE) TAGS (`name` VARCHAR(30), `fleet` VARCHAR(30), `driver` VARCHAR(30), `model` VARCHAR(30), `device_version` VARCHAR(30), `load_capacity` DOUBLE, `fuel_capacity` DOUBLE, `nominal_fuel_consumption` DOUBLE)
