cef591d97bc11db3ae4421ca4dc6a13fe7bd003e
Alex Duan <417921451@qq.com>
2025-11-21 16:55:52 +0800

    feat: db1 and db2 replace with db

--- tools/tsbs-tdengine/resource/config/diagnostics.sql
+++ tools/tsbs-tdengine/resource/config/diagnostics.sql
@@ -1,2 +1,3 @@
-CREATE DATABASE db2 vgroups 8;
-CREATE TABLE db2.`diagnostics` (`ts` TIMESTAMP, `fuel_state` DOUBLE, `current_load` DOUBLE, `status` BIGINT) TAGS (`name` VARCHAR(30), `fleet` VARCHAR(30), `driver` VARCHAR(30), `model` VARCHAR(30), `device_version` VARCHAR(30), `load_capacity` DOUBLE, `fuel_capacity` DOUBLE, `nominal_fuel_consumption` DOUBLE)
+CREATE DATABASE IF NOT EXISTS db vgroups 8;
+DROP TABLE IF EXISTS db.`diagnostics`;
+CREATE TABLE db.`diagnostics` (`ts` TIMESTAMP, `fuel_state` DOUBLE, `current_load` DOUBLE, `status` BIGINT) TAGS (`name` VARCHAR(30), `fleet` VARCHAR(30), `driver` VARCHAR(30), `model` VARCHAR(30), `device_version` VARCHAR(30), `load_capacity` DOUBLE, `fuel_capacity` DOUBLE, `nominal_fuel_consumption` DOUBLE)
