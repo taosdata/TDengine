d043646a9ce1d1eddef2c30464abebdfb62f1d56
Haojun Liao <hjliao@taosdata.com>
2025-11-24 16:48:52 +0800

    refactor: add a new configure file.

---
+++ tools/tdgpt/cfg/taosanode.config.py
@@ -0,0 +1,52 @@
+# gunicorn_config.py
+
+# list address and port
+bind = '0.0.0.0:6035'
+
+# Number of worker processes (typically recommended 2 * CPU cores + 1)
+workers = 2
+
+# Specify worker type, using default sync worker here
+# For IO-intensive applications, consider eventlet or gevent
+worker_class = 'sync'
+
+# Number of threads per process (recommended for model deployment)
+threads = 2
+
+# Maximum number of requests, worker will restart after reaching limit, helps release memory
+max_requests = 1000
+
+# Random jitter added to max_requests to avoid all workers restarting simultaneously
+max_requests_jitter = 50
+
+# Timeout settings
+timeout = 1200
+keepalive = 1200 # keep-alive time
+
+# Log Setting
+accesslog = '/var/log/taos/taosanode/access.log'
+errorlog = '/var/log/taos/taosanode/error.log'
+loglevel = 'debug' # log level: debug, info, warning, error, critical
+
+# Set process name
+proc_name = 'tdgpt_taosanode_app'
+
+# Preload application to ensure each worker has independent model instance
+preload_app = True
+
+# [taosanode]
+# default app log file
+app_log = '/var/log/taos/taosanode/taosanode.app.log'
+
+# model storage directory
+model_dir = '/usr/local/taos/taosanode/model/'
+
+# default log level
+log_level = 'DEBUG'
+
+# draw the query results
+draw_result = False
+
+# moe default service host
+tdtsfm_1 = 'http://127.0.0.1:6036/tdtsfm'
+timemoe_fc = 'http://127.0.0.1:6037/ds_predict'
