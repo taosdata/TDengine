3244bc15b0a3b52158f1290fe0f94ad02acd8104
Simon Guan <slguan@taosdata.com>
2025-11-27 13:38:25 +0800

    Revert "fix: create /etc/taos directory in run case shell (#33733)" (#33734)
    This reverts commit 6c06b4d4bae94f27ce17d233a6fba412185217fe.

--- test/ci/run_case.sh
+++ test/ci/run_case.sh
@@ -76,7 +76,6 @@ fi
 mkdir -p /var/lib/taos/subscribe
 mkdir -p /var/log/taos
 mkdir -p /var/lib/taos
-mkdir -p /etc/taos
 
 cd $CONTAINER_TESTDIR/$target_dir || { echo "Can't enter the target dirctory: ${CONTAINER_TESTDIR}/${target_dir}"; exit 1; }
 ulimit -c unlimited
