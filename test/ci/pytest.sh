b16a3f7c95d8b75b2f7bd328d99054b511a16ed8
chenhaoran <haoran920c@163.com>
2025-12-03 17:26:57 +0800

    fix: improve readability and consistency in pytest.sh

--- test/ci/pytest.sh
+++ test/ci/pytest.sh
@@ -9,56 +9,56 @@
 set +e
 # set -x
 
+# Detect OS type
 if [[ "${OSTYPE}" == "darwin"* ]]; then
   TD_OS="Darwin"
 else
   # Extract OS name safely
   OS=$(grep "^NAME=" /etc/*-release | head -n1 | cut -d= -f2 | tr -d '"')
-  TD_OS=$(echo "$OS" | awk '{print $1}')
+  TD_OS=$(echo "${OS}" | awk '{print $1}')
 fi
 
 export ASAN_OPTIONS=detect_odr_violation=0
 cd .
 
 # Get responsible directories
-CODE_DIR=$(dirname $0)
-CODE_DIR=$(pwd)
+CODE_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 IN_TDINTERNAL="community"
-if [[ "$CODE_DIR" == *"$IN_TDINTERNAL"* ]]; then
+if [[ "${CODE_DIR}" == *"${IN_TDINTERNAL}"* ]]; then
   cd ../../
 else
   cd ../
 fi
 
-TOP_DIR=$(pwd)
+TOP_DIR="$(pwd)"
 TAOSD_DIR=$(find . -name "taosd" | grep bin | head -n1)
 
 cut_opt="-f "
 
-if [[ "$TAOSD_DIR" == *"$IN_TDINTERNAL"* ]]; then
+if [[ "${TAOSD_DIR}" == *"${IN_TDINTERNAL}"* ]]; then
   BIN_DIR=$(find . -name "taosd" | grep bin | head -n1 | cut -d '/' ${cut_opt}2,3)
 else
   BIN_DIR=$(find . -name "taosd" | grep bin | head -n1 | cut -d '/' ${cut_opt}2)
 fi
 
-declare -x BUILD_DIR=$TOP_DIR/$BIN_DIR
-declare -x SIM_DIR=$TOP_DIR/sim
-PRG_DIR=$SIM_DIR/tsim
-ASAN_DIR=$SIM_DIR/asan
+BUILD_DIR="${TOP_DIR}/${BIN_DIR}"
+SIM_DIR="${TOP_DIR}/sim"
+PRG_DIR="${SIM_DIR}/tsim"
+ASAN_DIR="${SIM_DIR}/asan"
 
-chmod -R 777 $PRG_DIR
+chmod -R 777 "$PRG_DIR"
 echo "------------------------------------------------------------------------"
 echo "Start TDengine Testing Case ..."
-echo "BUILD_DIR: $BUILD_DIR"
-echo "SIM_DIR  : $SIM_DIR"
-echo "CODE_DIR : $CODE_DIR"
-echo "ASAN_DIR : $ASAN_DIR"
+echo "BUILD_DIR: ${BUILD_DIR}"
+echo "SIM_DIR  : ${SIM_DIR}"
+echo "CODE_DIR : ${CODE_DIR}"
+echo "ASAN_DIR : ${ASAN_DIR}"
 
-# prevent delete / folder or /usr/bin
+# Prevent deleting / folder or /usr/bin
 if [ ${#SIM_DIR} -lt 10 ]; then
-   echo "len(SIM_DIR) < 10 , danger so exit. SIM_DIR=$SIM_DIR"
-   exit 1
+  echo "len(SIM_DIR) < 10 , danger so exit. SIM_DIR=${SIM_DIR}"
+  exit 1
 fi
 
 rm -rf "${SIM_DIR:?}/"*
@@ -66,39 +66,39 @@ rm -rf "${SIM_DIR:?}/"*
 mkdir -p "${PRG_DIR}"
 mkdir -p "${ASAN_DIR}"
 
-cd "$CODE_DIR" || exit
+cd "${CODE_DIR}" || exit
 ulimit -n 600000
 ulimit -c unlimited
 
-#sudo sysctl -w kernel.core_pattern=$TOP_DIR/core.%p.%e
+# sudo sysctl -w kernel.core_pattern=$TOP_DIR/core.%p.%e
 echo "ExcuteCmd:" "$@"
 
 if [[ "$TD_OS" == "Alpine" ]]; then
   "$@"
 else
-  AsanFile=$ASAN_DIR/psim.info
-  echo "AsanFile:" $AsanFile
+  AsanFile="${ASAN_DIR}/psim.info"
+  echo "AsanFile:" "$AsanFile"
 
   unset LD_PRELOAD
-  #export LD_PRELOAD=libasan.so.5
-  #export LD_PRELOAD=$(gcc -print-file-name=libasan.so)
+  # export LD_PRELOAD=libasan.so.5
+  # export LD_PRELOAD=$(gcc -print-file-name=libasan.so)
   LD_PRELOAD="$(realpath "$(gcc -print-file-name=libasan.so)") $(realpath "$(gcc -print-file-name=libstdc++.so)")"
   export LD_PRELOAD
   echo "Preload AsanSo:" $?
 
   "$@" -A 2>"$AsanFile"
 
-
   unset LD_PRELOAD
   for ((i = 1; i <= 20; i++)); do
-    AsanFileLen=$(cat "${AsanFile}" | wc -l)
+    AsanFileLen=$(wc -l < "${AsanFile}")
     echo "AsanFileLen:" "${AsanFileLen}"
     if [ "$AsanFileLen" -gt 10 ]; then
       break
     fi
     sleep 1
   done
-  # check case successful
+
+  # Check case successful
   AsanFileSuccessLen=$(grep -w -a -c "successfully executed" "$AsanFile")
   echo "AsanFileSuccessLen:" "$AsanFileSuccessLen"
 
