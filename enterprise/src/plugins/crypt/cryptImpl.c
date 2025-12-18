37978df1a9d595199726e7bf2273b0b1784525a2
dongming chen <cademfly@hotmail.com>
2025-12-18 09:38:35 +0800

    feat(TS-7270): return error in terrno (#2427)

--- enterprise/src/plugins/crypt/cryptImpl.c
+++ enterprise/src/plugins/crypt/cryptImpl.c
@@ -20,54 +20,71 @@
 #include "crypt.h"
 #include "sm4.h"
 
-int32_t Builtin_CBC_DecryptImpl(SCryptOpts *opts) {
-  int NewLen = 0;
 
+int32_t Builtin_CBC_DecryptImpl(SCryptOpts *opts) {
+  int32_t newLen = 0;
   int32_t count = 0;
+  terrno = 0;
+
   while (count < opts->len) {
-    (void)SM4_CBC_Decrypt(opts->key, 16, opts->key, 16, opts->source + count, opts->unitLen, opts->result + count,
-                          &NewLen);
-    count += NewLen;
+    if (SM4_CBC_Decrypt(opts->key, 16, opts->key, 16, opts->source + count, opts->unitLen, opts->result + count,
+                        &newLen) == 0)
+      count += newLen;
+    else {
+      terrno = TSDB_CODE_UTIL_CRYPT_INVALID_PARA;
+      break;
+    }
   }
+
   return count;
 }
 
 int32_t Builtin_CBC_EncryptImpl(SCryptOpts *opts) {
-  int NewLen = 0;
-
+  int32_t newLen = 0;
   int32_t count = 0;
+  terrno = 0;
+
   while (count < opts->len) {
-    (void)SM4_CBC_Encrypt(opts->key, 16, opts->key, 16, opts->source + count, opts->unitLen, opts->result + count,
-                          &NewLen);
-    count += NewLen;
+    if (SM4_CBC_Encrypt(opts->key, 16, opts->key, 16, opts->source + count, opts->unitLen, opts->result + count,
+                        &newLen) == 0)
+      count += newLen;
+    else {
+      terrno = TSDB_CODE_UTIL_CRYPT_INVALID_PARA;
+      break;
+    }
   }
+
   return count;
 }
 
 #if defined(TD_ENTERPRISE) && defined(LINUX)
 int32_t CBC_EncryptImpl(SCryptOpts *opts) {
   int outlen = -1;
+  terrno = 0;
 
-  if (opts == NULL) return -1;
-  if (opts->pOsslAlgrName == NULL) return -1;
-  if (strlen(opts->pOsslAlgrName) == 0) return -1;
+  if (opts == NULL || opts->pOsslAlgrName == NULL || strlen(opts->pOsslAlgrName) == 0) {
+    terrno = TSDB_CODE_UTIL_CRYPT_INVALID_PARA;
+    return outlen;
+  }
 
   EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
   if (ctx == NULL) {
+    terrno = TSDB_CODE_UTIL_CRYPT_FAIL_NEW_CXT;
     uError("No context");
-    return -1;
+    return outlen;
   }
 
   EVP_CIPHER *cipher = EVP_CIPHER_fetch(NULL, opts->pOsslAlgrName, NULL);
   if (cipher == NULL) {
     uError("No evp! in encrypt %s", opts->pOsslAlgrName);
     ERR_print_errors_fp(stderr);
-    outlen = TSDB_CODE_UTIL_CIPHER_NOT_EXIST;
+    terrno = TSDB_CODE_UTIL_CIPHER_NOT_EXIST;
     goto out;
   }
 
   if (!EVP_EncryptInit(ctx, cipher, (unsigned char *)opts->key, (unsigned char *)opts->key)) {
     uError("Failed EVP_EncryptInit!");
+    terrno = TSDB_CODE_UTIL_CRYPT_FAIL_INIT;
     goto out;
   }
 
@@ -77,6 +94,7 @@ int32_t CBC_EncryptImpl(SCryptOpts *opts) {
 
   if (!EVP_EncryptUpdate(ctx, opts->result, &outlen, opts->source, opts->len)) {
     uError("Failed EVP_EncryptUpdate!");
+    terrno = TSDB_CODE_UTIL_CRYPT_FAIL_EXEC;
     goto out;
   }
   uTrace("source len:%d, result len:%d", opts->len, outlen);
@@ -84,6 +102,7 @@ int32_t CBC_EncryptImpl(SCryptOpts *opts) {
   int tmplen = 0;
   if (!EVP_EncryptFinal(ctx, &opts->result[outlen], &tmplen)) {
     uError("Failed EVP_EncryptFinal!");
+    terrno = TSDB_CODE_UTIL_CRYPT_FAIL_EXEC;
     goto out;
   }
   uTrace("tmplen:%d", tmplen);
@@ -98,26 +117,31 @@ out:
 
 int32_t CBC_DecryptImpl(SCryptOpts *opts) {
   int outlen = -1;
+  terrno = 0;
 
-  if (opts == NULL) return -1;
-  if (opts->pOsslAlgrName == NULL) return -1;
+  if (opts == NULL || opts->pOsslAlgrName == NULL || strlen(opts->pOsslAlgrName) == 0) {
+    terrno = TSDB_CODE_UTIL_CRYPT_INVALID_PARA;
+    return outlen;
+  }
 
   EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
   if (ctx == NULL) {
+    terrno = TSDB_CODE_UTIL_CRYPT_FAIL_NEW_CXT;
     uError("No context");
-    return -1;
+    return outlen;
   }
 
   EVP_CIPHER *cipher = EVP_CIPHER_fetch(NULL, opts->pOsslAlgrName, NULL);
   if (cipher == NULL) {
     uError("No evp! in decrypt %s", opts->pOsslAlgrName);
     ERR_print_errors_fp(stderr);
-    outlen = TSDB_CODE_UTIL_CIPHER_NOT_EXIST;
+    terrno = TSDB_CODE_UTIL_CIPHER_NOT_EXIST;
     goto out;
   }
 
   if (!EVP_DecryptInit(ctx, cipher, (unsigned char *)opts->key, (unsigned char *)opts->key)) {
     uError("Failed EVP_DecryptInit!");
+    terrno = TSDB_CODE_UTIL_CRYPT_FAIL_INIT;
     goto out;
   }
 
@@ -127,6 +151,7 @@ int32_t CBC_DecryptImpl(SCryptOpts *opts) {
 
   if (!EVP_DecryptUpdate(ctx, opts->result, &outlen, opts->source, opts->len)) {
     uError("Failed EVP_DecryptUpdate!");
+    terrno = TSDB_CODE_UTIL_CRYPT_FAIL_EXEC;
     goto out;
   }
   uTrace("source len:%d, result len:%d", opts->len, outlen);
@@ -134,6 +159,7 @@ int32_t CBC_DecryptImpl(SCryptOpts *opts) {
   int tmplen = 0;
   if (!EVP_DecryptFinal(ctx, &opts->result[outlen], &tmplen)) {
     uError("Failed EVP_DecryptFinal!");
+    terrno = TSDB_CODE_UTIL_CRYPT_FAIL_EXEC;
     goto out;
   }
   uTrace("tmplen:%d", tmplen);
