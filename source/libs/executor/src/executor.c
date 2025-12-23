7ff28e7f65ed6456c4eb3eec4e3ff80b0f6f4880
Simon Guan <guanshengliang@qq.com>
2025-12-23 15:33:55 +0800

    fix: conflicts

--- source/libs/executor/src/executor.c
+++ source/libs/executor/src/executor.c
@@ -508,7 +508,7 @@ static int32_t filterUnqualifiedTables(const SStreamScanInfo* pScanInfo, const S
       }
 
       if (!qualified) {
-        qInfo("table uid:0x%" PRIx64 " is unqualified for tag condition, %s", info.uid, idstr);
+        qInfo("table uid:0x%" PRIx64 " is unqualified for tag condition, %s", t->childUid, idstr);
         continue;
       }
     }
