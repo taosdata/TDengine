b0601ab996986f4a8dbb7beea74e71b96771c912
haoranchen <haoran920c@163.com>
2025-12-26 23:07:59 +0800

    fix: optimize process termination commands in install and remove scripts (#34064)
    * fix: optimize process termination commands in install and remove scripts
    
    * fix: improve process handling in install and remove scripts

--- packaging/tools/install.sh
+++ packaging/tools/install.sh
@@ -396,9 +396,9 @@ function install_services() {
 }
 
 function kill_process() {
-  pid=$(ps -ef | grep "$1" | grep -v "grep" | awk '{print $2}')
+  pid=$(pgrep -x  "$1")
   if [ -n "$pid" ]; then
-    kill -9 $pid || :
+    kill -9 "$pid" || :
   fi
 }
 
