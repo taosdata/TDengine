dd948c6e96ccebba20242b1ae6dc77ab25e96108
Jinqing Kuang <kuangjinqingcn@gmail.com>
2025-12-29 11:02:54 +0800

    feat(stream): support sub event in event window trigger

--- source/libs/nodes/src/nodesUtilFuncs.c
+++ source/libs/nodes/src/nodesUtilFuncs.c
@@ -3066,7 +3066,8 @@ int32_t nodesCollectColumnsExt(SSelectStmt* pSelect, ESqlClause clause, SSHashOb
   return TSDB_CODE_SUCCESS;
 }
 
-int32_t nodesCollectColumnsFromNode(SNode* node, const char* pTableAlias, ECollectColType type, SNodeList** pCols) {
+int32_t nodesCollectColumnsFromMultiNodes(SNode** pNodes, int32_t numNodes, const char* pTableAlias,
+                                          ECollectColType type, SNodeList** pCols) {
   if (NULL == pCols) {
     return TSDB_CODE_FAILED;
   }
@@ -3089,7 +3090,12 @@ int32_t nodesCollectColumnsFromNode(SNode* node, const char* pTableAlias, EColle
   }
   *pCols = NULL;
 
-  nodesWalkExpr(node, collectColumns, &cxt);
+  for (int32_t i = 0; i < numNodes; ++i) {
+    nodesWalkExpr(pNodes[i], collectColumns, &cxt);
+    if (TSDB_CODE_SUCCESS != cxt.errCode) {
+      break;
+    }
+  }
 
   taosHashCleanup(cxt.pColHash);
   if (TSDB_CODE_SUCCESS != cxt.errCode) {
@@ -3103,6 +3109,10 @@ int32_t nodesCollectColumnsFromNode(SNode* node, const char* pTableAlias, EColle
   }
 
   return TSDB_CODE_SUCCESS;
+}
+
+int32_t nodesCollectColumnsFromNode(SNode* node, const char* pTableAlias, ECollectColType type, SNodeList** pCols) {
+  return nodesCollectColumnsFromMultiNodes(&node, 1, pTableAlias, type, pCols);
 }
 
 typedef struct SCollectFuncsCxt {
