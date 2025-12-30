8961bc1a60a7c94438644b0e34f3a19605f5dee5
yihaoDeng <luomoxyz@126.com>
2025-12-30 16:41:06 +0800

    change default value

--- source/libs/parser/src/parAstCreater.c
+++ source/libs/parser/src/parAstCreater.c
@@ -4131,7 +4131,8 @@ SUserOptions* createDefaultUserOptions(SAstCreateContext* pCxt) {
   pOptions->connectTime = TSDB_USER_CONNECT_TIME_DEFAULT;
   pOptions->connectIdleTime = TSDB_USER_CONNECT_IDLE_TIME_DEFAULT;
   pOptions->callPerSession = TSDB_USER_CALL_PER_SESSION_DEFAULT;
-  pOptions->vnodePerCall = -1; // not implemented yet, so use -1 instead of TSDB_USER_VNODE_PER_CALL_DEFAULT;
+  pOptions->vnodePerCall = TSDB_USER_VNODE_PER_CALL_DEFAULT;  // not implemented yet, so use -1 instead of
+                                                                // TSDB_USER_VNODE_PER_CALL_DEFAULT;
   pOptions->failedLoginAttempts = TSDB_USER_FAILED_LOGIN_ATTEMPTS_DEFAULT;
   pOptions->passwordLifeTime = TSDB_USER_PASSWORD_LIFE_TIME_DEFAULT;
   pOptions->passwordReuseTime = TSDB_USER_PASSWORD_GRACE_TIME_DEFAULT;
