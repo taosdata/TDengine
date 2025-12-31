4ae0fb0526211922463870b80b39b48a6bdc68f5
qevolg <2227465945@qq.com>
2025-12-31 09:33:29 +0800

    docs: update taos-ws-py version to 0.6.5 and add TOTP/token authentication support in documentation

--- tests/parallel_test/run_case.sh
+++ tests/parallel_test/run_case.sh
@@ -85,9 +85,9 @@ md5sum /home/TDinternal/debug/build/lib/libtaos.so
 md5sum /usr/lib/libtaosnative.so.1
 md5sum /home/TDinternal/debug/build/lib/libtaosnative.so
 
-# get python connector and update: taospy 2.8.6 taos-ws-py 0.6.4
+# get python connector and update: taospy 2.8.6 taos-ws-py 0.6.5
 pip3 install taospy==2.8.6
-pip3 install taos-ws-py==0.6.4
+pip3 install taos-ws-py==0.6.5
 $TIMEOUT_CMD $cmd
 RET=$?
 echo "cmd exit code: $RET"
