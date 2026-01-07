b6b73fc7f31dbf3ff12a967d0746ee63fc1716f0
Alex Duan <417921451@qq.com>
2026-01-07 11:50:21 +0800

    test: 5 option for create user finished

--- test/cases/24-Users/test_user_security.py
+++ test/cases/24-Users/test_user_security.py
@@ -2,6 +2,7 @@ from new_test_framework.utils import tdLog, tdSql, TDSql, TDCom
 import os
 import platform
 import time
+import threading
 
 class TestUserSecurity:
     def setup_class(cls):
@@ -42,6 +43,8 @@ class TestUserSecurity:
         if login:
             self.login(user=user_name, password=password)
             
+    def drop_user(self, user_name):
+        tdSql.execute(f"DROP USER {user_name}")            
             
     def create_session(self, user_name, password, num):
         conns = []
@@ -52,21 +55,56 @@ class TestUserSecurity:
             conn.execute("show users")
             conns.append(conn)
         
-        #for conn in conns:
-            #conn.close()    
+        for conn in conns:
+            conn.close()
             
     def create_session_failed(self, user_name, password, num):
         try:
             self.create_session(user_name, password, num)
         except Exception as e:
-            tdLog.info(f"Max sessions reached as expected: {e}")
-            return
-        
-        for i in range(60):
-            print(f" wait to see session ....{i}")
-            time.sleep(1)
-            
+            print(f" max sessions({num}) reached as expected: {e}")
+            return            
+
         raise Exception("Exceeded max sessions but was expected to fail")
+    
+    def check_user_option(self, user_name, option_name, expected_value):
+        res = tdSql.getResult(f"show users full")
+        idx = tdSql.fieldIndex(option_name)
+        for row in res:
+            if row[0] == user_name:
+                actual_value = row[idx]
+                if actual_value != expected_value:
+                    raise Exception(f"User option value mismatch for {option_name}: expected {expected_value}, got {actual_value}")
+                return
+        raise Exception(f"User {user_name} not found in show users output")
+    
+    def thread_exec_sql(self, user_name, password, sql, results, index):
+        try:
+            tdCom = TDCom()
+            conn = tdCom.newTdSql()
+            conn.connect(user=user_name, password=password)
+            conn.execute(sql)
+            results[index] = True
+            conn.close()
+        except Exception as e:
+            results[index] = False
+            print(f" Thread {index} failed to execute SQL: {sql} error:{e}")
+    
+    def create_multiple_sessions(self, user_name, password, sql, num):
+        
+        threads = []
+        results = [False] * num
+        for i in range(num):
+            t = threading.Thread(target=self.thread_exec_sql, args=(user_name, password, sql, results, i))
+            threads.append(t)
+            t.start()
+        
+        for t in threads:
+            t.join()
+
+        print(f" threads results:{results}")            
+        if not all(results):
+            raise Exception("Not all sessions executed the SQL successfully")        
     
     #
     # --------------------------- create user ----------------------------
@@ -142,12 +180,83 @@ class TestUserSecurity:
         self.create_session(user, password, 500)
 
         print("option SESSION_PER_USER ............... [ passed ] ")
+
+    # option CONNECT_TIME
+    def options_connect_time(self):
+        # defalut check (-1, unlimited)
+        user = "user_connect_time1"
+        self.create_user(user)
+        self.check_user_option(user, "CONNECT_TIME", -1)
+
+        # min check (1 minute)
+        user = "user_connect_time2"
+        # check option value
+        self.login()
+        self.create_user(user, options="CONNECT_TIME 1", login=True)
+        for i in range(60):
+            tdSql.execute("show databases")
+            time.sleep(1)
+
+        time.sleep(5)
+        tdSql.error("show databases")
+        print(f" user {user} disconnected as expected after CONNECT_TIME 1 minute")
+
+        # check option value
+        self.login()
+        self.check_user_option(user, "CONNECT_TIME", 60)
+
+        print("option CONNECT_TIME ................... [ passed ] ")
+
+    # option CONNECT_IDLE_TIME
+    def options_connect_idle_time(self):
+        # defalut check (-1, unlimited)
+        user = "user_connect_idle_time1"
+        self.create_user(user)
+        self.check_user_option(user, "CONNECT_IDLE_TIMEOUT", -1)
+
+        # min check (1 minute)
+        user = "user_connect_idle_time2"
+        # check option value
+        self.login()
+        self.create_user(user, options="CONNECT_IDLE_TIME 1", login=True)
+        time.sleep(65)
+        tdSql.error("show databases")
+        print(f" user {user} disconnected as expected after CONNECT_IDLE_TIME 1 minute")
+
+        # check option value
+        self.login()
+        self.check_user_option(user, "CONNECT_IDLE_TIMEOUT", 60)
+
+        print("option CONNECT_IDLE_TIME .............. [ passed ] ")
+
+
+    # option CALL_PER_SESSION
+    def options_call_per_session(self):
+        password = "abcd@1234"
+        self.login()        
+        # defalut check (-1, unlimited)
+        user = "user_call_per_session1"
+        self.create_user(user, password=password)
+        self.check_user_option(user, "CALL_PER_SESSION", -1)
+        self.create_multiple_sessions(user, password, "show databases", 10)
+
+        # min check (1 minute)
+        user = "user_call_per_session2"
+        # check option value
+        self.create_user(user, options="CALL_PER_SESSION 5")
+        self.check_user_option(user, "CALL_PER_SESSION", 5)
+        self.create_multiple_sessions(user, password, "show databases", 5)
+
+        print("option CALL_PER_SESSION .............. [ passed ] ")
         
     
     def do_create_user(self):
         print("\n")
         self.options_changepass()
         self.options_session_per_user()
+        self.options_connect_time()
+        self.options_connect_idle_time()
+        self.options_call_per_session()
 
     #
     # --------------------------- show user ----------------------------
