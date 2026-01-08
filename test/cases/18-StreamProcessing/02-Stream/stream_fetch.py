4ebd3206c81dff3f3912e66ae5cf1bcbd8c72d7e
Minglei Jin <mljin@taosdata.com>
2026-01-08 17:16:51 +0800

    test case for count window stream

--- test/cases/18-StreamProcessing/02-Stream/stream_fetch.py
+++ test/cases/18-StreamProcessing/02-Stream/stream_fetch.py
@@ -33,6 +33,9 @@ class TestStreamFetch:
         self.createIntervalStream()
         # check the interval stream result
         self.checkIntervalResult()
+        self.prepareCountData()
+        self.createCountStream()
+        self.checkCountResult()
 
     def prepareData(self):
         tdLog.info(f"prepare data")
@@ -90,5 +93,64 @@ class TestStreamFetch:
 
         tdSql.query(f"select * from db.`run_s_3#d0` order by ts limit 3065,10")
         tdSql.checkData(7, 1, 9218000)
+
+        tdLog.info(f"check stream result successfully.")
+
+    def prepareCountData(self):
+        tdLog.info(f"prepare count data")
+
+        sqls = [
+            "alter dnode 1 'debugflag 135';",
+            "drop database db",
+            "create database db vgroups 4;",
+            "create table db.meters (ts timestamp, current int) tags(`groupid` int);"
+        ]
+
+        ts = 1767196800000 # int(time.time())*1000
+        for t in range(0, 4):
+            sqls.append(f"create table db.d{t} using db.meters tags({t})")
+            for i in range(0, 4000000, 1000):
+                sqls.append(f"insert into db.d{t} values({ts+i},{i})")
+
+        tdSql.executes(sqls)
+        tdLog.info(f"create successfully.")
+
+    def createCountStream(self):
+        tdLog.info(f"create stream:")
+        sql = (
+        f"create stream db.meters_stream_count count_window(2,1) from db.meters     partition by tbname ,groupid stream_options (fill_history ('2026-01-01 00:00:00') |low_latency_calc|watermark(10s)) into db.stream_meters_count output_subtable (concat('run_c_3#', tbname)) tags (groupid int as groupid) as select _twstart as ts, last(current) as now_batch, first(current) as previous_batch, _twstart as starttime, _twend as endtime from %%tbname where ts >= _twstart and ts<=_twend "
+        )
+
+        tdLog.info(f"create stream:{sql}")
+
+        try:
+            tdSql.execute(sql)
+        except Exception as e:
+            if "No stream available snode now" not in str(e):
+                raise Exception(f" user cant  create stream no snode ,but create success")
+
+        while True:
+            tdSql.query(f"select status from information_schema.ins_streams")
+            if tdSql.getData(0,0) == "Running":
+                tdLog.info("Stream is running!")
+                break
+
+            tdLog.debug(f"current stream status: {tdSql.getData(0,0)}")
+            time.sleep(1)
+
+    def checkCountResult(self):
+        tdLog.info(f"checkIntervalResult start")
+
+        while True:
+            tdSql.query(f"select count(*) from db.`run_c_3#d0`")
+            if tdSql.getData(0,0) == 3999:
+                tdLog.info(f"get {tdSql.getData(0,0)} rows")
+                break
+
+            tdLog.debug(f"current row count: {tdSql.getData(0,0)}")
+            time.sleep(1)
+
+        tdSql.query(f"select * from db.`run_c_3#d0` order by ts limit 3065,10")
+        tdSql.checkData(7, 1, 3073000)
 
         tdLog.info(f"check stream result successfully.")
