e3d14b223b67445874797e258db467b6d7d44672
yihaoDeng <luomoxyz@126.com>
2026-01-21 16:07:06 +0800

    refactor code

--- source/libs/transport/src/transSvr.c
+++ source/libs/transport/src/transSvr.c
@@ -12,8 +12,6 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <bits/stdint-intn.h>
-#include "tmsg.h"
 #include "transComm.h"
 #include "transLog.h"
 #include "transSasl.h"
