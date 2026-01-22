8071259f2bcbc373d10cb8a76e68b703a379ee85
chenhaoran <haoran920c@163.com>
2026-01-22 21:16:54 +0800

    fix(run_case.sh): ensure target directory exists before copying logs

--- test/ci/run_case.sh
+++ test/ci/run_case.sh
@@ -97,15 +97,18 @@ RET=$?
 echo "cmd exit code: $RET"
 
 if [ -d "/var/log/taos" ]; then
+    mkdir -p /home/TDinternal/sim/var_taoslog/
     cp /var/log/taos/* /home/TDinternal/sim/var_taoslog/
 fi
 
 if [ -f "${CONTAINER_TESTDIR}/docs/examples/java/jdbc-out.log" ]; then
+     mkdir -p /home/TDinternal/sim/var_taoslog/
     cp ${CONTAINER_TESTDIR}/docs/examples/java/jdbc-out.log \
         /home/TDinternal/sim/var_taoslog/
 fi
 
 if [ -f "${CONTAINER_TESTDIR}/docs/examples/c/docs-c-test-out.log" ]; then
+     mkdir -p /home/TDinternal/sim/var_taoslog/
     cp ${CONTAINER_TESTDIR}/docs/examples/c/docs-c-test-out.log \
         /home/TDinternal/sim/var_taoslog/
 fi
