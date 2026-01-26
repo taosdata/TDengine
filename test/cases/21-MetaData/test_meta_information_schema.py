97b3e01df65512173edbe9d440484563b8cb057d
chenhaoran <haoran920c@163.com>
2026-01-26 17:16:49 +0800

    fix: update expected row count in ins_columns check

--- test/cases/21-MetaData/test_meta_information_schema.py
+++ test/cases/21-MetaData/test_meta_information_schema.py
@@ -1086,7 +1086,7 @@ class TestDdlInSysdb:
         tdSql.query("select * from information_schema.ins_columns where db_name ='information_schema'")
         
         tdSql.query("select * from information_schema.ins_columns where db_name ='performance_schema'")
-        tdSql.checkRows(64)
+        tdSql.checkRows(65)
 
     def ins_dnodes_check(self):
         tdSql.execute('drop database if exists db2')
