174aa0ea6855aca53106555108e2f9d4ee826e3a
qevolg <2227465945@qq.com>
2026-02-02 09:51:16 +0800

    test: fix failed test case

--- tools/keeper/testutil/testutil_test.go
+++ tools/keeper/testutil/testutil_test.go
@@ -8,15 +8,15 @@ import (
 )
 
 func TestTestUsername(t *testing.T) {
-	os.Setenv("KEEPER_TEST_USERNAME", "test_user")
-	defer os.Unsetenv("KEEPER_TEST_USERNAME")
+	os.Setenv("TEST_USERNAME", "test_user")
+	defer os.Unsetenv("TEST_USERNAME")
 	username := TestUsername()
 	assert.Equal(t, "test_user", username)
 }
 
 func TestTestPassword(t *testing.T) {
-	os.Setenv("KEEPER_TEST_PASSWORD", "test_pass")
-	defer os.Unsetenv("KEEPER_TEST_PASSWORD")
+	os.Setenv("TEST_PASSWORD", "test_pass")
+	defer os.Unsetenv("TEST_PASSWORD")
 	password := TestPassword()
 	assert.Equal(t, "test_pass", password)
 }
