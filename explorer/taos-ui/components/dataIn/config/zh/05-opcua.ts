a6bb13c09e4dcf6b9745e0e22702bb956f6191a8
Zhiyu Yang <69311263+zyyang90@users.noreply.github.com>
2026-02-06 16:17:24 +0800

    fix(opcua): few bugs #none (#3598)

--- explorer/taos-ui/components/dataIn/config/zh/05-opcua.ts
+++ explorer/taos-ui/components/dataIn/config/zh/05-opcua.ts
@@ -3,7 +3,7 @@ export default {
   id: 'opcua',
   type: 'uri',
   description:
-    'OPC 是工业自动化领域和其他行业中安全可靠地交换数据的互操作标准之一。\n\nOPC UA 是经典 OPC 规范的下一代标准，是一个平台无关的面向服务的架构规范，集成了现有 OPC Classic 规范的所有功能，提供了一条迁移到更安全和可扩展解决方案的路径。\n\n如果想了解更多关于 OPC UA 的信息，可以阅读 OPC Foundation 网站和一些有用的博客，例如：\n1. [What is OPC](https://opcfoundation.org/about/what-is-opc/)\n2. [What is OPC UA](https://opcfoundation.org/about/opc-technologies/opc-ua/)\n\ntaosX 使用 OPC 连接器从 OPC 服务器拉取或订阅数据。\n',
+    'OPC 是工业自动化领域和其他行业中安全可靠地交换数据的互操作标准之一。\n\nOPC UA 是经典 OPC 规范的下一代标准，是一个平台无关的面向服务的架构规范，集成了现有 OPC Classic 规范的所有功能，提供了一条迁移到更安全和可扩展解决方案的路径。\n\n如果想了解更多关于 OPC 和 OPC UA 的信息，可以阅读 OPC Foundation 网站：\n\n1. [What is OPC](https://opcfoundation.org/about/what-is-opc/)\n2. [What is OPC UA](https://opcfoundation.org/about/opc-technologies/opc-ua/)\n',
   config: [
     {
       label: '连接配置',
@@ -22,7 +22,7 @@ export default {
         },
         {
           label: '故障切换服务地址',
-          description: 'OPC 服务器的故障切换服务器端点，可以指定多个，用逗号分割。',
+          description: 'OPC 服务器的故障切换服务器端点，可以指定多个，用逗号分隔。',
           field: 'failover_endpoints',
           required: false,
           type: 'input'
@@ -112,7 +112,7 @@ export default {
         },
         {
           label: '安全通信私钥',
-          description: '私钥文件，对服务器发送的消息做签名检查或者解密。',
+          description: '证书的私钥文件，用于对服务器发送的消息做签名检查或者解密。',
           field: 'private_key',
           pattern: null,
           defaultValue: '',
@@ -224,38 +224,16 @@ export default {
       multiple: false,
       name: 'datasets',
       valueField: 'currentTab',
-      defaultValue: 'csv_config_file',
+      defaultValue: 'select_all_points',
       children: [
-        {
-          label: '上传 CSV 配置文件',
-          name: 'csv_config_file',
-          labelShow: false,
-          labelWidth: '0px',
-          category: 'csv_config_file',
-          description:
-            'OPC 数据写入使用 csv 文件定义每一个数据点位到 TDengine 数据子表的映射规则：\n\n(1) point_id：必填，数据点位在 OPC UA 服务器上的 id；\n\n(2) stable：必填，数据点位对应的 TDengine 超级表；\n\n(3) tbname：必填，数据点位对应的 TDengine 子表；\n\n(4) enable：可选，默认值 \'1\'，指定是否采集该点位数据。0-不采集并且删除对应子表，1-采集点位数据，没有子表时创建子表；\n\n(5) value_col：可选，默认值 \'val\'。数据点位采集值在 TDengine 中对应的列名；\n\n(6) value_transform：可选，数据点位采集值在 taosX 中执行的变换函数，目前仅支持数值计算表达式，详见 transform 文档的 expr 表达式说明；\n\n(7) type：可选，默认值取源数据类型。数据点位采集值的数据类型，可用于替换超级表名称中的占位符 {type}；\n\n(8) quality_col：可选，数据点位采集值质量在 TDengine 中对应的列名；\n\n(9) ts_col/request_ts_col/received_ts_col：必填，TDengine 时间戳主键定义：可只保留其中一列，保留的时间戳列将作为主键；也可填写多列，居前的时间戳列作为主键；其中 ts_col 使用数据点位上报 opc server 时间，request_ts_col 使用 observe 采集模式下每次轮询的发起请求时间，received_ts_col 使用从 opc server 接收到数据的时间；\n\n(10) xx_ts_transform：可选，时间戳变换函数，参考 transform 数值计算表达式 expr 的说明；\n\n(11) tag::VARCHAR(200)::name：可选/可配置多个tag列；数据点位在 TDengine 中对应的 Tag 列；其中 tag 为保留关键字，表示该列为一个 tag 列；VARCHAR(200) 表示该 tag 的类型，也可以是其它合法的类型；name 是该 tag 的列名。\n\n更多填写规则请参考<a target="_blank" href="/docs/advanced/data-in/opcua/">企业版文档</a>。\n',
-          field: 'csv_config_file',
-          type: 'dataset',
-          accept: '.csv',
-          templateUrl: 'template-zh.csv',
-          placeholder: '上传 CSV 配置文件，定义数据点位到 TDengine 数据子表的映射规则。\n',
-          required: true,
-          requiredDependsOn: ['datasets/currentTab'],
-          requiredDependsOnValues: {
-            currentTab: ['csv_config_file']
-          },
-          multiple: true,
-          editable: true,
-          selectable: true,
-          defaultValue: '',
-          info2: true
-        },
         {
           label: '选择数据点位',
           name: 'select_all_points',
           labelShow: false,
           labelWidth: '0px',
           category: 'select_all_points',
+          radio: true,
+          description: 'OPC UA 点位配置。\n',
           field: 'select_all_points',
           type: 'dataset',
           accept: '.csv',
@@ -273,7 +251,7 @@ export default {
                 type: 'str'
               },
               description: '从该节点开始遍历所有子节点。\n',
-              placeholder: '例如 ns=3;i=1001',
+              placeholder: '例如 ns=1;i=1001',
               label: '根节点 ID',
               field: 'root',
               defaultValue: '',
@@ -283,7 +261,7 @@ export default {
             {
               name: 'namespaces',
               display: '命名空间',
-              description: '支持多选,只查询这些 namespace 下的数据点位。\n',
+              description: '支持多选，只查询这些 namespace 下的数据点位。\n',
               multiple: true,
               placeholder: '连通性检查通过后，可选择，支持多选',
               label: '命名空间',
@@ -291,14 +269,40 @@ export default {
               type: 'namespace'
             },
             {
-              name: 'node_id_pattern',
-              display: '节点 ID',
-              // "if": "!pattern",
+              name: 'opc_points_mode',
+              display: '节点类型',
               hint: {
-                type: 'str'
+                type: 'str',
+                choices: ['all', 'variable', 'object']
               },
+              description:
+                '按 NodeClass 过滤节点，支持 Variable、Object 和 All。All 表示所有类型。默认值为 `Variable`。\n',
+              label: '节点类型',
+              required: false,
+              field: 'opc_points_mode',
+              defaultValue: 'all',
+              multiple: false,
+              type: 'select',
+              options: [
+                {
+                  label: 'all',
+                  value: 'all'
+                },
+                {
+                  label: 'variable',
+                  value: 'variable'
+                },
+                {
+                  label: 'object',
+                  value: 'object'
+                }
+              ]
+            },
+            {
+              name: 'node_id_pattern',
+              display: '节点 ID 正则表达式',
               description: '数据点位 id 需要满足设置的正则表达式。\n',
-              label: '节点 ID',
+              label: '节点 ID 表达式',
               field: 'node_id_pattern',
               defaultValue: '',
               multiple: false,
@@ -306,25 +310,18 @@ export default {
             },
             {
               name: 'browse_name_pattern',
-              display: '节点名称',
-              // "if": "!pattern",
-              hint: {
-                type: 'str'
-              },
+              display: '节点名称表达式',
+              if: '!pattern',
               description: '数据点位名称需要满足设置的正则表达式。\n',
-              label: '节点名称',
+              label: '节点名称表达式',
               field: 'browse_name_pattern',
               defaultValue: '',
               multiple: false,
               type: 'pattern'
             },
-
             {
               name: 'super_table_expression',
               display: '超级表名称',
-              hint: {
-                type: 'str'
-              },
               description: '支持 `<super table prefix>_{type}` 格式，`{type}` 表示点位的数据类型。\n',
               required: true,
               value: 'opc_{type}',
@@ -337,31 +334,45 @@ export default {
             {
               name: 'child_table_expression',
               display: '表名称',
-              hint: {
-                type: 'str'
-              },
               description:
-                '支持 `<child table prefix>_{ns}_{id}` 格式，`{ns}` 表示点位的namespace，`{id}` 为点位的 id。比如：点位的 point_id 为`ns=3;i=1001`，那么`{ns}`为3，`{id}`为1001。',
+                '支持 `<child table prefix>_{ns}_{id}` 格式，`{ns}` 表示点位的 namespace，`{id}` 为点位的 id。例如：当点位的 point_id 为 `ns=3;i=1001`，则 `{ns}` 为 3，`{id}` 为 1001。\n',
               required: true,
-              value: 't_{ns}_{id}',
               label: '表名称',
               field: 'child_table_expression',
-              defaultValue: 't_{ns}_{id}',
+              defaultValue: 't_{ns}_{id#/_}',
+              multiple: false,
+              type: 'input'
+            },
+            {
+              name: 'value_col',
+              display: '值列名称',
+              description: '指定目标 TSDB 表中值列的名称。例如 `value_col=val` 表示将值列名称设置为 `val`。\n',
+              required: false,
+              label: '值列名称',
+              field: 'value_col',
+              defaultValue: 'val',
+              multiple: false,
+              type: 'input'
+            },
+            {
+              name: 'value_transform',
+              display: '值变换',
+              description:
+                '对写入 TSDB 的 `value` 进行变换表达式。例如：`value_transform=(val-32)/1.8` 表示按表达式计算。\n',
+              required: false,
+              label: '值变换',
+              field: 'value_transform',
+              defaultValue: '',
               multiple: false,
               type: 'input'
             },
             {
               name: 'table_primary_key',
-              display: '主键列',
-              hint: {
-                type: 'str',
-                choices: ['original_ts', 'request_ts', 'received_ts']
-              },
+              display: '时间戳列',
               description:
-                '目标数据表主键将使用选择的值作为时间戳主键列，original_ts 表示使用数据点位上报 opc server 时间，request_ts 是 observe 采集模式下每次轮询的发起请求时间，received_ts 表示从 opc server 接收到数据的时间。\n',
+                '在目标数据表中作为时间戳列使用。original_ts 表示使用数据点位上报到 OPC server 的时间；request_ts 表示 observe 采集模式下每次轮询的发起请求时间；received_ts 表示从 OPC server 接收到数据的时间。\n',
               required: false,
-              value: 'original_ts',
-              label: '主键列',
+              label: '时间戳列',
               field: 'table_primary_key',
               defaultValue: 'original_ts',
               multiple: false,
@@ -384,9 +395,6 @@ export default {
             {
               name: 'table_primary_key_alias',
               display: '主键别名',
-              hint: {
-                type: 'str'
-              },
               description: '在目标数据表中的主键列名称。\n',
               required: false,
               value: 'ts',
@@ -399,19 +407,43 @@ export default {
             {
               name: 'custom_tags',
               display: '自定义标签',
-              hint: {
-                type: 'str'
-              },
               description:
-                '可以配置多个自定义标签，以";"分隔。每个自定义标签的格式为：<TagType>::<TagName>::<TagPattern>，以"::"做分隔符。第一项是 Tag 的数据类型，第二项是 Tag 的名称，第三项是 Tag 值的表达式。\n',
+                '可以配置多个自定义标签，使用逗号分隔。支持静态值和从 OPC 点位属性提取的动态值。例如：`location=building1,floor={BrowseName}`，其中 `{BrowseName}` 将替换为该点位的实际 BrowseName 属性。\n',
+              required: false,
               label: '自定义标签',
               field: 'custom_tags',
-              defaultValue: 'VARCHAR(1024)::name::{id#/.}',
+              defaultValue:
+                'VARCHAR(1024)::name::{id#/.};VARCHAR(1024)::BrowseName::{BrowseName};VARCHAR(1024)::DisplayName::{DisplayName};VARCHAR(1024)::Description::{Description};VARCHAR(1024)::Path::{Path}',
               multiple: false,
               type: 'input'
             }
           ],
           defaultValue: ''
+        },
+        {
+          label: '上传 CSV 配置文件',
+          name: 'csv_config_file',
+          labelShow: false,
+          labelWidth: '0px',
+          category: 'csv_config_file',
+          radio: false,
+          description:
+            'OPC 数据写入使用 csv 文件定义每一个数据点位到 TDengine 数据子表的映射规则：\n\n(1) point_id：必填，数据点位在 OPC UA 服务器上的 id；\n\n(2) stable：必填，数据点位对应的 TDengine 超级表；\n\n(3) tbname：必填，数据点位对应的 TDengine 子表；\n\n(4) enable：可选，默认值 \'1\'，指定是否采集该点位数据。0-不采集并且删除对应子表，1-采集点位数据，没有子表时创建子表；\n\n(5) value_col：可选，默认值 \'val\'。数据点位采集值在 TDengine 中对应的列名；\n\n(6) value_transform：可选，数据点位采集值在 taosX 中执行的变换函数，目前仅支持数值计算表达式，详见 transform 文档的 expr 表达式说明；\n\n(7) type：可选，默认值取源数据类型。数据点位采集值的数据类型，可用于替换超级表名称中的占位符 {type}；\n\n(8) quality_col：可选，数据点位采集值质量在 TDengine 中对应的列名；\n\n(9) ts_col/request_ts_col/received_ts_col：必填，TDengine 时间戳主键定义：可只保留其中一列，保留的时间戳列将作为主键；也可填写多列，居前的时间戳列作为主键；其中 ts_col 使用数据点位上报 opc server 时间，request_ts_col 使用 observe 采集模式下每次轮询的发起请求时间，received_ts_col 使用从 opc server 接收到数据的时间；\n\n(10) xx_ts_transform：可选，时间戳变换函数，参考 transform 数值计算表达式 expr 的说明；\n\n(11) tag::VARCHAR(200)::name：可选/可配置多个 tag 列；数据点位在 TDengine 中对应的 Tag 列；其中 tag 为保留关键字，表示该列为一个 tag 列；VARCHAR(200) 表示该 tag 的类型，也可以是其它合法的类型；name 是该 tag 的列名。\n\n更多填写规则请参考<a target="_blank" href="/docs/advanced/data-in/opcua/">企业版文档</a>。\n',
+          field: 'csv_config_file',
+          type: 'dataset',
+          accept: '.csv',
+          templateUrl: 'template-zh.csv',
+          placeholder: '上传 CSV 配置文件，定义数据点位到 TDengine 数据子表的映射规则。\n',
+          required: true,
+          requiredDependsOn: ['datasets/currentTab'],
+          requiredDependsOnValues: {
+            currentTab: ['csv_config_file']
+          },
+          multiple: true,
+          editable: true,
+          selectable: true,
+          defaultValue: '',
+          info2: true
         }
       ]
     },
@@ -543,7 +575,7 @@ export default {
     {
       label: '高级选项',
       field: 'advanced_options',
-      description: '对数据源性能、日志等其他参数进行调整，可修改以下选项。\n',
+      description: '包括读写并发、采集选项、性能调优等的高级选项。通常保持默认即可使用推荐配置。\n',
       type: 'collapse',
       defaultValue: true,
       collapsible: 'one',
@@ -585,8 +617,8 @@ export default {
         {
           label: '最大写入并发数',
           field: 'write_concurrency',
-          description: '写入 taosX 的最大并发数限制，当默认参数性能不足时，可增大此参数。\n',
-          defaultValue: '0',
+          description: '单次并发写入的最大数量。默认值由采集器自动设置。如果数据源响应较慢，可适当增大此值。\n',
+          defaultValue: 0,
           required: false,
           hint: {
             type: 'integer',
@@ -600,7 +632,7 @@ export default {
         {
           label: '批次大小',
           field: 'batch_size',
-          description: '单次发送的最大消息数或行数。\n',
+          description: '单次请求写入的最大点位数量。默认值为 1000。如果数据源响应较慢，可适当减小此值。\n',
           defaultValue: 1000,
           required: false,
           hint: {
@@ -615,7 +647,7 @@ export default {
         {
           label: '批次延时',
           field: 'batch_timeout',
-          description: '单次读取最大延时（单位为秒），当超时结束时，只要有数据，即使不满足 Batch Size，也立即发送。\n',
+          description: '发送批次数据前的最大等待时间（单位：秒）。默认值为 1s。当数据源响应较慢时，可适当增大此值。\n',
           defaultValue: 1,
           required: false,
           hint: {
@@ -634,6 +666,9 @@ export default {
             '开启后，当 taosX 由于性能不足或者下游 TDengine 写入慢时，会将实时数据暂存，等恢复时再将缓存数据重新写入下游 TDengine.\n',
           defaultValue: false,
           required: false,
+          hint: {
+            type: 'bool'
+          },
           type: 'switch'
         },
         {
@@ -663,7 +698,7 @@ export default {
           label: '最大保留天数',
           field: 'keep_raw_data_days',
           description: '原始数据最大保存天数，默认 1 天。\n',
-          defaultValue: '1',
+          defaultValue: 1,
           required: false,
           type: 'number',
           min: 1,
@@ -679,6 +714,9 @@ export default {
           description: '自定义原始数据存储目录，默认存储到系统数据目录下。\n',
           placeholder: '$DATA_DIR/tasks/:id/rawdata/',
           required: false,
+          hint: {
+            type: 'str'
+          },
           type: 'input',
           displayDependsOn: ['advanced_options/keep_raw_data'],
           displayDependsOnValues: {
@@ -759,7 +797,7 @@ export default {
           label: '写入错误阈值',
           field: 'max_errors_in_window',
           description: '表示健康监测时段中允许写入错误的数量。超出阈值，则发送 Fatal 警告。',
-          defaultValue: '10',
+          defaultValue: 10,
           required: false,
           hint: {
             type: 'integer',
