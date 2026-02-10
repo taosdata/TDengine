fb29fac1e44e49c2688a23769f0e49c8e54a29c0
yihaoDeng <luomoxyz@126.com>
2026-02-10 20:24:12 +0800

    opt validate virtual table

--- source/common/src/systable.c
+++ source/common/src/systable.c
@@ -749,7 +749,7 @@ static const SSysDbTableSchema virtualTablesReferencing[] = {
     {.name = "src_column_name", .bytes = SYSTABLE_SCH_COL_NAME_LEN , .type = TSDB_DATA_TYPE_VARCHAR, .sysInfo = false},
     {.name = "type", .bytes = 4, .type = TSDB_DATA_TYPE_INT, .sysInfo = false},
     {.name = "err_code", .bytes = 8, .type = TSDB_DATA_TYPE_BIGINT, .sysInfo = false},
-    {.name = "err_msg", .bytes = 8, .type = TSDB_DATA_TYPE_BIGINT, .sysInfo = false},
+    {.name = "err_msg", .bytes = TSDB_SHOW_VALIDATE_VIRTUAL_TABLE_ERROR, .type = TSDB_DATA_TYPE_VARCHAR, .sysInfo = false},
 };
 
 
