8b3f4a99b364f90238eef15b218ac672dd9b5e86
facetosea <285808407@qq.com>
2026-02-27 17:33:13 +0800

    fix: test case

--- test/cases/13-TimeSeriesExt/08-ExternalWindow/test_external.py
+++ test/cases/13-TimeSeriesExt/08-ExternalWindow/test_external.py
@@ -3,10 +3,8 @@ from new_test_framework.utils import tdLog, tdSql, sc, clusterComCheck
 
 class TestExternal:
 
-    def setup_class(cls):
-        tdLog.debug(f"start to execute {__file__}")
-
     def test_External(self):
+        tdLog.debug(f"start to execute {__file__}")
         """External basic
 
         1. Test the basic usage of External window
@@ -35,6 +33,7 @@ class TestExternal:
         self.prepareData()
 
         self.basic_query()
+        self.partition_by_group_regression()
 
     def prepareData(self):
         tdSql.execute(f"use {self.dbName}")
@@ -66,8 +65,7 @@ class TestExternal:
         # todo xs fix external window column placeholder issue
         # select _wstart, _wend, w.fc1 + 1, ts from st1_1 external_window((select first(c1) fc1  from st2) w);
         
-        select _wstart, _wend, w.fc1, count(*) from st1_1 external_window((select first(c1) fc1  from st2) w); 
-        
+        # select _wstart, _wend, w.fc1, count(*) from st1_1 external_window((select first(c1) fc1  from st2) w); 
         # select _wstart, _wend, w.fc1, count(*) from st1_1 external_window((select ts, ts+1, first(c1) fc1 from st2) w);
         # select _wstart, _wend, w.fc1, ts from st1_1 external_window((select ts, ts+1, first(c1) fc1 from st2) w);
         # select _wstart, _wend, _wduration, ts from st1_1 external_window((select ts, ts+1, first(c1) c1 from st2) w);
@@ -82,6 +80,61 @@ class TestExternal:
         # select _wstart, count(*) from st1 external_window((select ts, ts+10 from st2) w);
         # select _wstart, count(*) from st1 external_window((select ts, ts+10 from st2 interval(2m)) w);
         # select _wstart, count(*) from st1 external_window((select _wstart, _wend from st2 interval(2m)) w);
+        
+        # todo xsren: 从超级表查询
+        
+        # 投影查询 + patition by , 自带 ts
+        # select _wstart, _wend, w.fc1 as fc1, v2 from st1_1 partition by v2 external_window((select first(c1) fc1  from st2) w);
+        # todo 投影查询 + patition by , 不带 ts，有问题，需要修复，通过给 partition 算子增加 ts 列解决
+        # select _wstart, _wend, w.fc1 as fc1, v2, ts from st1_1 partition by v2 external_window((select first(c1) fc1  from st2) w);
         return
     
         # explain verbose true select count(*) from st1_1 external_window((select ts, ts+10, first(c1) c1 from st2) w) \G;
+
+    def partition_by_group_regression(self):
+        tdLog.info("=============== regression: partition by + external window group calculation")
+
+        tdSql.execute(f"use {self.dbName}")
+
+        tdSql.execute("drop table if exists ext_src")
+        tdSql.execute("drop table if exists ext_win")
+
+        tdSql.execute("create table ext_src (ts timestamp, v int) tags(g int)")
+        tdSql.execute("create table ext_win (ts timestamp, v int) tags(g int)")
+
+        tdSql.execute("create table ext_src_1 using ext_src tags(1)")
+        tdSql.execute("create table ext_src_2 using ext_src tags(2)")
+        tdSql.execute("create table ext_win_1 using ext_win tags(1)")
+
+        t0 = 1700000000000
+
+        tdSql.execute(f"insert into ext_win_1 values({t0}, 1)({t0 + 600000}, 1)")
+
+        tdSql.execute(f"insert into ext_src_1 values({t0 + 60000}, 10)({t0 + 120000}, 11)")
+        tdSql.execute(f"insert into ext_src_2 values({t0 + 660000}, 20)")
+
+        sql = (
+            "select tbname, cast(_wstart as bigint) as ws, count(*) as c "
+            "from ext_src partition by tbname "
+            "external_window((select _wstart, _wend, count(*) as wc from ext_win interval(10m)) w) "
+            "order by tbname, ws"
+        )
+
+        tdSql.query(sql)
+        tdSql.checkRows(4)
+
+        tdSql.checkData(0, 0, "ext_src_1")
+        tdSql.checkData(0, 1, t0)
+        tdSql.checkData(0, 2, 2)
+
+        tdSql.checkData(1, 0, "ext_src_1")
+        tdSql.checkData(1, 1, t0 + 600000)
+        tdSql.checkData(1, 2, 0)
+
+        tdSql.checkData(2, 0, "ext_src_2")
+        tdSql.checkData(2, 1, t0)
+        tdSql.checkData(2, 2, 0)
+
+        tdSql.checkData(3, 0, "ext_src_2")
+        tdSql.checkData(3, 1, t0 + 600000)
+        tdSql.checkData(3, 2, 1)
