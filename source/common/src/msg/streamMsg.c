b97c93569e152d746a0136300ebf3551678242a1
Minglei Jin <mljin@taosdata.com>
2026-02-28 14:58:46 +0800

    remove calc scan plan freeing

--- source/common/src/msg/streamMsg.c
+++ source/common/src/msg/streamMsg.c
@@ -2199,6 +2199,15 @@ void tFreeSStmStreamDeploy(void* param) {
   }
   
   SStmStreamDeploy* pDeploy = (SStmStreamDeploy*)param;
+  /*
+  int32_t readerNum = taosArrayGetSize(pDeploy->readerTasks);
+  for (int32_t i = 0; i < readerNum; ++i) {
+    SStmTaskDeploy* pReader = taosArrayGet(pDeploy->readerTasks, i);
+    if (!pReader->msg.reader.triggerReader) {
+      taosMemoryFreeClear(pReader->msg.reader.msg.calc.calcScanPlan);
+    }
+  }
+  */
   taosArrayDestroy(pDeploy->readerTasks);
 
   if (pDeploy->triggerTask) {
@@ -2221,13 +2230,6 @@ void tDeepFreeSStmStreamDeploy(void* param) {
   }
   
   SStmStreamDeploy* pDeploy = (SStmStreamDeploy*)param;
-  int32_t readerNum = taosArrayGetSize(pDeploy->readerTasks);
-  for (int32_t i = 0; i < readerNum; ++i) {
-    SStmTaskDeploy* pReader = taosArrayGet(pDeploy->readerTasks, i);
-    if (!pReader->msg.reader.triggerReader) {
-      taosMemoryFreeClear(pReader->msg.reader.msg.calc.calcScanPlan);
-    }
-  }
   taosArrayDestroyEx(pDeploy->readerTasks, tFreeSStmTaskDeploy);
   tFreeSStmTaskDeploy(pDeploy->triggerTask);
   taosMemoryFree(pDeploy->triggerTask);
