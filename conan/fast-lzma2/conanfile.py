62dac9d6286b982f5410547f980dbc647ae44f7b
Linhe Huo <linhehuo@gmail.com>
2026-02-28 13:28:37 +0800

    chore: fix fast-lzma2 build error in macos

--- conan/fast-lzma2/conanfile.py
+++ conan/fast-lzma2/conanfile.py
@@ -1,5 +1,5 @@
 from conan import ConanFile
-from conan.tools.files import copy, get
+from conan.tools.files import copy, get, replace_in_file
 import os
 
 
@@ -47,6 +47,46 @@ class FastLzma2Conan(ConanFile):
     def build(self):
         # Enter source code directory
         source_folder = self.source_folder
+
+        # Patch upstream Makefile for macOS (Darwin ld doesn't support ELF-style -soname)
+        makefile = os.path.join(source_folder, "Makefile")
+        if os.path.isfile(makefile) and self.settings.os == "Macos":
+            # Build a proper dylib when shared=True, and make the linker invocation portable.
+            replace_in_file(
+                self,
+                makefile,
+                "SONAME:=libfast-lzma2.so.1",
+                "SONAME:=libfast-lzma2.1.dylib",
+                strict=False,
+            )
+            replace_in_file(
+                self,
+                makefile,
+                "REAL_NAME:=libfast-lzma2.so.1.0",
+                "REAL_NAME:=$(SONAME)",
+                strict=False,
+            )
+            replace_in_file(
+                self,
+                makefile,
+                "LINKER_NAME=libfast-lzma2.so",
+                "LINKER_NAME=libfast-lzma2.dylib",
+                strict=False,
+            )
+            replace_in_file(
+                self,
+                makefile,
+                "-Wl,-soname,$(SONAME)",
+                "-Wl,-install_name,@rpath/$(SONAME)",
+                strict=False,
+            )
+            replace_in_file(
+                self,
+                makefile,
+                " -shared -pthread ",
+                " -dynamiclib -pthread ",
+                strict=False,
+            )
 
         # Build make command
         cflags = "-Wall -O2 -pthread"
