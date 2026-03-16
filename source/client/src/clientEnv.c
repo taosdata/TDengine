82d042557775bc0e833a22196e63a77363707730
dapan1121 <wpan@taosdata.com>
2026-03-16 17:41:44 +0800

    fix: windows issues

--- source/client/src/clientEnv.c
+++ source/client/src/clientEnv.c
@@ -1122,7 +1122,7 @@ void taos_init_imp(void) {
     return;
   }
 #endif
-#if !defined(WINDOWS) && !defined(TD_ASTRA)
+#if !defined(TD_ASTRA)
   ENV_ERR_RET(tzInit(), "failed to init timezone");
 #endif
   ENV_ERR_RET(monitorInit(), "failed to init monitor");
