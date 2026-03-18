52145174fe01ca524453c140351eea3c0d2d6589
dmchen <cademfly@hotmail.com>
2026-03-18 09:31:22 +0800

    fix/6921454687-improve-election-log

--- source/libs/sync/src/syncRaftStore.c
+++ source/libs/sync/src/syncRaftStore.c
@@ -140,23 +140,37 @@ bool raftStoreHasVoted(SSyncNode *pNode) {
 }
 
 void raftStoreVote(SSyncNode *pNode, SRaftId *pRaftId) {
+  int32_t code = 0;
+  SRaftId voteForCopy;
+  int32_t vgId;
+
   (void)taosThreadMutexLock(&pNode->raftStore.mutex);
   pNode->raftStore.voteFor = *pRaftId;
-  int32_t code = 0;
-  if ((code = raftStoreWriteFile(pNode)) != 0) {
-    sError("vgId:%d, failed to write raft store file since %s", pNode->vgId, tstrerror(code));
-  }
+  voteForCopy = *pRaftId;
+  vgId = pNode->vgId;
+  code = raftStoreWriteFile(pNode);
   (void)taosThreadMutexUnlock(&pNode->raftStore.mutex);
+  if (code == 0) {
+    sInfo("vgId:%d, succeed to store vote, voteFor:0x%" PRIx64, vgId, voteForCopy.addr);
+  } else {
+    sError("vgId:%d, failed to store vote since %s", vgId, tstrerror(code));
+  }
 }
 
 void raftStoreClearVote(SSyncNode *pNode) {
+  int32_t code = 0;
+  int32_t vgId;
+
   (void)taosThreadMutexLock(&pNode->raftStore.mutex);
   pNode->raftStore.voteFor = EMPTY_RAFT_ID;
-  int32_t code = 0;
-  if ((code = raftStoreWriteFile(pNode)) != 0) {
-    sError("vgId:%d, failed to write raft store file since %s", pNode->vgId, tstrerror(code));
-  }
+  vgId = pNode->vgId;
+  code = raftStoreWriteFile(pNode);
   (void)taosThreadMutexUnlock(&pNode->raftStore.mutex);
+  if (code == 0) {
+    sInfo("vgId:%d, succeed to clear vote", vgId);
+  } else {
+    sError("vgId:%d, failed to clear vote since %s", vgId, tstrerror(code));
+  }
 }
 
 void raftStoreNextTerm(SSyncNode *pNode) {
