0b7ad9558e618c6bd776b88e4dc50df4d1547b9d
Jinqing Kuang <kuangjinqingcn@gmail.com>
2026-03-30 19:04:03 +0800

    fix(stream): fix sliding value check for stream

--- source/libs/parser/src/parTranslater.c
+++ source/libs/parser/src/parTranslater.c
@@ -9082,6 +9082,9 @@ static int32_t checkStreamIntervalWindow(STranslateContext* pCxt, SIntervalWindo
     if (IS_CALENDAR_TIME_DURATION(pSliding->unit)) {
       PAR_ERR_RET(generateSyntaxErrMsg(&pCxt->msgBuf, TSDB_CODE_PAR_INTER_SLIDING_UNIT));
     }
+    if (pInter == NULL && pSliding->datum.i == 0) {
+      PAR_ERR_RET(generateSyntaxErrMsgExt(&pCxt->msgBuf, TSDB_CODE_PAR_INTER_VALUE_TOO_SMALL, "sliding value must be greater than 0"));
+    }
   } else {
     PAR_ERR_RET(generateSyntaxErrMsgExt(&pCxt->msgBuf, TSDB_CODE_PAR_INVALID_STREAM_QUERY,
                                         "Sliding window is required for stream query"));
