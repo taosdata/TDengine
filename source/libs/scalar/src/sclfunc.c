642d563375b26b1f4f68b424c715e1a3efd67040
stephenkgu <mljin@taosdata.com>
2026-04-10 03:07:18 -0700

    fix inf ub issue

--- source/libs/scalar/src/sclfunc.c
+++ source/libs/scalar/src/sclfunc.c
@@ -4499,10 +4499,10 @@ int32_t sleepFunction(SScalarParam *pInput, int32_t inputNum, SScalarParam *pOut
                    typeGetTypeModFromColInfo(&pInput[0].columnData->info));
 
     int32_t result = 0;
-    if (sleepSec < 0) {
+    if (sleepSec < 0 || isnan(sleepSec)) {
       result = 1;
-    } else if (sleepSec > 0) {
-      int64_t totalMs = (int64_t)(sleepSec * 1000);
+    } else if (sleepSec > 0 && !isinf(sleepSec)) {
+      int64_t totalMs = (int64_t)(TMIN(sleepSec, (double)(INT64_MAX / 1000)) * 1000);
       int64_t elapsed = 0;
       while (elapsed < totalMs) {
         if (gTaskScalarExtra.isTaskKilled && gTaskScalarExtra.isTaskKilled(gTaskScalarExtra.pTaskInfo)) {
