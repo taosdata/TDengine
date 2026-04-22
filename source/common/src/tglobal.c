50b5164d8a12e20b7d34c68b4fff5e61bf6271e9
xiao-77 <berylbao@taosdata.com>
2026-04-22 11:30:45 +0800

    feat(wal): add walRecoveryPolicy config parameter
    - Add tsWalRecoveryPolicy global variable (default 0)
    - Only affects single replica: 0=refuse, 1=delete corrupted
    - Three replicas always auto-recover regardless of this config

--- source/common/src/tglobal.c
+++ source/common/src/tglobal.c
@@ -357,6 +357,7 @@ bool    tsStartUdfd = true;
 int64_t tsWalFsyncDataSizeLimit = (100 * 1024 * 1024L);
 bool    tsWalForceRepair = 0;
 bool    tsWalDeleteOnCorruption = false;
+int32_t tsWalRecoveryPolicy = 0;  // Default: refuse to start for single replica
 
 // ttl
 bool    tsTtlChangeOnWrite = false;  // if true, ttl delete time changes on last write
@@ -982,6 +983,7 @@ static int32_t taosAddServerCfg(SConfig *pCfg) {
   TAOS_CHECK_RETURN(cfgAddInt32(pCfg, "syncRoutineReportInterval", tsRoutineReportInterval, 5, 600, CFG_SCOPE_SERVER, CFG_DYN_SERVER,CFG_CATEGORY_LOCAL));
   TAOS_CHECK_RETURN(cfgAddBool(pCfg, "syncLogHeartbeat", tsSyncLogHeartbeat, CFG_SCOPE_SERVER, CFG_DYN_SERVER,CFG_CATEGORY_LOCAL));
   TAOS_CHECK_RETURN(cfgAddBool(pCfg, "walDeleteOnCorruption", tsWalDeleteOnCorruption, CFG_SCOPE_SERVER, CFG_DYN_NONE,CFG_CATEGORY_LOCAL));
+  TAOS_CHECK_RETURN(cfgAddInt32(pCfg, "walRecoveryPolicy", tsWalRecoveryPolicy, 0, 1, CFG_SCOPE_SERVER, CFG_DYN_NONE, CFG_CATEGORY_LOCAL));
 
   TAOS_CHECK_RETURN(cfgAddInt32(pCfg, "syncTimeout", tsSyncTimeout, 0, 60 * 24 * 2 * 1000, CFG_SCOPE_SERVER, CFG_DYN_SERVER,CFG_CATEGORY_GLOBAL));
 
@@ -2145,6 +2147,9 @@ static int32_t taosSetServerCfg(SConfig *pCfg) {
 
   TAOS_CHECK_GET_CFG_ITEM(pCfg, pItem, "walDeleteOnCorruption");
   tsWalDeleteOnCorruption = pItem->bval;
+
+  TAOS_CHECK_GET_CFG_ITEM(pCfg, pItem, "walRecoveryPolicy");
+  tsWalRecoveryPolicy = pItem->i32;
 
   TAOS_RETURN(TSDB_CODE_SUCCESS);
 }
