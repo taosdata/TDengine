00eedd8229c8ff3affe3d7297e009c0a19d1f9cb
Simon Guan <slguan@taosdata.com>
2026-05-12 17:02:54 +0800

    refactor: shrink SBuiltinFuncDefinition to reduce binary size by 17MB per executable

--- source/libs/function/inc/builtins.h
+++ source/libs/function/inc/builtins.h
@@ -31,7 +31,9 @@ typedef EFuncDataRequired (*FFuncDynDataRequired)(void* pRes, SDataBlockInfo* pB
 typedef EFuncReturnRows (*FEstimateReturnRows)(SFunctionNode* pFunc);
 
 #define MAX_FUNC_PARA_NUM             16
-#define MAX_FUNC_PARA_FIXED_VALUE_NUM 16
+#define MAX_FUNC_PARA_PATTERN_NUM     2
+#define MAX_FUNC_INPUT_PARA_NUM       8
+#define MAX_FUNC_PARA_FIXED_VALUE_NUM 8
 
 
 typedef struct SParamRange {
@@ -57,7 +59,7 @@ typedef struct SFunctionParaInfo {
   int8_t     minParamNum;
   int8_t     maxParamNum;
   uint8_t    paramInfoPattern;
-  SParamInfo inputParaInfo[MAX_FUNC_PARA_NUM][MAX_FUNC_PARA_NUM];
+  SParamInfo inputParaInfo[MAX_FUNC_PARA_PATTERN_NUM][MAX_FUNC_INPUT_PARA_NUM];
   SParamInfo outputParaInfo;
 } SFunctionParaInfo;
 
