70a5c34f6ce6010d6ef1cba91a8b0c311f4637a2
chen dongming <dmchen@taosdata.com>
2026-05-20 18:40:12 +0800

    feat: add vgId parameter to missing file list deserialization functions

--- source/dnode/vnode/src/vnd/vnodeSnapshot.c
+++ source/dnode/vnode/src/vnd/vnodeSnapshot.c
@@ -150,7 +150,7 @@ static int32_t vnodeSnapReaderDealWithSnapInfo(SVSnapReader *pReader, SSnapshotP
           void   *missingFiles = NULL;
           int32_t missingFileCount = 0;
           code = tDeserializeMissingFileList(buf, bufLen, &missingFiles, &missingFileCount, &pReader->missingFileHash,
-                                             &pReader->missingSttHash);
+                                             &pReader->missingSttHash, TD_VID(pVnode));
           if (code) {
             vError("vgId:%d, failed to deserialize missing file list since %s", TD_VID(pVnode), tstrerror(code));
             goto _out;
