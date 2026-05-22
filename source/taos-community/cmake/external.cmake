b3a231291a91418d2350b99851e6302f84dd3573
bxiao <bxiao@taosdata.com>
2026-05-22 14:58:42 +0000

    fix(cmake): guard DOWNLOAD_EXTRACT_TIMESTAMP for CMake < 3.24 and fix duplicate Cargo.lock entry (rd-public/tsdb!371)

--- source/taos-community/cmake/external.cmake
+++ source/taos-community/cmake/external.cmake
@@ -1606,10 +1606,14 @@ get_from_local_if_exists(
     "https://github.com/taosdata/libs3/archive/f727a1e5da21ed518c323a849dda70d39ccfe647.tar.gz"
     "libs3-f727a1e5da21.tar.gz"
 )
+set(_libs3_ts_args "")
+if(CMAKE_VERSION VERSION_GREATER_EQUAL "3.24")
+    list(APPEND _libs3_ts_args DOWNLOAD_EXTRACT_TIMESTAMP TRUE)
+endif()
 ExternalProject_Add(ext_libs3
     URL ${_url}
     URL_HASH SHA256=008ce6c8881b84313b22025303b0076b75a2da9a94e7cb255e25ec39d01b096c
-    DOWNLOAD_EXTRACT_TIMESTAMP TRUE
+    ${_libs3_ts_args}
     DEPENDS ${_libs3_depends}
     PREFIX "${_base}"
     CMAKE_ARGS -DCMAKE_BUILD_TYPE:STRING=${TD_CONFIG_NAME}
@@ -2031,10 +2035,14 @@ else()
         "-DCMAKE_C_FLAGS_RELWITHDEBINFO:STRING=/Zi /O1 /Ob1 /DNDEBUG /Gy /Gw"
     )
 endif()
+set(_arrow_ts_args "")
+if(CMAKE_VERSION VERSION_GREATER_EQUAL "3.24")
+    list(APPEND _arrow_ts_args DOWNLOAD_EXTRACT_TIMESTAMP TRUE)
+endif()
 ExternalProject_Add(ext_arrow
     URL ${_url}
     URL_HASH SHA256=4c898504958841cc86b6f8710ecb2919f96b5e10fa8989ac10ac4fca8362d86a
-    DOWNLOAD_EXTRACT_TIMESTAMP TRUE
+    ${_arrow_ts_args}
     PREFIX         "${_base}"
     SOURCE_SUBDIR  cpp
     CMAKE_ARGS -DCMAKE_BUILD_TYPE:STRING=${TD_CONFIG_NAME}
