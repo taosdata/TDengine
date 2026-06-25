a42c780c104acc9569419a16d976cd3888195bb4
hrchen <hrchen@taosdata.com>
2026-06-25 11:50:15 +0000

    ci: use internal PyPI group for TDGPT torch deps (rd-public/tsdb!944)

--- tools/tdgpt/tests/forecast_test.py
+++ tools/tdgpt/tests/forecast_test.py
@@ -13,6 +13,20 @@ from taosanalytics.log import setup_log_info
 
 class ForecastTest(unittest.TestCase):
     """forecast unit test cases"""
+    airline_passengers = [
+        112, 118, 132, 129, 121, 135, 148, 148, 136, 119, 104, 118,
+        115, 126, 141, 135, 125, 149, 170, 170, 158, 133, 114, 140,
+        145, 150, 178, 163, 172, 178, 199, 199, 184, 162, 146, 166,
+        171, 180, 193, 181, 183, 218, 230, 242, 209, 191, 172, 194,
+        196, 196, 236, 235, 229, 243, 264, 272, 237, 211, 180, 201,
+        204, 188, 235, 227, 234, 264, 302, 293, 259, 229, 203, 229,
+        242, 233, 267, 269, 270, 315, 364, 347, 312, 274, 237, 278,
+        284, 277, 317, 313, 318, 374, 413, 405, 355, 306, 271, 306,
+        315, 301, 356, 348, 355, 422, 465, 467, 404, 347, 305, 336,
+        340, 318, 362, 348, 363, 435, 491, 505, 404, 359, 310, 337,
+        360, 342, 406, 396, 420, 472, 548, 559, 463, 407, 362, 405,
+        417, 391, 419, 461, 472, 535, 622, 606, 508, 461, 390, 432,
+    ]
 
     @classmethod
     def setUpClass(cls):
@@ -21,15 +35,15 @@ class ForecastTest(unittest.TestCase):
         loader.register_all_services()
 
     def get_input_list(self):
-        """ load data from csv """
-        url = ('https://raw.githubusercontent.com/jbrownlee/Datasets/refs/heads/master/'
-               'airline-passengers.csv')
-        data = pd.read_csv(url, index_col='Month', parse_dates=True)
-        
-        ts_list = data[['Passengers']].index.tolist()
+        """Load deterministic forecast data without external network access."""
+        ts_list = pd.date_range(
+            start="1949-01-01",
+            periods=len(self.airline_passengers),
+            freq="MS",
+        ).tolist()
         dst_list = [int(item.timestamp()) for item in ts_list]
 
-        return data['Passengers'].values.tolist(), dst_list
+        return self.airline_passengers, dst_list
 
 
     def test_holt_winters_forecast(self):
